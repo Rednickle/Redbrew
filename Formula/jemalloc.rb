a1d63bff9f685bd9daf39a020be4850086245263
Shaun Jackman <sjackman@gmail.com>
2016-04-29 10:48:40 -0700

    jemalloc: update 4.0.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#61.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/jemalloc.rb
+++ Formula/jemalloc.rb
@@ -10,6 +10,7 @@ class Jemalloc < Formula
     sha256 "2d9d3b8a36e1ffda6d5f4c5e1fabd95f496e4562a75cafba6ae975faa49e9bcd" => :el_capitan
     sha256 "2167fc05024156684666e97527784c9a260db0c9308e604fb27fd314e4be70e7" => :yosemite
     sha256 "5f3dbdc9c6a55e0cd7b8f53a4f001f36937d7c2f6afe92fe8cd0ce3ea39b922f" => :mavericks
+    sha256 "3991e2dfbed8cec7600ea1c265a0037fe2931a0a115185d284798cb8a60fe03e" => :x86_64_linux
   end
 
   def install
