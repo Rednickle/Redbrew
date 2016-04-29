110ad8ddd986d01c41321c36ddf41061e24a0481
Shaun Jackman <sjackman@gmail.com>
2016-04-29 13:43:59 -0700

    readline: Bump revision for ncurses 6.0 for Linux
    The current bottle is linked against libncursesw.so.5
    
    Closes Linuxbrew/homebrew-core#76.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/readline.rb
+++ Formula/readline.rb
@@ -5,6 +5,7 @@ class Readline < Formula
   mirror "https://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz"
   version "6.3.8"
   sha256 "56ba6071b9462f980c5a72ab0023893b65ba6debb4eeb475d7a563dc65cafd43"
+  revision 1 if OS.linux?
 
   bottle do
     cellar :any
