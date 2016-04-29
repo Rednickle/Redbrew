d281ab51994fe62d932584aa85b30012e8cebfad
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-04-29 22:24:51 +0000

    jbigkit: update 2.1 bottle for Linuxbrew.

--- Formula/jbigkit.rb
+++ Formula/jbigkit.rb
@@ -11,6 +11,7 @@ class Jbigkit < Formula
     sha256 "764396342e87b84253aa06f5046f90c778cacca998ce970900cb2fdf1cfdc3fa" => :yosemite
     sha256 "0ce925915b984307d2e679622138143c5cc5baf832b0a16003fa1e6111a5df9f" => :mavericks
     sha256 "0afb6297101bc3269f0ebca1590cda66a62cbd90e3fdbec38dc011131711d32b" => :mountain_lion
+    sha256 "d7b87245e8682383dc10c4730d5248fabfab9c7ae5b76662da50a24785c0a710" => :x86_64_linux
   end
 
   head "https://www.cl.cam.ac.uk/~mgk25/git/jbigkit",
