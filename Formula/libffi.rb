10ad7546196022b6a621860f5e3311988e0a3be8
Shaun Jackman <sjackman@gmail.com>
2016-04-28 21:28:37 -0700

    libffi: update 3.0.13 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#39.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/libffi.rb
+++ Formula/libffi.rb
@@ -12,6 +12,7 @@ class Libffi < Formula
     sha256 "aa60d56351d36a45f2e7f16114fc17f9bd8fe805931f36d744c6ccb5fa5df238" => :mavericks
     sha256 "fad1fe049554d37471408fe451ef2e46628177c94eaafb23a3af56336603baad" => :mountain_lion
     sha256 "dc4718ebb77ff384386e0ef1782d8418c821637044b5dff7c08f21c401d0668d" => :lion
+    sha256 "318910b584301480336972ed21161aae751697c9564fc9fcf2c5f7ebfa73edc1" => :x86_64_linux
   end
 
   head do
