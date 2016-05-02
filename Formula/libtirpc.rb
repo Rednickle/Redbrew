28ae5eace9ea35ad6d62fa6366f0da10e5870c4d
Shaun Jackman <sjackman@gmail.com>
2016-05-01 21:39:50 -0700

    libtirpc: add 1.0.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#121.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/libtirpc.rb
+++ Formula/libtirpc.rb
@@ -5,6 +5,11 @@ class Libtirpc < Formula
   sha256 "5156974f31be7ccbc8ab1de37c4739af6d9d42c87b1d5caf4835dda75fcbb89e"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "7c093d0948f7794df11cae0a0f0e974e8b0734d46beb2f7977db39d84aa16720" => :x86_64_linux
+  end
+
   depends_on "homebrew/dupes/krb5" => :optional unless OS.mac?
 
   def install
