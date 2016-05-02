56913b6adb1ff419077c6d35d21e1378e28d78af
Shaun Jackman <sjackman@gmail.com>
2016-05-01 21:45:57 -0700

    strace: add 4.10 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#124.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/strace.rb
+++ Formula/strace.rb
@@ -1,12 +1,15 @@
 class Strace < Formula
   desc "A useful diagnostic, instructional, and debugging tool"
   homepage "http://sourceforge.net/projects/strace/"
-  # tag "linuxbrew"
-
   url "https://downloads.sourceforge.net/project/strace/strace/4.10/strace-4.10.tar.xz"
   sha256 "e6180d866ef9e76586b96e2ece2bfeeb3aa23f5cc88153f76e9caedd65e40ee2"
-
   head "git://strace.git.sourceforge.net/gitroot/strace/strace"
+  # tag "linuxbrew"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "038d8d843299fe9d92b858e5349354004d71c3aa14e4e1c91c076445ccbd3669" => :x86_64_linux
+  end
 
   depends_on "linux-headers"
 
