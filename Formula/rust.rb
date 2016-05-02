5dc2b0c4942c7d3555d01b06343bc79c2ddcce11
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2016-05-02 20:17:30 +0200

    rust: Build a bottle for Linuxbrew
    Closes Linuxbrew/homebrew-core#150.
    
    Signed-off-by: Piotr Gaczkowski <DoomHammerNG@gmail.com>

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -1,3 +1,4 @@
+# rust: Build a bottle for Linuxbrew
 class Rust < Formula
   desc "Safe, concurrent, practical language"
   homepage "https://www.rust-lang.org/"
@@ -54,6 +55,8 @@ class Rust < Formula
   end
 
   def install
+    # Reduce memory usage below 4 GB for Circle CI.
+    ENV["MAKEFLAGS"] = "-j12" if ENV["CIRCLECI"]
     args = ["--prefix=#{prefix}"]
     args << "--disable-rpath" if build.head?
     args << "--enable-clang" if ENV.compiler == :clang
