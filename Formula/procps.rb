c829db2bc63043060f7fa7b41baa96b9ece6f336
Shaun Jackman <sjackman@gmail.com>
2016-05-01 21:43:24 -0700

    procps: add 3.3.11 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#123.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/procps.rb
+++ Formula/procps.rb
@@ -2,10 +2,13 @@ class Procps < Formula
   desc "Utilities for browsing procfs"
   homepage "https://gitlab.com/procps-ng/procps"
   url "https://gitlab.com/procps-ng/procps/repository/archive.tar.gz?ref=v3.3.11"
-  version "3.3.11"
   sha256 "69e421cb07d5dfd38100b4b68714e9cb05d4fe58a7c5145c7b672d1ff08ca58b"
   head "https://gitlab.com/procps-ng/procps.git"
   # tag "linuxbrew"
+
+  bottle do
+    sha256 "96622dce78d108700b6a7b55c745eba71f923da26ed8d7466eb6109b02234c62" => :x86_64_linux
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
