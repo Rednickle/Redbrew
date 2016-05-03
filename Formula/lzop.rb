8765583de70a4f514e1b86c21702510506bf3cd2
Bob W. Hogg <rwhogg@linux.com>
2016-05-02 21:56:40 -0400

    lzop: update 1.03 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#137.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/lzop.rb
+++ Formula/lzop.rb
@@ -9,6 +9,7 @@ class Lzop < Formula
     sha256 "441a5396549a826b805a181f3b7c6115ee81714acd06978ebf18396ab2fda717" => :el_capitan
     sha256 "8948b68bd5f6ca91371dfbd3522c60015f1010e389ff138e12460bec3ade52b6" => :yosemite
     sha256 "dd86655bbf09a2d39c2b357fd8d19daec04516ed6d9d471e5d7031aed1a64d0d" => :mavericks
+    sha256 "f0b9130eed60d46210d227f78f56a06f7dfc9afba8045d8bb7d8fc24f2705c05" => :x86_64_linux
     sha256 "0fe2af0dba0474808f9f4979e4db3cd2c1be4df1f2237c75a9a323283c894280" => :mountain_lion
   end
 
