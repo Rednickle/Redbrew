3fd96060c782bd34a0a2e7d93767c020895a3636
Shaun Jackman <sjackman@gmail.com>
2016-05-12 16:00:00 -0700

    aspcud: update 1.9.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#251.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/aspcud.rb
+++ Formula/aspcud.rb
@@ -9,6 +9,7 @@ class Aspcud < Formula
     sha256 "58d3c2e37d9e6d45229c4486169e62f15a87072219cf696d5a4b277e27908488" => :el_capitan
     sha256 "55340a5126484de0f00f27f409904e7a7ddd1aefdf478a22c3af6a514c14b4d9" => :yosemite
     sha256 "b576548c9bf028f731a2990f5ce137d5835f5a4accac4f2698ca17216b2e00f1" => :mavericks
+    sha256 "7e155a9471cb1871e75d1731063324c34e77dea56f00c6908f370f69b32c9001" => :x86_64_linux
   end
 
   depends_on "boost" => :build
