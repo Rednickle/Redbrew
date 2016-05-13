5c08fdfa62d8c36d75ad366c22f0f261fd52b1ab
Shaun Jackman <sjackman@gmail.com>
2016-05-13 15:18:43 -0700

    libbsd 0.8.3 (new formula)
    Utility functions from BSD systems
    
    Closes Linuxbrew/homebrew-core#268.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ Formula/libbsd.rb
@@ -0,0 +1,20 @@
+class Libbsd < Formula
+  desc "Utility functions from BSD systems"
+  homepage "https://libbsd.freedesktop.org/"
+  url "https://libbsd.freedesktop.org/releases/libbsd-0.8.3.tar.xz"
+  sha256 "934b634f4dfd865b6482650b8f522c70ae65c463529de8be907b53c89c3a34a8"
+  # tag "linuxbrew"
+
+  def install
+    system "./configure",
+      "--disable-debug",
+      "--disable-dependency-tracking",
+      "--disable-silent-rules",
+      "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "strtonum", shell_output("nm #{lib/"libbsd.so"}")
+  end
+end
