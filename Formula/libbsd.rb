ad50d67d896175ba0368a14123dedfc1c51db26f
Shaun Jackman <sjackman@gmail.com>
2016-05-13 18:24:08 -0700

    libbsd: add 0.8.3 bottle for Linuxbrew.

--- Formula/libbsd.rb
+++ Formula/libbsd.rb
@@ -5,6 +5,11 @@ class Libbsd < Formula
   sha256 "934b634f4dfd865b6482650b8f522c70ae65c463529de8be907b53c89c3a34a8"
   # tag "linuxbrew"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "0efc964be0372ef6082dce1b54a2ab515b7a2a62279a177c476d925da023c0d1" => :x86_64_linux
+  end
+
   def install
     system "./configure",
       "--disable-debug",
