4f97d41895b25a97612f73a20562d078abf43318
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-22 23:10:05 +0000

    gnu-getopt: update 1.1.6 bottle for Linuxbrew.

--- Formula/gnu-getopt.rb
+++ Formula/gnu-getopt.rb
@@ -8,6 +8,7 @@ class GnuGetopt < Formula
     sha256 "5e9e87fe18c5681e80f1cf940fed275ed895831304326bc5e7be6fb6e53e8594" => :el_capitan
     sha256 "f8dbbec03aaaeb1bc774d9bf606701901cc9a8ad15cecc5473567e51845057e6" => :yosemite
     sha256 "27938c615808c8e4ff2eacac0a4059c76dee5518a5c8bbfb304b24b70736b429" => :mavericks
+    sha256 "6a61f51ff07f119e254fe400887adf322775795ff28fe74b62049d5381b84e43" => :x86_64_linux
     sha256 "88a02cd609a91253e9b996a1fcb1e8837161673e413fe792e5d05aa3ff9a94cf" => :mountain_lion
   end
 
