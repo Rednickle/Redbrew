4c13f35556d79c2c7f27ac49df2b2582a4ac548c
Bob W. Hogg <rwhogg@linux.com>
2016-05-23 06:46:46 -0400

    joe: update 4.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#339.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/joe.rb
+++ Formula/joe.rb
@@ -8,6 +8,7 @@ class Joe < Formula
     sha256 "26d743b8a2a4d5774b6bf6f205b6b30dd8fe44411f894d0bd7d6d09acad615e5" => :el_capitan
     sha256 "d6739911e38e9017999136d04c9b852110f2d625cd6048188d2618f072aaec0b" => :yosemite
     sha256 "4a7b57c3bf747ba2814f18a5f0b2a53ef005c0686b8c7c9650db67961cf384f8" => :mavericks
+    sha256 "2f33d1ba2ecc364ceed3ac8ba6b0abe4d8f88a24ebfa1a2e611408ab65026b9a" => :x86_64_linux
     sha256 "d7b0e974e3c23620df690dbbde753f87008112981b26c3166ea845d29d28a81e" => :mountain_lion
   end
 
