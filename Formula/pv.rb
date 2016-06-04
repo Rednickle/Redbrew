b9ed0fa81fe355063115e9d2810d2e151844d850
Bob W. Hogg <rwhogg@linux.com>
2016-06-04 07:24:35 -0400

    pv: update 1.6.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#391.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/pv.rb
+++ Formula/pv.rb
@@ -10,6 +10,7 @@ class Pv < Formula
     sha256 "630d2923a804c7a442f6466adf5d6aa21f5f1602f6b005760199a85c14758adc" => :el_capitan
     sha256 "82d8e9279d977a9591450142bf90015df62f800df026038a170d7db6d20a198a" => :yosemite
     sha256 "48c70bdab7cffbc780d15c1d903ae1b7dd80552ed7762e8168629dc1df920fce" => :mavericks
+    sha256 "64d9283deb7d6a7dcbbf4f0e57dafa6668d14379aca1e4ebfa2a1480c0e47368" => :x86_64_linux
     sha256 "1b48570302d9357be80e10a3dfd0362863ced93c111cc827c7fab7b2e79dae9e" => :mountain_lion
   end
 
