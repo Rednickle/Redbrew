c4271558c93b3c59abedf3a2229f78a48f3545b8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-06-09 22:53:47 +0000

    netpbm: update 10.68 bottle for Linuxbrew.

--- Formula/netpbm.rb
+++ Formula/netpbm.rb
@@ -14,6 +14,7 @@ class Netpbm < Formula
     sha256 "ad369fbec6067be0355b02aa02f5b542224cbf9835974de7d0d36ea2a1966e2f" => :el_capitan
     sha256 "a3c04a8257065886cf7f19d246a586a011b8c7d8fd708e5e669b769340e007fa" => :yosemite
     sha256 "5d668e7795efa192da1bf14e1cfc27f458b58a40dfaa63873d7a642b5ec06a4f" => :mavericks
+    sha256 "246ed13eeb79eaf97d4c4423c9971aa7cadf9a0badaf528c050d1ff8e8198d72" => :x86_64_linux
     sha256 "b0c160558d0f764360041069129877bfc7e6d9fc81b3dabfa6cf4fe9a9efc21b" => :mountain_lion
   end
 
