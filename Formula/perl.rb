45f9b2181833778b63bf1e480341d8c23445a731
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2016-06-18 13:46:15 +0200

    perl: Use local include path
    Closes Linuxbrew/homebrew-core#432.
    
    Signed-off-by: Piotr Gaczkowski <DoomHammerNG@gmail.com>

--- Formula/perl.rb
+++ Formula/perl.rb
@@ -51,6 +51,8 @@ class Perl < Formula
 
     args << "-Dusedtrace" if build.with? "dtrace"
     args << "-Dusedevel" if build.head?
+    # Fix for https://github.com/Linuxbrew/homebrew-core/issues/405
+    args << "-Dlocincpth=#{HOMEBREW_PREFIX}/include" if OS.linux?
 
     system "./Configure", *args
     system "make"
