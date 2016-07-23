6175adc46cf128fc7b82670cf1dad0a2d1ba6159
Shaun Jackman <sjackman@gmail.com>
2016-07-23 10:04:31 -0700

    asciinema: update 1.3.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#559.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/asciinema.rb
+++ Formula/asciinema.rb
@@ -11,6 +11,7 @@ class Asciinema < Formula
     sha256 "6205d485d86ea24d0a0396cc56fd76b09490a83c5210e74ff505aa03cd4fcb1d" => :el_capitan
     sha256 "6578e8f11c7d2918f1f7a6c00350c485a937ae1a7037e4d715ecf66e80001cd7" => :yosemite
     sha256 "25455ff63f3f68f17671727ba02f09bb7b03bfa4fadab3fa0c9bd4a7d687cbf0" => :mavericks
+    sha256 "d7f0cf76143d4f545845846ceebf51b27125fca71d8ea9ac686fe7c8a038b6b9" => :x86_64_linux
   end
 
   depends_on :python3
