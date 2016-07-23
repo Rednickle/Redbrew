7e133889cfc3370f9003e6d20cc7fd338c71408e
Bob W. Hogg <rwhogg@linux.com>
2016-07-23 13:27:00 -0400

    jmeter: update 3.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#574.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/jmeter.rb
+++ Formula/jmeter.rb
@@ -9,6 +9,7 @@ class Jmeter < Formula
     sha256 "23f10e9ac40c5c81f71b97611f1d257c64bfbb154d03222067d2859a882cad67" => :el_capitan
     sha256 "0059f8c7a7fece02cbd0440108cca171052300f0ca74364b625c76c30f2e50ff" => :yosemite
     sha256 "b7d006f86f7711ab37c8acb86546148b6ba3251a49ec38436fc7d826e4aa5f1d" => :mavericks
+    sha256 "099bdee7e54c3a5c04c342a5293e32aa449256d41ee1de069d22317a37596635" => :x86_64_linux
   end
 
   option "with-plugins", "add JMeterPlugins Standard, Extras, ExtrasLibs, WebDriver and Hadoop"
