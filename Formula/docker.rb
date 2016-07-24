ee93b8e52fdb45b821a1414c504905307b09f5b5
Shaun Jackman <sjackman@gmail.com>
2016-07-24 09:26:37 -0700

    docker: update 1.11.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#580.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -12,6 +12,7 @@ class Docker < Formula
     sha256 "85376c2591bb8e87981608764ee34d94b2341b6dd428fe75a415af0fd3be64d9" => :el_capitan
     sha256 "44a84556a9d7ab7d631f847e3f761a09db9f0e1fffa9e7e9a341c27f0ead4be3" => :yosemite
     sha256 "5d4657dfcebd6d2e1fe59fe6487f6fe54ed9f6971969ccc0b93088e70d3c9633" => :mavericks
+    sha256 "805f8663caafd649ce4a4fc8f200e6b48774d83808937ff3465e10e8b5723206" => :x86_64_linux
   end
 
   option "with-experimental", "Enable experimental features"
