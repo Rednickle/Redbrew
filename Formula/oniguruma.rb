d9c30e6dfd00da8ef5ddd59fc51f203ce46ab88c
Bob W. Hogg <rwhogg@linux.com>
2016-07-24 07:42:17 -0400

    oniguruma: update 6.0.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#605.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/oniguruma.rb
+++ Formula/oniguruma.rb
@@ -9,6 +9,7 @@ class Oniguruma < Formula
     sha256 "48b9a51758acc36c5217cb696910c5ffa344740955f822352346cc136e9d476e" => :el_capitan
     sha256 "3d14f28ef7d925a56f582946d75333fbe8193843886c9dd2be198f0304ee0c8b" => :yosemite
     sha256 "7dc18d75d30b020bcdca7105552277a52e78dbd90e9d963bb3812706a542179c" => :mavericks
+    sha256 "02f932d6008bb404f9908c8ef2116df982e77f57a6f6f01341f59ab4120248f3" => :x86_64_linux
   end
 
   def install
