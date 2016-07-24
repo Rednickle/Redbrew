bfa6fd5239c9a5bcbc8b9f3cf83ac9173c27a4bf
Bob W. Hogg <rwhogg@linux.com>
2016-07-24 07:29:50 -0400

    mongoose: update 6.5 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#602.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/mongoose.rb
+++ Formula/mongoose.rb
@@ -9,6 +9,7 @@ class Mongoose < Formula
     sha256 "1eeec9281c060972b1dd182d8f761eb92f9ddae63c069b064312948592145913" => :el_capitan
     sha256 "91c28ea470a99e7f04822f495929fe048ed06b2dbc9f0b37b8680aaa0ef587fe" => :yosemite
     sha256 "b1804618ed47cca9b9d231d591fffc2acc2fb47abd3ff8c70d2ea481ddb4d30a" => :mavericks
+    sha256 "20f2d343a7e9bf52722e17a713352b381cb371523fd1d58ae61195167af3f9dc" => :x86_64_linux
   end
 
   depends_on "openssl" => :recommended
