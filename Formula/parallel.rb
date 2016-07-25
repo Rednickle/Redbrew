fb4b9ebac4a2cbe79f2565ab3a5f408119e41908
Maxim Belkin <maxim.belkin@gmail.com>
2016-07-24 22:02:11 -0500

    parallel: update 20160622 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#622.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/parallel.rb
+++ Formula/parallel.rb
@@ -11,6 +11,7 @@ class Parallel < Formula
     sha256 "421bf3c3f5dc9ed02bd547fcc0dd17840cf00746b28ad6f38009b829515a4631" => :el_capitan
     sha256 "ed566d860296a06d09760936a80900f1ce520f83275916d4f505215b3d5c26f3" => :yosemite
     sha256 "cc93ff4ba1288030ba27c11cd930e02d383cb4c36d766ff7e0845db8ec324b2d" => :mavericks
+    sha256 "29e81004e1c5f95ed6d808b94c154e1b7a6417a3a2dc3dd5c1fa9e8fb840977a" => :x86_64_linux
   end
 
   conflicts_with "moreutils", :because => "both install a 'parallel' executable."
