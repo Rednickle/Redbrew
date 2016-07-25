8b23b71063e8c0efc1dc5aef68bb926526c005a5
Shaun Jackman <sjackman@gmail.com>
2016-07-24 23:20:29 -0700

    libfixposix: Fix test for Linuxbrew
    Closes Linuxbrew/homebrew-core#615.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/libfixposix.rb
+++ Formula/libfixposix.rb
@@ -35,12 +35,13 @@ class Libfixposix < Formula
       int main(void)
       {
           fd_set rset, wset, eset;
+          unsigned i;
 
           lfp_fd_zero(&rset);
           lfp_fd_zero(&wset);
           lfp_fd_zero(&eset);
 
-          for(unsigned i = 0; i < FD_SETSIZE; i++) {
+          for(i = 0; i < FD_SETSIZE; i++) {
               if(lfp_fd_isset(i, &rset)) {
                   printf("%d ", i);
               }
