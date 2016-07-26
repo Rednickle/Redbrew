f05dc82745024f7ccea8dd42f9988ef7e9f29a25
Maxim Belkin <maxim.belkin@gmail.com>
2016-07-26 11:25:24 -0500

    youtube-dl: update 2016.07.24 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#662.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/youtube-dl.rb
+++ Formula/youtube-dl.rb
@@ -12,6 +12,7 @@ class YoutubeDl < Formula
     sha256 "194351cd7fc3093820b90e76caac2e1c7d49cbf0554d6d43fabf8e44caa06433" => :el_capitan
     sha256 "b0e67c120d22afa44000732ff4608a5261fea7b111477c348b6032c553428175" => :yosemite
     sha256 "e0857711e5c9db837b8ef21081d90a6b9b1546ac4accd99bb204f893b467a9dd" => :mavericks
+    sha256 "dd4ef7c2e17dec783aa7c3534e7b0a1377e693dd8774fada13d2cdd8c843475a" => :x86_64_linux
   end
 
   head do
