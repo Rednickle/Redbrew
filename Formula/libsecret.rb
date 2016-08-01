1cf391e7e20261db8612168fbcab33f001cbfbf6
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2016-08-01 12:20:25 +0200

    libsecret: update 0.18.5 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#706.
    Signed-off-by: Piotr Gaczkowski <DoomHammerNG@gmail.com>

--- Formula/libsecret.rb
+++ Formula/libsecret.rb
@@ -8,6 +8,7 @@ class Libsecret < Formula
     sha256 "07d7fd02e1c3857a2a27415575248ea89a19c7dd1a86efe53fcf1866dce1bf46" => :el_capitan
     sha256 "090ea52539396135f710956cd051cbab59d6dd7fb94d8666dc4ed4e0312cddac" => :yosemite
     sha256 "b7f878856f2f272de7d0456ad3c82a6e260a9a272a7c4eefc1504a668a430fb0" => :mavericks
+    sha256 "40394105302903085cf2679cd2c0c3255ae8edd4fa6dee451eb674d4f9a8fc5b" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
