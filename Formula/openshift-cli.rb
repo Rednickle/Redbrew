5b902540f8e78d0a96e486e7b85dc8a4e153b3f5
Bob W. Hogg <rwhogg@linux.com>
2016-08-27 20:01:42 -0400

    openshift-cli: update 1.2.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#830.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/openshift-cli.rb
+++ Formula/openshift-cli.rb
@@ -13,6 +13,7 @@ class OpenshiftCli < Formula
     sha256 "d20ef7c33fc7abf164220b9d750dff98b576c37c17da5783fc320dc13e4bd1f0" => :el_capitan
     sha256 "b076f18b911f07739b5d77394e511eb16df43a78eb1074f350a5c08da930889e" => :yosemite
     sha256 "561edbcedb37887ccb241c6e415a11c827e44fb99657e73ad7d30857f35fa577" => :mavericks
+    sha256 "b8cd1a96af983604669ec20d918d28d6d9d775494dfc71e13c69016b48f762fd" => :x86_64_linux
   end
 
   devel do
