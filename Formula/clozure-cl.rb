48f6324b064a5ca5010ed74d2d5060e17fd92fbf
Alex Dunn <dunn.alex@gmail.com>
2016-08-28 22:24:48 -0700

    clozure-cl: add linux sources

--- Formula/clozure-cl.rb
+++ Formula/clozure-cl.rb
@@ -1,22 +1,27 @@
 class ClozureCl < Formula
   desc "Common Lisp implementation with a long history"
   homepage "http://ccl.clozure.com/"
-  url "http://ccl.clozure.com/ftp/pub/release/1.11/ccl-1.11-darwinx86.tar.gz"
   version "1.11"
-  sha256 "2fc4b519f26f7df3fbb62314b15bd5d098082b05d40c3539f8204aa10f546913"
-  head "http://svn.clozure.com/publicsvn/openmcl/trunk/darwinx86/ccl"
+
+  if OS.mac?
+    url "http://ccl.clozure.com/ftp/pub/release/1.11/ccl-1.11-darwinx86.tar.gz"
+    sha256 "2fc4b519f26f7df3fbb62314b15bd5d098082b05d40c3539f8204aa10f546913"
+    head "http://svn.clozure.com/publicsvn/openmcl/trunk/darwinx86/ccl"
+  elsif OS.linux?
+    url "http://ccl.clozure.com/ftp/pub/release/1.11/ccl-1.11-linuxx86.tar.gz"
+    sha256 "08e885e8c2bb6e4abd42b8e8e2b60f257c6929eb34b8ec87ca1ecf848fac6d70"
+    head "http://svn.clozure.com/publicsvn/openmcl/trunk/linuxx86/ccl"
+  end
 
   bottle :unneeded
 
   conflicts_with "cclive", :because => "both install a ccl binary"
 
   def install
-    if build.head?
-      ENV["CCL_DEFAULT_DIRECTORY"] = buildpath
-      system "scripts/ccl64", "-n",
-             "-e", "(ccl:rebuild-ccl :full t)",
-             "-e", "(quit)"
-    end
+    ENV["CCL_DEFAULT_DIRECTORY"] = buildpath
+    system "scripts/ccl64", "-n",
+           "-e", "(ccl:rebuild-ccl :full t)",
+           "-e", "(quit)"
 
     # Get rid of all the .svn directories
     rm_rf Dir["**/.svn"]
