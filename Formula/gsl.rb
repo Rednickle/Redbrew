8eabb473bf2947e4bc3788641430fd907d891271
Bob W. Hogg <rwhogg@linux.com>
2016-09-13 17:26:43 -0400

    gsl: update 2.2.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#881.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/gsl.rb
+++ Formula/gsl.rb
@@ -10,6 +10,7 @@ class Gsl < Formula
     sha256 "89462bab1b0b7001ce6b71db851960f005c0436ffac840f3b9cc255ed7cb8d44" => :el_capitan
     sha256 "00e0d7aa1202bededfa2bdce311dd7ff08db0e412e45bd87b783eb29305ccde5" => :yosemite
     sha256 "00a05716a23a7bc333782dd77f547942563912650445d557aa5c8c941ac22c7e" => :mavericks
+    sha256 "22d4749909581c63f2d1e3a00921bef8be6628655e69b6fc206d3d1cfb83e43a" => :x86_64_linux
   end
 
   option :universal
