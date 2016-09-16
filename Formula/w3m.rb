422705b2c6e16bed25e2a8cc5269991f7d5e54f4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-09-16 17:06:26 +0000

    w3m: update 0.5.3 bottle for Linuxbrew.

--- Formula/w3m.rb
+++ Formula/w3m.rb
@@ -8,6 +8,7 @@ class W3m < Formula
     sha256 "c6443287699a058e58ff0e378a8f6459370de79f89246ac7217e11f9f748abed" => :el_capitan
     sha256 "57a644921789316e92cbc37d2e0c51eaf5591876992626a9bcf9f4a56c0e3897" => :yosemite
     sha256 "e2972a26e7c734e6814257516ebda796e907df5787906c4144321fc63e70f1a1" => :mavericks
+    sha256 "1ead87bf254d3d49574588945c73a934d1ddf104547389d41cb67df090c92b44" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
