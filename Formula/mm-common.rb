0d62f21337b0c519a6f320b05fa4d3c6cfb8b318
Bob W. Hogg <rwhogg@linux.com>
2016-09-19 19:17:18 -0400

    mm-common: update 0.9.10 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#934.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/mm-common.rb
+++ Formula/mm-common.rb
@@ -9,6 +9,7 @@ class MmCommon < Formula
     sha256 "8f34165d9e854d0d3cf261775c53191115537c343d65771a458ed827357f05a2" => :el_capitan
     sha256 "8b131b53a8ace806b0b6a3c75be145312377b0f25d92ea578ba07e27809fa852" => :yosemite
     sha256 "d04a14c639c497aea15e39ab5c70fb6753d3a7983528cb8b18a7e309babedbde" => :mavericks
+    sha256 "42a2073f5d3cd4314bb01e293a67b6dcbdd01b64c7b84105fda2da049e0c0dda" => :x86_64_linux
   end
 
   def install
