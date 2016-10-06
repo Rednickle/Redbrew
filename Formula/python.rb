c94dd4dd0ea9bc3fbb61e0d5041e0f6a6e62b7d6
Bob W. Hogg <rwhogg@linux.com>
2016-10-06 08:41:49 -0400

    python: Bump revision for Linuxbrew
    revision 1 already existed for Linuxbrew as of
    Linuxbrew/homebrew-core#673, but upstream also bumped the revision since
    then (when readline changed its soname).
    
    The 2.7.12_1 bottle therefore already exists.
    
    Bump to revision 2 so we can publish a new Linuxbrew bottle
    after building against the new readline.
    
    Closes Linuxbrew/homebrew-core#1008.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/python.rb
+++ Formula/python.rb
@@ -3,7 +3,7 @@ class Python < Formula
   homepage "https://www.python.org"
   url "https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tar.xz"
   sha256 "d7837121dd5652a05fef807c361909d255d173280c4e1a4ded94d73d80a1f978"
-  revision 1
+  revision OS.linux? ? 2 : 1
 
   head "https://hg.python.org/cpython", :using => :hg, :branch => "2.7"
 
