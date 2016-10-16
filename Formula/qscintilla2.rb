504353e4eaba2ff6f188a0d7827ad1af3accc1c5
Shaun Jackman <sjackman@gmail.com>
2016-10-16 14:21:03 -0700

    qscintilla2: Fix for Linuxbrew
    Fix Error: undefined method `>=' for nil:NilClass

--- Formula/qscintilla2.rb
+++ Formula/qscintilla2.rb
@@ -34,7 +34,7 @@ class Qscintilla2 < Formula
   # Originally reported 7 Oct 2016 https://www.riverbankcomputing.com/pipermail/qscintilla/2016-October/001160.html
   # Patch below posted 13 Oct 2016 https://www.riverbankcomputing.com/pipermail/qscintilla/2016-October/001167.html
   # Same as Alan Garny's OpenCOR commit https://github.com/opencor/opencor/commit/70f3944e36b8b95b3ad92106aeae2f511b3f0e90
-  if DevelopmentTools.clang_build_version >= 800
+  if !DevelopmentTools.clang_build_version.nil? && DevelopmentTools.clang_build_version >= 800
     patch do
       url "https://raw.githubusercontent.com/Homebrew/formula-patches/a651d71/qscintilla2/xcode-8.patch"
       sha256 "1a88309fdfd421f4458550b710a562c622d72d6e6fdd697107e4a43161d69bc9"
