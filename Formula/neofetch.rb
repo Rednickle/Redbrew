d850d30597fbe6ca39d233c2796cfc6dfaef87ad
Bob W. Hogg <rwhogg@linux.com>
2016-10-18 10:49:57 -0400

    neofetch: update 1.8.1 bottle for Linuxbrew.
    Closes linuxbrew/homebrew-core#1093.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -10,6 +10,7 @@ class Neofetch < Formula
     sha256 "1c5cbcf6dee70c3d9a537cf2ff73cd4d7adffc2267c555c835b448358f9cc76e" => :sierra
     sha256 "1c5cbcf6dee70c3d9a537cf2ff73cd4d7adffc2267c555c835b448358f9cc76e" => :el_capitan
     sha256 "1c5cbcf6dee70c3d9a537cf2ff73cd4d7adffc2267c555c835b448358f9cc76e" => :yosemite
+    sha256 "bb7a234cc1ac2c0068eebebc1070a5aa7e2c22c47770ee9cdc9f1e434fba2c85" => :x86_64_linux
   end
 
   depends_on "screenresolution" => :recommended if OS.mac?
