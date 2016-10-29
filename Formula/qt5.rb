493d63712a49f0b5e240a6d66cb040dd9a1f6039
Shaun Jackman <sjackman@gmail.com>
2016-10-29 09:34:21 -0700

    qt5: update 5.7.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1144.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/qt5.rb
+++ Formula/qt5.rb
@@ -64,6 +64,7 @@ class Qt5 < Formula
     sha256 "4be3f39a46f38bd596d2198b3535a4329072b6dc3e75717a0fcf23d392b84ed0" => :sierra
     sha256 "3085e765270fbae1df00a0a56df1e287234345cd5532cb54dafd991463813868" => :el_capitan
     sha256 "17bec6a0f29a0fb74c3dd8d7b8e45cb50a10b0b6860bca5f54590e04cbf7e48b" => :yosemite
+    sha256 "7f98bf380380f570494f72dba8b8a01757bde1712fe01d752f7289863a09e4d1" => :x86_64_linux
   end
 
   keg_only "Qt 5 conflicts Qt 4"
