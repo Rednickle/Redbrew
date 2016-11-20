6d3f5a3026ad149669c8a1646c1ba5dd929fa606
Bob W. Hogg <rwhogg@linux.com>
2016-11-20 09:01:46 -0800

    android-platform-tools: Fix for Linuxbrew (#1207)

--- Formula/android-platform-tools.rb
+++ Formula/android-platform-tools.rb
@@ -1,11 +1,17 @@
 class AndroidPlatformTools < Formula
   desc "Tools for the Android SDK"
   homepage "https://developer.android.com/sdk"
+  version "24"
+
   # the url is from:
   # https://dl.google.com/android/repository/repository-12.xml
-  url "https://dl.google.com/android/repository/platform-tools_r24-macosx.zip"
-  version "24"
-  sha256 "5eb758fe3ddddd8e522c17244bbcb886f399529855bad60c8ba14711dc5a8a12"
+  if OS.mac?
+    url "https://dl.google.com/android/repository/platform-tools_r24-macosx.zip"
+    sha256 "5eb758fe3ddddd8e522c17244bbcb886f399529855bad60c8ba14711dc5a8a12"
+  elsif OS.linux?
+    url "https://dl.google.com/android/repository/platform-tools_r24-linux.zip"
+    sha256 "ef1672ecc3430bb11511fce57370f28b0db47d37df7f1b3298970880b2e221de"
+  end
 
   bottle :unneeded
 
