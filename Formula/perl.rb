4e34e113e5ee0323e5fee8ab21455393bd501080
Shaun Jackman <sjackman@gmail.com>
2016-12-05 21:37:38 -0800

    perl: update 5.24.0_2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1246.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/perl.rb
+++ Formula/perl.rb
@@ -25,6 +25,7 @@ class Perl < Formula
     sha256 "2d17be7f00decaec2d9d9d25335962e78319b5ee121112ae6e6325227c50313a" => :sierra
     sha256 "bbc3eb4e2a1e7d9585918862adf718e5be80e4dae793e547bf71da8a07b372d8" => :el_capitan
     sha256 "7f4410ad668128cb66085a8e7fa995258cb60ba8b2551ab170ae612d3101d021" => :yosemite
+    sha256 "41f7a7813d33cec8ed783bc072dd4841fc3b889bcee2a678bd63458a39f3fb4a" => :x86_64_linux
   end
 
   option "with-dtrace", "Build with DTrace probes"
