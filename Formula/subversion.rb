2e24e815ca64cd0f3ae500534d451763408a66a5
Shaun Jackman <sjackman@gmail.com>
2016-12-06 20:07:09 -0800

    subversion: update 1.9.5_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1311.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/subversion.rb
+++ Formula/subversion.rb
@@ -10,6 +10,7 @@ class Subversion < Formula
     sha256 "a1eb24f126de1cbe776690c6d8320638c9c917d3d45aeb06f38530236272e6ce" => :sierra
     sha256 "b63e9e5af0e18ae59e851795ca551e2b2161fcea99dc7d4b147ca44f572e0062" => :el_capitan
     sha256 "5be40371113a10bfb441edeceea9df3705290df1db296adf4a2616ed81c73185" => :yosemite
+    sha256 "b1c56acb1c9295f698516de674d4a10c70f85e581457da58c16a2f7f8623ccb0" => :x86_64_linux
   end
 
   deprecated_option "java" => "with-java"
