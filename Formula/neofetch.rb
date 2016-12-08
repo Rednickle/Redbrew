924c45c84a8b4f51c111b21f9ebd13337dae8752
Shaun Jackman <sjackman@gmail.com>
2016-12-07 23:19:53 -0800

    neofetch: update 2.0.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1335.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/neofetch.rb
+++ Formula/neofetch.rb
@@ -10,6 +10,7 @@ class Neofetch < Formula
     sha256 "eb1cbc74bba6487b48d9c7c6f9118bd49ce0572751f42461f52341d9b0b43f86" => :sierra
     sha256 "eb1cbc74bba6487b48d9c7c6f9118bd49ce0572751f42461f52341d9b0b43f86" => :el_capitan
     sha256 "eb1cbc74bba6487b48d9c7c6f9118bd49ce0572751f42461f52341d9b0b43f86" => :yosemite
+    sha256 "56e527d5b523094faa9bc8e915f73d7836cc30730b91c809a12294a0da4f2467" => :x86_64_linux
   end
 
   depends_on "screenresolution" => :recommended if OS.mac?
