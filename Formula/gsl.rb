fdd9744b95a3bafa4363dd0d935d2b86999d95bf
Shaun Jackman <sjackman@gmail.com>
2016-12-12 22:06:01 -0800

    gsl: update 2.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1355.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/gsl.rb
+++ Formula/gsl.rb
@@ -10,6 +10,7 @@ class Gsl < Formula
     sha256 "1bb5851b23af3a29907fe944b0223f84a2a13f9f493a124b9f659f3ac77cfbce" => :sierra
     sha256 "ca5dd754f6988ee2083be33660530854b3a8d4ab2ab6026d9da6613cfec7431e" => :el_capitan
     sha256 "27e916ed0e2bec74aeb88e694a22f0076e225c6a0b8c9b5450d5cbc0db842e3e" => :yosemite
+    sha256 "99d57cebb43fbc890e74e7e71a650b038f639c87ec1197e42ce3179b10378cf6" => :x86_64_linux
   end
 
   option :universal
