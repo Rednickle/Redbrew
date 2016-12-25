b0120befa9db68b6b516e0400025afb78f18c4e9
Bob W. Hogg <rwhogg@linux.com>
2016-12-25 09:29:22 -0800

    libreadline-java: Fix for Linuxbrew
    Fix error:
      fatal error: jni_md.h: No such file or directory
    
    Additionally, don't use install_name_tool and don't change file
    extensions from so to jnilib
    
    Closes Linuxbrew/homebrew-core#1409.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/libreadline-java.rb
+++ Formula/libreadline-java.rb
@@ -34,7 +34,7 @@ class LibreadlineJava < Formula
     # original Apple/Sun JDK used to.
     if File.exist? "#{java_home}/include/jni.h"
       ENV["JAVAINCLUDE"] = "#{java_home}/include"
-      ENV["JAVANATINC"]  = "#{java_home}/include/darwin"
+      ENV["JAVANATINC"]  = "#{java_home}/include/#{OS.mac? ? "darwin" : "linux"}"
     elsif File.exist? "/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/jni.h"
       ENV["JAVAINCLUDE"] = "/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/"
       ENV["JAVANATINC"]  = "/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/"
@@ -48,7 +48,7 @@ class LibreadlineJava < Formula
       s.change_make_var! "JAVALIBDIR", "$(PREFIX)/share/libreadline-java"
       s.change_make_var! "JAVAINCLUDE", ENV["JAVAINCLUDE"]
       s.change_make_var! "JAVANATINC", ENV["JAVANATINC"]
-      s.gsub! "*.so", "*.jnilib"
+      s.gsub! "*.so", "*.jnilib" if OS.mac?
       s.gsub! "install -D", "install -c"
     end
 
@@ -60,8 +60,8 @@ class LibreadlineJava < Formula
       s.change_make_var! "INCLUDES", "-I $(JAVAINCLUDE) -I $(JAVANATINC) -I #{readline.opt_include}"
       s.change_make_var! "LIBPATH", "-L#{readline.opt_lib}"
       s.change_make_var! "CC", "cc"
-      s.gsub! "LIB_EXT := so", "LIB_EXT := jnilib"
-      s.gsub! "$(CC) -shared $(OBJECTS) $(LIBPATH) $($(TG)_LIBS) -o $@", "$(CC) -install_name #{HOMEBREW_PREFIX}/lib/$(LIB_PRE)$(TG).$(LIB_EXT) -dynamiclib $(OBJECTS) $(LIBPATH) $($(TG)_LIBS) -o $@"
+      s.gsub! "LIB_EXT := so", "LIB_EXT := jnilib" if OS.mac?
+      s.gsub! "$(CC) -shared $(OBJECTS) $(LIBPATH) $($(TG)_LIBS) -o $@", "$(CC) -install_name #{HOMEBREW_PREFIX}/lib/$(LIB_PRE)$(TG).$(LIB_EXT) -dynamiclib $(OBJECTS) $(LIBPATH) $($(TG)_LIBS) -o $@" if OS.mac?
     end
 
     pkgshare.mkpath
