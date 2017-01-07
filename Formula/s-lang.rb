a8272416b8ce28162b2dd0373355c95c81be5f6d
Shaun Jackman <sjackman@gmail.com>
2017-01-06 23:24:48 -0800

    s-lang: update 2.3.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1511.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/s-lang.rb
+++ Formula/s-lang.rb
@@ -11,6 +11,7 @@ class SLang < Formula
     sha256 "52fba342bc32cf218d575154b655a95bcd0e3e1dc1e1ea8e98e78455abf1ec68" => :yosemite
     sha256 "bc5d35bdfbfa639e3b6403b25a36a49c1dca66cd85ad25adedcc9b67db9873e2" => :mavericks
     sha256 "751b0127d64c72f502a2c197c625d0a505325c993fb39d1bf6dbdc4f9bb515c8" => :mountain_lion
+    sha256 "5c7d80228ab2639315c7259fbe21659026221f62c2a81d23531ebbb23b4951f4" => :x86_64_linux
   end
 
   depends_on "libpng"
