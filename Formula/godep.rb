47a9c2ffd786bffe6a7ad4b56e9fc94ffbac4173
Shaun Jackman <sjackman@gmail.com>
2017-01-11 08:41:58 -0800

    godep: update 75_3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1546.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/godep.rb
+++ Formula/godep.rb
@@ -10,6 +10,7 @@ class Godep < Formula
     sha256 "b9c34eb3bc69fc2d23a266140996857fd7dc2f006069b4211a7937f4ef7de14b" => :sierra
     sha256 "1d3fc2732d7619676df9045d3724170187f12ea85c946f2bf06dfacd80730dcf" => :el_capitan
     sha256 "fa609d65f0bd90fe4f2e409ae1ac254d3a0efb57d80665205dfee314c4d5a962" => :yosemite
+    sha256 "336166687d4f90d1521c9ed1bb2729fd5d01cdc95d228c676936edd3fdce1bd7" => :x86_64_linux
   end
 
   depends_on "go"
