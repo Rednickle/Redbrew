3bcf08513f0361c4d945ad52dbb600c8745fac9f
Shaun Jackman <sjackman@gmail.com>
2017-01-31 18:37:37 -0800

    gd: update 2.2.4_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1687.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/gd.rb
+++ Formula/gd.rb
@@ -18,6 +18,7 @@ class Gd < Formula
     sha256 "23a18720365fd4c7aaa4d8097f339ef4177a5d708990db6711f72661d04035c9" => :sierra
     sha256 "f466f3c052633de8e1a649345890e3da2579791a8876e8ddc81e582654319e92" => :el_capitan
     sha256 "b1db65caa81c5bfdcf16b63b44dcbccbad82a7007111479a5a94a2bbc497a2d1" => :yosemite
+    sha256 "e377d7b9bd03d97f0de81a1d46c637a9fd5d907911803af1d76dc04af5819711" => :x86_64_linux
   end
 
   head do
