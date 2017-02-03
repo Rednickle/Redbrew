5606c505efd4ffbb3dfdb7d2e5b418523d639bc1
Shaun Jackman <sjackman@gmail.com>
2017-02-02 23:39:07 -0800

    docker: update 1.13.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1709.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -12,6 +12,7 @@ class Docker < Formula
     sha256 "62f05dee992f846622e5292678f502c15e6881a91f90dce5cda3fa5fea5571d9" => :sierra
     sha256 "2d5a37d49f19eeed0169b0d6cc84a2b7e5110a2ebc3eb2fd1701de92d11d8098" => :el_capitan
     sha256 "680e3fb1ee0c54cbd458777b0a9aac2ce2be60a96af740c1053b9f73533957e7" => :yosemite
+    sha256 "8443db27e6daf9ea663f74c509f9a9b15fd78ebdedbe8e4c2472ffac89e3d6b0" => :x86_64_linux
   end
 
   option "with-experimental", "Enable experimental features"
