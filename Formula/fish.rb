e15358c17ebb417fb85f963d2b6af13670f81af3
Shaun Jackman <sjackman@gmail.com>
2017-02-04 00:25:11 -0800

    fish: update 2.5.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1717.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/fish.rb
+++ Formula/fish.rb
@@ -12,6 +12,7 @@ class Fish < Formula
     sha256 "d012c7b4d1dce5e766fe9990f19ec7ac0ae5c4a69fc15e01cf123e8f88c1910f" => :sierra
     sha256 "2b28972cc66472fa11782b344c64b51569a43d0fd8b098ad4b787a1c79aca2b1" => :el_capitan
     sha256 "385def41fe9c29237b7319218ae0184f64b6b750e525f92ee6913cf20c478a69" => :yosemite
+    sha256 "ba4c12195f04f315cbda1325fab1ab27034a323fdde16897386fd95600664f6b" => :x86_64_linux
   end
 
   head do
