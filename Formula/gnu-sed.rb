6d31efeb2fa2aa4953fdd634481712cc57d7d080
Bob W. Hogg <rwhogg@linux.com>
2017-02-05 21:36:37 -0800

    gnu-sed: update 4.4 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1730.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/gnu-sed.rb
+++ Formula/gnu-sed.rb
@@ -10,6 +10,7 @@ class GnuSed < Formula
     sha256 "dbb273a424c68a41670b1bdc7630960ed3d81b08d16b4e2de89da3b08d8042f8" => :sierra
     sha256 "9b221159fc84c8774053bf6611f9da3652636f1531a68819f8ca59fc3c7321c1" => :el_capitan
     sha256 "8abddf2922e6151ee21c84f2b997f9cfe55eeadbbe13ca28ea4097afa0c91f9d" => :yosemite
+    sha256 "0d814f6fcdb1a41038f3a217379bb2c666852eb26f3fbe2ff7216c58159bcdb6" => :x86_64_linux
   end
 
   option "with-default-names", "Do not prepend 'g' to the binary"
