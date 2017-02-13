65c5395a65147dd7995661bb60976b69046eab31
Bob W. Hogg <rwhogg@linux.com>
2017-02-13 09:40:25 -0800

    libmagic: update 5.30 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1786.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/libmagic.rb
+++ Formula/libmagic.rb
@@ -9,6 +9,7 @@ class Libmagic < Formula
     sha256 "7b8f1701d7565c1d89de611d2633962d58411fff01027b47327934932bfa2f66" => :sierra
     sha256 "d8481b614f8583f862b18b27dcff69c046e3c6f20d4eddde8d777a3e4be95688" => :el_capitan
     sha256 "2ca4f55351b129933f8625b19958ecc21ce9d301ef18f771572f0bcba484f1e5" => :yosemite
+    sha256 "3bbb0cc20fd18dee15a3df8c7a5c4783781ea4456d104900ca5e595a0a920c49" => :x86_64_linux
   end
 
   option :universal
