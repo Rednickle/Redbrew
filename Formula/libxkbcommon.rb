cc487534a00c100f5d5877dab6830e2b294c728c
Maxim Belkin <maxim.belkin@gmail.com>
2017-02-19 05:18:08 -0600

    libxkbcommon: Fix for Linuxbrew
    * do not depend on x11 and bison
    * depend on xkeyboardconfig and libxcb
    * do not change configure files to avoid dependency on automake
    
    Closes #1800
    Closes #1815
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libxkbcommon.rb
+++ Formula/libxkbcommon.rb
@@ -18,19 +18,25 @@ class Libxkbcommon < Formula
     depends_on "libtool" => :build
   end
 
-  depends_on :x11
-  depends_on "bison" => :build
+  if OS.mac?
+    depends_on :x11
+    depends_on "bison" => :build
+  end
   depends_on "pkg-config" => :build
+  depends_on "linuxbrew/xorg/xkeyboardconfig" unless OS.mac?
+  depends_on "linuxbrew/xorg/libxcb" unless OS.mac?
 
   def install
     system "./autogen.sh" if build.head?
-    inreplace "configure" do |s|
-      s.gsub! "-version-script $output_objdir/$libname.ver", ""
-      s.gsub! "$wl-version-script", ""
-    end
-    inreplace %w[Makefile.in Makefile.am] do |s|
-      s.gsub! "-Wl,--version-script=${srcdir}/xkbcommon.map", ""
-      s.gsub! "-Wl,--version-script=${srcdir}/xkbcommon-x11.map", ""
+    if OS.mac?
+      inreplace "configure" do |s|
+        s.gsub! "-version-script $output_objdir/$libname.ver", ""
+        s.gsub! "$wl-version-script", ""
+      end
+      inreplace %w[Makefile.in Makefile.am] do |s|
+        s.gsub! "-Wl,--version-script=${srcdir}/xkbcommon.map", ""
+        s.gsub! "-Wl,--version-script=${srcdir}/xkbcommon-x11.map", ""
+      end
     end
 
     system "./configure", "--disable-dependency-tracking",
