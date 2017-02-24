5a00ffae76ceebef808175ac428d3f21d4b11d8c
Shaun Jackman <sjackman@gmail.com>
2017-02-24 11:55:05 -0800

    xdot: update 0.6 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1868.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/xdot.rb
+++ Formula/xdot.rb
@@ -13,6 +13,7 @@ class Xdot < Formula
     sha256 "18110705b33c93b878b20c73b897cde121b4e4e2c054c1af2e0d5c863f16017f" => :el_capitan
     sha256 "d074dd582b0ca17aa565f75a2b53048f97360ac03d53e6fd0edb3f520c8efc06" => :yosemite
     sha256 "6c9a134c44cb444421e29cc398c9c00ca6f8849def89012dce1d69581b41227e" => :mavericks
+    sha256 "bf3a3af1735f7705b5844465639b3d99806cac30dd9b976579c930ab8507c74b" => :x86_64_linux
   end
 
   depends_on "graphviz"
