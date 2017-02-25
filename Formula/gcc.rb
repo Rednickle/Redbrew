0546c695df831f411c002b28a2a4b9902f291c3b
Shaun Jackman <sjackman@gmail.com>
2017-02-24 18:21:53 -0800

    gcc: Fix style [Linux]
    * Stable: Please use https://
    * 'fails_with :llvm' is now a no-op so should be removed
    * Don't need 'FileUtils.' before mv.
    * Use the `mv` Ruby method instead of `system "mv `

--- Formula/gcc.rb
+++ Formula/gcc.rb
@@ -221,7 +221,7 @@ class Gcc < Formula
     # Move lib64/* to lib/ on Linuxbrew
     lib64 = Pathname.new "#{lib}64"
     if lib64.directory?
-      system "mv #{lib64}/* #{lib}/" # Do not use FileUtils.mv with Ruby 1.9.3
+      mv Dir[lib64/"*"], lib
       rmdir lib64
       prefix.install_symlink "lib" => "lib64"
     end
