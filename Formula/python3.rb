0c18947d440be14a899027ac6e2097bc291904f6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-03-07 01:15:17 +0000

    python3: update 3.6.0_1 bottle for Linuxbrew.

--- Formula/python3.rb
+++ Formula/python3.rb
@@ -10,6 +10,7 @@ class Python3 < Formula
     sha256 "e8b177926ce5e110d7cf9f03f9d11c60b08ad74ccee07bc72fe38ac981bf0cef" => :sierra
     sha256 "0ddeba54962af4c14b115e279f5cefb72df55fc9bc98a1a60a5b4a2d5cf2fc77" => :el_capitan
     sha256 "10b8bf96e157e88188492923ba0130f5f8b5e1d56a5bf4dc9f6dbc2b028bb24b" => :yosemite
+    sha256 "4a176efe8c7e5af0c791351282e2087ccca827804c93ee269fb5b74602c3339b" => :x86_64_linux
   end
 
   option "with-tcl-tk", "Use Homebrew's Tk instead of macOS Tk (has optional Cocoa and threads support)"
