771fac143a80ff805eea3b998eea2df8024942f2
Shaun Jackman <sjackman@gmail.com>
2017-03-06 22:50:25 -0800

    jasper: update 2.0.12 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1965.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -8,6 +8,7 @@ class Jasper < Formula
     sha256 "386f26d020cbe06b80066c34cf6a4f679562b93211ef74d244ca119604af8d13" => :sierra
     sha256 "be03c9d8ef6f4a3dd056ce7358c0ebbdd366542e9c00639c223ea0d5bb941ada" => :el_capitan
     sha256 "9390f990b9bd0e4605c2c012c80e8ec40ac2f539cd79814fcf276a12bbe32b4a" => :yosemite
+    sha256 "dba554bebae0e3685308b0568eb4619cdf15cc63f643993c51e2fbbe812f6960" => :x86_64_linux
   end
 
   option :universal
