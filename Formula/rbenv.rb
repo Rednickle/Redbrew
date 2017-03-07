67dcdda660ee5173b6b69b363769fbdd9e3ad49d
Shaun Jackman <sjackman@gmail.com>
2017-03-06 18:26:43 -0800

    rbenv: update 1.1.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#1959.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/rbenv.rb
+++ Formula/rbenv.rb
@@ -10,6 +10,7 @@ class Rbenv < Formula
     sha256 "c9d34dee07b2a04d24aa1a8da30b0b5663529d69d7a46b29d979ceba79c81e67" => :sierra
     sha256 "2bdafe7927a69de0aedad237694431fddaa7cd2167855094a3c7caeeccc0a347" => :el_capitan
     sha256 "f829928b4796a022a60f10ab89c48f050ad8c69e40a74aa5fd3ba9b1b721769c" => :yosemite
+    sha256 "f7b7afe5ccf38b9144134144fdbcb2ce394a36290bf65812715745520ec4d13e" => :x86_64_linux
   end
 
   depends_on "ruby-build" => :recommended
