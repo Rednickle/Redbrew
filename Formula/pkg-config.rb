1b555400d096380bfca99078b3399d83d7cdb615
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-03-23 15:32:47 -0700

    pkg-config: update 0.29.2 bottle for Linuxbrew.

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -9,6 +9,7 @@ class PkgConfig < Formula
     sha256 "8eb723bfc03cd468d779d54d015d47d2e8ab1dd4d35e595ab4abaca8833b3277" => :sierra
     sha256 "93f044f166bcbd84db14133ee4f56104031c65409cfd2801c7ac0d182936dc78" => :el_capitan
     sha256 "d9ccc19f1a55919408a1b27260b0404aa36dc6782a4a5964e6fd8409abf3b830" => :yosemite
+    sha256 "5ed3c456955fc6aae4658fc3165004dce567a5826dcd4df42011c8c95f159652" => :x86_64_linux
   end
 
   def install
