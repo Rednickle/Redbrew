7ec80f845d6934f9ef25434e5d50901bcbbf7576
Shaun Jackman <sjackman@gmail.com>
2017-03-27 23:08:49 -0700

    dialog: update 1.3-20170131 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#2195.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -10,6 +10,7 @@ class Dialog < Formula
     sha256 "15f85afce2306e17031370299bb97cdf2a6f8ede915fd2c19d8bb06847dbacdd" => :sierra
     sha256 "6042c7e4b069367cb607c97708eb097207f37230d7bc22fbc4e0924e20d134b3" => :el_capitan
     sha256 "3a5f37ec2481464ee96e383d619f1f28c14ed0b30645df91c1768dc6825592cf" => :yosemite
+    sha256 "b300fde81391d3ad875ead3ad8850d22fad1e30024f8d3be156051684f1a9935" => :x86_64_linux
   end
 
   depends_on "homebrew/dupes/ncurses" unless OS.mac?
