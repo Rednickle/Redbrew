008ed2b751b959b6a2119256f295b2d3ab51416f
Maxim Belkin <maxim.belkin@gmail.com>
2017-03-30 14:53:08 -0500

    elfutils: new version 0.168 and fixes
    Closes Linuxbrew/homebrew-core#2237
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/elfutils.rb
+++ Formula/elfutils.rb
@@ -1,17 +1,22 @@
 class Elfutils < Formula
   desc "Libraries and utilities for handling ELF objects."
   homepage "https://fedorahosted.org/elfutils/"
-  url "https://fedorahosted.org/releases/e/l/elfutils/0.163/elfutils-0.163.tar.bz2"
-  sha256 "7c774f1eef329309f3b05e730bdac50013155d437518a2ec0e24871d312f2e23"
+  url "https://sourceware.org/elfutils/ftp/0.168/elfutils-0.168.tar.bz2"
+  sha256 "b88d07893ba1373c7dd69a7855974706d05377766568a7d9002706d5de72c276"
   # tag "linuxbrew"
 
   bottle do
     sha256 "99c141bcce532d5f70a3754028faf3c6c93ed3e6c6861ad633173ad334afef5e" => :x86_64_linux
   end
 
+  option "with-valgrind", "Run tests with valgrind"
+
   depends_on "xz"
   depends_on "bzip2" unless OS.mac?
   depends_on "zlib" unless OS.mac?
+  depends_on "valgrind" => [:build, :optional]
+
+  conflicts_with "libelf",  :because => "both install `libelf.a` library"
 
   fails_with :clang do
     build 700
@@ -24,7 +29,10 @@ class Elfutils < Formula
       "--disable-dependency-tracking",
       "--disable-silent-rules",
       "--program-prefix=elfutils-",
-      "--prefix=#{prefix}"
+      "--prefix=#{prefix}",
+      *("--enable-valgrind" if build.with? "valgrind")
+    system "make"
+    system "make", "check"
     system "make", "install"
   end
 
