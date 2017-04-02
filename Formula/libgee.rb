1a52e4e2d9e6fbb382b96a9c328f6b4ee06fec60
Shaun Jackman <sjackman@gmail.com>
2017-04-02 08:37:20 -0700

    libgee: Fix test for Linuxbrew
    Closes Linuxbrew/homebrew-core#2259.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/libgee.rb
+++ Formula/libgee.rb
@@ -51,8 +51,8 @@ class Libgee < Formula
       -lgee-0.8
       -lglib-2.0
       -lgobject-2.0
-      -lintl
     ]
+    flags << "-lintl" if OS.mac?
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
