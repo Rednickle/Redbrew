4a885035b636a7d00fd7cce0d365e662cbd242b6
Shaun Jackman <sjackman@gmail.com>
2017-04-05 10:58:01 -0700

    ncurses: Fix dylib symlinks for Linuxbrew

--- Formula/ncurses.rb
+++ Formula/ncurses.rb
@@ -61,15 +61,26 @@ class Ncurses < Formula
     major = version.to_s.split(".")[0]
 
     %w[form menu ncurses panel].each do |name|
-      lib.install_symlink "lib#{name}w.#{major}.dylib" => "lib#{name}.dylib"
-      lib.install_symlink "lib#{name}w.#{major}.dylib" => "lib#{name}.#{major}.dylib"
+      if OS.mac?
+        lib.install_symlink "lib#{name}w.#{major}.dylib" => "lib#{name}.dylib"
+        lib.install_symlink "lib#{name}w.#{major}.dylib" => "lib#{name}.#{major}.dylib"
+      else
+        lib.install_symlink "lib#{name}w.so.#{major}" => "lib#{name}.so"
+        lib.install_symlink "lib#{name}w.so.#{major}" => "lib#{name}.so.#{major}"
+      end
       lib.install_symlink "lib#{name}w.a" => "lib#{name}.a"
       lib.install_symlink "lib#{name}w_g.a" => "lib#{name}_g.a"
     end
 
     lib.install_symlink "libncurses++w.a" => "libncurses++.a"
     lib.install_symlink "libncurses.a" => "libcurses.a"
-    lib.install_symlink "libncurses.dylib" => "libcurses.dylib"
+    if OS.mac?
+      lib.install_symlink "libncurses.dylib" => "libcurses.dylib"
+    else
+      lib.install_symlink "libncurses.so" => "libcurses.so"
+      lib.install_symlink "libncurses.so" => "libtermcap.so"
+      lib.install_symlink "libncurses.so" => "libtinfo.so"
+    end
 
     (lib/"pkgconfig").install_symlink "ncursesw.pc" => "ncurses.pc"
 
