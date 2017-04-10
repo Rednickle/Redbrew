cfc225f1530c3d82768a153b0eb8da56f72276d8
Bob W. Hogg <rwhogg@linux.com>
2017-04-10 08:30:26 -0700

    docker-compose: update 1.12.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#2336.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -12,6 +12,7 @@ class DockerCompose < Formula
     sha256 "a2b574e654393ce7590dc987f03b862d53da271413453187e5d759938e37e9d8" => :sierra
     sha256 "c206f3582891337e09e855a6f460e96e6373ba5d7a4b0cb2adf3964037f23d4c" => :el_capitan
     sha256 "9aed20bef54a106586a7b28c90b71d946adc2f7779ce01b5c407b92692290613" => :yosemite
+    sha256 "79f32337c59b1934d6fb9aad620021694c312106387e9c7c78e635369a3d740e" => :x86_64_linux
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
