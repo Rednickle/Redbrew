bac5f32a3ca215eb84e42a0e87e3edf65165edff
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-13 19:37:31 +0000

    lean-cli: update 0.7.3 bottle for Linuxbrew.

--- Formula/lean-cli.rb
+++ Formula/lean-cli.rb
@@ -10,6 +10,7 @@ class LeanCli < Formula
     sha256 "97224f89de293146b2d85a75f901dd7cb731a07907f29762a1702aa6de9c5d3e" => :sierra
     sha256 "3388e699dfc9f61e2a5240f65014922ecf0c7a7afad4cc51f416724ea60da6d6" => :el_capitan
     sha256 "b8a07390feabe9ec79df979281a7176a1e8f93540de1ae8e519c336ad61ef574" => :yosemite
+    sha256 "9c84a66203f0064d1bc96f06dabce598adb78baf8a72712d4736d74421b09ab7" => :x86_64_linux
   end
 
   depends_on "go" => :build
