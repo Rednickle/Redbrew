3ecbbb68795f27b58f736013925c4edeee32fa55
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-13 19:37:33 +0000

    ansifilter: update 2.4 bottle for Linuxbrew.

--- Formula/ansifilter.rb
+++ Formula/ansifilter.rb
@@ -9,6 +9,7 @@ class Ansifilter < Formula
     sha256 "72b1165dcfbe32608cd66e338d699bcf1bb3b484d0e7d8bcf4a60f9b40e942fc" => :sierra
     sha256 "a4224e9f94a2f5dd72ee72e37f19de493420f686846350c62259bd18e71c49f2" => :el_capitan
     sha256 "8e8cc89775c4ba044958da52448d3406f252888459e06af99876858794fa740a" => :yosemite
+    sha256 "9ef7573d77f39c6b03d2bcc932342a22e388f8b4cbc3e29a90dd4d5b5d688b00" => :x86_64_linux
   end
 
   def install
