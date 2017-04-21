bbe441d7b1f1f050aa44020cd5ddaaf3a310a981
Maxim Belkin <maxim.belkin@gmail.com>
2017-04-21 11:43:55 -0500

    procps: update to 3.3.12 and fixes for Linuxbrew
    Fixes:
      * depend on ncurses
      * depend on pkg-config
    
    Closes Linuxbrew/homebrew-core#2425
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/procps.rb
+++ Formula/procps.rb
@@ -1,8 +1,8 @@
 class Procps < Formula
   desc "Utilities for browsing procfs"
   homepage "https://gitlab.com/procps-ng/procps"
-  url "https://gitlab.com/procps-ng/procps/repository/archive.tar.gz?ref=v3.3.11"
-  sha256 "69e421cb07d5dfd38100b4b68714e9cb05d4fe58a7c5145c7b672d1ff08ca58b"
+  url "https://gitlab.com/procps-ng/procps/repository/archive.tar.gz?ref=v3.3.12"
+  sha256 "b1036c109f271c7c50325b11a748236f8e58aa1dbafefb30c995ff1d05b4a1a8"
   head "https://gitlab.com/procps-ng/procps.git"
   # tag "linuxbrew"
 
@@ -10,9 +10,11 @@ class Procps < Formula
     sha256 "96622dce78d108700b6a7b55c745eba71f923da26ed8d7466eb6109b02234c62" => :x86_64_linux
   end
 
+  depends_on "pkg-config" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "gettext" => :build
+  depends_on "ncurses"
 
   def install
     system "./autogen.sh"
