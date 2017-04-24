452ce5dd2f6b48922b0db0277da3a8c3f034dc12
Maxim Belkin <maxim.belkin@gmail.com>
2017-04-24 16:29:28 -0500

    libgeotiff: update 1.4.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#2441.
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libgeotiff.rb
+++ Formula/libgeotiff.rb
@@ -8,6 +8,7 @@ class Libgeotiff < Formula
     sha256 "194dc211dc45fe89183b53245ed494f4338afedaf65635202ae61df4495139aa" => :sierra
     sha256 "2256a133251e48d5df3719d1e83e9b2ac2e8be7bea41fcff25d036dce1e3cef7" => :el_capitan
     sha256 "0e283a6d16503c98c978092d98573f68a8d4e59aefe0b86f7ba6e6e3f33597c8" => :yosemite
+    sha256 "d908989d2bb74e97c919efa448680aa71cd9aabbc22337c903a7f7cc25e165d5" => :x86_64_linux
   end
 
   head do
