2e1895e32b5d2c1bdeadb62321f78cfe599dcb4d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-12 16:25:02 +0000

    wabt: update 0xd bottle for Linuxbrew.

--- Formula/wabt.rb
+++ Formula/wabt.rb
@@ -9,6 +9,7 @@ class Wabt < Formula
     sha256 "58b9c542ba305f58925927e69d4d82398c692edfd88763d87d0c92b4051cf255" => :sierra
     sha256 "5b44ad223cf74abb50649839fb7151e53875e87ef92bb2b5ec9ab006ea97a2b2" => :el_capitan
     sha256 "907457be2158b3a004b61f22db0497c47bf06e0af3b61c6778ad0e9e20ae12b4" => :yosemite
+    sha256 "919900f86040729e00e878057eeb0fa74b24bc68ffb563b252005b9470388e6f" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
