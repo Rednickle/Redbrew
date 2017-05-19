70eca4316e50b0edf1399ece18958c1fcf1ad0dc
Shaun Jackman <sjackman@gmail.com>
2017-05-19 08:23:02 -0700

    bmake: update 20170510 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#2667.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -8,6 +8,7 @@ class Bmake < Formula
     sha256 "3868471c0c312301d63f67d7580da16008a1ce5df3ee184eeba149dc9d4a3419" => :sierra
     sha256 "51fe93b1b3e62af08ed45b5d5a22bcc03bbb0d1fd0e87addc0662ebc2466d8b7" => :el_capitan
     sha256 "ebc184e50bb311c51f40af0b0358cbe910ab81933be3e2eaa9b11ca43e586450" => :yosemite
+    sha256 "8a14a01041e13837edc91a781428ee40bae54fea079db1047a9a90e5b4cecdf7" => :x86_64_linux
   end
 
   def install
