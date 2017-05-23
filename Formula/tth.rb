7c124d883813a66fa9600eb731be745fce4f2c86
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-23 23:05:07 +0000

    tth: update 4.09 bottle for Linuxbrew.

--- Formula/tth.rb
+++ Formula/tth.rb
@@ -9,6 +9,7 @@ class Tth < Formula
     sha256 "9f3771daa6780c062624717bd4ccf921267b2318781f86ed26ad213a3eb3b273" => :sierra
     sha256 "eb5c7de0bb431b91b4e2cb2d131ef6d0e9d8f4c7bd22dfc11d97078649413afb" => :el_capitan
     sha256 "ebd874131b52829584cf26db15f6199c133a3cf96b90e5a71985c4bac298f7ba" => :yosemite
+    sha256 "5206a9440cfcf2f7fc36089b676ca8bb5c3e3b5da8d08600d0d8d3d868724565" => :x86_64_linux
   end
 
   def install
