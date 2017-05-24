ede03038dfa66f3a33a0c46b786cbd7977b09ad5
Shaun Jackman <sjackman@gmail.com>
2017-05-24 14:45:44 -0700

    hyper: update 1.10.11 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#2716.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/hyper.rb
+++ Formula/hyper.rb
@@ -12,6 +12,7 @@ class Hyper < Formula
     sha256 "02a49cb75bb64e0b52251b13f344e5d35d12f32ecd61137afd066cf7ac755e7c" => :sierra
     sha256 "fb8056e9d46315ef17b8c00180bd77b37bbad5cdfc224138144ee435c5edc3e9" => :el_capitan
     sha256 "35c0c3b0d8953924091a9738d57de3c4a1b46c9d5bdec4329493d02a7dc94381" => :yosemite
+    sha256 "fa47e62b652b9b41b4823f12f7726109d786222631cbdf62a38bced7874562ee" => :x86_64_linux
   end
 
   depends_on "go" => :build
