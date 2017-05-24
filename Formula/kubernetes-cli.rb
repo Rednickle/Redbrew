94e1a7f3e1917175e010c3520327360daba112c5
Shaun Jackman <sjackman@gmail.com>
2017-05-24 14:46:23 -0700

    kubernetes-cli: update 1.6.4 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#2720.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/kubernetes-cli.rb
+++ Formula/kubernetes-cli.rb
@@ -11,6 +11,7 @@ class KubernetesCli < Formula
     sha256 "c75434193dbeef5aa056672149fd5e11e9ec7ddc72f4d139658370cef1657f37" => :sierra
     sha256 "1568c4630728384e2b95510e313090f788478ff39f873ab7c83b43be94864691" => :el_capitan
     sha256 "57eb0506413dc457a71d2246f4b22a83f4e9e92208003ff92f1fffd5b6384e4c" => :yosemite
+    sha256 "6e4d6737cf84fba1b3ef7b0b3c2f96e27e16e7ec4bd225e30ed6eb9c1ac707dd" => :x86_64_linux
   end
 
   depends_on "go" => :build
