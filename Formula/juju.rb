9ef63a6091b07ae5009efda8b5499ff5e6b1d59c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-05-28 20:10:30 +0000

    juju: update 2.1.3 bottle for Linuxbrew.

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -9,6 +9,7 @@ class Juju < Formula
     sha256 "c7e0c35c9264ababf10d1286234ed3756e96d7dc69f549a1880bebc760f4384b" => :sierra
     sha256 "5dcee49167f905d08975b27f7d25b94fd5c88437889c0e7308bdb0a81217ce7a" => :el_capitan
     sha256 "e7f179d5d9ba60634bbd0bfa7ffd1f10bc004bec78c779834808f920c0383fc7" => :yosemite
+    sha256 "011879ec749b96eecccc501477ba56b612fd0badd67509747b4c663a529acba5" => :x86_64_linux
   end
 
   devel do
