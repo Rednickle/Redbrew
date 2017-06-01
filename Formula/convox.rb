377338e08abc83c92544f5797da0d7573a57e33e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-01 19:19:31 +0000

    convox: update 20170531220422 bottle for Linuxbrew.

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -9,6 +9,7 @@ class Convox < Formula
     sha256 "a5d4aaed7bc40562a481e771d9193b15fcce162477a674570629f34857b1d4ca" => :sierra
     sha256 "a9f9856d68b32e04921dedd865b449e3e6f2c4ecaa5cb6041cbed89069ec9d96" => :el_capitan
     sha256 "62bdfdf18553027ecb5d27db3e30b150173a125e89600a9e0bb2883409b7ab69" => :yosemite
+    sha256 "7e714c4775658a44b526ec61298338573943a95c6fd3e911947469c361ae5c0d" => :x86_64_linux
   end
 
   depends_on "go" => :build
