9bcf9db38015476d50cff0f323a45623dbbbdee4
Shaun Jackman <sjackman@gmail.com>
2017-06-02 12:51:13 -0700

    roswell: update 17.5.8.78 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#2776.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -9,6 +9,7 @@ class Roswell < Formula
     sha256 "7aa2279a4c6ca84586f3cadc0ef00591e58dcf35e5032694eee0d2b70de622e0" => :sierra
     sha256 "349523e5e1f328fa2647a88a38db466bf209f56c923d247ee89e9df36d22dfc1" => :el_capitan
     sha256 "f5a7482fc23d1008f7be776f9ae1dc8498de2e33596d122c6e135634afa70fd7" => :yosemite
+    sha256 "92b92b78dfd0eeefd790156ea41c5b839e32c629475519f72a3873bec40c7272" => :x86_64_linux
   end
 
   depends_on "automake" => :build
