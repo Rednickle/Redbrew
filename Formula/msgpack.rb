c3e54d5a082ba917f8bdcd69b77e5b246b9056e9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-07 18:47:49 +0000

    msgpack: update 2.1.2 bottle for Linuxbrew.

--- Formula/msgpack.rb
+++ Formula/msgpack.rb
@@ -9,6 +9,7 @@ class Msgpack < Formula
     sha256 "064d3c8ba328543bd318981865f7122425e24b059e8c2afedf6c6bac4b26c4d4" => :sierra
     sha256 "f566a320cdbff585778035131c541c37446ed906f4f9a0576f82e2b6311b1566" => :el_capitan
     sha256 "cb8bd1b6fb71c3d33662746bbc4b45a0ccb5f269433286b30b56b4720591aa03" => :yosemite
+    sha256 "b1ddd8f840b778acc0ea228b0a6005dec12f29ea73a73f243a3b2587a8ee5612" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
