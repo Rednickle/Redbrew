866b46478c1825bf74c8eeeb3e2314b2d55a321b
Shaun Jackman <sjackman@gmail.com>
2017-06-14 10:57:55 -0700

    freetds: update 1.00.44 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#2931.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/freetds.rb
+++ Formula/freetds.rb
@@ -9,6 +9,7 @@ class Freetds < Formula
     sha256 "de47fd82cff91232230ad9637e0cccc04acb8c346d65b44ec34996bd5ea86f4c" => :sierra
     sha256 "f17b982e10f87e367780af21f1443b99102d648f9bfdd36068feda64aa6b494a" => :el_capitan
     sha256 "7252e0c301746e8cfb0a2b52a172fa2607c485f6ca097f66500bfc79e31da204" => :yosemite
+    sha256 "9af158db78ee76b1b47bf8b1bd8303470d78c49d6f440deaaf5f66a6e78f34cb" => :x86_64_linux
   end
 
   head do
