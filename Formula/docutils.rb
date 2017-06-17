24869373c5a9363029b752d1824b3becd8d7f041
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-17 06:04:51 +0000

    docutils: update 0.13.1_1 bottle for Linuxbrew.

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -12,6 +12,7 @@ class Docutils < Formula
     sha256 "6818d075546c76d51350b02b1ca003ca1ffe1b1eb36ba6ad82073ca65898087f" => :sierra
     sha256 "68facf0a5a3540d63e554dd5225683ca514193823468038d8ef0f6a7d7204c25" => :el_capitan
     sha256 "f889bbf58533bd947c0b5db906e7e1a8f3ec8439f13f6532b33f8d325a83a380" => :yosemite
+    sha256 "4aabe59f179ca81e0d3c095b2bb0660d71be89cbead3472d10b463051c8fbbec" => :x86_64_linux
   end
 
   depends_on "zlib" unless OS.mac?
