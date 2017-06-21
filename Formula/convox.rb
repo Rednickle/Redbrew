f0146eb68c050b5d963da4c22c05e19574fcdd2f
Shaun Jackman <sjackman@gmail.com>
2017-06-21 13:55:27 -0700

    convox: update 20170620151049 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#3014.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -9,6 +9,7 @@ class Convox < Formula
     sha256 "e6620ebfc0f25705fc5cdd1513f2a2d3ce702301b9b6899afd7f28d5ec788ae1" => :sierra
     sha256 "f6c5f95069cf1590194581b88d67e4bf68fd2536ea1bb1304e207ddcb56bb505" => :el_capitan
     sha256 "9bbf0f8a67d6d457e34ff8edcada02e974e4048f190043b32488469c98c1de9c" => :yosemite
+    sha256 "a0f664b055c33258e41d7beb32101b62b3f4ca29d783165e1056602e125ddf0e" => :x86_64_linux
   end
 
   depends_on "go" => :build
