f54281a496bb7d3dd2f46b2f3067193d05f5013b
Shaun Jackman <sjackman@gmail.com>
2017-06-28 16:49:31 -0700

    binutils: update 2.28 bottle for Linuxbrew.
    This portable bottle was built using glibc 2.5 on CentOS 5.

--- Formula/binutils.rb
+++ Formula/binutils.rb
@@ -11,6 +11,7 @@ class Binutils < Formula
     sha256 "ccacdbe617addcb6f41c2fba544077987ecd2dca62cf6c7884b0bfc639f99d45" => :sierra
     sha256 "7674e8693c4af0a738c721bbb530e432f5258d516a780c109c5d1ce0458e0de3" => :el_capitan
     sha256 "1b48a19196ea84d7c2d0fcfc933967b9d1596dd5f30935948ee0d5baaa9f6f65" => :yosemite
+    sha256 "77a433a64a86f48bbb0092f0fe505dc87f37edba6d39c1c0816156a92e29f545" => :x86_64_linux
   end
 
   # No --default-names option as it interferes with Homebrew builds.
