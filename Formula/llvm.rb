8b6c64f2db87a414e2b1957c3b50e22b4d333abe
Maxim Belkin <maxim.belkin@gmail.com>
2017-07-07 13:04:47 +0000

    llvm: strip binary files & fix style
    1. Fix style for "glibc" dependency
    2. Remove `if OS.linux?` for "binutils" dependency
      (Because it is within `unless OS.mac?`)
    3. Strip executables, libraries, and object files to reduce their size
    
    Closes Linuxbrew/homebrew-core#3163.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -157,8 +157,8 @@ class Llvm < Formula
 
   unless OS.mac?
     depends_on "gcc" # <atomic> is provided by gcc
-    depends_on "glibc" => GlibcRequirement.system_version.to_f >= 2.19 ? :optional : :recommended
-    depends_on "binutils" if OS.linux? # needed for gold plugin
+    depends_on "glibc" => (GlibcRequirement.system_version.to_f >= 2.19) ? :optional : :recommended
+    depends_on "binutils" # needed for gold and strip
     depends_on "libedit" # llvm requires <histedit.h>
     depends_on "ncurses"
     depends_on "libxml2"
@@ -326,6 +326,12 @@ class Llvm < Formula
     # libgomp.so conflicts with gcc.
     # libunwind.so conflcits with libunwind.
     rm [lib/"libgomp.so", lib/"libunwind.so"] if OS.linux?
+
+    # Strip executables/libraries/object files to reduce their size
+    system "strip", *(Dir[bin/"*", lib/"*"]).select do |f|
+      f = Pathname.new(f)
+      f.file? && (f.elf? || f.extname == ".a")
+    end unless OS.mac?
   end
 
   def caveats
