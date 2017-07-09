5d3358739d66cfd862bb224562259849911fdcfd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-09 10:48:17 +0000

    awscli: update 1.11.117 bottle for Linuxbrew.

--- Formula/awscli.rb
+++ Formula/awscli.rb
@@ -12,6 +12,7 @@ class Awscli < Formula
     sha256 "a2f1c9e84233b300a0db07f16f32bee791ac5e1d6a08f6b0dc2e0e260f719468" => :sierra
     sha256 "04de2777178e8ddcd61af3befa9d27b5a8b3424420b4d80b5d68c94dcc6582ac" => :el_capitan
     sha256 "64fcea9641458e81a121e75f240447c995d07d673097da2f7148c18fdabd985f" => :yosemite
+    sha256 "347a290c9d5c214e295d2d648058fc8df0494c8067a8c5c59b9f2153ec6e5df9" => :x86_64_linux
   end
 
   # Use :python on Lion to avoid urllib3 warning
