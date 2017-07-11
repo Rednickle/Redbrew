b147e632a4d70f31998a195760d404f309b71f60
Shaun Jackman <sjackman@gmail.com>
2017-07-11 08:03:02 -0700

    myman: update 2009-10-30 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#3229.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/myman.rb
+++ Formula/myman.rb
@@ -10,11 +10,13 @@ class Myman < Formula
     sha256 "376c71ad2f5abcc0233b3873d70cc963e54ac0ca00a552eceb025ac09b931ff6" => :sierra
     sha256 "d3b66de7eae03edecb2573524d94239bd013ffd57eeb1980411da12f6d2b2b98" => :el_capitan
     sha256 "b318e0b227a3ad281afe95edc5a0cc7ab0b1d5e46b1699e6221eb201de869b48" => :yosemite
+    sha256 "a34e620acc560cb86f943e84762cd51b37c865114ab522bed502cfd8b366fa84" => :x86_64_linux
   end
 
   depends_on "coreutils" => :build
   depends_on "gnu-sed" => :build
   depends_on "groff" => :build
+  depends_on "ncurses" unless OS.mac?
 
   def install
     ENV["RMDIR"] = "grmdir"
