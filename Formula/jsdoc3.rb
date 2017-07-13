c346fc4ba41f9027d6ac188508b47e4193eb13d4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-13 11:26:48 +0000

    jsdoc3: update 3.5.1 bottle for Linuxbrew.

--- Formula/jsdoc3.rb
+++ Formula/jsdoc3.rb
@@ -12,6 +12,7 @@ class Jsdoc3 < Formula
     sha256 "6492f8807cb2cd3beb24bc3f8d47b883794c05afa1ca939dca0b42bfedfd4032" => :sierra
     sha256 "ac9e284a94fd0c4395241956f7485ac8775432d2fa1cd97e3acf2d24374f3264" => :el_capitan
     sha256 "9e9a5edd5a22e467cfb4ad6e37b7383993042de4ff9b12f6815058e1f0a974aa" => :yosemite
+    sha256 "123e8fd841706506da8f7f80734449d5019c533d47aec46a7e57f93875ef60fd" => :x86_64_linux
   end
 
   depends_on "node"
