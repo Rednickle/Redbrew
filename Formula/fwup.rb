8fb88f8bb298f1f9c82f518049e78e27b1c74730
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-07-17 01:12:22 +0000

    fwup: update 0.15.3 bottle for Linuxbrew.

--- Formula/fwup.rb
+++ Formula/fwup.rb
@@ -9,6 +9,7 @@ class Fwup < Formula
     sha256 "819be337b851be08b0996bef273befc2d57ca8e4c8cad1540440620ff5772d62" => :sierra
     sha256 "e936d51d544f3cf906bead78b679328577be00dd02c875b147be9f9db408f91f" => :el_capitan
     sha256 "572d68bf7e3f7b1ce205d494ffc7ff1096f03790fa753ab9b830dd3ea163de0a" => :yosemite
+    sha256 "e0af385d9c46e1de7da664e433a56eaf6275cf5dfa8de49870869f4ba9a3b157" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
