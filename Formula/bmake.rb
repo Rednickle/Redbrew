6fd21a635e6d2091ac3972e9e701c53d0e4341d8
Bob W. Hogg <rwhogg@linux.com>
2017-07-21 18:37:20 -0700

    bmake: update 20170711 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#3332.
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -8,6 +8,7 @@ class Bmake < Formula
     sha256 "19500ae09004d53ebf685af9aef205457e809aa188cd148431a4ce463078075e" => :sierra
     sha256 "5eaafa69f0314746a18867b40a9d189ba360ec9ef0a5cf0aa16b53ecbcea57ff" => :el_capitan
     sha256 "3af8e2503f182b5023726658cce725a9434b7c0644a62fc6fff1e9e14d447d9d" => :yosemite
+    sha256 "5c9739649b8b8eba50e3b6635409a71bf5c8954467326490c85dcd026953176b" => :x86_64_linux
   end
 
   def install
