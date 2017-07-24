217f31ca94fa296819e1fdb678e84c86193b94a0
Bob W. Hogg <rwhogg@linux.com>
2017-07-23 21:13:53 -0700

    r: Restore Linux support
    Mostly missing dependencies and configure args.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/r.rb
+++ Formula/r.rb
@@ -4,6 +4,10 @@ class R < Formula
   url "https://cran.rstudio.com/src/base/R-3/R-3.4.1.tar.gz"
   sha256 "02b1135d15ea969a3582caeb95594a05e830a6debcdb5b85ed2d5836a6a3fc78"
 
+  # Do not remove executable permission from these scripts.
+  # See https://github.com/Linuxbrew/linuxbrew/issues/614
+  skip_clean "lib/R/bin" unless OS.mac?
+
   bottle do
     sha256 "ead3a96538eb9bade8990d67cf97bda800107887e0ebd8c0017410fdfa1a244c" => :sierra
     sha256 "93e5072a56a26fc212a9e617d36a786fe807c70e2487551f616484cede800623" => :el_capitan
@@ -18,7 +22,15 @@ class R < Formula
   depends_on "readline"
   depends_on "xz"
   depends_on :fortran
+
   depends_on "homebrew/science/openblas" => :optional
+
+  unless OS.mac?
+    depends_on "cairo"
+    depends_on "curl"
+    depends_on "tcl-tk" => :optional
+    depends_on :x11 => :recommended
+  end
 
   def install
     # Fix dyld: lazy symbol binding failed: Symbol not found: _clock_gettime
@@ -30,17 +42,33 @@ class R < Formula
     args = [
       "--prefix=#{prefix}",
       "--enable-memory-profiling",
-      "--without-cairo",
-      "--without-x",
-      "--with-aqua",
       "--with-lapack",
-      "SED=/usr/bin/sed", # don't remember Homebrew's sed shim
     ]
+
+    # don't remember Homebrew's sed shim
+    args << "SED=/usr/bin/sed" if File.exist?("/usr/bin/sed")
+
+    if OS.linux?
+      args << "--libdir=#{lib}" # avoid using lib64 on CentOS
+      args << "--enable-R-shlib"
+      args << "--with-cairo"
+
+      # FIXME: do I need to add a hack here for linuxbrew/xorg/xorg?
+      args << "--with-x" if build.with?("x11")
+
+      # If LDFLAGS contains any -L options, configure sets LD_LIBRARY_PATH to
+      # search those directories. Remove -LHOMEBREW_PREFIX/lib from LDFLAGS.
+      ENV.remove "LDFLAGS", "-L#{HOMEBREW_PREFIX}/lib"
+    elsif OS.mac?
+      args << "--without-cairo"
+      args << "--with-aqua"
+      args << "--without-x"
+    end
 
     if build.with? "openblas"
       args << "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas"
       ENV.append "LDFLAGS", "-L#{Formula["openblas"].opt_lib}"
-    else
+    elsif OS.mac?
       args << "--with-blas=-framework Accelerate"
       ENV.append_to_cflags "-D__ACCELERATE__" if ENV.compiler != :clang
     end
