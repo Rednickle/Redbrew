6cfac1c6144222071433ef0e5408a0344af7d149
Shaun Jackman <sjackman@gmail.com>
2017-07-29 20:57:21 -0700

    dub: update 1.4.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#3402.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -11,6 +11,7 @@ class Dub < Formula
     sha256 "d90795924f04dcd73daf64dae1f5c7ddb3c5515c83e3ab7a921ad92952ebc1b5" => :sierra
     sha256 "3e679d35a0c7e43321e49680b9bb3322cdbab28d662af211017d08236527f190" => :el_capitan
     sha256 "9b41c338802264a16d670bf1f0e90441ca4411992abcc8f9da33f49150785956" => :yosemite
+    sha256 "e2a573cc0b73edaad1b1f6f1036594e17e7baa874458e09cfbc880de82fcf29b" => :x86_64_linux
   end
 
   depends_on "pkg-config" => [:recommended, :run]
