7d5e32bf0155183f8c6275ac3b947798475a1c81
Michka Popoff <michkapopoff@gmail.com>
2017-08-01 22:30:59 +0200

    webpack: update 3.4.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#3503.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/webpack.rb
+++ Formula/webpack.rb
@@ -11,6 +11,7 @@ class Webpack < Formula
     sha256 "a5c79e78a9fb348a7c9931e4d6063d2942e37b68f89b8258d299c418203740f4" => :sierra
     sha256 "f5e08deb4cdd592c1e3b14b009aa85771689b8b4fd25557bd93a8d32965da9d1" => :el_capitan
     sha256 "eba8f6ebce7355575804e9509f61752407bd44b883859761f9dab8fae7fb09b8" => :yosemite
+    sha256 "7895beac5d83df67c0493dcd9eb348ad8f679043c1f02799ddcca7c93ab92aa2" => :x86_64_linux
   end
 
   depends_on "node"
