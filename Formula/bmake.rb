657368819cb5d34654a5fd7b899a5a1b2fc1704a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-01 06:08:36 +0000

    bmake: update 20170720 bottle for Linuxbrew.

--- Formula/bmake.rb
+++ Formula/bmake.rb
@@ -8,6 +8,7 @@ class Bmake < Formula
     sha256 "b140c0563d6dc5b1315883684449f99354f86cb6dfb0907ca8e71ced75cf8c8f" => :sierra
     sha256 "5a23861d3812765390abf05be7ccf367886e66495e6070a3f88b164b7522b90f" => :el_capitan
     sha256 "cb084669c4e47b33945e49f373f019ccaf69929b6c57c509c75a37d445dbc7b5" => :yosemite
+    sha256 "9e2ea06784a07c62e581a26f4632371904705b51f5b882800bcfa66c2e47c86b" => :x86_64_linux
   end
 
   def install
