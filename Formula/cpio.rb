cf3fe178cf1c772f6e86e6932120af85b94299a2
VonUniGE <VonUniGE@github.com>
2017-08-01 08:56:17 +0000

    cpio-2.12
    Update from cpio-2.11 to cpio-2.12
    The patch is removed because no more necessary.

--- Formula/cpio.rb
+++ Formula/cpio.rb
@@ -1,15 +1,10 @@
 class Cpio < Formula
   desc "Copies files into or out of a cpio or tar archive"
   homepage "https://www.gnu.org/software/cpio/"
-  url "https://ftpmirror.gnu.org/cpio/cpio-2.11.tar.bz2"
-  mirror "https://ftp.gnu.org/gnu/cpio/cpio-2.11.tar.bz2"
-  sha256 "bb820bfd96e74fc6ce43104f06fe733178517e7f5d1cdee553773e8eff7d5bbd"
+  url "https://ftp.gnu.org/gnu/cpio/cpio-2.12.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/cpio/cpio-2.12.tar.bz2"
+  sha256 "70998c5816ace8407c8b101c9ba1ffd3ebbecba1f5031046893307580ec1296e"
   # tag "linuxbrew"
-
-  # Fix the error:
-  # ./stdio.h:358:1: error: 'gets' undeclared here (not in a function)
-  # _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
-  patch :DATA
 
   def install
     system "./configure",
@@ -24,17 +19,3 @@ class Cpio < Formula
     system "#{bin}/cpio", "--version"
   end
 end
-
-__END__
---- a/gnu/stdio.in.h  2010-03-10 09:27:03.000000000 +0000
-+++ b/gnu/stdio.in.h  2014-11-08 16:56:30.000000000 +0000
-@@ -139,7 +139,9 @@
-    so any use of gets warrants an unconditional warning.  Assume it is
-    always declared, since it is required by C89.  */
- #undef gets
-+#ifdef HAVE_RAW_DECL_GETS
- _GL_WARN_ON_USE (gets, "gets is a security hole - use fgets instead");
-+#endif
- 
- #if @GNULIB_FOPEN@
- # if @REPLACE_FOPEN@
