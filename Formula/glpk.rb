52d39875c2207e0dc12dfeef08655ab2415ee16d
Dominique Orban <dominique.orban@gerad.ca>
2017-08-01 01:25:41 -0400

    glpk: update 4.62 bottle for Linuxbrew.
    Closes #3418.
    Signed-off-by: Dominique Orban <dominique.orban@gerad.ca>

--- Formula/glpk.rb
+++ Formula/glpk.rb
@@ -10,6 +10,7 @@ class Glpk < Formula
     sha256 "eb9e4fe20ad44eedc7e084493045fabeb6b4341f7c65c77ee7f338065d3bc2df" => :sierra
     sha256 "619f16ce7a6a0060171a9ad18fd60eb0d106628de1a5854a6a1851a189a60b71" => :el_capitan
     sha256 "a092c4d68cbf2625e6fbcda6a24201fd6fb820f44861005770ed0593f7c41f6f" => :yosemite
+    sha256 "1da04c75a8bef3ac707670b3ba7113d25fb2d12537a5a675f107e990b4f2932d" => :x86_64_linux
   end
 
   depends_on "gmp"
