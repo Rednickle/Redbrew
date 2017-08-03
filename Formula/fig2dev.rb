52ac36738672825c71f936e91a2cb58af4811c11
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-03 04:32:03 +0000

    fig2dev: update 3.2.6a bottle for Linuxbrew.

--- Formula/fig2dev.rb
+++ Formula/fig2dev.rb
@@ -8,6 +8,7 @@ class Fig2dev < Formula
     sha256 "31feda2231f672f7c15c2a74f9b392e0fa8378a0b7598010ed04e4a532f7a079" => :sierra
     sha256 "ab4631a22c7d46b00f5271c390e64cf07f7acd54cd382937b153d0507c7d568f" => :el_capitan
     sha256 "f144d58e99b3fa4b3798c30ba6c70b35b28c9840193357d89b748928200a4a48" => :yosemite
+    sha256 "1783b4bb62d38eb3180c1d1f5a211bed3ba46314cab81d205597f8313f257219" => :x86_64_linux
   end
 
   depends_on :x11
