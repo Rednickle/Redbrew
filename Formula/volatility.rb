43325a0ab8a02d43ae897319da6ea9dab4ed70c5
Bob W. Hogg <rwhogg@linux.com>
2017-08-07 22:39:28 -0700

    volatility: Fix for Linuxbrew
    Only force the use of the system zlib on macOS
    
    Closes Linuxbrew/homebrew-core#3707.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/volatility.rb
+++ Formula/volatility.rb
@@ -154,7 +154,8 @@ class Volatility < Formula
       inreplace "setup.py" do |s|
         sdkprefix = MacOS::CLT.installed? ? "" : MacOS.sdk_path
         s.gsub! "openjpeg.h", "probably_not_a_header_called_this_eh.h"
-        s.gsub! "ZLIB_ROOT = None", "ZLIB_ROOT = ('#{sdkprefix}/usr/lib', '#{sdkprefix}/usr/include')"
+        zlib_prefix = OS.mac? ? sdkprefix/"usr" : Formula["zlib"].opt_prefix
+        s.gsub! "ZLIB_ROOT = None", "ZLIB_ROOT = ('#{zlib_prefix}/lib', '#{zlib_prefix}/include')"
         s.gsub! "JPEG_ROOT = None", "JPEG_ROOT = ('#{Formula["jpeg"].opt_prefix}/lib', '#{Formula["jpeg"].opt_prefix}/include')"
         s.gsub! "FREETYPE_ROOT = None", "FREETYPE_ROOT = ('#{Formula["freetype"].opt_prefix}/lib', '#{Formula["freetype"].opt_prefix}/include')"
       end
