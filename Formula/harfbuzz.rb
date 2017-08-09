d1fbd36af3ee03e653cb8a0c9266d698565f4f8c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-09 20:26:42 +0000

    harfbuzz: update 1.4.8 bottle for Linuxbrew.

--- Formula/harfbuzz.rb
+++ Formula/harfbuzz.rb
@@ -8,6 +8,7 @@ class Harfbuzz < Formula
     sha256 "b8718336276a808df0bc6f7304fa141d806561a63f7e4fe55e40a5e817ab9fae" => :sierra
     sha256 "0aa772ec870adabd8d4ab32e0a320890b5342ea9eea30d9e92762767aad62364" => :el_capitan
     sha256 "44223a941491679f12df431cbbb122a5877e48f66e2587e785ba53716f55e139" => :yosemite
+    sha256 "282f01dbdca7465e5df5688e35b036268ec53d67c128edf0f3b3cd731f6605c5" => :x86_64_linux
   end
 
   head do
