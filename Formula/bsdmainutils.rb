a9528a74f14432518c0da66068163b96818b0a5d
Bob W. Hogg <rwhogg@linux.com>
2017-08-08 23:01:05 -0700

    bsdmainutils 9.0.12+nmu1
    Fix the audit while I'm at it.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/bsdmainutils.rb
+++ Formula/bsdmainutils.rb
@@ -1,9 +1,10 @@
 class Bsdmainutils < Formula
   desc "Collection of utilities from FreeBSD"
   homepage "https://packages.debian.org/sid/bsdmainutils"
-  url "http://ftp.debian.org/debian/pool/main/b/bsdmainutils/bsdmainutils_9.0.12.tar.gz"
-  mirror "https://mirror.csclub.uwaterloo.ca/debian/pool/main/b/bsdmainutils/bsdmainutils_9.0.12.tar.gz"
-  sha256 "9b6989932eabb43fa0137f4e5589e9b1ec70ea0249276a31dd311b3664bfc97f"
+  url "http://ftp.debian.org/debian/pool/main/b/bsdmainutils/bsdmainutils_9.0.12+nmu1.tar.gz"
+  mirror "https://mirror.csclub.uwaterloo.ca/debian/pool/main/b/bsdmainutils/bsdmainutils_9.0.12+nmu1.tar.gz"
+  version "9.0.12+nmu1"
+  sha256 "46ae19dcd28b2879379d70d149ea4fbe79b29d6c48e9ba4b576c5b38252043e8"
   # tag "linuxbrew"
 
   bottle do
@@ -11,12 +12,14 @@ class Bsdmainutils < Formula
     sha256 "b52153c929451ce34b8b0f3f28080b15477b74600527d5def404739f6f0779d6" => :x86_64_linux
   end
 
-  depends_on "ncurses" unless OS.mac?
-  depends_on "libbsd" unless OS.mac?
+  unless OS.mac?
+    depends_on "ncurses"
+    depends_on "libbsd"
+  end
 
   def install
-    File.open("debian/patches/series").each do |patch|
-      system "patch -p1 <debian/patches/#{patch}"
+    File.open("debian/patches/series") do |file|
+      file.each { |patch| system "patch -p1 <debian/patches/#{patch}" }
     end
     inreplace "Makefile", "/usr/", "#{prefix}/"
     inreplace "config.mk", "/usr/", "#{prefix}/"
