cc18e69072f8ee577e30cc85391ddd4a8931c0a0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-10 22:33:51 +0000

    mediaconch: update 17.07 bottle for Linuxbrew.

--- Formula/mediaconch.rb
+++ Formula/mediaconch.rb
@@ -6,10 +6,8 @@ class Mediaconch < Formula
   sha256 "c7d026dcff1b4734f62d10541765e78de80c52fc509660ecdc784a80f781e7e9"
 
   bottle do
-    cellar :any
-    sha256 "ccd656a1aae8d3bb12f9dfbe45fde69a0c9d345ac00cacc7e9194d82fcba840f" => :sierra
-    sha256 "7b94aafd6ffb9f7e21d027f8e07eb303dd4bb43a3cd0f9f63c80391d53adefcb" => :el_capitan
-    sha256 "b344b238ce82d76d434a3054568d6d268a4f9696f6095263713c9ef7057085a2" => :yosemite
+    cellar :any_skip_relocation
+    sha256 "4b695a97d50782d9a5d0d9c6aa45aeb36f59eef7defb1577f58cbdd79f4854d6" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
