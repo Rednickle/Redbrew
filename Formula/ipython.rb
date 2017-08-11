117c7644f81b75fd74e010475a8576e7267705f6
Bob W. Hogg <rwhogg@linux.com>
2017-08-10 18:32:58 -0700

    ipython: Fix for Linuxbrew
    Don't download appnope, which is only for macOS
    and refuses to install anywhere else.
    
    Closes Linuxbrew/homebrew-core#3766.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/ipython.rb
+++ Formula/ipython.rb
@@ -20,7 +20,7 @@ class Ipython < Formula
   resource "appnope" do
     url "https://files.pythonhosted.org/packages/26/34/0f3a5efac31f27fabce64645f8c609de9d925fe2915304d1a40f544cff0e/appnope-0.1.0.tar.gz"
     sha256 "8b995ffe925347a2138d7ac0fe77155e4311a0ea6d6da4f5128fe4b3cbe5ed71"
-  end
+  end if OS.mac?
 
   resource "decorator" do
     url "https://files.pythonhosted.org/packages/cc/ac/5a16f1fc0506ff72fcc8fd4e858e3a1c231f224ab79bb7c4c9b2094cc570/decorator-4.0.11.tar.gz"
