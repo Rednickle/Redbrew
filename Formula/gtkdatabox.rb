af01db78d030b31dbf14bd36f13a5bd5edbcdb46
Bob W. Hogg <rwhogg@linux.com>
2017-08-11 07:30:52 -0700

    gtkdatabox: Fix test for Linuxbrew
    Only link against -lintl on macOS,
    and use the X11 versions of GDK
    and GTK+.
    
    Closes Linuxbrew/homebrew-core#3770.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Formula/gtkdatabox.rb
+++ Formula/gtkdatabox.rb
@@ -66,17 +66,17 @@ class Gtkdatabox < Formula
       -L#{pango.opt_lib}
       -latk-1.0
       -lcairo
-      -lgdk-quartz-2.0
+      -lgdk-#{OS.mac? ? "quartz" : "x11"}-2.0
       -lgdk_pixbuf-2.0
       -lgio-2.0
       -lglib-2.0
       -lgobject-2.0
-      -lgtk-quartz-2.0
+      -lgtk-#{OS.mac? ? "quartz" : "x11"}-2.0
       -lgtkdatabox
-      -lintl
       -lpango-1.0
       -lpangocairo-1.0
     ]
+    flags << "-lintl" if OS.mac?
     system ENV.cc, "test.c", "-o", "test", *flags
     system "./test"
   end
