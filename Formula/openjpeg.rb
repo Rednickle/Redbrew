add94c7bcb9835b82c574dacde15aafed74364ce
Michka Popoff <michkapopoff@gmail.com>
2017-08-12 13:34:24 +0200

    openjpeg: update 2.2.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#3751.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/openjpeg.rb
+++ Formula/openjpeg.rb
@@ -10,6 +10,7 @@ class Openjpeg < Formula
     sha256 "437b7f58d8f2e8944adea7481a233bdf7f5c06609bfcf209169e677c93ab621c" => :sierra
     sha256 "ceebb6f74ce06b2a9ea716cd6f72bdbe4590b23819e4d0a980a320ff150760bd" => :el_capitan
     sha256 "bd0c66eb1f759d447a35203a0861698283ee148c97b96ed13922d83adaab4ab7" => :yosemite
+    sha256 "8019678399b84b81e65463e3231ef78167895956b991be5874c8d4e8350121a8" => :x86_64_linux
   end
 
   option "without-doxygen", "Do not build HTML documentation."
@@ -44,8 +45,8 @@ class Openjpeg < Formula
         return 0;
       }
     EOS
-    system ENV.cc, "-I#{include}/openjpeg-2.2", "-L#{lib}", "-lopenjp2",
-           testpath/"test.c", "-o", "test"
+    system ENV.cc, "-I#{include}/openjpeg-2.2",
+           testpath/"test.c", "-L#{lib}", "-lopenjp2", "-o", "test"
     system "./test"
   end
 end
