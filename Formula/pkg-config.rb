2699b440bee67cad055d043b2ca53c3b05c19381
Shaun Jackman <sjackman@gmail.com>
2017-08-13 11:13:21 -0700

    pkg-config: Bump revision for Linuxbrew
    Closes Linuxbrew/homebrew-core#3813.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/pkg-config.rb
+++ Formula/pkg-config.rb
@@ -4,6 +4,7 @@ class PkgConfig < Formula
   url "https://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz"
   mirror "https://dl.bintray.com/homebrew/mirror/pkg-config-0.29.2.tar.gz"
   sha256 "6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "8eb723bfc03cd468d779d54d015d47d2e8ab1dd4d35e595ab4abaca8833b3277" => :sierra
