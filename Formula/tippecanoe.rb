104b383d500153b73c8ea3f95c1b35323601de16
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-17 22:38:22 +0000

    tippecanoe: update 1.22.1 bottle for Linuxbrew.

--- Formula/tippecanoe.rb
+++ Formula/tippecanoe.rb
@@ -9,6 +9,7 @@ class Tippecanoe < Formula
     sha256 "20f716e335cf51eea04726b05c8caf3cfe66184d56b16d44f6712184e3bd90d6" => :sierra
     sha256 "7468a2708b26bb84d6c92e83f0923a5973c56954620d4a8fb2844347014b874c" => :el_capitan
     sha256 "a8b095ca7c495f14a299a6d78a3396789ff3e21f0ca1efdd5f203cda8b934a33" => :yosemite
+    sha256 "b50cd03c0d565e4adafab3a1880056a726d805403859d5b9b86429284ccc76dc" => :x86_64_linux
   end
 
   unless OS.mac?
