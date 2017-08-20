90b002aa52f73a550d2d5072a993aad17d6061d5
Michka Popoff <michkapopoff@gmail.com>
2017-08-20 21:30:39 +0200

    datamash: update 1.1.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#3919.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/datamash.rb
+++ Formula/datamash.rb
@@ -10,6 +10,7 @@ class Datamash < Formula
     sha256 "6b0e4f5e26b38b4db687a3e9eaeb103ef580461a40ddbc31af1eaf0758d54efa" => :sierra
     sha256 "296cf7d1f291f1ffb0f6f877d1d1a7724877195f0a53e7abdeb7b91389742757" => :el_capitan
     sha256 "734d31b032f5dc7ed2968c20d04938f9b028ed3e3b9587efc548bdfba2d589ce" => :yosemite
+    sha256 "3c95905432f18c74aeb29b9ad8bb3ba2dfe59514c47e9e1d88dab1ad47e5cca1" => :x86_64_linux
   end
 
   head do
