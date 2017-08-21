a619fa9c54b9675728674f3e096333d151f51206
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-21 18:27:32 +0000

    libxlsxwriter: update 0.7.4 bottle for Linuxbrew.

--- Formula/libxlsxwriter.rb
+++ Formula/libxlsxwriter.rb
@@ -9,6 +9,7 @@ class Libxlsxwriter < Formula
     sha256 "a0ad27b0aaaf650c15eca6115e7559ceaf3d75b4d16cbc29ed58672608200859" => :sierra
     sha256 "8e3a337398602bf37e5100e95613a5ab7f300c341c5ee72271402e3eb22b9481" => :el_capitan
     sha256 "378013e1ca6dabf13e069c61ed997a5ca07264f31ef5ad65dae9e267ff98737e" => :yosemite
+    sha256 "bfa8b28caaedb316db56c1cb6fe53ba57e849d5efb432b45e57a941bae44b026" => :x86_64_linux
   end
 
   depends_on "zlib" unless OS.mac?
