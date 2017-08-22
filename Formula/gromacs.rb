cfe1c189f56cfc9af366b580997d6bc29de4804e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-21 21:50:08 -0700

    gromacs: update 2016.3 bottle.

--- Formula/gromacs.rb
+++ Formula/gromacs.rb
@@ -5,9 +5,10 @@ class Gromacs < Formula
   sha256 "7bf00e74a9d38b7cef9356141d20e4ba9387289cbbfd4d11be479ef932d77d27"
 
   bottle do
-    sha256 "90faa8b279f78823dae87bea50c3f1e88f9ede89678fb3f1711f643f576359a1" => :sierra
-    sha256 "10f94061644f511939db8cc4d4c06ee767901c7aebd54c95872cc547b85826f7" => :el_capitan
-    sha256 "3b93cbd26c93a4abb5401b7d87bfc8cb27d4b9641acecf47c26675f0fbd0f8e6" => :yosemite
+    root_url "https://linuxbrew.bintray.com/bottles"
+    prefix "/home/linuxbrew/.linuxbrew"
+    cellar "/home/linuxbrew/.linuxbrew/Cellar"
+    sha256 "1cceab50734d5240051aafaa44cd40726a996de3e92ef523c5e347e15d304ad8" => :x86_64_linux
   end
 
   option "with-double", "Enables double precision"
