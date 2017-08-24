863570320823f0d4f431a4c22bb4dde4d320b989
Shaun Jackman <sjackman@gmail.com>
2017-08-23 18:35:40 -0700

    bastet: remove bottle for boost for Linuxbrew

--- Formula/bastet.rb
+++ Formula/bastet.rb
@@ -8,7 +8,6 @@ class Bastet < Formula
     sha256 "debdb55e854497dec7fcf2b8324513ae435ba52df6f963608ec97519368b1587" => :sierra
     sha256 "6480a6a2fcb155c29db0baf135a9cb8d31ef7540866a2f9bb9bfc6d2b50a7690" => :el_capitan
     sha256 "dab9a1b75d5da4c467b37dc49b76a0bbe5c9202eaeb321d60a341ae7cd256098" => :yosemite
-    sha256 "f2b62465a3e3d1a0a943bcf853a9d27e6db53b0a4f227faa5e6f6e90c005b33f" => :x86_64_linux
   end
 
   depends_on "boost"
