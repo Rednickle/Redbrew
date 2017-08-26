090b255f2260a41caa095a2bf02361fbe923aeba
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-08-26 22:24:51 +0000

    corebird: update 1.6 bottle for Linuxbrew.

--- Formula/corebird.rb
+++ Formula/corebird.rb
@@ -8,6 +8,7 @@ class Corebird < Formula
     sha256 "9e5d3a0bec0d58044b35eafc2a6434440c7a88d8f6fdcb8498e60737fbb0ab79" => :sierra
     sha256 "0f56d671657ca272fe9a4ffab69e7c4e8f8fb9de74aacf3a3230cee00c973085" => :el_capitan
     sha256 "7f735dbdb4262c8c11ea67ff8de771eb1e4f4dfa3391c42c4904a41732a38b56" => :yosemite
+    sha256 "2f9a23d786f4c7bfddd4345334a21e428ce9208e13b58ea0f401088ece3968f9" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
