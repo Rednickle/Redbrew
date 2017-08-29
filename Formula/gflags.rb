82b96ef01a5a7076977ce0ee0011bd2ac9a32385
Michka Popoff <michkapopoff@gmail.com>
2017-08-29 23:02:24 +0200

    gflags: Bump revision for g++-5
    Closes Linuxbrew/homebrew-core#4041.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gflags.rb
+++ Formula/gflags.rb
@@ -3,13 +3,13 @@ class Gflags < Formula
   homepage "https://gflags.github.io/gflags/"
   url "https://github.com/gflags/gflags/archive/v2.2.1.tar.gz"
   sha256 "ae27cdbcd6a2f935baa78e4f21f675649271634c092b1be01469440495609d0e"
+  revision 1 unless OS.mac? # g++-5
 
   bottle do
     cellar :any
     sha256 "519562ecebe66cf89803786d0b0ba9ac02cfa2376c822c8726fa274c1e380d0a" => :sierra
     sha256 "9e9c1a067b324ccd372ac00cd0ac00545415cb4407ea90b15c181cbfb67a4260" => :el_capitan
     sha256 "5e6fcff184b2d6caf333a74cb24222da11bc1721eb6ab12a31bda7802cf8dfd9" => :yosemite
-    sha256 "28ad8eee5bbbb8533ba88097b512b39aa8dc893bb9447a6fc97b9fa002e1daf1" => :x86_64_linux # glibc 2.19
   end
 
   option "with-debug", "Build debug version"
