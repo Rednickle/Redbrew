86293cb4f95ff2c580ebba1a48829b264b2de44b
Michka Popoff <michkapopoff@gmail.com>
2017-08-29 22:55:28 +0200

    glog: Bump revision for g++-5
    Closes Linuxbrew/homebrew-core#4040.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/glog.rb
+++ Formula/glog.rb
@@ -3,14 +3,13 @@ class Glog < Formula
   homepage "https://github.com/google/glog"
   url "https://github.com/google/glog/archive/v0.3.5.tar.gz"
   sha256 "7580e408a2c0b5a89ca214739978ce6ff480b5e7d8d7698a2aa92fadc484d1e0"
-  revision 1
+  revision OS.mac? ? 1 : 2 # g++-5
 
   bottle do
     cellar :any
     sha256 "c5ed5359196732957cfabbe516c87437df4969547471d8ba6dff78414ab0feb8" => :sierra
     sha256 "2a7a2aef322dfd7431221bb8b5807c67d06bbe8d6407d44ed6cedceda8a45098" => :el_capitan
     sha256 "ccb8f5022bffec4a768851feb5a8cc1bf52ee7d9b8b87b8f9ce6bc4cd28278f9" => :yosemite
-    sha256 "3bbee4f46928ab46890ce6eabed9d746c0d50e3df0a7cb57a6ebd69f17f7a856" => :x86_64_linux # glibc 2.19
   end
 
   depends_on "gflags"
