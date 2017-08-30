87b6475b8f524258afebb57d269288fd11363351
Michka Popoff <michkapopoff@gmail.com>
2017-08-30 17:45:10 +0200

    s6: update 2.6.1.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#4054.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/s6.rb
+++ Formula/s6.rb
@@ -21,6 +21,7 @@ class S6 < Formula
     sha256 "b310708d3412090fc959c0517882804b5b17c9f9500b3f6d4aca8c3fc180c4bd" => :sierra
     sha256 "c9c42a57a5c90ab67a52a295e7643e854c3696a7cc4eac122ebe25490815bf2b" => :el_capitan
     sha256 "4a5760024698c19b23460ac840d166936e3e61bd30d71e28019d35a99d5ac017" => :yosemite
+    sha256 "b0b14fbb9399754fe66e1ada130d86dcb0a20b4b83397259904b4aadfac26fb7" => :x86_64_linux
   end
 
   head do
