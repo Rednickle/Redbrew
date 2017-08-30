351d4b75a4e81f1f1bf2509d6ac68d5f0d334790
Michka Popoff <michkapopoff@gmail.com>
2017-08-30 17:42:57 +0200

    kube-aws: update 0.9.8 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#4052.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/kube-aws.rb
+++ Formula/kube-aws.rb
@@ -10,6 +10,7 @@ class KubeAws < Formula
     sha256 "43b4567874d330c91191d155c699c9a7b5522246bc5fd204954182a5f9a04b50" => :sierra
     sha256 "c27239463b5d9d28c3adaa0bf5e112637b3a266bf28bfb97b076d952c9d24e53" => :el_capitan
     sha256 "4bde1b4c7934815860f2e94731b9e70843f6d2bcf79a886f96dcb9f47be1d057" => :yosemite
+    sha256 "542ae5473d2f076713e9d10235508a172fdbc778282ac7884e766b7b00310f0c" => :x86_64_linux
   end
 
   depends_on "go" => :build
