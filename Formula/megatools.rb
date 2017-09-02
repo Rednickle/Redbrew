578cb5a3e565c03c4615649c72e1a68012140f61
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-02 03:13:06 +0000

    megatools: update 1.9.98 bottle for Linuxbrew.

--- Formula/megatools.rb
+++ Formula/megatools.rb
@@ -9,6 +9,7 @@ class Megatools < Formula
     sha256 "570c02df45849ba7a223391ebb9df3bc31faabeb399a902256f013dbef167441" => :sierra
     sha256 "8a661afef3e014425b600bb65c4e20a3e71cd96b179e9d86cfde5e974a596d0a" => :el_capitan
     sha256 "26b90b76a9e2170b0c336d4175eff71665bc4f606e7ed4a86e66d3170fd4c4cf" => :yosemite
+    sha256 "7ceca85be24834ffa98b2813996548d46fdc8b05e103b2d1ebeac97d39a9e670" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
