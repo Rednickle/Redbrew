2df52bca775aa04f5c17a0ff051027f7ac7bf836
Michka Popoff <michkapopoff@gmail.com>
2017-09-03 10:18:03 +0200

    watchman: Reduce memory usage below 4 GB for Circle CI
    Closes Linuxbrew/homebrew-core#4114.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/watchman.rb
+++ Formula/watchman.rb
@@ -28,6 +28,9 @@ class Watchman < Formula
   depends_on "pcre"
 
   def install
+    # Reduce memory usage below 4 GB for Circle CI.
+    ENV["MAKEFLAGS"] = "-j16" if ENV["CIRCLECI"]
+
     system "./autogen.sh"
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
