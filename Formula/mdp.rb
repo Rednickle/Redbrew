ab074afc7c253dd0d27abbe3b6bcfae022d862cc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-03 20:58:24 +0000

    mdp: update 1.0.10 bottle for Linuxbrew.

--- Formula/mdp.rb
+++ Formula/mdp.rb
@@ -10,6 +10,7 @@ class Mdp < Formula
     sha256 "5ede61a6bd6ece5dfc361f03b8bb9ef11fec560e4d49e2db66fd9d1df5e9693f" => :sierra
     sha256 "aae896e1e1b4b5615d0440cfc92e541dabcc760d7a9f60527655fbcb7c61ac93" => :el_capitan
     sha256 "1518c8a1c67a38cd92053823e21712712623d3637497f86b65807b982919d0c0" => :yosemite
+    sha256 "6758e2926bb882485831fbe5f25d9e3523e22ce3ab172dbc6fcf5df1d7c0fa46" => :x86_64_linux
   end
 
   def install
