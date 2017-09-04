b453b616195fa2f46dc5b1ec72df800d032f1d3d
Michka Popoff <michkapopoff@gmail.com>
2017-09-04 21:30:27 +0200

    vtk: update 8.0.1 bottle for Linuxbrew.

--- Formula/vtk.rb
+++ Formula/vtk.rb
@@ -10,6 +10,7 @@ class Vtk < Formula
     sha256 "fc0c622b9cbe231f6bd6ae06f6ab39ccef01f95b28238439d8cf02041a56204b" => :sierra
     sha256 "3aa06ce8d7e3f1d781d56018f2daaea1705b9440c5b5b6eabb826dfe946749c3" => :el_capitan
     sha256 "0ec79613df46e73d166fe6341c5697ea9f6f84be1f933c0d334c436b8481148e" => :yosemite
+    sha256 "eb040479efa650e4d91a8ebe7e0a343a6a40e8eef9b1f822f91467782a97261b" => :x86_64_linux
   end
 
   option "without-python", "Build without python2 support"
