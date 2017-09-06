da34f523d1e57f4972b0dcf2ce97d7a59b51683a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-06 08:40:39 +0000

    avro-c: update 1.8.2 bottle for Linuxbrew.

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -9,6 +9,7 @@ class AvroC < Formula
     sha256 "babc65c0442620b00470abdda6d86d7035390b75b6ae113319c69df1fc24dc8b" => :sierra
     sha256 "52ba34bf67cee89d559bd075baff35e7be512b5f53419783a82c1f0cf2ca3f8a" => :el_capitan
     sha256 "f77867d708068930b40fb0e3e4e5bd5146487699599e87d4987c205fb6a3275d" => :yosemite
+    sha256 "d9352232d9e1a933fbab4a0c636341481d340f0de1105184669445569d9da5ad" => :x86_64_linux
   end
 
   option "with-snappy", "Build with Snappy codec support"
