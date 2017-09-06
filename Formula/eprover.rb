689d497ccd17c96a8523f0ea52d2e7f3f7df2532
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-06 18:24:04 +0000

    eprover: update 2.0 bottle for Linuxbrew.

--- Formula/eprover.rb
+++ Formula/eprover.rb
@@ -9,6 +9,7 @@ class Eprover < Formula
     sha256 "fffcc75e400937600fd2c059ecb5427476b216af5c56249c756400de4906885b" => :sierra
     sha256 "8d3dd0289c6f77d68dc2e1c9f06c039e106f2b6e324788315815a8c37b433e9c" => :el_capitan
     sha256 "6d13db7618b7a1d3e567165000a81d34384941c0f3de51177cf7b2a9b8f39ce7" => :yosemite
+    sha256 "81a487bf782e422d27969e6c63c00a2a037ff79126feceb091e3a3a0f5132e0e" => :x86_64_linux
   end
 
   def install
