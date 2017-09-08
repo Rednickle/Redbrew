7af49b819e62e6c47693ae695e33ec10c6428737
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-08 18:49:23 +0000

    hfstospell: update 0.4.5 bottle for Linuxbrew.

--- Formula/hfstospell.rb
+++ Formula/hfstospell.rb
@@ -9,6 +9,7 @@ class Hfstospell < Formula
     sha256 "6d42bafd9ece439af37329183b65644973e8a64bf1b4a93a13fe543d8ecdbe38" => :sierra
     sha256 "e6c21f06dc9a48e9106b26598db0fad40f4cf98310818e4ec1659ca5228614e9" => :el_capitan
     sha256 "749db395f939581fbf9c7302caea13f7defab3d14d76e8829e0d2e50f7673558" => :yosemite
+    sha256 "754279b3d2e133433fe8f0dd6c37acf10dc2282534d3837e77d81124ff61c9ef" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
