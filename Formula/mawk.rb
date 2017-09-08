d1f4efa207c22b303f023e025653ab042e2d8250
Michka Popoff <michkapopoff@gmail.com>
2017-09-08 08:29:58 +0200

    mawk: update 1.3.4-20161120 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#4155.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mawk.rb
+++ Formula/mawk.rb
@@ -9,6 +9,7 @@ class Mawk < Formula
     sha256 "52d61963ba6494674cdea5d3fd95e9551b8277b9bc5e189f93b1126d5ae3dd27" => :sierra
     sha256 "d39e71dfe41cdf2e480a5858938918509e2a00e6b5dcb181d14fc5cdedd91b24" => :el_capitan
     sha256 "8ef45e294dbab1b4c4615861b6eee4fdd179649072fe41eb554e6bc93f2f5a42" => :yosemite
+    sha256 "ef4c2554e2983346830743ff7ecf2714bb8d86adbc86be3e660da77ea860f17a" => :x86_64_linux
   end
 
   def install
