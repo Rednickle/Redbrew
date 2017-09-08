146243d7a45e82f5fc7bbce399bddb405d51fc52
Alyssa Ross <hi@alyssa.is>
2017-09-08 01:08:41 +0100

    gmp: update to 6.1.2 for Linuxbrew
    gmp: depends on m4 for Linuxbrew
    
    Without this, building gmp from source failed on my Fedora machine.
    
    Closes Linuxbrew/homebrew-core#4157.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gmp.rb
+++ Formula/gmp.rb
@@ -1,9 +1,9 @@
 class Gmp < Formula
   desc "GNU multiple precision arithmetic library"
   homepage "https://gmplib.org/"
-  url "https://gmplib.org/download/gmp/gmp-6.1.1.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/gmp/gmp-6.1.1.tar.xz"
-  sha256 "d36e9c05df488ad630fff17edb50051d6432357f9ce04e34a09b3d818825e831"
+  url "https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.xz"
+  sha256 "87b565e89a9a684fe4ebeeddb8399dce2599f9c9049854ca8c0dfbdea0e21912"
 
   bottle do
     cellar :any
@@ -11,19 +11,22 @@ class Gmp < Formula
     sha256 "cd4a916966007092af477a76655cc1f66546d00bf5e581a5dfef334f8436aeb0" => :sierra
     sha256 "01b24de832db7aa24ee14159feb5a16e0e3e18932e6f38d221331bb45feb6a1a" => :el_capitan
     sha256 "3752709f0bab1999fa9d5407bcd3135a873b48fc34d5e6ea123fd68c4cf3644d" => :yosemite
-    sha256 "a9b92c1ac7cc79df39c5bf7f3c2b0abeb26b9bc417c13d55e3628058aaaa301c" => :x86_64_linux # glibc 2.19
   end
+
+  depends_on "m4" unless OS.mac?
 
   option :cxx11
 
   def install
     ENV.cxx11 if build.cxx11?
     args = %W[--prefix=#{prefix} --enable-cxx]
+
     if OS.mac?
       args << "--build=core2-apple-darwin#{`uname -r`.to_i}" if build.bottle?
-    else
-      args << "ABI=32" if Hardware::CPU.intel? && Hardware::CPU.is_32_bit?
+    elsif Hardware::CPU.intel? && Hardware::CPU.is_32_bit?
+      args << "ABI=32"
     end
+
     system "./configure", *args
     system "make"
     system "make", "check"
