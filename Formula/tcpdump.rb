2e601434c2f19513a020438962e926082e58e812
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-10 08:58:17 +0000

    tcpdump: update 4.9.2 bottle for Linuxbrew.

--- Formula/tcpdump.rb
+++ Formula/tcpdump.rb
@@ -11,6 +11,7 @@ class Tcpdump < Formula
     sha256 "f383f086f232e06e01c9d206c98f65d9df5109366f13ba684910b8e249e35a6e" => :sierra
     sha256 "10486fd04e4b4df5f7fbd2b9aba3d48c903730c53df3ee9b7f57887db0347df8" => :el_capitan
     sha256 "97fc8337c3808fa208b72f1eea5eea6d53bf67c083ca6b1b3ddf751b8342c574" => :yosemite
+    sha256 "d5c62a9c93c5935974994d3df90cc8e8b18afc29720c316de3e3cc755c4f0051" => :x86_64_linux
   end
 
   depends_on "openssl"
