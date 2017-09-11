51437c454c57e0c9fafced11f14131f5d3c9839b
Michka Popoff <michkapopoff@gmail.com>
2017-09-11 20:20:15 +0200

    iozone: Build a bottle for Linuxbrew
    Remove patches that should have been removed
    during the last core merge
    
    Fix build target

--- Formula/iozone.rb
+++ Formula/iozone.rb
@@ -10,20 +10,9 @@ class Iozone < Formula
     sha256 "aa525afb22a1c4909ffa5e6d74474431f91ee6cb5997f8bba7e02884f294bd7b" => :el_capitan
   end
 
-  # Patch by @nijotz, adds O_DIRECT support when using -I flag.
-  # See: https://github.com/Homebrew/homebrew/pull/10585
-  patch :DATA
-
-  # Fix build failure "error: conflicting types for 'mythread_create'"
-  # Reported 10 Sep 2017 to capps AT iozone DOT org
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/56c6104/iozone/mythread_create.diff"
-    sha256 "ec1a1be0d0096c29711f2edba50c8183cfde886a55f18b41c2c7d1580d8f68c8"
-  end
-
   def install
     cd "src/current" do
-      system "make", "macosx", "CC=#{ENV.cc}"
+      system "make", OS.mac? ? "macosx" : "linux", "CC=#{ENV.cc}"
       bin.install "iozone"
       pkgshare.install %w[Generate_Graphs client_list gengnuplot.sh gnu3d.dem
                           gnuplot.dem gnuplotps.dem iozone_visualizer.pl
