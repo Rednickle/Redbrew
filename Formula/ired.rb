ccc59966cc98ae9b27b097bce9e0a9d4487436e0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-12 22:36:51 +0000

    ired: update 0.6 bottle for Linuxbrew.

--- Formula/ired.rb
+++ Formula/ired.rb
@@ -8,6 +8,7 @@ class Ired < Formula
     cellar :any_skip_relocation
     sha256 "5d10dfac87e4a4ca589a9fa76e8f9aff62625ef6358b6ab29360e79fe4a6dc35" => :sierra
     sha256 "4fc558225913b629b144661385841e63ebb167beb9900475fadb0c0e886b4997" => :el_capitan
+    sha256 "de7d82b64115f1e9f84b6bdcedf8d2bbff7730092d6f8df50e3c4613b0224608" => :x86_64_linux
   end
 
   def install
