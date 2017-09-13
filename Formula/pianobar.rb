3a62805d7711cd9e220fcb9e7b76fb8a0881771a
Michka Popoff <michkapopoff@gmail.com>
2017-09-13 19:54:35 +0200

    pianobar: Add curl dependency for Linuxbrew
    Closes Linuxbrew/homebrew-core#4241.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pianobar.rb
+++ Formula/pianobar.rb
@@ -4,13 +4,13 @@ class Pianobar < Formula
   url "https://6xq.net/pianobar/pianobar-2017.08.30.tar.bz2"
   sha256 "ec14db6cf1a7dbc1d8190b5ca0d256021e970587bcdaeb23904d4bca71a04674"
   head "https://github.com/PromyLOPh/pianobar.git"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any
     sha256 "327b53a2b9e2fd6824d0c854b5ea411b29b07ba5804b443c453aee05a86e36d4" => :sierra
     sha256 "ea582da39bf90a0122d74678cdf82349af53adcbab48ff5bd2d57c64ca16150f" => :el_capitan
     sha256 "41d44c9f1680fa404639ae9b907df30bbb019b1628454e8d77eba95af6a1a883" => :yosemite
-    sha256 "406d403ca6f2818fe073a504f3de5e10721b2820a58dd664ebb482eb12f0b341" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
@@ -21,6 +21,7 @@ class Pianobar < Formula
   depends_on "libgcrypt"
   depends_on "json-c"
   depends_on "ffmpeg"
+  depends_on "curl" unless OS.mac?
 
   def install
     # Discard Homebrew's CFLAGS as Pianobar reportedly doesn't like them
