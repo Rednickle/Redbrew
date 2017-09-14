d161484a9b6066b4aa177f877c627777d931a5a0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-14 18:30:32 +0000

    xorriso: update 1.4.8 bottle for Linuxbrew.

--- Formula/xorriso.rb
+++ Formula/xorriso.rb
@@ -9,6 +9,7 @@ class Xorriso < Formula
     cellar :any_skip_relocation
     sha256 "ec7b002127b53ef237e0c7004e567b0b15ee23deca813ae9ba93dbbc8e9d37d5" => :sierra
     sha256 "050eba14607cb39d805ac97cd2869f59e617b01b537c4ee4d7189a6a2bb11d46" => :el_capitan
+    sha256 "e4c10b89adf25ffb492ce486d9e0f205dc78d566aa233b99d1e8f4b30cde1bb4" => :x86_64_linux
   end
 
   def install
