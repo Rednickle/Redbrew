4f826a70ee8578b58a963cab88edce182841ee8c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-15 23:41:53 +0000

    jasper: update 2.0.14 bottle for Linuxbrew.

--- Formula/jasper.rb
+++ Formula/jasper.rb
@@ -5,9 +5,7 @@ class Jasper < Formula
   sha256 "85266eea728f8b14365db9eaf1edc7be4c348704e562bb05095b9a077cf1a97b"
 
   bottle do
-    sha256 "086de22e8e8a01299962f3bea5374c90490b66e84b7e10a4078f172e64b0079f" => :high_sierra
-    sha256 "86058296fb5efea3ca509bd335bf7da48a83078fc237c1ccb1bb2d428ef2343b" => :sierra
-    sha256 "c481b8887b8d29e3c63735dd2151c9246e08f21bf50334033de4a054f700a6db" => :el_capitan
+    sha256 "aa57e6dbabe1df745c5327ba7ba14ece051b7804ab9e9fd75d1a950d08200de5" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
