44914b24de9293ecfe38001688471b172716062b
Michka Popoff <michkapopoff@gmail.com>
2017-09-16 17:53:13 +0200

    shadowsocks-libev: update 3.1.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#4281.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/shadowsocks-libev.rb
+++ Formula/shadowsocks-libev.rb
@@ -9,6 +9,7 @@ class ShadowsocksLibev < Formula
     sha256 "9dc98453a95b5f00f659167fd1cbf169e33e3672555a73e5bd3a2d553129b94b" => :high_sierra
     sha256 "a916884173ccc51afbb830d03b36447880883608cd4f9642d252955757208a0a" => :sierra
     sha256 "2bb3de97af9ceeb9ba70c9183c76112375fc177071bdf2b4eacce7089da405d1" => :el_capitan
+    sha256 "38a51346e6b420200705f12bba73fb6b7980d258560c6712ed2904c508504d79" => :x86_64_linux
   end
 
   depends_on "asciidoc" => :build
