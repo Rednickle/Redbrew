229c50befc4f32fff3d084c2de245b5e4f88f2a2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-18 15:49:00 +0000

    cli53: update 0.8.10 bottle for Linuxbrew.

--- Formula/cli53.rb
+++ Formula/cli53.rb
@@ -8,6 +8,7 @@ class Cli53 < Formula
     cellar :any_skip_relocation
     sha256 "51cb27467e305eb6cde457e13e8a698e10ce2828085653c7df50fe2d5310b499" => :sierra
     sha256 "c51377552bc5a6438dac6f0703ce97f6f027083a14936a9424baa7979382e05e" => :el_capitan
+    sha256 "d6fab661cd5a93aef4b0d49e0832f5ce77668de3978a7a7fca69560aab9cfab8" => :x86_64_linux
   end
 
   depends_on "go" => :build
