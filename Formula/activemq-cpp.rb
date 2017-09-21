ab54bf53a63d6fcc159f7734a44a5af29b60526f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-21 07:35:16 +0000

    activemq-cpp: update 3.9.4 bottle for Linuxbrew.

--- Formula/activemq-cpp.rb
+++ Formula/activemq-cpp.rb
@@ -8,6 +8,7 @@ class ActivemqCpp < Formula
     cellar :any
     sha256 "eda1e1feb50e5ffdceb93e3161eab96b389aaa65d64961c6803406176c89f198" => :sierra
     sha256 "799696b515fbff76de2277327d074dc96b74e676df72aa347b23eee12ffbc03b" => :el_capitan
+    sha256 "87d76f6f4e2c6f8c6081f3308a8724b3ed47ae3b0b184386893bd65715b45a98" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
