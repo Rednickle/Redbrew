55f02c73ac8b8f118621395f0306e9baf22ac26a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-21 15:26:52 +0000

    pre-commit: update 1.1.2 bottle for Linuxbrew.

--- Formula/pre-commit.rb
+++ Formula/pre-commit.rb
@@ -10,6 +10,7 @@ class PreCommit < Formula
     cellar :any_skip_relocation
     sha256 "e0dc2f37f3d969c171d64da46ceb30eb8868c215e2a74e780037c19592336dee" => :sierra
     sha256 "fce58b216c9329a96323c98567414cbffe4f3118e4e4a15a2614eb7c2192a9b1" => :el_capitan
+    sha256 "d6e0f2743e3e2df4ffe4d73f2334091c0fdd3a6abd42de14991cfc874293bb5f" => :x86_64_linux
   end
 
   depends_on :python3
