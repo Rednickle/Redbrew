80dc2e4f2ef7f2ea9c6bc1335a806ec150e87297
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-24 20:15:25 +0000

    xa: update 2.3.8 bottle for Linuxbrew.

--- Formula/xa.rb
+++ Formula/xa.rb
@@ -9,6 +9,7 @@ class Xa < Formula
     sha256 "8b72ad60db64443a0f7a821059221573e258e4a3e99621e5defebe71bda1d322" => :high_sierra
     sha256 "c3148f1d7318b3e1757bdae5c7cca5d0f5cd15d1dbb3fbf33880c0b22ee525f1" => :sierra
     sha256 "937cdf0951ddd716ff654d3d959f862bb838c830a5194de220c3c60c118895c5" => :el_capitan
+    sha256 "68a7eb1baf6fa2ba45715c022d1914852f8b041330719e5878344fc8c29da331" => :x86_64_linux
   end
 
   def install
