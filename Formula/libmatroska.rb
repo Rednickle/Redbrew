92d109f12063ad2badc53751d7d54f7b5f4d87c8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-26 21:37:01 +0000

    libmatroska: update 1.4.8 bottle for Linuxbrew.

--- Formula/libmatroska.rb
+++ Formula/libmatroska.rb
@@ -9,6 +9,7 @@ class Libmatroska < Formula
     sha256 "e4f4d97646cb4860e229532d6a37ac3edeabac4dcc35fb66ff73aa4bd2baad62" => :high_sierra
     sha256 "d9d618b79e33db74df3d594bbb8ce997c7fb3347861ad2ea6e5677cd607a3197" => :sierra
     sha256 "53cffc64b37eec80b8325437f3fd7d84ffdf0626bb87499c1565eaa063c63854" => :el_capitan
+    sha256 "eacf78f9491de1f42621d63b5374a274f83138dd1107c3dfc58688c9739cbe12" => :x86_64_linux
   end
 
   head do
