c37e1df94c727d0f2747ba62dfd58e04ce04fd47
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-09-29 22:13:04 +0000

    faac: update 1.29.7.7 bottle for Linuxbrew.

--- Formula/faac.rb
+++ Formula/faac.rb
@@ -9,6 +9,7 @@ class Faac < Formula
     sha256 "2d57f0cd1c775e6fdf846bfcce8ccd80c41075fd10b57a3fbdca71c1ab2111be" => :high_sierra
     sha256 "95ea002facf23eddefa87cdfa4c8d98cee8beb268802b6c20c1333b6141ebe0c" => :sierra
     sha256 "881372bf82be152b0c72a029f89af750490202faa83d734cb0651b2a23bdc8c4" => :el_capitan
+    sha256 "f5a4f045f54b4b8ba96d67d7d00dd2305e66f9fd1a3106044679eadb73750d9d" => :x86_64_linux
   end
 
   def install
