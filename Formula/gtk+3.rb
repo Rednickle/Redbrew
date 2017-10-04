4b3e4017e9b8b40cae24383d894bdcb03987dac0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-04 23:06:19 +0000

    gtk+3: update 3.22.22 bottle for Linuxbrew.

--- Formula/gtk+3.rb
+++ Formula/gtk+3.rb
@@ -8,6 +8,7 @@ class Gtkx3 < Formula
     sha256 "1fd960c90848f5c33972c233321a81fedee6284990b935ea1906c7d70e3e2a31" => :high_sierra
     sha256 "46a4f9a050c0ae969b0cc12b1afac2642be84972edf3ee6e5306ae950e47b06d" => :sierra
     sha256 "0a9440ea74a4d4f1e1c96a427d32e8cbf110f456afdca52b21fc70c7665ac9af" => :el_capitan
+    sha256 "1e6b097c61006170c4a38cf4d5994239dce380ff770ce2f802cacc8e0fd4e6f4" => :x86_64_linux
   end
 
   option "with-quartz-relocation", "Build with quartz relocation support"
