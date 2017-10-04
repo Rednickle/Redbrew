dd7dd60d8597a597a188392334cec9390a29d1d4
Michka Popoff <michkapopoff@gmail.com>
2017-10-04 21:30:14 +0200

    cryptol: update 2.5.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#4572.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cryptol.rb
+++ Formula/cryptol.rb
@@ -15,6 +15,7 @@ class Cryptol < Formula
     sha256 "04ed08859f18cb0fdbc5c37b2db15f474ad241b908a06aeb0d890145d84fdf8b" => :high_sierra
     sha256 "caad7d780203e43fd57a8cb0eb0d30e59bc7dea5bc258dfdf938b84e26a9e207" => :sierra
     sha256 "6679c45e92d3093e89b54bcddeb1e107445afb47243a766b197240362c281d1a" => :el_capitan
+    sha256 "0d11cef18ca7c6edec0ab58f9f4f5ce9000bc5d94f394cfee9bc67a1f77fa5d1" => :x86_64_linux
   end
 
   depends_on "ghc" => :build
