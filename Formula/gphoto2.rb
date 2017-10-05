6ef4704af0756a91162336a4039a2ba1e7395553
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-05 14:58:06 +0000

    gphoto2: update 2.5.15 bottle for Linuxbrew.

--- Formula/gphoto2.rb
+++ Formula/gphoto2.rb
@@ -9,6 +9,7 @@ class Gphoto2 < Formula
     sha256 "28856ab94ba2f28245649734265ac41cb29911d78818bd5aea0dddbbfc005510" => :high_sierra
     sha256 "1c1e980bde3f0cd72de42d9196254ffbec516b4ee667232090f0f68bb9284a73" => :sierra
     sha256 "f663e168d3e139ea2544e560aa4666441622a95bc425fa755c7fbd6060c5aaa0" => :el_capitan
+    sha256 "9288e27b06c1deb974b0929c0d33d8c0b0cd42e009557b4621de01f47f0579f1" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
