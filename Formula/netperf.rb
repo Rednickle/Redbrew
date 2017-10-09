77f7987b55419773cc588d4029c827312f2cc52a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-09 17:08:00 +0000

    netperf: update 2.7.0 bottle for Linuxbrew.

--- Formula/netperf.rb
+++ Formula/netperf.rb
@@ -11,6 +11,7 @@ class Netperf < Formula
     sha256 "cf086e0d276a572aba8318f7080cedc94b36a7b612cdbb4bcc3ceefef0080c53" => :high_sierra
     sha256 "4d3f648081c84ad697d608b56bcfce3237de7c34c4e4a53d9851628f9d50cd5d" => :sierra
     sha256 "c6e96625b1f83a7f83d3c9b53b8584ab65d73cfd59bc38672588ba82d37ecc1d" => :el_capitan
+    sha256 "a1d056b27016b8262bf9960a1d466f536222dba7369e954d1be8fae1a53de83c" => :x86_64_linux
   end
 
   def install
