0ba07dca35fee8e5eca2cbd1665f130f4a0a4630
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-10 23:44:24 +0000

    udptunnel: update 1.1 bottle for Linuxbrew.

--- Formula/udptunnel.rb
+++ Formula/udptunnel.rb
@@ -12,6 +12,7 @@ class Udptunnel < Formula
     sha256 "acbc74c384071aa6be92bea754274c5025de0123afd389d1b174e444a7beec42" => :el_capitan
     sha256 "8c4b8fb805fa94098925417a8fa61ced0d546b1b77330fd03cd5a72ee6e43b70" => :yosemite
     sha256 "8f0051f653f62bef0e85f8ced4806a8bae244a911e05129e4fc2bf05912c9412" => :mavericks
+    sha256 "7b61046eb375d1abe286e360557ee85c7bd697a42317203074dde73819331e12" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
