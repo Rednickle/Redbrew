cd6e828cf8c5410cc216a18666e86490bdd3065c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-16 12:12:00 +0000

    laszip: update 3.1.1 bottle for Linuxbrew.

--- Formula/laszip.rb
+++ Formula/laszip.rb
@@ -9,6 +9,7 @@ class Laszip < Formula
     sha256 "c99dc1884cf70eae67cc22c2e7c1c4a981027f39db33c98dc7e548321ce5469b" => :high_sierra
     sha256 "9262196c5ff19095f656aa9884a997d1d57b1bcf3d01baca5759963d7770fc2a" => :sierra
     sha256 "47d656195a3b4e8fb0f675b8bd0ee6933b4b3c3aca2c17e9db271a00c8fe9383" => :el_capitan
+    sha256 "0018fed1b76b1dadcfcc7567ab045a517667bd9936fd0eb913c06a7693114d6d" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
