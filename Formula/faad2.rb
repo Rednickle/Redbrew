14ab3115808bdad4e8ceed5765d271612400f4e2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-16 09:02:57 +0000

    faad2: update 2.8.6 bottle for Linuxbrew.

--- Formula/faad2.rb
+++ Formula/faad2.rb
@@ -9,6 +9,7 @@ class Faad2 < Formula
     sha256 "ca4c735b958cc393e8ccfa90ca3b0c372febc7175c7be3070aee64fded14d593" => :high_sierra
     sha256 "b543a97001b82b54a7b650be495394d73b3c81822ca989e154380630ce503ccb" => :sierra
     sha256 "6dacb8f2ebff6a348a0b1be0a8bbec37a4cee81a28dad9fe2fa2f809461e9e89" => :el_capitan
+    sha256 "550b8b53e3f8f786317373d2039f3dcaa49e9453064d62c3b5262d47e53fbfa8" => :x86_64_linux
   end
 
   def install
