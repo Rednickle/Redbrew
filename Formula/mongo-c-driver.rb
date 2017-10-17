1e91e652a1f330b63f5940721c9cabc1de16606e
Michka Popoff <michkapopoff@gmail.com>
2017-10-17 18:53:02 +0200

    mongo-c-driver: update 1.8.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#4708.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mongo-c-driver.rb
+++ Formula/mongo-c-driver.rb
@@ -9,6 +9,7 @@ class MongoCDriver < Formula
     sha256 "26fa516f8b9a54b28f7ca3d018eab0447bc6f4c805a007b025b884bc29fc6e3f" => :high_sierra
     sha256 "c259c59633fb7148af5125a41757116b676781e33bf6baaa6602e821a69607f7" => :sierra
     sha256 "b5f92db860024a4dfda2bc7fc51f2e4856d3702df75261502161eb2d264f6115" => :el_capitan
+    sha256 "cebdc82dc5cc0c8910d6bdb25c035e31c0ba696e4d20e00110ef36edec884009" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
