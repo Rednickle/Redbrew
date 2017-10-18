50d9b7ec61f35a256bdeb106d7c2a524e957b8d1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-18 21:25:45 +0000

    sdl_gfx: update 2.0.26 bottle for Linuxbrew.

--- Formula/sdl_gfx.rb
+++ Formula/sdl_gfx.rb
@@ -9,6 +9,7 @@ class SdlGfx < Formula
     sha256 "b1040e970fe68325a37c4a6af037206c28d12ae77f49851a0d28333e7c19a5e4" => :high_sierra
     sha256 "643210ccd7a2d9f2fc92d519900bbeb51c1f168729e40860c40e67629ce2ef8a" => :sierra
     sha256 "072983d26bc7e50acd12ef27adab047c3e14e45dff83e98be9ea005c7c107524" => :el_capitan
+    sha256 "8180b7aa9d29e8eb44d3d58a71f56aa9be0aba2d75034db7ba2370051682b252" => :x86_64_linux
   end
 
   depends_on "sdl"
