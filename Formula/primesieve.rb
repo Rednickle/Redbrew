e139c38f145713315534bc52684df634a0aeaa7e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-18 17:17:04 +0000

    primesieve: update 6.2 bottle for Linuxbrew.

--- Formula/primesieve.rb
+++ Formula/primesieve.rb
@@ -9,6 +9,7 @@ class Primesieve < Formula
     sha256 "f03b9925f3e732b47989196b1a09ce1cefcd45c3181516630bdadd6de1549084" => :high_sierra
     sha256 "6497a4d9e833a240271d2934c9726a36e971d08b1d5a0097901eb2e9a2d8bb1f" => :sierra
     sha256 "b4d46e227db20da387247c244bf9a13dfe1c23b19b922cbcf20a4b47994eb059" => :el_capitan
+    sha256 "33f6bec0d8625671e5442efa6f7cfbc84154aa64bb792189eae1d09b773377db" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
