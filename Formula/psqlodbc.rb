9868c483a16f5acba144c5a3cc00c1d522611c6d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-18 13:21:38 +0000

    psqlodbc: update 10.00.0000 bottle for Linuxbrew.

--- Formula/psqlodbc.rb
+++ Formula/psqlodbc.rb
@@ -9,6 +9,7 @@ class Psqlodbc < Formula
     sha256 "19018de925f884c2cc09f6c83f57d8617dfbc058ae04e4a077ff7c408ee42ef2" => :high_sierra
     sha256 "257fbe1e9149dc8f61f374ba3af4773a7872b3a5deab3de499411e432d5b2775" => :sierra
     sha256 "81eb56bbd0a47c475f1441aa757135e1b3ae668dee66ac0c10cd76e1de1c6273" => :el_capitan
+    sha256 "da663562905231d88065c3b77160b512769b37aee117ca9efc3acf0d00ee04ce" => :x86_64_linux
   end
 
   head do
