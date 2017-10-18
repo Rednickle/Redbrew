aef6d0ca5e3026c4222f94697877553a8ef9ea60
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-18 19:25:47 +0000

    fwup: update 0.17.0 bottle for Linuxbrew.

--- Formula/fwup.rb
+++ Formula/fwup.rb
@@ -9,6 +9,7 @@ class Fwup < Formula
     sha256 "5bc1a5bc7e779d2eeb43ddb824680a5dd908127380c60368d0e99ae2863ff7ee" => :high_sierra
     sha256 "7a567711e6323d423457464516ae1d513aefbf8db2931689ddfac74213af8964" => :sierra
     sha256 "fe29e43fcad130de8513b25d2d109c00747a9304aa99229f2ab4a62cff536bb5" => :el_capitan
+    sha256 "fcc7f6df5c3161dc5b0d1b34dc71d6a2c207c35bc0e883b527a854aad070580e" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
