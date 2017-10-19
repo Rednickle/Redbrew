cfed724ff52a627c1959ecc786e9a2ac47c909b7
Michka Popoff <michkapopoff@gmail.com>
2017-10-19 10:14:24 +0200

    dmd: Fix devel for Linuxbrew
    Closes Linuxbrew/homebrew-core#4744.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dmd.rb
+++ Formula/dmd.rb
@@ -1,6 +1,7 @@
 class Dmd < Formula
   desc "D programming language compiler for macOS"
   homepage "https://dlang.org/"
+  revision 1 unless OS.mac?
 
   stable do
     url "https://github.com/dlang/dmd/archive/v2.076.1.tar.gz"
@@ -26,31 +27,6 @@ class Dmd < Formula
     sha256 "3b52b4fd9f660e97e654b9040b9467c998c77aa57fc17997fb75773587196cf1" => :high_sierra
     sha256 "ce763c7da5b88d2f6e63d990e26cc286a6d96534e975fa1d04c45729885e0f21" => :sierra
     sha256 "fd4a9e180181c408001afc14fdaf6c5118b180c2bf814ff09c5ea4814b3f9285" => :el_capitan
-    sha256 "94f880952aee92e7770294c4517cb635aa0978955827f73ba0aca72ad49a241e" => :x86_64_linux
-  end
-
-  devel do
-    url "https://github.com/dlang/dmd/archive/v2.076.1-b1.tar.gz"
-    version "2.076.1-b1"
-    sha256 "7151244b37e46e19b2d71be4d88fbae5becbc29b903705e13937e40c721a4e36"
-
-    resource "druntime" do
-      url "https://github.com/dlang/druntime/archive/v2.076.1-b1.tar.gz"
-      version "2.076.1-b1"
-      sha256 "80071589fbe75c2ed9549e007d79f4feccbd2dda9580c45e401ca89a296c8ce0"
-    end
-
-    resource "phobos" do
-      url "https://github.com/dlang/phobos/archive/v2.076.1-b1.tar.gz"
-      version "2.076.1-b1"
-      sha256 "992ff709069810bebb5bc26263f6e939d52dff1f27ee80ee095075e6f587f5da"
-    end
-
-    resource "tools" do
-      url "https://github.com/dlang/tools/archive/v2.076.1-b1.tar.gz"
-      version "2.076.1-b1"
-      sha256 "a56cf6f00ea0fd17aa8a1219aba08291f71b63cf8a41614b4f59b39b72982280"
-    end
   end
 
   devel do
