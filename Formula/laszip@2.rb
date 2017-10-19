80ce86aabd01f72041dd1fb0ccddc424b3df3b68
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-19 15:18:35 +0000

    laszip@2: update 2.2.0 bottle for Linuxbrew.

--- Formula/laszip@2.rb
+++ Formula/laszip@2.rb
@@ -5,10 +5,11 @@ class LaszipAT2 < Formula
   sha256 "b8e8cc295f764b9d402bc587f3aac67c83ed8b39f1cb686b07c168579c61fbb2"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     sha256 "1552904730867b91d1c33d5d11ec29ba285f7dc164a17805e02555d16e4bf501" => :high_sierra
     sha256 "c8c8f7ec659721bdc5a44c78d521517a99bc083f3a7e6406cd0b9060c09de09f" => :sierra
     sha256 "4d2f138afacf7750ddb56c4cfbd09590b38c63c5fae9765045192565e7508465" => :el_capitan
+    sha256 "14ef1745b0765281286006b83de845babd661df6cd0c29c37e108f036d2a6a3b" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
