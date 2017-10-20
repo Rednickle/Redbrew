e5c3f978cfcd877988c841cf24fbf9ad2f17e3a9
Michka Popoff <michkapopoff@gmail.com>
2017-10-20 11:18:11 +0200

    jhipster: update 4.10.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#4746.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -11,6 +11,7 @@ class Jhipster < Formula
     sha256 "8fea8c186c824b7c9d93c1020e474d3d3881997543b564bde9ee8cf2df754dbb" => :high_sierra
     sha256 "d863094dbe302c4ed341f225bda22dbbb1518ecce8f890389fd42a7b8288d7ae" => :sierra
     sha256 "9744c8273ef52babed03ba1b66a5e06152375a2c3e535f7bcf6648085719732c" => :el_capitan
+    sha256 "0ebbeb5fffc463dab3bd8e22698ff67dc0e1c0c50ef5bb2d986767e085c8e729" => :x86_64_linux
   end
 
   depends_on "node"
