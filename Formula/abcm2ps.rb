acb89d7ccdd63afaa609cd2c992d6d117dcf4ace
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-25 08:30:16 +0000

    abcm2ps: update 8.13.16 bottle for Linuxbrew.

--- Formula/abcm2ps.rb
+++ Formula/abcm2ps.rb
@@ -8,6 +8,7 @@ class Abcm2ps < Formula
     sha256 "e5d2004858c71cb5b5b4b149c4c8403badd083916c350cfbeade40e325c8e054" => :high_sierra
     sha256 "dd03cbbc24dfa5421259f302fe5f99ba45777b51d097c2d89472e1583a40aa11" => :sierra
     sha256 "593817fd008f0bd24708779957e72efcdf3340671761a424a7f1855bf2090ffc" => :el_capitan
+    sha256 "39a87b02761b8324f6612f20b3b0d73754ed0ecedb14995b36811457201ec380" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
