50d30241d9400a21f00a25248d8e119823a11875
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-25 09:39:11 +0000

    gauge: update 0.9.4 bottle for Linuxbrew.

--- Formula/gauge.rb
+++ Formula/gauge.rb
@@ -9,6 +9,7 @@ class Gauge < Formula
     sha256 "dce11edbf9996c1a7967d7ff4ad58de43d4146a9652b2941af31752f2edb88dd" => :high_sierra
     sha256 "b5461dcf70512fe8dbbaf9760a2785d06fd3e3f150c22cb199599ab7eb273e3f" => :sierra
     sha256 "fa60a25654a2e73caff1c0b0a001331b723d85621ea43518579d9678934ba174" => :el_capitan
+    sha256 "029a10f3d8cde31ea7288eca61f34e1ed56f474def470978632904511c1f8b01" => :x86_64_linux
   end
 
   depends_on "go" => :build
