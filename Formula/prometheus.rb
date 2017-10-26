cbb4f74eca4a1f69910e7af5a117549d49da533f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-26 11:29:19 +0000

    prometheus: update 1.8.1 bottle for Linuxbrew.

--- Formula/prometheus.rb
+++ Formula/prometheus.rb
@@ -9,6 +9,7 @@ class Prometheus < Formula
     sha256 "70d9869fcd13dfabdc0c97a1fa317607ad61d8d0637cf3a03abf58cdd8761b2f" => :high_sierra
     sha256 "e0a2295cbe3307973eebbffd409be1fd74bc952d6dd21b63470f0778e868ede5" => :sierra
     sha256 "6ceb4c2ed497b36cfb684f67615c5b4195e2b019a702b4e8498a2b5edd1b600a" => :el_capitan
+    sha256 "4f2dffa39fddd2ebb01112fa0038af68aed34c9fa74b5741e3a77e143d505e2d" => :x86_64_linux
   end
 
   depends_on "go" => :build
