5c311b5cc2d7ad8f8775812c535bb72a21b35b53
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-27 08:09:31 +0000

    fd: update 5.0.0 bottle for Linuxbrew.

--- Formula/fd.rb
+++ Formula/fd.rb
@@ -9,6 +9,7 @@ class Fd < Formula
     sha256 "bbb1501c7d891c056937a8ae0b670d0cf2fba972b92ac44e2fda3ae920b15ca6" => :high_sierra
     sha256 "d9f699b01a6e15e7943f0f1a3794c056a5b1647a90fc4911472874853e5098a5" => :sierra
     sha256 "b44e1a1edc935865e61e438cef25fa6b71f734674b505697811d60d13abfcaea" => :el_capitan
+    sha256 "14f84a8a5df6d8c4729430deed2f2bb320ba71e8780c523432bd75da2f9c029b" => :x86_64_linux
   end
 
   depends_on "rust" => :build
