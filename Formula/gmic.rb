02f878e2b02f8b4ba63c447b299dfaae935c686a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-27 18:35:43 +0000

    gmic: update 2.1.3 bottle for Linuxbrew.

--- Formula/gmic.rb
+++ Formula/gmic.rb
@@ -10,6 +10,7 @@ class Gmic < Formula
     sha256 "86926c82431646ae3ca47ec36a1db525f44d86068fc6aeff480dfd4c08e47387" => :high_sierra
     sha256 "686b770c6a8707757355f124b592a9eb867e21626081993e0e7e11b060555c21" => :sierra
     sha256 "596c7db4aa25f3e0378a7b3ed19e729d1ffe1e892c45e7b609f6c7ab2bf134fa" => :el_capitan
+    sha256 "d96d86fde4601fbd497cc45d93835e90f5d062b907e246bf2fe7b46821bb0ab3" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
