764b5b07b59fba2e0eb699e1292c78c535ec915a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-28 09:08:30 +0000

    apr-util: update 1.6.1 bottle for Linuxbrew.

--- Formula/apr-util.rb
+++ Formula/apr-util.rb
@@ -8,6 +8,7 @@ class AprUtil < Formula
     sha256 "66ce47afb4180040b50ddf1840d1f0fb2495300114b43827f2710506d0dfb34e" => :high_sierra
     sha256 "abce2cedacd5c498bf2bc1057eda069692bf96236ba445e229874c34a0a9c51e" => :sierra
     sha256 "23b84ef6330490e10baaffe8e9fdd617d39cfc9b938fb77d9efaa6a770359162" => :el_capitan
+    sha256 "8ecf774f206e0e93cb7fa5aafcdb9fe8c258501e1bf28f07e47992cdb4e50aba" => :x86_64_linux
   end
 
   keg_only :provided_by_osx, "Apple's CLT package contains apr"
