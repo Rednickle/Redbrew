e8f366cfdd4ffa88c8a5ebbe507980d345628108
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-10-30 09:52:52 +0000

    libconfig: update 1.7 bottle for Linuxbrew.

--- Formula/libconfig.rb
+++ Formula/libconfig.rb
@@ -9,6 +9,7 @@ class Libconfig < Formula
     sha256 "b9b5c4811c278a7ea7e193685a5b352e80121d28defb23715df4b66e98e33af2" => :high_sierra
     sha256 "31ed172343eb35e862fe8e8eb1d4850845ea376b8857a1e44239ba8873e9e4e3" => :sierra
     sha256 "ac4deedc89f0c4712ffde498f1eae572c34a5ef1b804a18a1d087fda0f5b43d9" => :el_capitan
+    sha256 "2b6fb5b1985cb1fc12d208db1b806cb894eb54e29f6b8b043964c6bf1dac775a" => :x86_64_linux
   end
 
   depends_on "automake" => :build
