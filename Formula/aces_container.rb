330f3d7e43373c09a6e0a8d4ac3a0af6ebf358a8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-01 12:24:45 +0000

    aces_container: update 1.0.2 bottle for Linuxbrew.

--- Formula/aces_container.rb
+++ Formula/aces_container.rb
@@ -9,6 +9,7 @@ class AcesContainer < Formula
     sha256 "4297afa069f1cd305e93038ed43260b3643f0bd27f39e33355061fc111fb7f6f" => :high_sierra
     sha256 "6b276491297d4052538bd2fd22d5129389f27d90a98f831987236a5b90511b98" => :sierra
     sha256 "16cf230afdfcb6306c208d169549cf8773c831c8653d2c852315a048960d7e72" => :el_capitan
+    sha256 "ace21c341d22453d12eb11544a2c84e2623c1be01abafa34f697305660f45601" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
