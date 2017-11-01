5861037be12807cd6cd09cebc9c9e80b17a98f77
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-01 13:45:29 +0000

    frugal: update 2.12.0 bottle for Linuxbrew.

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -9,6 +9,7 @@ class Frugal < Formula
     sha256 "8a62bd838948efb98079ebd7e52878581985a3b34348d9a09a13f7784f52659f" => :high_sierra
     sha256 "5ea9410bedbec3d19f700fd5fa6593a7b68fec10ed3c8c4f5ae8b5b7d1178911" => :sierra
     sha256 "8acc48dd41f782a2dca694c77ffd7a987a9e65ce8edec0097c39dca434b8c60a" => :el_capitan
+    sha256 "0e35dd5644e388189df7c27f3186bb0b2a9dc341974d8a55481c154e7ed25aeb" => :x86_64_linux
   end
 
   depends_on "go" => :build
