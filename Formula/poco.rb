b0f4872154ff8d3d3a629cb8b1a710ba6a5d6ac2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-05 23:07:09 +0000

    poco: update 1.7.9p1 bottle for Linuxbrew.

--- Formula/poco.rb
+++ Formula/poco.rb
@@ -12,6 +12,7 @@ class Poco < Formula
     sha256 "a660300244ce2567e1abeebf18db634f517692dbb2b2d4765074050fb522539a" => :high_sierra
     sha256 "995f536da783631be5266076fd0caae50657d13f682e089102e56d747c6f09ca" => :sierra
     sha256 "e8b196801ed82c2190847c5a7bcd9af79e1adbaabcd2841fc3645a7412df364e" => :el_capitan
+    sha256 "c9994bf53c4177827116131088f8d9be60171fc57e8fef1d6cd36dce43308488" => :x86_64_linux
   end
 
   option "with-static", "Build static libraries (instead of shared)"
