5472a4ae4f167c5dfd6d9277abcf964750c4cd47
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-05 21:34:44 +0000

    afl-fuzz: update 2.52b bottle for Linuxbrew.

--- Formula/afl-fuzz.rb
+++ Formula/afl-fuzz.rb
@@ -8,6 +8,7 @@ class AflFuzz < Formula
     sha256 "4cd6b08ecfe35a62136b7a52222c59e055b80aef599404e29c7c9b8bf4f8fd50" => :high_sierra
     sha256 "b45ff3036dddc75cf64689b3f2660938834f393256315ed33ba72ed1924c695e" => :sierra
     sha256 "ef5e7c2e25020bf2d468c81ced4fdd9014dbdc0bb523f5acd8d91d8badc97d59" => :el_capitan
+    sha256 "f1c7f1387a454d2571dcd23439fd62403b4970f9640e5bf5318a999785c74073" => :x86_64_linux
   end
 
   def install
