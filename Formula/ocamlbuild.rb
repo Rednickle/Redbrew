6c4b655b6a70704a7b1a7fee4f2e274a58fc4de8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-05 17:57:43 +0000

    ocamlbuild: update 0.11.0_3 bottle for Linuxbrew.

--- Formula/ocamlbuild.rb
+++ Formula/ocamlbuild.rb
@@ -10,6 +10,7 @@ class Ocamlbuild < Formula
     sha256 "16c1d84895f766e95e5a4d5b52c6ef94a392d570a0ceeb1c0d884d06e17f88f9" => :high_sierra
     sha256 "31aa1670e71e813d2c84ba3d593e51ac99c66bcee2d00d0ed5f2d3fcd58fec3b" => :sierra
     sha256 "3634d6b1a4be0876183c94b30bd9a80a620051a7d67761dd1403703dbd51b80e" => :el_capitan
+    sha256 "3e10c87f979f0ae39b0d34335901b0fd7d63d416d74b2fc3857d25ee173411ba" => :x86_64_linux
   end
 
   depends_on "ocaml"
