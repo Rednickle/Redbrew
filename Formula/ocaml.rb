9adbf746397a5ae320ab782098723ddb6e971c93
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-05 17:57:42 +0000

    ocaml: update 4.06.0 bottle for Linuxbrew.

--- Formula/ocaml.rb
+++ Formula/ocaml.rb
@@ -31,6 +31,7 @@ class Ocaml < Formula
     sha256 "377756d07b0253fd30eb0d5761d7a0e53d4305daa742122dbcbe70a378277f5c" => :high_sierra
     sha256 "e62b761a4e814661d3ec110d3e3da378316ad57416d9d01bdc2dbd445d92e2b6" => :sierra
     sha256 "0422ddaa6ee523fc6c9b30ad239118ae4de011b64bcb9687b917b586b7794595" => :el_capitan
+    sha256 "dd2fa23948979cb8713a5b81720cb59bed8e3ae31b0c95699be2bf65a8d2a161" => :x86_64_linux
   end
 
   option "with-x11", "Install with the Graphics module"
