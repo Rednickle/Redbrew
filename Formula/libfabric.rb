85d33d6dceb84776bea180d02a5aec93a3cbc407
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-09 20:16:04 +0000

    libfabric: update 1.5.2 bottle for Linuxbrew.

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -9,6 +9,7 @@ class Libfabric < Formula
     sha256 "73eff6b37ac05aa1b6174453a4019befa474d3714e0b35e2cd18fc63d01cac7d" => :high_sierra
     sha256 "40153ea7be894b3d4ca933bc39fb705c3e3be313de98c97111f305bc4120de2b" => :sierra
     sha256 "3f69a1ce47cc4fa738fe9e057b69f04be7b4602e78ee030d27445cd49e36118e" => :el_capitan
+    sha256 "5ef117c47340df3bc8487c13e86cd75adb176c5a5315b13da8a96e124a1c0adc" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
