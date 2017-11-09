366d723e0e7b1fdb1aee05b4e5ce082ffa3d4756
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-09 20:16:03 +0000

    libfixbuf: update 1.8.0 bottle for Linuxbrew.

--- Formula/libfixbuf.rb
+++ Formula/libfixbuf.rb
@@ -9,6 +9,7 @@ class Libfixbuf < Formula
     sha256 "3362c36ab1a1d373a576ced24ece03b0a957f50aec7ba32d772d11e610b77835" => :high_sierra
     sha256 "ee245e1d7f3f82d2d84041b701b456bb10839dd82df214f1dfc5c76ddc9867f4" => :sierra
     sha256 "bae025cc2353fdc08b2ad56651e5e44e41701ee22c03ed8265e6ffcf84ea2847" => :el_capitan
+    sha256 "8e666a2b54a09e2526fc1474e74933c09a78aa9c1569a9e1721ce7adb4575f67" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
