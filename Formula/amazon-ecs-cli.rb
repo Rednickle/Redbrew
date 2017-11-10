fdd50a69746be61cee0e178a1fd6da80ba22288e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-10 21:13:31 +0000

    amazon-ecs-cli: update 1.0.0 bottle for Linuxbrew.

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -9,6 +9,7 @@ class AmazonEcsCli < Formula
     sha256 "4c4817db980c69151f7445e781273eb8ec96fe3de06ac13483d88fd558b38a3a" => :high_sierra
     sha256 "bbc460c2d8f420ed6eef36368d7fbd6c19d2cc623f6fed5842587d559e72920c" => :sierra
     sha256 "0656ca15e030093201614a0779914d9e6c56d19ccaa2c5d415d3f988e7c0b8b2" => :el_capitan
+    sha256 "cf0d4a30545c27d83f6c28c0f8d2cd2b6076b39927527c5813d51fdf0fe967b1" => :x86_64_linux
   end
 
   depends_on "go" => :build
