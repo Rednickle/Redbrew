22c8642799ce3241c84e8bffd067c3374c703d4f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-13 22:53:59 +0000

    bchunk: update 1.2.0_1 bottle for Linuxbrew.

--- Formula/bchunk.rb
+++ Formula/bchunk.rb
@@ -10,6 +10,7 @@ class Bchunk < Formula
     sha256 "0c9b730ab1db0beafaf377a84a5a8022a1578afa3900365774fefc6feb5fdcca" => :high_sierra
     sha256 "44166914413d7da28d342557b6a839f3aa181741fd99095ee5a671a3223cc427" => :sierra
     sha256 "5154795c1d3087e8c15791b3530a3b098859324cac18a4eabb7ba9e03b5dbb1f" => :el_capitan
+    sha256 "f742aa113b664c1f60ce99a797b532a5bbfb1e77f9fae0b031fc7bd417ff1e9a" => :x86_64_linux
   end
 
   # Last upstream release was in 2004, so probably safe to assume this isn't
