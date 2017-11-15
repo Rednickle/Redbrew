8a1c9712eef13b560200c554f5d25abc7cde5514
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-15 06:41:07 +0000

    zimg: update 2.6.2 bottle for Linuxbrew.

--- Formula/zimg.rb
+++ Formula/zimg.rb
@@ -10,6 +10,7 @@ class Zimg < Formula
     sha256 "8dd8283881f4d519a2c3a376abf6e76e2ca52da090d74f3309bec74999eb9451" => :high_sierra
     sha256 "1edba9d02df895f7aacb63a3db0338fa5b30f02bbe27413e2919795a17842d60" => :sierra
     sha256 "8357eaa481985bc22ebd1a335125caecf13b63db6fa178d19ed7b45aa8552d92" => :el_capitan
+    sha256 "4976f75ec9476c699d1084b3cd34d4b8e22d4444858eafb3cebd8c30bc8ca1f3" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
