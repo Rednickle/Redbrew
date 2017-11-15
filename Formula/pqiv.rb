d439a8859759cb70106cda640e2fe99fb2f4496c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-15 23:41:04 +0000

    pqiv: update 2.10 bottle for Linuxbrew.

--- Formula/pqiv.rb
+++ Formula/pqiv.rb
@@ -10,6 +10,7 @@ class Pqiv < Formula
     sha256 "34ce5fbe3862892bec535178bd21c1d7583d1aa24bfaeab563c17260d6dde688" => :high_sierra
     sha256 "2ce461b056e8501f943d3aad2d2a16365e614a75d5064d1cecb8c55b874173fc" => :sierra
     sha256 "98ba26ff046536172ae69703946409d5714345904c07728b69a374da39669ea6" => :el_capitan
+    sha256 "3196a1074c52868ee8ef662613252137f71dfb3b76052ff0b68d976bafb0cd66" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
