2215540bcd102ff1cca99b227a3bc1a0e346c52b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-18 19:03:35 +0000

    cocot: update 1.2-20171118 bottle for Linuxbrew.

--- Formula/cocot.rb
+++ Formula/cocot.rb
@@ -11,6 +11,7 @@ class Cocot < Formula
     sha256 "0070eb38e06043e3c1a4ad1b77205a6ed978ed300e8d0bb407391fecb191b050" => :high_sierra
     sha256 "a91ba93032e33b6a062b82f2df0b9170d5269cf0312d75eb6f16341fca54f9bd" => :sierra
     sha256 "60cbdadb074b019535319e5089d5c55c43b68e0b52a73b01cec3a9a8311e51a4" => :el_capitan
+    sha256 "3ebf62b8bae739c78d96ebe840e53d06d7088421519c7b634deb6c4ed386842c" => :x86_64_linux
   end
 
   def install
