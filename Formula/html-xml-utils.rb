00c4a221855b9b8391f209bcff91d634d8e4ff64
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-19 12:59:14 +0000

    html-xml-utils: update 7.3 bottle for Linuxbrew.

--- Formula/html-xml-utils.rb
+++ Formula/html-xml-utils.rb
@@ -9,6 +9,7 @@ class HtmlXmlUtils < Formula
     sha256 "80fbef5853a14f820be9936de99456001aabe014e30dc50ee6f7e42b9c514bda" => :high_sierra
     sha256 "8a167685dbaf9e08d6e40c300326914403f8a72d76eef71c37819923c27a2c74" => :sierra
     sha256 "a7559f75d244d994bd2934422e8309b5dc4c28bf5282207b88e80ebeece8428b" => :el_capitan
+    sha256 "c795f650bf7a2616717e42b6bd03ec984df739637b2efce81efbf524b1929199" => :x86_64_linux
   end
 
   def install
