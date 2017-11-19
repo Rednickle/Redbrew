5426e670f005f3e1c222f33c94e5a4d726418848
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-19 15:03:52 +0000

    pike: update 8.0.498 bottle for Linuxbrew.

--- Formula/pike.rb
+++ Formula/pike.rb
@@ -8,6 +8,7 @@ class Pike < Formula
     sha256 "9b3c1d8c9a1eceef90ce1bfe78137b0ccd726ec5dfc12f9746a471c5e855f92a" => :high_sierra
     sha256 "c8abc1e8fcc26523b257cd2144c22a27ed6063c07e33193c70e3d0898c94fa86" => :sierra
     sha256 "1ecfea274c44b5d087003b51acb934c4c3cc8494667cbaa4f6d57c1e919a2e20" => :el_capitan
+    sha256 "9cc884189e822ef6a29401795a212569adae31c9d752c5f3e2d7e9ce0c2e6a8e" => :x86_64_linux
   end
 
   option "with-gettext", "Include Gettext support"
