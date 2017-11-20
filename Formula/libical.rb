da102e770ea440768027d76e5af80a0f771fbac0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-20 18:40:34 +0000

    libical: update 3.0.1 bottle for Linuxbrew.

--- Formula/libical.rb
+++ Formula/libical.rb
@@ -8,6 +8,7 @@ class Libical < Formula
     sha256 "3474b7ac728488ac0ea463ff327d9068a7414ae01dbdcef43db7075541d9ac19" => :high_sierra
     sha256 "f1a8b9de2fd437ae29828fa2f5edb1da77d4cf1b047d60412cbdef14b4362b6b" => :sierra
     sha256 "f03bb074f6c3ff1cd42726eb779f8c05c91f5dfb5118d06317f31159d886ee8e" => :el_capitan
+    sha256 "51849c668bb30ad9711356e5f1dd8b060e2cdcdc5ac4dec7d1848a3b0ed0b71b" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
