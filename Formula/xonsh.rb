6fad4e00f4711c6c82fb46cecc4d9d5bf41ffe59
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-23 22:36:26 +0000

    xonsh: update 0.6.0 bottle for Linuxbrew.

--- Formula/xonsh.rb
+++ Formula/xonsh.rb
@@ -12,6 +12,7 @@ class Xonsh < Formula
     sha256 "4793150048d03d8fbc28d40482f4941bf75c7e25b55f6af22c33e9be6c617b01" => :high_sierra
     sha256 "e5a42b5da4d1aca63541f0e6422330ddaf15d9a55cbef35c9f9f7ca6f113f733" => :sierra
     sha256 "f95ff4dfbabdc802c792d4e2d448807acd33d13b4fcb2e3c93bc89e3517ab79d" => :el_capitan
+    sha256 "c9944e901fbac2ab1b5a69df82511d92542a769a7d71fe4989615176328d21f5" => :x86_64_linux
   end
 
   depends_on :python3
