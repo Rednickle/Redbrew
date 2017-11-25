430635c0fc203c715977822ed10dc71b78d1925c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-25 14:21:52 +0000

    thefuck: update 3.25 bottle for Linuxbrew.

--- Formula/thefuck.rb
+++ Formula/thefuck.rb
@@ -12,6 +12,7 @@ class Thefuck < Formula
     sha256 "081d1d9cf18435961fb62648a1f4fc841e0bc27611f67302ffe11c5576b8d8f3" => :high_sierra
     sha256 "4f91f880f0050c2301cae9019fa67ee53d367d7fdfafbe6bac11c05bc86289b1" => :sierra
     sha256 "f222599a8bd17a176d6c77263c604b371e16094c8c5c12517510dcb713cba0b3" => :el_capitan
+    sha256 "e9f44a17c2e6a4d6096b88b249609d85ce869b246633aab65c768ae346547cd3" => :x86_64_linux
   end
 
   depends_on :python3
