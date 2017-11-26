61fc11b6b3bebbd94ce2e85db16f8da3039ac20b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-11-26 10:00:06 +0000

    libcddb: update 1.3.2_2 bottle for Linuxbrew.

--- Formula/libcddb.rb
+++ Formula/libcddb.rb
@@ -10,6 +10,7 @@ class Libcddb < Formula
     sha256 "ac3157158ee8a29b8fbf0c9bd785c47e74308c0713a9a618054dd9f3e923c037" => :high_sierra
     sha256 "886b8905d25218094b9a21626a0a7f0b6a20edf9285cdbc0a2e6724e05aac60b" => :sierra
     sha256 "f4e146e63fc161edab98d70386c32550c1343b98a08f102fb0190e6372a24f7a" => :el_capitan
+    sha256 "a9c3960fbfad98e1f416defaa0e9cc3699112aaa0102899e08b9fa3901509090" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
