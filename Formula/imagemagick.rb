8afdd5426137c4f1cd9c0d96d8b05c721a4bc94c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-01 22:49:10 +0000

    imagemagick: update 7.0.7-13 bottle for Linuxbrew.

--- Formula/imagemagick.rb
+++ Formula/imagemagick.rb
@@ -13,6 +13,7 @@ class Imagemagick < Formula
     sha256 "62fa77410cd16e04f58328d385742a90e03a40d3d598c79b06e700cfa6f3e023" => :high_sierra
     sha256 "5bdfea29a35363d59457a0f57513d6a8460aa10d0462d2253679345541587104" => :sierra
     sha256 "ece84a2fc9cfb33e1dd21153e078eff0897fdda512a137e146ac2d73589690bd" => :el_capitan
+    sha256 "03a219c97cd1b75f70f605aa2a7201f6e30feea3ec02727525f9cae13c7d8dec" => :x86_64_linux
   end
 
   option "with-fftw", "Compile with FFTW support"
