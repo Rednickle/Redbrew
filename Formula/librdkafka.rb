24b8b85a617b6d08d84af2344d337e62777f175c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-04 20:43:45 +0000

    librdkafka: update 0.11.3 bottle for Linuxbrew.

--- Formula/librdkafka.rb
+++ Formula/librdkafka.rb
@@ -10,6 +10,7 @@ class Librdkafka < Formula
     sha256 "dce656349af51404992cc1d56911f9339adaf45074675bcc1d8e48b8230006c2" => :high_sierra
     sha256 "772484a79bccead9e8964c572a0cb6a35432fdd3979f3f2f876f608db3d1d96f" => :sierra
     sha256 "444a966aeed17701a3ee4d7d1087aef507695600144bcf7e0ff747c0f9d662b8" => :el_capitan
+    sha256 "0a9088b5899dcb3d2a48c152b66ea508a964df6c3946687dc67044dc78530cb9" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
