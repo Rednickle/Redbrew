d4bf729be7851e5fb4514ed0db33942b757c7f8d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-07 22:49:29 +0000

    aptly: update 1.2.0 bottle for Linuxbrew.

--- Formula/aptly.rb
+++ Formula/aptly.rb
@@ -10,6 +10,7 @@ class Aptly < Formula
     sha256 "bc6eac38589d708fc99ec4dbe6d4b0f22765ed893f7f2ab19ccee50306d870f0" => :high_sierra
     sha256 "fdc4509d1f83c16c4d4b1eec87e6cfe5bc517eec3a67ad515244eeb9eb994d3e" => :sierra
     sha256 "5fbb7c41aeea6dbcc1c91e3bc1cf9c156e4d8820a828b8c935e07a9b3d3df942" => :el_capitan
+    sha256 "0d73a344cc9574df901e0da3ab4d4a9f35e52ae3668a38cb010ecb80877552c3" => :x86_64_linux
   end
 
   depends_on "go" => :build
