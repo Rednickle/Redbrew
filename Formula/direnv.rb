bf88e9f98c6bb73f32888f548f47418b00bb49f4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-15 22:42:23 +0000

    direnv: update 2.14.0 bottle for Linuxbrew.

--- Formula/direnv.rb
+++ Formula/direnv.rb
@@ -10,6 +10,7 @@ class Direnv < Formula
     sha256 "1dd24f6c9cb5082091f62a9a98aef11305e7a5f5d545d8368de421f0179318d4" => :high_sierra
     sha256 "642d28694bda5a4471919a5b73709eb8da6655e188f58640b13d95da3aecb973" => :sierra
     sha256 "257cba635f99eb52ba20be9558fe20fd67040cef64b758106a48ea241d500b72" => :el_capitan
+    sha256 "fa30f5f3f67a4eed57518f22a99458bcc04245248f3d0386349574ce517b64e3" => :x86_64_linux
   end
 
   depends_on "go" => :build
