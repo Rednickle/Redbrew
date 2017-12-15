54af8b69b3353ea5cf9de65d2a9061e5e73ed932
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-15 20:36:54 +0000

    fuse-zip: update 0.4.4 bottle for Linuxbrew.

--- Formula/fuse-zip.rb
+++ Formula/fuse-zip.rb
@@ -10,6 +10,7 @@ class FuseZip < Formula
     sha256 "76bda2baea148ceda1a4af12e94686c499a4acd5033dd846a148e07fc49c4b63" => :high_sierra
     sha256 "8f4054cc64b699d8ce29c33cca1e73e05c55e5151ab6eb4c6a8e6e0eb0ae9579" => :sierra
     sha256 "56131c4207a94a518a2a6ebdadeb6db58662b36990c15492fb03cccba89f58aa" => :el_capitan
+    sha256 "71e6ca7cf0b5310a85c70d685ee8b9a4af3e98be12b49f14e96d0ed324477089" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
