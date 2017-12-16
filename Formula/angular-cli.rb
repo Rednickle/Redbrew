24152a257f08267999b1a44144c687b1bd9fd5f6
Michka Popoff <michkapopoff@gmail.com>
2017-12-16 22:28:12 +0100

    angular-cli: update 1.6.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5188.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -10,6 +10,7 @@ class AngularCli < Formula
     sha256 "6510329fcfad270c73e5580b5a75ed23553c86dc31d3c6ba136a4785feaa75de" => :high_sierra
     sha256 "ee2e4dab0d2cb7cf6a2a581ca9c50a2ea9bf7d716d63db0e54639694fd2f6aa7" => :sierra
     sha256 "560ffde60f7126446ac421383a354aa4ca23ea9e3ae7a848483fcaeb6c043048" => :el_capitan
+    sha256 "bf651a866659d06547de7f75a2f5bc0ff23fb2b6ff594d0b4a9956fce437cd49" => :x86_64_linux
   end
 
   depends_on "node"
