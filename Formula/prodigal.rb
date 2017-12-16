8669ffb52612ae2d1db3a854beb0f3888af05d65
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-16 13:02:37 +0000

    prodigal: update 2.6.3 bottle for Linuxbrew.

--- Formula/prodigal.rb
+++ Formula/prodigal.rb
@@ -9,6 +9,7 @@ class Prodigal < Formula
     sha256 "c120fed8e29bb3b1a4ff69d5ca05e051a0fe3822784b3d585e142da3452d1ac1" => :high_sierra
     sha256 "a27fe5316181d4826e5aa5291d0fc1b1a7087c32c7b4e6aedabf1209d5a8ac36" => :sierra
     sha256 "70b432e3d3da1f4089680b06c0745b7dac3611f05d8ec9440faa918bc82d6fe5" => :el_capitan
+    sha256 "8ed04aa4b59962ddfeb1ff3a4bd58355071f7a1a33f7614f28425aaed39873c2" => :x86_64_linux
   end
 
   def install
