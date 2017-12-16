058aadb440c1ec06cf14772fedd7f21ac78d52c9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-16 12:07:09 +0000

    conan: update 0.30.1 bottle for Linuxbrew.

--- Formula/conan.rb
+++ Formula/conan.rb
@@ -12,6 +12,7 @@ class Conan < Formula
     sha256 "41a918a66238c5ee3eccc379f5635d7dad72a5f7f4aeadd95887dec3ba6e40ea" => :high_sierra
     sha256 "418f47c4c39283d3ef36897197550118d49de1116573c96199e1d2106d5a2e54" => :sierra
     sha256 "c8476c787c62980bc19ad1a2a6b0c468a3af12550b85e54082b87d891fc468ae" => :el_capitan
+    sha256 "8b65804688ecf6543c01cbfe6aa39c3947d10e6ac27ad5f84eb1f201cf25097f" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
