4208a3413d6f5dfffb3cd18e3916be5913dae555
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-16 15:37:21 +0000

    icu4c: update 60.2 bottle for Linuxbrew.

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -13,6 +13,7 @@ class Icu4c < Formula
     sha256 "cd6252141fd00076f67a01daceec4c5dfdea033c6a63b1a0ea7c98653757eb48" => :high_sierra
     sha256 "2d8e104b3346632e091386e8640d2f3033e38654341b9a670d334832b1432c11" => :sierra
     sha256 "54cc020520f422f5a708b20e1bec4e009cb7a52e7a150a3dd8edcfaa4f591200" => :el_capitan
+    sha256 "b83739278018fbb0014d306301e9bb93ac5a3e8bffa956ec3eb2c4f1aad78a36" => :x86_64_linux
   end
 
   keg_only :provided_by_osx, "macOS provides libicucore.dylib (but nothing else)"
