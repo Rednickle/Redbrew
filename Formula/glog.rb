1f21c344e582eea8e5530142148cb9adbd7db63c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-17 16:19:09 +0000

    glog: update 0.3.5_3 bottle for Linuxbrew.

--- Formula/glog.rb
+++ Formula/glog.rb
@@ -11,6 +11,7 @@ class Glog < Formula
     sha256 "2611ad281e7bf92bc8fb1480661ac1e28e7472d3eecad63572aa1f205f494722" => :high_sierra
     sha256 "8f4b25fe4396b3f32c7a7d058260b453adf2506e1d3a607d0ff48e664489526d" => :sierra
     sha256 "24561c61283ee126c107a5fbb2131ebcab0903df9f4af99bebf4fbf04a0fdf90" => :el_capitan
+    sha256 "49067c0b1c9924c7b740875a5aaa8bbb82f6363a407bb4231af94fcf9ff747c4" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
