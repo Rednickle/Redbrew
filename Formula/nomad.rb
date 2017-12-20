ccc592565fdd5896a833f941b42b186ed85aead2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-20 21:55:13 +0000

    nomad: update 0.7.1 bottle for Linuxbrew.

--- Formula/nomad.rb
+++ Formula/nomad.rb
@@ -10,6 +10,7 @@ class Nomad < Formula
     sha256 "650feea1f9bf5813aea5390d0eb0a6c6b8491ecff46be090774bd27b5cba49e9" => :high_sierra
     sha256 "e0e420c63315e67b6e7aa4ee6b32380ef7b4a95113d6c8ab6c0131393056e3a7" => :sierra
     sha256 "0a1d0ab2283e6b93de5e576b9c88e4ed230a31317138c88df0d2011a62a3ef0a" => :el_capitan
+    sha256 "72dd0a46e4bd55b95bdecfa7b9c98719f55a2def518614d60f9b61289c9ccbd1" => :x86_64_linux
   end
 
   option "with-dynamic", "Build dynamic binary with CGO_ENABLED=1"
