a9f54cdb172c57e21a573347637dc37bd29fb06e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-20 21:55:12 +0000

    lxc: update 2.21 bottle for Linuxbrew.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -9,6 +9,7 @@ class Lxc < Formula
     sha256 "325f979603960d5035c7fa17f6149d436addea2f8570d481bc30883cb864c32b" => :high_sierra
     sha256 "04b78e8696cf811513245c5471f15f30fd6b166cb584a78ebc58ade999fbdff7" => :sierra
     sha256 "d5616ea2105fd7f76638b4fb19160cb3492ba54c2b34afeef783e9d281cfde28" => :el_capitan
+    sha256 "9a98f0aaf1aec4f501db33187fbba4d12c2ede7787c1dda46b2102fde6c65a1c" => :x86_64_linux
   end
 
   depends_on "go" => :build
