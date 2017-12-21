72356c90b534c288a2c715666161527e2153ced2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-21 18:09:25 +0000

    stress-ng: update 0.09.06 bottle for Linuxbrew.

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -8,6 +8,7 @@ class StressNg < Formula
     cellar :any_skip_relocation
     sha256 "d12df90843e3e9280cea73363a34e2d1aa05e068cacaf6468b16496678509646" => :high_sierra
     sha256 "db2810739690804afa23f0df40233271352a792ffbdf0e4ead7f9be8ed636bf2" => :sierra
+    sha256 "ff233339fca651a41037730644e1540d23b3e38bc1bfe63db56a3f7a3172b8f9" => :x86_64_linux
   end
 
   depends_on :macos => :sierra
