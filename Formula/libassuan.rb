abcfee7042c1c9ed3402cf20e9ffc0cd7ced469d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-22 20:46:32 +0000

    libassuan: update 2.5.1_1 bottle for Linuxbrew.

--- Formula/libassuan.rb
+++ Formula/libassuan.rb
@@ -11,6 +11,7 @@ class Libassuan < Formula
     sha256 "a05af2506efccbd153b216c2ed6b17c7b28b1c50fce16489166782877695b8ee" => :high_sierra
     sha256 "3aefafdbaf62a8319b618c73654aef88f9ebe8b9cba51a304aebaa09bfc92c4c" => :sierra
     sha256 "6e4ef55130d531e2e24226379f70c6ea59e62caf4fcc8fabffdecf8b2a0d018e" => :el_capitan
+    sha256 "ecd72b3ee211005723b01a867b38fe96a2f7bf3c460250eba4bf57f370b2adee" => :x86_64_linux
   end
 
   depends_on "libgpg-error"
