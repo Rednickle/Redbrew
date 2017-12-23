e87fd962d98ed21def0699ccc8f9b20e76a01dc9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-23 14:21:38 +0000

    vultr: update 1.13.0 bottle for Linuxbrew.

--- Formula/vultr.rb
+++ Formula/vultr.rb
@@ -11,6 +11,7 @@ class Vultr < Formula
     sha256 "afc91aa3d98447dcdc93892d94dc2b00ed5d4af0b6f1095a80ae4641f0abb23b" => :high_sierra
     sha256 "87683a1ade9779c8431f08a76a3801890a437a499144646c5232db0567ea4712" => :sierra
     sha256 "7ca573144e22487f7c506073cda826ba46e768ab0f34230b1f2375799580c39b" => :el_capitan
+    sha256 "76796a8aa5c9e126e4d93c0561d0eb3088068b410a1057042dffddf6973cbf90" => :x86_64_linux
   end
 
   depends_on "go" => :build
