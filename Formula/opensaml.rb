696f71b23fb52ca7509ddb8dd4d403f30ba51a4a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-24 19:20:44 +0000

    opensaml: update 2.6.1 bottle for Linuxbrew.

--- Formula/opensaml.rb
+++ Formula/opensaml.rb
@@ -9,6 +9,7 @@ class Opensaml < Formula
     sha256 "465fc254c7398254be2853488c53ca06c490dda783bbfc3041a5031a3e0507c4" => :high_sierra
     sha256 "dc7242ca3ea9c197b99c2a1f9ef1fe4a752a8d170656e045c524b87d48bce9c2" => :sierra
     sha256 "db4e0d6d46638982c5fb0c90cbd9aba13696761533c025c7852c4b4dbf5bc567" => :el_capitan
+    sha256 "9798da276ad7ca32b0f0317b59373a3f02e507cc823980783edccf5c6feb1340" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
