f1ee33c82a2ac44b55a439dd91c005adfbb0b5d6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-24 16:16:59 +0000

    libatomic_ops: update 7.6.2 bottle for Linuxbrew.

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -9,6 +9,7 @@ class LibatomicOps < Formula
     sha256 "eab4cb5ef3fd4aca17588e7ef5925fce490fbc4bd4f970db1420aa0fd5dc0a46" => :high_sierra
     sha256 "f4490f641b310ff0abd18ef8ecd4bfc542d6c99567ef9fc2aab716c8048bc5ba" => :sierra
     sha256 "f44a1ebafef8923b58d81dd1ef26fd1b136300c900c593c22f2c74eddb0c9f61" => :el_capitan
+    sha256 "4d330d43a0c6647514254122b7305f499a3250c31934418d2d6bc323b23f5441" => :x86_64_linux
   end
 
   def install
