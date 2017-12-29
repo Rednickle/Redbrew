81b9aae0f699539167158f10f28f3e97f26e7042
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-29 20:42:09 +0000

    pspg: update 0.9.1 bottle for Linuxbrew.

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -10,6 +10,7 @@ class Pspg < Formula
     sha256 "90106c7f56507802a0678c0d978835a1b7b4b2a5bb7a6004488d463f2f8daee7" => :high_sierra
     sha256 "d1e29f14696b0daacd11ff58b80d404068107cf5e5b65734cd28ed8a7581f5b2" => :sierra
     sha256 "b2b6fff8b571781a81222edcb480258e7ae561470a29a6b7e76e1c8324e08a13" => :el_capitan
+    sha256 "a666a032c811abaa8fe8d17fbf13f330a86e9ebda4bac4735886685c3a73baaf" => :x86_64_linux
   end
 
   depends_on "ncurses"
