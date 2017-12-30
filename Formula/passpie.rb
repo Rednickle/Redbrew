6af28bc0abebe01d7937ee86ac18f1497020f9b0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-30 17:07:11 +0000

    passpie: update 1.6.0_1 bottle for Linuxbrew.

--- Formula/passpie.rb
+++ Formula/passpie.rb
@@ -13,6 +13,7 @@ class Passpie < Formula
     sha256 "c76cf8962d2cef65aa5a59672e58be1ad0fd74f48ef94608f34275800f243d29" => :high_sierra
     sha256 "858a53fbd86235af38263ff97b7f283f434fe59623f6bf170845fc51a0ebc0c7" => :sierra
     sha256 "9676b9237428cd46ce1fcfb42b7911ebf96a7df2535e936bb60d3f57507db8f4" => :el_capitan
+    sha256 "80853bbcd7a24fa8d1aefcef47a38376b85f7d96b2a9c05c90765047a369b151" => :x86_64_linux
   end
 
   depends_on :python if MacOS.version <= :snow_leopard
