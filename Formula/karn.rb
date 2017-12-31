cb514fe764776276d1c80f8af1b4908ab38671e7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-12-31 10:55:06 +0000

    karn: update 0.0.4 bottle for Linuxbrew.

--- Formula/karn.rb
+++ Formula/karn.rb
@@ -9,6 +9,7 @@ class Karn < Formula
     sha256 "a837fd65265db402d67fda5ff5bb4337822d1efd945bee56f7a664e6bc67c343" => :high_sierra
     sha256 "0b29500ed8d75753402ea041190021d679624b739665b3a4d11df3d4a3100e59" => :sierra
     sha256 "bc217bf56d073ffabd11c81382387029aa09d216ef8060b3134a6190997ef0f5" => :el_capitan
+    sha256 "03533f5131e54f5c363cdb27a03a245770cfd952a0eb0d8b4ed6d3242a4194ac" => :x86_64_linux
   end
 
   depends_on "dep" => :build
