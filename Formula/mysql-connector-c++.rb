16a0149e5b7337b16387116dab9e343a837f54a5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-01 19:46:49 +0000

    mysql-connector-c++: update 1.1.9_1 bottle for Linuxbrew.

--- Formula/mysql-connector-c++.rb
+++ Formula/mysql-connector-c++.rb
@@ -10,6 +10,7 @@ class MysqlConnectorCxx < Formula
     sha256 "6c70466e62e77f91a8f3d8e8d824941740af1d00ae5903f2f5267a99eec65f63" => :high_sierra
     sha256 "1ed6ca22cd4a44056080eb829eeda0acd781f2e83d931ba7482c96b78e7863d6" => :sierra
     sha256 "2c6358f7a032c66a18ed1f7dc87ae1b409ae61c689ccb4961afe1fdf97f11103" => :el_capitan
+    sha256 "c1f0f5d9f2b9122c852e0aa9a85358bd08ddc5b9aa8d1aba939d86f95c5c7129" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
