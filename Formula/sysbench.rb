23c569e42997ada5434f65f915139efb3ab3eb34
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-01 19:46:50 +0000

    sysbench: update 1.0.11_1 bottle for Linuxbrew.

--- Formula/sysbench.rb
+++ Formula/sysbench.rb
@@ -9,6 +9,7 @@ class Sysbench < Formula
     sha256 "ac12259c76de51291ae4bb909647fcaf1133c5dfd7f2f0d983f3a1fb0727a909" => :high_sierra
     sha256 "1428b77fd48206693f787cc3028e3ad9e4e3ad8b4159a00a424d567180af388f" => :sierra
     sha256 "91d311d1b5a362777fe8957235d7ece948376a63afcbaf03631fc64822637ee8" => :el_capitan
+    sha256 "a09b674605cd5db3a079c1f97fb01cd4c5ef59a3fe4a66594a287930d8ca46b1" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
