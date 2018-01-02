0656cfc32d66249dd66d82e38f16ecd66d5d2852
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-02 20:24:20 +0000

    tnef: update 1.4.16 bottle for Linuxbrew.

--- Formula/tnef.rb
+++ Formula/tnef.rb
@@ -9,6 +9,7 @@ class Tnef < Formula
     sha256 "70930528cc6ae6e186694bc4de8b176b1a0eb42cf144be531753d0360929456f" => :high_sierra
     sha256 "9677d96c4bb20efe6dc7692f8b436efd983031e310a8bb2b63cd3ed3ab78e7ed" => :sierra
     sha256 "b6deb78ba081850f86c5f641799626be827b40db01257c3ab3680aa8b06baa9a" => :el_capitan
+    sha256 "615f20ab7b900a9c08c9133b5dde3ac78ccf389e9c178b0349ee8e4ca0cf9724" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
