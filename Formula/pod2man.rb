cd40d622caaf2ba3fb985a310897f348403817ee
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-02 06:27:06 +0000

    pod2man: update 4.10 bottle for Linuxbrew.

--- Formula/pod2man.rb
+++ Formula/pod2man.rb
@@ -9,6 +9,7 @@ class Pod2man < Formula
     sha256 "e92e6b707bc43455372737a425e346a85ef58f02d0f1ba1f9aa3c5a39342e8aa" => :high_sierra
     sha256 "d14e362b3cd8f1f5b3b949ba80c260e67a7263db05b55aa6917e8d078768fd72" => :sierra
     sha256 "a1d9b068b86149457b8c99e473ed880120b456826341d540854ffc116d301ead" => :el_capitan
+    sha256 "728bde6c39f0a34e6d4ec5c43f92bfc69eff342e1b9e20e8016d2ee315eacc22" => :x86_64_linux
   end
 
   keg_only :provided_by_osx
