b051e5b2715ed8e8c6206079c370e94ab8e69cbb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-02 17:46:26 +0000

    libcds: update 2.3.2 bottle for Linuxbrew.

--- Formula/libcds.rb
+++ Formula/libcds.rb
@@ -9,6 +9,7 @@ class Libcds < Formula
     sha256 "b1fb46232f36fd9bc2fef02b1118e5398609b3343b3376d5130a5c7633c7c9ae" => :high_sierra
     sha256 "cf6b35c96cdf54f090fb02f519bb00581ae9e4214416dbeabdc89235a5c94e57" => :sierra
     sha256 "5f6db23a55e3bc9ce3358662b1f9122e9eb032f7eedef4b47e6bf9f27ebc052d" => :el_capitan
+    sha256 "8ae646faf09b7b2178e2335b3eb948b43d1406c21b80ee3f4ca5b1b837967e22" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
