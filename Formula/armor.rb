1017b6478afdb1cb5bbd8ed2331045e8cc83a282
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-03 19:51:52 +0000

    armor: update 0.3.7 bottle for Linuxbrew.

--- Formula/armor.rb
+++ Formula/armor.rb
@@ -10,6 +10,7 @@ class Armor < Formula
     sha256 "7ab8d42ba1c05939f3d28fc05813150af90dcd6db793d9673a2196fa74c833e4" => :high_sierra
     sha256 "0e293038c3999a34c135fc762ae662f3cfd9c4d0fa3706e09d243b71ab075283" => :sierra
     sha256 "ca58b916a530eae4c2ad964699cf740134887c3b3e8aad6b6e33600fe065d11a" => :el_capitan
+    sha256 "c585b775a764f032cfbf29eb4bdc3c20c22b7a159ec3a9e47632041daea1d429" => :x86_64_linux
   end
 
   depends_on "go" => :build
