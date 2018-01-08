39b65be48820c79ea021a621bef25767ac541887
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-08 20:05:03 +0000

    miller: update 5.3.0 bottle for Linuxbrew.

--- Formula/miller.rb
+++ Formula/miller.rb
@@ -9,6 +9,7 @@ class Miller < Formula
     sha256 "c833decfa926e2ba3a1815f2a1d924a1c22b8e87a3e915cf6df843965e967189" => :high_sierra
     sha256 "3242d8648f791da23243f2ba44e081ff9cdb875fb35669e722fc9b2fcf3805f2" => :sierra
     sha256 "abb0e04f1704a2601f613f6ecea3a360425f194084a87f5be0f37ba677b91869" => :el_capitan
+    sha256 "3333fb93f0de86476c41406d3c36f518c7bdf7940fb64eebad272ab49be69007" => :x86_64_linux
   end
 
   head do
