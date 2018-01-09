2657053bf01f685dd296ec446bfc22399cee7c72
Char-Aznable <treinz@163.com>
2018-01-09 10:23:46 -0800

    pygobject: build a new bottle (issue #5439)
    Closes Linuxbrew/homebrew-core#5447.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pygobject.rb
+++ Formula/pygobject.rb
@@ -3,13 +3,13 @@ class Pygobject < Formula
   homepage "https://live.gnome.org/PyGObject"
   url "https://download.gnome.org/sources/pygobject/2.28/pygobject-2.28.7.tar.xz"
   sha256 "bb9d25a3442ca7511385a7c01b057492095c263784ef31231ffe589d83a96a5a"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any
     sha256 "5d0cfd09b590acbb425aac203d351e90d27a9b00ff7a15ba9dfd2a3805a44c4f" => :high_sierra
     sha256 "e738e8e735daf77d83d251434fb3dd43b64b5e07b6259dc1d8fd7db1c6c85ea8" => :sierra
     sha256 "f5fcee187ce5ebdedf6c023b8f5fa474df263e1ce2854de8d968540c89648b31" => :el_capitan
-    sha256 "6f18a9403eee032f0560431fe0aab7cf51164e1c7ff92687c09a7b10aab50605" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
