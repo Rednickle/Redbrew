6fa23f0e35b1902dfbc088d13912e401de8b0582
Michka Popoff <michkapopoff@gmail.com>
2018-01-10 23:40:29 +0100

    buku: update 3.6 bottle for Linuxbrew.

--- Formula/buku.rb
+++ Formula/buku.rb
@@ -11,6 +11,7 @@ class Buku < Formula
     sha256 "e71283396a853d30d0ea6ce6275dfd6309ad23764bd29e504c8e03bbb82f494e" => :high_sierra
     sha256 "2bcaf5944032ee5d30ae090f5fa694f2ddbcdd1fa45832212cfcb08094913676" => :sierra
     sha256 "710f3421de05e8ebbfbbb39e2c9efefa03ffdbfa800780e9e27d5f42c5a1f944" => :el_capitan
+    sha256 "06850534a9efd00104dad72c4bd7f006e5d8e7127c0d94346e9bffa91bb1d4bc" => :x86_64_linux
   end
 
   depends_on "openssl"
