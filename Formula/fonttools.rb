107e0414e25570e3e709fc1d44cdf42282fd4081
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-10 07:22:04 +0000

    fonttools: update 3.21.2 bottle for Linuxbrew.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -12,6 +12,7 @@ class Fonttools < Formula
     sha256 "b442df81be2116971ab68015b73df4db12084e8c5847024c4de45b549abd9680" => :high_sierra
     sha256 "62b11fa5dd51c4aa8e94be0bdf866065f34c987c8b3171c6e3613fb7440c26ee" => :sierra
     sha256 "709484897698914e051a22d827f201b9be8a8ffff48e178d7f1356638785f948" => :el_capitan
+    sha256 "7d6ffe7f1a939f3a9a4fa22edb4d533c4e05fc26998d7a33286eac02b8babd2c" => :x86_64_linux
   end
 
   option "with-pygtk", "Build with pygtk support for pyftinspect"
