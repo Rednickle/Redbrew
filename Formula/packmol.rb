29dfc13dac1334a5a9c2f1a2ce62bc0fe0cdd766
Michka Popoff <michkapopoff@gmail.com>
2018-01-10 18:52:14 +0100

    packmol: update 18.008 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5458.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/packmol.rb
+++ Formula/packmol.rb
@@ -9,6 +9,7 @@ class Packmol < Formula
     sha256 "30074ccecc3a1086a925a4ac4ef679c658f0e893d99cd192a6fb75d7e3bf1359" => :high_sierra
     sha256 "96f907b693394e52f9e7d5b20754d13c7b17aaf73b932b798bb601623b46780d" => :sierra
     sha256 "0ea1df64f15e6526737b75a982a3cd2bf4becc609ffc0b47e73658a7c7fff8e8" => :el_capitan
+    sha256 "4182d902231d2b07b58a360f1584c21c33e12ef11ad629702caea71660c583e3" => :x86_64_linux
   end
 
   depends_on "gcc"
