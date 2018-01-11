a04238bc1ef9202ec2ac56cb21116327afb2c938
Michka Popoff <michkapopoff@gmail.com>
2018-01-11 06:53:29 +0100

    u-boot-tools: update 2018.01 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5490.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/u-boot-tools.rb
+++ Formula/u-boot-tools.rb
@@ -9,6 +9,7 @@ class UBootTools < Formula
     sha256 "cd5663b4ca5e3d25eb2702e2c8abaae54946ef34fd45e62bac6146c6b7c9e687" => :high_sierra
     sha256 "0cc15099ae1e45facf124f8ccb3e49be2ea0f81ba60f33039c7b16ffec92d9e5" => :sierra
     sha256 "81e15d2aee2ab3e413f632c4cc6cc3180f5e48b74e1fa0a59ad6f623f79a0c64" => :el_capitan
+    sha256 "2014c8832183406a2f7ac949ce11fca9b5c7cb8dfcf6d948d03f13c716557321" => :x86_64_linux
   end
 
   depends_on "openssl"
