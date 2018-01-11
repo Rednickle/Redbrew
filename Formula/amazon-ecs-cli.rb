25475af0782cf128ef4f03fa12a578b62ffee415
Michka Popoff <michkapopoff@gmail.com>
2018-01-11 20:35:57 +0100

    amazon-ecs-cli: update 1.2.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5465.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -9,6 +9,7 @@ class AmazonEcsCli < Formula
     sha256 "a9f6ba3103a3907d186e34749f3f251b43f68ad1f429a84d401b22a496be1c80" => :high_sierra
     sha256 "c52e010481d1dd645f14ce7f8e8501afe5f32035f3c8053d139ead561e314093" => :sierra
     sha256 "6cd4bdcbb688afa61f55536e26acf19ab92cc76e26dd829c3a5240a892623c33" => :el_capitan
+    sha256 "a2ccb7ad31f062a4ed3f90aa4c5c6f705516e6f1812d1f1822afd187ba4945c6" => :x86_64_linux
   end
 
   depends_on "go" => :build
