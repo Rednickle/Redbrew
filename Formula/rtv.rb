3ee05072e68994f6aa6877a33b3b6cca666308d4
Michka Popoff <michkapopoff@gmail.com>
2018-01-12 20:30:23 +0100

    rtv: update 1.21.0_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5511.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/rtv.rb
+++ Formula/rtv.rb
@@ -13,6 +13,7 @@ class Rtv < Formula
     sha256 "c9ee98d9223e0410254b749751c2a175b900e133e5c029c5ae74ae36b1d7ce0f" => :high_sierra
     sha256 "cb33c61261304d00d606f9666071234f8c8227a4fc7750c94864ac395322ab74" => :sierra
     sha256 "fb36bbf09f27ca20d22cbdac955363333a5a67bca625237a5049e366091cd732" => :el_capitan
+    sha256 "ce0333f759f52e886199d48ba76819dadc5ed4e76ca94317f982ca7899f53adf" => :x86_64_linux
   end
 
   depends_on "python3"
