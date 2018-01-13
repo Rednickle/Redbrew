6b04f99a74243cb1e07220f407a7468d364ae1b8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-13 11:25:13 +0000

    taktuk: update 3.7.7 bottle for Linuxbrew.

--- Formula/taktuk.rb
+++ Formula/taktuk.rb
@@ -9,6 +9,7 @@ class Taktuk < Formula
     sha256 "9cc466f8a75eea1974143fedecd42547eb14401d772e527776f387aec4832f77" => :high_sierra
     sha256 "0ffc0bb09703bbf32afbcd302850803f94ecbb311eaa77353275e7dcb1549f62" => :sierra
     sha256 "4a731d243e6915729240deb75dc99cfee513bb7d0f69169981623b14ce6601c1" => :el_capitan
+    sha256 "209fb6acfe7019c707a7f7e1779c3bfb4cdd0de237145e35970e938e8e88de4a" => :x86_64_linux
   end
 
   def install
