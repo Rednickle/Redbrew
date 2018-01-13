61f2a8ec1669c3b258e944f64632a48e513bcd48
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-13 10:45:24 +0000

    pygobject: update 2.28.7_1 bottle for Linuxbrew.

--- Formula/pygobject.rb
+++ Formula/pygobject.rb
@@ -10,6 +10,7 @@ class Pygobject < Formula
     sha256 "5d0cfd09b590acbb425aac203d351e90d27a9b00ff7a15ba9dfd2a3805a44c4f" => :high_sierra
     sha256 "e738e8e735daf77d83d251434fb3dd43b64b5e07b6259dc1d8fd7db1c6c85ea8" => :sierra
     sha256 "f5fcee187ce5ebdedf6c023b8f5fa474df263e1ce2854de8d968540c89648b31" => :el_capitan
+    sha256 "b17c5888455bfde9a8c9c118a29933231ef3f3e499ccd0ea408f03011f564e5f" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
