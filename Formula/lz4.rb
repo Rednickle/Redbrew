4fb951fdb7ca15c7be75588d439703b3ecfd4626
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-13 10:39:24 +0000

    lz4: update 1.8.1 bottle for Linuxbrew.

--- Formula/lz4.rb
+++ Formula/lz4.rb
@@ -10,6 +10,7 @@ class Lz4 < Formula
     sha256 "18fce11b03002004968347a5e1068918bf8efd3f2f73e8ff8f49e0ccee2e0b14" => :high_sierra
     sha256 "60ac08cee152ca6ef3c470f1c5434566faae0ea1ed91d0d411610ccf70221499" => :sierra
     sha256 "089fb3dc78b935fb36f04ff8afb4d4147cfc34f1e6639bc5d6d9ecae117f42c9" => :el_capitan
+    sha256 "4ea365c6262c1ce7ccc5379c205f26709b1feeef97937e95a1ad89294cf791c8" => :x86_64_linux
   end
 
   def install
