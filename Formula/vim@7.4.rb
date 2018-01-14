0043db0c8a5f878c76ede66f5f7baab0ac4fe157
Michka Popoff <michkapopoff@gmail.com>
2018-01-14 10:04:03 +0100

    vim@7.4: update 7.4.2367_7 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5555.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/vim@7.4.rb
+++ Formula/vim@7.4.rb
@@ -9,6 +9,7 @@ class VimAT74 < Formula
     sha256 "00743decb59ff8b042a71e2cdd9b406e2cd0cce0cf83b51c6a7f88d077632d93" => :high_sierra
     sha256 "5af2152df6680a1d23c6a57b95d81cbbb17de0964ed8deaf4b14528b06aa8afb" => :sierra
     sha256 "2f07d60810a03059a3e72d8ae8dd195db23af44539deadf1d99b9c462c4b5f62" => :el_capitan
+    sha256 "c5e4dc7d8e421baee6c4e558ba42ed6d3575ddb3b0e9471cc3994992e5f06ccd" => :x86_64_linux
   end
 
   keg_only :versioned_formula
