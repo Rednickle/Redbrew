898d5ae30dab70d4e7ce8a8fae26a4c8650dc24d
Michka Popoff <michkapopoff@gmail.com>
2018-01-14 21:08:01 +0100

    wxpython: update 3.0.2.0_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5606.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/wxpython.rb
+++ Formula/wxpython.rb
@@ -10,6 +10,7 @@ class Wxpython < Formula
     sha256 "b3ed6dbe3c7b58b55d98025a7e2799137eeb78ec6260d18cd1b4e2057fb969f5" => :high_sierra
     sha256 "2ada5517b6cb456d65ad6849a65355893676d09f6c7a997b03325f0079a849f4" => :sierra
     sha256 "073777ade162a654d43d5866f69e6a857a7cedc677d7115200972e91261d9e0f" => :el_capitan
+    sha256 "ea35d80e98cda71b4942b9c744ea0f74550f4e04eb7ab06072579a4f8b8c0000" => :x86_64_linux
   end
 
   depends_on "python" if MacOS.version <= :snow_leopard || !OS.mac?
