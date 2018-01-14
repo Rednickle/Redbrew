ca86f8c494aed22c62cd3f5cef090407f352a06b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-14 12:59:47 +0000

    mpich: update 3.2.1_1 bottle for Linuxbrew.

--- Formula/mpich.rb
+++ Formula/mpich.rb
@@ -10,6 +10,7 @@ class Mpich < Formula
     sha256 "bab0862c4f607c8f411d16b754fb6474f536c5340c2d1c9be4a339ca4a6d9bf9" => :high_sierra
     sha256 "bfb708826242dd9e27ce8c46bba866cbec2c95ee6db419c56e7942c32a88b3b3" => :sierra
     sha256 "214e469d1b5bdbdc685cc31a53e8673880640a2551274c8f06c08b347db7ee54" => :el_capitan
+    sha256 "fc2ae124539eeae51740490bbcedc782ff700998f78176d77a79a52c64f5c49f" => :x86_64_linux
   end
 
   devel do
