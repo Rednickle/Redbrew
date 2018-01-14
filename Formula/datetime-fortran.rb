1f067de602ff7c9bfccb50cdcb9703085bc0213c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-14 10:16:10 +0000

    datetime-fortran: update 1.6.0_2 bottle for Linuxbrew.

--- Formula/datetime-fortran.rb
+++ Formula/datetime-fortran.rb
@@ -10,6 +10,7 @@ class DatetimeFortran < Formula
     sha256 "887a64af4a2366598e0e374ddb664a303381c9ce849bff4de6297f522999daae" => :high_sierra
     sha256 "b3106afade8eeb808df615f809a185ea9e7da4f01974b5ae39f244eb89ca4545" => :sierra
     sha256 "39b4cbe4d95db475a36909c9d4241d3726523cab239f3b468c4fef6679abf1a0" => :el_capitan
+    sha256 "24a1bdffffb92dbafd3270215ee677b12af347af9bdda748147e4a9e570a2894" => :x86_64_linux
   end
 
   head do
