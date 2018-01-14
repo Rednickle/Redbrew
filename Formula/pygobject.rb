6210c209f03c228acb97982a19522ed98d5251a9
Michka Popoff <michkapopoff@gmail.com>
2018-01-14 22:18:20 +0100

    pygobject: update 2.28.7_2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5602.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pygobject.rb
+++ Formula/pygobject.rb
@@ -10,6 +10,7 @@ class Pygobject < Formula
     sha256 "44d2fbe065a06b2f874b8d0b770375b0e8628a77f67823a09cf354d4bf1fd3e3" => :high_sierra
     sha256 "85854484b61200e295a9767b34bb86eb06b33045429f160c19f84cca39abaf17" => :sierra
     sha256 "47b48d492fad94254508ce966c3e9261b74879049a5fc9a4fb34e3209c0f4ce5" => :el_capitan
+    sha256 "9f1e1758c537f90aefdb3116089d76bf860c287293f0119df7dc2f468baee256" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
