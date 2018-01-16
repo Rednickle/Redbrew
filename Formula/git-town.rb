d127ee64b80bc9665fb48391b4e4408b7138a344
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-16 20:16:59 +0000

    git-town: update 6.0.0 bottle for Linuxbrew.

--- Formula/git-town.rb
+++ Formula/git-town.rb
@@ -9,6 +9,7 @@ class GitTown < Formula
     sha256 "17bf26bc0a68b03bec2ba4450d0730fe1636e6db043d6064436038564fa58100" => :high_sierra
     sha256 "e967d48b8f65d1173965f3f8eabd95ba218d4224d83f4bd79f9d91dfb97fa66c" => :sierra
     sha256 "6fc6244729e3487901c5c6b4c7c35a390786ce648617b0f6a951fbabfce8155a" => :el_capitan
+    sha256 "4f21b52c7592834250e033fa7b0ab0abede2332c147219549cadb197e5de9356" => :x86_64_linux
   end
 
   depends_on "go" => :build
