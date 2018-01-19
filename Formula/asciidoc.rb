e9ee017b603e9c0e0dc493c05b0f1d53786a4069
Michka Popoff <michkapopoff@gmail.com>
2018-01-19 21:20:53 +0100

    asciidoc: Depend on python formula for Linuxbrew
    Closes Linuxbrew/homebrew-core#5655.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/asciidoc.rb
+++ Formula/asciidoc.rb
@@ -4,7 +4,7 @@ class Asciidoc < Formula
   # This release is listed as final on GitHub, but not listed on asciidoc.org.
   url "https://github.com/asciidoc/asciidoc/archive/8.6.10.tar.gz"
   sha256 "9e52f8578d891beaef25730a92a6e723596ddbd07bfe0d2a56486fcf63a0b983"
-  revision 1
+  revision OS.mac? ? 1 : 2
   head "https://github.com/asciidoc/asciidoc.git"
 
   bottle do
@@ -12,7 +12,6 @@ class Asciidoc < Formula
     sha256 "c82aaf006c1e61742f4b0d01106b34efa0bba4c7b315f31298b6cce1d9616947" => :high_sierra
     sha256 "c82aaf006c1e61742f4b0d01106b34efa0bba4c7b315f31298b6cce1d9616947" => :sierra
     sha256 "c82aaf006c1e61742f4b0d01106b34efa0bba4c7b315f31298b6cce1d9616947" => :el_capitan
-    sha256 "40b6be71483748a03362b9729dfeba218169a31ee61f835c230c643e2f1f7cac" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
@@ -22,7 +21,7 @@ class Asciidoc < Formula
     depends_on "libxml2" => :build
     depends_on "xmlto" => :build
     depends_on "libxslt" => :build # for xsltproc
-    depends_on :python
+    depends_on "python"
   end
 
   def install
