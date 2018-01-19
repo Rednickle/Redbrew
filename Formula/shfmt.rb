b6d8790652796ac204547ef937c461e305d9814e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-19 19:45:55 +0000

    shfmt: update 2.2.0 bottle for Linuxbrew.

--- Formula/shfmt.rb
+++ Formula/shfmt.rb
@@ -10,6 +10,7 @@ class Shfmt < Formula
     sha256 "45bd5112c69c48f5b19bce1d7c92aeb8cbef3d03716b3b4ca9a5d06926d129fb" => :high_sierra
     sha256 "181aa0535d88e80803e7a77fd67a8ba4d736fcdafcb1f971db7e3ae8b8fcb9fe" => :sierra
     sha256 "680f36f0009d752c1543b702f7c5f677dde7b4fab1595b4256de51e4d30a6939" => :el_capitan
+    sha256 "67c42f8b6db5ec2d15e6e01e6086fef7ec7cd9978d8655fd68c59345ecf17ea9" => :x86_64_linux
   end
 
   depends_on "go" => :build
