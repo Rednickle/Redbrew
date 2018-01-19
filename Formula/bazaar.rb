67ddac06dfee240f38a9b3f1725184a5813f8cb6
Michka Popoff <michkapopoff@gmail.com>
2018-01-19 21:23:40 +0100

    bazaar: Depend on python formula for Linuxbrew
    Closes Linuxbrew/homebrew-core#5657.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/bazaar.rb
+++ Formula/bazaar.rb
@@ -3,14 +3,13 @@ class Bazaar < Formula
   homepage "https://bazaar.canonical.com/"
   url "https://launchpad.net/bzr/2.7/2.7.0/+download/bzr-2.7.0.tar.gz"
   sha256 "0d451227b705a0dd21d8408353fe7e44d3a5069e6c4c26e5f146f1314b8fdab3"
-  revision 1
+  revision OS.mac? ? 1 : 2
 
   bottle do
     cellar :any_skip_relocation
     sha256 "102d9ec9fbed0c83402195d77f20bbcf94210face42bcddf98c3feda6c9587f7" => :high_sierra
     sha256 "4a927398b0b1fc8ce43e16a227f40c63409a752338fd36600d05b907d960cab5" => :sierra
     sha256 "6d1409dc49d838c0209bc59ebeb4ec5b70c5c1caef3b27b97f0ebedd6d8ff515" => :el_capitan
-    sha256 "b372b768e40f525b82e67db6e5f3e43a1e4707de049a2825b0cef788acb938df" => :x86_64_linux
   end
 
   # CVE-2017-14176
@@ -22,7 +21,7 @@ class Bazaar < Formula
     apply "patches/27_fix_sec_ssh"
   end
 
-  depends_on :python unless OS.mac?
+  depends_on "python" unless OS.mac?
 
   def install
     ENV.deparallelize # Builds aren't parallel-safe
