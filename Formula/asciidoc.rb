e50f70190b8225207c77220762bdd796ac78d927
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-20 10:14:58 +0000

    asciidoc: update 8.6.10_2 bottle for Linuxbrew.

--- Formula/asciidoc.rb
+++ Formula/asciidoc.rb
@@ -12,6 +12,7 @@ class Asciidoc < Formula
     sha256 "c82aaf006c1e61742f4b0d01106b34efa0bba4c7b315f31298b6cce1d9616947" => :high_sierra
     sha256 "c82aaf006c1e61742f4b0d01106b34efa0bba4c7b315f31298b6cce1d9616947" => :sierra
     sha256 "c82aaf006c1e61742f4b0d01106b34efa0bba4c7b315f31298b6cce1d9616947" => :el_capitan
+    sha256 "fef651417eeae42905dcd1da05e413eb23a466fe807aab45da12dff8ae919343" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
