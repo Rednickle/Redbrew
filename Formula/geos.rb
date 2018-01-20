7f77b8e61d3f81f6866d03e961dfb282874c16cd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-20 10:55:18 +0000

    geos: update 3.6.2_1 bottle for Linuxbrew.

--- Formula/geos.rb
+++ Formula/geos.rb
@@ -11,6 +11,7 @@ class Geos < Formula
     sha256 "a435dcf855256300793f56c3d0af6597f39958bde52adc29d07736f897b40c1b" => :sierra
     sha256 "8fa4f17ee4b0f8c52bdb155f4264043f12245858e413975cfea4355d569db207" => :el_capitan
     sha256 "5966c5ecea54189c67a3ffb5856176f4bb070ca72b3c3628ad7b76fb67e35de8" => :yosemite
+    sha256 "44cbe59f4b1b92f4c3beae5fab480328783a87cb5ff16040e1e51cd93f39cc08" => :x86_64_linux
   end
 
   option "without-python", "Do not build the Python extension"
