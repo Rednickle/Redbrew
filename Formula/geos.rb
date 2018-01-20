f532e63d422b90a807c02db26bfe5e28917292a1
Michka Popoff <michkapopoff@gmail.com>
2018-01-20 11:42:48 +0100

    geos: Depend on Python formula for Linuxbrew
    Closes Linuxbrew/homebrew-core#5664.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/geos.rb
+++ Formula/geos.rb
@@ -3,6 +3,7 @@ class Geos < Formula
   homepage "https://trac.osgeo.org/geos"
   url "http://download.osgeo.org/geos/geos-3.6.2.tar.bz2"
   sha256 "045a13df84d605a866602f6020fc6cbf8bf4c42fb50de237a08926e1d7d7652a"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any
@@ -10,14 +11,13 @@ class Geos < Formula
     sha256 "a435dcf855256300793f56c3d0af6597f39958bde52adc29d07736f897b40c1b" => :sierra
     sha256 "8fa4f17ee4b0f8c52bdb155f4264043f12245858e413975cfea4355d569db207" => :el_capitan
     sha256 "5966c5ecea54189c67a3ffb5856176f4bb070ca72b3c3628ad7b76fb67e35de8" => :yosemite
-    sha256 "68d2c33f2cff05d4ba5ab476adbce4398db16e6ae9815cea33e13d6e685f0ca0" => :x86_64_linux # glibc 2.19
   end
 
   option "without-python", "Do not build the Python extension"
   option "with-ruby", "Build the ruby extension"
 
   depends_on "swig" => :build if build.with?("python") || build.with?("ruby")
-  depends_on :python unless OS.mac?
+  depends_on "python" unless OS.mac?
 
   def install
     # https://trac.osgeo.org/geos/ticket/771
