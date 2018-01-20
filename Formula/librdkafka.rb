3a40d064810b13d57e3a516e713cd1f88a874eeb
Michka Popoff <michkapopoff@gmail.com>
2018-01-20 13:27:08 +0100

    librdkafka: Depend on Python formula for Linuxbrew
    Closes Linuxbrew/homebrew-core#5675.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/librdkafka.rb
+++ Formula/librdkafka.rb
@@ -4,20 +4,20 @@ class Librdkafka < Formula
   url "https://github.com/edenhill/librdkafka/archive/v0.11.3.tar.gz"
   sha256 "2b96d7ed71470b0d0027bd9f0b6eb8fb68ed979f8092611c148771eb01abb72c"
   head "https://github.com/edenhill/librdkafka.git"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any
     sha256 "dce656349af51404992cc1d56911f9339adaf45074675bcc1d8e48b8230006c2" => :high_sierra
     sha256 "772484a79bccead9e8964c572a0cb6a35432fdd3979f3f2f876f608db3d1d96f" => :sierra
     sha256 "444a966aeed17701a3ee4d7d1087aef507695600144bcf7e0ff747c0f9d662b8" => :el_capitan
-    sha256 "0a9088b5899dcb3d2a48c152b66ea508a964df6c3946687dc67044dc78530cb9" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
   depends_on "lzlib"
   depends_on "openssl"
   depends_on "lz4" => :recommended
-  depends_on :python unless OS.mac?
+  depends_on "python" unless OS.mac?
 
   def install
     system "./configure", "--disable-dependency-tracking",
