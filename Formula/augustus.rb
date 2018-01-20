c2fc4ebe65c583328e72619e8cb5835000f884bf
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-20 00:06:35 +0000

    augustus: update 3.3 bottle for Linuxbrew.

--- Formula/augustus.rb
+++ Formula/augustus.rb
@@ -9,6 +9,7 @@ class Augustus < Formula
     sha256 "628ec61d36ac8c0cfa088bf0aaf8facb59888d8df407027d6099385a97da9a5a" => :high_sierra
     sha256 "8e6f909d5feb3a6890935646d31b70d99f7faf9315c0613c4362af3d19912c1f" => :sierra
     sha256 "57d0cdab04164c968240245b95159b21e464d7988dd4063d758dbfbb93e1b25a" => :el_capitan
+    sha256 "85b08744bb2f1d343fe280bc8f72a59ee0749333b87ad6b17893c4999cf4fd3c" => :x86_64_linux
   end
 
   depends_on "boost"
