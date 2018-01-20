f65447691b000e6e8f3e48b05287077e9feb7457
Michka Popoff <michkapopoff@gmail.com>
2018-01-20 11:39:19 +0100

    blockhash: Depend on Python formula for Linuxbrew
    Closes Linuxbrew/homebrew-core#5663.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/blockhash.rb
+++ Formula/blockhash.rb
@@ -3,7 +3,7 @@ class Blockhash < Formula
   homepage "https://github.com/commonsmachinery/blockhash"
   url "https://github.com/commonsmachinery/blockhash/archive/v0.3.tar.gz"
   sha256 "a4fbe16dc8d1e9b82de860d97222ce6259495f7832c7dfe3de9e0bb42f85995e"
-  revision 1
+  revision OS.mac? ? 1 : 2
   head "https://github.com/commonsmachinery/blockhash.git"
 
   bottle do
@@ -11,12 +11,11 @@ class Blockhash < Formula
     sha256 "d93acc3ec087d317d7e0fbe166d83745f516d2f85d32c3acc8fa0a18b764853c" => :high_sierra
     sha256 "e4cce87334c21ede18f40f74ccb2f00a9d298e3b4f4e46b4aaedeefe0341e95e" => :sierra
     sha256 "41faab24b6b7e72dff9719972dd7c2fc16864c2b99a69aa5d6250816b27aa2e3" => :el_capitan
-    sha256 "499791aaf183b777691cafa0835ab5ade562012ec7d8bcdd7bd1299091b92660" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
   depends_on "imagemagick"
-  depends_on :python unless OS.mac?
+  depends_on "python" unless OS.mac?
 
   resource "testdata" do
     url "https://raw.githubusercontent.com/commonsmachinery/blockhash/ce08b465b658c4e886d49ec33361cee767f86db6/testdata/clipper_ship.jpg"
