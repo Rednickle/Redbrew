af2500f2c3adc61ebe51c26cbd89fa3083e9a640
Michka Popoff <michkapopoff@gmail.com>
2018-01-20 13:06:40 +0100

    ranger: Depend on Python formula for Linuxbrew
    Closes Linuxbrew/homebrew-core#5670.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ranger.rb
+++ Formula/ranger.rb
@@ -3,7 +3,7 @@ class Ranger < Formula
   homepage "http://ranger.nongnu.org/"
   url "http://ranger.nongnu.org/ranger-1.8.1.tar.gz"
   sha256 "1433f9f9958b104c97d4b23ab77a2ac37d3f98b826437b941052a55c01c721b4"
-  revision 1 unless OS.mac?
+  revision 2 unless OS.mac?
 
   head "https://github.com/ranger/ranger.git"
 
@@ -13,10 +13,9 @@ class Ranger < Formula
     sha256 "53183d431e6bcf9cf49a462130fb5e0929a4bf557a2d109c354e3194bfd36cc0" => :sierra
     sha256 "224dce8bf10cb4f29a182e00d8a684a388f5dc1544f427149ee85e050c07a833" => :el_capitan
     sha256 "224dce8bf10cb4f29a182e00d8a684a388f5dc1544f427149ee85e050c07a833" => :yosemite
-    sha256 "487beb167e7f44cdcf86ce6fb8bcc12f5406bf070f8a662d55a554d03bb9139e" => :x86_64_linux # glibc 2.19
   end
 
-  depends_on :python unless OS.mac?
+  depends_on "python" unless OS.mac?
 
   def install
     man1.install "doc/ranger.1"
