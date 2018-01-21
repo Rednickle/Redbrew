5da3c63fa252edd778ea56b19a84b57c8b9128aa
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-21 21:49:30 +0000

    wrk: update 4.1.0 bottle for Linuxbrew.

--- Formula/wrk.rb
+++ Formula/wrk.rb
@@ -10,6 +10,7 @@ class Wrk < Formula
     sha256 "8167375044c6ff20f33e2d94f92fe2977bb25f19f561f1d5f2eb241b0feda62c" => :high_sierra
     sha256 "1ace345f489c0fc589b619e96a748bf841ff9134a70016b76cde5ae06795369f" => :sierra
     sha256 "23f7c0c7b86691a810238679dee61662781a70b13db95c6dfee4d469845867c1" => :el_capitan
+    sha256 "967ee5aa9cb75f8a498af15c2dbaa8a9d5f868ee8edfc1f1e7fda530e0a4b3c8" => :x86_64_linux
   end
 
   depends_on "openssl"
