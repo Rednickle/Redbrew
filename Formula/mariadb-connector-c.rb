cc49893668d6e1ea17892bec8c6622ed426dc1a9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-24 20:22:52 +0000

    mariadb-connector-c: update 3.0.3 bottle for Linuxbrew.

--- Formula/mariadb-connector-c.rb
+++ Formula/mariadb-connector-c.rb
@@ -8,6 +8,7 @@ class MariadbConnectorC < Formula
     sha256 "43b657d33bd13473ccd6692e6d33ec6abb01a56891b610e75e0e4db5f6501feb" => :high_sierra
     sha256 "a89b9f6276d8c29b0775883686447cb593d9b2e50c7ac7af4e57870db0652998" => :sierra
     sha256 "4c63069a76d6fe43ebd97654884b06dab7b67782b3776cb49c8b11624ca4ba92" => :el_capitan
+    sha256 "3475dc7377dad7ea80f894455f0af7be40b238de24eb96b83f9083d341ba727a" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
