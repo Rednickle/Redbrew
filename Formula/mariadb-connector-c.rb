b4df6070405844a5ebfa2f08d2f5b4a322ac4d86
Michka Popoff <michkapopoff@gmail.com>
2018-01-24 21:31:08 +0100

    mariadb-connector-c: Depend on curl for Linuxbrew

--- Formula/mariadb-connector-c.rb
+++ Formula/mariadb-connector-c.rb
@@ -3,16 +3,17 @@ class MariadbConnectorC < Formula
   homepage "https://downloads.mariadb.org/connector-c/"
   url "https://downloads.mariadb.org/f/connector-c-3.0.3/mariadb-connector-c-3.0.3-src.tar.gz"
   sha256 "210f0ee3414b235d3db8e98e9e5a0a98381ecf771e67ca4a688036368984eeea"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "43b657d33bd13473ccd6692e6d33ec6abb01a56891b610e75e0e4db5f6501feb" => :high_sierra
     sha256 "a89b9f6276d8c29b0775883686447cb593d9b2e50c7ac7af4e57870db0652998" => :sierra
     sha256 "4c63069a76d6fe43ebd97654884b06dab7b67782b3776cb49c8b11624ca4ba92" => :el_capitan
-    sha256 "3475dc7377dad7ea80f894455f0af7be40b238de24eb96b83f9083d341ba727a" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
   depends_on "openssl"
+  depends_on "curl" unless OS.mac?
 
   conflicts_with "mysql", "mariadb", "percona-server",
                  :because => "both install plugins"
