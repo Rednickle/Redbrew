883d83f8dbd013cb3538e26c35f45f8245566d14
Michka Popoff <michkapopoff@gmail.com>
2018-01-27 21:10:00 +0100

    kompose: update 1.8.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5766.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/kompose.rb
+++ Formula/kompose.rb
@@ -9,6 +9,7 @@ class Kompose < Formula
     sha256 "d71defa99aeb19ca7f514cfdc5ecc98b5cfb5ba17dde4808431985c89e85b070" => :high_sierra
     sha256 "96bbc7554c2109d3d4b92847bde14dccf13b334f53ff1b6376201f1660740c4a" => :sierra
     sha256 "d85ef67b16ca84f17f916d42df0526028920bd2ca05eb5fbe5dda12d1c91b157" => :el_capitan
+    sha256 "7db0a3d9dae56caa2e1b17037410ac4fee3ca3c6ecf8b19865aadd21b997dfdf" => :x86_64_linux
   end
 
   depends_on "go" => :build
