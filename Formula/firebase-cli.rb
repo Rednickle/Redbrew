d5f6bbdfbe7c86ca7d3a75c860b0fa5ce7290d13
Michka Popoff <michkapopoff@gmail.com>
2018-01-27 22:55:30 +0100

    firebase-cli: update 3.17.4 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5783.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -11,6 +11,7 @@ class FirebaseCli < Formula
     sha256 "019552e6d9925966f6c3e654d72b2a710a4aacc1c939c7216b782f050da08fc8" => :high_sierra
     sha256 "b99b0b9b489c1b72ba3bc29ead0ce9aeed62029624e3d11a02244f7d14beb277" => :sierra
     sha256 "7baf5350cdde83a1dde85223795d81afd4e9748a62d6df3ba8fb4afd04a786d2" => :el_capitan
+    sha256 "c15889f511ddc80d4e99ac0ca72b5cfb0d65f0510d368dec4856068d045ae2c2" => :x86_64_linux
   end
 
   depends_on "node"
