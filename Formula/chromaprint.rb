a46c0b9ad94b548a2a9ddea2817b5602da37ed44
Michka Popoff <michkapopoff@gmail.com>
2018-01-27 15:16:32 +0100

    chromaprint: update 1.4.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5735.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/chromaprint.rb
+++ Formula/chromaprint.rb
@@ -9,6 +9,7 @@ class Chromaprint < Formula
     sha256 "3d0ae4279d4d3fa0fad24c434e82032384256ed1613e89e7b601e1f02ceaa596" => :high_sierra
     sha256 "fc91e7116562484496de75f3e3e0a7a61771b9826b6f3a4497af411df2683290" => :sierra
     sha256 "799c9c907b020aed48763162b996a672d42b3b8dde5a53381e28818ccd0981dc" => :el_capitan
+    sha256 "94cb5a7fe84dbb14e84832689151480c8067aefb12a9015d947fd6f47a23f240" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
