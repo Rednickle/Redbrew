fa19c079f51211f1699ca6eac212eb9c30f4dc76
Michka Popoff <michkapopoff@gmail.com>
2018-01-28 19:51:16 +0100

    zile: update 2.4.14_2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5807.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/zile.rb
+++ Formula/zile.rb
@@ -10,6 +10,7 @@ class Zile < Formula
     sha256 "f17882759df971a7e3184638a13b889576eba380d61a005e3bbd33d3d32cc84e" => :high_sierra
     sha256 "5be3657e02aa7dca41fd0432bbff60a3d96623a89179280a31b18cbde1a6b836" => :sierra
     sha256 "25330278315a0e2fbc7cd731d69e88ba8205cd4d622ca7a0abbc638f95e6d937" => :el_capitan
+    sha256 "7be8a01bcbb729217d76a3387a797ae63d85f40540a1e18f5d75ed9b318e5c6a" => :x86_64_linux
   end
 
   # https://github.com/mistydemeo/tigerbrew/issues/215
