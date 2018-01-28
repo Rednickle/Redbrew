badb5b8a680b4fc1d4132a9f09b4693064b6c42f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-28 18:44:04 +0000

    lbdb: update 0.46 bottle for Linuxbrew.

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -9,6 +9,7 @@ class Lbdb < Formula
     sha256 "c6afe8aedfafc7b8ac1bc1e766d3d8ecabc3c6cff576b6f217b2859bb1256e35" => :high_sierra
     sha256 "ec24bdf809b1226a1c5ffe6b805b58f70551fbde5529cd68321be71c43c17a7e" => :sierra
     sha256 "a78fabea88eaa0265505a6034f70988338e433bd0f6f70f8a6e4c4d3d317b185" => :el_capitan
+    sha256 "04095635904fefa910b0ce423f5ce96babad0698096d88f65b0845dc6fb9ca92" => :x86_64_linux
   end
 
   depends_on "abook" => :recommended
