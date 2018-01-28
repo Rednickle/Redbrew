7e7d241cfea1cbcf2a6eded9b1b7d6b97cb270f8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-01-28 18:36:41 +0000

    samtools: update 1.7 bottle for Linuxbrew.

--- Formula/samtools.rb
+++ Formula/samtools.rb
@@ -9,6 +9,7 @@ class Samtools < Formula
     sha256 "368dbaf769be26a7c383ee6ab33b9368d70d15b144701489fa33a3475ca5301e" => :high_sierra
     sha256 "9be1d5c1a480482885a880c3004906d51b7cb1a89dac25c239513a8b04acfbba" => :sierra
     sha256 "931f8245341783d7d96789c8cc5b3eea6c8071d1ce82644486884cece294e54e" => :el_capitan
+    sha256 "eb1340430ede2a625b2410d1c4683d321f403c9efa997af332fe9656b4611a92" => :x86_64_linux
   end
 
   depends_on "htslib"
