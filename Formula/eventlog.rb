5369c5712ab00befce693386aa89dc5286c79aaf
Michka Popoff <michkapopoff@gmail.com>
2018-01-28 19:02:38 +0100

    eventlog: update 0.2.13 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5813.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/eventlog.rb
+++ Formula/eventlog.rb
@@ -11,6 +11,7 @@ class Eventlog < Formula
     sha256 "9073fb11ae9c20375295c36b5bb6845639ea1f9c17a677c1d93ff206075ff871" => :el_capitan
     sha256 "2bdc1f762ea05e79f486e7e78b8a173ea99a5a76b4bedd28a03a1c8912f39925" => :yosemite
     sha256 "9d747019f60dfa8fc13472815c18c20c46c2cb2cd53dd754a99e8029afb85cbf" => :mavericks
+    sha256 "dafbd7123de97b0a29ca89c6e965a2123664ec34851c815b8bd1a616397dc525" => :x86_64_linux
   end
 
   def install
