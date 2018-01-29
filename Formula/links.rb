9342c93f030ad47a7019a0da3f97012f08c2ca9e
Michka Popoff <michkapopoff@gmail.com>
2018-01-29 21:32:27 +0100

    links: Use xorg dep instead of X11 for Linuxbrew
    Closes Linuxbrew/homebrew-core#5851.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/links.rb
+++ Formula/links.rb
@@ -3,14 +3,13 @@ class Links < Formula
   homepage "http://links.twibright.com/"
   url "http://links.twibright.com/download/links-2.14.tar.bz2"
   sha256 "f70d0678ef1c5550953bdc27b12e72d5de86e53b05dd59b0fc7f07c507f244b8"
-  revision 1
+  revision OS.mac? ? 1 : 2
 
   bottle do
     cellar :any
     sha256 "c5548c20d4e677218f870fb19c2275f51b3a737da1fd78532b88cf90af4e4dd5" => :high_sierra
     sha256 "5a8045be375cb674122da0342e04f47ff14c3360e1f7eebe9f827284aba318ed" => :sierra
     sha256 "3a74e6b5c260ee7ac380cf0e15e0c718fb6c06c113ac2311a0dae9b1be755fe3" => :el_capitan
-    sha256 "a16de4ba6c7a6d6e8195827db7e01502cca51826629a89810df72dbe6d966479" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
@@ -19,6 +18,7 @@ class Links < Formula
   depends_on "jpeg" => :optional
   depends_on "librsvg" => :optional
   depends_on :x11 => :optional
+  depends_on "linuxbrew/xorg/xorg" unless OS.mac?
 
   def install
     args = %W[
