ef98fb9db050c083699d3ac787d8c43298a3324a
Michka Popoff <michkapopoff@gmail.com>
2018-01-29 21:14:28 +0100

    jed: Use xorg dep instead of X11 for Linuxbrew
    Closes Linuxbrew/homebrew-core#5848.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/jed.rb
+++ Formula/jed.rb
@@ -4,6 +4,7 @@ class Jed < Formula
   url "https://www.jedsoft.org/releases/jed/jed-0.99-19.tar.gz"
   mirror "https://mirrors.kernel.org/debian/pool/main/j/jed/jed_0.99.19.orig.tar.gz"
   sha256 "5eed5fede7a95f18b33b7b32cb71be9d509c6babc1483dd5c58b1a169f2bdf52"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "c1e26f1498c5b47b04cde3bfaa7788acb72a52b54c50eb277368990ad2f27330" => :high_sierra
@@ -12,7 +13,6 @@ class Jed < Formula
     sha256 "3b316c792feabf9622a70a8ccdf2d2e985e7f991dbcd49a104b2ee6b8ea078cb" => :yosemite
     sha256 "f0e02951e534d96baad147970e51b2f5c09155ad0e51114cfc72b3f49301dff3" => :mavericks
     sha256 "595aff2e43b8ec8c31626831dcab83ffe4ffc129ea85efa61bc2e35877137a29" => :mountain_lion
-    sha256 "ece85f6ba321074d7f8e2ad4f229653ae9b81911eb8c83ffe170bae696cc983b" => :x86_64_linux # glibc 2.19
   end
 
   head do
@@ -23,6 +23,7 @@ class Jed < Formula
 
   depends_on "s-lang"
   depends_on :x11 => :optional
+  depends_on "linuxbrew/xorg/xorg" unless OS.mac?
 
   def install
     if build.head?
