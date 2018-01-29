f34d44bd6806cd5cfb9f4800a5349aec8f10d698
Michka Popoff <michkapopoff@gmail.com>
2018-01-29 21:25:38 +0100

    libxkbcommon: Use xorg dep instead of X11 for Linuxbrew
    Closes Linuxbrew/homebrew-core#5850.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libxkbcommon.rb
+++ Formula/libxkbcommon.rb
@@ -3,12 +3,12 @@ class Libxkbcommon < Formula
   homepage "https://xkbcommon.org/"
   url "https://xkbcommon.org/download/libxkbcommon-0.8.0.tar.xz"
   sha256 "e829265db04e0aebfb0591b6dc3377b64599558167846c3f5ee5c5e53641fe6d"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "7428e9599baa3dfca4a9c181c4d3a2ab934f37987aaad270c8a6fc3921da2c41" => :high_sierra
     sha256 "62e85d6d91d4f603d0ab2796904f07a754a782f6a0f23f424810a08b5deff347" => :sierra
     sha256 "32ee1c478aa17d7120d86370fd619de9b9ac39671d45d77a7a31ac550b0453d4" => :el_capitan
-    sha256 "939a4428ea1ada693d2499847310a3b3cd185487bc87d5065413890d5dc7352f" => :x86_64_linux
   end
 
   head do
@@ -19,12 +19,13 @@ class Libxkbcommon < Formula
     depends_on "libtool" => :build
   end
 
-  depends_on :x11
+  depends_on :x11 if OS.mac?
   depends_on "bison" => :build
   depends_on "pkg-config" => :build
 
   unless OS.mac?
     depends_on "linuxbrew/xorg/xkeyboardconfig"
+    depends_on "linuxbrew/xorg/xorg"
     depends_on "linuxbrew/xorg/libxcb"
   end
 
