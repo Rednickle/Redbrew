0b4ea4988d553d31b112dfc0de4f95b8b421e990
Michka Popoff <michkapopoff@gmail.com>
2018-01-30 20:54:18 +0100

    pari: Use xorg dep instead of X11 for Linuxbrew
    Closes Linuxbrew/homebrew-core#5859.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pari.rb
+++ Formula/pari.rb
@@ -4,17 +4,18 @@ class Pari < Formula
   url "https://pari.math.u-bordeaux.fr/pub/pari/unix/pari-2.9.4.tar.gz"
   mirror "https://mirrors.kernel.org/debian/pool/main/p/pari/pari_2.9.4.orig.tar.gz"
   sha256 "4f9cdc739630a039c0fcc9afac2e5647659c4a709dcc6f7a69d5908cd4a82647"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "e395e67785175c625c794e0535c4224bd15cbc15f661cbcf6d8bc96f15e58aba" => :high_sierra
     sha256 "e23557107118210578fcd65b8b48ae2f2ceb817ba4fb40b7afab2f3f4658b4e3" => :sierra
     sha256 "6d5cd278c72dfedac177af118a3762028a87d4748b5044d30d73d9dda55c5a3e" => :el_capitan
-    sha256 "920c4cd0bbc080760a06a3c88fe8052f7958445ea68b28a21d783d8a0e04289f" => :x86_64_linux
   end
 
   depends_on "gmp"
   depends_on "readline"
-  depends_on :x11
+  depends_on :x11 if OS.mac?
+  depends_on "linuxbrew/xorg/xorg" unless OS.mac?
 
   def install
     readline = Formula["readline"].opt_prefix
