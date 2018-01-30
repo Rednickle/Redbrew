1d3c94dc1760f27e3118088246aa55786779a19a
Michka Popoff <michkapopoff@gmail.com>
2018-01-30 20:50:43 +0100

    ode: Use xorg dep instead of X11 for Linuxbrew
    Closes Linuxbrew/homebrew-core#5858.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ode.rb
+++ Formula/ode.rb
@@ -4,13 +4,13 @@ class Ode < Formula
   url "https://bitbucket.org/odedevs/ode/downloads/ode-0.15.2.tar.gz"
   sha256 "2eaebb9f8b7642815e46227956ca223806f666acd11e31708bd030028cf72bac"
   head "https://bitbucket.org/odedevs/ode/", :using => :hg
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
     sha256 "3425f2c71a9b413185437a0976d3380a2f00d573b73f4afdeee28b3124158c47" => :high_sierra
     sha256 "2ff984f39f6825aed23e41289ebbf7302b414ff2f29df0dcc0bb0c900be1633c" => :sierra
     sha256 "819a4d6c09a2fc3cda1866ac1915a8bc7166d4ecb03dc6c18dbed103ccdc70e9" => :el_capitan
-    sha256 "1975f27cde4f1dc954cb60c82c6789f94fbe6a758626d67b83cd923756119a5c" => :x86_64_linux
   end
 
   option "with-double-precision", "Compile ODE with double precision"
@@ -27,6 +27,7 @@ class Ode < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
   depends_on :x11 => :optional
+  depends_on "linuxbrew/xorg/xorg" unless OS.mac?
 
   def install
     args = ["--prefix=#{prefix}"]
