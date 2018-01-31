5d0ff3c7b67a457dd52aec69e35af76c2e459765
Michka Popoff <michkapopoff@gmail.com>
2018-01-31 22:13:36 +0100

    log4cplus: update 1.2.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5887.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/log4cplus.rb
+++ Formula/log4cplus.rb
@@ -9,6 +9,7 @@ class Log4cplus < Formula
     sha256 "b744574eb3dbc662c7143d2369feaef039c5a3959fb5ea8cdea5410efab7c3a5" => :high_sierra
     sha256 "f5f8ec470c5a4e1b0043ff44f85b8deda8ad27189f5766f330e60e162f7054c8" => :sierra
     sha256 "8819df40a1477a3fb87a4a3dcd36ec8e2292b5aed362851549e8e2df74ffad9d" => :el_capitan
+    sha256 "6a68ff51ee0b941fea461c106b01fa8e32baccd9ff78ebb67b64cbd541f1753d" => :x86_64_linux
   end
 
   def install
