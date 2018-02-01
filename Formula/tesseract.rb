438fda6f178f55b94446cf3d37942c683e04a7a7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-01 12:12:01 +0000

    tesseract: update 3.05.01_1 bottle for Linuxbrew.

--- Formula/tesseract.rb
+++ Formula/tesseract.rb
@@ -10,6 +10,7 @@ class Tesseract < Formula
     sha256 "6b3a0e12659b57344a67bfde229353593ba06b4fe26b7eb1eb9e585f8906a032" => :sierra
     sha256 "788a83d200ba3aa34140fa0d2dbc2ecbf6cfb2a3eeb412417cbbf68dfc4678b4" => :el_capitan
     sha256 "8798858e2d3846eb7b10335cb08c349fd18bb040a2e8b5e9bb3737d1bf2071d5" => :yosemite
+    sha256 "dc38e38694fead02a9bf88f5af3d415a52ad3900835c0af3ea719df551c7f593" => :x86_64_linux
   end
 
   head do
