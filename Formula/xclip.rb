96241365f2bab1328a6e44772210b09c50a86a1d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-01 21:51:37 +0000

    xclip: update 0.13_1 bottle for Linuxbrew.

--- Formula/xclip.rb
+++ Formula/xclip.rb
@@ -10,6 +10,7 @@ class Xclip < Formula
     sha256 "0963015158b7d4ae2981503edc18427737a0586b7155da5cd2ddaa93fb3b92bd" => :high_sierra
     sha256 "bb26c2bb6d7ce8f15ab50144f38d11ddde113bb400326ccea990ca9a5d0a9c69" => :sierra
     sha256 "9e17790e9a94ae1e29317f013a65f2d639ae9063db48ed7fa0aed7449f221abb" => :el_capitan
+    sha256 "9de13067a4d4ab379e82ab78d97c559695551fd51ee99c23879e2503de6c4d7e" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
