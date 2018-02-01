7c6246c6d1dcdb2e75a4b14feb9f1e0f55469af7
Michka Popoff <michkapopoff@gmail.com>
2018-02-01 07:36:14 +0100

    xrootd: update 4.8.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5893.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/xrootd.rb
+++ Formula/xrootd.rb
@@ -10,6 +10,7 @@ class Xrootd < Formula
     sha256 "17fd0df59c94f95e6c849c62155527a1cc61b14c1324c35e424175bd7847c49d" => :high_sierra
     sha256 "a9cf8e475ec61737b216d287354d9f953f66e6ed2e0ef2af71476bb37374b306" => :sierra
     sha256 "76ce93c37b9d8543816c985b32c8c9a9d519815891e42f909d040a35c70f9fb4" => :el_capitan
+    sha256 "784d0d03fe7f848dff06d7c3f6c4a932bb6b79c3bddecfc181b8b9c38105dc6c" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
