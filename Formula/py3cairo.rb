0326734c19caa3d76617cb9716746da530157cc7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-04 07:18:24 +0000

    py3cairo: update 1.15.6 bottle for Linuxbrew.

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -9,6 +9,7 @@ class Py3cairo < Formula
     sha256 "233eabdb45cf583fc810f829ab548bbb56348a59e881e79842383a349bb6de20" => :high_sierra
     sha256 "a7b13e9fc1052c70d7ebf619da0a84d6c0530a7e36e7be8805535231bd85ed66" => :sierra
     sha256 "7757b4c703d273d6e63250790433f3c1477f8b0419ee275ca9322dbafd7b0ee3" => :el_capitan
+    sha256 "e7b60ded8865b85d26ac01bb7182deb0469d280652b032477bf8f19e84d73f6f" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
