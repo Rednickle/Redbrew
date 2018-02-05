e37ec142aa5363d032c667c76b1d9ae68f4ac7fe
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-05 19:11:04 +0000

    exempi: update 2.4.4 bottle for Linuxbrew.

--- Formula/exempi.rb
+++ Formula/exempi.rb
@@ -9,6 +9,7 @@ class Exempi < Formula
     sha256 "38ea47dd4aa6ba60039ffc10c57b71dfeebd11f4ea4504c536f513e047f2caf6" => :high_sierra
     sha256 "9d9150d0bc98637f83d3f41805094f34c937659f10dfef110ce9bdeb379f17dc" => :sierra
     sha256 "4e9be935ecef717a08b180f753d4321388cb1979113d6a08024df0a54a29581e" => :el_capitan
+    sha256 "dd588549d44217035699287f7625ae0c1642ce6f64dccb3d417cce90207c4fa8" => :x86_64_linux
   end
 
   depends_on "boost"
