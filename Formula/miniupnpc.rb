1ede3c9133e5db9eaf78a8e8ce0a1ded4c9b6095
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-05 19:16:54 +0000

    miniupnpc: update 2.0.20180203 bottle for Linuxbrew.

--- Formula/miniupnpc.rb
+++ Formula/miniupnpc.rb
@@ -9,6 +9,7 @@ class Miniupnpc < Formula
     sha256 "9257bd89a0790bb878ee24980ca273a16cbfded8aa2526b8529ea244dcce6ac9" => :high_sierra
     sha256 "ebc5201204aa7e6d8eadeb453c33998e62889263f2b6d9401855b0d6667b4c21" => :sierra
     sha256 "92482050397214509f9a14c2d8815d8d9b84eb0715f077535bdcb3dda1a4cab3" => :el_capitan
+    sha256 "6066bb59e4ea4a77b52863685323a9fea7c0f897bdc2d8d4d2694d35496ca166" => :x86_64_linux
   end
 
   def install
