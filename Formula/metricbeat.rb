083874a8412e990efd8c90fa1c30c376d6a5ae03
Michka Popoff <michkapopoff@gmail.com>
2018-02-08 17:26:59 +0100

    metricbeat: update 6.2.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5989.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/metricbeat.rb
+++ Formula/metricbeat.rb
@@ -11,6 +11,7 @@ class Metricbeat < Formula
     sha256 "45759c5a7981523ac15dd78f2a63b68ac1431fb2c2084463a55a23a1269c59db" => :high_sierra
     sha256 "7d9ad800f09d924d61472df8bfc3b9b2e3296c951a5dff43f013fc98f0c19af9" => :sierra
     sha256 "210c3c3fc18292566904fe2af1839270cbf3e72db419eb195c3ae338fbdfefbf" => :el_capitan
+    sha256 "c258b5b9f4b6925bc0840dccbc18518dc4adf5a9d5c6652e1fadf85a78fa55f3" => :x86_64_linux
   end
 
   depends_on "go" => :build
