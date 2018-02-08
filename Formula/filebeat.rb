ceb46e9cbe409e3c97dd4533225a222bde673129
Michka Popoff <michkapopoff@gmail.com>
2018-02-08 07:51:59 +0100

    filebeat: update 6.2.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#5987.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/filebeat.rb
+++ Formula/filebeat.rb
@@ -10,6 +10,7 @@ class Filebeat < Formula
     sha256 "fdde06d0c2f53d3feef1f8534c35cd278778a1d4f4f6e9ae3fde08565655a209" => :high_sierra
     sha256 "9f48e27763ad36256f4fc08163ff83320423479c2e9b869117d1dae46c378aa8" => :sierra
     sha256 "c9fdfdc4f4a9cfa2720f469816f5d7eea62722cd65eedba388d803c918c86117" => :el_capitan
+    sha256 "ad932e7049c623111d0d4bfefe12d4df6ae89ac892e0acf8f12a38c9c18f39ec" => :x86_64_linux
   end
 
   depends_on "go" => :build
