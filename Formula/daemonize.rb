58d07d176218011032391c9bb94646e7c77f09a6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-11 12:22:25 +0000

    daemonize: update 1.7.8 bottle for Linuxbrew.

--- Formula/daemonize.rb
+++ Formula/daemonize.rb
@@ -9,6 +9,7 @@ class Daemonize < Formula
     sha256 "bc501e9e4ba9fd11390fa9749a7b9a38a70353edaf75499bd969c45921d06bfe" => :high_sierra
     sha256 "d4d5109292158ef32eb73a37b9b6a037dcae620e234be945410ea927322bb998" => :sierra
     sha256 "5e05991cf0462e4fe32dd70354d2520a378831d2b1c0fc2cf0b4fbca8dc85489" => :el_capitan
+    sha256 "81cb7cd8608e51f75e9aa312249e8cca0112ef7144c45e86a6559638c3d7d242" => :x86_64_linux
   end
 
   def install
