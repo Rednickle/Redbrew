be65dde70106954c5449a1eac851446057fe51e0
Michka Popoff <michkapopoff@gmail.com>
2018-02-11 15:15:54 +0100

    sword: update 1.8.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6065.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/sword.rb
+++ Formula/sword.rb
@@ -8,6 +8,7 @@ class Sword < Formula
     sha256 "3279c77477c21d1636f573202df976bd37fbaca39ed7e3b310158dad4e961641" => :high_sierra
     sha256 "794afe687eb7933fd3aeaee7e480224295614fc0138b8d89b7d9b81be55239a1" => :sierra
     sha256 "032c83b3302b78c198d1e346258a1d09c542a1361e1b0f000f82306d8c82acb4" => :el_capitan
+    sha256 "daa1f0360c2a9b72ecdba5ca9574bfe9688f4c852d1313456502a2538b93b1e7" => :x86_64_linux
   end
 
   option "with-clucene", "Use clucene for text searching capabilities"
