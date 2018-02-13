c102aa157fd49f841ed2afaee46472dee5b5eeed
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-13 20:22:01 +0000

    advancecomp: update 2.1 bottle for Linuxbrew.

--- Formula/advancecomp.rb
+++ Formula/advancecomp.rb
@@ -9,6 +9,7 @@ class Advancecomp < Formula
     sha256 "4e02eb49cfa3587545852bb5412ec4bed7a5a23bf7348798aa508fb1fe3c3840" => :high_sierra
     sha256 "0142bd73b77ee344b76be047323dea7593a03ee08b7041f789d8e1a330c5f7ec" => :sierra
     sha256 "adf23429f3c808c367520a4225caee3b2c7a2a8c4dea5bc8b28c8654f7e5b989" => :el_capitan
+    sha256 "21559c7edb3d8b62371eebc72e2b8291cfc997ed0adc05cc6f2ffae9862db08a" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
