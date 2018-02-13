0402aecfac3fc8d0196bf9c3ea416d5433371105
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-13 22:30:37 +0000

    sslh: update 1.19c bottle for Linuxbrew.

--- Formula/sslh.rb
+++ Formula/sslh.rb
@@ -10,6 +10,7 @@ class Sslh < Formula
     sha256 "4f3429c456314ead9330497258f58dbc07c620a497101d451b37538f773e7138" => :high_sierra
     sha256 "4c02e4c94f732f382abb18e3831f629a0dd108211aca169ae7b93c3e387a6b70" => :sierra
     sha256 "a457cede9fdc5903b4d4cf390ccf34caf716c1cf8ddfef315197876c250438c0" => :el_capitan
+    sha256 "fa720ca3303cf1b6b89a83313ca759c11184ea924698a9a9833a01c64c2ec543" => :x86_64_linux
   end
 
   depends_on "libconfig"
