1a653330c365a5e92b3c7cb6164ebcd635fe2277
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-15 12:28:56 +0000

    fruit: update 3.1.0 bottle for Linuxbrew.

--- Formula/fruit.rb
+++ Formula/fruit.rb
@@ -5,10 +5,11 @@ class Fruit < Formula
   sha256 "d3307e02b8c85421290fc0c748ef2f964a9313596f963ae14176f35674d7230c"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     sha256 "74f2573185454e82dc1f55eb2cd298a72d94df3b971c84d78a14778f6ccd70f8" => :high_sierra
     sha256 "30c60fc08a0129e49649deec027506f915d0b9484ef868dc53e6b9fc716dd02a" => :sierra
     sha256 "1056dd805dc9fa3ea04833f66cf6e7aedc83c277493603bf62c2a60f9a5ed872" => :el_capitan
+    sha256 "7ceabf2fc5a94c072f0d116914a2c797d14ea6dd38f63e41b775a9e3564e2b9a" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
