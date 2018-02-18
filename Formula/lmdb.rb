631c3701ef4ab72636eb5965269a305f1fc41a57
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-18 06:31:13 +0000

    lmdb: update 0.9.21_1 bottle for Linuxbrew.

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -13,6 +13,7 @@ class Lmdb < Formula
     sha256 "f7d8acade2be32886edf1d039e57070e35db0bdeca6f37ef7f42530ad404cc91" => :sierra
     sha256 "b4dd9a1387599e22744bbb769ce71fd1588e5ea6f1ef1d83a2d32f71190cb6ff" => :el_capitan
     sha256 "66e48198e7bf2a466a614727a71b10e3b865cf1f65451fe10c7d5d4068e46a5c" => :yosemite
+    sha256 "b5b5bcbaa6df4b52fc2b36772ec42b4e076192f5b922a272830af6c3d609ac26" => :x86_64_linux
   end
 
   def install
