cf8f77b54c178e5d00e06455e63bdb6bc4a17031
Shaun Jackman <sjackman@gmail.com>
2018-02-17 22:24:30 -0800

    lmdb: Fix the extension of the shared library [Linux]
    Closes Linuxbrew/homebrew-core#6168.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/lmdb.rb
+++ Formula/lmdb.rb
@@ -4,6 +4,7 @@ class Lmdb < Formula
   url "https://github.com/LMDB/lmdb/archive/LMDB_0.9.21.tar.gz"
   sha256 "1187b635a4cc415bb6972bba346121f81edd996e99b8f0816151d4090f90b559"
   version_scheme 1
+  revision 1 if OS.linux?
   head "https://github.com/LMDB/lmdb.git", :branch => "mdb.master"
 
   bottle do
@@ -12,14 +13,14 @@ class Lmdb < Formula
     sha256 "f7d8acade2be32886edf1d039e57070e35db0bdeca6f37ef7f42530ad404cc91" => :sierra
     sha256 "b4dd9a1387599e22744bbb769ce71fd1588e5ea6f1ef1d83a2d32f71190cb6ff" => :el_capitan
     sha256 "66e48198e7bf2a466a614727a71b10e3b865cf1f65451fe10c7d5d4068e46a5c" => :yosemite
-    sha256 "b1c913ea4600c8613432d1b34065172486647fd8bc44ab4faebca9a5d4a8110a" => :x86_64_linux # glibc 2.19
   end
 
   def install
     cd "libraries/liblmdb" do
-      system "make", "SOEXT=.dylib"
-      system "make", "test", "SOEXT=.dylib"
-      system "make", "install", "SOEXT=.dylib", "prefix=#{prefix}"
+      args = *("SOEXT=.dylib" if OS.mac?)
+      system "make", *args
+      system "make", "test", *args
+      system "make", "install", "prefix=#{prefix}", *args
     end
   end
 
