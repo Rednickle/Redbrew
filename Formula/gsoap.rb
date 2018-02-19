bb6ea5fa4079170c29ab99e48269748ca663cd48
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-19 21:23:02 +0000

    gsoap: update 2.8.63 bottle for Linuxbrew.

--- Formula/gsoap.rb
+++ Formula/gsoap.rb
@@ -8,6 +8,7 @@ class Gsoap < Formula
     sha256 "f679d033483903a9be5ed20f39f514439fe8652f9864a08fd361ef7f9a49a7ee" => :high_sierra
     sha256 "5b1ad4ede9de7834fcd144a2212068d67851c7f41eaa796ba40ddcc254673aa8" => :sierra
     sha256 "e0f5e6acf7f21b5505cff2e230e3e9e196f3433d595b2072b4f46a494de2f212" => :el_capitan
+    sha256 "93404f6c9edfcd502db40fc6183ebd2c12eb5cf7a1e5b5fb54ff398bc038ff8f" => :x86_64_linux
   end
 
   depends_on "openssl"
