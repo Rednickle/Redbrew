0f5df0911e319ea03775021a58a91a62a971eca6
Michka Popoff <michkapopoff@gmail.com>
2018-02-22 08:00:25 +0100

    logstalgia: update 1.1.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6087.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/logstalgia.rb
+++ Formula/logstalgia.rb
@@ -8,6 +8,7 @@ class Logstalgia < Formula
     sha256 "e2be7c6d44bb1abb4ff78afa540a0c874b3a0875c4135abb12a4537ffeb0ae16" => :high_sierra
     sha256 "2d6564479e750420eeb411aa0a616ed9cf124744df82f7db2cb131cafa5ff9cb" => :sierra
     sha256 "d50b766fb0b9ef5eafea93bf1e0640ce5de6f9456e3576ed46be7dd4e1bd8f2f" => :el_capitan
+    sha256 "574129eff0b902e2c3b5441381bfdda4c8e4e345e5343f198818492f098a19a8" => :x86_64_linux
   end
 
   head do
