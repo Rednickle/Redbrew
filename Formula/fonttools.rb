f4bdfd6a31abca28d3c3789ff4dcb32fb67e1079
Michka Popoff <michkapopoff@gmail.com>
2018-02-27 22:23:13 +0100

    fonttools: update 3.23.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6315.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -12,6 +12,7 @@ class Fonttools < Formula
     sha256 "e0436c59dc2f262ac2157e49ffb96b67947ddc0c8eaaebb959e902999cd5bf08" => :high_sierra
     sha256 "7e94717fb5591da52656ebe6f5ac8c715de35c69e7bc8c97d6c53de2a82bb9bc" => :sierra
     sha256 "0a4fa59a945005c6d129c1c83c0b1f503e6dd8b116ad03bb059e94c361c036f4" => :el_capitan
+    sha256 "6c942134fadad3ce0d5650536acf8ec9635421a7aaf0bf400f819e8cd2851199" => :x86_64_linux
   end
 
   option "with-pygtk", "Build with pygtk support for pyftinspect"
