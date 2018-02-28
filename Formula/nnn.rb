25772e0463cb9773b759850596cc73be3574ed46
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-28 21:37:52 +0000

    nnn: update 1.7 bottle for Linuxbrew.

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -9,6 +9,7 @@ class Nnn < Formula
     sha256 "ff5d1ec8531b1e5994b6b822e94c9c92bfaed8d7918257e08b37c76aa4920d51" => :high_sierra
     sha256 "8be6a30a848f30382065ffdfcf0aaf17f59ce5239bba5b263f19e69ff3ea3a2d" => :sierra
     sha256 "c4884ba21bdcc444dfb2ef3df4ddd8f7f56194c159fec96fcef038092564c794" => :el_capitan
+    sha256 "5b9cf055ab1843d9af54e6d7db4057220ef5d83620defab564b8fbf1ad9f61c4" => :x86_64_linux
   end
 
   depends_on "readline"
