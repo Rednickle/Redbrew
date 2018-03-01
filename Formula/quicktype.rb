1fcfeb5b46cfaa4b753fabc4e38c217e9bff683e
Michka Popoff <michkapopoff@gmail.com>
2018-03-01 20:59:52 +0100

    quicktype: update 8.5.81 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6332.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/quicktype.rb
+++ Formula/quicktype.rb
@@ -11,6 +11,7 @@ class Quicktype < Formula
     sha256 "e3b33ce565c7edd43e4e376f3b5b11bb548da7f6f152c2b7e5d410879a490260" => :high_sierra
     sha256 "6186b69a8ef1a4d144869544581be7cf8220c41f86a12b8e9dd657978bc76e2a" => :sierra
     sha256 "945f9c2996aefd5ad141feea5c6c5eea60559e051e75f9eb52ac933b9d78d8da" => :el_capitan
+    sha256 "48a341bacf0a2de3daa08b5765acc05f0ceb01e09922bc0b33f94082793f7fd3" => :x86_64_linux
   end
 
   depends_on "node"
