f579eeb8c8745c0414ed19c5223b75a1aeb6e4af
Michka Popoff <michkapopoff@gmail.com>
2018-03-05 21:55:48 +0100

    python: Fix formula name after merge

--- Formula/python.rb
+++ Formula/python.rb
@@ -1,4 +1,4 @@
-class Python3 < Formula
+class Python < Formula
   desc "Interpreted, interactive, object-oriented programming language"
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tar.xz"
