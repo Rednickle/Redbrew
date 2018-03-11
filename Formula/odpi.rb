89c137cc664e3a53d4da73e6733636d868544496
Michka Popoff <michkapopoff@gmail.com>
2018-03-11 10:56:42 +0100

    odpi: update 2.2.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6460.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/odpi.rb
+++ Formula/odpi.rb
@@ -9,6 +9,7 @@ class Odpi < Formula
     sha256 "2cf3381b3ac13730425587f7713cec41415b2433de6c1501c5d06d45e5fcba04" => :high_sierra
     sha256 "ddee0c92562aabb96e5d64056f2bc1d3aec62710dbf9095fbd503cb867df7aae" => :sierra
     sha256 "f3b1efc4e54ebf61133cd6977c87c6bf76e9081dfba7b645cb224d44ac7953e1" => :el_capitan
+    sha256 "2cb212e777f9f61fe4467f2b13950e1d7dfeb47fc3391c7bb31aa911ba350cca" => :x86_64_linux
   end
 
   def install
