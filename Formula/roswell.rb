d2680924cea1f78ec8a4178febc82fee9c28bbe2
Michka Popoff <michkapopoff@gmail.com>
2018-03-11 11:51:49 +0100

    roswell: update 18.3.10.89 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6469.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -9,6 +9,7 @@ class Roswell < Formula
     sha256 "c87b74c9daa7220742540d9b68736db5125f0229a4ee8b94b1bf3d8078eace7b" => :high_sierra
     sha256 "ec64d8f24316cb562698df1204364417eadba35cc885effe77d50ea11de9624c" => :sierra
     sha256 "460379c66ee483de0807bb1c5b8beb91ef62ebae56c5aa4a3714bbefaf485604" => :el_capitan
+    sha256 "4c2f4821338d1ebde037f8175cd8e3da59aefde0501ce8c7750f3d52f27c87d8" => :x86_64_linux
   end
 
   depends_on "automake" => :build
