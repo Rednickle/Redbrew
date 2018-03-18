e8c8c7d27e21a8aed2687014304d089a613dcc8b
Michka Popoff <michkapopoff@gmail.com>
2018-03-18 22:56:47 +0100

    dfix: update 0.3.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6666.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dfix.rb
+++ Formula/dfix.rb
@@ -10,6 +10,7 @@ class Dfix < Formula
     sha256 "77c5f989b77e63a53aea939dce24c99c203073eba08c687eb0feb8146ea3d630" => :high_sierra
     sha256 "75570e29a01423b0806c248a1daf6ff6c69681c3c530a34d296604b4663920a9" => :sierra
     sha256 "6dbc07c50bbcd397af748e38fea9bbb63876caba64777ff63ae775764258f70f" => :el_capitan
+    sha256 "0734f0bc23f139107fa676e4ff4b4a47b4e2693edefb33d95bf2c4cce8d7e563" => :x86_64_linux
   end
 
   depends_on "dmd" => :build
