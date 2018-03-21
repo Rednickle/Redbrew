6b5d81e467596f4d76772b1f63d662badb123bd6
Michka Popoff <michkapopoff@gmail.com>
2018-03-21 23:46:27 +0100

    spigot: update 20180321 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6810.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/spigot.rb
+++ Formula/spigot.rb
@@ -11,6 +11,7 @@ class Spigot < Formula
     sha256 "7db0b8cf1c023c33977d2f0b3a1a7bfc7aa8e56e0d6db91f228b6d264a20f12f" => :high_sierra
     sha256 "6e7183d292159ac738e2fa6e678fc07879d9650ed601c7cecc21e8c2e89c8ba2" => :sierra
     sha256 "7fda0379690a75e870f8c8f3605223d6fe3cba8cfeccc0206a5d6210d4e12de9" => :el_capitan
+    sha256 "764e0557bb5ac7941dd6ae769557f69a469c6304b72323136b484bd0f91eaab2" => :x86_64_linux
   end
 
   depends_on "gmp" unless OS.mac?
