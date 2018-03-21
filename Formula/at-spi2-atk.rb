4856f94e24d9e89366f488009c1fb29f8eae772f
Michka Popoff <michkapopoff@gmail.com>
2018-03-21 22:32:13 +0100

    at-spi2-atk: update 2.26.1_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6797.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/at-spi2-atk.rb
+++ Formula/at-spi2-atk.rb
@@ -10,6 +10,7 @@ class AtSpi2Atk < Formula
     sha256 "55378269fb96de1448cb89ab226da2138503b713875d6ce009b455a52e1b3023" => :high_sierra
     sha256 "6d815c2cf4ffc7881f884e3a9b80771a3150a1c43aecb56fdf8b7f94e4bc17e8" => :sierra
     sha256 "574185424627a2c24041e448c77df13f57e614b08ff74371d29e6177557ba938" => :el_capitan
+    sha256 "20810c0ad8060020a25d1088d3edde4402eb9ea79e8e1811ea74c3140a82cca3" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
