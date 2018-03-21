e8c86a2e8a7bd2d8e747c41297babfd22ea2311b
Michka Popoff <michkapopoff@gmail.com>
2018-03-21 21:45:55 +0100

    gcab: Fix build for Linuxbrew
    Closes Linuxbrew/homebrew-core#6795.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gcab.rb
+++ Formula/gcab.rb
@@ -23,6 +23,9 @@ class Gcab < Formula
   patch :DATA
 
   def install
+    # Needed by intltool (xml::parser)
+    ENV.prepend_path "PERL5LIB", "#{Formula["intltool"].libexec}/lib/perl5" unless OS.mac?
+
     system "./configure", "--disable-debug",
                           "--prefix=#{prefix}"
     system "make", "install"
