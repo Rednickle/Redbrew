7b04b9cbc2b759149908ed9ef9c21defe1e32905
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-22 23:16:35 +0000

    unixodbc: update 2.3.6 bottle for Linuxbrew.

--- Formula/unixodbc.rb
+++ Formula/unixodbc.rb
@@ -8,6 +8,7 @@ class Unixodbc < Formula
     sha256 "cd5a1ea2f0ba6db321b63514b2c33c0c1d74ada9541de8390cca0bc349f4845d" => :high_sierra
     sha256 "2ccf3f7384697dd2460631a4afb685465a93c8f64fb0217c7485ab8919606e7d" => :sierra
     sha256 "3330aee67c21712979a5e0cca8360f691b3e3bd0dd751fdeb15af998d2e6814c" => :el_capitan
+    sha256 "17d3ae4d85f7db8255246e9df382faead0bf53d6cebae49a55edd9fce8ab8b97" => :x86_64_linux
   end
 
   depends_on "libtool"
