8d61629a2d95925ea4f2c09010e16df76641ff9f
Michka Popoff <michkapopoff@gmail.com>
2018-03-22 21:59:58 +0100

    oniguruma: update 6.8.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6669.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/oniguruma.rb
+++ Formula/oniguruma.rb
@@ -9,6 +9,7 @@ class Oniguruma < Formula
     sha256 "e7bbd5885d2695ea8488eec1dcf7deed3e17de40eca94bf278820cdb2590cb38" => :high_sierra
     sha256 "f91ef3bdee096c1be69c337328fe4ded134f7010f04ae0261c68e7f4d5e3afab" => :sierra
     sha256 "8ba0df2621715ba6c47d4380fb554f7e6f554d983e1062b16a5ae81c8c99024f" => :el_capitan
+    sha256 "239c76e91755fe0be37b3ef6f58dac456c8099836e127e53edcb75c3eac2e09a" => :x86_64_linux
   end
 
   def install
