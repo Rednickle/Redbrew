841989c997a7adeda8842ea4cc67b747a890c3e0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-24 15:05:56 +0000

    simgrid: update 3.19.1 bottle for Linuxbrew.

--- Formula/simgrid.rb
+++ Formula/simgrid.rb
@@ -8,6 +8,7 @@ class Simgrid < Formula
     sha256 "8c0b44df4f9c237a8ba7751e3c7362c2a9718e3a92d4c55bd2c24fc858e1ed71" => :high_sierra
     sha256 "1c910c345aea71e528869c4e0d33804f24cc57239909443842c9f6387cc9c4c1" => :sierra
     sha256 "2700b47361aa99d64b31e0b6a262e34599d241aea3f26237fd715733126cdba1" => :el_capitan
+    sha256 "576f58edde8edef95a12ac0d46cf9af07d80c1cb2b601abeabdb6a2432168ec9" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
