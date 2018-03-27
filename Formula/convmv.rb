a96f9f6700bb8e73b378dfc9ad4854f6b6cfab2f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-27 19:28:08 +0000

    convmv: update 2.05 bottle for Linuxbrew.

--- Formula/convmv.rb
+++ Formula/convmv.rb
@@ -9,6 +9,7 @@ class Convmv < Formula
     sha256 "856021a73afb22052e496ced9eb1a7386d810a6d75903aac99feb98298801ea8" => :high_sierra
     sha256 "cc6cf7ff9cfd8909a76e29dd6ddbdddc9ad95638e154b72a36fe6c255e3a367d" => :sierra
     sha256 "43278a7c7ef7720e20fed3179ff8519230678d3fd4e98f4b0e8e796b5fdb40ac" => :el_capitan
+    sha256 "a13979825940c67173e3fe04fffe23c4e1239b1a808cf365ce7a667eeaaf5bd5" => :x86_64_linux
   end
 
   def install
