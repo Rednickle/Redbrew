a956462f04171d4cc61f5234ae113cf2eb9b5e92
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-28 11:27:30 +0000

    terragrunt: update 0.14.4 bottle for Linuxbrew.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -10,6 +10,7 @@ class Terragrunt < Formula
     sha256 "ce8269d0a49d19f4cef725efa8a8b544d3e3b6b0c502a848e5801759c29588a9" => :high_sierra
     sha256 "f543c6f643ac8c4284af18d16e04530f5d5582b900ffc6406773f6a5c03a25e5" => :sierra
     sha256 "1a6708a9c0168ee2ff05df0f2b27b05c1ac571f25cbe66a67175f3d99b076ccb" => :el_capitan
+    sha256 "adee2aa787c04376b814acab9e4b07604bec9c266d9b1368d1b5c9e792f577d1" => :x86_64_linux
   end
 
   depends_on "glide" => :build
