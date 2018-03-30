04eb556187bb726cc2a293ae6c987e61be086525
Michka Popoff <michkapopoff@gmail.com>
2018-03-30 06:51:33 +0200

    icu4c: update 61.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6936.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -12,6 +12,7 @@ class Icu4c < Formula
     sha256 "be8b3ba9420c4da1a75b2e6ebb7a0b835e2919d6499216383f0f313b1d9bb26b" => :high_sierra
     sha256 "07bad03c12d39c9216caa94ff85a2308ab187417d667f59cba9bc727eddcf2ec" => :sierra
     sha256 "5c24ef444ff69c23872efeca53ee9dc98a67c141d4a162bc1349d8bd73a79cd8" => :el_capitan
+    sha256 "9b2c0602e87376bb08ad8e79ada8befa5c33556b3def9b10419ca1764556d6eb" => :x86_64_linux
   end
 
   keg_only :provided_by_macos, "macOS provides libicucore.dylib (but nothing else)"
