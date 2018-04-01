7a15e4910c16480271537a822b44785d5aa3bbbc
Michka Popoff <michkapopoff@gmail.com>
2018-04-01 23:11:30 +0200

    nano: update 2.9.5 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#6972.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -8,6 +8,7 @@ class Nano < Formula
     sha256 "457885aa6c8c26084f35ec111a7acdd70494d3e694532dfc0c1c3d8dbd7ff5cf" => :high_sierra
     sha256 "72d7563e10af34d79363f865a053575903d2d991408f061013a3de8ff459a966" => :sierra
     sha256 "9ed7f4595a62a48981364e172e0acd995ef8f26f571aa841b88729d38638107c" => :el_capitan
+    sha256 "d9e066c27a9fe9b959d602b253b3da21d44b61f48d5d3328a2af937f92f94162" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
