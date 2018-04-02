94a71391131bd1e669d364d2af1babc7d02160c8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-02 19:31:12 +0000

    hledger: update 1.9 bottle for Linuxbrew.

--- Formula/hledger.rb
+++ Formula/hledger.rb
@@ -13,6 +13,7 @@ class Hledger < Formula
     sha256 "565012c295cccdf2458a7c8b97a753406dce0dff3272a3195f3c9603a93532bf" => :high_sierra
     sha256 "783c9ac9f6f52c2980e228f67f5bc27cf06c3bcfe36d6493f47164374a57ed26" => :sierra
     sha256 "1701f551254a3e7d1cd76abc881e2080d0431f2d70bd25932653b508a80a8999" => :el_capitan
+    sha256 "1654384c5f1705848011ec390774d46fe2b557605555693e96eb636405831979" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
