731f34851b2888b8412613f95ac8aa4ca435ea45
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-03 21:33:18 +0000

    git: update 2.17.0 bottle for Linuxbrew.

--- Formula/git.rb
+++ Formula/git.rb
@@ -9,6 +9,7 @@ class Git < Formula
     sha256 "afaaaff6d415d09fce5d624f0c883672fff7f1b9b710f862be8c8a30e7e1ab2b" => :high_sierra
     sha256 "a79c0cb609eba78071bdebd168f3cb70cb2eb4943c8712a4af5d722a217aaef3" => :sierra
     sha256 "b25864491b600776a662ff2ec6332d7b7cf788f9a0b5136b945b2ebae09bb565" => :el_capitan
+    sha256 "c24c7173fa461216013c7228cac30bc709899388d63e802ccdf2edb5c944177d" => :x86_64_linux
   end
 
   option "with-blk-sha1", "Compile with the block-optimized SHA1 implementation"
