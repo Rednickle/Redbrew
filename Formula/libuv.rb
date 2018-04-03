3f3835858e91e539c625139cf2becf1a7dd9cbb4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-03 21:08:28 +0000

    libuv: update 1.20.0 bottle for Linuxbrew.

--- Formula/libuv.rb
+++ Formula/libuv.rb
@@ -10,6 +10,7 @@ class Libuv < Formula
     sha256 "476c88880da45abdbf7a544eec324460e47d166781f3a04f3291535d71531d21" => :high_sierra
     sha256 "15dfa2c0d814b37c546d3f292ee8e3f0b23e976a4dc6902d51548224ba0389af" => :sierra
     sha256 "89ee31a4552be7aeccfb6430a12925f05960bd314a77621ddf7e4272bcacf9b9" => :el_capitan
+    sha256 "2a7f6873d36249b807cd17eb310edc09cac8fc22173415767ba005b3b6c9260d" => :x86_64_linux
   end
 
   option "with-test", "Execute compile time checks (Requires Internet connection)"
