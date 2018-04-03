4a65065a087bd14fbe5facee49555ee33621d62a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-03 21:08:29 +0000

    lbdb: update 0.47 bottle for Linuxbrew.

--- Formula/lbdb.rb
+++ Formula/lbdb.rb
@@ -9,6 +9,7 @@ class Lbdb < Formula
     sha256 "6b26c9ddcde88f674957eb32f7efbba04f77c1bc994d4b9268681eb07ae0fbfa" => :high_sierra
     sha256 "23c943bd75b4ce851e6b5cf8561b0d7293e503709f6df3e7f79d18d13ea97894" => :sierra
     sha256 "8922d0c2e0905ea7ee43ec738042333b3cfa79558c8529f10d6c26c0c57fbc85" => :el_capitan
+    sha256 "94a6c0dadf03de4e75d2d3aa6fb4dd6c1594939f9df0eec26e4fa4b141d106d4" => :x86_64_linux
   end
 
   depends_on "abook" => :recommended
