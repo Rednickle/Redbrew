de3a3bcb2aee1841f098fbf3e626fa64ef276771
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-05 21:51:12 +0000

    talloc: update 2.1.12 bottle for Linuxbrew.

--- Formula/talloc.rb
+++ Formula/talloc.rb
@@ -9,6 +9,7 @@ class Talloc < Formula
     sha256 "7c253d713cfee5cfc5802deebb527093d737fcf339396d0fd804c22d912d711f" => :high_sierra
     sha256 "cd39c2b0c43622780862b4725b0ff7d178a704c1c6020e633ab78c3d37205c05" => :sierra
     sha256 "bed9fd9c2af02a6cf42eda10539c1e5e7360a9c7bc6338bdf8524bc4f73119cc" => :el_capitan
+    sha256 "3ec9fe7ededa2e3c185ff0809cd5a555ab6e6b2987130c8cdc0c744f032a9d4b" => :x86_64_linux
   end
 
   depends_on "python@2" => :build unless OS.mac?
