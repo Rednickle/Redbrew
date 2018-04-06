a649cfdd109c47c9264a7f6af0cb1d608b2685ed
Michka Popoff <michkapopoff@gmail.com>
2018-04-06 22:03:35 +0200

    hugo: update 0.38.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7068.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -10,6 +10,7 @@ class Hugo < Formula
     sha256 "29d5f18388495ea144b459454810adba0da42ca2c5bcd2901e87e618f5d86838" => :high_sierra
     sha256 "01b8eb74b83df9b46f0c091f428debed67a92dbdf4c0cf1d3492524df478313c" => :sierra
     sha256 "c5c210f2b0e15a642b41b63aae78896f0c18e229b6a0ab88924cbb6514ecc261" => :el_capitan
+    sha256 "5314fb923ca69972d9a6cd98c3d65e92b7e60b5318a7b299a7cf8d0450f21408" => :x86_64_linux
   end
 
   depends_on "dep" => :build
