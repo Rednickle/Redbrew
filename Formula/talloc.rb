945052fafdd8750f0eb0af88724039c365889953
Michka Popoff <michkapopoff@gmail.com>
2018-04-06 21:53:07 +0200

    talloc: update 2.1.13 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7071.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/talloc.rb
+++ Formula/talloc.rb
@@ -9,6 +9,7 @@ class Talloc < Formula
     sha256 "89badf631b95b7d4f447da5c6d7ca93949df26ecdf0ba57f965314f70d3aeaa3" => :high_sierra
     sha256 "68d41383b977d79d569e740a06480f0419a840d06fd108df1198a8f14521f85c" => :sierra
     sha256 "50b9fc4ccf02a98b2d44b0b34781e89f9160c9b37e3d9a8a133ead93a8b3839d" => :el_capitan
+    sha256 "20a442231ed4fe0c47fbfe506f41bb2b45872c73b22a39b14037c55d52e4749e" => :x86_64_linux
   end
 
   depends_on "python@2" => :build unless OS.mac?
