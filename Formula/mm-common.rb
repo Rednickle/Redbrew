8161fca737550ff6ebda16f8d77e5b1b29b2e2b1
Michka Popoff <michkapopoff@gmail.com>
2018-04-09 22:42:49 +0200

    mm-common: update 0.9.12 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7088.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mm-common.rb
+++ Formula/mm-common.rb
@@ -9,6 +9,7 @@ class MmCommon < Formula
     sha256 "42c9654bebbc472d90bc31d14e0832d55367d8d86d6750ab546a129a48de342b" => :high_sierra
     sha256 "42c9654bebbc472d90bc31d14e0832d55367d8d86d6750ab546a129a48de342b" => :sierra
     sha256 "42c9654bebbc472d90bc31d14e0832d55367d8d86d6750ab546a129a48de342b" => :el_capitan
+    sha256 "85398e2c7a5ab5f48e1e03476b3c774e6afba282a3f4ea7842176838b26efe6c" => :x86_64_linux
   end
 
   def install
