b9b473cd107de913b0e8b2a5f04599b8bc0a9515
Michka Popoff <michkapopoff@gmail.com>
2018-04-11 00:01:32 +0200

    miniupnpc: update 2.0.20180410 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7117.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/miniupnpc.rb
+++ Formula/miniupnpc.rb
@@ -9,6 +9,7 @@ class Miniupnpc < Formula
     sha256 "1c0b7e56919ce59d6ef76e7804df53ef7b5923a6f67ec78a653e671b9478c532" => :high_sierra
     sha256 "835144ebca86307b1bb99e21440af2a7322eaa1add8d1035af5d795ea0fe509d" => :sierra
     sha256 "6d07c2529e80fdb4d3459be841ae533ec19e45aed8093f49dbd069ce3156c6b0" => :el_capitan
+    sha256 "39cc2e0576e6e4c5d9eb3d46479c02286a7ac6dca803dc1bf84c3cdc4445ed96" => :x86_64_linux
   end
 
   def install
