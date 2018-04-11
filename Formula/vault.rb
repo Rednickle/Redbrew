6f7c5d88044a396dd4e7a73f2f208e987c36b455
Michka Popoff <michkapopoff@gmail.com>
2018-04-11 13:00:03 +0200

    vault: update 0.10.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7131.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -14,6 +14,7 @@ class Vault < Formula
     sha256 "4ff1b5e8eb778ffc3ec619a5eec1a0de8ebbd8ad9fd7bd5dc5995b63a86d7ad3" => :high_sierra
     sha256 "26806f016116d3e7b398c3a9103e7959d9f5e0df91f798c3f42f280806133b73" => :sierra
     sha256 "6f1fb1b31efd2c5be1398ca564618dc9c3aa8ae37b58033014f12215c5a640d6" => :el_capitan
+    sha256 "558a2dd38585a077abdb3e06e1dd04472453928c8a5f45b5bcacc5106093a423" => :x86_64_linux
   end
 
   option "with-dynamic", "Build dynamic binary with CGO_ENABLED=1"
