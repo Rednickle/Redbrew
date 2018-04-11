35c309fbc5217f2778c737eada6e85b83a383587
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-11 05:42:43 +0000

    di: update 4.45 bottle for Linuxbrew.

--- Formula/di.rb
+++ Formula/di.rb
@@ -9,6 +9,7 @@ class Di < Formula
     sha256 "c6b73f6857b8b029f1b2aaf6bd6318c4ce6ddb6d2acb984136197da2105edf4d" => :high_sierra
     sha256 "5af2e6470606f15bc327aa250238dbfe61d288aa39396e6567f06a495fb9f143" => :sierra
     sha256 "0d0d0ac1a487899b51382a6e462bd0085f0e1181b8a4f3b65fcb04ebf608aa73" => :el_capitan
+    sha256 "0c182e6d7044d00c579c65bd0960b33804f71a3eeb0a147f90b691b0050af818" => :x86_64_linux
   end
 
   def install
