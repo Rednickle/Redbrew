187b52ee43b38e07c87759400b7912da94467102
Michka Popoff <michkapopoff@gmail.com>
2018-04-12 13:42:55 +0200

    sops: update 3.0.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7162.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/sops.rb
+++ Formula/sops.rb
@@ -12,6 +12,7 @@ class Sops < Formula
     sha256 "fa8cbfd43154b3511caa88f1fedd25d335592304891b6565a1b04f9e52f836e9" => :high_sierra
     sha256 "674a7a6966646fd04bf7ce4a38406820d4bb1a8a0a683f3654f3ddd3d32b3f75" => :sierra
     sha256 "f99d3db5b21b95900f814f10639cab492ca8a234da808d250bb413f4db9a63c1" => :el_capitan
+    sha256 "ec7cbf75225b9f1ee7d3f0acab51d16b29ce188c44b18724421f9e08114b59b2" => :x86_64_linux
   end
 
   depends_on "go" => :build
