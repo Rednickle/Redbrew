088301e46d6ada282343a3365e79cc616f9f09e9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-16 17:40:23 +0000

    sqliteodbc: update 0.9996 bottle for Linuxbrew.

--- Formula/sqliteodbc.rb
+++ Formula/sqliteodbc.rb
@@ -9,6 +9,7 @@ class Sqliteodbc < Formula
     sha256 "7d825f83232825a51c8fd871368a2ff8cce3a76fe9ba1646f20e15f121ecf79e" => :high_sierra
     sha256 "907c1b32398eb7f3276e1da956723ac27868d9bcae27fb55ef76277cf2f67cb7" => :sierra
     sha256 "7c550f77c2db4e4927b9f23ed8a57610727b438f6a0fad98e1adecee3c8c1aa1" => :el_capitan
+    sha256 "25ff46c773af4b633504f96ba20e1232d637fbc873c7af41ded2888483a0d318" => :x86_64_linux
   end
 
   depends_on "sqlite"
