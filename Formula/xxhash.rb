035fd98b788f9320601a5d8fac559e230ec64f73
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-19 17:48:28 +0000

    xxhash: update 0.6.5 bottle for Linuxbrew.

--- Formula/xxhash.rb
+++ Formula/xxhash.rb
@@ -9,6 +9,7 @@ class Xxhash < Formula
     sha256 "283532f22d512db5c2266a64f84a425a0fa3f6048aeb751fcb159df191b70763" => :high_sierra
     sha256 "fedfdf613f1d60af81bb1bc5bbc125249a5ae0d8c6fe0863d677e641b5cad52f" => :sierra
     sha256 "7cbdee7d1220206000efe718a6c599d70142f2ce006e9d6743e1f447cd2d3dbd" => :el_capitan
+    sha256 "c6975a34b8419e2064a108bc23c9ee7b1c6e157d52a5ab3a052f84d2dcc91c28" => :x86_64_linux
   end
 
   def install
