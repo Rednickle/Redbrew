d0a98caf6853b53e96d8ae86d7ee83d099155d9d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-22 11:35:11 +0000

    gperftools: update 2.6.3_2 bottle for Linuxbrew.

--- Formula/gperftools.rb
+++ Formula/gperftools.rb
@@ -10,6 +10,7 @@ class Gperftools < Formula
     sha256 "0d36f2291e12318c900358e5c5b38d1322533173d66cc64ec17e404333b40a70" => :high_sierra
     sha256 "498699638aa7e06c11c7ceee5b7c4bb96eaffa02ef0fe043b22140fe5f22c575" => :sierra
     sha256 "d91d9e54d3522a3c6aca928660d6521455fa2ddc21e766e8f386fd58ebaac17b" => :el_capitan
+    sha256 "dc18f4881b770a7e02afdb3c359c2d13ce54439f4fe8e1831d161a8348bb7411" => :x86_64_linux
   end
 
   unless OS.mac?
