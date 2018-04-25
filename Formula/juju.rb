fb4e79ce8af461ef37c7a8527d1a342b94c79d9f
Michka Popoff <michkapopoff@gmail.com>
2018-04-25 21:06:53 +0200

    juju: update 2.3.7 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7340.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -9,6 +9,7 @@ class Juju < Formula
     sha256 "6fc18f158224fbb30ec1bcbbb3fe0a0cf526682b3a8c4ab32f8ebf0ec2ec2eac" => :high_sierra
     sha256 "027c2f130e33193f4200c454de81a6d8517fe62c97e6d216ca958e4101100f76" => :sierra
     sha256 "9228b79b6e9a82f141ab6e6983b17b6f9d436d10be12b5911854b4c51288f9ad" => :el_capitan
+    sha256 "ffde2aafb4d9d260feee5895298c2dddd9730498f9c047dcfd3f09e20b2a669d" => :x86_64_linux
   end
 
   depends_on "go" => :build
