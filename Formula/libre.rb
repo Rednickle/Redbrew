410ca9a930447a2e1cfe29735f53ee0531ba08b8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-25 20:11:07 +0000

    libre: update 0.5.8 bottle for Linuxbrew.

--- Formula/libre.rb
+++ Formula/libre.rb
@@ -9,6 +9,7 @@ class Libre < Formula
     sha256 "538415bf11082804ed7ab100bf72fa9618ba22e4d51b54dc21314d0ea8d63620" => :high_sierra
     sha256 "a46b8e91cdf25d78db8c0857d89f8123abeb1ecdccf296c4d169326daf72ac04" => :sierra
     sha256 "8fa0389f4c16bc17ae9cfa9f1cba79facbaca9b96f3a02476d4ba2e3541ce568" => :el_capitan
+    sha256 "f245cd65d5879897cf430b623da0598efbd60f714ed74d9d5692c945fc67063a" => :x86_64_linux
   end
 
   depends_on "openssl"
