dcb1750133d23b0ffb4cd4441e302e0b87876b30
Michka Popoff <michkapopoff@gmail.com>
2018-04-25 21:14:11 +0200

    tokei: update 7.0.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7344.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/tokei.rb
+++ Formula/tokei.rb
@@ -8,6 +8,7 @@ class Tokei < Formula
     sha256 "3955b971c68a46c26e6433c9a71038961bf4facbdff7b86f82cae2046cedd5a2" => :high_sierra
     sha256 "a2a9f4b5b019bf1dca116c9f10e7ca6871f4e78d110d13edf6b46b7a61c43a88" => :sierra
     sha256 "4b271a5d3f85fcf29be673deb2271216f3574e336eb6e954c1611811e4210dc5" => :el_capitan
+    sha256 "f6fb1226669d349e22c6850644eff70f1811863a22d0002231c3fba6d3b6f4e8" => :x86_64_linux
   end
 
   depends_on "rust" => :build
