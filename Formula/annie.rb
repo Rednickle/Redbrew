d56265f513cffe074fdb1b9e4951b39a254c462b
Michka Popoff <michkapopoff@gmail.com>
2018-04-27 21:40:13 +0200

    annie: update 0.6.9 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7381.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/annie.rb
+++ Formula/annie.rb
@@ -9,6 +9,7 @@ class Annie < Formula
     sha256 "25a2f20ba454b3ab9ae49f7de5917ab4dacae34e5e179ef36ee3ea94811cac60" => :high_sierra
     sha256 "d03dcb05e8e3dfd51dd6c2c3a232ee966f5c5925b2b9ca022f477a7598849713" => :sierra
     sha256 "aef201163b1dc90a1a654beadd0b138015503a02728ba75232e237ba3d3d7c4a" => :el_capitan
+    sha256 "b96a25768769c3b7d7968500ac8fe38998e682a77151341d5cb5af438765bd70" => :x86_64_linux
   end
 
   depends_on "go" => :build
