cb69e632b6935c723be36827cd83446c3d9fd3a1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-28 09:29:40 +0000

    acpica: update 20180427 bottle for Linuxbrew.

--- Formula/acpica.rb
+++ Formula/acpica.rb
@@ -10,6 +10,7 @@ class Acpica < Formula
     sha256 "adaa7d1ce2342578a8bae5db72fe75d47f34305d56a86c99ea7304eca5f82d61" => :high_sierra
     sha256 "8d808727a689efba1f5821d8d2c29ae0f0a9e24623fd3675827c837e3ae3a61b" => :sierra
     sha256 "b96b602f9e260b758ed9c2a6399fa3b6b346a2f9ff62b2ab658dfc4a16028dbe" => :el_capitan
+    sha256 "6fb66f087223d1393fed86b1533b8091616158a8094f1b1b5cdf1fdb578eb778" => :x86_64_linux
   end
 
   unless OS.mac?
