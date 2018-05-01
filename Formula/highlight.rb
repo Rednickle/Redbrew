74fe609cab664c4f88ce386dbd1510a4c0904771
Michka Popoff <michkapopoff@gmail.com>
2018-05-01 19:30:48 +0200

    highlight: update 3.43 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7415.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/highlight.rb
+++ Formula/highlight.rb
@@ -9,6 +9,7 @@ class Highlight < Formula
     sha256 "0f4e807b8f34844013f250cd2199866eca2a7344dc0928cfc0c4c65b2dba1791" => :high_sierra
     sha256 "01915f2af644d25025efcff0a2d58164fcde94ca08ce0911037f7d1cdb99c4f9" => :sierra
     sha256 "e37f2f9fc6b324f8267ad768475f464e7abd3403bc2dd124e3fda2a15b055e36" => :el_capitan
+    sha256 "c23bd1e36f6970c0dcf1251ffa6e712abb8b04804c6974b77557cee1e1fa3913" => :x86_64_linux
   end
 
   depends_on "boost" => :build
