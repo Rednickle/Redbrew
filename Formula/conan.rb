bdf9554720c936b784e76529caebe30df8620d35
Michka Popoff <michkapopoff@gmail.com>
2018-05-01 22:28:20 +0200

    conan: update 1.3.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7427.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/conan.rb
+++ Formula/conan.rb
@@ -9,9 +9,7 @@ class Conan < Formula
 
   bottle do
     cellar :any
-    sha256 "2687cb16a7b2d453189db9bffc06ff1eb118ab07dd4015e7f92d51446963152e" => :high_sierra
-    sha256 "c9702d8e42d9761fd134dd1fbb5f5ba9486a04a379a2727934e0c0191d753d30" => :sierra
-    sha256 "0bc600a6385ce5b7a1dc23c8df5f72ac1e860f6de423774c9b9ce5223e4d22f7" => :el_capitan
+    sha256 "c02257a821c6a1e8f29fab6f4fbe7507334f5995ce24038e726fadc6e159dd24" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
