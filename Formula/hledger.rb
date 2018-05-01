97ceee34629ff92c7a546bc2cfb6271ea2d61131
Michka Popoff <michkapopoff@gmail.com>
2018-05-01 21:03:12 +0200

    hledger: update 1.9.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7431.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/hledger.rb
+++ Formula/hledger.rb
@@ -13,6 +13,7 @@ class Hledger < Formula
     sha256 "b98a55685dc4c619e59f102c91b75af384711b4d41ad05a184cec258cc5f9995" => :high_sierra
     sha256 "7ca8c6444c3f5226f7e7831d31c9f389aa2f56540c9f9310a5b3842520dbffac" => :sierra
     sha256 "59566873ae4f71158ea1f0ea4a551372a5b3b88bc121347d620428d4e30b326a" => :el_capitan
+    sha256 "ee69922d0efd4de698800774e4168f68e6173d99928e4a8630555e0728ee8a7f" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
