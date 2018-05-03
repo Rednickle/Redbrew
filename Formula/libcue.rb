8599f4d5c63e0b123fffc8ff569d5cdc0a62b581
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-03 06:00:53 +0000

    libcue: update 2.2.1 bottle for Linuxbrew.

--- Formula/libcue.rb
+++ Formula/libcue.rb
@@ -9,6 +9,7 @@ class Libcue < Formula
     sha256 "209e548399503830e0f786c6faef21836aa350d67db644b9ad291703ebe2e9c5" => :high_sierra
     sha256 "14a6edb39d2887ad6beeb34dad944501d01f70480a529cb7e50d838833404f4f" => :sierra
     sha256 "27f8ab5419958ea5817e5e44b68f24ea2a0c27d12a664556b12f6789866d0da5" => :el_capitan
+    sha256 "c06e978e72a9220195abe2ee63b3b5caca3b7d23fa90565b6277b0c24dc41775" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
