1a50e76b4158cf2797620385a39241c6f83419e2
Michka Popoff <michkapopoff@gmail.com>
2018-05-04 23:41:13 +0200

    fswatch: update 1.11.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7501.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/fswatch.rb
+++ Formula/fswatch.rb
@@ -6,9 +6,7 @@ class Fswatch < Formula
 
   bottle do
     cellar :any
-    sha256 "542ed7f8f6b66806524a6ddfab9c9be79d98d741a0cae229bb8e76829f17f954" => :high_sierra
-    sha256 "772fe9d50aff488e4cbf24af9d15c0f1fafe6629abb12d107d850cf4c751264c" => :sierra
-    sha256 "087d3325d1faf25c8f1b25920a25d169c255bc17ee357e4734c2b999139bc0ce" => :el_capitan
+    sha256 "3690ff441fc3318b46922b404d7e467012218a9afe643045479cfd94008b985d" => :x86_64_linux
   end
 
   needs :cxx11
