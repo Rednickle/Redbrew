620c5ce76f45d69a13cbfd4ba6e915dd6a9796a3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-05 18:27:42 +0000

    genact: update 0.4.0 bottle for Linuxbrew.

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -8,6 +8,7 @@ class Genact < Formula
     sha256 "cda0788c8c4b3430489284c98970a45fa4111cbcaada6112ec2171e70bc2de17" => :high_sierra
     sha256 "0eba28cdb57f5bb297796668c65aff4eda911b7b23698306f27b0dcf19706156" => :sierra
     sha256 "be6e7870976653c8d2a91f42fb8380531aa333f729d8d3298b4009efacc6a09d" => :el_capitan
+    sha256 "afe659bf95e65585579c00b26d8454d7ac497590e0421032e67f1fc16c9ab20a" => :x86_64_linux
   end
 
   depends_on "rust" => :build
