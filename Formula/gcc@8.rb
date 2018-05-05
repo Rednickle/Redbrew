4e9744f214be46c685a41f35e3d7708770086677
Xu Cheng <git@xuc.me>
2018-05-05 14:29:36 +0800

    gcc@8: Fix for Linuxbrew

--- Formula/gcc@8.rb
+++ Formula/gcc@8.rb
@@ -6,6 +6,11 @@ class GccAT8 < Formula
   sha256 "1d1866f992626e61349a1ccd0b8d5253816222cdc13390dcfaa74b093aa2b153"
   head "svn://gcc.gnu.org/svn/gcc/trunk"
 
+  # gcc is designed to be portable.
+  bottle do
+    cellar :any
+  end
+
   option "with-jit", "Build just-in-time compiler"
   option "with-nls", "Build with native language support (localization)"
 
@@ -13,6 +18,12 @@ class GccAT8 < Formula
   depends_on "isl"
   depends_on "libmpc"
   depends_on "mpfr"
+
+  unless OS.mac?
+    depends_on "zlib"
+    depends_on "binutils" if build.with? "glibc"
+    depends_on "glibc" => (Formula["glibc"].installed? || !GlibcRequirement.new.satisfied?) ? :recommended : :optional
+  end
 
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
   cxxstdlib_check :skip
@@ -33,6 +44,9 @@ class GccAT8 < Formula
   end
 
   def install
+    # Reduce memory usage below 4 GB for Circle CI.
+    ENV["MAKEFLAGS"] = "-j8 -l2.5" if ENV["CIRCLECI"]
+
     # GCC will suffer build errors if forced to use a particular linker.
     ENV.delete "LD"
 
@@ -45,10 +59,45 @@ class GccAT8 < Formula
     # JIT compiler is off by default, enabling it has performance cost
     languages << "jit" if build.with? "jit"
 
-    osmajor = `uname -r`.chomp
+    args = []
 
-    args = [
-      "--build=x86_64-apple-darwin#{osmajor}",
+    if OS.mac?
+      osmajor = `uname -r`.chomp
+      args += [
+        "--build=x86_64-apple-darwin#{osmajor}",
+        "--with-system-zlib",
+        "--with-bugurl=https://github.com/Homebrew/homebrew-core/issues",
+      ]
+    else
+      args += [
+        "--with-bugurl=https://github.com/Linuxbrew/homebrew-core/issues",
+        # Fix Linux error: gnu/stubs-32.h: No such file or directory.
+        "--disable-multilib",
+      ]
+
+      # Change the default directory name for 64-bit libraries to `lib`
+      # http://www.linuxfromscratch.org/lfs/view/development/chapter06/gcc.html
+      inreplace "gcc/config/i386/t-linux64", "m64=../lib64", "m64="
+
+      if build.with? "glibc"
+        args += [
+          "--with-native-system-header-dir=#{HOMEBREW_PREFIX}/include",
+          # Pass the specs to ./configure so that gcc can pickup brewed glibc.
+          # This fixes the building failure if the building system uses brewed gcc
+          # and brewed glibc. Document on specs can be found at
+          # https://gcc.gnu.org/onlinedocs/gcc/Spec-Files.html
+          # Howerver, there is very limited document on `--with-specs` option,
+          # which has certain difference compared with regular spec file.
+          # But some relevant information can be found at https://stackoverflow.com/a/47911839
+          "--with-specs=%{!static:%x{--dynamic-linker=#{HOMEBREW_PREFIX}/lib/ld.so} %x{-rpath=#{HOMEBREW_PREFIX}/lib}}",
+        ]
+        # Set the search path for glibc libraries and objects.
+        # Fix the error: ld: cannot find crti.o: No such file or directory
+        ENV["LIBRARY_PATH"] = Formula["glibc"].opt_lib
+      end
+    end
+
+    args += [
       "--prefix=#{prefix}",
       "--libdir=#{lib}/gcc/#{version_suffix}",
       "--enable-languages=#{languages.join(",")}",
@@ -58,10 +107,8 @@ class GccAT8 < Formula
       "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
       "--with-mpc=#{Formula["libmpc"].opt_prefix}",
       "--with-isl=#{Formula["isl"].opt_prefix}",
-      "--with-system-zlib",
       "--enable-checking=release",
       "--with-pkgversion=Homebrew GCC #{pkg_version} #{build.used_options*" "}".strip,
-      "--with-bugurl=https://github.com/Homebrew/homebrew-core/issues",
     ]
 
     args << "--disable-nls" if build.without? "nls"
@@ -69,10 +116,10 @@ class GccAT8 < Formula
 
     # Ensure correct install names when linking against libgcc_s;
     # see discussion in https://github.com/Homebrew/homebrew/pull/34303
-    inreplace "libgcc/config/t-slibgcc-darwin", "@shlib_slibdir@", "#{HOMEBREW_PREFIX}/lib/gcc/#{version_suffix}"
+    inreplace "libgcc/config/t-slibgcc-darwin", "@shlib_slibdir@", "#{HOMEBREW_PREFIX}/lib/gcc/#{version_suffix}" if OS.mac?
 
     mkdir "build" do
-      unless MacOS::CLT.installed?
+      if OS.mac? && !MacOS::CLT.installed?
         # For Xcode-only systems, we need to tell the sysroot path.
         # "native-system-headers" will be appended
         args << "--with-native-system-header-dir=/usr/include"
@@ -90,8 +137,6 @@ class GccAT8 < Formula
 
       system "make", *make_args
       system "make", "install"
-
-      bin.install_symlink bin/"gfortran-#{version_suffix}" => "gfortran"
     end
 
     # Handle conflicts between GCC formulae and avoid interfering
@@ -100,6 +145,14 @@ class GccAT8 < Formula
     Dir.glob(man7/"*.7") { |file| add_suffix file, version_suffix }
     # Even when we disable building info pages some are still installed.
     info.rmtree
+
+    unless OS.mac?
+      # Strip the binaries to reduce their size.
+      system("strip", "--strip-unneeded", "--preserve-dates", *Dir[prefix/"**/*"].select do |f|
+        f = Pathname.new(f)
+        f.file? && (f.elf? || f.extname == ".a")
+      end)
+    end
   end
 
   def add_suffix(file, suffix)
@@ -107,6 +160,71 @@ class GccAT8 < Formula
     ext = File.extname(file)
     base = File.basename(file, ext)
     File.rename file, "#{dir}/#{base}-#{suffix}#{ext}"
+  end
+
+  def post_install
+    unless OS.mac?
+      gcc = bin/"gcc-#{version_suffix}"
+      libgcc = Pathname.new(Utils.popen_read(gcc, "-print-libgcc-file-name")).parent
+      raise "command failed: #{gcc} -print-libgcc-file-name" if $CHILD_STATUS.exitstatus.nonzero?
+
+      glibc = Formula["glibc"]
+      glibc_installed = glibc.any_version_installed?
+
+      # Symlink crt1.o and friends where gcc can find it.
+      ln_sf Dir[glibc.opt_lib/"*crt?.o"], libgcc if glibc_installed
+
+      # Create the GCC specs file
+      # See https://gcc.gnu.org/onlinedocs/gcc/Spec-Files.html
+
+      # Locate the specs file
+      specs = libgcc/"specs"
+      ohai "Creating the GCC specs file: #{specs}"
+      specs_orig = Pathname.new("#{specs}.orig")
+      rm_f [specs_orig, specs]
+
+      system_header_dirs = ["#{HOMEBREW_PREFIX}/include"]
+
+      # Locate the native system header dirs if user uses system glibc
+      unless glibc_installed
+        target = Utils.popen_read(gcc, "-print-multiarch").chomp
+        raise "command failed: #{gcc} -print-multiarch" if $CHILD_STATUS.exitstatus.nonzero?
+        system_header_dirs += ["/usr/include/#{target}", "/usr/include"]
+      end
+
+      # Save a backup of the default specs file
+      specs_string = Utils.popen_read(gcc, "-dumpspecs")
+      raise "command failed: #{gcc} -dumpspecs" if $CHILD_STATUS.exitstatus.nonzero?
+      specs_orig.write specs_string
+
+      # Set the library search path
+      # For include path:
+      #   * `-isysroot #{HOMEBREW_PREFIX}/nonexistent` prevents gcc searching built-in
+      #     system header files.
+      #   * `-idirafter <dir>` instructs gcc to search system header
+      #     files after gcc internal header files.
+      # For libraries:
+      #   * `-nostdlib -L#{libgcc}` instructs gcc to use brewed glibc
+      #     if applied.
+      #   * `-L#{libdir}` instructs gcc to find the corresponding gcc
+      #     libraries. It is essential if there are multiple brewed gcc
+      #     with different versions installed.
+      #     Noted that it should only be passed for the `gcc@*` formulae.
+      #   * `-L#{HOMEBREW_PREFIX}/lib` instructs gcc to find the rest
+      #     brew libraries.
+      libdir = HOMEBREW_PREFIX/"lib/gcc/#{version_suffix}"
+      specs.write specs_string + <<~EOS
+        *cpp_unique_options:
+        + -isysroot #{HOMEBREW_PREFIX}/nonexistent #{system_header_dirs.map { |p| "-idirafter #{p}" }.join(" ")}
+
+        *link_libgcc:
+        #{glibc_installed ? "-nostdlib -L#{libgcc}" : "+"} -L#{libdir} -L#{HOMEBREW_PREFIX}/lib
+
+        *link:
+        + --dynamic-linker #{HOMEBREW_PREFIX}/lib/ld.so -rpath #{libdir} -rpath #{HOMEBREW_PREFIX}/lib
+
+      EOS
+    end
   end
 
   test do
@@ -143,7 +261,7 @@ class GccAT8 < Formula
       write(*,"(A)") "Done"
       end
     EOS
-    system "#{bin}/gfortran", "-o", "test", "test.f90"
+    system "#{bin}/gfortran-#{version_suffix}", "-o", "test", "test.f90"
     assert_equal "Done\n", `./test`
   end
 end
