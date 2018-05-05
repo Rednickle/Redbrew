a893a81c4d51a8c20b00af245b371378fc845194
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-05 16:35:58 +0000

    scamper: update 20180504 bottle for Linuxbrew.

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -9,6 +9,7 @@ class Scamper < Formula
     sha256 "8eebcd5d3451744d3020a8925c49988271e8c96f1ade51692362eb0f401fccc7" => :high_sierra
     sha256 "5c0144da8cbe504095f8613d64134dabb97a1a1478ba1104519ab9f77c0fa725" => :sierra
     sha256 "49389540e24b1bb0a606569a1c3cda007cc86e7a1abda920089a326f35ce5d0f" => :el_capitan
+    sha256 "e5ee0f6a175ca5e9a99b5ac40cd879a1dbca4b3e66c5672ea829df719c6dbe1c" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
