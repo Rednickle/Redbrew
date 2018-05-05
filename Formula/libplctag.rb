2cf4355e5c43804632f3b80fc3d603de86d37cb7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-05 18:27:43 +0000

    libplctag: update 1.5.8 bottle for Linuxbrew.

--- Formula/libplctag.rb
+++ Formula/libplctag.rb
@@ -5,10 +5,11 @@ class Libplctag < Formula
   sha256 "03847aeb4bdbfad255e62feff029b9b0c81758e66b840237023243b1c1b764ec"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     sha256 "a784c2b1d50c673cf3557e774f78b44bd1968a162f715afeca9b31e27f136d5c" => :high_sierra
     sha256 "76e29f82f14a9f0a7b08cae7a8c13bdbbf46a8bf947ea1a2c7b1b7eea2a25165" => :sierra
     sha256 "13703497dc39ded95c332fd2eb4cae4aa34bb0ee77c7027de1b37b0ec276052b" => :el_capitan
+    sha256 "c5754739c9ab4635561bfdfedd53495e66a2b4b2bf9e06455089216d97112f30" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
