147030c78922a2b0e283f68838be460f80433010
Michka Popoff <michkapopoff@gmail.com>
2018-05-06 00:01:36 +0200

    ilmbase: update 2.2.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7542.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ilmbase.rb
+++ Formula/ilmbase.rb
@@ -9,6 +9,7 @@ class Ilmbase < Formula
     sha256 "5621509767a95332eff8e26f7fe80c6bce9c3c271fa8521e234263b3c3d67454" => :high_sierra
     sha256 "7b40da5907be805067a7af87b5a5af2dac9e446478de06316a059fa9c4f9a9c0" => :sierra
     sha256 "402fe7453b9ca2c4c4a3fbdb9557156819e26c959e18c096dcceab8b1b6ce9a5" => :el_capitan
+    sha256 "d2e681058d004b7d1df74154cd405e851ec935c82443cdffd29b32877a101f4f" => :x86_64_linux
   end
 
   def install
