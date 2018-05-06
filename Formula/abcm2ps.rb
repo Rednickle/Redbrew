4a10edd148f370629671e8d000d67cadc409e465
Michka Popoff <michkapopoff@gmail.com>
2018-05-06 11:00:13 +0200

    abcm2ps: update 8.13.21 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7550.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/abcm2ps.rb
+++ Formula/abcm2ps.rb
@@ -5,9 +5,7 @@ class Abcm2ps < Formula
   sha256 "2c3f9bdac281feddc384e5719e3cd29f85c88cb326077516760425ebd0be6cd1"
 
   bottle do
-    sha256 "fecc68642aa55935a287aeedc2bb9a2d4cc4b4636a0c5115d4758733959d9017" => :high_sierra
-    sha256 "e545a3be9a66e75aa8ae21cf4280783a7cff006c08187f92ba239fa15329c391" => :sierra
-    sha256 "69accbb26b98f014107d898cb9c354faf8c8a0be966a9e8d0266a160bf53987a" => :el_capitan
+    sha256 "58e9a00033aad2f146b4c6b1c183fd3ed5f5e34dc26bf0aea61e3368f7ac66e6" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
