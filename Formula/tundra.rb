e529296c91a59cfa40d908bff41d86bb6f926d6b
Michka Popoff <michkapopoff@gmail.com>
2018-05-06 11:07:35 +0200

    tundra: update 2.07 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7560.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/tundra.rb
+++ Formula/tundra.rb
@@ -6,9 +6,7 @@ class Tundra < Formula
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "ed4b8f4972973c2c1eb1ba4cc68523bb77fae42b9e05b32d7af8f2ee62c6479a" => :high_sierra
-    sha256 "f64f3d3677204d32b25c1819dc3cce4c61bc0a18e66256921862e8d472bc4049" => :sierra
-    sha256 "e0c6dd40d2efdcd58bb6976b924b51104e1435f2b93255894d766cca80ff894c" => :el_capitan
+    sha256 "a7dd8f873e984fb4d577632fd3732ab535e76569950c2fb7c1dafac1e9968a88" => :x86_64_linux
   end
 
   resource "gtest" do
