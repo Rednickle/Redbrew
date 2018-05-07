5f43e4c06b9c7ed937dc0d65963c31b8d4a34563
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-07 19:22:51 +0000

    pgpdump: update 0.33 bottle for Linuxbrew.

--- Formula/pgpdump.rb
+++ Formula/pgpdump.rb
@@ -10,6 +10,7 @@ class Pgpdump < Formula
     sha256 "2d5ad982f29c20cad30f5a90d4fcd8af3d369432e2c4ab4f35fcfa3b31712a1f" => :high_sierra
     sha256 "9c2ed5f4eb7e0c833a90d53fc8d96d613b781b36c3524959fa102ae62a4d167e" => :sierra
     sha256 "1cfd7cb5b0cdbc7e70031841d7efb1196ddbbd6f11f5af3cce4b38b6f7358ae2" => :el_capitan
+    sha256 "8aff5b4553f8db403d33fac7202a3d59cfab9f19bf6b630736eef81453c33913" => :x86_64_linux
   end
 
   def install
