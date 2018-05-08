64110175cf52ee6decadcd14b878940bafe6541e
Michka Popoff <michkapopoff@gmail.com>
2018-05-08 22:13:10 +0200

    odpi: update 2.3.2 bottle for Linuxbrew.

--- Formula/odpi.rb
+++ Formula/odpi.rb
@@ -9,6 +9,7 @@ class Odpi < Formula
     sha256 "4b2c0caf3da652eda4abfdf08615adc482d4ab05475e32e087e36b7beb863240" => :high_sierra
     sha256 "c6bd8bb09f912272b134e93c874213ee4857e27c3d1cb032ba66575ae9d2ba02" => :sierra
     sha256 "617d7483d60f14fa341b6952691c6a56d5a572b3133b71ed8a3de034cfa67521" => :el_capitan
+    sha256 "fdaf437497e486d434c0eb96060b0e39a69dff4369fb7b5516faf078a6aa4a7d" => :x86_64_linux
   end
 
   def install
