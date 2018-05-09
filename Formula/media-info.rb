e73e8488f131f916d4e0a155d510391f7b4ba525
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-09 19:27:24 +0000

    media-info: update 18.05 bottle for Linuxbrew.

--- Formula/media-info.rb
+++ Formula/media-info.rb
@@ -10,6 +10,7 @@ class MediaInfo < Formula
     sha256 "99bc2eae9ac0f372663c2ad2de1970f0d3bda860061240d6dd20a8eb8332b475" => :high_sierra
     sha256 "77c90fb595990beed8f5436e2dec309f065c5dddeacea648e2b8d7b081ead289" => :sierra
     sha256 "726ed97a08cfa889a344b5177ac76512f1a07d85bfaa06ab49bdefa287f54e23" => :el_capitan
+    sha256 "d4acda70d975bdd19df0acd88cb0dfaf0fb5be00ea330c48bd4c0fa958657757" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
