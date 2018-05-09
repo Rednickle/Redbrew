fff0ae91899dd0bb34150356d06c77c785ed55c1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-09 20:20:53 +0000

    mpfi: update 1.5.3 bottle for Linuxbrew.

--- Formula/mpfi.rb
+++ Formula/mpfi.rb
@@ -9,6 +9,7 @@ class Mpfi < Formula
     sha256 "cf0a5912aafe1a39c596d9303f12361e403f96bd52c25db6aaee1dd9a6139360" => :high_sierra
     sha256 "81e48e4445dbe6e7fa4ede18ba9c5a7a75c8816d3b0268333d4535afa4b5e7cd" => :sierra
     sha256 "9345063d6fdde9da2d16c233251300aa563668ab17675dd2dc11e4c75de88fb4" => :el_capitan
+    sha256 "92a7465e0236689443a0c66fd2ba39795a79c3c5eda08c2fa36ebc5286ddce48" => :x86_64_linux
   end
 
   depends_on "gmp"
