ee278592a7ba72000d2e85ba1a25fbd057b1b64b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-11 19:19:41 +0000

    byacc: update 20180510 bottle for Linuxbrew.

--- Formula/byacc.rb
+++ Formula/byacc.rb
@@ -9,6 +9,7 @@ class Byacc < Formula
     sha256 "24b76c770a8ad4bc95c87a7429dca550803f72d8c78bb72017a0732cc1e675a3" => :high_sierra
     sha256 "82f7e36647c1161ebc189c82ccca0416eaced470ae61ce9da5ac2aff84720f3c" => :sierra
     sha256 "9b5bbfdae8bba9f981d1586ce618a6c5ea9726006c75df0b7a9a68c3d9a6e757" => :el_capitan
+    sha256 "2ef08d5cb16fd49b14b78c079ad118fcefeebdf48bb5820b3280a05bf148e4d8" => :x86_64_linux
   end
 
   def install
