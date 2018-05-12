758ff4d764eae2710fd53fc97e6941f1faf98503
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-12 09:09:27 +0000

    fstrm: update 0.4.0 bottle for Linuxbrew.

--- Formula/fstrm.rb
+++ Formula/fstrm.rb
@@ -9,6 +9,7 @@ class Fstrm < Formula
     sha256 "b6c9ae4e8e88d65b3e7f2caac619b3aec15d6b50ed670a2a6cf61f2cab919ab7" => :high_sierra
     sha256 "c8b5b02ccac2d87c90cb33a686e428e1263c0ae19a2e6cf4847f0b7780110ee2" => :sierra
     sha256 "c18f60d06f6965bd0dbb7d063d61145461be190bad3503b693381d78a432c156" => :el_capitan
+    sha256 "65f1357b9e939ae742b39a3a56bb68c16333276b5714602992ef739275012d46" => :x86_64_linux
   end
 
   head do
