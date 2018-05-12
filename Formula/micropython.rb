70a81c753c2cbf74bc924f6e7a3975903ce51d0f
Michka Popoff <michkapopoff@gmail.com>
2018-05-12 09:57:03 +0200

    micropython: Fix build for Linuxbrew
    Closes Linuxbrew/homebrew-core#7637.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/micropython.rb
+++ Formula/micropython.rb
@@ -14,7 +14,7 @@ class Micropython < Formula
 
   depends_on "pkg-config" => :build
   depends_on "libffi" # Requires libffi v3 closure API; macOS version is too old
-  depends_on "python" unless OS.mac?
+  depends_on "python@2" unless OS.mac?
 
   def install
     cd "ports/unix" do
