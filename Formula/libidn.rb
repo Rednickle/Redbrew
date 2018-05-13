20abfe4c706fec6a7761e72147ef9b732a694bf5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-13 12:33:01 +0000

    libidn: update 1.35 bottle for Linuxbrew.

--- Formula/libidn.rb
+++ Formula/libidn.rb
@@ -10,6 +10,7 @@ class Libidn < Formula
     sha256 "94e508f0b66a0c37c7a05dad6fff49542439a05cf913980caff4142e58ae998f" => :high_sierra
     sha256 "ddbdf99759b5bf8840afdf4b50de61b3770fab70d26e08c0cd54c8df54c7da7d" => :sierra
     sha256 "11f95f7689146cda4ee3960295cd39f9ec5c613871e35c27d0260c7bd8eeb199" => :el_capitan
+    sha256 "6cd5a84f577a405d27ed9a90338eed72fedf37cbd26865a51e34670d32ef6553" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
