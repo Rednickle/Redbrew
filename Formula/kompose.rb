0ea752e3fa255d768e12d614558257a961045261
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-15 06:06:49 +0000

    kompose: update 1.13.0 bottle for Linuxbrew.

--- Formula/kompose.rb
+++ Formula/kompose.rb
@@ -9,6 +9,7 @@ class Kompose < Formula
     sha256 "fa3368daae3ea18a12d78d785cacbec1d6c576ea55a3315eebd39ad32f7ce0b8" => :high_sierra
     sha256 "dbfa58fede1ada8624322bceffd903bf6e41b8ab1bfa1c66587e66e37859f552" => :sierra
     sha256 "5801f974f26687c7123af27792c2d2afe9098c6f2ed7513bcb3f4a142b8eb463" => :el_capitan
+    sha256 "244a63f9bb5cc2e59b354b41752158317af18ccc5f80c9cdd5af994814c03548" => :x86_64_linux
   end
 
   depends_on "go" => :build
