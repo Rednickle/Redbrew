1a6159a74dd98c5895c525b6e2191a78a96d8fc9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-15 06:06:51 +0000

    annie: update 0.6.12 bottle for Linuxbrew.

--- Formula/annie.rb
+++ Formula/annie.rb
@@ -9,6 +9,7 @@ class Annie < Formula
     sha256 "ef8506c979fa1b1ab3b2d0ade30e35b6873870aab94d5ba9bad14288d4dc60d0" => :high_sierra
     sha256 "bfcc7a9d6168473923606baf6f82dd578f25e6112cf514eb85c8309f73ee54b4" => :sierra
     sha256 "d93f6de6fee1eaca66cb95d0fb32a7287ac067fcf137b4fd3aa22c5b0bade72d" => :el_capitan
+    sha256 "acd7d778a4c9d96ab37e69623e75fbdaf65002465ffce152648a86a0ee5917cc" => :x86_64_linux
   end
 
   depends_on "go" => :build
