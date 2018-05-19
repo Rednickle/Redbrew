d7f69b8108b233f5a9ba5bcf7d955f85171cd61b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-19 12:05:41 +0000

    diff-pdf: update 0.2_30 bottle for Linuxbrew.

--- Formula/diff-pdf.rb
+++ Formula/diff-pdf.rb
@@ -10,6 +10,7 @@ class DiffPdf < Formula
     sha256 "9ee3f39da8c432adacee20454ebb57c09cb3a1cf4241b1d7b427a23bb0a40b42" => :high_sierra
     sha256 "101f7e6636915bbecfa2e6258beb8b8ae5a784d79dbef934bdd3394efa80e99c" => :sierra
     sha256 "138baf9a0dc5e9793a113e9dc8de3a3fb6a0a0c28e52df5911d2694112c9bbdf" => :el_capitan
+    sha256 "c4f5d235ee76c2ca5b3570b5aafb339c096c8bf8f0233dba7373ffcbcf26b637" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
