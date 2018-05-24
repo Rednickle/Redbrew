950312332998205d4d3c95619ce40077aedf32db
Michka Popoff <michkapopoff@gmail.com>
2018-05-24 22:20:37 +0200

    nativefier: update 7.6.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7817.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nativefier.rb
+++ Formula/nativefier.rb
@@ -11,6 +11,7 @@ class Nativefier < Formula
     sha256 "adf3d341d9e7cfd79ed3ba6b0e6a7149f07edb73b66ecfa6452a9b0ba26c906c" => :high_sierra
     sha256 "b48e332714a4598c2a4b6380545ec520538fcfa65878ec3ab2cceae4ee931d4d" => :sierra
     sha256 "0fb9d888ac4baa5f60fb70c9e0b75e23dc104b904cee7a457e09edb5dea7c4e3" => :el_capitan
+    sha256 "494d9bba288ffe8f558bef3664a5a9e19e4cf51f52b0c6f3f30d1fa7ee264296" => :x86_64_linux
   end
 
   depends_on "node"
