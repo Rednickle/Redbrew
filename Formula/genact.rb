633b3514ce087bf7cff37a88e5cdaafb69a0b447
Michka Popoff <michkapopoff@gmail.com>
2018-05-24 22:47:51 +0200

    genact: update 0.5.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7822.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/genact.rb
+++ Formula/genact.rb
@@ -8,6 +8,7 @@ class Genact < Formula
     sha256 "87d766593e0414423a19d44e2fcb390f1962cc28d936e572c04207fc8ae24dc8" => :high_sierra
     sha256 "a91317384a098631c4393858027f79d96515e611400fa850a50ec29898d5a85e" => :sierra
     sha256 "ea6da90fb396dd9c690c0d9db851fd91af49c19873c6e4aba047baa26921135b" => :el_capitan
+    sha256 "903263ee5c25d6e73c5b8aadd8defc4f9a13ecc71113106ff29077ea6f281ed0" => :x86_64_linux
   end
 
   depends_on "rust" => :build
