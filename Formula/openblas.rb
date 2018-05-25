99378ed6930074b0fd6bba61b5787c0149133d49
Michka Popoff <michkapopoff@gmail.com>
2018-05-25 06:57:32 +0200

    openblas: update 0.3.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#7824.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/openblas.rb
+++ Formula/openblas.rb
@@ -9,6 +9,7 @@ class Openblas < Formula
     sha256 "895e681d843b6613e0a1328a212cae2d6152dea94629a545b6a4af64c181e061" => :high_sierra
     sha256 "ec229c12ff1beca5b659fa751e347b470085bb52c2731d908da548ff4381e86f" => :sierra
     sha256 "763e80edbd88cc75c96057a75198ce0e643d52f607399dec71432e1338a7a865" => :el_capitan
+    sha256 "1368ba2dd714b60fb3c549016368ca6f460fb7745294007c22e80cc0d31b25de" => :x86_64_linux
   end
 
   keg_only :provided_by_macos,
