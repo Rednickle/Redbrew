b3f93bfa74971cfaacdaa1493db247a0d56c4247
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-25 20:11:51 +0000

    angular-cli: update 6.0.5 bottle for Linuxbrew.

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -10,6 +10,7 @@ class AngularCli < Formula
     sha256 "283af0df30e42cbb1bcce1efc8cdd20eaf05cbe767e7d9e7a17d701e2aa048ce" => :high_sierra
     sha256 "2b682b5428e1ccd6b869868e25eb6225f5d85bd48eafa30c25f0c4663eac3f66" => :sierra
     sha256 "880b3ba23e81c70bee66d4e89dedad5e509a6a0d139b13e4f6e724eb56df8e1e" => :el_capitan
+    sha256 "4ad3530ef2b436638854b5c6e22afd186f03de89fd6bffc32d74947ea469e201" => :x86_64_linux
   end
 
   depends_on "node"
