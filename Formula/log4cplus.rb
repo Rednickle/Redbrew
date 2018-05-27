07748c03255703028a8371497d1a979d31ce5a2c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-27 17:50:02 +0000

    log4cplus: update 2.0.1 bottle for Linuxbrew.

--- Formula/log4cplus.rb
+++ Formula/log4cplus.rb
@@ -9,6 +9,7 @@ class Log4cplus < Formula
     sha256 "eacdee3aa723c125f98c7c5d548b918711dcdbc495694607e72b3b497310f3c1" => :high_sierra
     sha256 "4508ad12bb420fbfc24554c874a2d232353984aeba4c44f107f0173e54bbc0ec" => :sierra
     sha256 "91cf1b0b9bd6daae6bd30c26ae7eeecc589e0ca236c07076737de1e41285ade9" => :el_capitan
+    sha256 "f056c3a0ef8682b1f8fffc972e1d39afe9542d19e21e1b1515096188e808724d" => :x86_64_linux
   end
 
   needs :cxx11
