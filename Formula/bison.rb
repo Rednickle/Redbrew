c47d65e3848190faa8e1e89173fb933c5c012abc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-28 19:49:08 +0000

    bison: update 3.0.5 bottle for Linuxbrew.

--- Formula/bison.rb
+++ Formula/bison.rb
@@ -9,6 +9,7 @@ class Bison < Formula
     sha256 "05b65293f4429cafef03ec3011e0edf90c4b3399fd6c917fe6e21a649c96ef66" => :high_sierra
     sha256 "06481bf85b252a3b362523d67b10b0e910cf353dccec93b28fb6e62e2bf0cd8b" => :sierra
     sha256 "19c9c94ff3e215c60be1d4c85ee2f385528760cbf9b1348f62e21e7dfb5f49cc" => :el_capitan
+    sha256 "1e5b776c487c724ab682dacc5d12406d810ec4f25a2133d0816369764b639ec9" => :x86_64_linux
   end
 
   keg_only :provided_by_macos, "some formulae require a newer version of bison"
