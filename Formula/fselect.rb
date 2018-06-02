1d05ce8a1c3515097e74f9e56f4aec6b0348dddb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-02 10:48:38 +0000

    fselect: update 0.4.1 bottle for Linuxbrew.

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -8,6 +8,7 @@ class Fselect < Formula
     sha256 "c9e2a8232f2e2a0cf90605d6bca20fa2557161a5705002abc1f9604098052ed8" => :high_sierra
     sha256 "aeaa8e4491997ce983d3a09b960fb73e841c6c9e450bbe673ed8a4d529e76cab" => :sierra
     sha256 "4fede4a9a9fb5208bba40e3f70594c7a0c4a8e2f268f8aac46d2153a7b80fe2f" => :el_capitan
+    sha256 "cf004c0158845a2e9dc48493bad29577bb6a3a7fcf0be6501121141937900acb" => :x86_64_linux
   end
 
   depends_on "rust" => :build
