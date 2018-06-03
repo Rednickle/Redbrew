f566bc1928556d0f971bc70385e10768e2029c16
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-03 20:32:08 +0000

    nano: update 2.9.8 bottle for Linuxbrew.

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -8,6 +8,7 @@ class Nano < Formula
     sha256 "1b7f5f7842d849709b209d48346bc2dc502b538eeee460ef63513775d82f70ec" => :high_sierra
     sha256 "0f14e7427708f6105143a6c03f962bf047f7162adfc1a9a7f67749d13a42614c" => :sierra
     sha256 "f2ec710b129cb095c1107a53c632c5009f8ecddb53db4b4b95eb08edca0037ae" => :el_capitan
+    sha256 "37e445b997f88663cd1279ed41fe5c7c6c929e340a0ebcb7e6e29b1f468da261" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
