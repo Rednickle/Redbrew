3ff65823627f9f964a53808278dd7da64430b6b1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-05 17:25:41 +0000

    jfrog-cli-go: update 1.16.1 bottle for Linuxbrew.

--- Formula/jfrog-cli-go.rb
+++ Formula/jfrog-cli-go.rb
@@ -9,6 +9,7 @@ class JfrogCliGo < Formula
     sha256 "bd5953f911e053189b4dec13f732857a9d4a6eab14ce2114e375a66f2ce7c0a3" => :high_sierra
     sha256 "70875a28c30127a9c97cd1f7055751539cff042abf678890c9ded8cd168e80bd" => :sierra
     sha256 "a5c18ace45a2f8fd9f3f22f7cdff41ec9fdcd8e9a075fcbda384baf5cfcfc2dd" => :el_capitan
+    sha256 "101a60c627004fea43d7aea5948188e267435b5d62d684eb0f2f18570f41964f" => :x86_64_linux
   end
 
   depends_on "go" => :build
