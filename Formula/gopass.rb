330c3221001c411a887a325d5f135ce40f53d3b8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-07 11:38:10 +0000

    gopass: update 1.8.0 bottle for Linuxbrew.

--- Formula/gopass.rb
+++ Formula/gopass.rb
@@ -10,6 +10,7 @@ class Gopass < Formula
     sha256 "03af544c1fbc4ab575f6eb8817cfebdf1c769deb09d5fd297c3d2a3542beff8b" => :high_sierra
     sha256 "7dc01d3a42ae35562b13b32e19121baca20971d764b9fe0865b02e4a5b0a07d9" => :sierra
     sha256 "3f842a2a034dd1c673c5d9b917580751fb1a75f45f589121e00348587e6cc58b" => :el_capitan
+    sha256 "9c64cb46fbe4fb49f36ac6390fed8e605e3f50d08f0beac87087adec2a06cd56" => :x86_64_linux
   end
 
   depends_on "go" => :build
