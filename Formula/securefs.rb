a88df20eced4d1c8c706b7558d9ca3e6d851c3be
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-08 17:52:42 +0000

    securefs: update 0.8.2 bottle for Linuxbrew.

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -10,6 +10,7 @@ class Securefs < Formula
     sha256 "d851b6a0987ac11b3ff5a6ede8b88bfa9e09251ef1fdd76237ad7e9910688242" => :high_sierra
     sha256 "13d67e18b133621ddb49ddc5b34caff453aec85d95607d2ab83f03cdf0c79b76" => :sierra
     sha256 "f42c0d8e15ddcd2212235b4954e8ce0896e8f3fc1f07c388cfcf49dca96888f3" => :el_capitan
+    sha256 "a39c8bb5e026edb3edf556c154d622ed6d3da7e64c8a497e45ba119d5f0680f4" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
