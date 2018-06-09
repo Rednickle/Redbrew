4e20e21d01b97f7a49dedd3f09594b76c4ccf957
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-09 12:58:10 +0000

    shellcheck: update 0.5.0 bottle for Linuxbrew.

--- Formula/shellcheck.rb
+++ Formula/shellcheck.rb
@@ -15,6 +15,7 @@ class Shellcheck < Formula
     sha256 "372c28d50b8cee2b31a6c63c308c7ca8ecc9db5d0bb0ec235ed3295780762d92" => :high_sierra
     sha256 "ff3f24bc8d38042ead62820906b9907c3bd7fd3240ab5b309f579a0984a2af2d" => :sierra
     sha256 "3e82106d209775ec04a0a85bf99961d392a4ea22bc41644122959e5ca0798a25" => :el_capitan
+    sha256 "8b9ff234f16ec2cfba3e542784a6305dc9c2605ec01d774bf8e0e85e55d9c795" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
