decc5426913b4d9e70905b53f18ba2ea5024def3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-11 02:39:00 +0000

    cglm: update 0.4.7 bottle for Linuxbrew.

--- Formula/cglm.rb
+++ Formula/cglm.rb
@@ -9,6 +9,7 @@ class Cglm < Formula
     sha256 "4fd398f7eee4f84aab35a0adcb4fff189f63efd2b35b0f85d67075e48fff6775" => :high_sierra
     sha256 "6d9450442f80bc905713142d8d97f67f93bd94a464f8fee7d8773fefaf128444" => :sierra
     sha256 "cc2a2d0c0d630290a63d5c46a76af23a8fa5c759133dfe7853160a7356f24db3" => :el_capitan
+    sha256 "b8b0128135b60075100b542e2872da18b3f05febaebee698ca5ed8d2d9b3609e" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
