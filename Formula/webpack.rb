b2b11a9e26f340105490abf8aef3829f20410f1e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-11 02:38:58 +0000

    webpack: update 4.12.0 bottle for Linuxbrew.

--- Formula/webpack.rb
+++ Formula/webpack.rb
@@ -12,6 +12,7 @@ class Webpack < Formula
     sha256 "1552a3d7399401e6f134f98e86ff4d9403b51f8ba4da5cee101db1c56c1cae02" => :high_sierra
     sha256 "47fe41d11465133e7b78ec71e43977f114a18f74f34236e224c1ef374b7a6536" => :sierra
     sha256 "fb01401795f233248a4f2a1505dd3f27ef268aca67102f1233dc8dcd73f64637" => :el_capitan
+    sha256 "c93195ee15cc74860d9a509eb28a2ef119f98ee2b273270c34f3c02ff89ceefa" => :x86_64_linux
   end
 
   depends_on "node"
