234e9242349263c3cc62ff687ba430015c2a621e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-12 17:52:04 +0000

    goreleaser: update 0.77.2 bottle for Linuxbrew.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -9,6 +9,7 @@ class Goreleaser < Formula
     sha256 "31a39769408464b31c3237093ad6694a4e003691aa547de79d87e0d73f242994" => :high_sierra
     sha256 "806b62effaec19eba19063d2407a25ad1ccbaa4b90b514dfeb8fbdbd0b58be4d" => :sierra
     sha256 "1f65fba8be7c0138a4cbaaa7c6b9185b44eede51dddea4d8d2ddeb75a63c6ffa" => :el_capitan
+    sha256 "3e1815428835456e30cbd057e09aaa613b58b0fb0d08a4419db3ccbaca522fd6" => :x86_64_linux
   end
 
   depends_on "dep" => :build
