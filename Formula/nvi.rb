4fca69723c1ede00fd91b6d33038c403ec8427aa
Michka Popoff <michkapopoff@gmail.com>
2018-06-16 22:17:21 +0200

    nvi: update 1.81.6_5 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8044.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nvi.rb
+++ Formula/nvi.rb
@@ -11,6 +11,7 @@ class Nvi < Formula
     sha256 "dcaa5dd43e6edfc5c8188761cc8aad6b80a06abc7382b8ceac4d92498354b5c4" => :high_sierra
     sha256 "1327ea05ec82ec05e9ec7b00b95ac3f7329b198a613385042a0814265b393f13" => :sierra
     sha256 "e188b0a9fa040c6a11f7ed6338d28d96428e11cfa019aaa1d0aa69e0f2b87bc3" => :el_capitan
+    sha256 "a3a18b39e16e0bfd592b5f52a063973e38037f28e3ae3f35082da0e0dffdde19" => :x86_64_linux
   end
 
   depends_on "xz" => :build # Homebrew bug. Shouldn't need declaring explicitly.
