f17b553c3a0db26407f73f4e8433eb8900155ede
Michka Popoff <michkapopoff@gmail.com>
2018-06-18 18:47:55 +0200

    ansible-lint: update 3.4.23 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8084.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ansible-lint.rb
+++ Formula/ansible-lint.rb
@@ -11,6 +11,7 @@ class AnsibleLint < Formula
     sha256 "494790e006d6976bda7e7d2887a9edd5533b188218f4e33c95f392a31dd5e7a1" => :high_sierra
     sha256 "cfac21a2999fd81328589d5180aea8730314c252b7f48066e931d929002863fc" => :sierra
     sha256 "ecccace7e301d03b6e979a42cfe0d5c210ef5baebb10a7e683a5e7cd02dc6751" => :el_capitan
+    sha256 "cf5117b9be55257574676561e2de25be0ee407f1db35345ffc5e1a9270bda9dd" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
