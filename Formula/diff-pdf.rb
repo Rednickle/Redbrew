183e0474c6e7980af1b3286596afa2b6eff594b9
Michka Popoff <michkapopoff@gmail.com>
2018-06-21 07:09:02 +0200

    diff-pdf: update 0.2_32 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8126.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/diff-pdf.rb
+++ Formula/diff-pdf.rb
@@ -10,6 +10,7 @@ class DiffPdf < Formula
     sha256 "1c3f935573f83f451c6b584e357ee69bec9c5bafd00ff29324b4233bf776d951" => :high_sierra
     sha256 "c6b89e9e450202ad45152b5f6bb78416484ca62e7e50ebdc4803c90ca96e8b81" => :sierra
     sha256 "3f54a8562101f2e5377b1dd89f466578a94ad0f28e441de34a07e3da09662171" => :el_capitan
+    sha256 "525720915c4306c395e8845b6c8fe486be045211ebedfb5546a4f817cca56db1" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
