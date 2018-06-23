f03cbf3e65e0c6febbaf680bb0d61defa0412430
Michka Popoff <michkapopoff@gmail.com>
2018-06-23 15:43:30 +0200

    nss: update 3.38 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8178.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nss.rb
+++ Formula/nss.rb
@@ -9,6 +9,7 @@ class Nss < Formula
     sha256 "5313d2e11cd6c25cbbb1234667b27bf5a2d144d7a47d0ef9f1eadf62a616f3b4" => :high_sierra
     sha256 "4a6fc7b79a81f24cb00f320c6912b24e4f5e9aca6ec8d4edc3d30e36012a264b" => :sierra
     sha256 "7a82236aadb98b6ee65699d369d64138d8e5dd016a84a03c976d6aae16dcb247" => :el_capitan
+    sha256 "7afa048c5b03cd145c61d6acab8bff11d21b1b4ebcbc7b945de5ba4336db5b5b" => :x86_64_linux
   end
 
   keg_only <<~EOS
