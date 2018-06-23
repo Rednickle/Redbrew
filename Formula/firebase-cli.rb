565300dd870ca1d24b0bbc4fb35e0b3463c06420
Michka Popoff <michkapopoff@gmail.com>
2018-06-23 15:36:36 +0200

    firebase-cli: update 3.19.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8173.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -12,6 +12,7 @@ class FirebaseCli < Formula
     sha256 "cf42e09d8a818233cb38e0888dec3d713d76bbf8d7c5f8179bd97bde8018c1f2" => :high_sierra
     sha256 "d4ef3ace1666b50f8194600d13ce326b02cbeac91cd8c51531689212c52eb346" => :sierra
     sha256 "d5064dce53a5eb6e84f7f7c7cdedab6a590aeffb574e0608124aea941907970b" => :el_capitan
+    sha256 "66228e132a85f0aee2b4131405da3085e230265278febf8755eb51ddb71a5550" => :x86_64_linux
   end
 
   depends_on "node"
