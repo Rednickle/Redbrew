f4da9278afdabdb28a863ae828c4dad04556dfa4
Michka Popoff <michkapopoff@gmail.com>
2018-06-24 21:44:49 +0200

    grpc: update 1.12.0_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8136.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -10,6 +10,7 @@ class Grpc < Formula
     sha256 "614a3e4328d88928290cc8ce9e89405b18843f3b9be9bc1d9ab126ebdf0c941c" => :high_sierra
     sha256 "edd04139bdb23bd823ee3ba8b29e14eba955ce22f389c0468f14caba296f374e" => :sierra
     sha256 "cad4604da47849ae73bbc49a66f926dbd78a76a45805b67612cc5e7030bc9e20" => :el_capitan
+    sha256 "f1b5e857ede86cb577ab72b85b2305968befe2b661177a6d97452f60f7267987" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
