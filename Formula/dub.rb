aa8d7a49195eb9136f51566ff86d6a1b645e3d2e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-07-05 05:33:25 +0000

    dub: update 1.10.0 bottle for Linuxbrew.

--- Formula/dub.rb
+++ Formula/dub.rb
@@ -15,6 +15,7 @@ class Dub < Formula
     sha256 "502b6a1a71ad2011885eaf6172e20a5cf626e16c201357bb1072aab7f9b77ed7" => :high_sierra
     sha256 "18799640f91b1210ccd5d1349f34c1c91b32cf3603bc2ea442b68ce0694f74dd" => :sierra
     sha256 "30ae62ba7e76e4ec3e73e5454a3d96bb607bddee02af6a1e7644b077940dd52e" => :el_capitan
+    sha256 "38c8cc2a0d11457efc21b7b3b8ea010b5f1de6c98cd85e192b51ca24f20ffa96" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :recommended
