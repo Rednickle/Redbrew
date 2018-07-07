d2a27876502e2f248bbda01b7daa00f7fd9a23e2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-07-07 13:30:32 +0000

    ephemeralpg: update 2.5 bottle for Linuxbrew.

--- Formula/ephemeralpg.rb
+++ Formula/ephemeralpg.rb
@@ -10,6 +10,7 @@ class Ephemeralpg < Formula
     sha256 "fa778995e1b3d3adb26a3ebd0a584376dd85e5239cdf8417643cb2984040bb6c" => :high_sierra
     sha256 "23c036094f518eb3d98539e410ea95bbe237bda31b205f3741fb46bb7d5e32c5" => :sierra
     sha256 "fe44d3d4814322c408e59944d86d56214c6056547111957efdbf08e382d4672b" => :el_capitan
+    sha256 "4725e8fcb7d8eb091480dce812d37711f783153196f88ec05f48776e7d851d2f" => :x86_64_linux
   end
 
   depends_on "postgresql"
