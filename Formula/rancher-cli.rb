72aca0279892da771049c583df4998e8a6d93348
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-07-08 12:27:26 +0000

    rancher-cli: update 2.0.3 bottle for Linuxbrew.

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -9,6 +9,7 @@ class RancherCli < Formula
     sha256 "af1afea63264a1e98f606c974d733319ed7cfb735dd61ed6ac0df114cfa7994e" => :high_sierra
     sha256 "7e44d5d08413e49d9e1f53fcc8d469a8b4f908c2c4be744c86fdcc231285cb69" => :sierra
     sha256 "80d5d295f4b4ea2d4535d970cb32d0e6efbc633ca84e9121cf24b0f2ee131947" => :el_capitan
+    sha256 "bd294ffc067300b91c1eb9e3ea2477d00b4c5fb5243d3ac2511b7f3cad564cdc" => :x86_64_linux
   end
 
   depends_on "go" => :build
