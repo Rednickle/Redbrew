b84444fa78eb442c91b3fe055bb9237da317bc87
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-07-12 19:39:22 +0000

    wiredtiger: update 3.1.0 bottle for Linuxbrew.

--- Formula/wiredtiger.rb
+++ Formula/wiredtiger.rb
@@ -9,6 +9,7 @@ class Wiredtiger < Formula
     sha256 "4afe18e05ee55cf263b57026e8fecaba3084ef76dd92b5aa1620c6eca85948e2" => :high_sierra
     sha256 "e040bb1adc571f9f8d0929898dcf326bb97572fbe4bc9c52810e70b9d7e75afa" => :sierra
     sha256 "b141f6e07a7f6d0a1e1b7db637eb12fe908ded0a75b2415ed5f1b90a937959f6" => :el_capitan
+    sha256 "ee043d81948fd892ad7554a1ee10d3271ac6af8ef33809a0224b4651ce76aaa9" => :x86_64_linux
   end
 
   depends_on "snappy"
