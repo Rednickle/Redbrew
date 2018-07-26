b1f3d8397235e4a30f9a96876cc800506244083f
Michka Popoff <michkapopoff@gmail.com>
2018-07-26 12:23:39 +0200

    babel: update 6.26.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8476.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/babel.rb
+++ Formula/babel.rb
@@ -10,6 +10,7 @@ class Babel < Formula
     sha256 "283507db612687957e8855de052f97ebb8eafa9559f7edab627cb334012743c8" => :high_sierra
     sha256 "c4e2ca0a3d4f9d77b4d6c78c3982b333a8751fa36e5e0ceb506ef3d0e207c4f2" => :sierra
     sha256 "79ed0bda434eccc0d634407fcafebc419f529246472a807d16fb05739b9dc4f6" => :el_capitan
+    sha256 "bcb5092908955b9efacc48f47f98876ef75984fc170ddbde1c2d4ba837cdde87" => :x86_64_linux
   end
 
   devel do
