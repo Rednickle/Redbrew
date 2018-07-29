f0eb05e2b6b8b69cd0ba4ab6df450be409f970d4
Michka Popoff <michkapopoff@gmail.com>
2018-07-29 11:37:38 +0200

    libfixbuf: update 2.1.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8465.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libfixbuf.rb
+++ Formula/libfixbuf.rb
@@ -9,6 +9,7 @@ class Libfixbuf < Formula
     sha256 "8c0d314feb7ee75cfe4ef108d15285e6da0fdc7d3c5bcf0dd2a44c059af42bda" => :high_sierra
     sha256 "1cd8bd7f2552f30c27120a4a7ba803a2cc95e6b4d356e5489eda61b59ded001c" => :sierra
     sha256 "92f84cb8a1c4541693ff62533464fe2d04ff6b92c930cb4bb84eab6bac33fedb" => :el_capitan
+    sha256 "88afe32a100e21f81ec5f9c5e05cff34e2b6f19e1b8710fde57abb19356d85db" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
