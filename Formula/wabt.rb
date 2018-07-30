b813f0baceacd269fac07e8c2511f5ca703ed33c
Michka Popoff <michkapopoff@gmail.com>
2018-07-30 21:32:40 +0200

    wabt: update 1.0.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8616.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/wabt.rb
+++ Formula/wabt.rb
@@ -9,6 +9,7 @@ class Wabt < Formula
     sha256 "5465305c722c45351b3f5b883c4fba1789f14316dd2e34bee34ab913b0ba2125" => :high_sierra
     sha256 "9d196438eeb0a45d26ebb080d9f3fe0d3b13662d30f0167ba12f8db2debac15c" => :sierra
     sha256 "0f4ff53ff017c470baa1ab2f97d201a73d423e02289a3a20b5e990bc1c7def20" => :el_capitan
+    sha256 "416053a02559158f6c0e26ab8a4e4578fc11afad8551451e4c338917d6a5287e" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
