6279be0309eb5efd9fc419bb43e1671d7cb0a279
Michka Popoff <michkapopoff@gmail.com>
2018-07-30 19:56:56 +0200

    dscanner: update 0.5.8 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8562.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dscanner.rb
+++ Formula/dscanner.rb
@@ -10,6 +10,7 @@ class Dscanner < Formula
     sha256 "82f4f4431c569e21ab2d340c749fbae7f65a09dfcbc29822d7599ed9eff5c4d8" => :high_sierra
     sha256 "9191d8114416f8629e8ad43c1676c90e15f866092c1cb8cea7edd15e8e03c22e" => :sierra
     sha256 "2437e819fac2bfec60117af405ec3df43fa7ae29a71b22b6e5795c0b627d5d5e" => :el_capitan
+    sha256 "5b968b2cfb5e897f02ca2f23372de3e86c2c5b655fd6fc5bb3e0b9c95272927a" => :x86_64_linux
   end
 
   depends_on "dmd" => :build
