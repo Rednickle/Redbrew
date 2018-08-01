8d7c5bf93d1f9ee6b8609d3f7144abe0fbcf6223
Michka Popoff <michkapopoff@gmail.com>
2018-08-01 08:29:35 +0200

    distcc: update 3.3.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8630.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/distcc.rb
+++ Formula/distcc.rb
@@ -9,6 +9,7 @@ class Distcc < Formula
     sha256 "ba97d2888c977f6a5d51e690cebe27362ebbbef81767c4f349c41d7d9e822a19" => :high_sierra
     sha256 "90ee0314ef321c86ed4872f42043a02c385d95e0cb1f0fa4b14ac5877e12b07d" => :sierra
     sha256 "c13dc99f83f891e47906851ed16c2095bb2a3e72f0f320aae0ff53dc23fd3afa" => :el_capitan
+    sha256 "d49e1b500ca76597b5db546160be841731ddd25f0f91e24b91a02db187be4b2d" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
