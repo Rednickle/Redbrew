3dca595d3e45620f46c1dde37fc28067869406f9
Michka Popoff <michkapopoff@gmail.com>
2018-08-01 17:40:32 +0200

    sundials: update 3.1.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8707.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/sundials.rb
+++ Formula/sundials.rb
@@ -9,6 +9,7 @@ class Sundials < Formula
     sha256 "85d26a0fe45cc223074d986abfcdb70c06363a6cdbf5df8bff1f4f8a2ff7103d" => :high_sierra
     sha256 "9b679f316ad602b3b5057bb39a81b398f65bdae5fd7f80cb8d8ec3cba08cf7fa" => :sierra
     sha256 "2213f3ae580feabe0874cf229f39e9d9434cbaefe960f88616496a66a11512be" => :el_capitan
+    sha256 "1b60995f17aac7979b760b643f4f1d303befd1f5b914697e8bb999347243352f" => :x86_64_linux
   end
 
   option "with-openmp", "Enable OpenMP multithreading"
