bb5b44eedd8b443af09811bb640386c399351af1
Michka Popoff <michkapopoff@gmail.com>
2018-08-04 23:33:11 +0200

    dxpy: Fix build for Linuxbrew
    Closes Linuxbrew/homebrew-core#8729.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dxpy.rb
+++ Formula/dxpy.rb
@@ -14,6 +14,11 @@ class Dxpy < Formula
   end
 
   depends_on "python@2"
+  unless OS.mac?
+    depends_on "libffi" # for cffi
+    # pkg-config helps setuptools find libffi
+    depends_on "pkg-config" => :build
+  end
 
   resource "asn1crypto" do
     url "https://files.pythonhosted.org/packages/fc/f1/8db7daa71f414ddabfa056c4ef792e1461ff655c2ae2928a2b675bfed6b4/asn1crypto-0.24.0.tar.gz"
