e03972b8934ed52af2e289b8e76e1a3a0ad36dba
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-04 14:18:17 +0000

    go-statik: update 0.1.3 bottle for Linuxbrew.

--- Formula/go-statik.rb
+++ Formula/go-statik.rb
@@ -9,6 +9,7 @@ class GoStatik < Formula
     sha256 "df1233d8622a603cfc1376df589dde68eacfed61d936adb5ef5b76fc3f7498c2" => :high_sierra
     sha256 "a8d36c584b4ec26387492ad32470847890bc853734f072253050429d64430457" => :sierra
     sha256 "925e6aefe2b2a928cd79cdde8d7d840b52b4d9766daad7e331025eb2823f80af" => :el_capitan
+    sha256 "cbf3a4b4407d83a5831c8af00116815c65293ca1e1391b6bd1bbb07761467305" => :x86_64_linux
   end
 
   depends_on "go" => :build
