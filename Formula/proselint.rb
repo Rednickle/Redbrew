448b3d29d847630900e39e7ca942a27b7fbea938
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-04 14:18:16 +0000

    proselint: update 0.10.1 bottle for Linuxbrew.

--- Formula/proselint.rb
+++ Formula/proselint.rb
@@ -12,6 +12,7 @@ class Proselint < Formula
     sha256 "82c94cbcee61bff8f8caad4811aa43e3f94468711112453d015c4d22e863ba8e" => :high_sierra
     sha256 "dced8e141627ce6d735e24663839f1037f2b52e4a78945f5c6b9b5b735405146" => :sierra
     sha256 "9ae605c01455d630279ec13589d6c2577745a04ff02b6e1d63537e9a39976e14" => :el_capitan
+    sha256 "7d210427e1a5cb844bea1547c032ab217fdbecd0cc132af3074a0c4e3cccfdc1" => :x86_64_linux
   end
 
   depends_on "python@2"
