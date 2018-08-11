1564c6957ce28f8150082b3d4e95743e470d307b
Michka Popoff <michkapopoff@gmail.com>
2018-08-11 21:32:54 +0000

    qbs: update 1.12.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8592.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/qbs.rb
+++ Formula/qbs.rb
@@ -9,6 +9,7 @@ class Qbs < Formula
     sha256 "ff3f8252bae16a8144279d87ed0760cc5faa4db7eaf730515e4760eb7b7f4d21" => :high_sierra
     sha256 "127c523fbae8b0acdf68a638de8aed5d48868767aceb6b877f059e9cad2f1d85" => :sierra
     sha256 "d14ed282cb0db53f88b409186de31cf69ab22a577fa6778c61ccc43accd8786b" => :el_capitan
+    sha256 "de838b98b34fd32d597d3bb70e3eb3c4bf098435af038b0ac79982aa261f5a40" => :x86_64_linux
   end
 
   depends_on "qt"
