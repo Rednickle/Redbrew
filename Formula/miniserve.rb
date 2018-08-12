da0bdf40e28e7ea56f26543bcd2347fc9b9a894d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-12 01:33:03 +0000

    miniserve: update 0.2.0 bottle for Linuxbrew.

--- Formula/miniserve.rb
+++ Formula/miniserve.rb
@@ -8,6 +8,7 @@ class Miniserve < Formula
     sha256 "95064e1def8c354c4401f0082ffbd6956581a8b1b0bb65a00762b560c65f21ef" => :high_sierra
     sha256 "e5f0e8da39d4b8939a2de3662d944c3be2f86ecc51b8fb2b2ccc814684608e47" => :sierra
     sha256 "674c1b2e298092c866168ce996633b5130f084991810d9b74d2dcc6ab9adf38f" => :el_capitan
+    sha256 "f31cafa1d1c3405330d817cc43548d7e6fe7f539e9ee14f0662496ca51873c12" => :x86_64_linux
   end
 
   depends_on "rust" => :build
