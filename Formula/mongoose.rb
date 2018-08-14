2595064b429c99e67478dd618cfeb5af53f5220e
Michka Popoff <michkapopoff@gmail.com>
2018-08-14 20:02:59 +0000

    mongoose: update 6.12 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8871.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mongoose.rb
+++ Formula/mongoose.rb
@@ -9,6 +9,7 @@ class Mongoose < Formula
     sha256 "b55ab50af9bea08d026102370b6bf9b24d9d1926caa97dddf8a47520ff69d3ce" => :high_sierra
     sha256 "a71f64a6f888bb252486be06bc75bc02d5f7ec8dee799300fe49653ce914b4ce" => :sierra
     sha256 "e80290df08f6d557b0fe750c882de3d87c0edc2ee12853afd3628d06c26147f3" => :el_capitan
+    sha256 "1571a9f467b2cf0ffb2cd9e320b8cca9d38cc6a9fd7b7738281e3f32af924294" => :x86_64_linux
   end
 
   depends_on "openssl"
