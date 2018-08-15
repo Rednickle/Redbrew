5ac43606605d1870362e058c5977dcc24183bc60
Michka Popoff <michkapopoff@gmail.com>
2018-08-15 20:41:22 +0000

    lxc: update 3.4 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8890.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -9,6 +9,7 @@ class Lxc < Formula
     sha256 "c6df3c61a2e3d6345f5d219e0153be0c2bddea141101aa20cfb5649851a82c08" => :high_sierra
     sha256 "7584a5da68b9f3be6febf6e88c3353fc51a9d3d7d3ced74da1a8609cf2fd224d" => :sierra
     sha256 "49e32a3821d790f65ee7017ad40e0fa1a6a84af56f509d0e86cc17051c2a754d" => :el_capitan
+    sha256 "af1a75a3ee4739aaa647e94616f4e9bb532a24025c19d96aa87a871e52094b89" => :x86_64_linux
   end
 
   depends_on "go" => :build
