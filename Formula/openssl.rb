b079feb3619fb3d743906e3fdacb1c7da515862a
Michka Popoff <michkapopoff@gmail.com>
2018-08-15 20:38:32 +0200

    openssl: update 1.0.2p bottle for Linuxbrew
    Closes Linuxbrew/homebrew-core#8878.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/openssl.rb
+++ Formula/openssl.rb
@@ -1,3 +1,4 @@
+# openssl: Build a bottle for Linuxbrew
 # This formula tracks 1.0.2 branch of OpenSSL, not the 1.1.0 branch. Due to
 # significant breaking API changes in 1.1.0 other formulae will be migrated
 # across slowly, so core will ship `openssl` & `openssl@1.1` for foreseeable.
