2ef60c35cb46d808ea906635c74febe304501e91
Michka Popoff <michkapopoff@gmail.com>
2018-08-16 15:08:46 +0000

    libatomic_ops: update 7.6.6 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#8901.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libatomic_ops.rb
+++ Formula/libatomic_ops.rb
@@ -9,6 +9,7 @@ class LibatomicOps < Formula
     sha256 "321710f6a3a4446059ff63c8d609c46fdebbf1cb2270ffd0c812d93c2a55d246" => :high_sierra
     sha256 "83735d0d4dc412504092438a3ac7eb6efc17659b589ece28eb75aacba6d7eb97" => :sierra
     sha256 "af64bc9cc26b2222ecd233df18b908daf6de49a47c48505f2ee74858bfa499a4" => :el_capitan
+    sha256 "cc808d4c229e07cc523f4fab8c5761401ac8544634d3fdfc18ffa7b9bfb718eb" => :x86_64_linux
   end
 
   def install
