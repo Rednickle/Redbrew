adce62e41620af394937311230846ef205b765b4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-19 14:06:09 +0000

    flow: update 0.79.1 bottle for Linuxbrew.

--- Formula/flow.rb
+++ Formula/flow.rb
@@ -10,6 +10,7 @@ class Flow < Formula
     sha256 "f1adca4ae34e93a649946153f4dc7562a3915fc7fb22ff817e020e716fb0c880" => :high_sierra
     sha256 "72528fa1ab58593476c85c30556293e6cccc9e6d555d8d52ca277117cbfefb5f" => :sierra
     sha256 "92b91778d1ca773be0cc409714e9cdf60b6d8422539053780eb2c8f24499fcb2" => :el_capitan
+    sha256 "e05d013b27ba3608defd05e0af1080f1b5968fbe0f9a404f70df06d118222314" => :x86_64_linux
   end
 
   depends_on "ocaml" => :build
