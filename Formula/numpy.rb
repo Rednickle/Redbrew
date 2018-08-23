34a62334a7f119788335d44d08e77fd494f99d3c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-23 05:11:36 +0000

    numpy: update 1.15.1 bottle for Linuxbrew.

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -8,6 +8,7 @@ class Numpy < Formula
     sha256 "e26fdc4e56f65f5afd875156556c8975ddeda418e5b37727ab535f4b2dba088e" => :high_sierra
     sha256 "a15fc000cd2d90c579efaff1b5c7fe4f1377651dd8cf2712466619748c63d315" => :sierra
     sha256 "7d1c17824da21bf5c0555751e0721d4be9f3281b4791dcdd8f9c8f3aa83db818" => :el_capitan
+    sha256 "3e95de6a4aea7dd2290bb60b2c5db969c97fe3b9805261073365a7dc35806009" => :x86_64_linux
   end
 
   head do
