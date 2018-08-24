d04e5442ad9da1e3d04e6760b76fd9c969259906
Shaun Jackman <sjackman@gmail.com>
2018-08-24 14:29:53 -0700

    docutils: Rebuild the bottle [Linux]
    Fix the error:
    ImportError: No module named docutils.core
    
    Also fix:
    Dependencies with no linkage:
      zlib
    
    Closes Linuxbrew/homebrew-core#9048.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -5,6 +5,7 @@ class Docutils < Formula
   homepage "https://docutils.sourceforge.io"
   url "https://downloads.sourceforge.net/project/docutils/docutils/0.14/docutils-0.14.tar.gz"
   sha256 "51e64ef2ebfb29cae1faa133b3710143496eca21c530f3f71424d77687764274"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
@@ -12,18 +13,15 @@ class Docutils < Formula
     sha256 "c48f799c313e8c8008ced95c907c902284c289e7f1212ef96d8770f28dd56a01" => :sierra
     sha256 "823debb7f2b27abbd6e4deb33bcc6cf322c2f79d4b976262b852d95fad36c3dd" => :el_capitan
     sha256 "53e168f1335e9198d4948b2d89c434097b8878c835efeb2c6f0f116353212702" => :yosemite
-    sha256 "aaa9d38dce4d7fef184552c873bf67f118f059c699d7ea33b1153776d3727d79" => :x86_64_linux # glibc 2.19
   end
 
   depends_on "python@2"
-  depends_on "zlib" unless OS.mac?
 
   def install
     virtualenv_install_with_resources
   end
 
   test do
-    ENV.prepend_create_path "PYTHONPATH", HOMEBREW_PREFIX/"lib/python2.7/site-packages" unless OS.mac?
     system "#{bin}/rst2man.py", "#{prefix}/HISTORY.txt"
   end
 end
