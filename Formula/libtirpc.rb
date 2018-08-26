5f657560206083a0d495d52e821d500d08d386a2
Maxim Belkin <maxim.belkin@gmail.com>
2018-08-26 22:08:37 +0000

    libtirpc: fix audit warnings, add test block
    Closes Linuxbrew/homebrew-core#9094.
    
    Signed-off-by: Maxim Belkin <maxim.belkin@gmail.com>

--- Formula/libtirpc.rb
+++ Formula/libtirpc.rb
@@ -1,23 +1,40 @@
 class Libtirpc < Formula
   desc "Port of Sun's Transport-Independent RPC library to Linux"
-  homepage "http://sourceforge.net/projects/libtirpc/"
+  homepage "https://sourceforge.net/projects/libtirpc/"
   url "https://downloads.sourceforge.net/project/libtirpc/libtirpc/1.0.1/libtirpc-1.0.1.tar.bz2"
   sha256 "5156974f31be7ccbc8ab1de37c4739af6d9d42c87b1d5caf4835dda75fcbb89e"
+  revision 1
   # tag "linuxbrew"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "7c093d0948f7794df11cae0a0f0e974e8b0734d46beb2f7977db39d84aa16720" => :x86_64_linux # glibc 2.19
   end
 
   depends_on "krb5" => :optional unless OS.mac?
 
   def install
     system "./configure",
-      "--disable-debug",
       "--disable-dependency-tracking",
       "--disable-silent-rules",
-      "--prefix=#{prefix}"
+      "--prefix=#{prefix}",
+      *("--disable-gssapi" if build.without? "krb5")
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <rpc/des_crypt.h>
+      #include <stdio.h>
+      int main () {
+        char key[] = "My8digitkey1234";
+        if (sizeof(key) != 16)
+          return 1;
+        des_setparity(key);
+        printf("%d\\n", sizeof(key));
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-I#{include}/tirpc", "-ltirpc", "-o", "test"
+    system "./test"
   end
 end
