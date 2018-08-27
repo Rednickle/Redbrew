c122a4bfee4fa7684d0046c58a803e80210707c4
Maxim Belkin <maxim.belkin@gmail.com>
2018-08-27 16:16:54 +0000

    jdk@7: fix audit warnings
    Pull Request: Linuxbrew/homebrew-core#9090

--- Formula/jdk@7.rb
+++ Formula/jdk@7.rb
@@ -1,14 +1,14 @@
 class JdkAT7 < Formula
-  desc "Java Platform, Standard Edition Development Kit (JDK)."
-  homepage "http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html"
+  desc "Java Platform, Standard Edition Development Kit (JDK)"
+  homepage "https://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html"
   # tag "linuxbrew"
 
   version "1.7.0-80"
   if OS.linux?
-    url "http://ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-7u80-linux-x64.tar.gz"
+    url "https://ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-7u80-linux-x64.tar.gz"
     sha256 "bad9a731639655118740bee119139c1ed019737ec802a630dd7ad7aab4309623"
   elsif OS.mac?
-    url "http://java.com/"
+    url "https://java.com/"
   end
 
   bottle :unneeded
