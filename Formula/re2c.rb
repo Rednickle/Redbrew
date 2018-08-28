17d168794462dd1a149a8db5b8bc20044a2fba9f
Shaun Jackman <sjackman@gmail.com>
2018-08-28 16:48:28 -0700

    re2c: update 1.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9137.
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/re2c.rb
+++ Formula/re2c.rb
@@ -10,6 +10,7 @@ class Re2c < Formula
     sha256 "687cd0c2fb9d5b6c7993083eb70e5b4746c81c86f6b4f3912989569653f12c7f" => :high_sierra
     sha256 "191a467d5d45f6264bc5bf4db7ad9012cb7e91a06b303371cef5801871c218f6" => :sierra
     sha256 "2ce041b261549a7760303365af3300d41c0dd1a4f7e65177c04bdeed51447066" => :el_capitan
+    sha256 "63f480dcd9e6fd1c6717dac0757fa88e6f5bc0c5b7d3683c9342327e709b508c" => :x86_64_linux
   end
 
   def install
