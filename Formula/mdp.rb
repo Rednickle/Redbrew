786bc873eac97f1992b0382ab8c4739e3f6c4c3e
Michka Popoff <michkapopoff@gmail.com>
2018-09-03 04:52:19 +0000

    mdp: update 1.0.14 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9198.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mdp.rb
+++ Formula/mdp.rb
@@ -11,6 +11,7 @@ class Mdp < Formula
     sha256 "3cedd0d563f1993abfb31d9e9ccb5b6126cdc00f255f902f973a450f2bc003b4" => :high_sierra
     sha256 "659d3c92aa69a836f30bec57e3082c1a1666c51ddb84a15553ce2180e2c9eadf" => :sierra
     sha256 "97b65f395b1b0e303b66221573e619ffd518f465ef1cd67bd1c6af7d14928174" => :el_capitan
+    sha256 "24d68479231b86cbfc88bb44eb33e0d4bfd483cc6f9cfddf1db19333b0df6c00" => :x86_64_linux
   end
 
   depends_on "ncurses" unless OS.mac?
