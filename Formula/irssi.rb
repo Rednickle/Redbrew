debfc6c7b47f1e0fa0ccc68b8577c4501a261224
Michka Popoff <michkapopoff@gmail.com>
2018-09-05 15:39:19 +0000

    irssi: update 1.1.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9228.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/irssi.rb
+++ Formula/irssi.rb
@@ -10,6 +10,7 @@ class Irssi < Formula
     sha256 "d8a0ff73163c24d2d93bd4b030fa46d22ba7ecb4b27e0bbc604b1877e0298122" => :high_sierra
     sha256 "644e818a4e6718eea2ff0bebf5c8749aa3df552d4a24be6b109e95452b255942" => :sierra
     sha256 "8716dafbb1a1313d73edb9b9463eb063e59217eb7e32c3676eac05cc420ca42d" => :el_capitan
+    sha256 "bb355831a203e4a9b9f22c222ff827d8ffc2c7115674e9a0fed9501af49f2aee" => :x86_64_linux
   end
 
   head do
