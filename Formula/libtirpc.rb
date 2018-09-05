dc8512e667038bb2722285708bf17f46ec75734b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-05 22:04:21 +0000

    libtirpc: update 1.1.4 bottle for Linuxbrew.

--- Formula/libtirpc.rb
+++ Formula/libtirpc.rb
@@ -6,6 +6,8 @@ class Libtirpc < Formula
   # tag "linuxbrew"
 
   bottle do
+    cellar :any_skip_relocation
+    sha256 "8877610a23ff7fc029060517988cd0d07ad253448840bc6927ab5e6b381c3562" => :x86_64_linux
   end
 
   depends_on "krb5" => :optional unless OS.mac?
