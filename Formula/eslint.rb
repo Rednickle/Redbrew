79b7a1e87b9347103fe30da57558d7a8266db80c
Michka Popoff <michkapopoff@gmail.com>
2018-09-17 16:36:46 +0000

    eslint: update 5.6.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9413.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -12,6 +12,7 @@ class Eslint < Formula
     sha256 "c2c6415ac89b9f4740db79a9f3ebf9b0fcee6fe6face01927e140b281ff063c7" => :high_sierra
     sha256 "31c68f32b29e3c688cedcfa685e50c9806452110d72c7c7d3ddb18106fa6e6e7" => :sierra
     sha256 "0a70be324b639caf331920adc1537a7a286d085bf01dd29280c986dc8164c8d7" => :el_capitan
+    sha256 "b4efc6bd21a195e94f389992547370683ee1e56a31c0fadfc9d574a8faaaeb3c" => :x86_64_linux
   end
 
   depends_on "node"
