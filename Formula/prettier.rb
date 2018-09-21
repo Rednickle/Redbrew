7805e7279cc715f2a288f6f1dfb89bdd52fc1efa
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-21 17:58:34 +0000

    prettier: update 1.14.3 bottle for Linuxbrew.

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -12,6 +12,7 @@ class Prettier < Formula
     sha256 "49d81e2d36fd8d662a8b7f1eac8580fe705fab16daaf3c19cdf84ba5366dcea2" => :high_sierra
     sha256 "8f2446d1a727c56fe3df212c34481ce298f3101f714f752f472bff147310a98f" => :sierra
     sha256 "a0b468ee5bb4f5462f7d6f791c825396158fadbba20ccc8489e784d234398cfb" => :el_capitan
+    sha256 "2385ff61973f367d6a6ff606ca3480ea45b84dcae8d74723915ddca9d102d4de" => :x86_64_linux
   end
 
   depends_on "node"
