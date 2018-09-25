ded3f25c51cabf1d2a48f2cb1b56325c51dd3c01
VonUniGE <VonUniGE@github.com>
2018-09-25 19:07:30 +0000

    openjdk@11 11+28 (new formula)
    Closes Linuxbrew/homebrew-core#9547.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

---
+++ Formula/openjdk@11.rb
@@ -0,0 +1,35 @@
+class OpenjdkAT11 < Formula
+  desc "Java Development Kit"
+  homepage "https://jdk.java.net/11/"
+  # tag "linuxbrew"
+
+  version "11+28"
+  if OS.mac?
+    url "https://java.com/"
+  else
+    url "https://download.java.net/java/early_access/jdk11/28/GPL/openjdk-11+28_linux-x64_bin.tar.gz"
+    sha256 "3784cfc4670f0d4c5482604c7c513beb1a92b005f569df9bf100e8bef6610f2e"
+  end
+
+  depends_on :linux
+
+  def install
+    prefix.install Dir["*"]
+    share.mkdir
+  end
+
+  test do
+    (testpath/"Hello.java").write <<~EOS
+      class Hello
+      {
+        public static void main(String[] args)
+        {
+          System.out.println("Hello Homebrew");
+        }
+      }
+    EOS
+    system bin/"javac", "Hello.java"
+    assert_predicate testpath/"Hello.class", :exist?, "Failed to compile Java program!"
+    assert_equal "Hello Homebrew\n", shell_output("#{bin}/java Hello")
+  end
+end
