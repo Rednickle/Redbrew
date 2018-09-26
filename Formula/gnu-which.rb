662ad9dbf53684b14ac34aee83ed71fe5ff38ca5
Michka Popoff <michkapopoff@gmail.com>
2018-09-26 19:18:25 +0000

    gnu-which: update 2.21 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9560.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gnu-which.rb
+++ Formula/gnu-which.rb
@@ -13,6 +13,7 @@ class GnuWhich < Formula
     sha256 "04999e211cff8d6902f740ee08725b26b34ff1dfda82afc85d5e60243dbac927" => :high_sierra
     sha256 "27e6e35e4915cf7a6c59d3b0033962d1e643c004947dbb0eb97a41d766a8d571" => :sierra
     sha256 "e6b1179b99922a7d49b3dee829c1d31c3fa7269b000799f862361637594d34e1" => :el_capitan
+    sha256 "f4e5a8ba87eef9b0f6b7474f56c3e5ac678c4f8b6157f7d6f1124cb1817a310b" => :x86_64_linux
   end
 
   if OS.mac?
