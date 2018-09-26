75e04c33f9c9aca27827badd198b38508eb3b904
Michka Popoff <michkapopoff@gmail.com>
2018-09-26 05:49:15 +0000

    gnu-time: update 1.9 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9558.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gnu-time.rb
+++ Formula/gnu-time.rb
@@ -12,6 +12,7 @@ class GnuTime < Formula
     sha256 "22c847320019d76be1290c7e04943922bd91c39c8aaccca87e44d9a2327ceab8" => :high_sierra
     sha256 "2fa9dd90dc13901d0bc2ab792d334a264f992ee57302bbcdc42e45135457710f" => :sierra
     sha256 "a6ae05233326897ed622ab5c6ee63081e1c445b5e7496c68efa00dd5718b589c" => :el_capitan
+    sha256 "bab294c05643506de6d3e2defdf6ddfb2a6fc1aee2c760c7d47b4b64d4eab0a2" => :x86_64_linux
   end
 
   if OS.mac?
