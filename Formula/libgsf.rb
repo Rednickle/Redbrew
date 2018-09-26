876b250a1f7b435c136e38d4a5dbb39995d3f0ce
Michka Popoff <michkapopoff@gmail.com>
2018-09-26 20:03:11 +0000

    libgsf: update 1.14.44 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9589.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libgsf.rb
+++ Formula/libgsf.rb
@@ -10,6 +10,7 @@ class Libgsf < Formula
     sha256 "5ba6de277d54dd9b9ad5731bb5c942e73f820a731182bc8cc02a0382c484c7c6" => :high_sierra
     sha256 "ca613e132ce34edeb475550ce1202da43a170cd9a64d81f7d8b71ab4142c9280" => :sierra
     sha256 "d520330d8b278d76a727b1e2502bbcd4c661023199948fc374d007101839a4e3" => :el_capitan
+    sha256 "f4524ec2200bed54f2d6eb09d7c0fc14c305bd696157a7fd12f5226b38bf0d1f" => :x86_64_linux
   end
 
   head do
