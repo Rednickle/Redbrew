432f6774bf9ef7efdc90b1a4a6f7ebc365f3f3b6
Michka Popoff <michkapopoff@gmail.com>
2018-09-29 08:34:50 +0000

    skaffold: update 0.15.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9634.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -11,6 +11,7 @@ class Skaffold < Formula
     sha256 "45bb94f701aef1429dc6fc42c728ac32b452b1ba18a47d78a351ba7cd496ca8d" => :mojave
     sha256 "775eafc817ae0a87a50def3c5aba90eb9e4ce345e3dd29fbcc3266e8c2d088ac" => :high_sierra
     sha256 "f3772a9d4ba39f3531cdd85dd4ef0a3963789295dad391ed67dffffc81f6db89" => :sierra
+    sha256 "9cf8b10334f9bc1edd9b34913374e149a8ea41aef950757e87041b182acf16b5" => :x86_64_linux
   end
 
   depends_on "go" => :build
