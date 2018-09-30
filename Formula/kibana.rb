a4ed325a6bbf6c569b2ed0ebdcfe80f2a393e0d2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-30 11:33:01 +0000

    kibana: update 6.4.1 bottle for Linuxbrew.

--- Formula/kibana.rb
+++ Formula/kibana.rb
@@ -11,6 +11,7 @@ class Kibana < Formula
     sha256 "0bf531fe125f35eaba50902916e3c469220f66a402c83c6802f9cfd90e85b209" => :high_sierra
     sha256 "75b3a83605396fe8bb347fca5c9bd00279e8fa39b65e837994db309644c329a7" => :sierra
     sha256 "99bc5bb7e1e01216dfbf49357794fb87423ef27eee539050bd3901c955e4e021" => :el_capitan
+    sha256 "367527b9eff1a755b20c46bbd735ee5f0addc820c2c361f6d23e6093ade2563f" => :x86_64_linux
   end
 
   resource "node" do
