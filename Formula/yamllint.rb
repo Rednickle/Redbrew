76338de7a3b30f74a3ec58450ec262f78649ad68
Michka Popoff <michkapopoff@gmail.com>
2018-10-09 18:31:32 +0000

    yamllint: update 1.12.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9715.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/yamllint.rb
+++ Formula/yamllint.rb
@@ -11,6 +11,7 @@ class Yamllint < Formula
     sha256 "70c1a88c4bea93cfa2ec21645a42d3f11c7435db86d29e8821e0dd186dc54141" => :mojave
     sha256 "3314a7def6b931c04a4822ddfd381ee80b8be6b7ef4e419ff0727f64aa758251" => :high_sierra
     sha256 "00cc059b4f60dfac563b8b9ac4e138eb7ee639ad6d72d8e6947498605a89f280" => :sierra
+    sha256 "e93b455d39022391786b9bdedb837feef93a8d50c6f21fbfef682a0e1bcb4eaf" => :x86_64_linux
   end
 
   depends_on "libyaml"
