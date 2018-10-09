90898977810482f4d0e454ce607b4cc28cf96fe9
Michka Popoff <michkapopoff@gmail.com>
2018-10-09 19:50:01 +0000

    convox: update 20181006001642 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9720.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -9,6 +9,7 @@ class Convox < Formula
     sha256 "8d547bd15b8dee4fb0ef9b369078d9114ee751f010eaa01a218577d9c986bc41" => :mojave
     sha256 "ed99199cb4818398553644cdb70275dca22e502de844b304618b1ff61311f073" => :high_sierra
     sha256 "630ab6c31590a7fd4bebb90324bdd76e0aa3d03cec8d4dcebc9a0c757caf433d" => :sierra
+    sha256 "77a0fa37dddaa067b1c865ce44c843b7b890e3eec22136a37d54e07be779aa53" => :x86_64_linux
   end
 
   depends_on "go" => :build
