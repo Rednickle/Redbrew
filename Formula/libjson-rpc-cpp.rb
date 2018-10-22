a7553223ebebaad2f9b18eeef9ba0782663a955e
Michka Popoff <michkapopoff@gmail.com>
2018-10-22 04:47:16 +0000

    libjson-rpc-cpp: update 1.1.0_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9928.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libjson-rpc-cpp.rb
+++ Formula/libjson-rpc-cpp.rb
@@ -11,6 +11,7 @@ class LibjsonRpcCpp < Formula
     sha256 "22dcbf652674c827fe849f3c189dfd7b848a40a9041b6ca51c1c40d206d96c19" => :mojave
     sha256 "694ea5c8b16a902866c1e655f1fd80bad87387f0f98601938b8879cd96a04395" => :high_sierra
     sha256 "f58cc4a7838987035b2721c2a09ec4aa1efee76eae10bb15fc4493f0c3a2e2f5" => :sierra
+    sha256 "fdf9d7d6a8dbceabba7c7f8478df3e42e7242028d4258b22a159c4145d1c7921" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
