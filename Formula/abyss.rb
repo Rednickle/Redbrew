4f1d0ddecda4784a0d8f506d37701f1952f96137
Michka Popoff <michkapopoff@gmail.com>
2018-10-26 04:12:52 +0000

    abyss: update 2.1.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9997.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/abyss.rb
+++ Formula/abyss.rb
@@ -8,6 +8,7 @@ class Abyss < Formula
     sha256 "20117882aaab7c5ed8b14c6f0dcd99469fc9c1647635784ced685336229a2a0b" => :mojave
     sha256 "98b4de087149b934c42d56137c744a70d9f46f03a7620ef42df62c0029789ecb" => :high_sierra
     sha256 "2dae278d8ab9d91b0bf7057db60e903c7979686ea3aa77f6deff3eda3e19111c" => :sierra
+    sha256 "013d21ec2babf4297d7700930d62a17c4777f31fc74a98720a78935ca88336be" => :x86_64_linux
   end
 
   head do
