c461feff72977923b4d915eee8be7a659ac2602d
Michka Popoff <michkapopoff@gmail.com>
2018-10-26 18:07:00 +0000

    pgformatter: update 3.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10020.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pgformatter.rb
+++ Formula/pgformatter.rb
@@ -9,6 +9,7 @@ class Pgformatter < Formula
     sha256 "151286785ab2ee342fa33663e89ecec9892771c6bc86ec93f835b486e954d168" => :mojave
     sha256 "43e152a13a17b2dad47598f490bedbfc1a149ccb9e28e7c259330fc3ec934680" => :high_sierra
     sha256 "43e152a13a17b2dad47598f490bedbfc1a149ccb9e28e7c259330fc3ec934680" => :sierra
+    sha256 "f7dd99348170f9bc23a62595766e1a620815c980f6da97437a276d97e8cab567" => :x86_64_linux
   end
 
   def install
