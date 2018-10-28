981f190f4279822d450f6a50f97533eb5d0a0c64
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-10-28 19:30:51 +0000

    foma: update 0.9.18 bottle for Linuxbrew.

--- Formula/foma.rb
+++ Formula/foma.rb
@@ -12,6 +12,7 @@ class Foma < Formula
     sha256 "d50dfd48bb3418d809c7c95d6046a59f550e2089d7e6dbb80327015894f073e1" => :el_capitan
     sha256 "2113796151927413c1bc640f19d8a62083628a1a124657d5d6ca5c9e087b19dd" => :yosemite
     sha256 "19b54c8f060b5adf9e6a0c37a6e59dcf20017b6ae1fe642ea373f0ee0a03f01f" => :mavericks
+    sha256 "48c173c8cc3ebdc8807d8a5303046da608b51ca65e1fdfcc41b4a64703fc546b" => :x86_64_linux
   end
 
   unless OS.mac?
