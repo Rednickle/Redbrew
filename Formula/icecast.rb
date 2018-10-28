c10c34d181653d94d546c8b5f955ddfb00576a0b
Michka Popoff <michkapopoff@gmail.com>
2018-10-28 21:58:13 +0000

    icecast: update 2.4.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#9679.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/icecast.rb
+++ Formula/icecast.rb
@@ -9,6 +9,7 @@ class Icecast < Formula
     rebuild 1
     sha256 "f5d7003e198e5106a08c7ecdad8124c1bfba72a82e7d6ef7ffa479ca59aacc1b" => :mojave
     sha256 "29b910add5cd848b1cbfb91ef13632d7b6a0df8dc42fc8425b6a73062168efbc" => :high_sierra
+    sha256 "c30f3e18ff8e95db6517b48114412ef5aed180cf0a5298b0962efd1ffdb08cdb" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
