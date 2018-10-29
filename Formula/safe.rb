968087fadd33f0f7bab219cf3b71a9908dea005a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-10-29 19:49:15 +0000

    safe: update 0.9.9 bottle for Linuxbrew.

--- Formula/safe.rb
+++ Formula/safe.rb
@@ -9,6 +9,7 @@ class Safe < Formula
     sha256 "12d909578b737de009be9c5613ef50c9e4282162a5fc46a1f63bffebbfdc621f" => :mojave
     sha256 "264ca7e9c4351e2c445d1baceeca6f0f8792990e845746c3a83ebe46a4e89d9d" => :high_sierra
     sha256 "1e75ecfa450f8f76b87af4ae8b77acf4ad4dfc8a4f5da10fd779083d918e6e30" => :sierra
+    sha256 "88b5ffa47f9709e337678e8b31d09cc65c66f73f60a409bf94b9c3160fd82e94" => :x86_64_linux
   end
 
   depends_on "go" => :build
