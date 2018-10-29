8be5024a13b3602219d1ca570f3d8ecc73e98060
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-10-29 04:17:26 +0000

    sdl2: update 2.0.8_2 bottle for Linuxbrew.

--- Formula/sdl2.rb
+++ Formula/sdl2.rb
@@ -11,6 +11,7 @@ class Sdl2 < Formula
     sha256 "25cc31a9680beb16321613f740fee7fdd862489948a5280e4a5f94b8ed291dd6" => :high_sierra
     sha256 "81ae8deb6918e241fc0c3c47c11b1e5041deb297e9010f87e1a1584fcf2c17e8" => :sierra
     sha256 "d1cf341785b66ce316564564abe44d7e6e1d1d6e16b26dc9b1e307c68f0bd22d" => :el_capitan
+    sha256 "703fca91f7283c87b5e8744a8ec8ffc2ca7dfb91a321e5701764748aed3a1573" => :x86_64_linux
   end
 
   head do
