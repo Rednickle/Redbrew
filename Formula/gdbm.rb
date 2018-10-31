368eeda0426efa001f245904ebc67f201db2a9c6
Michka Popoff <michkapopoff@gmail.com>
2018-10-31 21:08:57 +0000

    gdbm: update 1.18.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10069.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gdbm.rb
+++ Formula/gdbm.rb
@@ -10,6 +10,7 @@ class Gdbm < Formula
     sha256 "4c2f6db42c5972e7061ace3c1fb0a41f2c0db92a7a3d29cc806f097794dbd898" => :mojave
     sha256 "7fdad0257e967b63f63b82d6ef48c718d26ead7b38376ed56609bacefa4a252b" => :high_sierra
     sha256 "9d0b8affbced6c6fb759a077cf5fac4fca0d4b69dd2f4a82ca2f38b20e0f0ece" => :sierra
+    sha256 "6497d8784138f7791a138fdd768073ba1bd629686c8e9322fa83a4f75f8d98df" => :x86_64_linux
   end
 
   if OS.mac?
