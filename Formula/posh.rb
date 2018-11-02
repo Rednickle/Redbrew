1a944939748811d7295b31e6226c318b229a8df6
Michka Popoff <michkapopoff@gmail.com>
2018-11-02 18:26:49 +0000

    posh: update 0.13.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10108.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/posh.rb
+++ Formula/posh.rb
@@ -9,6 +9,7 @@ class Posh < Formula
     sha256 "700e48f30128fa0fdd35c3117154dd24455ba2a8839bd56fda146f5cf8e4eb11" => :mojave
     sha256 "9c38d14973e3fd33480cfdb45442a1e2e5291d4a60008ea61c689d497f8ff7ef" => :high_sierra
     sha256 "d8528d6120edc6a45b43c7e3b148b5cb8859dcd10ee24c60ae7b1c7cfaa992d5" => :sierra
+    sha256 "ae6645a07a78361d0409b064e47a31ae6dd1c87477c999bdbb636f092fc0212c" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
