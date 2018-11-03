24121a853f7795f656654a0e0ca443cfb8e0ea54
Michka Popoff <michkapopoff@gmail.com>
2018-11-03 09:57:37 +0000

    treefrog: update 1.22.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10124.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/treefrog.rb
+++ Formula/treefrog.rb
@@ -10,6 +10,7 @@ class Treefrog < Formula
     sha256 "48c4597ce3646a2481946824dcb6aee5b1bcec9f7449c1054c265b70f66800d9" => :mojave
     sha256 "5c271befbf580381e9676937716a4c215b09b83ea75416cf6229d495956ce5d3" => :high_sierra
     sha256 "04a85f1d383d87e7d37be11b91360341c0686c758a4eda6b0185121f88824684" => :sierra
+    sha256 "a5969936e0da9a0fb88e959ee1a6ce0a3f4ac1e95c756404e75b8feab393cf2c" => :x86_64_linux
   end
 
   depends_on :xcode => ["8.0", :build] if OS.mac?
