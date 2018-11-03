fbc745baa2e32a19314a28f8664b6a075b561523
Michka Popoff <michkapopoff@gmail.com>
2018-11-03 13:38:03 +0000

    fselect: update 0.5.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10114.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -8,6 +8,7 @@ class Fselect < Formula
     sha256 "2f9eb65716c4000dea6ebfe7e9b3be8f79257c4a723d9751bc9b8476336a8f4a" => :mojave
     sha256 "340a2f1457d87e0dc7317cbc729dd929b6248aaf0f441218a0300be1ade0701b" => :high_sierra
     sha256 "06a1534b2cff43671bea6148e7a5db8469b0d033b027155edc74fd434388bf29" => :sierra
+    sha256 "d7f959bf9d62d4b43c586d04490cff03c4af1dcc97a52d1867a548df515af248" => :x86_64_linux
   end
 
   depends_on "rust" => :build
