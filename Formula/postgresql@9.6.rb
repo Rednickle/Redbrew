c5439fecc49b17d9f535ab3e19809ac8e5165573
Your Name <michkapopoff@gmail.com>
2018-11-03 14:38:01 +0100

    postgresql@9.6: Fix build for Linuxbrew
    Closes Linuxbrew/homebrew-core#10139.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/postgresql@9.6.rb
+++ Formula/postgresql@9.6.rb
@@ -62,9 +62,9 @@ class PostgresqlAT96 < Formula
 
     # The CLT is required to build Tcl support on 10.7 and 10.8 because
     # tclConfig.sh is not part of the SDK
-    if MacOS.version >= :mavericks || MacOS::CLT.installed?
+    if MacOS.version >= :mavericks || (OS.mac? && MacOS::CLT.installed?)
       args << "--with-tcl"
-      if File.exist?("#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework/tclConfig.sh")
+      if OS.mac? && File.exist?("#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework/tclConfig.sh")
         args << "--with-tclconfig=#{MacOS.sdk_path}/System/Library/Frameworks/Tcl.framework"
       end
     end
