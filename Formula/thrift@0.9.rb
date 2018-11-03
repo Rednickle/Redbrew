6992881134b73b36a89d2b4b7d0d3c518dc02be1
Michka Popoff <michkapopoff@gmail.com>
2018-11-03 09:59:18 +0000

    thrift@0.9: update 0.9.3 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10123.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/thrift@0.9.rb
+++ Formula/thrift@0.9.rb
@@ -10,6 +10,7 @@ class ThriftAT09 < Formula
     sha256 "79422a32dc72ec61bb4f0b9db57a08af6c7478ac676e52f14d05e9060acff2df" => :mojave
     sha256 "c48f3d1200f4cedd092622f380bee268caefa553822c4b0f7bf25aec13d19371" => :high_sierra
     sha256 "d0b173d367891df3d5a9398ea5f5e3a48cbd412fa88955e29d061b7707b7b9e4" => :sierra
+    sha256 "55ab0716e76fc43a4e326a95e61d5e1497a303a04d4086e281fb8140fe5ac7fa" => :x86_64_linux
   end
 
   keg_only :versioned_formula
