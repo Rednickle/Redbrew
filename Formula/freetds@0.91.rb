ff0d920425305ec782356beb3b0537cc6481cd40
Michka Popoff <michkapopoff@gmail.com>
2018-11-03 10:17:03 +0000

    freetds@0.91: update 0.91.112_1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10118.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/freetds@0.91.rb
+++ Formula/freetds@0.91.rb
@@ -9,6 +9,7 @@ class FreetdsAT091 < Formula
     sha256 "2e7675f7e1747c23583ff1d005f2d37f64159609d48993577813502f68010671" => :mojave
     sha256 "d5c53abae01fa996e458c240358f75aa5f34ec628637d9e20ae654c81a45f285" => :high_sierra
     sha256 "1458764cb5cb70d4b514502d3c94c0827dbba251cd03ff76960601e63f52faa1" => :sierra
+    sha256 "cb9a6bd0c4eb7581ef7a0e1d47ee54a9fb1e425bc9fb802d3fd79bb93e6e3002" => :x86_64_linux
   end
 
   keg_only :versioned_formula
