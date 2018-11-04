0c2adef1cdcee42dc653fbd7c34871f3c069996c
Michka Popoff <michkapopoff@gmail.com>
2018-11-04 19:05:15 +0000

    htop: update 2.2.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10200.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/htop.rb
+++ Formula/htop.rb
@@ -9,6 +9,7 @@ class Htop < Formula
     sha256 "668d7b889f1f15adf76efad7324688dc63f0529862cde70b11597cebbaeb4406" => :mojave
     sha256 "07cd259ed88e88ad7d1acbc92916dd3c6a069fdbbd65953de7838fd0d9ba0183" => :high_sierra
     sha256 "2e8c58bd5c46192178af2c31e8c7f97118ef09d301b2d3c402e08562d6927844" => :sierra
+    sha256 "533552a44ff01840d3482240d8e53a0a1c3c2368da44e7f87e7b36abc4b26bd7" => :x86_64_linux
   end
 
   head do
