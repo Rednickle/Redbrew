41c88c118022ddbcc623ce97aed565073f6d1dd8
Your Name <michkapopoff@gmail.com>
2018-11-04 21:44:45 +0100

    libprelude: bump revision for Python
    Closes Linuxbrew/homebrew-core#10210.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libprelude.rb
+++ Formula/libprelude.rb
@@ -3,27 +3,26 @@ class Libprelude < Formula
   homepage "https://www.prelude-siem.org/"
   url "https://www.prelude-siem.org/attachments/download/721/libprelude-3.1.0.tar.gz"
   sha256 "b8fbaaa1f2536bd54a7f69fe905ac84d936435962c8fc9de67b2f2b375c7ac96"
-  revision 2
+  revision 3
   # tag "linuxbrew"
 
   bottle do
-    sha256 "189967ff2af15f82c5e1ca70ab34e332f8c8c8ce0911f5ccb402baaefa39aba9" => :x86_64_linux
   end
 
   option "without-ruby", "Build without Ruby bindings"
 
-  depends_on "pkg-config" => :build
   depends_on "libtool" => :build
+  depends_on "lua" => [:build, :optional]
+  depends_on "perl" => [:build, :optional]
+  depends_on "pkg-config" => :build
+  depends_on "python@2" => [:build, :recommended]
+  depends_on "swig" => [:build, :recommended]
+  depends_on "valgrind" => [:build, :recommended]
+  depends_on "gnutls"
   depends_on "libgcrypt"
   depends_on "libgpg-error"
-  depends_on "gnutls"
-  depends_on "swig" => [:build, :recommended]
-  depends_on "perl" => [:build, :optional]
-  depends_on "python@2" => [:build, :recommended]
-  depends_on "python" => :recommended
-  depends_on "valgrind" => [:build, :recommended]
-  depends_on "lua" => [:build, :optional]
   depends_on "ruby"
+  depends_on "python" => :recommended
 
   skip_clean "etc", "lib64", "var", :la
 
@@ -45,8 +44,6 @@ class Libprelude < Formula
 
     system "./configure", *args
     system "make"
-    # Disabled due to https://lists.gnu.org/archive/html/bug-gnulib/2018-05/msg00059.html
-    system "make", "check" if OS.mac?
     system "make", "install"
   end
 
