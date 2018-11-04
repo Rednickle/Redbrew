9c151fa5f04f1ca08d7b2ef17550ffc5388a8e87
Michka Popoff <michkapopoff@gmail.com>
2018-11-04 17:50:57 +0000

    lean-cli: update 0.19.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10177.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/lean-cli.rb
+++ Formula/lean-cli.rb
@@ -10,6 +10,7 @@ class LeanCli < Formula
     sha256 "3f1cd9309524378a11a8e5df33b8abf6389b2033084588da021be4e3c8b3c8bb" => :mojave
     sha256 "b4c4dc6e03087fd9afb848f98cc3ff42b441e58deebf9999f57775817345453c" => :high_sierra
     sha256 "24244ce4fd50726f72dacd18e83d3e6734b07ad5dabdc49a9ca1ce2f116dff50" => :sierra
+    sha256 "5ac701187cdd90c80b31268dd6364dea616bbfc3f3e4477614dd29c8fa5c844f" => :x86_64_linux
   end
 
   depends_on "go" => :build
