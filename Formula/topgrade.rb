49906ea4e425bd6c92c74f13c2bf45723ba1396a
Michka Popoff <michkapopoff@gmail.com>
2018-11-04 18:49:06 +0000

    topgrade: update 0.18.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10196.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -8,6 +8,7 @@ class Topgrade < Formula
     sha256 "b7087d33d3b2fec5fbd2b61d684f2dac4fa6957a0508682de22db38a212d3297" => :mojave
     sha256 "9ac4e50c215f9c3485f5b7abad790eb1732e63c404443d4b20af1c0c3507b068" => :high_sierra
     sha256 "6d2d587990f5a2223140bda12ddd5937e20bf1b48d9cb5bb40d20e75442ff82e" => :sierra
+    sha256 "c6fba85ac9989a4cfce62da50bc0c203058dba1b08eadc77baead6410715774a" => :x86_64_linux
   end
 
   depends_on "rust" => :build
