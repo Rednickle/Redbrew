5557a530ca1ba82ffa0a38f5bf1ff6ae1e9c7c5c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-05 22:54:56 +0000

    dcd: update 0.9.13 bottle for Linuxbrew.

--- Formula/dcd.rb
+++ Formula/dcd.rb
@@ -10,6 +10,7 @@ class Dcd < Formula
     sha256 "7d4f107a95d461b88545bb23a0c773f23291f10a82401d749caeb50347438ab1" => :mojave
     sha256 "93c5b0c23def8f15e0a0fa326db8cfd90d6604e0b3f5ee8c1ff7ff7e28e4ef85" => :high_sierra
     sha256 "01efe880d58d23cc46f03e7907ad933cbe746400ec08ded499d19f7059350ef7" => :sierra
+    sha256 "1ebf1000fb779506e38a15d99ada817a52009dd8167e4509b4661c6b2a26e2da" => :x86_64_linux
   end
 
   depends_on "dmd" => :build
