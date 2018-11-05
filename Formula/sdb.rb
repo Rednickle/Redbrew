4c682f206d30ca27410f6f066cb777e804aff352
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-05 20:52:09 +0000

    sdb: update 1.3.0 bottle for Linuxbrew.

--- Formula/sdb.rb
+++ Formula/sdb.rb
@@ -10,6 +10,7 @@ class Sdb < Formula
     sha256 "863aea9679becb97cc5d9f64bbb714a226149db7f5a7b09143ad05517f9aed72" => :mojave
     sha256 "c25fed7f1923890c98ed5da6adb178feff0208d55f7c51bff55a1552d61339cb" => :high_sierra
     sha256 "c0be2ac314a7655b4b0b0f2928c1bacd3b568948dda7682386dee9c797d0a401" => :sierra
+    sha256 "4e2df321e6ed45c14a28baff3a9d93f80b492e29984cb4b3584ac5982422ac48" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
