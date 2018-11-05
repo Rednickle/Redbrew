c749caabdfe306d10483c0b3087b5256829a66e4
Michka Popoff <michkapopoff@gmail.com>
2018-11-05 19:06:56 +0000

    fwup: update 1.2.6 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10214.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/fwup.rb
+++ Formula/fwup.rb
@@ -9,6 +9,7 @@ class Fwup < Formula
     sha256 "e7c1f820c368c1e083f585e2ba61e9864d67b3f120873d2c95aae7f7f36dc1c1" => :mojave
     sha256 "1c3ce53c5cac1e2c2082f16d756630411bb787aea39438b17d13ac89c111f794" => :high_sierra
     sha256 "5ea71af2a6e0642743937ea9eca085c0e5aa87ba50394fd330cce5ee85437e99" => :sierra
+    sha256 "5aa56f7733b8e939b83f0bf1b798a794b8076e9ecc9e236a20b2960e620a429e" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
