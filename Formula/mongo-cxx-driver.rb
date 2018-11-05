64856a37918a5d92c3a2699f0e9c48f29bb45be6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-05 18:33:11 +0000

    mongo-cxx-driver: update 3.4.0 bottle for Linuxbrew.

--- Formula/mongo-cxx-driver.rb
+++ Formula/mongo-cxx-driver.rb
@@ -10,6 +10,7 @@ class MongoCxxDriver < Formula
     sha256 "da2aacd94c60bbdd1c4f7b4c0103ac90857d41733f0d95666370a878539a9084" => :mojave
     sha256 "f08a8bc08e9320b81a2142b100a43cc40aa010040c01815f345f9556d45e7a41" => :high_sierra
     sha256 "98fec6ef3256c3955dd29fcfe825faf575f9d71f0f3061b65be62f2b558148ed" => :sierra
+    sha256 "1d975f16ca42aa5560d55e737fd5909438fd56d586ea0c6e32261cfee07f1aee" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
