b8d8f793a79a3721820f6f653c21ec8fa661dd9a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-05 22:54:55 +0000

    rke: update 0.1.11 bottle for Linuxbrew.

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -10,6 +10,7 @@ class Rke < Formula
     sha256 "be1fced3aca5af994b7bdea1a4be26cf35dd3504faff7c6cb03ca6baa78ffd34" => :mojave
     sha256 "056f55ac6c5debf8a6bc9f40d0870a510609fd514fee5e57f682057cd270a1d5" => :high_sierra
     sha256 "069d35aa9bcdaebe8116a59e78c86a61291ae21c93d66d6003a24c11b9e309ca" => :sierra
+    sha256 "4fedb9ebe7d7aeae26b450bfb3fb6213ab1aedc1caa272eb831e824dfbf097e5" => :x86_64_linux
   end
 
   depends_on "go" => :build
