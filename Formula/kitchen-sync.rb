9f294fc6ae9ee1e15c25e8db28c8d711c2deff36
Michka Popoff <michkapopoff@gmail.com>
2018-11-05 19:18:41 +0000

    kitchen-sync: update 1.4 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10216.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/kitchen-sync.rb
+++ Formula/kitchen-sync.rb
@@ -10,6 +10,7 @@ class KitchenSync < Formula
     sha256 "1536494b29c6521a170a535b689207cd4dc9cd8783ea7c52148a53ad08314f44" => :mojave
     sha256 "8b67fe069f2a1f9ef38625023cc17a19609bd50a75dd90f6d1b39d77504ea563" => :high_sierra
     sha256 "8aab23fcc57e32a13becc71748eebdb95278228ed436b0b2e7b063274c19092e" => :sierra
+    sha256 "7657b7de941850846f73fe0212d78e7ef0628cef8b46a0d15bc390aa68da9e02" => :x86_64_linux
   end
 
   deprecated_option "without-mysql" => "without-mysql-client"
