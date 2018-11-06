acef3d7c0c6e9cfd4db18bfa7356dbcf75b8a713
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-06 06:36:22 +0000

    grunt-cli: update 1.3.2 bottle for Linuxbrew.

--- Formula/grunt-cli.rb
+++ Formula/grunt-cli.rb
@@ -11,6 +11,7 @@ class GruntCli < Formula
     sha256 "738d87cf6f7eb5a0b554980f1e77dc349fa092d34ef20b2f41583442d9249348" => :mojave
     sha256 "7df2a65c82959d478c0d384ad5e51046186750e0f744beea06916c663c4ca4eb" => :high_sierra
     sha256 "677a92a25a3f7d1a8e6ceeb6b869e4f4d784bbd10a32e37d7f32aa122c1176a4" => :sierra
+    sha256 "572ea977195137ed0dd300a1be859f3499b07a82b0ac3c2c43b5efab20d140b1" => :x86_64_linux
   end
 
   depends_on "node"
