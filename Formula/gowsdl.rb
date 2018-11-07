c875f5798da29c873070ed1b298ddc2d9a6a2cfb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-07 07:29:30 +0000

    gowsdl: update 0.3.0 bottle for Linuxbrew.

--- Formula/gowsdl.rb
+++ Formula/gowsdl.rb
@@ -10,6 +10,7 @@ class Gowsdl < Formula
     sha256 "60124759822dfa9cbb182818488f70f7dff36d68b1936b9a457844812f2034bf" => :mojave
     sha256 "631f836ce7d3f08f8becbd915ef2634b456e256c6cbb4f0450657cc6d1a13468" => :high_sierra
     sha256 "4395ff37e13fd146e3114beac5e8faa6a6a03819253760f5ef493c834374b905" => :sierra
+    sha256 "98c739e6e8956da6cfbda48e973cceeb4add7d8c30e0cd79d66aebd0708f0efe" => :x86_64_linux
   end
 
   depends_on "go" => :build
