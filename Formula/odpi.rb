47f91ffc78353d0c9a07e30cf3c492b7cf086819
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-07 16:34:46 +0000

    odpi: update 3.0.0 bottle for Linuxbrew.

--- Formula/odpi.rb
+++ Formula/odpi.rb
@@ -9,6 +9,7 @@ class Odpi < Formula
     sha256 "1e93ab7b8e0975900e250d695d07b8a9e6202feee4b726ed31a309bc9e36071d" => :mojave
     sha256 "f89cb672eb772e446103f55227dd17c4eeb346dc6a100098d70cbcb0a23294ef" => :high_sierra
     sha256 "c867fb98027fb2fc4695863ea5dd380697b666771c99276c47edf3769db7ef05" => :sierra
+    sha256 "d7d45304515dddf5e0f12c184a80a5c130d2d05c9c799d9dbd96298c719663b1" => :x86_64_linux
   end
 
   def install
