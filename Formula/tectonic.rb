46bb9c2afdedcdc9d9f6bbcee9a7959110d9bc7d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-09 23:24:40 +0000

    tectonic: update 0.1.11 bottle for Linuxbrew.

--- Formula/tectonic.rb
+++ Formula/tectonic.rb
@@ -8,6 +8,7 @@ class Tectonic < Formula
     sha256 "f402e832d1837832f6961f9c4219274fed1f27fafe9fcfd0a3129164d2c450e2" => :mojave
     sha256 "1490e54bf11846d199e6696d8841cb657c72b5ecb086978aa2e66054383695ca" => :high_sierra
     sha256 "b2427da372e639d3533f3396e82c4bd361684f6eeca432e62bf4fde9e01cbb0b" => :sierra
+    sha256 "57a18979cbd64131fc05621a51cea110440937661a4f757268fc51e74ed8f94e" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
