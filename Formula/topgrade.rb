1aaeee5cf9fdb5b7ab55e1de3c21776e69c3cc90
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-10 13:45:09 +0000

    topgrade: update 0.19.0 bottle for Linuxbrew.

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -8,6 +8,7 @@ class Topgrade < Formula
     sha256 "b5fb00a707e58950b825d8cafd23764b6dd673affbc248aa58d8bea0bd141afb" => :mojave
     sha256 "2a8b12f079420b78240ef935a2b51e78f6c925520e1cdee2d2c7516b3df383f8" => :high_sierra
     sha256 "e8ac26ffd69015f981be1af73f3d6f86eee700bc71beea18e0e674a8051b21ea" => :sierra
+    sha256 "bf0e186e095b23b1efe4a2f2dbb47461d4643b08e18c6916515231704c61abd8" => :x86_64_linux
   end
 
   depends_on "rust" => :build
