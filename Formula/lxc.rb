6ef47ba60423e144b22e4cdf3221e51226102477
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-10 21:22:10 +0000

    lxc: update 3.7 bottle for Linuxbrew.

--- Formula/lxc.rb
+++ Formula/lxc.rb
@@ -9,6 +9,7 @@ class Lxc < Formula
     sha256 "8d467f9219b9ed9da872b567571adb9550e1bc1f1d1dd7d719a4dd56d4820f00" => :mojave
     sha256 "e329578fa061b6bda1413814edb146a4c47a280b1a56c16792718076053d9328" => :high_sierra
     sha256 "d101238b769e7ef7d493b7214893119c09feaee8031400017a7927e39952b7ad" => :sierra
+    sha256 "18c78f3c3ab977316bbae899734ed287c0abdf4653a472a8ae7eb62ebab0af23" => :x86_64_linux
   end
 
   depends_on "go" => :build
