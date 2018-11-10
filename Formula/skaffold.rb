739bef13ceacdfea0ec909d9169e442733962094
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-10 23:24:59 +0000

    skaffold: update 0.18.0 bottle for Linuxbrew.

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -11,6 +11,7 @@ class Skaffold < Formula
     sha256 "85874bf1826ae861181cb132dc3dfb0937820b184e977529df451be9313e4e1c" => :mojave
     sha256 "291a04d5877b3f3055d9a1ee26d7df48a2431307f445b595ab6b0e398d98fe98" => :high_sierra
     sha256 "6f743845fc2f0619ec338cba95bf3d314833d21b437542e53645885c0ae5378b" => :sierra
+    sha256 "f702f499590367e9c5983a8226ff9b878deff7fe56dd670c5ad3b6563076118f" => :x86_64_linux
   end
 
   depends_on "go" => :build
