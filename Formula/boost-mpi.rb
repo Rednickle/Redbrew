875bfd1535ae33ced04718f0b3c0b5d9ac7c2abf
Michka Popoff <michkapopoff@gmail.com>
2018-11-10 17:35:08 +0000

    boost-mpi: update 1.68.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10276.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/boost-mpi.rb
+++ Formula/boost-mpi.rb
@@ -9,6 +9,7 @@ class BoostMpi < Formula
     sha256 "f3530194eb7aa995cb9d02fbbd2bbc31ac8f6d985a2930e6a255af356933c4e8" => :mojave
     sha256 "7b7870cdfd90c9bd3fbe165ed6b8a7b9c0d201df12e6b9e1a46ec96eff7a37b7" => :high_sierra
     sha256 "78c183f2594864fdf99c7780c00323a667fb54e6ca10421951bb9cac498ed9bc" => :sierra
+    sha256 "742481b5e60ed77f7f523ef85b7e6e4fcd44863ea79d148a95ac187e489d12fc" => :x86_64_linux
   end
 
   depends_on "boost"
