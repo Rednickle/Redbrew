976d5f91d8ccf236821b2cad601e654240fee2de
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-10 21:22:10 +0000

    dash: update 0.5.10.2_1 bottle for Linuxbrew.

--- Formula/dash.rb
+++ Formula/dash.rb
@@ -11,6 +11,7 @@ class Dash < Formula
     sha256 "aa6941a564fca697da6eb30e3691a8fa354de093d05ee84bbbc50c8045a55f66" => :mojave
     sha256 "49ebe51a7662187224ab620aa50b0473b11c1f88372f7c17da328559d895f5e0" => :high_sierra
     sha256 "4c7ca79c9b006065cb9bba57190103c518791b5a7ea078bb1f960e6f6c9dd7e9" => :sierra
+    sha256 "7b89662b945b9f67b47a226c0edee6404f54b029166c4e01f9ba5a5d2026ca82" => :x86_64_linux
   end
 
   head do
