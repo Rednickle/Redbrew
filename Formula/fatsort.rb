1a1f97fea0569f9b63af978496511eb6965bc624
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-12 18:33:35 +0000

    fatsort: update 1.5.0 bottle for Linuxbrew.

--- Formula/fatsort.rb
+++ Formula/fatsort.rb
@@ -10,6 +10,7 @@ class Fatsort < Formula
     sha256 "30317b1edc9c062183ca98bf20baca935bf129405e15fc89056659968e33a83b" => :mojave
     sha256 "d9e23a82a00b9747f24e78e2fc11f40ea8bebadfafa41fa7d5c2e6838d6514b7" => :high_sierra
     sha256 "fd941f1bc940edcd9d23860ce723355be2249e330ecc8bbab4c5713525c78963" => :sierra
+    sha256 "24dd654d86a9b55bedc1a1d6681f8e2efc7ee9002781a3f32155882885aff43f" => :x86_64_linux
   end
 
   depends_on "help2man"
