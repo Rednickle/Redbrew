4000d55302878701c650ad39c24cbda14eea5b8f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-22 22:28:27 +0000

    conan: update 1.9.2 bottle for Linuxbrew.

--- Formula/conan.rb
+++ Formula/conan.rb
@@ -12,6 +12,7 @@ class Conan < Formula
     sha256 "5360c1d4e12e3517d9b21e9a6723f86c882a7a2bd8a42b66bce4c8fabda34bf3" => :mojave
     sha256 "2b310000c6b739e14ac28e867929275dad4db3be94ec78c39281cd37f91b185c" => :high_sierra
     sha256 "b085ac23ca6401b6bd54e28af037bcf12344cce068867d6c7cd1d7ddde26dcd3" => :sierra
+    sha256 "38e2550bd652c10adf10b52fe0b24e0f60cd20d6f4bd205576caaf36761abb02" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
