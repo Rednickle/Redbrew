76151a52a101e79b0d9233adfcb1c95ddecc411b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-29 20:52:23 +0000

    ios-webkit-debug-proxy: update 1.8.3 bottle for Linuxbrew.

--- Formula/ios-webkit-debug-proxy.rb
+++ Formula/ios-webkit-debug-proxy.rb
@@ -10,6 +10,7 @@ class IosWebkitDebugProxy < Formula
     sha256 "c16647d7595ca6e09c09a6e6d4c7399326d88b1c1da5697a2ace225730550486" => :mojave
     sha256 "30ed9c9006d712cf6b550966978006323505840fff8522449664733d72f9600b" => :high_sierra
     sha256 "deb979ca5f8df37e7323f8d0169e51f0fb4781936e15e9f7135811e1fe6f152e" => :sierra
+    sha256 "068f6416da130d06420005cd0e609dbc6c03d82cee0964efb4f5e4f7a5854463" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
