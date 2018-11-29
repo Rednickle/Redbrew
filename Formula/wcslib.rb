9da0cc9430b7403ae269dfc0672bd582463cac86
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-29 20:52:21 +0000

    wcslib: update 6.2 bottle for Linuxbrew.

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -9,6 +9,7 @@ class Wcslib < Formula
     sha256 "791f633e087db6056f23904df39a7b0489a7c18c2fea5e3e25c71bf7365518e1" => :mojave
     sha256 "313a28f2170b0622ea7f72fe4371894be0ffb79663541bfeb5ed51c6a02571df" => :high_sierra
     sha256 "98ce5c443e70f687b3754f7ab01fca8248ab760d76145da546a46132e67bd7de" => :sierra
+    sha256 "b36da9a2eebf8d630e108940d7fbace1f928594b95032363eb293a182daac596" => :x86_64_linux
   end
 
   depends_on "cfitsio"
