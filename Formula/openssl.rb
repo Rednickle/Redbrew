c99c979ba58eff39292330904568ed8d3d66abb4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-01 14:53:16 +0000

    openssl: update 1.0.2q bottle for Linuxbrew.

--- Formula/openssl.rb
+++ Formula/openssl.rb
@@ -14,6 +14,7 @@ class Openssl < Formula
     sha256 "cdbee2befd8f2e178ff0c5f9e8796a73a7de20055aae51cba7cc749429e8c90f" => :mojave
     sha256 "d3ac5de6ccd9c604a5f2b8582ebd721ab421c0fdbfefa5a4b1190f83277f2c27" => :high_sierra
     sha256 "94881a8df581a9f63b6a0a6c9f362d873133c2a27f2708803bca778390356975" => :sierra
+    sha256 "67b0d3acbc6920651e8600073849b1c012ae6f413aa38e2678298e52efe41c27" => :x86_64_linux
   end
 
   keg_only :provided_by_macos,
