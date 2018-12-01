8ff15da715dda37c8e753b0b32105df4cd3fbf9e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-01 11:13:55 +0000

    pqiv: update 2.10.4_4 bottle for Linuxbrew.

--- Formula/pqiv.rb
+++ Formula/pqiv.rb
@@ -7,10 +7,11 @@ class Pqiv < Formula
   head "https://github.com/phillipberndt/pqiv.git"
 
   bottle do
-    cellar :any
+    cellar :any_skip_relocation
     sha256 "147a3c4adbf177df4f339874b33c9120a9f5191c0bb96539de9d4f98ea8ecad0" => :mojave
     sha256 "a3d08b8315eff78fe53885d40d2f207219f8c9d8e82c08a2ee10f458255afb7f" => :high_sierra
     sha256 "78408230b162677104caac334176523e89104006f3d993a607f5cf422d59aea5" => :sierra
+    sha256 "126f4ae104fc8790598e37d4fe509456ca328bda6fcee69b6522ac55469328d1" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
