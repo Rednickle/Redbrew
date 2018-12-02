5c16f47237658144ee01fb2c33da67670055c2f5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-02 19:21:15 +0000

    tippecanoe: update 1.32.5 bottle for Linuxbrew.

--- Formula/tippecanoe.rb
+++ Formula/tippecanoe.rb
@@ -9,6 +9,7 @@ class Tippecanoe < Formula
     sha256 "0f5bf2839e1ad6e48646c0b00ba293d30aef4966127e96c9ae120d38f8d1a06f" => :mojave
     sha256 "d2389067a42fefd4bd90681a92bb4c4bd70425d741c0b47849172f1517d6812e" => :high_sierra
     sha256 "f600aef99b96d6300adf8376b10eec208f9dc5839b2a898dfbdec132f6eae8d6" => :sierra
+    sha256 "cce9903cf44a300fd4f770ec006c0422e1c2f3c43d8cb53ffb15673bee848dcd" => :x86_64_linux
   end
 
   unless OS.mac?
