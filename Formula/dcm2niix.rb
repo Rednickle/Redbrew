a7be31e904604e0c5ddd74c7ff6de255e88b1d03
Michka Popoff <michkapopoff@gmail.com>
2018-12-02 21:39:42 +0000

    dcm2niix: update 1.0.20181125 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10464.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dcm2niix.rb
+++ Formula/dcm2niix.rb
@@ -10,6 +10,7 @@ class Dcm2niix < Formula
     sha256 "88007ff3d589db9d0d1bf279fdd5b4615832fb4705658d64ec75ba8392253276" => :mojave
     sha256 "abacf45887c15b756f296ae821978290ec707a76982a8ea3be0088e2147ea0a1" => :high_sierra
     sha256 "5a3b32eeef358216b95b6606dbd270f6393f3762372f185d3d5a778ad3e9d98d" => :sierra
+    sha256 "874d8b0746dfb4808a3f05a9c69d6f1044faee016d00f73baac4ff46218f5cda" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
