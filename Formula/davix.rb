5a5ca80c8674f408eca6d78e6f4bd65e5a7e0416
Michka Popoff <michkapopoff@gmail.com>
2018-12-03 19:43:49 +0000

    davix: update 0.7.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10477.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/davix.rb
+++ Formula/davix.rb
@@ -12,6 +12,7 @@ class Davix < Formula
     sha256 "86fff80d2ff8aae77220e2bc996151a34c0f4abd0f70abe330572564a71e0a39" => :mojave
     sha256 "f8c3f240c37c06dd16ab41b9fd458a2884296981ce898c13a5911ab628f3a41c" => :high_sierra
     sha256 "cf1a6eaba6c3208b20cc44174ddaa716f39cd9fe5394ade09681749c987c6198" => :sierra
+    sha256 "e23e82f1b9ccd9cdd1bae9d08c0fd18f52fb1be16df22afebefaac5805b626d7" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
