051284b89eee7762e9784fbac23827751ff621bb
Michka Popoff <michkapopoff@gmail.com>
2018-12-05 18:35:45 +0100

    tectonic: Build a bottle for Linuxbrew
    Closes Linuxbrew/homebrew-core#10536.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/tectonic.rb
+++ Formula/tectonic.rb
@@ -6,6 +6,7 @@ class Tectonic < Formula
   revision 1
 
   bottle do
+    cellar :any
     sha256 "1a7529e618b09f3b3c7852d7734f631847c7d31334ebe43d10a5da3dc14bc6b4" => :mojave
     sha256 "5bfc6c5d8d8f1c80ed55e7543bb081a6fa1f83faa8ca01759541b179f62c9c65" => :high_sierra
     sha256 "86202b51be7fe24c4306ca5e38aa39112bc879d0f651bffdee0d35e1ca71d72f" => :sierra
