94228c2459f7cf9c3f518042550d20d421dff6a5
Michka Popoff <michkapopoff@gmail.com>
2018-12-05 22:05:06 +0000

    minio: update 20181130035659 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10542.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -11,6 +11,7 @@ class Minio < Formula
     sha256 "47e9440c35bef6d83e95e5d8aab0229d65f5ac38ef43219f1fa99e8eccd34b0a" => :mojave
     sha256 "36118b3d896fb33e4be1a8fd1ab4029fa5d2c906b03c7414c148cc212dce7ad0" => :high_sierra
     sha256 "bb1354da45d6f22b62ca11ac9770d3144509407e407b85fc66f2d706999f57f4" => :sierra
+    sha256 "0f5a495112bc33cc2401b1ba6cd1cf073e98081636ec766987147df58de48bd4" => :x86_64_linux
   end
 
   depends_on "go" => :build
