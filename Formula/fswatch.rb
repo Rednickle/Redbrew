3f6f150cce15d6fea709555f05a7170115b55a26
Michka Popoff <michkapopoff@gmail.com>
2018-12-06 21:04:55 +0100

    fswatch: Fix bottling for Linuxbrew
    Closes Linuxbrew/homebrew-core#10587.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/fswatch.rb
+++ Formula/fswatch.rb
@@ -5,7 +5,7 @@ class Fswatch < Formula
   sha256 "44d5707adc0e46d901ba95a5dc35c5cc282bd6f331fcf9dbf9fad4af0ed5b29d"
 
   bottle do
-    cellar :any
+    cellar "/home/linuxbrew/.linuxbrew/Cellar"
     sha256 "7310f4dbbbdeed582cb713b3d08bf982553747cbda75eb76d2dfe00f309ed3f7" => :mojave
     sha256 "68cfeb10ae04d00cc90e0d87d48a61c7c22b38f386410dbf2eb6c004200ddc8a" => :high_sierra
     sha256 "9362bc3b3321bf0238fa70d2f2825e4118e18deb2207af3a2633b6772bb33666" => :sierra
