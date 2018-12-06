021f5fdbcc67fcb170a8a2901861f3ee7f4cc2ac
Michka Popoff <michkapopoff@gmail.com>
2018-12-06 13:05:39 +0100

    angular-cli: Fix bottling for Linuxbrew
    Closes Linuxbrew/homebrew-core#10556.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -7,6 +7,7 @@ class AngularCli < Formula
   sha256 "5ee2a304349236c567069810e030454298d2f3c5d2dd18f5bd91c149d2c16c43"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "76eac3dac06a6e78e26d13c4c06d9d7395f7ffe4917bf0184fc4eefb98edad4c" => :mojave
     sha256 "7479d505a262985b151ac8568293b06a908fe3286bc724d30d32856d8b5a6201" => :high_sierra
     sha256 "eb740bf7b8090a5caa99357c48bb33cda73d842e889b0ff7724452ecb6c64bb5" => :sierra
