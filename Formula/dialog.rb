f1be8f01f237331582a2aac9f29eaead2e4ed226
Michka Popoff <michkapopoff@gmail.com>
2018-12-06 19:36:03 +0000

    dialog: update 1.3-20181107 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10567.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -9,6 +9,7 @@ class Dialog < Formula
     sha256 "c356e3a474ebf3d580ec4674d87aec9c88ba7137e0f6fefd8c73cbf99d327b76" => :mojave
     sha256 "cf169dff8e776ac85d3a662db2abd9fbd578affc994dcb866547ee06630b68f6" => :high_sierra
     sha256 "68b0c43919cde70242f56ba550b666c2a6f9e6baefca50615923efa262052d5b" => :sierra
+    sha256 "8b4bbf34651db00003ac40107b4265c6069de37f573c5494e8b83efa32b7113b" => :x86_64_linux
   end
 
   depends_on "ncurses" unless OS.mac?
