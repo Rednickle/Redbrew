a9ea83a8107252a5b681ceccbadd21d1238c0e81
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-06 21:27:56 +0000

    tbb: update 2019_U3 bottle for Linuxbrew.

--- Formula/tbb.rb
+++ Formula/tbb.rb
@@ -10,6 +10,7 @@ class Tbb < Formula
     sha256 "e71225b00b1600e1f5d6fb863b7e4ed2331d2f003966f7360cb2ceb5a5c62ca2" => :mojave
     sha256 "65ab527434fae8d12d0945065ecf41ba37a5a6ebff179adceb60744a524816e1" => :high_sierra
     sha256 "3df673cbf9c0d3b44866b5154e528e8f1a6d941a112bdccaa0b4951ae10077a1" => :sierra
+    sha256 "412f15e9fa8a87abdb92edea0a8715c449cbba6f0a3aec65f75af2a77147f28e" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
