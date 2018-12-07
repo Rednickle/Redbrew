4dd753737d5ab5044b7067195f5a1110165e72cc
Michka Popoff <michkapopoff@gmail.com>
2018-12-07 17:04:45 +0000

    rke: update 0.1.13 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10598.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -10,6 +10,7 @@ class Rke < Formula
     sha256 "5f3198954212d9592973fe09e0c897edf25790e2f39519d7c2e4c542eb5a919d" => :mojave
     sha256 "beb97422bce1a8dae494e4e981e383d8786adaf3b32215667b9edc20967a0b7f" => :high_sierra
     sha256 "cfeaedad890402e5ea0539e167f9d6b3f6409c016227719f248467db95b4ea55" => :sierra
+    sha256 "54dd9756e9f6617dccdd4e586ac5c53a22271085e97609d746a7ceaf8e8493bd" => :x86_64_linux
   end
 
   depends_on "go" => :build
