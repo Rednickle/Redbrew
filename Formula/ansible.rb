d4365e9c5d79a7b64fdcf7f347fb26575742367e
Michka Popoff <michkapopoff@gmail.com>
2018-12-09 21:54:43 +0100

    ansible: Fix style for Linuxbrew
    Closes Linuxbrew/homebrew-core#10618.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ansible.rb
+++ Formula/ansible.rb
@@ -255,10 +255,12 @@ class Ansible < Formula
     sha256 "fb04cfd54d8d7e4cc533108750047e9ccf43139c3c0220f8a082274b19564e98"
   end
 
-  resource "kerberos" do
-    url "https://files.pythonhosted.org/packages/34/18/9c86fdfdb27e0f7437b7d5a9e22975dcc382637b2a68baac07843be512fc/kerberos-1.3.0.tar.gz"
-    sha256 "f039b7dd4746df56f6102097b3dc250fe0078be75130b9dc4211a85a3b1ec6a4"
-  end if OS.mac?
+  if OS.mac?
+    resource "kerberos" do
+      url "https://files.pythonhosted.org/packages/34/18/9c86fdfdb27e0f7437b7d5a9e22975dcc382637b2a68baac07843be512fc/kerberos-1.3.0.tar.gz"
+      sha256 "f039b7dd4746df56f6102097b3dc250fe0078be75130b9dc4211a85a3b1ec6a4"
+    end
+  end
 
   resource "keystoneauth1" do
     url "https://files.pythonhosted.org/packages/3b/c1/ef5904e9d1961b45c2a2cc0bfd9ac3aaeabfab8580683009fb9a94b65b78/keystoneauth1-3.11.1.tar.gz"
