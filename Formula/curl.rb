c204d724c65fafdee202d1df482cb46328a0e062
Michka Popoff <michkapopoff@gmail.com>
2018-12-15 10:19:16 +0100

    curl: make cellar any for Linuxbrew

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -6,6 +6,7 @@ class Curl < Formula
   sha256 "9bab7ed4ecff77020a312d84cc5fb7eb02d58419d218f267477a724a17fd8dd8"
 
   bottle do
+    cellar :any
     sha256 "9cb9b63ba604679be2bbaa9ad5f2cc864d1f4312ca02c46aa2d7ca3e7728f702" => :mojave
     sha256 "1cb7daf7992bebe4fa3a966d4fdb410f606c8899519a30de7a0de53052085fbd" => :high_sierra
     sha256 "2f577b9aebe369d2d024f9fbbbb2158fc7d48f6014c39c53af9451a5294cca5a" => :sierra
