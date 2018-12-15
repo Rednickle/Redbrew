7c8cc414a0d25866184257587683736c3ab4cc31
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-15 16:43:59 +0000

    gmsh: update 4.0.7 bottle for Linuxbrew.

--- Formula/gmsh.rb
+++ Formula/gmsh.rb
@@ -10,6 +10,7 @@ class Gmsh < Formula
     sha256 "5284401833c614c32f5d42628a40730cdbe668cf11156e7a66df4e54823304c6" => :mojave
     sha256 "96db869685bdcfeb45f7ec36f438b253d35b70d7f7f11354c99c0daca8af1bc4" => :high_sierra
     sha256 "088e0d81dd58de384d59868a56cef684ef0bb48c6122d3a5c75221d381eb6e02" => :sierra
+    sha256 "68d7b180a51289bfe61eb1182a00cd1b51adfd32f4df475941e4081194499735" => :x86_64_linux
   end
 
   option "with-opencascade", "Build with opencascade support"
