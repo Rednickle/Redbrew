7b9dfc5db0504557baa28416b359345bed91783b
Michka Popoff <michkapopoff@gmail.com>
2018-12-17 06:49:56 +0100

    gtk+3: Fix build for Linuxbrew
    Closes Linuxbrew/homebrew-core#10700.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gtk+3.rb
+++ Formula/gtk+3.rb
@@ -35,6 +35,8 @@ class Gtkx3 < Formula
   end
 
   def install
+    inreplace "docs/tools/shooter.c", "gdk_screen_get_dfeault", "gdk_screen_get_default" unless OS.mac?
+
     args = %W[
       --enable-debug=minimal
       --disable-dependency-tracking
