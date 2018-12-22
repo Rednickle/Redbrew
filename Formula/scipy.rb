6262b8f5821bad3d4f83410256d6fec7fc05101d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-22 22:48:13 +0000

    scipy: update 1.2.0 bottle for Linuxbrew.

--- Formula/scipy.rb
+++ Formula/scipy.rb
@@ -6,10 +6,10 @@ class Scipy < Formula
   head "https://github.com/scipy/scipy.git"
 
   bottle do
-    cellar "/home/linuxbrew/.linuxbrew/Cellar"
     sha256 "59dfc86e0269bc3c798b50f60e3cac705122a461ef1a272af5608455d19ea1c2" => :mojave
     sha256 "2241f09398eab14f666d5cc50db36e95541a86d9a4156c9f1d3ab64e7a13aedb" => :high_sierra
     sha256 "b0678887da4c5547004001986684c3a5588ad31d457258f060591cd9ce0de7bb" => :sierra
+    sha256 "e7fb85fcf39b7daae66d77c77ce1ed771f9a3839a368f77af2306943eee9e52a" => :x86_64_linux
   end
 
   option "without-python", "Build without python2 support"
