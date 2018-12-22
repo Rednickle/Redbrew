f9abeda5aa60ccf482ac52c001c4b1e2f8a87c80
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-22 21:15:17 +0000

    numpy: update 1.15.4_1 bottle for Linuxbrew.

--- Formula/numpy.rb
+++ Formula/numpy.rb
@@ -6,10 +6,10 @@ class Numpy < Formula
   revision 1
 
   bottle do
-    cellar "/home/linuxbrew/.linuxbrew/Cellar"
     sha256 "a3b8540e1d529354ffd8a6ddc9a931e22258f06488f9bf104cea6c5b8e00ecde" => :mojave
     sha256 "4d4b469f66db66247d51580464845e30f59a6ba285427f7f833e1a3f6acaa1c0" => :high_sierra
     sha256 "100fd310cd3133fc998b473aabfd7d82e55db3d74d739ceb5f144271fa255390" => :sierra
+    sha256 "9a1de4ff21cc264722a6a3981e6578ad6cd64eaa478fa64d2e24deaa5ab5db21" => :x86_64_linux
   end
 
   head do
