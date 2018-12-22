a0ae1eb80cf473c08594c74b3762c5fb6f12974a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-22 22:31:29 +0000

    arcade-learning-environment: update 0.6.0_2 bottle for Linuxbrew.

--- Formula/arcade-learning-environment.rb
+++ Formula/arcade-learning-environment.rb
@@ -7,10 +7,10 @@ class ArcadeLearningEnvironment < Formula
   head "https://github.com/mgbellemare/Arcade-Learning-Environment.git"
 
   bottle do
-    cellar "/home/linuxbrew/.linuxbrew/Cellar"
     sha256 "650f4b525c0ec6d8c4716b4ef4acf3bcf6fb754bc425fe02986a28b42f11ed1d" => :mojave
     sha256 "23210afaa7fdf64dbe85d75682ef7234d9e0d84a94e3f0691015d5e0bccc9d3b" => :high_sierra
     sha256 "ce438bce03014df33bb3354032f62427e9018fc3e5febe26748bdbb5615d3844" => :sierra
+    sha256 "8dacd72fcb601ac94cbfc9f96898c41e61ed5663af012f67a97e7fe67969cf9e" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
