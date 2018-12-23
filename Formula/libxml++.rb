c5fcd262b3f041d07d4b3fd897bf7c2591717366
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-23 12:09:44 +0000

    libxml++: update 2.40.1_2 bottle for Linuxbrew.

--- Formula/libxml++.rb
+++ Formula/libxml++.rb
@@ -13,6 +13,7 @@ class Libxmlxx < Formula
     sha256 "9bcaa205d33dbb8d44851e5f6c41ab95b322125cfd56215c55ce3abd0ac0b00e" => :el_capitan
     sha256 "53f18b1f5fe05dc545a8629292a498615ffd0c546c9fab98e5152be284081cbe" => :yosemite
     sha256 "8d120ff026529306553a82ca53c73747ee106b96ceb904d7cca54b807b32c4ff" => :mavericks
+    sha256 "3965c29813f8c0ea1a1135d03c2c0c8b194ede3a96b8640f60008f42817ddec9" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
