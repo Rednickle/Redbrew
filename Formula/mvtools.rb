f95f71d9da652621b9a07a8e47c4fc58f9813f61
Michka Popoff <michkapopoff@gmail.com>
2018-12-24 16:36:35 +0100

    mvtools: Fix test for Linuxbrew
    Closes Linuxbrew/homebrew-core#10815.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mvtools.rb
+++ Formula/mvtools.rb
@@ -30,19 +30,21 @@ class Mvtools < Formula
   end
 
   def caveats
+    dylib = OS.mac? ? "dylib" : "so"
     <<~EOS
       MVTools will not be autoloaded in your VapourSynth scripts. To use it
       use the following code in your scripts:
 
-        core.std.LoadPlugin(path="#{HOMEBREW_PREFIX}/lib/libmvtools.dylib")
+        core.std.LoadPlugin(path="#{HOMEBREW_PREFIX}/lib/libmvtools.#{dylib}")
     EOS
   end
 
   test do
+    dylib = OS.mac? ? "dylib" : "so"
     script = <<~EOS.split("\n").join(";")
       import vapoursynth as vs
       core = vs.get_core()
-      core.std.LoadPlugin(path="#{lib}/libmvtools.dylib")
+      core.std.LoadPlugin(path="#{lib}/libmvtools.#{dylib}")
     EOS
 
     system "python3", "-c", script
