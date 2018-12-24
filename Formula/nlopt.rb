fe5526b9e87e8a4f676a02a6192854cc4f9a8553
Michka Popoff <michkapopoff@gmail.com>
2018-12-24 14:35:26 +0000

    nlopt: update 2.4.2_2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10812.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nlopt.rb
+++ Formula/nlopt.rb
@@ -11,6 +11,7 @@ class Nlopt < Formula
     sha256 "6b0c91aa1a690600875d5e00aafa407f2a1394f803b31fde0867d5162a587c15" => :mojave
     sha256 "6865527f6074f1ea737a80bd2b4fd5e6e39df01e744fd111e593fd1dd1c4e0de" => :high_sierra
     sha256 "e3ea4064beff9f39ab2fed6a9e5f643a18fd7abb2b2302713ce7f58f970ceb09" => :sierra
+    sha256 "fa08629de136212f6b2bf687be31dbeaba20d672827ff80b9dbdb5d170ee8015" => :x86_64_linux
   end
 
   head do
