511a408dfb7cf3e6bc6a6b7d5a7d4d68e5cab0f1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-31 09:44:56 +0000

    subversion: update 1.11.0_2 bottle for Linuxbrew.

--- Formula/subversion.rb
+++ Formula/subversion.rb
@@ -10,6 +10,7 @@ class Subversion < Formula
     sha256 "e7bbcad66645e19497f86fc53ffbc6ab8565b9c00b27ded3ce768a7c8d5a37d2" => :mojave
     sha256 "dccecc8c5673437a8b7001f31ef22af3e95915fd11d7fd63ecccbdd6ba321a25" => :high_sierra
     sha256 "bd36436ffe7bc83dd96b4c44802413c838e04d80430df735fe7462a78e5ca0e2" => :sierra
+    sha256 "7a5e033f44787bc22636cedd38ca12c8507156f250be0d8c13da12c4193e3996" => :x86_64_linux
   end
 
   head do
