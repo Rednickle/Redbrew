30000500a5964bde18c3522fe23e05c06279cad1
Michka Popoff <michkapopoff@gmail.com>
2019-01-02 20:25:48 +0000

    topgrade: update 1.4.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10944.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -9,6 +9,7 @@ class Topgrade < Formula
     sha256 "46a0142c7c50ca1a66278c2273b7146bf3256bf49c27f35ebad5cd82fe3494f1" => :mojave
     sha256 "15735e275ec3da0b6c7ecb637dbfc20623ec8b9e35999cf83ce86e72bb62f133" => :high_sierra
     sha256 "a971770d8af0c12ce5a53a34cb707520a9226af356ccfe17bf3854a5ccc2d3c3" => :sierra
+    sha256 "ee724d6758dbea2f81a6958a4c4fa8a98aec529fc8f29bc9f931d5c679c11d9a" => :x86_64_linux
   end
 
   depends_on "rust" => :build
