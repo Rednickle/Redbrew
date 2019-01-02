55f00d21b475c6300a8f66b6f34b6a6661babb8a
Michka Popoff <michkapopoff@gmail.com>
2019-01-02 20:21:18 +0000

    nnn: update 2.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#10939.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -10,6 +10,7 @@ class Nnn < Formula
     sha256 "f678ffcf42043c17dd4aaf4857d5d6cb738ae50e35485b81eca6382b2c1562a0" => :mojave
     sha256 "4337e0b9968a516f0518eb722f0757d34330d5967fec6a69840ce91b0592d6ac" => :high_sierra
     sha256 "f4d378f77db6f6b0cfaf65b530e6e0ed6cf39b58ce1924981ae60fc7f2d5f548" => :sierra
+    sha256 "b12bd1090633adcf30432fccfcdee21af213e2461e8c36895b5d0fdfc37525d5" => :x86_64_linux
   end
 
   depends_on "readline"
