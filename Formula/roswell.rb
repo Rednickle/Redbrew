2590db84df68afe3fe81ed49b165783b7b3c8f3a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-02 22:27:33 +0000

    roswell: update 19.1.10.96 bottle for Linuxbrew.

--- Formula/roswell.rb
+++ Formula/roswell.rb
@@ -9,6 +9,7 @@ class Roswell < Formula
     sha256 "f26aa80640ed3b95153667017089ea52ba9be8a9e43c648ef90f7706701c17e1" => :mojave
     sha256 "df2487a5d7805bd506db021b6a5d0ed9cd4a49b5d8a09f2156e554163a178961" => :high_sierra
     sha256 "d12cd33d8fa47367503f25c88c1ead0fdaa4778886f92e455492b4dde26056ce" => :sierra
+    sha256 "62c82ea867714e5e0616f386dc28292554b45d3cd389b8507d64cbfb3bf94690" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
