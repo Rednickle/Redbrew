fafe3bb9cf3201a40fd0032df4b98df7fdcda4d9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-02 21:13:51 +0000

    diffutils: update 3.7 bottle for Linuxbrew.

--- Formula/diffutils.rb
+++ Formula/diffutils.rb
@@ -9,6 +9,7 @@ class Diffutils < Formula
     sha256 "4ec2a5ef0ca889d6c449b31ed43c797a0656ff7a2acfd913d0f87d8f14248031" => :mojave
     sha256 "fe012f4e981c3df3b2d1b3eb2b77009991148e3bdc08dd974d6f6071108e8937" => :high_sierra
     sha256 "3a04e2e2de81458a8fc75482a97a02883a1cdf231ee340ce30d9a712d0475305" => :sierra
+    sha256 "14aedfee9f25b2e2d0ba28dfc62941767d078919fce26cdcc60ff05ddba50f5b" => :x86_64_linux
   end
 
   def install
