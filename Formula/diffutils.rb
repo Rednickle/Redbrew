12a1bf4b7969bab05f11f5c0cccc874d75c3b498
Michka Popoff <michkapopoff@gmail.com>
2019-01-02 21:16:51 +0100

    diffutils: Fix bottling for Linuxbrew
    Closes Linuxbrew/homebrew-core#10946.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/diffutils.rb
+++ Formula/diffutils.rb
@@ -6,7 +6,6 @@ class Diffutils < Formula
   sha256 "b3a7a6221c3dc916085f0d205abf6b8e1ba443d4dd965118da364a1dc1cb3a26"
 
   bottle do
-    cellar :any_skip_relocation
     sha256 "4ec2a5ef0ca889d6c449b31ed43c797a0656ff7a2acfd913d0f87d8f14248031" => :mojave
     sha256 "fe012f4e981c3df3b2d1b3eb2b77009991148e3bdc08dd974d6f6071108e8937" => :high_sierra
     sha256 "3a04e2e2de81458a8fc75482a97a02883a1cdf231ee340ce30d9a712d0475305" => :sierra
