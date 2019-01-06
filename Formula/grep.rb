6b4e8a5387f91bd1126b146d8e4ac468bd2061f7
Michka Popoff <michkapopoff@gmail.com>
2019-01-06 19:24:34 +0100

    grep: Fix test for Linuxbrew
    Closes Linuxbrew/homebrew-core#11019.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/grep.rb
+++ Formula/grep.rb
@@ -37,26 +37,35 @@ class Grep < Formula
     end
   end
 
-  def caveats; <<~EOS
-    All commands have been installed with the prefix "g".
-    If you need to use these commands with their normal names, you
-    can add a "gnubin" directory to your PATH from your bashrc like:
-      PATH="#{opt_libexec}/gnubin:$PATH"
+  def caveats
+    return unless OS.mac?
+    <<~EOS
+      All commands have been installed with the prefix "g".
+      If you need to use these commands with their normal names, you
+      can add a "gnubin" directory to your PATH from your bashrc like:
+        PATH="#{opt_libexec}/gnubin:$PATH"
 
-    Additionally, you can access their man pages with normal names if you add
-    the "gnuman" directory to your MANPATH from your bashrc as well:
-      MANPATH="#{opt_libexec}/gnuman:$MANPATH"
-  EOS
+      Additionally, you can access their man pages with normal names if you add
+      the "gnuman" directory to your MANPATH from your bashrc as well:
+        MANPATH="#{opt_libexec}/gnuman:$MANPATH"
+    EOS
   end
 
   test do
     text_file = testpath/"file.txt"
     text_file.write "This line should be matched"
 
-    grepped = shell_output("#{bin}/ggrep match #{text_file}")
-    assert_match "should be matched", grepped
+    if OS.mac?
+      grepped = shell_output("#{bin}/ggrep match #{text_file}")
+      assert_match "should be matched", grepped
 
-    grepped = shell_output("#{opt_libexec}/gnubin/grep match #{text_file}")
-    assert_match "should be matched", grepped
+      grepped = shell_output("#{opt_libexec}/gnubin/grep match #{text_file}")
+      assert_match "should be matched", grepped
+    end
+
+    unless OS.mac?
+      grepped = shell_output("#{bin}/grep match #{text_file}")
+      assert_match "should be matched", grepped
+    end
   end
 end
