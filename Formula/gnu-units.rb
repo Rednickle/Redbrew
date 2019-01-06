9153acdca1c486986933ae8361e92f4a22622758
Michka Popoff <michkapopoff@gmail.com>
2019-01-06 19:23:30 +0100

    gnu-units: Fix test for Linuxbrew
    Closes Linuxbrew/homebrew-core#11017.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gnu-units.rb
+++ Formula/gnu-units.rb
@@ -31,21 +31,28 @@ class GnuUnits < Formula
     end
   end
 
-  def caveats; <<~EOS
-    All commands have been installed with the prefix "g".
-    If you need to use these commands with their normal names, you
-    can add a "gnubin" directory to your PATH from your bashrc like:
-      PATH="#{opt_libexec}/gnubin:$PATH"
+  def caveats
+    return unless OS.mac?
+    <<~EOS
+      All commands have been installed with the prefix "g".
+      If you need to use these commands with their normal names, you
+      can add a "gnubin" directory to your PATH from your bashrc like:
+        PATH="#{opt_libexec}/gnubin:$PATH"
 
-    Additionally, you can access their man pages with normal names if you add
-    the "gnuman" directory to your MANPATH from your bashrc as well:
-      MANPATH="#{opt_libexec}/gnuman:$MANPATH"
-  EOS
+      Additionally, you can access their man pages with normal names if you add
+      the "gnuman" directory to your MANPATH from your bashrc as well:
+        MANPATH="#{opt_libexec}/gnuman:$MANPATH"
+    EOS
   end
 
   test do
-    units = OS.mac? ? "gunits" : "units"
-    assert_equal "* 18288", shell_output("#{bin}/#{units} '600 feet' 'cm' -1").strip
-    assert_equal "* 18288", shell_output("#{opt_libexec}/gnubin/units '600 feet' 'cm' -1").strip
+    if OS.mac?
+      assert_equal "* 18288", shell_output("#{bin}/gunits '600 feet' 'cm' -1").strip
+      assert_equal "* 18288", shell_output("#{opt_libexec}/gnubin/units '600 feet' 'cm' -1").strip
+    end
+
+    unless OS.mac?
+      assert_equal "* 18288", shell_output("#{bin}/units '600 feet' 'cm' -1").strip
+    end
   end
 end
