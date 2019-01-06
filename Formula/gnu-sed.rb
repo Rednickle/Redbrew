b7dac1b7f5cc633266182ed201d9772c9c89fdc6
Michka Popoff <michkapopoff@gmail.com>
2019-01-06 19:21:57 +0100

    gnu-sed: Fix test for Linuxbrew
    Closes Linuxbrew/homebrew-core#11014.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gnu-sed.rb
+++ Formula/gnu-sed.rb
@@ -6,7 +6,6 @@ class GnuSed < Formula
   sha256 "2885768cd0a29ff8d58a6280a270ff161f6a3deb5690b2be6c49f46d4c67bd6a"
 
   bottle do
-    cellar :any_skip_relocation
     rebuild 1
     sha256 "a1d45377723857fe063e64d19023cce633c8abd2a40b96e1331ebac93d07e08f" => :mojave
     sha256 "9a8f2e31cb6ab729429ba4bf5c7fb0f9333cda7a78d7e6f052cf3534ab4cbddc" => :high_sierra
@@ -32,26 +31,35 @@ class GnuSed < Formula
     end
   end
 
-  def caveats; <<~EOS
-    GNU "sed" has been installed as "gsed".
-    If you need to use it as "sed", you can add a "gnubin" directory
-    to your PATH from your bashrc like:
+  def caveats
+    return unless OS.mac?
+    <<~EOS
+      GNU "sed" has been installed as "gsed".
+      If you need to use it as "sed", you can add a "gnubin" directory
+      to your PATH from your bashrc like:
 
-        PATH="#{opt_libexec}/gnubin:$PATH"
+          PATH="#{opt_libexec}/gnubin:$PATH"
 
-    Additionally, you can access its man page with normal name if you add
-    the "gnuman" directory to your MANPATH from your bashrc as well:
+      Additionally, you can access its man page with normal name if you add
+      the "gnuman" directory to your MANPATH from your bashrc as well:
 
-        MANPATH="#{opt_libexec}/gnuman:$MANPATH"
-  EOS
+          MANPATH="#{opt_libexec}/gnuman:$MANPATH"
+    EOS
   end
 
   test do
     (testpath/"test.txt").write "Hello world!"
-    system "#{bin}/gsed", "-i", "s/world/World/g", "test.txt"
-    assert_match /Hello World!/, File.read("test.txt")
+    if OS.mac?
+      system "#{bin}/gsed", "-i", "s/world/World/g", "test.txt"
+      assert_match /Hello World!/, File.read("test.txt")
 
-    system "#{opt_libexec}/gnubin/sed", "-i", "s/world/World/g", "test.txt"
-    assert_match /Hello World!/, File.read("test.txt")
+      system "#{opt_libexec}/gnubin/sed", "-i", "s/world/World/g", "test.txt"
+      assert_match /Hello World!/, File.read("test.txt")
+    end
+
+    unless OS.mac?
+      system "#{bin}/sed", "-i", "s/world/World/g", "test.txt"
+      assert_match /Hello World!/, File.read("test.txt")
+    end
   end
 end
