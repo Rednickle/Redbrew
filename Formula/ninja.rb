633889abc8be52f49d4a810f4dfa468b20c490c9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-08 22:48:01 +0000

    ninja: update 1.8.2_2 bottle for Linuxbrew.

--- Formula/ninja.rb
+++ Formula/ninja.rb
@@ -12,6 +12,7 @@ class Ninja < Formula
     sha256 "eeba4fff08b3ed4b308250fb650f7d06630acd18465900ba0e27cecfe925a6cc" => :high_sierra
     sha256 "90ecf90948f0fa65c82011d79338d7c5ca2a4d0cb7cb8dc3892243f749fbe2eb" => :sierra
     sha256 "675165ce642fa811e1a0a363be0ba66a7b907d46056f89fd20938aa33e7d59f7" => :el_capitan
+    sha256 "38075ae7b8a5a135ec3cec0ea07b8158e142323441ab84f736980cf60ae6c6b9" => :x86_64_linux
   end
 
   depends_on "python@2" unless OS.mac?
