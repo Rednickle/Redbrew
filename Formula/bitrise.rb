c5dd12f84b3a45e72bf7870effa4af423e3132bd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-09 13:00:34 +0000

    bitrise: update 1.26.0 bottle for Linuxbrew.

--- Formula/bitrise.rb
+++ Formula/bitrise.rb
@@ -9,6 +9,7 @@ class Bitrise < Formula
     sha256 "f9bb239428a743afe7a42c08b9b30adddd01f91ded3dd8cb0a5dacd73a14619f" => :mojave
     sha256 "d72fe08e0b34123e086d0cd0876f76927e9832f411c7ea64e86da5e76f453c9e" => :high_sierra
     sha256 "4b5b414f71e0e990698f69df836dbe135ef04edbf3759e078847bf8dc58d46dc" => :sierra
+    sha256 "34ae0428592ef80b13ffefc708371331d693cf0f66a04de498e26c6d114d79ed" => :x86_64_linux
   end
 
   depends_on "go" => :build
