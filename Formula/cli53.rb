ecd5e279bec17e7d1e4b558d43b1cde772302a4d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-10 21:43:57 +0000

    cli53: update 0.8.13 bottle for Linuxbrew.

--- Formula/cli53.rb
+++ Formula/cli53.rb
@@ -9,6 +9,7 @@ class Cli53 < Formula
     sha256 "6638c669af3f120a6b641706702dc529f34836e2a7776b724ad4acead6aaeed9" => :mojave
     sha256 "e39a63d966ad1e667711f5d4ae7e344785d9aedf659dea7fb8460b737c07a60a" => :high_sierra
     sha256 "04b4f7c4b3e8c55230e358cdb32ea3844652946769cc81121e3688cd2f1c3918" => :sierra
+    sha256 "29cae6f6460d601c57cc71a906de19e32eca777af2053cc8ac35a3bb82b63cc5" => :x86_64_linux
   end
 
   depends_on "go" => :build
