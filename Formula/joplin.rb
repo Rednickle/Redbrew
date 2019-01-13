5f3abdb23d594050d32e18b25513f52d71e56093
Michka Popoff <michkapopoff@gmail.com>
2019-01-13 15:56:38 +0100

    joplin: Fix bottling for Linuxbrew
    Closes Linuxbrew/homebrew-core#11155.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/joplin.rb
+++ Formula/joplin.rb
@@ -7,6 +7,7 @@ class Joplin < Formula
   sha256 "80a96934f099ef45a35389184de718d683aabdd9b3371c179c0ae6fee5db9ab2"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "89d6e6d3c5cafb4684641ec30fae57038b4324d0b2bdd31ec9486b5f77cbbb3e" => :mojave
     sha256 "cfa458d0fc2ed1d8b304914cd1e1453ceb9d02e100a201bc627c3f18b5720b38" => :high_sierra
     sha256 "14d061ecac989c64beada4ce6b9d7414c7f9303f860998855a0d24d04b1617a4" => :sierra
