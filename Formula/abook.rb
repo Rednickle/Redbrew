a62ce2365a9bdd52a2c48fda687fb7f5b830dae2
Michka Popoff <michkapopoff@gmail.com>
2019-01-13 16:18:43 +0000

    abook: update 0.5.6_2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#11170.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/abook.rb
+++ Formula/abook.rb
@@ -10,6 +10,7 @@ class Abook < Formula
     sha256 "c1b909e5047e584971993e46ac28956479f1aca7edd28822df6de649fdb17bce" => :mojave
     sha256 "6dd4fd8e2f57239376ccbe02bc606829d0b976b18f94ae6e5204a7d546ae9a04" => :high_sierra
     sha256 "b078b7af5c5fca8c97e693b70a0700ab91d9bed44bdccbf037ed5eb800c32d7b" => :sierra
+    sha256 "42fcad33e407e37d155fa9d795f2c518ef8e634b13be01e5f91994c6bce2eb78" => :x86_64_linux
   end
 
   depends_on "readline"
