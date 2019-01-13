c50c0f5f4de6d875a653deba1d17590f54a375e0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-13 15:40:59 +0000

    joplin: update 1.0.120 bottle for Linuxbrew.

--- Formula/joplin.rb
+++ Formula/joplin.rb
@@ -11,6 +11,7 @@ class Joplin < Formula
     sha256 "89d6e6d3c5cafb4684641ec30fae57038b4324d0b2bdd31ec9486b5f77cbbb3e" => :mojave
     sha256 "cfa458d0fc2ed1d8b304914cd1e1453ceb9d02e100a201bc627c3f18b5720b38" => :high_sierra
     sha256 "14d061ecac989c64beada4ce6b9d7414c7f9303f860998855a0d24d04b1617a4" => :sierra
+    sha256 "0f32d362f44f522f3bdeb8edca5caeb1a8f9c56fbf42914593c09616beef9bc5" => :x86_64_linux
   end
 
   depends_on "python@2" => :build
