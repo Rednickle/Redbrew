e0170120ae07e3f7ebe3050d945f02b0e290e204
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-14 18:51:27 +0000

    mruby: update 2.0.0_1 bottle for Linuxbrew.

--- Formula/mruby.rb
+++ Formula/mruby.rb
@@ -11,6 +11,7 @@ class Mruby < Formula
     sha256 "3a1dcc1710f107e993a38b3262c7235f71cf12a80ca7a5dd0cd3c579430b7a20" => :mojave
     sha256 "235c0c374c2dce600e00ac2b95fa7575a541a725f0c55e06bd3f7577b5309ed8" => :high_sierra
     sha256 "5d41f43e7524997f9bcba2ca181dc838e47543941fc44ec686460ef7c675754c" => :sierra
+    sha256 "b45110478a84b22dcdc7a2c0003228795ec8079b35f94f9434c69a95045f57a2" => :x86_64_linux
   end
 
   depends_on "bison" => :build
