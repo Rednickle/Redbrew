c5edb45c661baadc44528d4022188e1f50c1f949
Michka Popoff <michkapopoff@gmail.com>
2019-01-14 19:19:18 +0100

    mruby: bump revision for readline
    Closes Linuxbrew/homebrew-core#11223.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mruby.rb
+++ Formula/mruby.rb
@@ -4,13 +4,13 @@ class Mruby < Formula
   url "https://github.com/mruby/mruby/archive/2.0.0.tar.gz"
   sha256 "fa495898d51130c69480a13e90df5dc18cb1a9d9a31836268a895989d902048f"
   head "https://github.com/mruby/mruby.git"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
     sha256 "3a1dcc1710f107e993a38b3262c7235f71cf12a80ca7a5dd0cd3c579430b7a20" => :mojave
     sha256 "235c0c374c2dce600e00ac2b95fa7575a541a725f0c55e06bd3f7577b5309ed8" => :high_sierra
     sha256 "5d41f43e7524997f9bcba2ca181dc838e47543941fc44ec686460ef7c675754c" => :sierra
-    sha256 "27ac59355313fec745b4537112e49162decc0782f206a1256d36a49764ee58a5" => :x86_64_linux
   end
 
   depends_on "bison" => :build
