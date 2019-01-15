699c6b8d48ba1e625b21422dc1c24e5169e90f86
Michka Popoff <michkapopoff@gmail.com>
2019-01-15 20:54:22 +0000

    tokei: update 9.0.0 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#11241.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/tokei.rb
+++ Formula/tokei.rb
@@ -9,6 +9,7 @@ class Tokei < Formula
     sha256 "6cedff1d132d6b496c803ccb6bf8381ff05363aaa30aa70bbc90ecb742496d4d" => :mojave
     sha256 "3b8f70b7be47c0c18c23a8b8a4e9c968d72e7c00be1dad633837e28b7a701994" => :high_sierra
     sha256 "440d1b527443015ef937933d7755a2242cfd88370250fc2948458b4f5fc64179" => :sierra
+    sha256 "06df5268354cfec76348850707971f912532f35543a63823923bfff8ac99f35b" => :x86_64_linux
   end
 
   depends_on "rust" => :build
