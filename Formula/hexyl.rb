ff6bd93d0b73973d0346e84ed5845ad69d24c076
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-16 13:10:30 +0000

    hexyl: update 0.4.0 bottle for Linuxbrew.

--- Formula/hexyl.rb
+++ Formula/hexyl.rb
@@ -9,6 +9,7 @@ class Hexyl < Formula
     sha256 "e05935d8bb534362f8f0c695084550dd39cac6b53506131e16b5e41ee8121dc8" => :mojave
     sha256 "a39ed75bc657df2dd48d78a4404619ecfc3920921e5668bd50e78125809696bb" => :high_sierra
     sha256 "af20c59d4422118f40dfcb768d458c43ed9eff46c3e0fa11e589cbafe0cd1331" => :sierra
+    sha256 "b1f50cc13575b6353fc487d42e1932536e49a07b8391828dedf95c9c863d1d24" => :x86_64_linux
   end
 
   depends_on "rust" => :build
