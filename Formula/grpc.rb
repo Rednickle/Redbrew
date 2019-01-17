f89b562302718c9ef79cd95c9466bd445086aba5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-17 21:29:42 +0000

    grpc: update 1.18.0 bottle for Linuxbrew.

--- Formula/grpc.rb
+++ Formula/grpc.rb
@@ -9,6 +9,7 @@ class Grpc < Formula
     sha256 "3783e11c4cb45ca91e26bd638f6f4280d25cb0814f0478980525bc08422940c9" => :mojave
     sha256 "7b50db8d76643f1d34ae06a32d7942021091aaeed05257f5ff6dbc19d6dc90e3" => :high_sierra
     sha256 "c45105cc0a7ad77b4ed46b8025a7a3dfa20da120b29fcb017b6d1a8e1610f71e" => :sierra
+    sha256 "f1aab799f010c2344eb6d332635a2a57aeb57f6a89c425fe9b4b5735cf85a87f" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
