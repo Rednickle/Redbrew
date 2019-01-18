126f196bed8d2d145fd8c10b27edc2541e363983
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-18 05:51:48 +0000

    ethereum: update 1.8.21 bottle for Linuxbrew.

--- Formula/ethereum.rb
+++ Formula/ethereum.rb
@@ -10,6 +10,7 @@ class Ethereum < Formula
     sha256 "088a8f9f6f35433e66c2b6ad8dadfcbb0c25b8d3cbbbee2aba5020566c6709fd" => :mojave
     sha256 "0a4fd03dc6151d5f3453192bca228cbcad425130be21e44c987596956e172746" => :high_sierra
     sha256 "7be903f50774d4bea67b7e464033f22f53478f94266c2dd96c618310bf93d4a7" => :sierra
+    sha256 "8de25637331a836d56bb6c70afc1b5bf1afa875a4e7c40656a76c39a3219c6b7" => :x86_64_linux
   end
 
   depends_on "go" => :build
