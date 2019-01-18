c70284e2021fdc7ef486ceed33a4366e857558a4
Michka Popoff <michkapopoff@gmail.com>
2019-01-18 20:05:27 +0100

    teleport: Fix build for Linuxbrew
    Closes Linuxbrew/homebrew-core#11268.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -12,12 +12,21 @@ class Teleport < Formula
   end
 
   depends_on "go" => :build
+  unless OS.mac?
+    depends_on "zip"
+    depends_on "curl" => :test
+    depends_on "netcat" => :test
+  end
 
   conflicts_with "etsh", :because => "both install `tsh` binaries"
 
   def install
-    ENV["GOOS"] = "darwin"
-    ENV["GOARCH"] = MacOS.prefer_64_bit? ? "amd64" : "386"
+    ENV["GOOS"] = OS.mac? ? "darwin" : "linux"
+    if OS.mac?
+      ENV["GOARCH"] = MacOS.prefer_64_bit? ? "amd64" : "386"
+    else
+      ENV["GOARCH"] = "amd64"
+    end
     ENV["GOPATH"] = buildpath
     ENV["GOROOT"] = Formula["go"].opt_libexec
 
@@ -25,6 +34,7 @@ class Teleport < Formula
     cd "src/github.com/gravitational/teleport" do
       ENV.deparallelize { system "make", "full" }
       bin.install Dir["build/*"]
+      (prefix/"web").install "web/dist" unless OS.mac?
       prefix.install_metafiles
     end
   end
@@ -36,13 +46,25 @@ class Teleport < Formula
       .gsub("/var/lib/teleport", testpath)
       .gsub("/var/run", testpath)
       .gsub(/https_(.*)/, "")
+    unless OS.mac?
+      inreplace testpath/"config.yml", "/usr/bin/hostname", "/bin/hostname"
+      inreplace testpath/"config.yml", "/usr/bin/uname", "/bin/uname"
+    end
     begin
-      pid = spawn("#{bin}/teleport start -c #{testpath}/config.yml")
-      sleep 5
-      system "/usr/bin/curl", "--insecure", "https://localhost:3080"
-      system "/usr/bin/nc", "-z", "localhost", "3022"
-      system "/usr/bin/nc", "-z", "localhost", "3023"
-      system "/usr/bin/nc", "-z", "localhost", "3025"
+      debug = OS.mac? ? "" : "DEBUG=1 "
+      pid = spawn("#{debug}#{bin}/teleport start -c #{testpath}/config.yml")
+      if OS.mac?
+        sleep 5
+        path = OS.mac? ? "/usr/bin/" : ""
+        system "#{path}curl", "--insecure", "https://localhost:3080"
+        # Fails on Linux:
+        # Failed to update cache: \nERROR REPORT:\nOriginal Error:
+        # *trace.NotFoundError open /tmp/teleport-test-20190120-15973-1hx2ui3/cache/auth/localCluster:
+        # no such file or directory
+        system "#{path}nc", "-z", "localhost", "3022"
+        system "#{path}nc", "-z", "localhost", "3023"
+        system "#{path}nc", "-z", "localhost", "3025"
+      end
     ensure
       Process.kill(9, pid)
     end
