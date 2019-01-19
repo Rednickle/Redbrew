fb1070f3742c122b3f42f9f6a04bc97f773c9263
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-19 20:04:05 +0000

    open-babel: update 2.4.1_1 bottle for Linuxbrew.

--- Formula/open-babel.rb
+++ Formula/open-babel.rb
@@ -12,6 +12,7 @@ class OpenBabel < Formula
     sha256 "61c3b83067ef6ecff739ad64af1f7dee20c66459838f2584b5ace1077261cbef" => :mojave
     sha256 "0383df12c965d3e2b0087812186992b32ed6d02fc7bcfef397b071db2b96e568" => :high_sierra
     sha256 "fac962fa1127ac9476e5dd1c44ba593c6184bb49444eac28804e45609243e8f9" => :sierra
+    sha256 "98c3032953a47edab4774faac9e8a2506cd58a79003f355c217bbad61a3d99e4" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
