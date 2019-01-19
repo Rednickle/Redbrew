704e0ef60a41344b9368d6551aa0da56f8f4eaf7
Michka Popoff <michkapopoff@gmail.com>
2019-01-19 18:15:45 +0000

    bower: update 1.8.7 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#11297.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/bower.rb
+++ Formula/bower.rb
@@ -12,6 +12,7 @@ class Bower < Formula
     sha256 "3b68e3317ee8d103693a9782152bb933a47ecd17a2a463dd7551e30c142d1280" => :mojave
     sha256 "3f84d1611116dbf28a68ac3dca79ba9c346b533da631066b8f3d48777074a765" => :high_sierra
     sha256 "bcfe567e0ed7cb3181fc35d5fcbbed408157ff341918283e7b73ed39eb9f20ce" => :sierra
+    sha256 "303c68a942eaf6148fb5c8ad6e1c95ce999d16fd967b2291ae1fb6a0dfa26c25" => :x86_64_linux
   end
 
   depends_on "node"
