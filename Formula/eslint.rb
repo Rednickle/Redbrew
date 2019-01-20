20ac0c0240b2b671a923ed1683e5d2eda1a8bce0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-20 15:16:46 +0000

    eslint: update 5.12.1 bottle for Linuxbrew.

--- Formula/eslint.rb
+++ Formula/eslint.rb
@@ -11,6 +11,7 @@ class Eslint < Formula
     sha256 "041f885262859ebd6c49270e700619c58acf062ec9c76ab353b23cebc2e4b2aa" => :mojave
     sha256 "493cb14ede50f54bea0e73767e091b40673a175fb0e9e32e76b768a29bc9973b" => :high_sierra
     sha256 "6ddf629a67dffcb1b625c003197193b113ad50913a662587d4c700d74151ef37" => :sierra
+    sha256 "970b78410e4da3af5625fda202e6c5fcfa19e326a34121ba9adea29fc3bcefcf" => :x86_64_linux
   end
 
   depends_on "node"
