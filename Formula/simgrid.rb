1c253ee47c1ade894bfa304746ea804c24147c03
Michka Popoff <michkapopoff@gmail.com>
2019-01-25 23:41:33 +0100

    simgrid: revision for boost
    Closes Linuxbrew/homebrew-core#11432.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/simgrid.rb
+++ Formula/simgrid.rb
@@ -3,13 +3,13 @@ class Simgrid < Formula
   homepage "http://simgrid.gforge.inria.fr"
   url "https://gforge.inria.fr/frs/download.php/file/37602/SimGrid-3.20.tar.gz"
   sha256 "4d4757eb45d87cf18d990d589c31d223b0ea8cf6fcd8c94fca4d38162193cef6"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "a9c5cb5da50ab2b224e5d1882834d1e7c4428393e05bec3731823597a2da30e9" => :mojave
     sha256 "75ce382a395f963425946d7f53361fa1143a2b6ca9292c63193e76139bcef91e" => :high_sierra
     sha256 "58895cb0aed9235972377d2460a18c8bc03203212ae8246450c429e837a01bcf" => :sierra
     sha256 "c0a6cf0fba37da1c281cac508bdde38aa4145fe7c50bcc9f466f5521cf95d722" => :el_capitan
-    sha256 "be03b776d478cde289e3bc65f7612a4a8212165c81873ddc3361b4a91b6bdb96" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
