a02ec2e801bc2448260f1af177bce65eacfafd7e
Michka Popoff <michkapopoff@gmail.com>
2019-01-25 19:52:00 +0000

    prettier: update 1.16.1 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#11397.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -11,6 +11,7 @@ class Prettier < Formula
     sha256 "22ed4c90ba994cfa12efdf61d9b56593004d7444906100244eb24236d1c8a422" => :mojave
     sha256 "445d7d52c2aeeb3a322ed37ceec6fb36ac72e50d2a4917b280ec7e58f647f6b8" => :high_sierra
     sha256 "f40d6fe0399f8e89e815f4a2a1d37076f3a56cfc07e9d825a072c4bbfddb8e38" => :sierra
+    sha256 "5733e481d7316df078c1d38afac58a053ffd0f416aa90cab649e17294e0f3b2f" => :x86_64_linux
   end
 
   depends_on "node"
