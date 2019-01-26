14bb4e4ac496e5f9b31687280fb8d56c3c372bcc
Michka Popoff <michkapopoff@gmail.com>
2019-01-26 08:32:45 +0000

    ucloud: update 0.1.8 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#11400.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ucloud.rb
+++ Formula/ucloud.rb
@@ -9,6 +9,7 @@ class Ucloud < Formula
     sha256 "cdf812d40e2c01fee77b5ea9711926578997a9cdc7a77132209a541f6c6814d7" => :mojave
     sha256 "ba282439388b4eb3964455be63b85304838548b56907dc5fe84ee246056bd4a9" => :high_sierra
     sha256 "a11eaa78c9de05bb4d6cd5e948c22d62e9fbe633cf2db04e9c73c7962ea3986b" => :sierra
+    sha256 "0ac888eae28fc7b33d4e9f88ba89af3da6c18b608b377fbcf481ce7ab1ef2b6b" => :x86_64_linux
   end
 
   depends_on "go" => :build
