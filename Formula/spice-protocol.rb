4dfa90686097d4e6def10c58275554d0c3b21e09
Michka Popoff <michkapopoff@gmail.com>
2019-01-28 20:21:41 +0000

    spice-protocol: update 0.12.15 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#11452.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/spice-protocol.rb
+++ Formula/spice-protocol.rb
@@ -9,6 +9,7 @@ class SpiceProtocol < Formula
     sha256 "b899ebffaf616bb0801dd3ad1b0f87b9b05b756257875a3311534c4408aecc08" => :mojave
     sha256 "0d946742733d9edd6fcb9c2b6ca5c50dda97655cec7e1baa4f4032875e4eed4e" => :high_sierra
     sha256 "0d946742733d9edd6fcb9c2b6ca5c50dda97655cec7e1baa4f4032875e4eed4e" => :sierra
+    sha256 "61d3a4afa80c58f3dd4bd41e8791d71b10b340196a3b4c6fa4ebebd3473f27d3" => :x86_64_linux
   end
 
   def install
