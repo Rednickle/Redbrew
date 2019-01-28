bf6c646eb8f0ebdd2f9b9fca8f544bc825c7d611
Hielke Walinga <hielkewalinga@gmail.com>
2019-01-28 20:59:08 +0100

    prodigal: Add patch to prodigal
    Prodigal will have incorrect output if installed with a wrong compiler.
    Also see:
    https://github.com/hyattpd/Prodigal/issues/34
    https://github.com/hyattpd/Prodigal/issues/41
    
    Signed-off-by: Hielke Walinga <hielkewalinga@gmail.com>
    
    Closes Linuxbrew/homebrew-core#11474.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/prodigal.rb
+++ Formula/prodigal.rb
@@ -3,6 +3,7 @@ class Prodigal < Formula
   homepage "https://github.com/hyattpd/Prodigal"
   url "https://github.com/hyattpd/Prodigal/archive/v2.6.3.tar.gz"
   sha256 "89094ad4bff5a8a8732d899f31cec350f5a4c27bcbdd12663f87c9d1f0ec599f"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -10,9 +11,18 @@ class Prodigal < Formula
     sha256 "c120fed8e29bb3b1a4ff69d5ca05e051a0fe3822784b3d585e142da3452d1ac1" => :high_sierra
     sha256 "a27fe5316181d4826e5aa5291d0fc1b1a7087c32c7b4e6aedabf1209d5a8ac36" => :sierra
     sha256 "70b432e3d3da1f4089680b06c0745b7dac3611f05d8ec9440faa918bc82d6fe5" => :el_capitan
-    sha256 "8ed04aa4b59962ddfeb1ff3a4bd58355071f7a1a33f7614f28425aaed39873c2" => :x86_64_linux
   end
 
+  # Prodigal will have incorrect output if compiled with certain compilers.
+  # This will be fixed in the next release. Also see:
+  # https://github.com/hyattpd/Prodigal/issues/34
+  # https://github.com/hyattpd/Prodigal/issues/41
+  unless OS.mac?
+    patch do
+      url "https://github.com/hyattpd/Prodigal/pull/35.patch?full_index=1"
+      sha256 "fd292c0a98412a7f2ed06d86e0e3f96a9ad698f6772990321ad56985323b99a6"
+    end
+  end
   def install
     system "make", "install", "INSTALLDIR=#{bin}"
   end
