5f3b33f96d24618b6435c700d5f03e33a3a86186
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-29 22:32:45 +0000

    goreleaser: update 0.99.0 bottle for Linuxbrew.

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -9,6 +9,7 @@ class Goreleaser < Formula
     sha256 "a207727a45c8c11a8699e286a8fa8c09053f3ce233cad65b368e84b0957efe0e" => :mojave
     sha256 "11c8a13e3484d117ee47f06fb15056296b81702a2802eb66312a19b5a82f1765" => :high_sierra
     sha256 "04141a75c6fd616b016deea86a53c35def257ec0208fcc3cd1968a1b16a9b946" => :sierra
+    sha256 "bf3c88998b1174194d0b5837d99a2079ad48a415730fcf7106c9c56f2af22b6a" => :x86_64_linux
   end
 
   depends_on "go" => :build
