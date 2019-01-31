5af25ade2e6985b40c787a9584f9986a72098dab
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-31 20:16:30 +0000

    fonttools: update 3.37.0 bottle for Linuxbrew.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -12,6 +12,7 @@ class Fonttools < Formula
     sha256 "eb0f97af2b0b70eb888c707757fa8ed22822e3d67759906ad727005c042d4439" => :mojave
     sha256 "4e60d504a5a24d1477a700f1399830d0805b437b7bc8e98dce27274288a4ca2c" => :high_sierra
     sha256 "728c5f5362415eb14d5ed291c27cc6939851e9913073fab0276aa143556e861e" => :sierra
+    sha256 "1713f89f4e30435cc1b1372056b41dce07f55aff512d12bfad3425169f376c70" => :x86_64_linux
   end
 
   depends_on "python"
