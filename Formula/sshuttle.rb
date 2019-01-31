348ea571efd278a0a364c79279ec8f7942d86ecf
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-01-31 12:37:33 +0000

    sshuttle: update 0.78.5 bottle for Linuxbrew.

--- Formula/sshuttle.rb
+++ Formula/sshuttle.rb
@@ -13,6 +13,7 @@ class Sshuttle < Formula
     sha256 "dff7259d647fd392e53de03660ece7be96452882938f387c199938f8054f4c2b" => :mojave
     sha256 "28f3d7fc8858779cb6020908302fb185810d574e8023e935b8410d0660973d49" => :high_sierra
     sha256 "04e8f60acb5131f58a0fbe66d7c8d847c4d423db3edb2671ff1319fc499a2c5f" => :sierra
+    sha256 "05999eb4b26f48b94a43d32e0835501ea9498b1b2cedf98ec46cc8a84b251fd0" => :x86_64_linux
   end
 
   depends_on "python"
