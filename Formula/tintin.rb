c83f987367af7e86a2829b116c73dc1d4eea6d0f
Michka Popoff <michkapopoff@gmail.com>
2019-02-03 23:31:22 +0000

    tintin: update 2.01.6 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#11505.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/tintin.rb
+++ Formula/tintin.rb
@@ -9,6 +9,7 @@ class Tintin < Formula
     sha256 "0095d467450f7e04547555ce696ab3c1237957522b5ad71abe344a8ebfbcde10" => :mojave
     sha256 "ab8ce7088ab4406f546b2875b47ba06ede4cdbfc3d83e698081343b3fcaa8ff8" => :high_sierra
     sha256 "c3c5c1bdc641d73852bbe723f6c6e441c4dc01ee4aaa84bb3bd584f0825ce9e4" => :sierra
+    sha256 "502a5ac1b5ba085712b4c9c444f7e50f9adc1ef6f261081c61b3a53079c0f686" => :x86_64_linux
   end
 
   depends_on "gnutls"
