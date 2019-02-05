2cce18a2c6b3060ada2f0ec662a6ef3db95e3f0b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-05 18:26:29 +0000

    octave: update 4.4.1_5 bottle for Linuxbrew.

--- Formula/octave.rb
+++ Formula/octave.rb
@@ -11,6 +11,7 @@ class Octave < Formula
     sha256 "b28615ae5e36a3d3202256305cba518f39de19bebb6a9635f8acfe868c67a3aa" => :mojave
     sha256 "266fbf84557ec6c4d9d672143ced419d29af3b83827aba7df191bc567ba13ba1" => :high_sierra
     sha256 "7af138a36e98c7adbb8ab2a7dc6e39dd60f28ae2b1524d8b5ff433958304af34" => :sierra
+    sha256 "57f7ed4e4a192f02023710095ffc8f2b1aa5ad6973ede426843da59922be5ce8" => :x86_64_linux
   end
 
   head do
