c129e7d71ab75ab162c7f981413bf74533390ef2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-09 15:39:33 +0000

    firebase-cli: update 6.3.1 bottle for Linuxbrew.

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -12,6 +12,7 @@ class FirebaseCli < Formula
     sha256 "003cadee665ee69164b1bc80918435f5a77d7d5db0b8b9c7fd88e0b2ed9fc90f" => :mojave
     sha256 "c7ce479a743eb8dec38a6b68452f5b1c4b75097b25e0b64b8774b499f348d922" => :high_sierra
     sha256 "1960619984940a77aa7a1e6011ab3cc424b635d57326c2fc4214eb5eb7a3b3f0" => :sierra
+    sha256 "10769632e6ae99aa24f9a7a07657eecc66ac72028eb9a59842f1077cc0f3c682" => :x86_64_linux
   end
 
   depends_on "node"
