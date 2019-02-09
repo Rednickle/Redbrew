51fbb10a46285e108705095a7b31f20c96696b85
Michka Popoff <michkapopoff@gmail.com>
2019-02-09 14:09:38 +0100

    bit: Fix bottling for Linuxbrew
    Closes Linuxbrew/homebrew-core#11566.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -8,6 +8,7 @@ class Bit < Formula
   head "https://github.com/teambit/bit.git"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "42420a8e139f35c5ce18013429dc96a7e8515c06af7ac8c33c479b6144975605" => :mojave
     sha256 "a34e6272d52e85f801f622e2d8ff29c61b9720baf2452ef806b1e51d05cfa8dc" => :high_sierra
     sha256 "bdbca7d3cb50604dadd11fadbdb5045208b946404a8c4a0f6a1a44a2c942e3e2" => :sierra
