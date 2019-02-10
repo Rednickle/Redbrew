6303ea3a843f316297da0ea7cae48d6cbadf8f20
Michka Popoff <michkapopoff@gmail.com>
2019-02-10 22:22:40 +0100

    htop: Fix bottling for Linuxbrew
    Closes Linuxbrew/homebrew-core#11582.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/htop.rb
+++ Formula/htop.rb
@@ -6,6 +6,7 @@ class Htop < Formula
   revision 1
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "77aa302765353b4085dcad52356d3264183e06310dda8d5bac64642299ea2902" => :mojave
     sha256 "0ebfb655b91566ba31f8effc94d642a43305ff95bdc9b30b46fadc132e2ced0c" => :high_sierra
     sha256 "ed93b86f011de155c5d261b8c9cc9cb81fd0017667bf3ebe26ee090716bcd650" => :sierra
