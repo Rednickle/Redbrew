1cb77725cef144382bce60aae140dd2190e6c161
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-11 21:30:44 +0000

    libgsm: update 1.0.18 bottle for Linuxbrew.

--- Formula/libgsm.rb
+++ Formula/libgsm.rb
@@ -10,6 +10,7 @@ class Libgsm < Formula
     sha256 "430c06d59d788bae3b9081924d68bb8c800cf0792fb533219e0e6c94887a0e22" => :high_sierra
     sha256 "1efa5fae6b9cf7bc802a42845836522bdb89fa0acf5bae88ca36dd75b823a5de" => :sierra
     sha256 "72ab3562f8bafc91a0c6dd0149956ed1c1e97ddacb409a695e2b4581317a9260" => :el_capitan
+    sha256 "c95a5e6a0e04462f48441b4934e5905b08ebc1d8f27187ff8ce82084fe7882fd" => :x86_64_linux
   end
 
   if OS.mac?
