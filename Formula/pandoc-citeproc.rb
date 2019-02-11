8d5ec7e1aef825cac667e6a462bf97c47a87df76
Michka Popoff <michkapopoff@gmail.com>
2019-02-11 05:48:54 +0000

    pandoc-citeproc: update 0.16.0.2 bottle for Linuxbrew.
    Closes Linuxbrew/homebrew-core#11594.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pandoc-citeproc.rb
+++ Formula/pandoc-citeproc.rb
@@ -13,6 +13,7 @@ class PandocCiteproc < Formula
     sha256 "ffe30c1bc54c788b3d9b05856fa7c74da14af1ee46dedc5e754abc69bd0afb08" => :mojave
     sha256 "b34d84d57ad30dc71ed17cd26728f3fde3b727c4dd9a3a38c15dee37e2da34a3" => :high_sierra
     sha256 "e14bc5a68ecc834a5bcda11c09d29ffee872b1022824e4b25601e77610353404" => :sierra
+    sha256 "55659fcb36ed1a481c71e9e8c8a12d778aaa24544c4c8002ce8a488af2caa682" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
