a068273fe9ba86b59ccd7c9a6efc0b54cf825c61
Michka Popoff <michkapopoff@gmail.com>
2019-02-12 22:45:13 +0100

    hadolint: Fix bottling for Linuxbrew
    Closes Linuxbrew/homebrew-core#11626.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/hadolint.rb
+++ Formula/hadolint.rb
@@ -9,7 +9,6 @@ class Hadolint < Formula
   sha256 "148b443b50a8d0c64ac10ca4a50c9b08647d27686279b630c67da395bd2a7e3a"
 
   bottle do
-    cellar :any_skip_relocation
     sha256 "f84a410f8ed431854590e1b19c74256a66df0448088a85796b379070b135684e" => :mojave
     sha256 "dd519c5d307e16452f809001d9a30e03e91c135605d15d8f39b1cd02a206f9cf" => :high_sierra
     sha256 "267c74f211d05f73804643fcefd3175ef640b971ef1b0856217022fa8e248804" => :sierra
