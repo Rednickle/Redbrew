6051f2109c5216f6425d8ee4929519de7c68fdd2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-20 21:58:44 +0000

    topgrade: update 1.7.0 bottle for Linuxbrew.

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -5,10 +5,12 @@ class Topgrade < Formula
   sha256 "e0dc1b9e50e2f023ab6922cfd2f44e27ef307dee51047800479ec2828269702b"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "0dd5089e4510f2cd5fde52693d866f4c3cb9c29083a28e325e4b90548242f270" => :mojave
     sha256 "897acde99af1b06e2b2a7747d71674a27bb5cba284fe6f89bb641e4d56f8d3a0" => :high_sierra
     sha256 "4fa220b969485c45efa9fd87ef32e139331bca6cf7b308faf2043b03124c57d0" => :sierra
+    sha256 "fa034e73f039bf8c4a23cd4f179bc7fd381c2cde6624272cdb5d39bc1bd0645b" => :x86_64_linux
   end
 
   depends_on "rust" => :build
