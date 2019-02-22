02b7b0edcc50eee0ded3db83c22a423c98aea166
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-22 19:10:01 +0000

    spatialindex: update 1.9.0 bottle for Linuxbrew.

--- Formula/spatialindex.rb
+++ Formula/spatialindex.rb
@@ -5,10 +5,12 @@ class Spatialindex < Formula
   sha256 "52d6875deea12f88e6918d192cbfd38d6e78d13f84e1fd10cca66132fa063941"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "bdda20e0607a6ac0a2bbf3ee37c0d507b7f9ede93f2210c3b33887b1682c173e" => :mojave
     sha256 "81cb9da70510a276cf06cc993f1b94aa27ce688b923aeac8170df372496bd371" => :high_sierra
     sha256 "98434cec34dcc25434dd5ea1ea669b4f4471f1b361f17dbe408c1e984eb6016f" => :sierra
+    sha256 "2d25fdb642a6827b21d13dae121e887d729dc12e558fe07c864b79bf722e26e4" => :x86_64_linux
   end
 
   def install
