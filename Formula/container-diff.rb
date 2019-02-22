a6272f9b850aad2619e49cf5f7fa67d712dd0944
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-22 23:20:36 +0000

    container-diff: update 0.15.0 bottle for Linuxbrew.

--- Formula/container-diff.rb
+++ Formula/container-diff.rb
@@ -5,10 +5,12 @@ class ContainerDiff < Formula
   sha256 "4bdd73a81b6f7a988cf270236471016525d0541f5fe04286043f3db28e4b250c"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "31af3976b5c63927f934d3155de81d6b6a241bae7244d103012d0d7cbfbeded7" => :mojave
     sha256 "6002efa7d3d475f95c9bec04896e338a99da5bd333f6fc0ccd20ca80eb6e9726" => :high_sierra
     sha256 "20eeaca03031026c546e493be1fc57560f9495f621526dae1c07fd4ac5f5d189" => :sierra
+    sha256 "602af5958215fc1d335342b60d71cc33437e9fad1994e14682b6a4aab274bc9e" => :x86_64_linux
   end
 
   depends_on "go" => :build
