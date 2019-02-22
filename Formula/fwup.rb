7bb6feb008976c5ea861f0d0479950d754995636
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-22 20:25:05 +0000

    fwup: update 1.3.0 bottle for Linuxbrew.

--- Formula/fwup.rb
+++ Formula/fwup.rb
@@ -5,10 +5,12 @@ class Fwup < Formula
   sha256 "aa273338c4b3813c7c9ffe2397a20ff88a882db2049be70c01fd3ed0657abef6"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "e4ed30081aece27325565f00be9fae461bbf50b9a9ab256d0265f72afb65265a" => :mojave
     sha256 "46e54bb9df5460b370d88cf4afb0f094b208e032b6de8a162f93696ff4e4bb9b" => :high_sierra
     sha256 "a2ee234f6351f7ce8f6028bb2ea67c00f58fe9bf055c23b0f1aee7ffd47f82f7" => :sierra
+    sha256 "1f8616fbae4c1d2d0e5b5410d18c9d76823ece22c085bb6db8e4921b80080172" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
