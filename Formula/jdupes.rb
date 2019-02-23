566cd0a9a85613c29f5b6fdcadef762d171f581a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-23 19:05:11 +0000

    jdupes: update 1.12 bottle for Linuxbrew.

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -5,10 +5,12 @@ class Jdupes < Formula
   sha256 "282d7ac60756507eec752e37747aedeaa74bc335d7c3c17c2987044520c23723"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "7e687e38fefc0938888c2a2ab4295090b2a924c7677d9201f59c6c3b71778aa1" => :mojave
     sha256 "890e0f94b9a83b36719830f64dd4df261a8837c0c8f204b5efe43a0c6b39b72b" => :high_sierra
     sha256 "9e007833b0ff6936bdf13631435004db18654d1c8ecff573134a29d23781cd1c" => :sierra
+    sha256 "a9961778fa1d7ab812d77efc86c4f7b5d3f155113c01eb22d55c11d2335cadd8" => :x86_64_linux
   end
 
   def install
