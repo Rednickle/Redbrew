7fb4a62e888089d889fd6e7148105b029758fb9d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-24 21:19:17 +0000

    afflib: update 3.7.18 bottle for Linuxbrew.

--- Formula/afflib.rb
+++ Formula/afflib.rb
@@ -5,10 +5,12 @@ class Afflib < Formula
   sha256 "5481cd5d8dbacd39d0c531a68ae8afcca3160c808770d66dcbf5e9b5be3e8199"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "8777c09fb89f6ef70f6324fe9765a2dfc1c3d9a86fcd0b583d9c8f2465be61f1" => :mojave
     sha256 "09d2326ac0d816477129b38f85af29b8f2ab42448b76f603ec77bf8b64aebb24" => :high_sierra
     sha256 "771fa58a6d3470a4a40a64b6c771533e4a5705077d9ea02a006f0d71074892b0" => :sierra
+    sha256 "10aadebd770c0fa575e2cc714b5de2f686139f115209d4ac6f5f383ae58040a9" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
