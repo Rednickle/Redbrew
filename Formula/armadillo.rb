80c0bc9e2460f8a89e4abffe4c2de21e292bae84
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-24 09:20:29 +0000

    armadillo: update 9.200.7 bottle for Linuxbrew.

--- Formula/armadillo.rb
+++ Formula/armadillo.rb
@@ -5,10 +5,12 @@ class Armadillo < Formula
   sha256 "e2787d40bcd46edf70f65ba4efd523ccb8b96a93fdb8f76da6adf1e921cb7df8"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "6652fbd21f3c18f0f8082ee7d417b6e7d2f5adf4e9a8ec662dfc048b019a5437" => :mojave
     sha256 "1511561213039eb89f3d3f837f36c742fccb59dbb06c3640343ed03b199c890a" => :high_sierra
     sha256 "8bded7bcfae6922656b810ca6085827268d92805dcd7eac9712dfe6d7c722ff7" => :sierra
+    sha256 "23482c3472af4709607e5657389652daaf76c2fcdba893d5d69f1a21977ad02c" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
