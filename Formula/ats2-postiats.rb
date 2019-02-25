4c6a009577a6198ffe5c5d34396aca828cb0bb3c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-02-25 06:30:27 +0000

    ats2-postiats: update 0.3.12 bottle for Linuxbrew.

--- Formula/ats2-postiats.rb
+++ Formula/ats2-postiats.rb
@@ -5,10 +5,12 @@ class Ats2Postiats < Formula
   sha256 "63eb02b225a11752745e8f08691140ed764288ab4ceda3710670cde24835b0d8"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "71301764b2f0f5e8e9a3dfadb3a232d758a48cc3f42a0c3c6e8588319d8a528c" => :mojave
     sha256 "009876aeeda5ff2a4294ce33b9f214a0e5fdf7e46f01bc8826702f84220bdad1" => :high_sierra
     sha256 "03fb2632e129c51dcf3da35df4094e5a699d61676f49428478fdb1899d76c006" => :sierra
+    sha256 "1901d4337497705336648139b6921b73612b390444befa9328caf5183fa49983" => :x86_64_linux
   end
 
   depends_on "gmp"
