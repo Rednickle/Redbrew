e2ad3b99d2d3d1c9755cccce64aaf9c4e70529ab
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-02 22:17:29 +0000

    fx: update 12.0.1 bottle.
    Closes #11809.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -12,6 +12,7 @@ class Fx < Formula
     sha256 "4163364b0bcaf765f82bb3fff8cab256af4baaab0ef8fc14c7396dacc362e43b" => :mojave
     sha256 "ae1d66ad8f33c2fde8af1bbe915401aa86a80977ef8c11cdf41e95e4ced5cdc1" => :high_sierra
     sha256 "3b29c04c8fb84e3f6077c18bee47ab82d525554527d3d8ca3329de176fda6b39" => :sierra
+    sha256 "744dbb61c35c8153906bfcbb188e7bde1957a29fd290b2bf7c1c06393f1ed99c" => :x86_64_linux
   end
 
   depends_on "node"
