bed185ce560f70e6b5453dbfccd87287508f5e1f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-02 21:28:13 +0000

    git: update 2.21.0 bottle.
    Closes #11795.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/git.rb
+++ Formula/git.rb
@@ -6,9 +6,11 @@ class Git < Formula
   head "https://github.com/git/git.git", :shallow => false
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     sha256 "7583e18dc171e1ff205b640456f4b2d2388a3bf6e72a0e168d07b92c22f6fc6f" => :mojave
     sha256 "216d660edef4fe016a5b25555c487d7f1555b2385d0668a2fc96bcb48cbc0c55" => :high_sierra
     sha256 "38834d0610fc44fb9246713cd0c3d90266d7d51ea0eebdc0fe0718b3c759cdab" => :sierra
+    sha256 "33c6ac006da045bc3f0042986a838068488d859ae3c706bbb4389a4614d1ec32" => :x86_64_linux
   end
 
   depends_on "gettext"
