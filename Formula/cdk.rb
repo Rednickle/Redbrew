955dbff3df57ca89fe9a91e78fe934307fe7bc80
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-02 22:06:18 +0000

    cdk: update 5.0.20190220 bottle.
    Closes #11805.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cdk.rb
+++ Formula/cdk.rb
@@ -6,10 +6,12 @@ class Cdk < Formula
   sha256 "378f9111c10933fc36bb29f52f565184daed1daf412ac17c630d1e851a830b28"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "5c3bf3e1869e3b2575c0cf2dec92224b80670315aa456dd095052efef2ed6ab2" => :mojave
     sha256 "0c3fcdf9ba94de9ac1ad148956e3e9d927964891223a2a2b32317ab8f9875104" => :high_sierra
     sha256 "c5bc4254da7f5378913c36ef878728fc865fe30fe0063317278f302247005f7b" => :sierra
+    sha256 "4e6f88afe22c775059bd874bd4b731c9b4fa3d767703676de12157b8a48265d1" => :x86_64_linux
   end
 
   depends_on "ncurses" unless OS.mac?
