adab477a7fe3746fc76b1bed1f4fcc4752e2c5a4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-03 21:30:38 +0000

    diffstat: update 1.62 bottle.
    Closes #11845.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/diffstat.rb
+++ Formula/diffstat.rb
@@ -6,10 +6,12 @@ class Diffstat < Formula
   sha256 "7f09183644ed77a156b15346bbad4e89c93543e140add9dab18747e30522591f"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "f2ddd2775174056c48eab541d32b99cfd2cc586e0227c4f2eec4b15bf5ce7128" => :mojave
     sha256 "ac1e5199d1776d52adc03842b378da475f1db1282150ed9ce22c365a5b0cf7dd" => :high_sierra
     sha256 "c6f9fd47c9736faf0cfb2f3e0ab6490e3974b4dca06d36f4bf01967c56aa1c14" => :sierra
+    sha256 "0434d91d510e208d0fae11ce3b30d074c6a6dfa4e564b2fe4a225f088076c9f7" => :x86_64_linux
   end
 
   def install
