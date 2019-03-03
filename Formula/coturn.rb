3efab4d2e245cbdc136fb2715354c289c44a8aac
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-03 08:54:02 +0000

    coturn: update 4.5.1.0 bottle.
    Closes #11819.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/coturn.rb
+++ Formula/coturn.rb
@@ -5,9 +5,11 @@ class Coturn < Formula
   sha256 "b84581a46dd40ad674f2905d680e9d0be9743fbbc001198dd498a584fd2fbd15"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     sha256 "daaa92dccec4095cc801e36318cd63dd64ab31626a21b46f4ba323766aad93b2" => :mojave
     sha256 "8f556656bccc21c5042b1fad00b6242ea783851a00141aa8cb54ff0e12cbb4fe" => :high_sierra
     sha256 "5debb5ffa15e2ecd75b14a79d4077cd2d4b47520c282033507286953226aeda0" => :sierra
+    sha256 "d08b53efdbd838654887e9c04e53828d4af65b469cc77c9e60203cb3bbe45a42" => :x86_64_linux
   end
 
   depends_on "libevent"
