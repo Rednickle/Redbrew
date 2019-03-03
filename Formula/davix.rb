7dad1ff512150bf9f8fb96ffab1b80899fef6d13
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-03 20:53:27 +0000

    davix: update 0.7.2 bottle.
    Closes #11835.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/davix.rb
+++ Formula/davix.rb
@@ -8,10 +8,12 @@ class Davix < Formula
   head "https://github.com/cern-it-sdc-id/davix.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "094a9a44f16fa66f249085a5a9c851aa683a970f32959caa0ceb3d011cfa1624" => :mojave
     sha256 "a7de5508066a1c6de8e3cfe0c8e0f48ecb642b2fd721b175ad3941872ec6e2ee" => :high_sierra
     sha256 "7c5254106571e6ccc40ae21eb5139baf328081e576dcaf4d097d835237ffe81d" => :sierra
+    sha256 "1e0abaf219df0027e05bf875155c7973c9ef957fe2acc4fd083fc757ebdafc2b" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
