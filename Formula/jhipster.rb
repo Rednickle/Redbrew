d7d490e911da2af5b3332f94f26ebdbb1e2be1c7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-04 07:17:15 +0000

    jhipster: update 5.8.2 bottle.
    Closes #11874.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -7,10 +7,12 @@ class Jhipster < Formula
   sha256 "153bfaf4e778a3797a22704d6a2d7630a1b4741131bc72352bacd075c51fed24"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "b6b825762f8c2fca4bf93bd5ff250cbbe0b1c30774ca90d9128a47595f2b9049" => :mojave
     sha256 "eed470613019026525142d32f5ac228c909dafc5ab3dae2ccce7abff6f5587d3" => :high_sierra
     sha256 "cba4b884da504ce15abaac2e692c59fa50068282d3e38ae5ec96756dafec0386" => :sierra
+    sha256 "9966af7ac6ba63dffea38e3c03bc2a44539dee2802ce66080c7d37f1f8e3c938" => :x86_64_linux
   end
 
   depends_on :java => "1.8+"
