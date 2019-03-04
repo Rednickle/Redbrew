07c14df143e6b61a350e362f2f4758cb8e7b37bb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-04 23:21:21 +0000

    dhex: update 0.69 bottle.
    Closes #11842.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dhex.rb
+++ Formula/dhex.rb
@@ -5,10 +5,12 @@ class Dhex < Formula
   sha256 "52730bcd1cf16bd4dae0de42531be9a4057535ec61ca38c0804eb8246ea6c41b"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "2d6043ce881b7b407fa40691639c25c72d73a427fcdd39a56e0683f48b16b328" => :mojave
     sha256 "bfc2aa6c73ffff61a471b4dd8e183e493781a5c34baeddffc56fa89b0542a9bd" => :high_sierra
     sha256 "b83e63ad0f1e2910e1f2495903ac4077aa5caaabe8cb2702094f42c3921c7a9c" => :sierra
+    sha256 "e9acf5875c20249ac61ecf9884713525aab0ae56d86f42155713f4151b1e72ff" => :x86_64_linux
   end
 
   depends_on "ncurses" unless OS.mac?
