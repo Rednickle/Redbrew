8aec602aa2bbea5f481beefc6d04baf37cdf00c9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-04 23:03:23 +0000

    ipfs: update 0.4.19 bottle.
    Closes #11901.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -7,10 +7,12 @@ class Ipfs < Formula
   head "https://github.com/ipfs/go-ipfs.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "76bf9fcba53fe32bf6d858e5b5d7c7d6987eaf18284d6db6ee70e88f2b052aab" => :mojave
     sha256 "d4fd3569b3947e978a997d05c7ffec4afde680ed40905578ecbb7a96a3c511d9" => :high_sierra
     sha256 "ab5a73f05d99d58f4748c29808cfdcb77916d85292fb57c16b0ec4e831a7fe3e" => :sierra
+    sha256 "655fe06ebe351bf82a0315980512e5bd54a62d7b6d271dce4f5faefccb104c7a" => :x86_64_linux
   end
 
   depends_on "go" => :build
