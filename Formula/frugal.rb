64eb0a52dd4aef467e9fd0ce7f566990e18e43ff
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-04 07:16:08 +0000

    frugal: update 2.28.0 bottle.
    Closes #11873.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -5,10 +5,12 @@ class Frugal < Formula
   sha256 "822506399ba5e619f535b9f06db2da2c3515a1c5de80be6eddc95a4c1881fb90"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "ae362b55b0c5db5f22819524e0c98e72bbf914ef5ba58f4afcb6736b55bc145b" => :mojave
     sha256 "cd5309f98aca286f9884afb3549bce4fff699feb282ec7c77ec5b13bb255bf9e" => :high_sierra
     sha256 "643ca390eb2ad6e5e494f9a34fb586c42ed45487479960f52bdbe9fc0e2d20e8" => :sierra
+    sha256 "d5237d9ec1cb10c37c27acd4b78296dac32640da5931df144d760cbf01407f74" => :x86_64_linux
   end
 
   depends_on "glide" => :build
