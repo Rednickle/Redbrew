a1dc428d5aa772dfd14d15ff48ba962562c80a13
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-05 22:38:29 +0000

    vala: update 0.42.6 bottle.
    Closes #11928.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/vala.rb
+++ Formula/vala.rb
@@ -5,9 +5,11 @@ class Vala < Formula
   sha256 "3774f46fed70f528d069beaa2de5eaeafa2851c3509856dd10030fa1f7230290"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     sha256 "97e54144bb3b93e096dae25aa1dd6dfc7cd9967dbd36c6abd3dd5823cb332f3a" => :mojave
     sha256 "c1ce33e3a66be542f39f84724851c41d7341cd1b99ce5c4a25ecf0c95ecea938" => :high_sierra
     sha256 "0f29ef805b047b66efda22d53b49c02d1b09e5b5f4c8d7eb99dc78afb63ce593" => :sierra
+    sha256 "c719aa68f38891e832743f99e89e0b4062fd9a35c2c4937081ff1b8efb182a69" => :x86_64_linux
   end
 
   depends_on "gettext"
