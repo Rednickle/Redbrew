e447e9751123f67c2d225bc35bb0cebd3f7765c4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-05 22:27:00 +0000

    vault: update 1.0.3 bottle.
    Closes #11929.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -10,10 +10,12 @@ class Vault < Formula
   head "https://github.com/hashicorp/vault.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "86ed3fa4e12a3a6fef990157c1bc60264c79c1c0a39ffaf4c3eae82856f9ac32" => :mojave
     sha256 "984e67cb1e2382b21b6c8174d3528b5612659e80793789a5c7c1ca4050cef4d7" => :high_sierra
     sha256 "97695935d05af03528a09ee83e884c3f65d86192b4b3dda9a29d282ac1dfec14" => :sierra
+    sha256 "6f204131c88c4fd6b36256427ceab28135e60b067d7ed3fbbee30f3c0a39854c" => :x86_64_linux
   end
 
   depends_on "go" => :build
