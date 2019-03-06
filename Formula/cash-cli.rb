0fbcc5cfd8a64a62b2da1ba8b92aa55ce8ea3d35
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-06 21:42:08 +0000

    cash-cli: update 3.0.0 bottle.
    Closes #11942.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cash-cli.rb
+++ Formula/cash-cli.rb
@@ -7,10 +7,12 @@ class CashCli < Formula
   sha256 "35990d2763a62bc08cb1f8c2aa0730b4463d41c84483cb40514ed418d3dc4283"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "f36142488dbc8a3a192d695b8f64e8772740c44fec4660dc8d655e62d269cb97" => :mojave
     sha256 "fa9e3e47fa5b8422b2293c45ac616e4da32e6fcd879127651562a44301f06a47" => :high_sierra
     sha256 "6a30fe3214d0241ff1f8a65582077cc7d6132987e839cd4028494b3b83a4f520" => :sierra
+    sha256 "f67191146689f02e0f0184f07e7dfc6173c337818453b43ef6fa7f7ff10f4874" => :x86_64_linux
   end
 
   depends_on "node"
