b457d50d9266daaa8c7ec373a77ddaa22fae8436
Michka Popoff <michkapopoff@gmail.com>
2019-03-06 19:29:10 +0100

    glib: add missing end

--- Formula/glib.rb
+++ Formula/glib.rb
@@ -60,6 +60,7 @@ class Glib < Formula
                 "Libs: -L${libdir} -lglib-2.0 -L#{gettext}/lib -lintl"
         s.gsub! "Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include",
                 "Cflags: -I${includedir}/glib-2.0 -I${libdir}/glib-2.0/include -I#{gettext}/include"
+      end
     end
   end
 
