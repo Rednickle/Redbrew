f6c9bb0a4ab11ac18501461eb1cc34caf233e1e9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-08 22:33:52 +0000

    flann: update 1.9.1_7 bottle.
    Closes #11984.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/flann.rb
+++ Formula/flann.rb
@@ -6,10 +6,12 @@ class Flann < Formula
   revision 7
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "d7a3870b59388e523bb964b92f0030e75823c84072a55dfdcbe676bd2b1b046f" => :mojave
     sha256 "0dbf4ddcdab675609aa59e953052fde8e557212127ee0befad52fc5d92a561f0" => :high_sierra
     sha256 "28479047aad4a062976572abf76feec33ed2b4a71373d2f9db1b7958d896ba71" => :sierra
+    sha256 "16b9b6ff6a95e43eed7f4b09c5a82ea487c14a7f994ae346958d4d6b1d8b662f" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
