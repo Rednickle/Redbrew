fbf5e00c1f2ebada621cfb79bfbc648608f4279a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-11 23:09:50 +0000

    lolcat: update 99.9.21_1 bottle.
    Closes #12011.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/lolcat.rb
+++ Formula/lolcat.rb
@@ -7,10 +7,12 @@ class Lolcat < Formula
   revision 1
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "e6782890a9d544a17aa6a9011474eaefea4f7422915177946e88985619aa2367" => :mojave
     sha256 "5cbddd57ab46ac3d73a43ea5ef6264096910d337c815948ec603211cb5c5455f" => :high_sierra
     sha256 "6ce278781b578837a6e23164c37b6515292a54de586318288795ce16a808d654" => :sierra
+    sha256 "ceff03af3860acba2818febe9a4b113f093cb87c3541d472183f0d8a3f3f6017" => :x86_64_linux
   end
 
   depends_on "ruby" if MacOS.version <= :sierra
