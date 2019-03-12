da12e64d12d39e608b8ad65a47af8411681775d6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-12 22:14:30 +0000

    juju: update 2.5.2 bottle.
    Closes #12037.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -5,10 +5,12 @@ class Juju < Formula
   sha256 "76c52c263d598e0a87c7a7a70b92030b31d751e0f7a068d388187df59312038e"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "2bef0dba1f1f5ce2b4b06a5a39d0ece18b4050ee470e542e8c171555e80c212d" => :mojave
     sha256 "31f57179868a2e00cc8af4fcd984f59fa9abbb88f94828280df4adf1a75916d5" => :high_sierra
     sha256 "27f867aac3215ccdac6e908625454e26c22896e0f161ab3744339b25e43385ec" => :sierra
+    sha256 "f1d37a26405acf9a9066236088b9f2f81636b8215f07ed4b67ecd187fd1a6235" => :x86_64_linux
   end
 
   depends_on "go" => :build
