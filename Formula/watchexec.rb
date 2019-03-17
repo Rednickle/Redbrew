fb9892181998a837b6351601e6e04b010d444792
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-17 19:54:45 +0000

    watchexec: update 1.10.1 bottle.
    Closes #12101.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/watchexec.rb
+++ Formula/watchexec.rb
@@ -5,10 +5,12 @@ class Watchexec < Formula
   sha256 "48de75d1a85e3675078e144ea34f4edcd3707751e67370f3f6386e79ca9ba4fa"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "b8f31ade640fe036bab0b7d246195b85f77d6154f5af15cdd34481e17c635ced" => :mojave
     sha256 "25793f00659f605cde2c852df2a64390fa77967329bb5a9e93c24ff8c3c2d32f" => :high_sierra
     sha256 "78a9129bea6954c9d0b0076b061bae7999e71aec29aea53712220264ccba2ac4" => :sierra
+    sha256 "4a5e05a7bbbe49bd10dbe0edcf095a46540c0f755c2db449f828f5c04b94e6c8" => :x86_64_linux
   end
 
   depends_on "rust" => :build
