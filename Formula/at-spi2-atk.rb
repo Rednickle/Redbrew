b31137431780563b6d2ea634c2471797bea7a6d3
Michka Popoff <michkapopoff@gmail.com>
2019-03-17 09:18:50 +0100

    at-spi2-atk 2.32.0
    Closes #12089.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/at-spi2-atk.rb
+++ Formula/at-spi2-atk.rb
@@ -1,15 +1,12 @@
 class AtSpi2Atk < Formula
   desc "Accessibility Toolkit GTK+ module"
   homepage "https://wiki.linuxfoundation.org/accessibility/"
-  url "https://download.gnome.org/sources/at-spi2-atk/2.30/at-spi2-atk-2.30.1.tar.xz"
-  sha256 "8b48ea5902a2b3e333ab21a2602e531e86bffbb8e58708b2a8a8dfa5d325bd6f"
+  url "https://download.gnome.org/sources/at-spi2-atk/2.32/at-spi2-atk-2.32.0.tar.xz"
+  sha256 "0b51e6d339fa2bcca3a3e3159ccea574c67b107f1ac8b00047fa60e34ce7a45c"
   # tag "linuxbrew"
 
   bottle do
     cellar :any
-    sha256 "6622e03beadde94a89f5a7916480bba649e9b3cf6379dd6dd8399c08a27aeaf8" => :mojave
-    sha256 "a6eeeb0fbd0094b0c9f85bf15f3cddaeb7de826f27630ba31a4dd8573f364464" => :high_sierra
-    sha256 "c69b12100018e7eef4133520e58bbf8dbc0c4e709ab509a3dcd70834910e0913" => :sierra
   end
 
   depends_on "meson-internal" => :build
