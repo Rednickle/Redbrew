31b96424f21f3dad305076a7c13aa19a802a4092
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-17 21:20:33 +0000

    nginx: update 1.15.9 bottle.

--- Formula/nginx.rb
+++ Formula/nginx.rb
@@ -8,9 +8,11 @@ class Nginx < Formula
   head "https://hg.nginx.org/nginx/", :using => :hg
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     sha256 "01235eb30ff0d8e79f47735ee0cf527d2dbe6cab12b33ec8e0525405a1d40f34" => :mojave
     sha256 "82dc5cb72d60737035d6c5e11cbdfe0a4cf5c17ca67856bd383083923a2ce3e2" => :high_sierra
     sha256 "a92aaf5fd66dc3b486c2c090d01470ed415c104cc71518fd4a427da627405818" => :sierra
+    sha256 "673487764f58ec52723c47d4693c311c8e9e191216a64bdbc4ebe82b0a94d19c" => :x86_64_linux
   end
 
   depends_on "openssl"
