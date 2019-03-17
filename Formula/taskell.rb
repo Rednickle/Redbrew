bc7ce5b02fee2c4fd6eae4041b61d33f8b56c0e1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-17 20:17:13 +0000

    taskell: update 1.3.6 bottle.
    Closes #12099.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -9,10 +9,12 @@ class Taskell < Formula
   sha256 "953ff826540761c5348011310ea58b7d101e381f357cbf91bacb999125a5369c"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "1eb4f86adf6091bd65d5299eccf2ba4f52895be1bff0a73cf420f6853433c884" => :mojave
     sha256 "b69e4374545751d9b2a0c2d9b8ee306f1a165c45627aa6143f0197b247b7a13e" => :high_sierra
     sha256 "557a5d3731d4c721a0e26002818cfb2af97c3e022dcd283b9a6f61d9771e2360" => :sierra
+    sha256 "f6a8122855eadabff0e39899c98d5a2223e6c3a62e7dc064b09f782bd6c6280d" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
