3386b938d1ecd19455ebb47381b21b8cc383574a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-18 22:40:44 +0000

    mkl-dnn: update 0.18.1 bottle.
    Closes #12115.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mkl-dnn.rb
+++ Formula/mkl-dnn.rb
@@ -10,6 +10,7 @@ class MklDnn < Formula
     sha256 "ac17d1c4e9d3519959d55c51dc5cd5644412457d097f816c93932e818d0e90c6" => :mojave
     sha256 "a23203dbbf531d6f0d8ffc07da7f72513ea048ccda4c19cd3e0e3ab102a3f56c" => :high_sierra
     sha256 "04709a34737fe09d980a57c9f69d252060a1cc27ed8d2af83dad3d82f96f737a" => :sierra
+    sha256 "f3a8728fabcd5763c436c83d58a52e23e851f4a1ae818ab158dab0a24e4264a5" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
