194aa004d561e6f61e81918024ceaf26985ad84f
Michka Popoff <michkapopoff@gmail.com>
2019-03-20 22:31:15 +0100

    conserver: Fix bottling for Linuxbrew
    Closes #12138.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/conserver.rb
+++ Formula/conserver.rb
@@ -5,6 +5,7 @@ class Conserver < Formula
   sha256 "764443b2798047f7429747510eeb3207240260590551700d13dbbad8a5bdee08"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "97403a07aac75ec35f200f6c804c55f6e011dd472146c87c53da209f130f74a6" => :mojave
     sha256 "1a5372d2617d47faea48393dcf6dffe38d778194a7e1e7deb32f49548884b2d4" => :high_sierra
     sha256 "b86cff5c3d9803a8840b06199065fe07b29d8361457412d7063f171a42cf6b2e" => :sierra
