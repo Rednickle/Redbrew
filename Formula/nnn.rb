e4c0e67b046fb3c4d1301f31b2dc563371766aa2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-20 22:51:27 +0000

    nnn: update 2.4 bottle.
    Closes #12142.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -6,10 +6,12 @@ class Nnn < Formula
   head "https://github.com/jarun/nnn.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "ddf0361deeb681303efc74a08e5bdce4a08225bb3b9ff6cdb607d6cfb4158860" => :mojave
     sha256 "b26f8e25a48ebfa64ec49c496a625121e367075778610643181f55740edabd25" => :high_sierra
     sha256 "997ecd4a9ef2d58c17dc8a23dd83143297a434a81e9e6567097e7f5610c86a6d" => :sierra
+    sha256 "7969f67f4a236a5a1c4f45df713f20859842442e5da388c57a46a0da1ef6bcf1" => :x86_64_linux
   end
 
   depends_on "readline"
