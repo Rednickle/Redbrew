88a6340b5e04330f4f746adb458a2b11540f9d3c
Michka Popoff <michkapopoff@gmail.com>
2019-03-25 22:37:37 +0100

    jpeg-turbo: Fix bottling for Linuxbrew
    Closes #12222.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/jpeg-turbo.rb
+++ Formula/jpeg-turbo.rb
@@ -6,6 +6,7 @@ class JpegTurbo < Formula
   head "https://github.com/libjpeg-turbo/libjpeg-turbo.git"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "8b1bebbe396aa8c87ffaf728e55206e6d87d431a1f26960e0043a4d314ecafaa" => :mojave
     sha256 "67e420053c229a4c33aaed0e6c585da8290b88c4fd6ca4e09d7c65c8d96656e8" => :high_sierra
     sha256 "196facd9c26ebc8cdc98f70c7b14b6b3c2271833fa664dcc1145101e9cb1312f" => :sierra
