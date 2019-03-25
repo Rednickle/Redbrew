3d5f47a80e3d735737d90f3ba7a2ed65935c3258
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-25 22:54:08 +0000

    jid: update 0.7.5 bottle.
    Closes #12221.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/jid.rb
+++ Formula/jid.rb
@@ -5,10 +5,12 @@ class Jid < Formula
   sha256 "f741dee63f90b866f695a279634b0b28e4b432f3f59229cc3f0c8b4e32d4d18b"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "e46c621f6dc073f1f26480523ccd638cd8c82678fb17ace4637530b5f858dad6" => :mojave
     sha256 "e90ea96c64fcd6e9cca04ebf53b4ebc40982f3b9bbc273801c913a11ae85cb1a" => :high_sierra
     sha256 "8337cf1ac00b14b9e0d9854d6434a96d8d67913d8b83cf44b7e21c6b28c11279" => :sierra
+    sha256 "c0546aaa68116c504240c1130f6c7620831289a6fe65d00188be32038813c926" => :x86_64_linux
   end
 
   depends_on "go" => :build
