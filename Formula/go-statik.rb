18d34b4316d70f8c1dd4212fffbd08b91fbcc511
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-25 21:13:16 +0000

    go-statik: update 0.1.6 bottle.
    Closes #12197.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/go-statik.rb
+++ Formula/go-statik.rb
@@ -5,10 +5,12 @@ class GoStatik < Formula
   sha256 "f157a1ada813eb643ddd9a60a0efe3158f1da25b1d11bc1ef6c7fa219d4b23bf"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "4e347596a83f169bdb0b72cd81d69ea275966fead97f7decb67c873380c179b8" => :mojave
     sha256 "4ec52d9626abcbb07f25640c9ecec4e620faa9ee619f6f289f172aa7bc509590" => :high_sierra
     sha256 "e0e8c356eb1ed32cf074d4abfa81fd8fa9d6d8dfb7c7724c696a521f974a3b44" => :sierra
+    sha256 "ed1a7016131c7d1f134337a26d72a57750054d816159efd37354614b8f68b9e7" => :x86_64_linux
   end
 
   depends_on "go" => :build
