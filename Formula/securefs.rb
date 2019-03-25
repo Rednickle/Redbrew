28b8d14aaf1a6487158b15272648c0b98930302d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-25 21:54:55 +0000

    securefs: update 0.8.3 bottle.
    Closes #12203.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/securefs.rb
+++ Formula/securefs.rb
@@ -6,10 +6,12 @@ class Securefs < Formula
   head "https://github.com/netheril96/securefs.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "07605d0d88a95902f1ee39d88c56dceadcacdf6e61a71431f68cdbf97003c848" => :mojave
     sha256 "e0a3b66b2dd99a8cd2a6f79b4fe537875c55aa240f7383bef450b008fff6dfff" => :high_sierra
     sha256 "a75071f5711a298f0223c4776891c2ccd5062f0a2830debfaa0191a0152d8bfc" => :sierra
+    sha256 "1de8baf2c55e06b9f90183bd248d5353a97bf0931eff408dd2ec094eda905776" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
