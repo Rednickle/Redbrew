733793280739dece51184b7ec48fc2ff7fac4c89
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-26 07:28:26 +0000

    exomizer: update 3.0.2 bottle.
    Closes #12207.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/exomizer.rb
+++ Formula/exomizer.rb
@@ -5,10 +5,12 @@ class Exomizer < Formula
   sha256 "cf94a2d9e66c104489846bb703c497aab8146ac4a54964def01692d418b16075"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "653ce86b30882fae1deae16bbf40b50f9b46b7f4f3f51e1952a988121e4fd5dc" => :mojave
     sha256 "46b2f9b190847344ab7a16c94eb4b2bad48009a8f08e5463427af75c9eb67409" => :high_sierra
     sha256 "6c6389b18ce3be2c7ffdb919e79273ecc8e26b9067bd06b29474d37c9e162e83" => :sierra
+    sha256 "b4b63981c014d8978621e8125dd54efd3f8435a92fa8b7a654d28e24a012793e" => :x86_64_linux
   end
 
   def install
