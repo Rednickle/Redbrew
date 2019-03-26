a8f5c50e1e1fea78143716357a7119373f0e79ef
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-26 05:48:08 +0000

    rke: update 0.2.0 bottle.
    Closes #12225.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -11,6 +11,7 @@ class Rke < Formula
     sha256 "71cce91f3daeb36cc6aa36134fc6509e2b2a8d43c0bcecc682dc6f3bd26b4986" => :mojave
     sha256 "eb9e015ddc0c3b0d5aea412e6c8f3269cecbb45e6024e5e99c650a5d4871d72b" => :high_sierra
     sha256 "604cb94f11cfe015db62b429fdd129b559f900b11d71a3fd124a649cfdd455df" => :sierra
+    sha256 "baca6468aa5815355375babce65690007f0cb18272e3fac82ebe75f534a84c22" => :x86_64_linux
   end
 
   depends_on "go" => :build
