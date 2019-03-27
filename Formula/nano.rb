1fc45af8919308e345907a2fadfae253f0601a6e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-27 12:18:28 +0000

    nano: update 4.0 bottle.
    Closes #12242.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nano.rb
+++ Formula/nano.rb
@@ -5,9 +5,11 @@ class Nano < Formula
   sha256 "5b3f67d7d187e9feb980e1482ba38c1bc424bace5282c6bbe85b4bb98371ef1e"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     sha256 "34a92e5858a02623f53514fed225a4f040551c647e7765a5489cc2918778f512" => :mojave
     sha256 "4cc90703d5be90a2ea09e3eef3113aca2a571c57d3fe98e22cfcc650502654d4" => :high_sierra
     sha256 "b7c6484f5c048b70814ba74560c04dcbcc8834e96d941f7b1efc7eda85b62e8c" => :sierra
+    sha256 "6c0754d8b01812e2864981099d71f264a538293005cbf17aa16b1d7ccdb6979c" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
