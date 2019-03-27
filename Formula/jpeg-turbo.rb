5271280470092308dd37b9e92a381ac4b22f5ce6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-27 21:23:12 +0000

    jpeg-turbo: update 2.0.2 bottle.

--- Formula/jpeg-turbo.rb
+++ Formula/jpeg-turbo.rb
@@ -6,10 +6,12 @@ class JpegTurbo < Formula
   head "https://github.com/libjpeg-turbo/libjpeg-turbo.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "8b1bebbe396aa8c87ffaf728e55206e6d87d431a1f26960e0043a4d314ecafaa" => :mojave
     sha256 "67e420053c229a4c33aaed0e6c585da8290b88c4fd6ca4e09d7c65c8d96656e8" => :high_sierra
     sha256 "196facd9c26ebc8cdc98f70c7b14b6b3c2271833fa664dcc1145101e9cb1312f" => :sierra
+    sha256 "cde978bbbc33833bdc2d69bfaacc488895279b5f66cdc10323bea0cb20facbda" => :x86_64_linux
   end
 
   keg_only "libjpeg-turbo is not linked to prevent conflicts with the standard libjpeg"
