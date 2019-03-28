951debfa77eeb374c3af7ec7f9dd33f8160a2fc6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-28 13:03:19 +0000

    help2man: update 1.47.10 bottle.
    Closes #12261.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/help2man.rb
+++ Formula/help2man.rb
@@ -6,10 +6,12 @@ class Help2man < Formula
   sha256 "f371cbfd63f879065422b58fa6b81e21870cd791ef6e11d4528608204aa4dcfb"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "fe3ddffa91085d678ab198ffe981edf55b6347faf55bcc04f93fc4eb457d036e" => :mojave
     sha256 "fe3ddffa91085d678ab198ffe981edf55b6347faf55bcc04f93fc4eb457d036e" => :high_sierra
     sha256 "c69b38804b45c6aafd0afc2ee9bda2eb530e5c0e1bff9e5bb3709cba4d7d58b7" => :sierra
+    sha256 "bde9767e652d6f0d56ce3daf359d00c03f8a65875ce2acfdc05bbe39855924fd" => :x86_64_linux
   end
 
   def install
