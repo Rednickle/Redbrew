f00fb83646d3f859064ffc0177c79bc02b14cabd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-28 19:57:23 +0000

    micropython: update 1.10 bottle.
    Closes #12267.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/micropython.rb
+++ Formula/micropython.rb
@@ -6,10 +6,12 @@ class Micropython < Formula
       :revision => "3e25d611ef3185b68558a20057d50b0d18dc67a0"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "c635e90b5127a6101de370032e4ac9230f98a223d3c53d594f9122c8dfd58b01" => :mojave
     sha256 "01611cf99b4623f5157a81ddf16f6b750d71010eead09203f8f7a72d30354fa9" => :high_sierra
     sha256 "54b499d93100baad80a31c32d997b1c97988abdc28d7cad3400f8f811eed4979" => :sierra
+    sha256 "303d7519a4b3c6389d72a558cc257484b92ff20e5677e99a47e800e0562dd21b" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
