daa46ef4a9400db24f4e4768bad995a6f7cc518f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-30 10:33:18 +0000

    bedtools: update 2.28.0 bottle.

--- Formula/bedtools.rb
+++ Formula/bedtools.rb
@@ -5,10 +5,12 @@ class Bedtools < Formula
   sha256 "0f3e5990b5713388531de699d43d195f1535a5772d832acfd47baa151a3a7e59"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "6c9819a00f8d56e97500c0eef57a3363fd18c4439ce18ac66c5b0d6638b1bd88" => :mojave
     sha256 "e1a7e656b89c764f9331a1924ef4b6113769051a260fb76c2a1eee8b4a39cf84" => :high_sierra
     sha256 "61677f8ce990c4b17ee07fdb9e4d78e2655253a1301f156ef0ad5698e77a37b3" => :sierra
+    sha256 "1281fb9912f81e09a603ee93a5b1fba93b18d0d5a6aef1937accb4e17e6a61c2" => :x86_64_linux
   end
 
   depends_on "xz"
