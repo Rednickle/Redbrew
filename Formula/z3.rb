62c6329f144103fd7fb49ffb21b024cedff3056d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-31 21:00:33 +0000

    z3: update 4.8.4_2 bottle.
    Closes #12355.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/z3.rb
+++ Formula/z3.rb
@@ -7,11 +7,13 @@ class Z3 < Formula
   head "https://github.com/Z3Prover/z3.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     rebuild 1
     sha256 "c40462a152d29d1a17828b42fe5aba6a6584d955ff668f95a77e2bbfeb4d9827" => :mojave
     sha256 "9f4ea1d8faf70a2510eeb93648c378067efd1b7b26e31dd26d4454eb5c503392" => :high_sierra
     sha256 "9930015dd6c3a7c18aca7284aa145ea2b9b37c5297c650ab8584912c56593a35" => :sierra
+    sha256 "46d0ae7ad6557dec6266594c826a65e6556061fda499fbad56daad5ef4e9d9f5" => :x86_64_linux
   end
 
   depends_on "python"
