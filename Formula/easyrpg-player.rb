0c5f54f898db4a927070c9c083a8a43b257d5f44
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-31 19:31:53 +0000

    easyrpg-player: update 0.6.0 bottle.
    Closes #12348.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/easyrpg-player.rb
+++ Formula/easyrpg-player.rb
@@ -5,10 +5,12 @@ class EasyrpgPlayer < Formula
   sha256 "d364a272aa3e14adfb696b537d957541c9fff9df9a4e3948a24d42ad89b9bf7d"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "83f79e7c021e82a781d50f3d7f731a2a4fbafc3dd166c5bdb2941859331982e4" => :mojave
     sha256 "75e146b0e863c2638c34e4d97affc301638cb6b041cf1b4a05aac1e93c7eead7" => :high_sierra
     sha256 "02aa23e4c5834569bf4b9c2dd5c38d5e557b9730a8fb78da4a382d9d90df41d4" => :sierra
+    sha256 "676576bc9aafdbf2c1cab152ce5eb649b05df5f56f38d4fca748c2f2f6db0c30" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
