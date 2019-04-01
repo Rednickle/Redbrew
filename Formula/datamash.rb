84d4b3e94394429f5ad152a5c1f967e27234de28
Michka Popoff <michkapopoff@gmail.com>
2019-04-01 18:43:34 +0200

    datamash: Fix bottling for Linuxbrew
    Closes #12377.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/datamash.rb
+++ Formula/datamash.rb
@@ -6,7 +6,6 @@ class Datamash < Formula
   sha256 "fa44dd2d5456bcb94ef49dfc6cfe62c83fd53ac435119a85d34e6812f6e6472a"
 
   bottle do
-    cellar :any_skip_relocation
     sha256 "c6df5d507e7486fd57449ff0a7c53fe7dda265c27ffbc3becef14fc7ecb5b0a2" => :mojave
     sha256 "10bf8d835e2cc4401875db546d0336563f259fb9fc5cae86e58d91ede2cff943" => :high_sierra
     sha256 "35bf3994eb36de7e6f7c9e7010322c58aeb44e42676060c5b28d92f97789dfab" => :sierra
