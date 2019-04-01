790c60ff8f694a76f4e321afb6625a8d3ed5186a
Michka Popoff <michkapopoff@gmail.com>
2019-04-01 18:48:05 +0200

    gifski: Fix bottling for Linuxbrew
    Closes #12382.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -5,6 +5,7 @@ class Gifski < Formula
   sha256 "e8d8d2fdb816953315989b3ecf39ac88f33b8c49aa7c79a0df8f53b4032755ab"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "41b0c73f3364637a793137b5ff167847f04104a29ef1db89fce744839786f20b" => :mojave
     sha256 "33268907c1359250acb2993847a179ae6dfd9c0173b9c62def3dfdb41168072b" => :high_sierra
     sha256 "7a4cceb9e6b9ea907dc9244ae6e7e2a44069588034ac7937eb642f7cc1252ec8" => :sierra
