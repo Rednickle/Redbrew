bd9dea95c69492b4758fb564686ef9cff081b676
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-01 15:51:33 +0000

    docker: update 18.09.4 bottle.
    Closes #12365.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -11,6 +11,7 @@ class Docker < Formula
     sha256 "3ddd6a5d7a186fdba4ab5af2be313d57d796cef2d6b8ebd1396d8a86b49086af" => :mojave
     sha256 "48cb06562764012acfe5336036e5d2ded9ac56e3682df16580658ca4917bf502" => :high_sierra
     sha256 "fcf79e61ee5a9998bfd6e1927ef8e86b96f639349e309bb91d0aa2cdc1920edf" => :sierra
+    sha256 "7d961bab9e2ae0d9a98149e425e0be75216392834293258e87632cb993e8cdb8" => :x86_64_linux
   end
 
   depends_on "go" => :build
