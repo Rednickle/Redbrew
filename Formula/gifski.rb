256617aecdc14da1a237f89ecd75e7792c5c71a4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-01 19:14:21 +0000

    gifski: update 0.8.7 bottle.

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -5,10 +5,12 @@ class Gifski < Formula
   sha256 "e8d8d2fdb816953315989b3ecf39ac88f33b8c49aa7c79a0df8f53b4032755ab"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "41b0c73f3364637a793137b5ff167847f04104a29ef1db89fce744839786f20b" => :mojave
     sha256 "33268907c1359250acb2993847a179ae6dfd9c0173b9c62def3dfdb41168072b" => :high_sierra
     sha256 "7a4cceb9e6b9ea907dc9244ae6e7e2a44069588034ac7937eb642f7cc1252ec8" => :sierra
+    sha256 "2b161f56c24f3c832f1087a144a2341c8fdd22155ca5fdd0d81342cb2b0edde1" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
