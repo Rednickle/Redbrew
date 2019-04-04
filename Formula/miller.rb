a693cdbd06846b0d5119c71988045bd8278bed83
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-04 16:46:43 +0000

    miller: update 5.4.0 bottle.

--- Formula/miller.rb
+++ Formula/miller.rb
@@ -5,10 +5,12 @@ class Miller < Formula
   sha256 "d9a7ed10a1a5e71082c33d489f77ab9caba6e6c4f9bf0f6c3958201b64974f7e"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "a2a049c66f1a1f3d8bb14a316afa5624c290ddca8a5723417fe7bfb0b682d62e" => :mojave
     sha256 "dfe0d02b8fe42510d2637a6016ac7527b7e7fef981b6677f9171236780ffd05b" => :high_sierra
     sha256 "2d28eda0033407a1ca4b191d67b9e742d16d0b7af106525062ab5bc4151eb6a8" => :sierra
+    sha256 "7f6690b11762a4d6fcb9f42a9dbf142716726a4c70ea9aafa1791f7aa9041508" => :x86_64_linux
   end
 
   head do
