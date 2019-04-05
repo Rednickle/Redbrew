7ce741f5c0add50672b29aadc6d047553db44ec7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-05 05:58:07 +0000

    gromacs: update 2019.1 bottle.
    Closes #12436.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gromacs.rb
+++ Formula/gromacs.rb
@@ -5,9 +5,11 @@ class Gromacs < Formula
   sha256 "b2c37ed2fcd0e64c4efcabdc8ee581143986527192e6e647a197c76d9c4583ec"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     sha256 "94d33c3b2aab69176bb9ae38225a4486f8e648e775628046cc376d6110f70c64" => :mojave
     sha256 "f5e98222c231b867623809636fe56ec550bf533f517b7312bb554a915bdf4fe9" => :high_sierra
     sha256 "c4479eb9a92d1615c2247c9ba3ab185de46a35cc37b55c2a6443f0f73eb7c0cb" => :sierra
+    sha256 "70c263db88f1c106d194288f43961167a702b5e7a1711a07f08b67fee065145f" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
