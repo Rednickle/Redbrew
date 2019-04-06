8b6981707a2953356073adc07c313bfe3fe039ce
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-06 09:28:28 +0000

    jfrog-cli-go: update 1.24.3 bottle.
    Closes #12463.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/jfrog-cli-go.rb
+++ Formula/jfrog-cli-go.rb
@@ -10,6 +10,7 @@ class JfrogCliGo < Formula
     sha256 "eb470ca00e5d66ceeeac1e69f3f9380b6f742d78f079538d62553a9d646055a6" => :mojave
     sha256 "b5556e8f9ad509f4d743e833eec2851f8c4e76152085f7a48dcbadda7c0a55e5" => :high_sierra
     sha256 "84cfa6122767dadcef0326f18ae958e3ad188de9ebec06ec90517d64179f3fef" => :sierra
+    sha256 "bb3acc3dbbebc41493295cd7b3e928a33c780dbce27fd3b01e49bccf1604a37f" => :x86_64_linux
   end
 
   depends_on "go" => :build
