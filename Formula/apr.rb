c47d9d9b12fc232442fcd68db45163410d9973d1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-08 19:06:16 +0000

    apr: update 1.7.0 bottle.
    Closes #12476.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/apr.rb
+++ Formula/apr.rb
@@ -5,10 +5,12 @@ class Apr < Formula
   sha256 "e2e148f0b2e99b8e5c6caa09f6d4fb4dd3e83f744aa72a952f94f5a14436f7ea"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "3a245185ed7280d1a19e7c639786b4c21dd0088878be8ac87ca58510eb5c9cc1" => :mojave
     sha256 "4d01f24009ea389e2c8771c5c0bc069ae09c0f5812d7fdb0d0079106c3fc0838" => :high_sierra
     sha256 "a49a1725c76754297c0f9a268423ee9a1772d23d264360504cc3401a21d2aa7e" => :sierra
+    sha256 "55aa3f0c5dcb612e8c201e0ddc63ef52db99f314a14ae0aab4b6f7d6b4f365d3" => :x86_64_linux
   end
 
   keg_only :provided_by_macos, "Apple's CLT package contains apr"
