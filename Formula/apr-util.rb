cd6b939b98f59068e5f59c062cf2c17eee3a7e9b
Michka Popoff <michkapopoff@gmail.com>
2019-04-08 22:46:51 +0200

    apr-util: depend on sqlite for Linuxbrew
    Closes #12485.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/apr-util.rb
+++ Formula/apr-util.rb
@@ -3,14 +3,13 @@ class AprUtil < Formula
   homepage "https://apr.apache.org/"
   url "https://www.apache.org/dyn/closer.cgi?path=apr/apr-util-1.6.1.tar.bz2"
   sha256 "d3e12f7b6ad12687572a3a39475545a072608f4ba03a6ce8a3778f607dd0035b"
-  revision 1
+  revision OS.mac? ? 1 : 2
 
   bottle do
     sha256 "e4927892e16a3c9cf0d037c1777a6e5728fef2f5abfbc0af3d0d444e9d6a1d2b" => :mojave
     sha256 "1bdf0cda4f0015318994a162971505f9807cb0589a4b0cbc7828531e19b6f739" => :high_sierra
     sha256 "75c244c3a34abab343f0db7652aeb2c2ba472e7ad91f13af5524d17bba3001f2" => :sierra
     sha256 "bae285ada445a2b5cc8b43cb8c61a75e177056c6176d0622f6f87b1b17a8502f" => :el_capitan
-    sha256 "c6f01dd4bd4ad3f3c8bc0b84be39361efc2b36101d34c88651076f0ed8588052" => :x86_64_linux
   end
 
   keg_only :provided_by_macos, "Apple's CLT package contains apr"
@@ -19,6 +18,7 @@ class AprUtil < Formula
   depends_on "openssl"
   unless OS.mac?
     depends_on "expat"
+    depends_on "sqlite"
     depends_on "util-linux" # for libuuid
   end
 
