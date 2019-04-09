1d8049eb29fd84b3f9ddfe436ceab643b4f7daa1
Michka Popoff <michkapopoff@gmail.com>
2019-04-09 21:55:08 +0200

    gnu-getopt: Fix bottling and style for Linuxbrew
    Closes #12492.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gnu-getopt.rb
+++ Formula/gnu-getopt.rb
@@ -5,12 +5,13 @@ class GnuGetopt < Formula
   sha256 "86e6707a379c7ff5489c218cfaf1e3464b0b95acf7817db0bc5f179e356a67b2"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "702e009224e60952ea5b673fabb9cb9e3c05ce5e7923a5485fd34bfc73542c88" => :mojave
     sha256 "54596caa40b03e440c44f3a0bd7475a5c3f2bee187feb825ec51ef59e9e00862" => :high_sierra
     sha256 "3339c54159a904d773848cbd8c33dd89ee3459db313b98ac4f33d258b4bb5ac5" => :sierra
   end
 
-  keg_only (OS.mac? ? :provided_by_macos : "This formula conflicts with util-linux.")
+  keg_only (OS.mac? ? :provided_by_macos : "this formula conflicts with util-linux")
 
   def install
     system "./configure", "--disable-dependency-tracking",
