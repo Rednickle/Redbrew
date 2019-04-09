a2c63233a20e43ab78e0e219aec1ffb77b91c437
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-09 20:10:20 +0000

    rtv: update 1.26.0 bottle.
    Closes #12494.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/rtv.rb
+++ Formula/rtv.rb
@@ -8,10 +8,12 @@ class Rtv < Formula
   head "https://github.com/michael-lazar/rtv.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "5c6e84e7021b748a2d2e763b22aec9ec04b92a2dbad74f4b95d5ada3a18ad733" => :mojave
     sha256 "1ec04997bca5a311edf8451907c9f3e1400da2606858a025276c81f167446012" => :high_sierra
     sha256 "2ecdfa0d308a52c4f63c71f3a3f01467c4839f9991cae1099a9e6664fc3910f1" => :sierra
+    sha256 "c560c2bb839f437c1d962e3fc8d5e181b422ac0929e8e7aaffc0c41ce8125c26" => :x86_64_linux
   end
 
   depends_on "python"
