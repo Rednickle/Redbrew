e28d283b9d62cd3b521cb140933430d2d1714011
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-16 06:18:54 +0000

    jid: update 0.7.6 bottle.
    Closes #12554.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/jid.rb
+++ Formula/jid.rb
@@ -10,6 +10,7 @@ class Jid < Formula
     sha256 "2552c0ab94b892750ecff994b5f3fa963d8ab69298fc27abff7ca92f1e371532" => :mojave
     sha256 "4cfdd2d04b54fdd4d63a1297205bc7867b8bd7049c727ab8ad306c12f0d270d6" => :high_sierra
     sha256 "1905385bc2a0dbd606c4d90f9b119839b3674231ce3b3fa43f0bef314a1684ca" => :sierra
+    sha256 "78c1d4e799ff7b5aa56ddaaeb5a471899475bd674e75c91e416c88b1fbb221a4" => :x86_64_linux
   end
 
   depends_on "go" => :build
