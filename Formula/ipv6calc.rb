7fc95590b69be45959a511fb88c07411be72e50b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-16 06:13:46 +0000

    ipv6calc: update 2.1.0 bottle.
    Closes #12553.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ipv6calc.rb
+++ Formula/ipv6calc.rb
@@ -5,10 +5,12 @@ class Ipv6calc < Formula
   sha256 "a0ec4a1ff197210162f8a644381dd08b2c552cf50c68041748ea7e067df70080"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "e5816eae9ca8eed194d698a289d1f3d9dedcb93b983cd9761c6dca233d3a5ed7" => :mojave
     sha256 "8803971206dffa1bf5d040199f774e32f052d0d142b674ca41777e3736ab2bb3" => :high_sierra
     sha256 "a5fd2f9e54ffaf981be6f9b4e16a397799ca67ac26bef7680edd4387d734dd65" => :sierra
+    sha256 "3186ab40ff19c4e64df3f686f1dc34ec0759a4ec9eb7bc1fcbc3ec8fd5304a35" => :x86_64_linux
   end
 
   def install
