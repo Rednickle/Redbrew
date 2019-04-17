f7e6f9c90ad41863a607fd14a5084aadeb55ac21
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-17 19:58:28 +0000

    ilmbase: update 2.3.0 bottle.
    Closes #12609.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ilmbase.rb
+++ Formula/ilmbase.rb
@@ -5,10 +5,12 @@ class Ilmbase < Formula
   sha256 "456978d1a978a5f823c7c675f3f36b0ae14dba36638aeaa3c4b0e784f12a3862"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "436dbe30d0bc520c5c056dac23a3558dd2595e5f5b68c6c17e18566716c71e56" => :mojave
     sha256 "4ef6417909dee0313b9d493b5689d04382907beb650abc669fc5a6346e4c4d5b" => :high_sierra
     sha256 "f81d4a8993861dbde4c91b0783b03a943c710c060b938095511f3cf26beee589" => :sierra
+    sha256 "401c030b5721fa3774f998342f06798c1bc45803a2defcaf938fa42d14f137d8" => :x86_64_linux
   end
 
   def install
