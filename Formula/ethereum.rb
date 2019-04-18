6eea8c2b3f12368aec43e5fda91a97e747d5431f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-18 23:11:16 +0200

    ethereum: update 1.8.26 bottle.
    Closes #12539.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ethereum.rb
+++ Formula/ethereum.rb
@@ -6,10 +6,12 @@ class Ethereum < Formula
   head "https://github.com/ethereum/go-ethereum.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "fad1df7622e29afb1f6e0d074e2730bb6e79700319a0e299bd5570c423c906de" => :mojave
     sha256 "123cc5316d7dd8f9b9b79f297bae2403ea06bb204daca04dec859c771c9406e3" => :high_sierra
     sha256 "cddc04fe913fa53ccdd1afe67707afd7a81c3f62c794fef9a3f22462a0938faa" => :sierra
+    sha256 "1313b0147e255f300ef4e7576e7e33dea67ab3a9bc5d7e0402995690edf3f4c6" => :x86_64_linux
   end
 
   depends_on "go" => :build
