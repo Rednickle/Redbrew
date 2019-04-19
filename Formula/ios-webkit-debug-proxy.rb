ae88a6b31b223a77838509e6892cfc48d27ffa71
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-19 06:02:19 +0000

    ios-webkit-debug-proxy: update 1.8.4 bottle.
    Closes #12646.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ios-webkit-debug-proxy.rb
+++ Formula/ios-webkit-debug-proxy.rb
@@ -6,10 +6,12 @@ class IosWebkitDebugProxy < Formula
   head "https://github.com/google/ios-webkit-debug-proxy.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "b8058d0cc84cd94c50c1e0c66a783f5ea4328d10ef1a166dd87e6ca236e533d9" => :mojave
     sha256 "e7b603f9c7beb0d3a30cc770596ecb1d56dadfa7cea6c3b299119dfa00b868b6" => :high_sierra
     sha256 "8f25c038350c5a577589d5a91b2f7a0a2f4a6823c0e385a6cf5ec90836bf4b50" => :sierra
+    sha256 "9f798915f08d0fa9c61d24c7ec66ad95989a347f53f60dcbe05285b404cdd77f" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
