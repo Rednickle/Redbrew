0b089213d9a4f91076594163888fa025a05be1bf
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-21 21:04:31 +0000

    hivemind: update 1.0.6 bottle.
    Closes #12690.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/hivemind.rb
+++ Formula/hivemind.rb
@@ -6,10 +6,12 @@ class Hivemind < Formula
   head "https://github.com/DarthSim/hivemind.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "8e6f70ca5e0c8eb1e42d47bee207ec5333b453660d808103a36d53c51a7fb59a" => :mojave
     sha256 "7a89018774693681975cea22dcdebe35df043507476d1318f195e6d194978693" => :high_sierra
     sha256 "4aa25b52b5c7fd3dc7ae29ab31cf19eeddde4e7685fd7e9838be0ea8cf09f3c1" => :sierra
+    sha256 "e74b7949bb598fe11915c409c0028787bf0d97a7dc915df165dac884077a372b" => :x86_64_linux
   end
 
   depends_on "go" => :build
