c78a1a3b26ae08c5fe697fdc5f9fd311da1ea5e6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-21 21:25:38 +0000

    ipfs: update 0.4.20 bottle.
    Closes #12697.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ipfs.rb
+++ Formula/ipfs.rb
@@ -12,6 +12,7 @@ class Ipfs < Formula
     sha256 "ded3070262a29b67ae5793665728e998aaa0f3b9be92cc0a5b9a5f020493164a" => :mojave
     sha256 "051958731368a985c9b78fd30c70c0ca1880fe50a24135a49a389b9760de1eef" => :high_sierra
     sha256 "6b6ba9d2f2528a81ef3af055c726df76eca519089f3c0ac57c7375f85beda442" => :sierra
+    sha256 "91c3e36000ff078bff9e61522a80baab534c0adacfc30dd602ea02f267eb0ca7" => :x86_64_linux
   end
 
   depends_on "go" => :build
