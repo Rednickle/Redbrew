a9ef3bd282ea75b011224a5fc3d7531bc4f3113a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-21 21:24:31 +0000

    iozone: update 3.487 bottle.
    Closes #12695.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/iozone.rb
+++ Formula/iozone.rb
@@ -5,10 +5,12 @@ class Iozone < Formula
   sha256 "2c488a7ccddd624fd557af16e71442c367b131d6178e1b4023bbd532bacdda59"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "ed77f59ddfb45e302b60e80d2f4a842e838af5973df7e6dc7d95a3d27c06d0e8" => :mojave
     sha256 "c0f0f7a10f585f40435d1d0a07322739925c9cae2afeb8c8714fe9e121a41572" => :high_sierra
     sha256 "09eda4f32813be63ba4e05dee24a9cac6805667d022fb8a2ed42ef8b0fafe9dc" => :sierra
+    sha256 "c16c33619fa043dabccb7e43ed18ab116b804b5499ab0287489d46fa538bed5e" => :x86_64_linux
   end
 
   def install
