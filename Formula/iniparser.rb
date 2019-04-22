6669448e9433968098c53bd140c1ba889ce63960
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-22 16:36:00 +0000

    iniparser: update 4.1 bottle.
    Closes #12694.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/iniparser.rb
+++ Formula/iniparser.rb
@@ -6,10 +6,12 @@ class Iniparser < Formula
   head "https://github.com/ndevilla/iniparser.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "69dde8e886645f5b89f83f36835c18449afe7f6c4f119d466d7f204e994952c7" => :mojave
     sha256 "cec20d33114e7a5811acb41f9f9a36a411ffd2eebb7d537167b9b541b03fff8d" => :high_sierra
     sha256 "7ad8eb3b8a66c08b78d2d9d3db18bd50e842d1c5962600ad0c9c8244d296dea8" => :sierra
+    sha256 "0ad7f484c703d421dbe3b974c5a8a0c63132fc34b8b9c34f7306a7d802b32961" => :x86_64_linux
   end
 
   conflicts_with "fastbit", :because => "Both install `include/dictionary.h`"
