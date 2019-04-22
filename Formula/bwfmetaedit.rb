1109a816d2fcc3511d7c025d6384e61989a67cb6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-22 17:52:50 +0000

    bwfmetaedit: update 1.3.7 bottle.
    Closes #12707.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/bwfmetaedit.rb
+++ Formula/bwfmetaedit.rb
@@ -6,10 +6,12 @@ class Bwfmetaedit < Formula
   sha256 "15bce139edfa03adb63235803b2817205b5abea3f69643afae3da71d3298ad2d"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "e258954081e5d8c34c3421bb825274f7f2e75a3ba907a9daa742735c8779d4d6" => :mojave
     sha256 "d4b195aef2be90ae4b9e37145637d35131748ab92b2b48a65e2f5e0ec5652fc4" => :high_sierra
     sha256 "d3f0d70d2b6a54d2f9c2aa00ce4bf304df76419eb6c0acd09efb709ad8c85564" => :sierra
+    sha256 "8acd1b15ff619ff2eea0af9734ace6bb6eade298bf9002a475ffa80e5ec5774c" => :x86_64_linux
   end
 
   def install
