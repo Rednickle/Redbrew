6ba86f462021f3dd5bd5596ea3c155a9427d1949
TANIGUCHI Masaya <ta2gch@gmail.com>
2019-04-23 14:24:02 +0900

    adoptopenjdk (new formula)
    Closes #12716.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

---
+++ Formula/adoptopenjdk.rb
@@ -0,0 +1,29 @@
+class Adoptopenjdk < Formula
+  desc "Prebuilt binaries produced from OpenJDK class libraries"
+  homepage "https://adoptopenjdk.net/"
+  url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz"
+  version "1.8.0.212"
+  sha256 "dd28d6d2cde2b931caf94ac2422a2ad082ea62f0beee3bf7057317c53093de93"
+
+  depends_on :linux
+
+  def install
+    prefix.install Dir["*"]
+    share.install prefix/"man"
+  end
+
+  test do
+    (testpath/"Hello.java").write <<~EOS
+      class Hello
+      {
+        public static void main(String[] args)
+        {
+          System.out.println("Hello Homebrew");
+        }
+      }
+    EOS
+    system bin/"javac", "Hello.java"
+    assert_predicate testpath/"Hello.class", :exist?, "Failed to compile Java program!"
+    assert_equal "Hello Homebrew\n", shell_output("#{bin}/java Hello")
+  end
+end
---
+++ Formula/adoptopenjdk@11.rb
@@ -0,0 +1,29 @@
+class AdoptopenjdkAT11 < Formula
+  desc "Prebuilt binaries produced from OpenJDK class libraries"
+  homepage "https://adoptopenjdk.net/"
+  url "https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.3%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.3_7.tar.gz"
+  version "11.0.3.7"
+  sha256 "23cded2b43261016f0f246c85c8948d4a9b7f2d44988f75dad69723a7a526094"
+
+  depends_on :linux
+
+  def install
+    prefix.install Dir["*"]
+    share.install prefix/"man"
+  end
+
+  test do
+    (testpath/"Hello.java").write <<~EOS
+      class Hello
+      {
+        public static void main(String[] args)
+        {
+          System.out.println("Hello Homebrew");
+        }
+      }
+    EOS
+    system bin/"javac", "Hello.java"
+    assert_predicate testpath/"Hello.class", :exist?, "Failed to compile Java program!"
+    assert_equal "Hello Homebrew\n", shell_output("#{bin}/java Hello")
+  end
+end
