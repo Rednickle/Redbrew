cf5f6fb7135dca0d63b8a978e21d1d27eab125a2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-26 05:58:46 +0000

    frugal: update 3.3.1 bottle.
    Closes #12778.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/frugal.rb
+++ Formula/frugal.rb
@@ -10,6 +10,7 @@ class Frugal < Formula
     sha256 "a74917b7d8dfacdf362d84e77ebed45133c5ec60326bf7343f2ddc7c1c7d887b" => :mojave
     sha256 "387d228b3da35ab3585696ebe690489903f5ff9d182bdd04f18ffbcc19a6aa30" => :high_sierra
     sha256 "598847ee5457b77a9213d3322896072e802dbe884eb155646bffdb57169bf3ef" => :sierra
+    sha256 "f9ddb1e9a4ea27bbdd963d39d6812749757343f0825a7fe6ddc0c2e24fb69fb8" => :x86_64_linux
   end
 
   depends_on "glide" => :build
