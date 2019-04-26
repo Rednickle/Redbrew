7a58e689b7242038acaffec30c943b8a84c8d749
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-26 18:40:23 +0000

    conan: update 1.14.4 bottle.
    Closes #12789.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/conan.rb
+++ Formula/conan.rb
@@ -13,6 +13,7 @@ class Conan < Formula
     sha256 "0d5845b530b86a82451464e690b54ce78b8a5c2993fe5f3f933279c1be507fd0" => :mojave
     sha256 "589ae18c132ab455e7812a655f53abc96b401716884069491a15811faba12d90" => :high_sierra
     sha256 "bf42107eeb69068861bd8d08eafc7d64c71edd79044a738fc07cc2631df2901d" => :sierra
+    sha256 "f4c88ee7f00a7b81a1bafc1ccbac53c0038df4c3c10845b452a50df90b828812" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
