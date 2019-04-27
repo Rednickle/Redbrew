d29f3612411067b6babee1bef5fb6322f3e725e5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-27 16:56:05 +0000

    libjson-rpc-cpp: update 1.2.0 bottle.
    Closes #12835.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libjson-rpc-cpp.rb
+++ Formula/libjson-rpc-cpp.rb
@@ -6,10 +6,12 @@ class LibjsonRpcCpp < Formula
   head "https://github.com/cinemast/libjson-rpc-cpp.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "c6efeb76438b2951e0dc8225e97a87a699662da1b6f0965957cd850e90dc8421" => :mojave
     sha256 "e444d5b2fcc34189562fd4e01f2bcd20707acbd5588753f35a93dd1e0892b3ae" => :high_sierra
     sha256 "af4c5fd9f026676f7e89cf1ef94af01f685347cc1e928b72c681b84849156c0d" => :sierra
+    sha256 "c5653cba798a22db991df4e068ca1ad26c4808bd703adcceab45bcb929e2e0d3" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
