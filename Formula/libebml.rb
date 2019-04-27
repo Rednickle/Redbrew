bcc4ccc2a536f2e60cf4b6131b77bb611950cc8f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-27 22:13:03 +0000

    libebml: update 1.3.7 bottle.

--- Formula/libebml.rb
+++ Formula/libebml.rb
@@ -6,10 +6,12 @@ class Libebml < Formula
   head "https://github.com/Matroska-Org/libebml.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "98c762c686a8565ee45a71b7e779f5021eabdd1bafa62ad1e70b723a5ebdcdb0" => :mojave
     sha256 "edd3178663b28140e2ab39ebb91d8fbfd484842fdbd7648ae38c622040737b37" => :high_sierra
     sha256 "446ff193f844a84320328b9e675fb1a3dade456031d800a00684f3c24b99a99a" => :sierra
+    sha256 "5f94dd463d18cbb9c57c7a4f4afdc68a8b62a76b40ebf89ac60f192b455ca444" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
