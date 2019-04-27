1af28314095523b49cf9bfa93bbd9b526e9071ba
Michka Popoff <michkapopoff@gmail.com>
2019-04-27 23:57:38 +0200

    nghttp2: rebuild bottle for Linuxbrew
    Closes #12843.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nghttp2.rb
+++ Formula/nghttp2.rb
@@ -5,7 +5,6 @@ class Nghttp2 < Formula
   sha256 "ef75c761858241c6b4372fa6397aa0481a984b84b7b07c4ec7dc2d7b9eee87f8"
 
   bottle do
-    sha256 "95feb6ee217174380299fb08040897dcdbfc4c2693499e6740effca36194e493" => :x86_64_linux
     sha256 "aebb18502b1abd48b42022d09c37eff0486bdecd250e15e2cc98ca265f06e03a" => :mojave
     sha256 "20703302a2bd99bc0580c5b76fcaec062e65a6f40d356ff9f1e2d2134d92f117" => :high_sierra
     sha256 "b373c7c7f0df4e544a19fb623169bc3387515eb5aa53ca4a059624c20a5dec54" => :sierra
