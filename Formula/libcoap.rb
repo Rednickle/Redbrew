a0b7f907d0c28eb4d2bf2f6b813b80854b933142
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-27 16:37:59 +0000

    libcoap: update 4.2.0 bottle.
    Closes #12833.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libcoap.rb
+++ Formula/libcoap.rb
@@ -5,10 +5,12 @@ class Libcoap < Formula
   sha256 "9523e38da6ee8b2a8f5ce83ded64107dd1e514c7ad00cd74ccfe3454b679c271"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "e43618d83730577d3a647565e919d8b8db6ccd6d947995b001fc75a6e02655f0" => :mojave
     sha256 "c39515311e41413a6a569dd3b652cac35c15fe21a51140976e7f3294b14193b7" => :high_sierra
     sha256 "bfe2db4593b4da91a11ccb7e73e4626a68f5d27463cc8db4149e0da7e42372f8" => :sierra
+    sha256 "e48fb7cfee9511afcad4ddd3584a2f208f3a697ba531a68bac8e722899c316e8" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
