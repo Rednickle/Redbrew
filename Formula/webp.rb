934fd8ca55e869c4457fea0e8c850b947ef273b3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-27 17:31:52 +0000

    webp: update 1.0.2 bottle.
    Closes #12831.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/webp.rb
+++ Formula/webp.rb
@@ -5,11 +5,13 @@ class Webp < Formula
   sha256 "3d47b48c40ed6476e8047b2ddb81d93835e0ca1b8d3e8c679afbb3004dd564b1"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     rebuild 1
     sha256 "38bdac64dbf41666310ae1589e6102b6bcd688540514f7af4334dd74996c4277" => :mojave
     sha256 "b324a2a6eeb5c7c916a903f7249b6233334f99e7394b9927784319f086e21f8e" => :high_sierra
     sha256 "32e6c391ab45b17b167d565ef1ac36eeb2267483425f4b0f710f5445fa75df61" => :sierra
+    sha256 "b7e1e72538152c8d00acf9a2caa825bf7b2e2f2709ff0381a7f50ccb9dbaec8c" => :x86_64_linux
   end
 
   head do
