81be36ef3e7c23d7b99b5beb5ec2c6441bedaeb1
Michka Popoff <michkapopoff@gmail.com>
2019-04-27 18:00:22 +0200

    libfixbuf: Fix bottling for Linuxbrew
    Closes #12834.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libfixbuf.rb
+++ Formula/libfixbuf.rb
@@ -5,7 +5,6 @@ class Libfixbuf < Formula
   sha256 "2ba7877c5b09c120a20eb320d5d9e2ac93520c8308624eac3064aaece239bad3"
 
   bottle do
-    cellar :any
     sha256 "e880e6bba9e44a26a962fc5dcec07b1b1083c23d46036f39bc330e4b75616c12" => :mojave
     sha256 "ae3d11e94915eb3d2bb48061d0d3cc8ec7749cb833137a9648190c486850a5d7" => :high_sierra
     sha256 "fab9df238cb38319886f2ecd02d724fd481c1634d9d35ef5d616337ad1c1ab87" => :sierra
