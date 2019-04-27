e3fdab3f086a92948fad878670226e9402edf82e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-27 10:38:18 +0200

    ethereum: update 1.8.27 bottle.
    Closes #12795.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ethereum.rb
+++ Formula/ethereum.rb
@@ -11,6 +11,7 @@ class Ethereum < Formula
     sha256 "52461b891abac02d0f0c4a480c801e1723f70a530f13b3f278a2c69f4df5753f" => :mojave
     sha256 "58de68950620fc13da9ec8c3da574ad2ebc2b6aa1fe689fd115775c240e88b53" => :high_sierra
     sha256 "2a0a8fdc7e7d1d08443223f82e842f7136af579557f8e5add144828213924db2" => :sierra
+    sha256 "737b0bfaeb99a3ded7ea07ca25f27cd5a2ec86b5a85ff314b395829712ef160b" => :x86_64_linux
   end
 
   depends_on "go" => :build
