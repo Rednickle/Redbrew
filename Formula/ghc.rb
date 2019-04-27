663925b3a4f63aed0bff141342aac05325c7ac47
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-27 21:57:47 +0200

    ghc: update 8.6.5 bottle.

--- Formula/ghc.rb
+++ Formula/ghc.rb
@@ -9,10 +9,12 @@ class Ghc < Formula
   sha256 "4d4aa1e96f4001b934ac6193ab09af5d6172f41f5a5d39d8e43393b9aafee361"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "ab7cfe45159057538fcde888c14ccf19b0154ac6e0e2d9bd92727c5a0b7734d5" => :mojave
     sha256 "6736c0d1bbff9d7c775c70979b4df8e54d2183ded84965bc44fa6b0c16a0996c" => :high_sierra
     sha256 "4cb157956afc659f762e22e54c189e609d93a7f04f0d1ae6919eaafa5b255f74" => :sierra
+    sha256 "b0dee47c3b10dded4ee8aaf621c1742b98f13626c8698be60b9a7052495c7e80" => :x86_64_linux
   end
 
   head do
