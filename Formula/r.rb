73376fe37dd2e0e8ce860229f00a718ae11a800d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-27 17:24:47 +0000

    r: update 3.6.0 bottle.
    Closes #12837.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/r.rb
+++ Formula/r.rb
@@ -5,9 +5,11 @@ class R < Formula
   sha256 "36fcac3e452666158e62459c6fc810adc247c7109ed71c5b6c3ad5fc2bf57509"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     sha256 "99d04c26f4c4a94b6158e106fd7e626acd4de442ddc36954b28707ff6aed3500" => :mojave
     sha256 "fd5ad3de002a942fd6bbaf81a236acaa4550e92c2644eebd29093b94bcf697c2" => :high_sierra
     sha256 "56e507ee73f01f8c497000d8b261078399657efb73b4dfe012fb0a29d90b0b79" => :sierra
+    sha256 "2e531ec64895e8d8e731fb2b770d348576cd753ac1f845f8308337f79795c622" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
