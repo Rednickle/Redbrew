1511a242fb4f8831daeeaf6a1b8255a5a62fb176
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-30 20:37:59 +0000

    txr: update 215 bottle.
    Closes #12880.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/txr.rb
+++ Formula/txr.rb
@@ -6,10 +6,12 @@ class Txr < Formula
   head "http://www.kylheku.com/git/txr", :using => :git
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "d9879aab4e7ce800b09978690a8cf6e820ac874f3ffbd471716b7e3ce7926c30" => :mojave
     sha256 "c0b16efd0c98e3557a5c1b4c0ebd5bfbfe448a4462265a10e6b875c256e3a6eb" => :high_sierra
     sha256 "a194b3a1758154eb6fac28ce71bd54624a748cae8d9ea3683cd727241924377b" => :sierra
+    sha256 "a6e8dd0569ca2a5607da6917adb022cc2340d7488179a04a21f70b61d53e1e80" => :x86_64_linux
   end
 
   unless OS.mac?
