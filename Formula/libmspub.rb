e614b8ca77f758ac059eb9554a592577a78afffc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-30 20:45:56 +0000

    libmspub: update 0.1.4_4 bottle.
    Closes #12667.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libmspub.rb
+++ Formula/libmspub.rb
@@ -6,10 +6,12 @@ class Libmspub < Formula
   revision 4
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "93f7e02d005fb22101eab93659f200ed8376130c9a4bad0c24b0cdde51b92f18" => :mojave
     sha256 "43347c63d9fb4a6746929b2127453241cdad9b13a9fd00ddf82af418d6a175b2" => :high_sierra
     sha256 "ebf70558240b3cc3c49994e865108d7d29796c45d1129e844ef1c99d8ae510a7" => :sierra
+    sha256 "39f94b870bd72b1ff1c76dcb92f087ce93257554f1749206a619f0227de7644c" => :x86_64_linux
   end
 
   depends_on "boost" => :build
