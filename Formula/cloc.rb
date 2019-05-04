acfd7cbce3a21c0087ef441270e8ca8c940e8cba
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-04 11:27:49 +0000

    cloc: update 1.82 bottle.
    Closes #12912.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cloc.rb
+++ Formula/cloc.rb
@@ -6,10 +6,12 @@ class Cloc < Formula
   head "https://github.com/AlDanial/cloc.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "661f3c3b8eb7e0a348e36b5b1b7aebf2b579eec5ae078e7f2d2c64ab302a3644" => :mojave
     sha256 "15a38a7a5172213f2c1577a0aef34260203d5e0c25d38c6a4a2252990a9c9414" => :high_sierra
     sha256 "7e7622a28e8b32af41a292174a2f1ac8aef6e7f6a4735fefa7a3c83f7a11dd65" => :sierra
+    sha256 "2fdfeb3c77e3f9377e8ea016acbccd6afd906ec9aa67e2413ab7739231c4ca7e" => :x86_64_linux
   end
 
   resource "Regexp::Common" do
