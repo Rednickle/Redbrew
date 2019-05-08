8b17dc844de8cda5a55b55ebccedd50ad53ab7ab
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-08 11:04:14 +0000

    leveldb: update 1.22 bottle.
    Closes #12951.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/leveldb.rb
+++ Formula/leveldb.rb
@@ -10,6 +10,7 @@ class Leveldb < Formula
     sha256 "22e4a129bedd5030525f749a5b5ec978bf6da0a9b0625fe829da482a5ab85755" => :mojave
     sha256 "b1cf697cad28caac418d2e0ef49bc90863f389402185d3cc0f1f7079516d02c2" => :high_sierra
     sha256 "810dbeba5e3f7d72d4772b9eff4d9022a1240c0abc6235afbd343c199741e6f7" => :sierra
+    sha256 "debca309da693fdb3461c429d0f9116bb8970bd08833276a6400a280c458d8b7" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
