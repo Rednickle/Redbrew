eabe9a8c4bcde26a547f78e744de5e0267fa1d16
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-08 20:43:37 +0000

    gcc@9: update 9.1.0 bottle.

--- Formula/gcc@9.rb
+++ Formula/gcc@9.rb
@@ -10,6 +10,8 @@ class GccAT9 < Formula
 
   # gcc is designed to be portable.
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
+    sha256 "514ef09a811d2990fe7b8a9fdb615089508daa0cfee97ad64b5f3efd028bb587" => :x86_64_linux
   end
 
   # The bottles are built on systems with the CLT installed, and do not work
