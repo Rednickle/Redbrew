1332a4251f058abf1a286998abc5fa9f557ecce6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-13 21:03:22 +0000

    prettier: update 1.17.0 bottle.
    Closes #13059.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -8,10 +8,12 @@ class Prettier < Formula
   head "https://github.com/prettier/prettier.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "860aa7cb7536018a2a788aab9571d0668107e8b828ec9debd0355d9d63f61bf2" => :mojave
     sha256 "7a230db140344256227f2a2ed1b589592d7528534c34c56c1253825c5663f405" => :high_sierra
     sha256 "d18f315da66cedcda63ee7a396d7f5451be1e05fe427dd0cbbfabeae06f458a7" => :sierra
+    sha256 "b220a5b2f3cf155b39b332850d4ce026ba1bab767c8c954980c988c1d09b861c" => :x86_64_linux
   end
 
   depends_on "node"
