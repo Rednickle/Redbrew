36eb71423589836e4b4f32f8d2adea815ea6a57c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-13 04:54:29 +0000

    angular-cli: update 7.3.9 bottle.
    Closes #13026.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -7,10 +7,12 @@ class AngularCli < Formula
   sha256 "d9cf779176ffa78457b32564ce408317c6bf0fbaa945de63bbf56dcf85e6b6a5"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "d329d7044b1f022968052ed53dc896439deebc7e9d44c29a66663c4a37553a21" => :mojave
     sha256 "cfd357bb53800e9ad79d4fe1dd169f1db4d5418190eb4d544c0717086cbf82b2" => :high_sierra
     sha256 "450aab9eea610acca59959108c5e31cc8ecf21b3528ecc81455961447f700dc1" => :sierra
+    sha256 "fe8578c672941687d037c29d9a00e9aab96128fb0e669859c8bb0c243de39ea3" => :x86_64_linux
   end
 
   depends_on "node"
