20c5e77bc806346be4c0158326b0656e62ed728f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-15 20:48:56 +0000

    mruby: update 2.0.1_1 bottle.
    Closes #13095.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mruby.rb
+++ Formula/mruby.rb
@@ -7,10 +7,12 @@ class Mruby < Formula
   revision 1 unless OS.mac?
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "3279a9fe511530c4fe0a56323efcbfd555d2fa981204a0d1595f0365f56cdd59" => :mojave
     sha256 "3e808c9c9a25a98a20a8af26a369bf688db7c9a635e3ae52836e2eda0e42b442" => :high_sierra
     sha256 "5244571e026eefa0158a177b1b6e1c42c91760cb8e3b5ef449277660b31aaec6" => :sierra
+    sha256 "2bbc50653fade6bce8920d2085b41fdf2ed610ec1945ad7abacba46080117964" => :x86_64_linux
   end
 
   depends_on "bison" => :build
