1f6634dcd7c21dad8b55d60ea9c19c7dbf1c3fd3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-15 07:43:41 +0200

    llvm: update 8.0.0 bottle.
    Closes #13084.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/llvm.rb
+++ Formula/llvm.rb
@@ -71,6 +71,7 @@ class Llvm < Formula
     sha256 "6a44cd91d7e3b01fef214c31e736c77cfb681d2d0e6e576ab725c8071858a36d" => :mojave
     sha256 "d92a2a8b0066e2b70190d59cc980a12a2362b56f9100c7ef4051dc08e762c52e" => :high_sierra
     sha256 "168806783f260662a1bb126f300c21060fbf24d01424fd8e64cc5c54e2c81b9c" => :sierra
+    sha256 "47ba2940345aa497762273314ea3ff7d46769c94cce5098af794d11c339a86d7" => :x86_64_linux
   end
 
   # Clang cannot find system headers if Xcode CLT is not installed
