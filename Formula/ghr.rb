8992bda851d8ac515afacbf6be15d693962e7981
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-16 11:21:01 +0000

    ghr: update 0.12.1 bottle.
    Closes #13128.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ghr.rb
+++ Formula/ghr.rb
@@ -5,10 +5,12 @@ class Ghr < Formula
   sha256 "d124f7ad2d4bd5be2d6c51ad4d780d69fffc19e41440f7f14bcf2a24d415e006"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "1cb38dd46fc38adda97cee26f0ae38d0defa2aa3eedaf3a4517d3ca304cd9448" => :mojave
     sha256 "33a8801ddcfb493a72775f62e1500e376afe0eec6087499be80c85001cbebe9a" => :high_sierra
     sha256 "659559bd0e30b2164ac4440a77a87861bdd8a5f6de9d938a3b43e9a0910fc7af" => :sierra
+    sha256 "70eb2913f8a3db2a6bf063cc3122f22516c914239036cccd816ba16e6581a130" => :x86_64_linux
   end
 
   depends_on "dep" => :build
