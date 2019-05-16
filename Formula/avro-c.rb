5cc8c2777a1d3ab2870599f415fdf8dbafa77be4
Michka Popoff <michkapopoff@gmail.com>
2019-05-16 12:42:57 +0200

    avro-c: Fix bottling for Linuxbrew
    Closes #13119.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/avro-c.rb
+++ Formula/avro-c.rb
@@ -5,6 +5,7 @@ class AvroC < Formula
   sha256 "80568644384828b7f926efe76965907c8fff0c49e02301bfe8b2cb7965fa08b2"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "72fbe9c22be502351d5dcffcb37a97c754f3bb3057742bf31b185b0a0f1d5eb4" => :mojave
     sha256 "6a8b708a052cd04ffb7135b4d69b0f6b6a60ac83dc071622dfaf241f26998c79" => :high_sierra
     sha256 "e194efdbbebb86f6f553b039251165fa87646a4e21d29916560f6ce73ef8281b" => :sierra
