43adcbdb86b7462e208978e6a08feb0cca5678d1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-16 06:46:57 +0000

    topgrade: update 1.11.2 bottle.
    Closes #13109.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -10,6 +10,7 @@ class Topgrade < Formula
     sha256 "b5f5eea6fdda89349d009650076d92c36fdce5184f073a26d0148d5c86b5fa67" => :mojave
     sha256 "fb8fb3b546fdd33a1bb8b42458bacc21b8b542abe7d09311893bc4f5b97afe8f" => :high_sierra
     sha256 "d9c3bedaa1eb18f05a0815584c109f32837c5dcbe4bbfd35e75324f437c751be" => :sierra
+    sha256 "ea751157e90fb5e8c80ca8799a7041f82784e2c43294a172e3bca39ecfc2d065" => :x86_64_linux
   end
 
   depends_on "rust" => :build
