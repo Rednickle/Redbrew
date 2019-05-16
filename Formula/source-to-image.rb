a0d5dc7bcafc9681aa327d404d5c3f633f8b5e0d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-16 15:17:43 +0000

    source-to-image: update 1.1.14 bottle.
    Closes #13131.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/source-to-image.rb
+++ Formula/source-to-image.rb
@@ -7,10 +7,12 @@ class SourceToImage < Formula
   head "https://github.com/openshift/source-to-image.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "179ed06aa8cee8c8c7c6b8658eed696359170116c2ac366dadfaf086d533b033" => :mojave
     sha256 "a315100e844a70c78cf11eae24c70ff663f239fe79274da5b99223fbe6081a87" => :high_sierra
     sha256 "02ffcf2849f2051b0a7355b626ca06b2b2560ce09f68fff64f6b7a79966c750b" => :sierra
+    sha256 "affb12d302fff61a4f282dd8bc668481d19479299393b9a91ef6ad4a7e4a1be1" => :x86_64_linux
   end
 
   depends_on "go" => :build
