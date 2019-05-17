27286dcd74b3235159d9ec8d15d9ac6486cc36dd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-17 20:32:48 +0000

    spin: update 6.4.9 bottle.
    Closes #13154.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -6,10 +6,12 @@ class Spin < Formula
   sha256 "94eee2e854dc07b4e74f8e810ea3ff798678e8180db3c8df25d73a8fc7e4671d"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "dc68606f55b50ce72635956696ef717dd0451f91c64fcae20d6cd346f3220871" => :mojave
     sha256 "4748d5e21c7498ef2abfb5984980dee23ce7a3ff3130a6b13ffb9dffe1f75177" => :high_sierra
     sha256 "e1fd8d4855d41b00b2495adddb07933c6c460776b854f85dbe790e75aebb6fb6" => :sierra
+    sha256 "980fcd7ee7d31013dc603e25f24921530248e0881ea349df8a17f566e460bb1e" => :x86_64_linux
   end
 
   depends_on "bison" => :build unless OS.mac?
