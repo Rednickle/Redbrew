5f45d31fec800b300fdd20018c724573b1c644ec
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-17 22:28:35 +0000

    texmath: update 0.11.2.2 bottle.

--- Formula/texmath.rb
+++ Formula/texmath.rb
@@ -9,9 +9,11 @@ class Texmath < Formula
   sha256 "fe4e24af001104ed3c95ee44076e6819ffad67684efdabee5ae07cf8ceb81087"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     sha256 "ef810c923e8a459cc4d18ab596c348e4e8610b177c642242b4bf07915eab7e16" => :mojave
     sha256 "1782096bfc10752b5916f16aa188a37e57609f101f334be92b1abf23bf8ed59d" => :high_sierra
     sha256 "45e18931b08c56b33ec7610ebeae791d5acf496d1e3cfe949dca28162e10c15e" => :sierra
+    sha256 "dc81583654a200b5695626b83a3e6d837f46c0ea7bb66f7f2f6c8b7250861ecc" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
