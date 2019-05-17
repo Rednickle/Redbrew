2afcd9ca8d0fc8e0fa59faf8c81c87d1862d036f
Michka Popoff <michkapopoff@gmail.com>
2019-05-17 22:13:10 +0200

    texmath: Fix bottling for Linuxbrew
    Closes #13161.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/texmath.rb
+++ Formula/texmath.rb
@@ -9,7 +9,6 @@ class Texmath < Formula
   sha256 "fe4e24af001104ed3c95ee44076e6819ffad67684efdabee5ae07cf8ceb81087"
 
   bottle do
-    cellar :any_skip_relocation
     sha256 "ef810c923e8a459cc4d18ab596c348e4e8610b177c642242b4bf07915eab7e16" => :mojave
     sha256 "1782096bfc10752b5916f16aa188a37e57609f101f334be92b1abf23bf8ed59d" => :high_sierra
     sha256 "45e18931b08c56b33ec7610ebeae791d5acf496d1e3cfe949dca28162e10c15e" => :sierra
