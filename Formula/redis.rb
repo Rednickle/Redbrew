4903042af537ff62b0ec78b80dc9365fbf22f6f3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-17 23:03:08 +0000

    redis: update 5.0.5 bottle.
    Closes #13179.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/redis.rb
+++ Formula/redis.rb
@@ -11,6 +11,7 @@ class Redis < Formula
     sha256 "eb5538dadb10570c5a8aa9e8aafa5d21b7747e7597c92954916b9b6ca55ae407" => :mojave
     sha256 "e9de9e750d86c5f63ea3e60cd75fbdf9f8162494938ed14616a2dc4490577558" => :high_sierra
     sha256 "a519190fdd7b8d23d9faa3e2f512830b6c272b396770811d9a72f1137b485f6f" => :sierra
+    sha256 "5afb1f14082dbcd2091e57e20d112ddeedc26dfb20a0a88f10905094036278ba" => :x86_64_linux
   end
 
   def install
