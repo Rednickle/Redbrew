bb4772aa22a7661c0f8d25ae1e09083b0b0fbd99
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-18 21:21:19 +0000

    serf: update 0.8.3 bottle.
    Closes #13208.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/serf.rb
+++ Formula/serf.rb
@@ -7,10 +7,12 @@ class Serf < Formula
   head "https://github.com/hashicorp/serf.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "692266b0fa6ba7a7772849938ba42dc9246d1f117a216bdcb8bd1a130181a814" => :mojave
     sha256 "979604efa2ecf8c34e88d7060c84ffb003fe9eeafd26871bbc3cc94a77e0eede" => :high_sierra
     sha256 "e6578320d01e78c038df703f072ef26b9a340ce00146cd96ae1b9585331fa48b" => :sierra
+    sha256 "3d76f4f20d7551eaf3aa0c1bad76d62f9e25bcc33015342b7a172e6a1c7cbeff" => :x86_64_linux
   end
 
   depends_on "go" => :build
