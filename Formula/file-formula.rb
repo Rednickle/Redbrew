8148cc399ee514ac58d51a1a2b4187ca5802dbd5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-20 18:09:19 +0000

    file-formula: update 5.37 bottle.
    Closes #13221.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/file-formula.rb
+++ Formula/file-formula.rb
@@ -7,10 +7,12 @@ class FileFormula < Formula
   head "https://github.com/file/file.git"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "63271d014690b6ac45ca3ad13d23d6756ef196bd60870f7fbcf08853b60576c5" => :mojave
     sha256 "add66c41a0a6d051f263b9082ae931c0eb0f177bd04d4b2c08b79f89c3e6730b" => :high_sierra
     sha256 "4638417e6d477be3048d44c6ba1ac04c3aa9cd584c7e80553c5c6d153c8e5d86" => :sierra
+    sha256 "f1b77035b2afbfcc016d8a398458b8b9e7d89758766ed9d7efa0d5a35625dbcb" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
