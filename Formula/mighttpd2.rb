c03913515beff283da69991c5ea963555a554eb7
Michka Popoff <michkapopoff@gmail.com>
2019-05-20 19:49:39 +0200

    mighttpd2: Fix build and bottling for Linuxbrew
    Closes #13226.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mighttpd2.rb
+++ Formula/mighttpd2.rb
@@ -9,7 +9,6 @@ class Mighttpd2 < Formula
   sha256 "ed86af6c6156f1847565043bd6b80552b8e5dfa8ec4bac387eda58a647fee358"
 
   bottle do
-    cellar :any_skip_relocation
     sha256 "121f2b0b496d2ebed1eb141e225e6c708c110463334e88d3f0ae03f8e1f0864e" => :mojave
     sha256 "ffb3a6449fa17c79ebbeec0e27f94506d00ef840eb3549e09c96730bcd91abb1" => :high_sierra
     sha256 "24d9423146d9c85960de6db87eace6081b3d3385b9f0c9110afe63d2e9e34a7e" => :sierra
@@ -17,6 +16,7 @@ class Mighttpd2 < Formula
 
   depends_on "cabal-install" => :build
   depends_on "ghc" => :build
+  depends_on "zlib" unless OS.mac?
 
   def install
     install_cabal_package
