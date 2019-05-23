0a0a56b366374a31bea9226b6036faca5bafba0b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-23 22:23:56 +0000

    radare2: update 3.5.1 bottle.
    Closes #13298.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/radare2.rb
+++ Formula/radare2.rb
@@ -9,6 +9,7 @@ class Radare2 < Formula
     sha256 "0ef7a91ce4b25b3b5eaced6755e41b73cccb4a8aace4f2a6e0ca86faf4e8d0ce" => :mojave
     sha256 "864612e414d3aaf8c77e2f0f63abb2b3517fc46a2d5b5c947429252cef24a12b" => :high_sierra
     sha256 "f097dca460f7578ffeeb16055bc432b67c8d9fb9e8b5c2f802d9134ba6947622" => :sierra
+    sha256 "b456b9a35e0687b915bb42ea8b1584c188ff5128e87ad0c0b3d8e95e4a1cd56b" => :x86_64_linux
   end
 
   def install
