037e35260a82922acfb592ee86a12f0836f96a43
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-23 21:52:37 +0000

    cocoapods: update 1.7.0 bottle.
    Closes #13292.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cocoapods.rb
+++ Formula/cocoapods.rb
@@ -5,11 +5,11 @@ class Cocoapods < Formula
   sha256 "eefe349e04e5a7c2f580f4427755437cdaed10ad1fe6890bab8d4dc059d40fe0"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "deacee4d64a8a6085e81e02e1af9b9eac17f453da36a2d913a4cd7296c17b7da" => :mojave
     sha256 "6498f14fa5ae5f1e578c518722fd9daed1e7c9e0547c62d82f8fca804b84002c" => :high_sierra
     sha256 "04ad35e4113f7167ba482531f1440a798c512b7152cc08470da296f556a1a1e0" => :sierra
+    sha256 "922eea228cfb4f8e3b6cb0a22e95ba16394a2c53698fbfb06b5318c49ad1230e" => :x86_64_linux
   end
 
   depends_on "ruby" if MacOS.version <= :sierra
