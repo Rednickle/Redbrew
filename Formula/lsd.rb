6389ab0bde6deaf90930c662696f43d760f9cef7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-24 22:13:38 +0000

    lsd: update 0.15.0 bottle.
    Closes #13314.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/lsd.rb
+++ Formula/lsd.rb
@@ -5,11 +5,11 @@ class Lsd < Formula
   sha256 "e12b4472b174c3d5669a110331f5c1040472824f1e85bf44dc008fff4218d44f"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "7f408c1b76d7d93711d1db808045d0d4ed6b66060d282ffe2b852915869c8838" => :mojave
     sha256 "5293a84429d3ee507d5b871fdd313b0ebd0473d2fbb7a88ac18e639c5fc1d7e9" => :high_sierra
     sha256 "cb88c76b6424929985dd399ae50440afa0c4779c432e62b125ed1e44f02a1561" => :sierra
+    sha256 "d04640f803c9140205f8c134e8fe930c0a413acdc635fa9d1e000289131936ea" => :x86_64_linux
   end
 
   depends_on "rust" => :build
