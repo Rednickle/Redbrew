d5f02ea3cf01bb7da17ccd9b25769cef32b85a09
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-25 07:33:07 +0000

    sdb: update 1.4.0 bottle.
    Closes #13327.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/sdb.rb
+++ Formula/sdb.rb
@@ -10,6 +10,7 @@ class Sdb < Formula
     sha256 "ce339646aae3c8ca070d2bb6d98650f58642fe33bc441f09eb041426dbf03602" => :mojave
     sha256 "6f2385c418765b70c5b10bdacad22e6dd58c8bbb773ac1ceed4295ed7927dc26" => :high_sierra
     sha256 "7d8bfa3c93e75136e19ab3dc914ff5b0906714ccc98be7f7f431ebd94e65e258" => :sierra
+    sha256 "3912ee7ebe44c150f6ed6618658ed3792a15e244542ccaddcac40a0dd3967986" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
