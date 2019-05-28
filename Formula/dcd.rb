b2122b8c68824219124568004f9d55c9630dd948
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-28 20:38:23 +0000

    dcd: update 0.11.1 bottle.
    Closes #13361.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dcd.rb
+++ Formula/dcd.rb
@@ -7,11 +7,11 @@ class Dcd < Formula
   head "https://github.com/dlang-community/dcd.git", :shallow => false
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "3aef244e6e6b1f65e1dbceabd543928e01545ee82438f186eac906fe7d084823" => :mojave
     sha256 "6ab87eabcaed6d879c8cffb83087765c79c28e7cf10ebb8e9fabfa8b63d237ad" => :high_sierra
     sha256 "b3af1626e65317a6a8280b66fdaa5ceda0861b996c54f6542c2f6601374231ae" => :sierra
+    sha256 "ef46cb6b5949420716d67a8678cf2bdfa1e99c467889684183ab9360b470e25e" => :x86_64_linux
   end
 
   depends_on "dmd" => :build
