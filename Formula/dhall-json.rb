16c1e98f2945320a3851b22c4865d19dbc19e824
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-28 20:44:32 +0000

    dhall-json: update 1.2.8 bottle.
    Closes #13362.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -13,6 +13,7 @@ class DhallJson < Formula
     sha256 "61a8c523c47ca8d2f95da8bff97f70f2bf627f8febc8f8c7f54521c46facff24" => :mojave
     sha256 "d4713f73f9b860ca4e311c637282430e66f9d72e823f8c9bcfe85b1fa0d734f9" => :high_sierra
     sha256 "2ded62ecaaae75894f0114d7e48d47bc14ba4eeb1a92ed7b9bc6821716475735" => :sierra
+    sha256 "ab3e3b41760f69312f4f17982e1613f29cdfebb31e53b77d481f774aec4b61dd" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
