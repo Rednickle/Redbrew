e0e5263cc5618ba99894442df852dee601d7afdd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-28 20:55:06 +0000

    sleuthkit: update 4.6.6 bottle.
    Closes #13367.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/sleuthkit.rb
+++ Formula/sleuthkit.rb
@@ -9,6 +9,7 @@ class Sleuthkit < Formula
     sha256 "bfbdb6c56045f3d156cff38715348d31689e85af228297460449c5cacac2240d" => :mojave
     sha256 "35582862b81d7885aa26b7888e41c462f943e3938b2a7752e072e47e91eac106" => :high_sierra
     sha256 "f9d09d2168a61bc4ecd679b694c8e0d6d0609786199aca994968bd2b8d3c2f7f" => :sierra
+    sha256 "f25c2b77674b1093faf6058617163e821a48efe1fc6d7d8f90ee0f921a5c5f2b" => :x86_64_linux
   end
 
   depends_on "ant" => :build
