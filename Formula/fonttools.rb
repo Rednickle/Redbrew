a51dc63205eb51dbb683f22dfa2ba086ede4a292
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-29 10:08:25 +0000

    fonttools: update 3.42.0 bottle.
    Closes #13392.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -8,11 +8,11 @@ class Fonttools < Formula
   head "https://github.com/fonttools/fonttools.git"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "ccef6063dbd1009a7801cd9b7feb23c9c2c5c1e5fadeb936889070bf26f1d6ef" => :mojave
     sha256 "886cbaf629dcdff21e61d44ab75dacf960fc5d282be51f643145112034f45109" => :high_sierra
     sha256 "c658ec13b72e9ce4b5dfa5b560cf5cbdbd19104ca4e0d050a173e8f11722f94e" => :sierra
+    sha256 "f6e6c8c0589ae26797db0bbcfd20d3f227c4cc94695071ca20999febd330d41a" => :x86_64_linux
   end
 
   depends_on "python"
