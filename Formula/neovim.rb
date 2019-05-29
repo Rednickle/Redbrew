358be516d64301cf036a496fb8055e4ea6b18028
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-29 10:32:21 +0000

    neovim: update 0.3.6 bottle.
    Closes #13398.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -9,6 +9,7 @@ class Neovim < Formula
     sha256 "94b6dacc7b66b24ebf067d0185293654a456325afa98f144bd98feefa95471e0" => :mojave
     sha256 "6a50d4267f40d01610106ff23d4aa1b6889e7d9208eae7f5cf273b7015966cd2" => :high_sierra
     sha256 "535b0a1344a5f64707e9d206eff2cd8f3fe457e7aa0e8a021447a553c07f1f53" => :sierra
+    sha256 "4edd3312a8407aa1c65c3e5141f3b37ee0aa8350f2eaeedfeee937446f261f1b" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
