3a1bdfae313151071cda8dd0b37cdb48219d213d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-29 22:28:00 +0000

    cgrep: update 6.6.28 bottle.

--- Formula/cgrep.rb
+++ Formula/cgrep.rb
@@ -13,6 +13,7 @@ class Cgrep < Formula
     sha256 "b030a59a21df9915712bd26ce07b6432153fd6bb4eade27d9b7dcf4b5914258f" => :mojave
     sha256 "a20cf59639012dcc241fa03944d4761f25bc44d1ba3787a734c2c536ddc02c80" => :high_sierra
     sha256 "a13e9cfb33930c0569befd0fdfa70e38b523b526e98847a6adfa8e137461c791" => :sierra
+    sha256 "a7401ddea5e108600fb74367e64d285b7487737059e68d338d5a1580793fee6c" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
