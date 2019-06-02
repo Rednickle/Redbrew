e5e964f5db69cab9d575014193071ad3c6930903
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-02 15:30:53 +0000

    hexyl: update 0.5.1 bottle.
    Closes #13431
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/hexyl.rb
+++ Formula/hexyl.rb
@@ -5,11 +5,11 @@ class Hexyl < Formula
   sha256 "9c12bc6377d1efedc4a1731547448f7eb6ed17ee1c267aad9a35995b42091163"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "2ea951a75e72e97075b3d4db45edbfc6cbb27b67c4655fcd1483ce6ad2c4165d" => :mojave
     sha256 "52d0981ef2edc478f11e8c66a959f2868c0ee7e20a3178064ec289dfaa71e397" => :high_sierra
     sha256 "688108b44f0fb529f1085a1e7416d445b6b6c12885a9a941727c950233f8b6eb" => :sierra
+    sha256 "4a9339ec2dcb8f7bc9869042a602f8d4d79a29df3099638e58447d49484c4288" => :x86_64_linux
   end
 
   depends_on "rust" => :build
