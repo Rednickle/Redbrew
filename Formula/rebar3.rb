bc1166dd63445f9114bffe9894820d09b4cd4d6c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-02 17:21:55 +0000

    rebar3: update 3.11.0 bottle.
    Closes #13454.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/rebar3.rb
+++ Formula/rebar3.rb
@@ -5,11 +5,11 @@ class Rebar3 < Formula
   sha256 "d0f567bf5cfd60e16650b151a7caa24bf8164fb1c31359ce8b0452a683209421"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "b7118470996e2db24ea674753a54a111d69c07dfba914b364fa2e060e440fe6a" => :mojave
     sha256 "d9190ecd5789c6ea39f4f803effd192ab0c5800eff7fc5a4f201c095d76a940e" => :high_sierra
     sha256 "a04351f9d088bf25f9ddd32746f20474c0fe25a77bb72cddf88af39eb2918e44" => :sierra
+    sha256 "07eea26fd537865377f53797f10dc434881f0a787f4a2da1237b76f4e785bbf8" => :x86_64_linux
   end
 
   depends_on "erlang"
