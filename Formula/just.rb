d8a14f3e683f6a3a211465cf680259594853278f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-03 18:53:41 +0000

    just: update 0.4.4 bottle.
    Closes #13487.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/just.rb
+++ Formula/just.rb
@@ -5,11 +5,11 @@ class Just < Formula
   sha256 "131955a54ff1622349382288ba20db64b01acff802a42b719dd7ebcb2e9c8983"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "501d83b4a2d6d5e24f1429320909aca6c05787884a6ad690c55d9e6eb719d957" => :mojave
     sha256 "7df7779bd57aa0c48e7f97e0d617a76591e88dc6d5363b57c187f96f1ac594ab" => :high_sierra
     sha256 "23f3ddf400ef6ce95436539da360e21e3c89a373ad731cfb15a16ff8c5f3b82a" => :sierra
+    sha256 "ad0a3cdc0b4498f1fcc22ad9f63334dc8515ae2539f70d01f35b1d34c5171e52" => :x86_64_linux
   end
 
   depends_on "rust" => :build
