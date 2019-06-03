e47189232e6be7e365f2a2754e15228aa751bdb8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-03 18:08:57 +0000

    cmake: update 3.14.5 bottle.
    Closes #13473
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -6,11 +6,11 @@ class Cmake < Formula
   head "https://cmake.org/cmake.git"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "5c421f98d773951dc9cdb0888e01262e65243c1c19c05e6126293c30e835f912" => :mojave
     sha256 "301e78f6aad3a0072abb17ca0661618d8091235cee3de5b2bca730f5c5030769" => :high_sierra
     sha256 "5e052f843afebb90e17d44ae00257dc8781661f5af7d9c6f6dacdb789c14b790" => :sierra
+    sha256 "9ff6beab20a8ea16c97f402deb551200cdcf775653578ca898910aac8de617d5" => :x86_64_linux
   end
 
   depends_on "sphinx-doc" => :build
