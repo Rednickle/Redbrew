699749a53e0bbb31926def748a6d5fce3f486066
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-04 17:50:32 +0000

    mksh: update 57 bottle.
    Closes #13499.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mksh.rb
+++ Formula/mksh.rb
@@ -9,6 +9,7 @@ class Mksh < Formula
     sha256 "0028f1d8028129a398937bed7fb3b2f7608932386d116d1b05527ff266c3470c" => :mojave
     sha256 "d666938f31f5c9e5a0dd4b5f0e823ca7d5f2015cc447284ff0204cdc874d70e4" => :high_sierra
     sha256 "1fbdae63a63116e3a321b71d632214ea2419d404db8939f7cb3471a2de35c761" => :sierra
+    sha256 "7de7227fd81ea6707e2573acf32fbcce284133d38eda5f0d7e8e20f8e7e12d09" => :x86_64_linux
   end
 
   def install
