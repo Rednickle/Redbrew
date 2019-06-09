a489f7c6830188c7c4becd85e8d164719a057270
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-09 11:42:47 +0000

    hyperfine: update 1.6.0 bottle.
    Closes #13578.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/hyperfine.rb
+++ Formula/hyperfine.rb
@@ -9,6 +9,7 @@ class Hyperfine < Formula
     sha256 "ae46a59b81f0c2d89955d2c2f4add6c1aba0c1beceb59076a6b1a356ccbd6572" => :mojave
     sha256 "b972f3144fa116ae1698e8479ab6cc76ab9fc2a907423e8bd88ad260425b6e06" => :high_sierra
     sha256 "dc3dd6d6a5d5c5001f66690f54fe0ba416ce2e9cafc6a914108d859d866af502" => :sierra
+    sha256 "c9e7983731b403d2faeea8ae7006cf5156c8b0d3197e3ea928680bb7f665c618" => :x86_64_linux
   end
 
   depends_on "rust" => :build
