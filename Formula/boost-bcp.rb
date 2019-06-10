25db188e37a666e115f6e936b80b48a82c99d179
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-10 19:16:46 +0000

    boost-bcp: update 1.70.0 bottle.
    Closes #13587.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/boost-bcp.rb
+++ Formula/boost-bcp.rb
@@ -6,11 +6,11 @@ class BoostBcp < Formula
   head "https://github.com/boostorg/boost.git"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "e8bc92d0fd7a062612c2e19ea8993b30ba583fb92a4bc45467092dca5e93bca4" => :mojave
     sha256 "dba120d947a05ceed5a225c23a2a67dd97c4fbffcf45e45c19bd47cad0730a2a" => :high_sierra
     sha256 "d2f3b79542144ebc71b239084a5bab68d6aef7637d7545fa8e70d14eea5e98ee" => :sierra
+    sha256 "c5d3a12777ac0d12d8439d81fada07c52482ac57246d6eb2436751e313aaff01" => :x86_64_linux
   end
 
   depends_on "boost-build" => :build
