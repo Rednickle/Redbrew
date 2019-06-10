6596e7aa3c3b9adc148796c0637dd11497fccae9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-10 21:40:21 +0000

    anyenv: update 1.1.1 bottle.
    Closes #13598.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/anyenv.rb
+++ Formula/anyenv.rb
@@ -5,11 +5,11 @@ class Anyenv < Formula
   sha256 "d7f386f74d5fd40ded51b4e83f05490493fc6b63d22eb3d502836020548b0137"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "0bc3795db6ae8df4151de4fb15be7122a7c0c416d4bc553976e61b23cb9e3c61" => :mojave
     sha256 "0bc3795db6ae8df4151de4fb15be7122a7c0c416d4bc553976e61b23cb9e3c61" => :high_sierra
     sha256 "c4073892fe99575a1f23eb24714392f098c2980eaaf465a9a8641d955da08306" => :sierra
+    sha256 "6428e6a1ef33852c1cfce1c6fca11471fd7575080fb5fae0a8cf33046d7d583a" => :x86_64_linux
   end
 
   def install
