525e2a205f7153da55c41b29f04e15b77b368abe
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-14 18:12:39 +0000

    sops: update 3.3.1 bottle.
    Closes #13635.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/sops.rb
+++ Formula/sops.rb
@@ -6,11 +6,11 @@ class Sops < Formula
   head "https://github.com/mozilla/sops.git"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "65df7ca6d13386edc9c99f3c95c2e7b18cf586fcd228f6644f974c5ca508b52e" => :mojave
     sha256 "a465ee3a048e1ef83497325dc76370d96404f6789178a4efbd19215630fd58dd" => :high_sierra
     sha256 "8345f9f6375d27756ad9a8b6adab4c249bdc223b863e20258f869da79f90ea69" => :sierra
+    sha256 "f35ed89563e62024be7b0498c414ca0e33f3c8f1c19169ec58ff30fb6a90c547" => :x86_64_linux
   end
 
   depends_on "go" => :build
