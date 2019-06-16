bb16ee966947a140bfabc6cdb30ca4be8734238f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-16 20:23:58 +0000

    terragrunt: update 0.19.3 bottle.
    Closes #13519.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -6,11 +6,11 @@ class Terragrunt < Formula
   head "https://github.com/gruntwork-io/terragrunt.git"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "8c97c1a567785b862e6124d7987c92f0fbff867571f69e42460a88fb80b1551e" => :mojave
     sha256 "03f16255e224c9a4129f0dd579c8f47828329dd307b8db30cca23760c5ec450d" => :high_sierra
     sha256 "3324a8c3993cf1aef29e8138b9f39fab694637ebee5333b36367c1575ced0b9c" => :sierra
+    sha256 "e9048f05b523da657d61367ccdfcdfe3c7da398160d0b816aadb50d4abadc7de" => :x86_64_linux
   end
 
   depends_on "dep" => :build
