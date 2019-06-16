29d75d0d7fa04fb51ee30b57475221f4db3ac88b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-16 17:32:54 +0000

    pcsc-lite: update 1.8.23 bottle.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/pcsc-lite.rb
+++ Formula/pcsc-lite.rb
@@ -10,6 +10,7 @@ class PcscLite < Formula
     sha256 "0ed981ad7244d50d3084cef08991c5662658cd321d24784b2a6c2f8586f8f205" => :high_sierra
     sha256 "16167530e755c8c59a43b72433e9e1aba53b14f4f7364e85fc3159ecdc8ce75b" => :sierra
     sha256 "9a2816b3aaa1717d6c5d2c59495681915b62de67e496bb3edfecf1e5b6d537c0" => :el_capitan
+    sha256 "5893a25cd181bd3711d8a0b935f4a1fc9ab3e314accd2f1c5c60a0b27e2ee947" => :x86_64_linux
   end
 
   keg_only :provided_by_macos,
