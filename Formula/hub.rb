fd29da70f6a07cb0eda7bc413ffdad12d94a7a2d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-16 12:23:36 +0000

    hub: update 2.12.0 bottle.
    Closes #13674.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/hub.rb
+++ Formula/hub.rb
@@ -6,11 +6,11 @@ class Hub < Formula
   head "https://github.com/github/hub.git"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "6ccecb218ad8807cce2109d881f8d047d824c9ad09731523af067cd140dd0920" => :mojave
     sha256 "b1480c8de56ffea13694671327d17b1366b56f025434390909484b05c74836f6" => :high_sierra
     sha256 "6d176af6a6dac59b274fb9c00a5b95fba35a670d3b45eaa922682ff5256f71a3" => :sierra
+    sha256 "e92b28fa7c4d0d695b38aa711e5a49adef7e9413544ee9212d4e944219f52936" => :x86_64_linux
   end
 
   depends_on "go" => :build
