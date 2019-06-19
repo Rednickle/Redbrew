ad189fc7a627bf2141df3ad3831a245a4d6bf5ed
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-19 09:18:16 +0000

    axel: update 2.17.3_1 bottle.
    Closes #13788.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/axel.rb
+++ Formula/axel.rb
@@ -10,6 +10,7 @@ class Axel < Formula
     sha256 "8fe00eb038cef32171dc01872a2e5b39f23c26cab473f07a2abf8a2687b6db91" => :mojave
     sha256 "bfbf134d645d02caa73b768eec3aa0fe70fb571a30f6614dba551727101fbf74" => :high_sierra
     sha256 "aef5edb4248938590426081822d6e6f996c0e463c3b860a88b3a8943e678a84e" => :sierra
+    sha256 "4d86b21d3809e0cf5977f6535ca98d4a9d8473b7e54a843b0cb9eeceeed71748" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
