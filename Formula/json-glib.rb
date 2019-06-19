851dc3c52c5d98b5f5b09b3cfb76584fa71ea408
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-19 06:38:40 +0000

    json-glib: update 1.4.4_1 bottle.
    Closes #13781.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/json-glib.rb
+++ Formula/json-glib.rb
@@ -9,6 +9,7 @@ class JsonGlib < Formula
     sha256 "223b5472cc71a1eea8efc818d66fa8e6ff05a4aff45d60d4deccba54f82d39dd" => :mojave
     sha256 "ad30f6f204dd27504d70e9ac22dcfdd482975a5e97879c0b4095527bde68d985" => :high_sierra
     sha256 "08dbbf2bcef7fdeccfbcd7a0391c4eafa67f914ba0f021c8a41298a6359f7c24" => :sierra
+    sha256 "19bdfb31bf40ddba74d76e4f0f3b5570dc6ed214665d6ba31162c28b68acc03f" => :x86_64_linux
   end
 
   depends_on "gobject-introspection" => :build
