091719d25a5b12eb41740ae349a0a3169b4883c6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-19 20:07:49 +0000

    libfixbuf: update 2.3.1_1 bottle.
    Closes #13755.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libfixbuf.rb
+++ Formula/libfixbuf.rb
@@ -9,6 +9,7 @@ class Libfixbuf < Formula
     sha256 "72d1509d9b0bea98eb09e2fd2aaaff75a2a8bddbef035ee5c244578ccf247832" => :mojave
     sha256 "a7bd2c731284fd783a6b1cc74efc0d7b3e3668b6b02afc33ce307aba8a2b3845" => :high_sierra
     sha256 "cd6ba9552c12bfb738591876977a2470c6132c3fa649d542f9a9a064ffa64488" => :sierra
+    sha256 "5dcd7666ca491b99cc0f83b7547facde01df3dd873ad2e92713ea3b66c866e1a" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
