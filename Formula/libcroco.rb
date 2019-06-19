522207613cca15680fc6cc19f6a05266c671dcf0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-19 11:27:14 +0000

    libcroco: update 0.6.13_1 bottle.
    Closes #13728.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libcroco.rb
+++ Formula/libcroco.rb
@@ -6,11 +6,11 @@ class Libcroco < Formula
   revision 1
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
     sha256 "edf97f493296bfe01b2a8cfe156f1e8052e181bed6ea34cabaf18ed59ef28b17" => :mojave
     sha256 "f6e7d7d608dfcf6e57eaad77eef3cca27c15db0746e102f6dc33cccdd5a8a7bc" => :high_sierra
     sha256 "a95e3733bd72b789cc9a3cb9dfc9a92153939b984c4d1d47b8aa806e99e99552" => :sierra
+    sha256 "41fe29bcc2ada0f0d6812b61f2ab2c3b21f20b16992f81082a65d496e89cd6cd" => :x86_64_linux
   end
 
   depends_on "intltool" => :build
