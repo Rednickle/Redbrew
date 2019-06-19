bb236f5fe675d549d2e522aea70da557be8bb79d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-19 11:39:34 +0000

    libidl: update 0.8.14_1 bottle.
    Closes #13733.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libidl.rb
+++ Formula/libidl.rb
@@ -10,6 +10,7 @@ class Libidl < Formula
     sha256 "6221a3b0ea37b55c26bc1f83c84ce3e027a8925b92d63055a51fe3a7d6bdff19" => :mojave
     sha256 "9b07bec68567266f1bc065b05afdb9b034c0c70548145d7cdd963b5958c8da30" => :high_sierra
     sha256 "ecabcc1a9cd229a135557f0f8bc32a38d03d399ff6816b0fc897cc4bcf72cd1c" => :sierra
+    sha256 "17f46235b3351f513b02ef92deca3ce690606760749765a2141a57ae3d960a15" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
