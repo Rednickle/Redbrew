d80dc8f797a1cadbbd6490727c61b98f50f80b05
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-22 12:44:15 +0000

    logtalk: update 3.27.0 bottle.
    Closes #13849.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -6,11 +6,11 @@ class Logtalk < Formula
   sha256 "ddfbd126bd6663462d72df113bf2a77ccc197a03a4133d465b0d07afa8d62363"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "2faf1ca1f9fe57601031c0608196961c85e2fddbbe7158749cf8192afea09a92" => :mojave
     sha256 "61d14b01abacb9f7dec610f7c08721b63cda9738a8b7a7cfe6f6c27908618b4c" => :high_sierra
     sha256 "41dad06e989e133d836bbe0fe2815a72a65cb1b07df76fde221bc6ef8440f9ad" => :sierra
+    sha256 "c003624f0f4d9446a9242a4fef589cacb26bf76f5805e1a5127f0349ba97e085" => :x86_64_linux
   end
 
   depends_on "gnu-prolog"
