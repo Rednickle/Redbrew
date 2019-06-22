79204d285c5249df724d3bf1fb04b5576ae26e8a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-22 08:24:31 +0000

    libfabric: update 1.7.2 bottle.
    Closes #13838.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libfabric.rb
+++ Formula/libfabric.rb
@@ -10,6 +10,7 @@ class Libfabric < Formula
     sha256 "ee7a6ad88ba710561df9a0f22f13788ca748d3705b1c4abd7e35dc048317ab78" => :mojave
     sha256 "53f47481aebfaac4d9311bbae548541a8267a5081619cb4509652045ca9fc51d" => :high_sierra
     sha256 "5917821fb2f1b096f14855f4df97648134eb2e013c0438a393d97751b2cda36d" => :sierra
+    sha256 "722984b6fd09e5ed8fa3540fcea657f81496df492a22aebf452aba704da9aa6b" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
