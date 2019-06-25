c053f92620dcf1053f639720b4dc9db957e93fcc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-25 22:09:35 +0000

    libssh2: update 1.9.0 bottle.
    Closes #13913.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libssh2.rb
+++ Formula/libssh2.rb
@@ -9,6 +9,7 @@ class Libssh2 < Formula
     sha256 "0226917e00c2f5c003c441c11d06852d582cd6ddcb42042bf1f82515623528d5" => :mojave
     sha256 "14fd7b3358ef6766754997059cb9126ec95e7c44d17cffdca39e986278a2220a" => :high_sierra
     sha256 "53824fb7b5aa32db07cd99799cbfe0efb362a66ffcb84aba405755ca0eaa9f0b" => :sierra
+    sha256 "db54c4d6d266ea62c420505909e0dbd679809f2de6ca568fbcfeac6373a915f5" => :x86_64_linux
   end
 
   head do
