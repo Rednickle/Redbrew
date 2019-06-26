f872c77b659fe81af937b366b3b758ff4443be71
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-26 19:45:14 +0000

    iperf3: update 3.7 bottle.
    Closes #13953.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/iperf3.rb
+++ Formula/iperf3.rb
@@ -9,6 +9,7 @@ class Iperf3 < Formula
     sha256 "c6f1a298b199949fc2642cd6eb5860a5d9be4433d1cfbe88ec6734a97fdbfaf6" => :mojave
     sha256 "86cfc8e385961af48f2432773e5a5574d0aae07b7db14904b98c1f9c8d4f5063" => :high_sierra
     sha256 "c0e071e3241558fe1e96ee08a90d65af8429f5aa077454b4956a1183228a8271" => :sierra
+    sha256 "3abc0bec67a3830e0715794e4f990454d908759349f8251dc1e6bc84fad73702" => :x86_64_linux
   end
 
   head do
