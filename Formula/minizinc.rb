9530c1c98b851079a475089b1826cc0d8fcc3ba5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-29 12:10:04 +0000

    minizinc: update 2.3.0 bottle.

--- Formula/minizinc.rb
+++ Formula/minizinc.rb
@@ -10,6 +10,7 @@ class Minizinc < Formula
     sha256 "523a0ed0ebca6cca9a8fba3747ab872df946a3c8f0ccd47914d5fff390d59c5f" => :mojave
     sha256 "dfc74fc8ce50bc36dfb17a2e3bcca61046eb74fbccfe84821605637278b51262" => :high_sierra
     sha256 "269c5adacea9f4f3f900ca0e4e26ca233c7267826341507303fc26ab5baa781e" => :sierra
+    sha256 "0fdbb80d923b47f3b458db5a7feb593be6bdd896df000dc580b992cfbfe4ba5b" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
