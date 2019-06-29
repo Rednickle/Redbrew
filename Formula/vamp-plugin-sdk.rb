4d2051b5a2b33c1c8d511495b22174cc02c55df6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-29 08:59:44 +0000

    vamp-plugin-sdk: update 2.8.0 bottle.
    Closes #13977.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/vamp-plugin-sdk.rb
+++ Formula/vamp-plugin-sdk.rb
@@ -10,6 +10,7 @@ class VampPluginSdk < Formula
     sha256 "b430ba0960d22a9c954269ea60a8bea7ddb5df16c3d6525d8902fda8f7ec3d79" => :mojave
     sha256 "abb1682737521c71fda29e96cf841811f4bee42e6b56a77f9e4a1e3635106a97" => :high_sierra
     sha256 "6d14588e7f5932a267bf7ce8c3d507a2247d93a3ad363089a2d7b04ede47f2de" => :sierra
+    sha256 "82034a57b5c064f43032244ad6ebefbfaec39f440a932f2602e30238821836f2" => :x86_64_linux
   end
 
   depends_on "automake" => :build
