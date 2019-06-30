537cd068672c2a003911a44f4081ed45542191f5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-30 12:15:39 +0000

    datetime-fortran: update 1.6.1 bottle.
    Closes #13991.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/datetime-fortran.rb
+++ Formula/datetime-fortran.rb
@@ -9,6 +9,7 @@ class DatetimeFortran < Formula
     sha256 "160cac6100d26f5e09387daac49d16c40aedd38a44dc5aa0da950a62fc4f601b" => :mojave
     sha256 "2b3663b87374f2aff2bbe1ddc6436a2e387cb0f158149fcc11f46b73aa530e89" => :high_sierra
     sha256 "16073e773f18e30155af5805975b085fbd9c715cf0dcfe006cd1091de97c2cc5" => :sierra
+    sha256 "d89c6c6fd8e1f4be546f9d20f94926489ada7e32446869fa6dcdd9cbaa74efd5" => :x86_64_linux
   end
 
   head do
