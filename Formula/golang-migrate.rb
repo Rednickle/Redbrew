c7015fc84858ec0a558c13be9c7801cee365e730
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-06-30 18:21:38 +0000

    golang-migrate: update 4.4.0 bottle.
    Closes #13515.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/golang-migrate.rb
+++ Formula/golang-migrate.rb
@@ -5,11 +5,11 @@ class GolangMigrate < Formula
   sha256 "fc410720df01dd5f996b044a3278d19974348e3eb9bc7e2b0404204a041f4aa7"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
     sha256 "9ed41be7f790942c8f8bbe8082fa15bd6d18a95fbd589502fb827a008a2b1f7e" => :mojave
     sha256 "5368d364f8a69dece59ea7560e2336bb1a7a3f42c6d3443fe1ff9ae740f086e0" => :high_sierra
     sha256 "503eec5cec3b9c1e2ea3629cc3eca7a670e3776106224aa0d744a42994251270" => :sierra
+    sha256 "0d9049235bead6d11b2537390c56a84755cfe6b2250746890dbe39a0af2d6c43" => :x86_64_linux
   end
 
   depends_on "go" => :build
