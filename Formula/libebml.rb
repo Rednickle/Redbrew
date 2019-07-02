05a983532c4e28ceaaae9f204e0f5e04b84f3db0
Michka Popoff <michkapopoff@gmail.com>
2019-07-02 19:07:18 +0000

    libebml: update 1.3.9 bottle.
    Closes #14046.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/libebml.rb
+++ Formula/libebml.rb
@@ -10,6 +10,7 @@ class Libebml < Formula
     sha256 "ac543015948d0f4068ffc45c24ae09129ebaef9971d5301ffe43787f3146e4f2" => :mojave
     sha256 "bf005e877a8b7abdef3fc39c5964ad202ab6797ab1467c3209bc947ac4cdf70a" => :high_sierra
     sha256 "60249b3b2a0d6b4c18bd5ee8eb9a475b5a8622c5919b0e22962ce2232b691728" => :sierra
+    sha256 "66564d4ca86c090e4809d06745011ac3f0c6772cf24503b2951e0c76d806c3ed" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
