8f6690b104f5909489b418aa06ccdc207755497a
Michka Popoff <michkapopoff@gmail.com>
2019-07-02 20:51:39 +0000

    topgrade: update 2.4.0 bottle.
    Closes #14066.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -9,6 +9,7 @@ class Topgrade < Formula
     sha256 "fd53280d27c8d952bcd0131bed94d9b877a3c9ba779a41476793b3a95ac1471c" => :mojave
     sha256 "106995c6f9deed2418c0e8035c09429e156fb2f03e303f7f6726de601a1df41b" => :high_sierra
     sha256 "4d5a581846697c85c9897beae5f8cce48973527a78d92c80481940eb8304cd56" => :sierra
+    sha256 "032890fdbe4e00ebe3c0dcdbbe3239fcc9136afce75a95b3e995a65f5d2e41ba" => :x86_64_linux
   end
 
   depends_on "rust" => :build
