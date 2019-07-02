77d8e999e249d4dc9ad927c6d1f88181ab37ca41
Michka Popoff <michkapopoff@gmail.com>
2019-07-02 18:48:51 +0000

    libsvm: update 3.23 bottle.
    Closes #14055.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/libsvm.rb
+++ Formula/libsvm.rb
@@ -12,6 +12,7 @@ class Libsvm < Formula
     sha256 "75d440e35a774490aea6cec6fd514779069d3ffa55febce89a3f1eb8bad45337" => :mojave
     sha256 "661d867329c2851e84d02e78d2debc78357c9aa0d576223a1011b4d5533a7391" => :high_sierra
     sha256 "e78ffd8fb5a4c430e206462619ef419cde99f48728d09baaf250dc1cbc121abc" => :sierra
+    sha256 "ab51e04e1c11a68ab5d77fa3ed63f54a6bbdf7a8f8b030201c0fa51cd85cd725" => :x86_64_linux
   end
 
   def install
