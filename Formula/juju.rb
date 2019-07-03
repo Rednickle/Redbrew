f5c2b06dbdff7d6c18efe6d532e4f7035258c7f6
Michka Popoff <michkapopoff@gmail.com>
2019-07-03 07:45:05 +0000

    juju: update 2.6.5 bottle.
    Closes #14078.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/juju.rb
+++ Formula/juju.rb
@@ -9,6 +9,7 @@ class Juju < Formula
     sha256 "e15b0f01ed40d97e8e7b97e7bdbed94ffac93fce02b2bd18fe8a9f8b3b950f6d" => :mojave
     sha256 "8116144194e98eb743f021c179c79950d4a6d588c601e5a859dff6978fcfbca4" => :high_sierra
     sha256 "15effd09d7bb1e72ed86c8408676187fee9d1b6549d95a5b59163381a1784fd4" => :sierra
+    sha256 "1d2c42e97e392b84fc39f16e906c7e95d1806fd363c013c9f3dcef9a1552f488" => :x86_64_linux
   end
 
   depends_on "go" => :build
