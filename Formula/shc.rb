c2ab0a8cee9d5802f071fd9be45a4c675cdc4222
Michka Popoff <michkapopoff@gmail.com>
2019-07-03 07:33:18 +0000

    shc: update 4.0.3 bottle.
    Closes #14084.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/shc.rb
+++ Formula/shc.rb
@@ -10,6 +10,7 @@ class Shc < Formula
     sha256 "ff3c55ef1d10c16066e97a20143dbd1e7781ceb9a2c5c8b46d140f6711bc79fa" => :mojave
     sha256 "c19f4586119be579006eace517045998138d83a17e2b5c8ec00ad73ea007b68c" => :high_sierra
     sha256 "6e1834ac7b4cc64ba972a59189512bb9ff9e0ec307df78f9e0fc1fee42378f6d" => :sierra
+    sha256 "07d4f2747a3777202794ca5d5fbcb1a79033df428ef6659d0cb4d83be3bd2c8b" => :x86_64_linux
   end
 
   def install
