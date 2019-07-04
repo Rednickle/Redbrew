12ae21ac5bdde39712f14aea7066da7e2b088da2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-04 10:08:53 +0000

    oxipng: update 2.2.2 bottle.
    Closes #14104.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/oxipng.rb
+++ Formula/oxipng.rb
@@ -9,6 +9,7 @@ class Oxipng < Formula
     sha256 "83f39a695788d36e81ba308da53403aa7b160bad1895be20970a9f5feeba6ab0" => :mojave
     sha256 "4137060ab6b019b98c1f9f5f9dc357062563f9639add33798b52e1bddf733aa7" => :high_sierra
     sha256 "629c0ba06af7be85b205bd89c0430b2bbaf4200cab77ac86fc73ec7e824cbabe" => :sierra
+    sha256 "e6c02f841d1e392f6618e59b13aa599a11a210087efd212e8e720ee6d5e23400" => :x86_64_linux
   end
 
   depends_on "rust" => :build
