84ee2f566b1414b9b844c86c630353d999edc873
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-05 18:36:58 +0100

    bzip2: update 1.0.7 bottle.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/bzip2.rb
+++ Formula/bzip2.rb
@@ -9,6 +9,7 @@ class Bzip2 < Formula
     sha256 "0e2b2fd73f5cc63d50278b89a66da9313b058e43a5621a9a61c204b908f3af18" => :mojave
     sha256 "0c791995ffa600c0f5d2d6566619ceffd309fee6f45b867bb0823e5812dc9425" => :high_sierra
     sha256 "48ba103cc8c92c4f63f0ff8557368d73625e767908accd69ac830df840b8561a" => :sierra
+    sha256 "fff765468176d9cad3d6c686aac213ba4e9e36c08226f0a3695ef1dc010f3876" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
