27f5cded539628539575a7e4305fcf188296ba8b
Michka Popoff <michkapopoff@gmail.com>
2019-07-11 21:08:16 +0000

    u-boot-tools: update 2019.07 bottle.
    Closes #14190.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/u-boot-tools.rb
+++ Formula/u-boot-tools.rb
@@ -9,6 +9,7 @@ class UBootTools < Formula
     sha256 "d8ed22b53de5ef5e3838383676c4f3a98ba2d0b1426d642ae966695776e24ded" => :mojave
     sha256 "19bb4b4cb1a75cdff0f03545d9a343d21159881b7db79bf1efc05c1093b5d371" => :high_sierra
     sha256 "700555f219bb29d8b01676e0c7c77d3603acdc4f8384d3ac4f322fbc621104dd" => :sierra
+    sha256 "b70707639a75aa8def12c59466fa4fc1a8562f79313b225dfffcd7fe14b621fd" => :x86_64_linux
   end
 
   depends_on "openssl"
