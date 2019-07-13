ac60fd6c1ce61f00a9dc20747a4b994d576a16ec
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-13 13:56:08 +0000

    modules: update 4.2.3 bottle.

--- Formula/modules.rb
+++ Formula/modules.rb
@@ -9,6 +9,7 @@ class Modules < Formula
     sha256 "6704ca06536aaa7b15d6f4b7a44a697e5e264585d0725f9a7a2add96699e420e" => :mojave
     sha256 "d3f08b74c54724430e0bf0b9bbbff9c1eeb1c22b98b14c9d4d3221c8ae2d4161" => :high_sierra
     sha256 "6c639f39e28ad98625f59540125fda9e86f4241ecdaa1d957b5f2ec413d014b0" => :sierra
+    sha256 "4d9af8225dfc37ff56fad29fadfac56fe0ccfe846efb6fd27ec6ad6898259ddf" => :x86_64_linux
   end
 
   unless OS.mac?
