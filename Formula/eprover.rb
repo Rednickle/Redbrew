ce0099f10de3ddd6835fdcd011005768428d569b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-14 14:56:11 +0100

    eprover: update 2.3 bottle.
    Closes #14274.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/eprover.rb
+++ Formula/eprover.rb
@@ -9,6 +9,7 @@ class Eprover < Formula
     sha256 "464244b8f862c83abbf2b5969790af6f80fd26d21cd1b1d8c575b44c6c73c9ec" => :mojave
     sha256 "6f51683fc53c488f2d16fa3b6fab7ed30e7b4a6c99dd42331c29631120891c67" => :high_sierra
     sha256 "d18b5d1b173a6061bd90ecb590ce95ae864c562786376583d57029ce651060e4" => :sierra
+    sha256 "e2549dc5ecf1fbe4895084b1b981a2e84041a289677b8204000b4b82d64cfad2" => :x86_64_linux
   end
 
   def install
