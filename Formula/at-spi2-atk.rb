4877087526b85d6bb3037b805b4cc4b9f1ce5366
Michka Popoff <michkapopoff@gmail.com>
2019-07-16 20:20:47 +0200

    at-spi2-atk: bump revision for at-spi2-core
    Closes #14311.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/at-spi2-atk.rb
+++ Formula/at-spi2-atk.rb
@@ -3,12 +3,11 @@ class AtSpi2Atk < Formula
   homepage "https://wiki.linuxfoundation.org/accessibility/"
   url "https://download.gnome.org/sources/at-spi2-atk/2.32/at-spi2-atk-2.32.0.tar.xz"
   sha256 "0b51e6d339fa2bcca3a3e3159ccea574c67b107f1ac8b00047fa60e34ce7a45c"
+  revision 1
   # tag "linuxbrew"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any_skip_relocation
-    sha256 "c7f6b4c70342f5b969a9fd38b950169cd26e41d33fa4946f6a9f515f3eb3e213" => :x86_64_linux
   end
 
   depends_on "meson-internal" => :build
