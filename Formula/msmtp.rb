cdff0a231cf40aae44c69d49d7316a560f6a2784
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-16 06:53:41 +0100

    msmtp: update 1.8.5 bottle.
    Closes #14293.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/msmtp.rb
+++ Formula/msmtp.rb
@@ -8,6 +8,7 @@ class Msmtp < Formula
     sha256 "b565cc6011abc3d4491ce6e35362dd97297bcce3d6694cba7bc20bcc756d6738" => :mojave
     sha256 "cd32f619d3b85794bc62b2374ca2ee9874014c2d31b609f34af437311ad2f028" => :high_sierra
     sha256 "a2008bd10e0465b0a165810000c18bf1a8a25d3de154ed2fbe0206b70ce5fb1f" => :sierra
+    sha256 "9f64b39a64ad60a4f5479537a6848f686dfc16ed0a23e3f235fb6ddd264adbb4" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
