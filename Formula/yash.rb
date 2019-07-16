5362217bc45a87d910e3a480443a2df7464dcd45
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-16 06:52:27 +0100

    yash: update 2.48 bottle.
    Closes #14291.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/yash.rb
+++ Formula/yash.rb
@@ -9,6 +9,7 @@ class Yash < Formula
     sha256 "78fbbc947f0cbe7484acc442be951dd47941358220bc4abf75f2e466b746efde" => :mojave
     sha256 "ff5e85274a16fedb468c62ce09866ca475f6c651f0fd139b46847e07f4c120a6" => :high_sierra
     sha256 "29e2875a5ea150b0517006280265ed2a11da8cb9c050b1b1c34777f2a90b3e22" => :sierra
+    sha256 "1bcb67c40c06172e6f5e3e2e91fe83d3e6796d44b4ad01ada641dedd80178575" => :x86_64_linux
   end
 
   depends_on "gettext"
