dbe31f7118549129a7e6f8c6bd654359ea7c5747
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-17 21:03:40 +0000

    txr: update 220 bottle.
    Closes #14336.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/txr.rb
+++ Formula/txr.rb
@@ -10,6 +10,7 @@ class Txr < Formula
     sha256 "7951f53389d09a07248fff9ec2a388efa4cc61a9d6b3a976d5df15dd6114de8a" => :mojave
     sha256 "81824c3ba4c8da89b9f95eb233a297ab8ba650373f00107624c8bac57b086689" => :high_sierra
     sha256 "dc143712f1109bf6e08c0b949a55b201b9ec9610952a4e139edcbeab15c5ce67" => :sierra
+    sha256 "260e470269a09ef03a27c9374acef8a4148c1f1fc5da16db6af55bb22741a8ac" => :x86_64_linux
   end
 
   unless OS.mac?
