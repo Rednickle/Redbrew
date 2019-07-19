13d5dc5be98ace1b7bd268101d1ab68bfb840c7a
Michka Popoff <michkapopoff@gmail.com>
2019-07-19 20:32:19 +0000

    esptool: update 2.7 bottle.
    Closes #14373.
    Signed-off-by: Michka Popoff <michkapopoff@gmail>

--- Formula/esptool.rb
+++ Formula/esptool.rb
@@ -11,6 +11,7 @@ class Esptool < Formula
     sha256 "8ba26f9a3c2457a2b5f6c3bc5b0b81697cea4ef7fb4bc0f48e30820b577be377" => :mojave
     sha256 "9f24cb9c1d5408306019e46886cc935ba253e1ea3a747092ba7a81f964bfdbb4" => :high_sierra
     sha256 "d5916d7cff50d27f1dd3928de976ae17a76d27f41c4ec62f80d658d43fd9b65e" => :sierra
+    sha256 "7caaf9ef080ac8b02f2ba012ba1219c5ec95c5055b84c71ad0505c5e4ec836cd" => :x86_64_linux
   end
 
   depends_on "python"
