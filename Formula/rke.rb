9a355a999bcc720a58b585fcaa70289c890013a8
Michka Popoff <michkapopoff@gmail.com>
2019-07-21 20:53:02 +0000

    rke: update 0.2.6 bottle.
    Closes #14424.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -10,6 +10,7 @@ class Rke < Formula
     sha256 "94abe8039ce77b9ce9029465b622d877bffe98e19f15f6ee5ae7321754f1f51a" => :mojave
     sha256 "8b543d43a61af691bb8b7bec4bc4390ab4076020cc8f0e451ffaf1ee0a180cf7" => :high_sierra
     sha256 "20795f930c598a60eb7515a4f9de770b9062b5eb4be4141f87d42013b8e5f320" => :sierra
+    sha256 "6b4726d75ca0a69707563e63d496a98371b728d7caa7203892196f172df7e8d0" => :x86_64_linux
   end
 
   depends_on "go" => :build
