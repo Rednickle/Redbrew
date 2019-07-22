ca92a2a82c81b1afaa230ed1a5003523f306c170
Michka Popoff <michkapopoff@gmail.com>
2019-07-22 19:45:45 +0000

    avfs: update 1.1.0 bottle.
    Closes #14406.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/avfs.rb
+++ Formula/avfs.rb
@@ -8,6 +8,7 @@ class Avfs < Formula
     sha256 "3aa296ad227248a43fcb7723c0f5c9779662ee3abc8b1e6477c0271a1e50c6fe" => :mojave
     sha256 "bcde7798b2fff699e68237479e4a106323becee2028d47080e82f0769b792120" => :high_sierra
     sha256 "bed10bc8e692380a485c14719e42dc6ba386cc756acb471f7b5e65a3428f0d68" => :sierra
+    sha256 "93ac30865e9766d551c0f1a3c6a2fe2e8b3576f247213fd2cdad4d244af60732" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
