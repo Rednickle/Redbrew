e98539de4dc309b9b4df3cc0df3ed01838d2b5db
Michka Popoff <michkapopoff@gmail.com>
2019-07-23 21:26:34 +0000

    armadillo: update 9.600.4 bottle.
    Closes #14435.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/armadillo.rb
+++ Formula/armadillo.rb
@@ -9,6 +9,7 @@ class Armadillo < Formula
     sha256 "b28e62bccc2d6607f7a1143f6e5433e90d9b5ee146b2d2e34b90e183738e8f63" => :mojave
     sha256 "5b5d6c39266b006b87f6a4052cd5a414ddd0c36617205fb2ce006acd74bd4bfd" => :high_sierra
     sha256 "b1b01095d45bfd6dc922d4acd27d0621efa93e192660f8bda69d16cd6ef8c554" => :sierra
+    sha256 "fb03199836c9e23b5fd3096e87818a0634415dbfeb7806878fca1a6524253f02" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
