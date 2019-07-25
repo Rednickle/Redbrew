2d812d1dbfad71dabb65c941cad9b67857922e8d
Michka Popoff <michkapopoff@gmail.com>
2019-07-25 05:48:17 +0000

    tintin: update 2.01.8 bottle.
    Closes #14464.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/tintin.rb
+++ Formula/tintin.rb
@@ -9,6 +9,7 @@ class Tintin < Formula
     sha256 "ec973156e15950829255f554229f0787465c3c2fe25fd8f69dd954be209f5bdc" => :mojave
     sha256 "6631fb2271ee2f791ead160d473de77dc249c6924f89e0d8dcb041e1ce338b7a" => :high_sierra
     sha256 "61cbde7c970e7f1eff79fe5397a349f086225c6b25f21c4d2d1119aa4c221c9f" => :sierra
+    sha256 "cf3a6f54a5244321a2ab92c5007a307b0fdc7552f5752ea09d3304cb150d3fd5" => :x86_64_linux
   end
 
   depends_on "gnutls"
