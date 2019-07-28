1f9d7f8dc7eb475fadf5b074f1191c9cc4cc2a8b
Issy Long <me@issyl0.co.uk>
2019-07-28 23:22:15 +0100

    h3: update 3.5.0 bottle.
    Closes #14493.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/h3.rb
+++ Formula/h3.rb
@@ -9,6 +9,7 @@ class H3 < Formula
     sha256 "d16ae178de9da08bafc054d99334ed906b0636a595ea825fb8d001bd24826e18" => :mojave
     sha256 "a7b8019bd60ed630389e01a344e30ae21aec35ca39c8e734e78f5fdaf6b0a891" => :high_sierra
     sha256 "b13a08358eba6b6c70056b16ab20823379af5bdb3bf23f5bac0ecbab1dabf13a" => :sierra
+    sha256 "b92117f73fe5d79c19fbe653721078c1b8bc54757248a2764f07b787d366bfc4" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
