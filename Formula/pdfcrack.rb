22a85c29e982a88da8f19cfd55da1e727487fe7b
Issy Long <me@issyl0.co.uk>
2019-07-30 17:51:31 +0100

    pdfcrack: update 0.17 bottle.
    Closes #14534.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/pdfcrack.rb
+++ Formula/pdfcrack.rb
@@ -9,6 +9,7 @@ class Pdfcrack < Formula
     sha256 "a90c034da71cc7aab72ea89c9693c73294258d33eff99449494a47824f7f74cd" => :mojave
     sha256 "5b04e434a4a7f19cda66fc9fd85d06af7911c6a8541da9b256d0286b2e4a3ef8" => :high_sierra
     sha256 "e62512a0ab06aef14644bfe289e9e22ec579c17f64767346ccd4562a389211ca" => :sierra
+    sha256 "323943953342d0f0521facf61dbbff4dccd1dc55a7937235570ae1e057e8d967" => :x86_64_linux
   end
 
   def install
