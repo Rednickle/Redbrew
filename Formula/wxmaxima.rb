b4286e9529d3e45226770ca9d1ff2ee59d71f00d
Issy Long <me@issyl0.co.uk>
2019-07-30 17:36:06 +0100

    wxmaxima: update 19.07.0 bottle.
    Closes #14530.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/wxmaxima.rb
+++ Formula/wxmaxima.rb
@@ -10,6 +10,7 @@ class Wxmaxima < Formula
     sha256 "175947405f4517d9f95ba58de56b3288b0040be440f85696c71fa29f7bee37bd" => :mojave
     sha256 "eeef1f6c1d67f75c8ad98b9443dc5b343c480bca0a1734a91b14e1aa613f26e2" => :high_sierra
     sha256 "2f1984b37e3b3c99e1b018014837b292bd3126176fda57cdf84df6ac38ca9afc" => :sierra
+    sha256 "084c21a8461cba29c0fd9ffa48a1e27f746e0214b173ee2eb89dd924b85cd2ab" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
