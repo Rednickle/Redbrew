a819d98609848d68e7819548669d90b75469d403
Issy Long <me@issyl0.co.uk>
2019-07-31 08:42:27 +0100

    docker: update 19.03.1 bottle.
    Closes #14558.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/docker.rb
+++ Formula/docker.rb
@@ -10,6 +10,7 @@ class Docker < Formula
     sha256 "ed32c0a9455ff5f193f6f6f8d1754a02534f6419f5d36af5c5c7adeb5a10f51d" => :mojave
     sha256 "bb3c3929bb7c1897442638ed57d0d8eb84b863b68d33122a5e6b39283e06453f" => :high_sierra
     sha256 "ceee85312a0a8fc94afa85060743486fa127893c8fd1f09f4edc65a2db526d55" => :sierra
+    sha256 "79350e4a51d6fd133f615865b1ad62eec85624792fb9050adb08b3c31bdb4107" => :x86_64_linux
   end
 
   depends_on "go" => :build
