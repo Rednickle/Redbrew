2e78abcd30f2f4f7636375ebbbe5b43c215d7ded
Issy Long <me@issyl0.co.uk>
2019-07-31 22:48:23 +0100

    dhall-json: update 1.4.0 bottle.
    Closes #14595.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -13,6 +13,7 @@ class DhallJson < Formula
     sha256 "c9a6bc1b5598ecc0c1139e907a334751c99fa56ea2eea29cd6b8d32abc487e22" => :mojave
     sha256 "7bd58337a9afd282eae2cc13ed54b5a28f2f89f3a2bc58591835aed804a565f5" => :high_sierra
     sha256 "b1e13d14655544535611979e8f2d4ff543501a42fcf2c700e2cd73a1090c5874" => :sierra
+    sha256 "e87452b58cae3f09951d79e4ea8a0a68140e95cbb7079f7679a251ae614cc7bf" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
