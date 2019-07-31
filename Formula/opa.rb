efc47a2c3f08dd344c5f080cc227e63402a109b5
Issy Long <me@issyl0.co.uk>
2019-07-31 22:41:32 +0100

    opa: update 0.12.2 bottle.
    Closes #14572.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -9,6 +9,7 @@ class Opa < Formula
     sha256 "cc29253c80c7349395c0ce774cc50b29a2310961fc619accb8861f907241aa66" => :mojave
     sha256 "53e73f344b15003d4568a2fd005e06100adda8052fa9b511db7b5a5be852cb6e" => :high_sierra
     sha256 "c4305ad6dcfa3417ff600bc6c8f9ec2b36b8bd446d5dd606ff97011fa32d3614" => :sierra
+    sha256 "ecd03026f98e71049814367e1e3ac8b5754559578d1fd5cf940359624adb723b" => :x86_64_linux
   end
 
   depends_on "go" => :build
