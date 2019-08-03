acd86728a65f021f1d5ea5e2d2a0c457cba4c677
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-03 20:27:43 +0100

    kubernetes-helm: update 2.14.3 bottle.
    Closes #14626.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/kubernetes-helm.rb
+++ Formula/kubernetes-helm.rb
@@ -11,6 +11,7 @@ class KubernetesHelm < Formula
     sha256 "35424b638f34e451a572959e2548c97a18589a97e997053f4c25e39511e17ac8" => :mojave
     sha256 "b0dabf5935ba18efd9c247f75b8b50efcdb34e6f2d74bc8ccb781bc78ca612c3" => :high_sierra
     sha256 "4e43a2e5ca35248314a84d9e791de028a9634b7060ecb6188192f59e7010faec" => :sierra
+    sha256 "c5192cfe3057b03d7b24255d93f134783a61b276fa2d57239c8553c839c47fda" => :x86_64_linux
   end
 
   depends_on "glide" => :build
