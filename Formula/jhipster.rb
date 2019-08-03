b214a3708a9010fccc3dc37834c22eec80524e9c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-03 17:56:21 +0100

    jhipster: update 6.2.0 bottle.
    Closes #14616.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/jhipster.rb
+++ Formula/jhipster.rb
@@ -11,6 +11,7 @@ class Jhipster < Formula
     sha256 "340d6d9d8f1a34fe9a07e8f33b3dd4ecfb274f6eddd45bac1e0f78a182c65881" => :mojave
     sha256 "20348a0a98e87751ead0c9359afbdeddd8f9bae8ebe29393ab8afb50e206bd72" => :high_sierra
     sha256 "54736305b2b8cb0d3d14a3daec676729f2c220f5449cac2957fead2bcf6bc3ab" => :sierra
+    sha256 "658aeef40e72f1ccc4a5772a17bceb52108c47c66d55e16440fef9c2877c6431" => :x86_64_linux
   end
 
   depends_on :java => "1.8+"
