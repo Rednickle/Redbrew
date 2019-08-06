566fb3585d6c6fae2c0919e2632088a5b074cdcb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-06 17:58:47 +0000

    lsof: update 4.93.2 bottle.

--- Formula/lsof.rb
+++ Formula/lsof.rb
@@ -9,6 +9,7 @@ class Lsof < Formula
     sha256 "3532e9650fc5b836d5584cd8733f83218229d170859bf2c85e62b4abad08d356" => :mojave
     sha256 "8ebe4f68ada3d1c1984bbfb660437984f5fc9d61c93b3da8024bfd0d797a2172" => :high_sierra
     sha256 "e07f28fd45b1eae5231393b45360920da4ad4386a83ccb6a256fc7ea26509f59" => :sierra
+    sha256 "2d8714fc3de7092cf1ccd7022fd3a420a00c6df64942712060f91bebaec41623" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
