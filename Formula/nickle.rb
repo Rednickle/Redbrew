dc3e0c61186d3c23d8a3869e32ae2f65c47a48a9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-07 08:25:25 +0100

    nickle: update 2.85 bottle.
    Closes #14654.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/nickle.rb
+++ Formula/nickle.rb
@@ -8,6 +8,7 @@ class Nickle < Formula
     sha256 "6294e2a7a132be9cbda76ff8a8529971a0a034a06455060fad7746cb32b0767a" => :mojave
     sha256 "e5ce6aaea5ca0a7f0680d57fc9ff2d74128bda489bd0f1cc89e7c2f40ddc6958" => :high_sierra
     sha256 "8204000310cac65645b48f68855aacd3f8257d11395f9e96b2751c0831c0628f" => :sierra
+    sha256 "992cf57d28b0be4897d851d70265be3996b132d3a1befc07d38a600069dd899e" => :x86_64_linux
   end
 
   depends_on "readline"
