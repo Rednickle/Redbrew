fd7c9796ca72ffc58e540047a439c3ce62811889
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-07 19:48:14 +0100

    jdupes: update 1.13.2 bottle.
    Closes #14668.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -9,6 +9,7 @@ class Jdupes < Formula
     sha256 "5914122e74633a5d22c16102e0ffbf43c6a5d5903ecc75d41b050339af913b96" => :mojave
     sha256 "f2132c6e2a0a1402a0dcc0205efc2ae80fcc1e70508f1910db3113de0d64ce1d" => :high_sierra
     sha256 "95ce9083f5ee6dabdaae7ac9165b51231aa8df5240452f3eaec9c172064082cc" => :sierra
+    sha256 "a4c8a9b749654706a4cc75d261d66e23084918a862697908c670d78b682bf7dd" => :x86_64_linux
   end
 
   def install
