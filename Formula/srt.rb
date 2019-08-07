f7d3986504e9409c2fbdf421bdc9a6085e0e57a4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-07 19:54:33 +0100

    srt: update 1.3.3 bottle.
    Closes #14678.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/srt.rb
+++ Formula/srt.rb
@@ -10,6 +10,7 @@ class Srt < Formula
     sha256 "27ca171a4c95ce4048fb55ede0ebefa98de2922ae843f2d592c02e07e18e5234" => :mojave
     sha256 "7b292fdf1a31d7b8a11abacb6bb03deed8b5729971daca91b20b7e03011d4e4a" => :high_sierra
     sha256 "ac561d9e33e57ef6dcd5ba37261f4ba8a94934eecff1e6598519e43464811c00" => :sierra
+    sha256 "176644c322323ff0988ca3bb06c38f4dedc843a363a0677b59f3415d4eeeb917" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
