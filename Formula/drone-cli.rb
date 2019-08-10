72995b21c7e913744869196c8cb91568db339e66
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-10 16:07:22 +0000

    drone-cli: update 1.1.4 bottle.
    Closes #14732.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/drone-cli.rb
+++ Formula/drone-cli.rb
@@ -10,6 +10,7 @@ class DroneCli < Formula
     sha256 "93e10a2deb01cfffbcb2e1912c9f3ae5acad176ffef865452b9cb4ec14d08dd5" => :mojave
     sha256 "57310b464cdb63d6eb63d522b743ed339b10308d4fc72ffe758e6d987bd6af50" => :high_sierra
     sha256 "9aa164f7eaf7867ecf1f40a69379028f730c2bce606cb33e2e9975145a610a80" => :sierra
+    sha256 "8be8502224749d25e5527796a56817f2a1b5edbb410047269680eff8aa1af49d" => :x86_64_linux
   end
 
   depends_on "dep" => :build
