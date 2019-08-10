bd831a6cf1e4dcb54a1b8b8625604278d243ba62
Michka Popoff <michkapopoff@gmail.com>
2019-08-10 16:18:07 +0000

    twemcache: update 2.6.3_1 bottle.
    Closes #14727.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/twemcache.rb
+++ Formula/twemcache.rb
@@ -11,6 +11,7 @@ class Twemcache < Formula
     sha256 "2c7fd2ce03cc16859264882f478137de35ece42a26ad9b10f23d668ddc1883d4" => :mojave
     sha256 "9cc173642f9e53b723321a3013f2327b8a712c528c53ac5bd9fd2b9420244fcb" => :high_sierra
     sha256 "ec7e5d41f887db3a41d89eadb64d16119a2d86427afd45de92e7a8ca55ce7ef2" => :sierra
+    sha256 "8af52d72baa82aa688966c52259d2e1f767461bd832e97840aa1bfb42b63113e" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
