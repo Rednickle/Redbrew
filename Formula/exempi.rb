5ec9fe46405a6e46c6fdf063c7951319796837a5
Michka Popoff <michkapopoff@gmail.com>
2019-08-10 19:41:22 +0000

    exempi: update 2.5.1 bottle.
    Closes #14740.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/exempi.rb
+++ Formula/exempi.rb
@@ -9,6 +9,7 @@ class Exempi < Formula
     sha256 "7ae283f3e160cf837544567cd015c8c2e7d68499584405bc1509916639f72c21" => :mojave
     sha256 "b7f6fc2e81ca747e6ed8c899bc8353cbba0006959834bf7bd495eb482fd75658" => :high_sierra
     sha256 "0b9773e6206ec4be656de18f2cb8e13e32dc892745867f048f9524d87381efab" => :sierra
+    sha256 "a37bbd928f0974e3144f6f4815caadfee948fe39b2823472b8beb62b068eda34" => :x86_64_linux
   end
 
   depends_on "boost"
