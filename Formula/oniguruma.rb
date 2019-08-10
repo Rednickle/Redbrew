5219c258e31dc011316b9d4cb278f766e03a23fa
Michka Popoff <michkapopoff@gmail.com>
2019-08-10 18:56:23 +0000

    oniguruma: update 6.9.3 bottle.
    Closes #14746.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/oniguruma.rb
+++ Formula/oniguruma.rb
@@ -9,6 +9,7 @@ class Oniguruma < Formula
     sha256 "f00f8c6f8afd8875fed685a9190cb0c5e9b5ceef58ef1e489fb17a42bddc9672" => :mojave
     sha256 "42f48f03fc7030dcdcd15b920e863c107982cabfc063e3ccb60032e31b01562c" => :high_sierra
     sha256 "67a2105211f270ed618fdb3d29946ad89e2cb6e7bbb5cbb7dc7f48bc4e94e6db" => :sierra
+    sha256 "d7bb2aeb46d7a939650d1fb9165fd0e33b6954b02d9298087dd860d9838fad06" => :x86_64_linux
   end
 
   def install
