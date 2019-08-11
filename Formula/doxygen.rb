6c172243db8cea74b16369dd2e646882b2be2d2b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-11 14:30:52 +0000

    doxygen: update 1.8.16 bottle.
    Closes #14799.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/doxygen.rb
+++ Formula/doxygen.rb
@@ -11,6 +11,7 @@ class Doxygen < Formula
     sha256 "709545ed8f509c407d1a8ac2f36f396f783bd732c98ed18d79e57aa26e79fd74" => :mojave
     sha256 "882a5c055350590d2dfa31cbc786dab9760acfd222a05ecbabd7833cd09a66d9" => :high_sierra
     sha256 "5d002c6ee6d2619c5c5c9752c65537b81a135361f2c99f566a3e178de4f448f8" => :sierra
+    sha256 "9013ab089a62f4f9b62085dfed45ac52a547cad16a105bfb0710e3a89058bf69" => :x86_64_linux
   end
 
   depends_on "bison" => :build
