7e667163e917834277fc6dc9bfd8acbe37e6185e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-14 09:25:16 +0100

    topgrade: update 2.7.0 bottle.
    Closes #14828.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -9,6 +9,7 @@ class Topgrade < Formula
     sha256 "4b41e709726c8d23f965726d5cbce8ab064739204ca417f766318b73b8439e44" => :mojave
     sha256 "40a2495d231f4a0f03aaa910a6784ffdafcedeb7fe8288ccf54e46c8c7667f47" => :high_sierra
     sha256 "da6151b92139020e4219133fa9eed19361810b642761f9130b1ab72b825fcbb7" => :sierra
+    sha256 "23f615ec56f30561284a563faefb3d4582ef80a9c7b59f42d620381f2f57a7c3" => :x86_64_linux
   end
 
   depends_on "rust" => :build
