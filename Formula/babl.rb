4036e61f005b5cd3b7c619f61062700c288e283b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-18 11:38:18 +0000

    babl: update 0.1.68 bottle.

--- Formula/babl.rb
+++ Formula/babl.rb
@@ -11,6 +11,7 @@ class Babl < Formula
     sha256 "298f41a91ed4b93bbc29254f40691537ffd14bee7322d862b1b96f5996cfd865" => :mojave
     sha256 "0d8bf0f5aea427b7d9ca8b05cdf49b0a2bce4c3f41b9d148f42103e2454de667" => :high_sierra
     sha256 "3cd4d6d3cc86dc1bc2b8411bf4ffb911df58458d54fc938730fdede11587c624" => :sierra
+    sha256 "b69df5b48aefa985952bc253ed0e8c2228c829a6098262c4410eb14bbbecd869" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
