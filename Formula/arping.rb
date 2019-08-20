8b7c0d39a2da81adbbea00c4d9ed8ad6794a8af7
Issy Long <me@issyl0.co.uk>
2019-08-20 17:45:10 +0000

    arping: update 2.20 bottle.
    Closes #14949.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/arping.rb
+++ Formula/arping.rb
@@ -9,6 +9,7 @@ class Arping < Formula
     sha256 "15e267fd6609e686236668578452b0d2f3fc7771b24fb9ef1486fe9752f44645" => :mojave
     sha256 "f9d6ddf4906c516e80b25f7a28e8d00f917af69653dc9ecfcc36a0f4bd0b2f99" => :high_sierra
     sha256 "3170e9a0f53d76b1ae18ae15fa5945578044debb5798c1955bb8d9437b83088e" => :sierra
+    sha256 "b6eb052b61caa4e98c95d857d1ce39b06c5461b9b5d491527fda9c9f29010b67" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
