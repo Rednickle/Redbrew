4560c40555dccee9d3fb4d58c867fff7a26678f7
Michka Popoff <michkapopoff@gmail.com>
2019-08-23 19:13:46 +0000

    opa: update 0.13.3 bottle.
    Closes #14975.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/opa.rb
+++ Formula/opa.rb
@@ -9,6 +9,7 @@ class Opa < Formula
     sha256 "739a12287279ed4d5765b2573b33f383e64a264b36d8dfd784006c490d74f2f4" => :mojave
     sha256 "6807cc9a6df74306ef978716fa7f3929151c1aafb1e47c7d59267c44facb1089" => :high_sierra
     sha256 "42deabb98be39ced00d89dc6a32590c71cad4994fe39a720b4d56b2507d9b532" => :sierra
+    sha256 "00dc145cb83a6e6001dbfa68333d753ffe22255cf90fbdcc5227ced172c938d0" => :x86_64_linux
   end
 
   depends_on "go" => :build
