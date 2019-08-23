57d45b9280f9e4e221b55fb33f048ce3628c0f3b
Michka Popoff <michkapopoff@gmail.com>
2019-08-23 19:14:19 +0000

    nspr: update 4.22 bottle.
    Closes #14974.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nspr.rb
+++ Formula/nspr.rb
@@ -9,6 +9,7 @@ class Nspr < Formula
     sha256 "44b8d6ae65760da94e14b9bb742378c81b72821abd6ac4bb0be88c218234210a" => :mojave
     sha256 "5a466d443e42a227e6c6d972725558068a2121ee69a2807af993837281c0bcd1" => :high_sierra
     sha256 "f8e389b885ebef82929c0b6afc8b0ffbccbebb98f7267d81cad3e0d6d177f8c0" => :sierra
+    sha256 "7436c632b351e174b08a6828c958e5ff237d1829a4e56f733e1e79e7a668ecc3" => :x86_64_linux
   end
 
   def install
