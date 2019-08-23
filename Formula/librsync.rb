69dd4db34f642893f02aedf8d198826e1acf2a25
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-23 21:50:55 +0000

    librsync: update 2.1.0 bottle.

--- Formula/librsync.rb
+++ Formula/librsync.rb
@@ -9,6 +9,7 @@ class Librsync < Formula
     sha256 "addc0756351610330977decc0ae1b31c6f2928e527faef5d38230f623b88dc07" => :mojave
     sha256 "9e813729589ad923be1fd040cd54a5c5083a824c05f09f7f8a77fc529a9516ce" => :high_sierra
     sha256 "a4523e8193af9a30986f706d22d53b937d3ffc9c1bfa5fda05d54654a616a0ef" => :sierra
+    sha256 "f14d994d007af6c8217ba2ed616f89e82b1642dcc8ec2691a87d51e5de2e0179" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
