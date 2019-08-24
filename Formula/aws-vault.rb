322f0a28e8daea14e06a82bcd8c143866804f417
Issy Long <me@issyl0.co.uk>
2019-08-24 18:40:38 +0100

    aws-vault: Migrate from Linuxbrew/homebrew-extra tap
    - This was added to Linuxbrew/homebrew-extra, but that repo is less
      discoverable than this one, requires another `brew tap` command, and
      we're gradually deprecating the Linuxbrew organisation.
    - This is tagged with `linuxbrew` so that it's visibly Linux-only. On
      macOS, `aws-vault` is a Cask.
    
    Closes #15023.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

---
+++ Formula/aws-vault.rb
@@ -0,0 +1,30 @@
+class AwsVault < Formula
+  desc "Securely store and access AWS credentials in development environments"
+  homepage "https://github.com/99designs/aws-vault"
+  url "https://github.com/99designs/aws-vault/archive/v4.6.3.tar.gz"
+  sha256 "89428b99568d67335c5a9a4f74bdc7f4511864aa1dd11a07e7912588bcaa2716"
+  # tag "linuxbrew"
+
+  bottle do
+    cellar :any_skip_relocation
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOOS"] = "linux"
+    ENV["GOARCH"] = "amd64"
+
+    flags = "-X main.Version=#{version} -s -w"
+
+    system "go", "build", "-ldflags=#{flags}"
+    bin.install "aws-vault"
+
+    zsh_completion.install "completions/zsh/_aws-vault"
+    bash_completion.install "completions/bash/aws-vault"
+  end
+
+  test do
+    assert_match("aws-vault: error: required argument 'profile' not provided, try --help", shell_output("#{bin}/aws-vault login 2>&1", 1))
+  end
+end
