4d12d7a38c04cb5b3790e72844ce6a78df5e0055
Michka Popoff <michkapopoff@gmail.com>
2019-08-24 07:37:21 +0000

    golang-migrate: update 4.6.1 bottle.
    Closes #14994.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/golang-migrate.rb
+++ Formula/golang-migrate.rb
@@ -9,6 +9,7 @@ class GolangMigrate < Formula
     sha256 "8b44c220f57f18d8c36810b5564dfc00b249834fd2556d32fc4e418dbb4c0ca6" => :mojave
     sha256 "7b8c8206b4e0b1125c4fdcaa2b49c3dffb9aa1df79eb53be61ee49ebf5aed0fb" => :high_sierra
     sha256 "4735974f1742bffd40f8d7e541c41935f51dbf971b80c8e5d3eb4aef4a044d94" => :sierra
+    sha256 "111603e4fed9f3274f02f48b1bdadaaca1fffc7a85f2ea15359593b6edfb0ea0" => :x86_64_linux
   end
 
   depends_on "go" => :build
