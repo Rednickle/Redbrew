44296a08fb1b0827e15d5761dcab884ee013f14a
Michka Popoff <michkapopoff@gmail.com>
2019-08-27 21:17:31 +0200

    miniserve: Fix build for Linuxbrew
    Closes #15064.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/miniserve.rb
+++ Formula/miniserve.rb
@@ -5,6 +5,7 @@ class Miniserve < Formula
   sha256 "5b7c91bdf35e1a17ca006efa0354712301886c5c50952a2162401aef77faced0"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "48938437585dac734e14af73c235da6a6c16d96186cfbe9be8c18d40c11d76d9" => :mojave
     sha256 "23fe1a66372f48f3db122f7cb5a6cde3201da294ee75d87dc887c6c9e1803a33" => :high_sierra
     sha256 "b21ba4b456e9f97d37c6fe667a6fdfb6d40c6b345c8f2a9baf0f85eb9cc39d98" => :sierra
@@ -14,8 +15,13 @@ class Miniserve < Formula
 
   # Miniserve requires a known-good Rust nightly release to use.
   resource "rust-nightly" do
-    url "https://static.rust-lang.org/dist/2019-08-24/rust-nightly-x86_64-apple-darwin.tar.xz"
-    sha256 "104ddea51b758f4962960097e9e0f3cabf2c671ec3148bc745344431bb93605d"
+    if OS.mac?
+      url "https://static.rust-lang.org/dist/2019-08-24/rust-nightly-x86_64-apple-darwin.tar.xz"
+      sha256 "104ddea51b758f4962960097e9e0f3cabf2c671ec3148bc745344431bb93605d"
+    else
+      url "https://static.rust-lang.org/dist/2019-08-24/rust-nightly-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "85d498de8d2683615548cb27e507fbf7f51451a54498b024b2031eb230869f88"
+    end
   end
 
   def install
