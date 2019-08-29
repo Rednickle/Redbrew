89da3a534d8bf6e268921e842bdc63cca7022d9d
Michka Popoff <michkapopoff@gmail.com>
2019-08-29 21:04:50 +0000

    http_load: update 20160309_2 bottle.
    Closes #15134.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/http_load.rb
+++ Formula/http_load.rb
@@ -11,6 +11,7 @@ class HttpLoad < Formula
     sha256 "d0d672723564b758fc3ef0721239e108ec063a395e183db033071200d5d9ee48" => :mojave
     sha256 "22e21275c49121c174024104f9b99c5f55d37e032ff7cae42bba89746c26bd88" => :high_sierra
     sha256 "a949ed2040faf49c7cdb6bf0110dfbbff465641c811e78a035998a4160170a05" => :sierra
+    sha256 "30aacd4e7c9db8332f96f490a653b0955aaeaddfe35fcde7aa1218187c044803" => :x86_64_linux
   end
 
   depends_on "openssl@1.1"
