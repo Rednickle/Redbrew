7714fd6b9ccedc6366a5c6f4d3e60b2f4910cc11
Michka Popoff <michkapopoff@gmail.com>
2019-08-29 20:38:20 +0000

    w3m: update 0.5.3_6 bottle.
    Closes #15121.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/w3m.rb
+++ Formula/w3m.rb
@@ -23,6 +23,7 @@ class W3m < Formula
     sha256 "c2a4f7208e98f575eadaff6af3dc9a93305008b93d2f069c53d687ba61b85d64" => :mojave
     sha256 "bc46bb9b70d7149058d2c757aa0b8ea68c7c6836faee26da0b697d81cca0927d" => :high_sierra
     sha256 "809a34cb2c14b98827cfe9f18008b0ebc545e359c5f8c1279e71948ac336bdd1" => :sierra
+    sha256 "120e4e608f97058318ca2f8ebff4393fd928ef3efdcca2ff4c4008569012c93c" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
