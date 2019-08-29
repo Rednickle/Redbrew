d36e571035486bc6c1446dd18533ae96dee0b243
Michka Popoff <michkapopoff@gmail.com>
2019-08-29 21:32:53 +0000

    dcmtk: update 3.6.4_2 bottle.
    Closes #15115.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dcmtk.rb
+++ Formula/dcmtk.rb
@@ -10,6 +10,7 @@ class Dcmtk < Formula
     sha256 "d7c5cbc32fdd0c44228884512eb0cf8068d6b169165a822f6c57e61cb7d40bce" => :mojave
     sha256 "c8be648beb4178829963b0029153f6d0ab1be921a9cd472760e661b98f17d94e" => :high_sierra
     sha256 "292276fd0f5f5a8c0782e6e5fc0d35895600c9835ff634f7b16e6b92e534f72b" => :sierra
+    sha256 "d5604bd8f18efeb994d08c0c04f7eac31c54623907f86b601a009297036589ef" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
