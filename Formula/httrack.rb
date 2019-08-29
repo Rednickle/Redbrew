eb7594b47fe06d34a64b7f8c76d3bdcb42a58cb5
Michka Popoff <michkapopoff@gmail.com>
2019-08-29 21:09:49 +0000

    httrack: update 3.49.2_1 bottle.
    Closes #15135.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/httrack.rb
+++ Formula/httrack.rb
@@ -11,6 +11,7 @@ class Httrack < Formula
     sha256 "6e0d2265e15d103a37b6b594f7f10c85af82012f1e3c1e25fc436e7430502b2c" => :mojave
     sha256 "612d8c3f9ee15fd7c4f42dbca3c5e3b58e968d626aa15f916f85c8cdb44ea31f" => :high_sierra
     sha256 "842d48bdb72573623a478a97a2c2abcafe34fb4b0443229216e35d30552dd27f" => :sierra
+    sha256 "5f71e5ffdd78774959dcafa0c8eeaebc8411663c8d1ce02fa8596b2747205f46" => :x86_64_linux
   end
 
   depends_on "openssl@1.1"
