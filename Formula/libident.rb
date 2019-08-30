8d85f267a99400d7463b334e963b7e0926216ad0
Issy Long <me@issyl0.co.uk>
2019-08-30 21:48:04 +0000

    libident: update 0.32 bottle.
    Closes #15167.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libident.rb
+++ Formula/libident.rb
@@ -13,6 +13,7 @@ class Libident < Formula
     sha256 "6236d3b4ee424795859cc64da30997ff67f7ac5bd42702e8eabe10f99339ca48" => :el_capitan
     sha256 "53db8e889d8efa34b4a3b6a145bcec2bcb53595e7db0cfdd55c8d857dff3a442" => :yosemite
     sha256 "d8e20cc9d2015b83785d4eb05110afc082a6389267df82546cc89e693dd2db6a" => :mavericks
+    sha256 "edb435b6f2b0049b9ed2bfb72471cd78471797f1b0de93d566a1e30a8aa40e68" => :x86_64_linux
   end
 
   def install
