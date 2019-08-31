beefd24a2caab3e7d996a5f2600243ec6e20a771
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-31 07:51:48 +0000

    ldns: update 1.7.1_1 bottle.
    Closes #15183.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ldns.rb
+++ Formula/ldns.rb
@@ -10,6 +10,7 @@ class Ldns < Formula
     sha256 "4658a9bdae49915184e10be212a19f11504ca731e66ad8a118a6cb80838a555b" => :mojave
     sha256 "2f2ee1ff4f7c6513b9c59fcee2d36b44e0421d238cbecbd6c6ed8f5801ba1803" => :high_sierra
     sha256 "e4fe44eec5ae7b987b264cdbb64a02a7e53a25d1203edc44cb820cd7cc93093c" => :sierra
+    sha256 "6b89f084cd0662179238268ba05309685f0f78708a2636eedbf6a3dd3911b2c8" => :x86_64_linux
   end
 
   depends_on "swig" => :build
