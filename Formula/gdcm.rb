f7f81a254345ab963ecc11b5867fefdcdc3c21b9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-31 14:00:37 +0000

    gdcm: update 3.0.1 bottle.
    Closes #13860.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gdcm.rb
+++ Formula/gdcm.rb
@@ -8,6 +8,7 @@ class Gdcm < Formula
     sha256 "02acc5b9d928de052b0a1ea8ce06c0184ad573d86797694f408d83c185d43624" => :mojave
     sha256 "fce24222b4aa0414b4acaee2622ecb2005bd048bc8d95e11a6e1a0a46f0252b6" => :high_sierra
     sha256 "05b046743c582b5bb7b604538e82e2c7a1c33caee2938c5a449716c404d1659a" => :sierra
+    sha256 "23803e288b8153d0ccab9c43f87b82844cf137455f2a607c5f6a6df0dc515f6c" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
