61577fa0ec9bb405f7372cd8c09834e3bf04bdc1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-01 16:13:39 +0100

    cgrep: update 6.6.30 bottle.
    Closes #15245.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/cgrep.rb
+++ Formula/cgrep.rb
@@ -13,6 +13,7 @@ class Cgrep < Formula
     sha256 "c48bd1796c39325751ad3b5526c3d0a1c657c1611c7c5178a1571b4d2f2c68a0" => :mojave
     sha256 "b5d1fe2cf3084d6639216f9073b079f415339599d41a8784b8018fb425828033" => :high_sierra
     sha256 "602d854d141b6a449d27afa47b6de20f917d6b361eac9a43c5937c2b5a548d47" => :sierra
+    sha256 "49885f23ef707b93617597937224c8e1b44b7647a2b699d08a6d049af215352a" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
