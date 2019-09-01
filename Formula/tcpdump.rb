d6a2e5ccb6bd56a96486bea3a3f4f5e1bfcfaff3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-01 16:09:28 +0100

    tcpdump: update 4.9.2_2 bottle.
    Closes #15240.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/tcpdump.rb
+++ Formula/tcpdump.rb
@@ -11,6 +11,7 @@ class Tcpdump < Formula
     sha256 "3c598caf02f9ca31d2892d9586a35c70400ca2a7895758b72e4672e83b646457" => :mojave
     sha256 "2fc5873438a618ba6140b37ba4e41f7f318b95e0608f17620104146843d99fbf" => :high_sierra
     sha256 "7b57d5f603ed82fb98c58198660596c7418af6b567c964d6873d23eb6629140b" => :sierra
+    sha256 "8a69c859d38c9eff5858d0f7ea44b3771f90980e87229aaceb6311d1f97f2c1f" => :x86_64_linux
   end
 
   depends_on "libpcap"
