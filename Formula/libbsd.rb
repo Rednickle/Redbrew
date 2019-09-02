67c87853e97788393e15a2410615f5dc1ee80357
Thomas Blaschke <tblaschke@users.noreply.github.com>
2019-09-02 15:05:17 +0200

    libbsd 0.10.0
    Closes #15259.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/libbsd.rb
+++ Formula/libbsd.rb
@@ -1,14 +1,12 @@
 class Libbsd < Formula
   desc "Utility functions from BSD systems"
   homepage "https://libbsd.freedesktop.org/"
-  url "https://libbsd.freedesktop.org/releases/libbsd-0.9.1.tar.xz"
-  sha256 "56d835742327d69faccd16955a60b6dcf30684a8da518c4eca0ac713b9e0a7a4"
+  url "https://libbsd.freedesktop.org/releases/libbsd-0.10.0.tar.xz"
+  sha256 "34b8adc726883d0e85b3118fa13605e179a62b31ba51f676136ecb2d0bc1a887"
   # tag "linuxbrew"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0efc964be0372ef6082dce1b54a2ab515b7a2a62279a177c476d925da023c0d1" => :x86_64_linux # glibc 2.19
-    sha256 "f10cb5aa72f4b880447ae2eb88ac5b83985bd82f6e1a9c286b23b338acd9a507" => :x86_64_linux
   end
 
   def install
