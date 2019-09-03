8c91e83d632d12b664fe1359c346f9205ecd54dd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-03 19:00:19 +0000

    gengetopt: update 2.23 bottle.

--- Formula/gengetopt.rb
+++ Formula/gengetopt.rb
@@ -12,6 +12,7 @@ class Gengetopt < Formula
     sha256 "2ae5eeef439a6abc4d1f65965e1bafa9ac5ad0620cb4ef5e9444a4b2dbef1872" => :mojave
     sha256 "00f2578e7697c01d060a422e1be0ce8f4c6d23b365967ff7b5501d5cd6306dd1" => :high_sierra
     sha256 "57acd0ca20988a1b4f0f16383edb985549597b8a5266316e3a314b7775bab3c0" => :sierra
+    sha256 "23b4bfb4be143ee387849515276205a351fd8ef55c4b083d26d69942e6a2355d" => :x86_64_linux
   end
 
   def install
