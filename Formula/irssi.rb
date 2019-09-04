ecd2028392e18ba66de032afd6dce0ac640f12ef
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-04 19:50:38 +0000

    irssi: update 1.2.2 bottle.
    Closes #15310.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/irssi.rb
+++ Formula/irssi.rb
@@ -8,6 +8,7 @@ class Irssi < Formula
     sha256 "c8dda1f1cd62fc666406f3db68a9a34cd16ce4acce1082db45d7b575af5860be" => :mojave
     sha256 "de978c2c68c4254d5fea004006201743cfcf686c76a56cba62ab72a7bf0d9936" => :high_sierra
     sha256 "b0ea780071ba74669e4f31db648ca4b5de95d3372cb0fc73d41a90e2b6c6947a" => :sierra
+    sha256 "66fb4b37827acc7dbf385eca34c8c4730886628c4d6610aa84dde5803318eb38" => :x86_64_linux
   end
 
   head do
