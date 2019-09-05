49c0d2950c6d7588c5642c425b599b4043b08dd5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-05 20:39:07 +0000

    amazon-ecs-cli: update 1.16.0 bottle.
    Closes #15335.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -10,6 +10,7 @@ class AmazonEcsCli < Formula
     sha256 "c62e3319637879d76bf2d9e5702349e64c10f5e1a7d31ac3528a4022952a6a31" => :mojave
     sha256 "8d02711af8d7ff1a3bcc33c2836e2c9664865e4313f8a6fcc1ac2d55b5cc9714" => :high_sierra
     sha256 "c2b8ac3320972e73fd07ebf9821f28b854141a6234bfd094ed1fe8a13991c51f" => :sierra
+    sha256 "9a5dd72c8c3e310e5e6599febe95f9f10610a4bef1aee6ee78b593ae5f9157d8" => :x86_64_linux
   end
 
   depends_on "go" => :build
