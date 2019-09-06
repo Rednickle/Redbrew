2c57aa23b24d32f0a114bc56e48f5f6959430683
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-06 10:25:11 +0000

    cmake: update 3.15.3 bottle.
    Closes #15351.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -10,6 +10,7 @@ class Cmake < Formula
     sha256 "df769c8be61785860e5c60870d522c0504eeb08b229a0d35800c6594038a9485" => :mojave
     sha256 "2e0ab82d0d7dcfb7fb06b8d47582edbd39224c4e0d0f95fda085edad39b56771" => :high_sierra
     sha256 "b723f970d0b6d44a673340525c157c2e52c42e9ea352a5c25fc5e1a84b9865a8" => :sierra
+    sha256 "7246fd8e031dfadb8a1324460a5e4e54c041f52e04e93facfd4eb7bccb13573d" => :x86_64_linux
   end
 
   depends_on "sphinx-doc" => :build
