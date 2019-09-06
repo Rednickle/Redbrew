156ab12a47a2c102e81498ea9eec190ae21197fd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-06 10:23:53 +0000

    convox: update 20190905141723 bottle.
    Closes #15352.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/convox.rb
+++ Formula/convox.rb
@@ -9,6 +9,7 @@ class Convox < Formula
     sha256 "e9b0d97a54e2d619a879426cdb4811ecd14da62608765edec9468d3cbcb3068e" => :mojave
     sha256 "4dfacf3fc7ba14bb7d196f0422acb723b6b0f441240a376c7fc8456ed7415b00" => :high_sierra
     sha256 "e9fecd043541f924bb4ef659b447af1799e157447cabbc0c27c4f155a9bcdb8b" => :sierra
+    sha256 "bf0025a6b8ca77b4f3d9b4314bc0889b430b482f147da602f8a6bbabb90ab022" => :x86_64_linux
   end
 
   depends_on "go" => :build
