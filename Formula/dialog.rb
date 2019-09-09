d1787099276629de42e29fa0b816288f7b40d8b4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-09 21:06:01 +0000

    dialog: update 1.3-20190808 bottle.
    Closes #15398.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dialog.rb
+++ Formula/dialog.rb
@@ -9,6 +9,7 @@ class Dialog < Formula
     sha256 "47d5c4ac5913ff36ec7b8d4aa2d4fa2206ba4408dbdf5a24d7faac324e4264f7" => :mojave
     sha256 "d47c369a985ab8761cbdb3ceda72ed9d059557b206252f36b31830c1dab8f462" => :high_sierra
     sha256 "20fc5cc9a9ea20a873d3143cd629ebff9cac22fd14fef6f6f46c6bb0febeadf9" => :sierra
+    sha256 "481555b32e99d0e2d217ffb1b8b362e4998a42b5eb053c7c5862e967b0f9a852" => :x86_64_linux
   end
 
   uses_from_macos "ncurses"
