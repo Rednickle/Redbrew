bf1667acc87302900a2e37f861e83655f1a09e82
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-09 21:46:20 +0000

    cgal: update 4.14_2 bottle.

--- Formula/cgal.rb
+++ Formula/cgal.rb
@@ -10,6 +10,7 @@ class Cgal < Formula
     sha256 "b565fe6648ad045b90c9be41c536e77fcedccc3680062a29940181df78a061f2" => :mojave
     sha256 "a8349e42e0d2882724631abd62f95ae84b028ca2354e37f83dde2bbdcb64c1f1" => :high_sierra
     sha256 "0695534a43bc5be0234d0d4184dccadd79197ad442a2e72773a7bf4b08e0376f" => :sierra
+    sha256 "f7a9753fb27d3bf1aeb1c1fd616cbd6120495cf8249965da8bcaaac77a8313ea" => :x86_64_linux
   end
 
   depends_on "cmake" => [:build, :test]
