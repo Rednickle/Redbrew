38737059a02971251ebd2d6a74875e8ab49b3af6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-10 21:01:26 +0000

    icarus-verilog: update 10.3 bottle.
    Closes #15438.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/icarus-verilog.rb
+++ Formula/icarus-verilog.rb
@@ -9,6 +9,7 @@ class IcarusVerilog < Formula
     sha256 "0237851e478bcb76567111f14c1e42fe79161a8cd28ca04127295fc40db14113" => :mojave
     sha256 "96a15af23212d29f9410e073418c9388447955245fa8c38cf3b27ccf8fabd178" => :high_sierra
     sha256 "ded40d14a1cd74f2b764d9cf667d48ee8b6c010e77d88ca47afc99188ace1255" => :sierra
+    sha256 "7454e7de194a151cf86970f7ec401344eb023c5314221ebaabe728048a51ee79" => :x86_64_linux
   end
 
   head do
