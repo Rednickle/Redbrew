16dfb080f7e87fe9416b4f6985c936fcc379e85b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-10 18:26:52 +0000

    libsigc++@2: update 2.10.2 bottle.

--- Formula/libsigc++@2.rb
+++ Formula/libsigc++@2.rb
@@ -9,6 +9,7 @@ class LibsigcxxAT2 < Formula
     sha256 "cfb591da46f83d5242f860533bb509e4966b32f45d96abd1aebe60ccce536de6" => :mojave
     sha256 "81130d69dd341e31f941f9d457745bbefb4eb59427a81c014e5cf6c1c8b861ad" => :high_sierra
     sha256 "d9c71c15b7d4d244aa74aa319a14f0bba110ca97160abe035a461409b2b1630b" => :sierra
+    sha256 "2a2174c441697c5e3b2a14413e615e9b45eae11add6da2c157b4766f6bad3963" => :x86_64_linux
   end
 
   depends_on "m4" => :build unless OS.mac?
