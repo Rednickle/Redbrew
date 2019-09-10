afdbd73f36df8f5a106cad0b8efce045251097c0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-10 18:10:30 +0000

    glib-networking: update 2.62.0 bottle.
    Closes #15417.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/glib-networking.rb
+++ Formula/glib-networking.rb
@@ -8,6 +8,7 @@ class GlibNetworking < Formula
     sha256 "ef976dbe9ac0965d8045453e4b97e068fc7aef97efebba6c0216a838d8bac600" => :mojave
     sha256 "efcce145579cfa2278fc9bea5664bab854cf568d1322bb1020c47daa294ce124" => :high_sierra
     sha256 "ac56e467375ea4cf7f3abc94dd5f3f32fb369a1225438a2c08722aaf2131b7ca" => :sierra
+    sha256 "db6abf0c6ac3390019314dccedd1e12fb2e105f1a9a83f8d10507a37af785b74" => :x86_64_linux
   end
 
   depends_on "meson" => :build
