88c6c53e0fe2355b500a36a2192dd24b258af31e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-11 20:43:38 +0000

    mutt: update 1.12.1_2 bottle.
    Closes #15522.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mutt.rb
+++ Formula/mutt.rb
@@ -18,6 +18,7 @@ class Mutt < Formula
     sha256 "60e75a481a2b25d50d0c404a14233e79579ee61e72cb14769c0a021dabf81428" => :mojave
     sha256 "7d51106017603dec20a15e483b8aca629b3d4d34e3aa92094842c2b8f8ef83d2" => :high_sierra
     sha256 "ae8f34c0378d4e1f40f31bbe841f4ee89fe26774305ad1c4a42d090b8b9f11ec" => :sierra
+    sha256 "f1750793a71b5c8e265134b62057e3428cef16d062dfb5f4a50c7d19dc2cf42f" => :x86_64_linux
   end
 
   head do
