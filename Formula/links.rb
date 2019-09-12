4812c167ffdf9edcfc55bbb6d1ebbdfa66b6f83a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-12 21:16:56 +0000

    links: update 2.20.1 bottle.
    Closes #15553.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/links.rb
+++ Formula/links.rb
@@ -9,6 +9,7 @@ class Links < Formula
     sha256 "66ed3179fd7e8e439d3c19b5e11a9903a16ac994baef6aa3b6595e697ebce14d" => :mojave
     sha256 "89b10181d05992aead0afb46a818c9fa3c1218b215146efe864574c877c84545" => :high_sierra
     sha256 "aa2902faaaff12df4013e05c7cd85ff09be788378729091fc0cf140a18c060c2" => :sierra
+    sha256 "b039f30c1e0400edc2ca877dbf873dd78330fd3c866e13d83017d91922436a9d" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
