ef5252600352745fe5823133e403434f38363837
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-13 17:39:02 +0000

    mosquitto: update 1.6.4_2 bottle.
    Closes #15494.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mosquitto.rb
+++ Formula/mosquitto.rb
@@ -10,6 +10,7 @@ class Mosquitto < Formula
     sha256 "a9fe32d552244cb10c2fa09302d5f5e3b749a93dac2012ea8729cc5d5b9a77ef" => :mojave
     sha256 "11a11f22a00c2c940c017af3be16b02c8bb007c25aa05b6ec6a08c8f9e1398c6" => :high_sierra
     sha256 "9ad9ae666ce4f6a5409b6c002ec0b17640a6e3985f3718d5fd8fd40a330d7d71" => :sierra
+    sha256 "6bb8829ad02393b60a6136f9d38cd25f5fe9b1a5ffd8b99f8e46ec72e0c26045" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
