928593bbbb142e152d348b94d87368a912175033
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-13 20:43:03 +0000

    typescript: update 3.6.3 bottle.
    Closes #15584.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -12,6 +12,7 @@ class Typescript < Formula
     sha256 "3bd0896b1c6a423c2f502932599931c7c2f9348bcb27856fa932ab56c0028be4" => :mojave
     sha256 "1049bf48f6cc02358a3a4ec2e1187f30ed7d8d5bc4a53b388661f7f205027df1" => :high_sierra
     sha256 "bffc050131abccb5dedd986667e4dcc263e736699c03f953afb52f1ec83174d5" => :sierra
+    sha256 "ff40a55890c60fd973d454570c298524d06304c1a2d4d472fc8b9d2f7e733c7c" => :x86_64_linux
   end
 
   depends_on "node"
