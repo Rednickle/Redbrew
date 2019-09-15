88e04989e13e2290b604c9b904d965e69584f762
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-15 14:59:26 +0100

    git-ftp: update 1.5.2 bottle.
    Closes #15612.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/git-ftp.rb
+++ Formula/git-ftp.rb
@@ -11,6 +11,7 @@ class GitFtp < Formula
     sha256 "18ee331150260d506bd251040a44e193b2ba465f0c0309432804d800f029e1c1" => :mojave
     sha256 "4774f0b5928275dad5d77e0374829fdf93b7c56e4a7cf8ea9bb44a750ff8e9cd" => :high_sierra
     sha256 "a1fab3f3590f66260a76fc494d4688d9e01be9d29e9c01cbf136aec176406c6a" => :sierra
+    sha256 "2ab0f3faf7dbd14fe4d57289d05af997280369a2f89f422f83105ec96439d234" => :x86_64_linux
   end
 
   depends_on "pandoc" => :build
