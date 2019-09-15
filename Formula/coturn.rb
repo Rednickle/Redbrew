e69c709e1060a6c917f98a40a33f239f67d13978
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-15 14:58:55 +0100

    coturn: update 4.5.1.1_1 bottle.
    Closes #15611.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/coturn.rb
+++ Formula/coturn.rb
@@ -10,6 +10,7 @@ class Coturn < Formula
     sha256 "3e61ac696114f38c9d6d361b1915b7a1fdae7bedf82c9bcc6bc9ed9a3dfdb986" => :mojave
     sha256 "3896e563b9c95d367303922cb19675f13e574d2bb99bad23cc098975d709a296" => :high_sierra
     sha256 "a39734b4c588d1d311ca1e6851ba7cefb0b0159c5932ad2a5440b0e82025e8f6" => :sierra
+    sha256 "be87ab884564cc702158e217935ea6bb6f8cf9cff06c807b97940d09ee81644d" => :x86_64_linux
   end
 
   depends_on "libevent"
