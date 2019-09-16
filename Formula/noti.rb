a7c0810e4a7da93ae866ad9e05ef328d05b95272
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-16 21:30:36 +0000

    noti: update 3.3.0 bottle.
    Closes #15687.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/noti.rb
+++ Formula/noti.rb
@@ -10,6 +10,7 @@ class Noti < Formula
     sha256 "483119c4bf2865ddc52208f33eeb3e8db9e4ff0e5901baf0e441d31492b2a5ff" => :mojave
     sha256 "44d31ac34449acb98d99c65403c6515f4f66f88cb0f0e9f4bed41f31b47107d3" => :high_sierra
     sha256 "2684f0630a200aac31d9ef08f757b431a956534d40776d80599b1a02d80d6ec8" => :sierra
+    sha256 "3212df3dcb1179c2bbc2176967d5c7029a3d29a93cc4a7127172770f6317c278" => :x86_64_linux
   end
 
   depends_on "go" => :build
