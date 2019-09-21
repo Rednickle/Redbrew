8004031690c05209edd22ba945e0c2c2a3d4d512
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-21 17:39:54 +0100

    logtalk: update 3.30.0 bottle.
    Closes #15718.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/logtalk.rb
+++ Formula/logtalk.rb
@@ -10,6 +10,7 @@ class Logtalk < Formula
     sha256 "a9ad7e87ed012430c2e32425ee501f319487fcde39daa2fdf5ff1730a616841a" => :mojave
     sha256 "e4ee20e909fd09b0cb5b85db0c35471fe669f6e8273a50411314807ab88afdd4" => :high_sierra
     sha256 "2e4427725333531dce3961ff743d8851a3be3e3ea1d196f8d417a2fba85f549c" => :sierra
+    sha256 "76e017db02fbb306eb5618fa39ce373942da9fb03f2b1c0529c3b096465c3557" => :x86_64_linux
   end
 
   depends_on "gnu-prolog"
