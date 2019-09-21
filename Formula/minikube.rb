7748b5eaa81b9077cf99dffd75d3e224bc2112ef
Issy Long <me@issyl0.co.uk>
2019-09-21 23:18:04 +0100

    minikube 1.4.0
    Closes #15734.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -1,13 +1,12 @@
 class Minikube < Formula
   desc "Run a Kubernetes cluster locally"
   homepage "https://github.com/kubernetes/minikube"
-  url "https://github.com/kubernetes/minikube/archive/v1.3.1.tar.gz"
-  sha256 "7aa57e5896852c499f1687fbc424abf93645e1801fc9f8c2833e0affbb76eb41"
+  url "https://github.com/kubernetes/minikube/archive/v1.4.0.tar.gz"
+  sha256 "091f76e5f0c086261e617f8b1c63009a1732bd5d7b559810572ac473e6da0411"
   # tag "linuxbrew"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "b188166ec706dd72c1326a86ad2b2d285a15c13e5e25dc21ee106ba71b3ed5f0" => :x86_64_linux
   end
 
   depends_on "go" => :build
