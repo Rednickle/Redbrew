edd820028ff6e564608b4525bacd5b9b5412eebf
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-25 20:08:47 +0000

    cquery: update 20180718_2 bottle.

--- Formula/cquery.rb
+++ Formula/cquery.rb
@@ -12,6 +12,7 @@ class Cquery < Formula
     sha256 "e35aabf6a4b2b0ac21c59b6e6121aa3bc8cc623ca7fe1bd763dd6430d3339eae" => :mojave
     sha256 "ee46fd2b279edc3b52df5ca9b84bdce404bb888f8a7d31e092b3fda33285a02b" => :high_sierra
     sha256 "ac3ad5aada30ca31d70f36e6e3ba76d96aa0e235ccdd62259a086578fe182b44" => :sierra
+    sha256 "d40c1ad58cc6c437eb718cbd26204d101cc140c05784428ac5cc0f7d93661613" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
