b9c7ce40d116148f5ba0aa188abace08830f18ba
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-25 21:34:29 +0000

    chapel: update 1.20.0 bottle.

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -8,6 +8,7 @@ class Chapel < Formula
     sha256 "8fcaebe6a3c465a29a66e691581b88b2fc9960726e1f94b3f21aa0f53c424044" => :mojave
     sha256 "4aee5a0ddf8a44897a2f03c458a8e7e70d76b07f04024119ed482fbc06cf330c" => :high_sierra
     sha256 "34a5eac538de8fb6ac632109a0154e1d14ff8551bc8f4fec8df8359568697338" => :sierra
+    sha256 "145b8eae5a156308c11564cc0e9693836045a63fba02ec023f357aa4af3253fe" => :x86_64_linux
   end
 
   depends_on "python@2" unless OS.mac?
