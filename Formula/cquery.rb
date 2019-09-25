534339c636d16d2c26d30496ffdfc0da043b19f5
Michka Popoff <michkapopoff@gmail.com>
2019-09-25 21:40:09 +0200

    cquery: bump revision for llvm
    Closes #15771.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cquery.rb
+++ Formula/cquery.rb
@@ -6,13 +6,12 @@ class Cquery < Formula
       :tag      => "v20180718",
       :revision => "b523aa928acf8ffb3de6b22c79db7366a9672489"
   head "https://github.com/cquery-project/cquery.git"
-  revision 1 unless OS.mac?
+  revision 2 unless OS.mac?
 
   bottle do
     sha256 "e35aabf6a4b2b0ac21c59b6e6121aa3bc8cc623ca7fe1bd763dd6430d3339eae" => :mojave
     sha256 "ee46fd2b279edc3b52df5ca9b84bdce404bb888f8a7d31e092b3fda33285a02b" => :high_sierra
     sha256 "ac3ad5aada30ca31d70f36e6e3ba76d96aa0e235ccdd62259a086578fe182b44" => :sierra
-    sha256 "0cc13cc0439b0fefc4bdcd582cbdebebf88e9596c8a0f1b7991ba8bc2abc74a0" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
