cdd397dc891bfba7063f2583bd92bbee3cb7d50c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-29 23:57:25 +0200

    dhall-json: update 1.4.1 bottle.
    Closes #15757.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -13,6 +13,7 @@ class DhallJson < Formula
     sha256 "31afa844765b1abc4d50cd64426e8c0b5c35b5b5e912cf1a4170c1a1884f5545" => :mojave
     sha256 "49188890f01ff3ab6ff9d373f8a3e6c9346642f345fa63cdcee5409050eeea3d" => :high_sierra
     sha256 "f4f05b08e94e0d3df8b48885b69c9ce9678f84d9791af0e10ee6d12335ecb992" => :sierra
+    sha256 "ff38cfd5ab654a76839a1995ed54d8b39d7a3fbfa9ba7d0e7ab916351cdb05f2" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
