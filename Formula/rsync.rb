e0aace7880c30caeb120d7b77c23cab63db034e7
Michka Popoff <michkapopoff@gmail.com>
2019-09-29 21:48:06 +0200

    rsync: Build a bottle for Linuxbrew
    Closes #15802.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/rsync.rb
+++ Formula/rsync.rb
@@ -18,20 +18,24 @@ class Rsync < Formula
   depends_on "autoconf" => :build
 
   # hfs-compression.diff is marked by upstream as broken as of 3.1.3
-  patch do
-    url "https://download.samba.org/pub/rsync/src/rsync-patches-3.1.3.tar.gz"
-    mirror "https://www.mirrorservice.org/sites/rsync.samba.org/rsync-patches-3.1.3.tar.gz"
-    mirror "https://launchpad.net/rsync/main/3.1.2/+download/rsync-patches-3.1.3.tar.gz"
-    sha256 "0dc2848f20ca75c07a30c3237ccf8d61b61082ae7de94758a27dac350c99fb98"
-    apply "patches/fileflags.diff",
-          "patches/crtimes.diff"
-  end if OS.mac?
+  if OS.mac?
+    patch do
+      url "https://download.samba.org/pub/rsync/src/rsync-patches-3.1.3.tar.gz"
+      mirror "https://www.mirrorservice.org/sites/rsync.samba.org/rsync-patches-3.1.3.tar.gz"
+      mirror "https://launchpad.net/rsync/main/3.1.2/+download/rsync-patches-3.1.3.tar.gz"
+      sha256 "0dc2848f20ca75c07a30c3237ccf8d61b61082ae7de94758a27dac350c99fb98"
+      apply "patches/fileflags.diff",
+            "patches/crtimes.diff"
+    end
+  end
 
   # Fix "error: too few arguments to function call, expected 4, have 2"
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/344bf3b/rsync/fix-crtimes-patch-3.1.3.diff"
-    sha256 "1a3c9043e19b55290bd6a6bc480544fe79a155c2c7ed003de185521f7516ecc3"
-  end if OS.mac?
+  if OS.mac?
+    patch do
+      url "https://raw.githubusercontent.com/Homebrew/formula-patches/344bf3b/rsync/fix-crtimes-patch-3.1.3.diff"
+      sha256 "1a3c9043e19b55290bd6a6bc480544fe79a155c2c7ed003de185521f7516ecc3"
+    end
+  end
 
   def install
     system "./prepare-source"
