02f0a4cff199285ff1b006253526c4c977b9ef68
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-30 17:24:07 +0200

    fselect: update 0.6.6 bottle.
    Closes #15816.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/fselect.rb
+++ Formula/fselect.rb
@@ -9,6 +9,7 @@ class Fselect < Formula
     sha256 "06dcd013ff905155292afb6c59403f360d4e5f87ec0e4ae9f14b9a8f0a8c722a" => :catalina
     sha256 "2ae76542d41194210969879ef52d905dfda7f1c86e7e37c3406febcfd500c5f6" => :mojave
     sha256 "b2ef2faf383f377140ee889256267f17be1da42d2c63c469341d7892f0f66ed9" => :high_sierra
+    sha256 "5c0908e2e9330d3d4d7cc727f292949ef60eaff67f9356453b5fa58e6f827d6f" => :x86_64_linux
   end
 
   depends_on "rust" => :build
