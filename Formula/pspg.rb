82642f214283daa36a6dcd2d51542441b8fc8dbd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-02 23:06:34 +0200

    pspg: update 2.1.2 bottle.
    Closes #15896.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -10,6 +10,7 @@ class Pspg < Formula
     sha256 "3b81d00a608cbf4ff982a1d51f43d07c96bc0029e137a1156fcaeb9b2470d93b" => :catalina
     sha256 "688833633c283530500313a0359a934426c8af130be40e6d15f5a7c01216a54a" => :mojave
     sha256 "3fd5c0598c3ac0332388faef921c93caf8ad11968420b34a3f0f6dce4632f5d7" => :high_sierra
+    sha256 "4249196736d2d58c1b1d86429e537248128cb6b1e130ffa2e78b712ebdcb8d1c" => :x86_64_linux
   end
 
   depends_on "ncurses"
