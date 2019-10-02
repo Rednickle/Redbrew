24d027cf6c889e37b4b78d55402475b165175e17
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-02 21:44:27 +0200

    rancher-cli: update 2.3.0 bottle.
    Closes #15883.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/rancher-cli.rb
+++ Formula/rancher-cli.rb
@@ -9,6 +9,7 @@ class RancherCli < Formula
     sha256 "713193e1ef71643163efdf54278aff7c0d89ebfa885b879f047146aef8c715ea" => :catalina
     sha256 "a59a2809bbc13f9ed37b130be216671199e0f3b63b795b2c9cf4beca4a19f142" => :mojave
     sha256 "82678c514123aab93a04f90902f903bdcde087a1a6a1e83d7ecf7d0304a6a230" => :high_sierra
+    sha256 "8442602a4c2469e3654dd7544ef53e9f46904127b7e9a01d9cf0ef985a8328c2" => :x86_64_linux
   end
 
   depends_on "go" => :build
