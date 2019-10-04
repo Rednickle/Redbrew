58cd62514b93f7b97a80ec2931cef4cb4c165ba7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-04 16:35:24 +0000

    traefik@1: update 1.7.18 bottle.
    Closes #15932.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/traefik@1.rb
+++ Formula/traefik@1.rb
@@ -10,6 +10,7 @@ class TraefikAT1 < Formula
     sha256 "6376c59be73c3125759c180711b6c9f32b60ea0d00e5789ba9b78adac8556bfb" => :catalina
     sha256 "20b8bb0a61e6c8b6f36fed34165767e9603de96f8839ee6abce80bea399d7a24" => :mojave
     sha256 "239c83202e27788414dc4656930ba67c10162d7776415c961f09ec33e5ed8294" => :high_sierra
+    sha256 "c36e2c591f7b5cf6511a3c7a87fb50d1b47ed35a9d598935064c8e5e0be4e790" => :x86_64_linux
   end
 
   keg_only :versioned_formula
