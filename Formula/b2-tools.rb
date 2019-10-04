416e53fa55458200a0b2b5fe735b3905d2886505
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-04 21:39:01 +0000

    b2-tools: update 1.4.2 bottle.
    Closes #15954.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/b2-tools.rb
+++ Formula/b2-tools.rb
@@ -11,6 +11,7 @@ class B2Tools < Formula
     sha256 "e21e5a9daf848f8a36850cad7d18073c8b8af9db6bf2e6860549b6e7123e2b00" => :catalina
     sha256 "6d034a4b3ef822dd53b09a17ff0e924d6ad130cc6b376287425cc0300d425bc6" => :mojave
     sha256 "450cd0e09c64458f5af99414f523f56b93275c5efa6ed2c0a6a18420917d2a31" => :high_sierra
+    sha256 "120a884ce13454cb2111a12e043859b1c4c561152dce5c35066bf85b22dc5ad8" => :x86_64_linux
   end
 
   depends_on "python"
