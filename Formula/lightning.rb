9cbf7b656ccf4b1979727e7164e62af383d8d8de
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-05 06:58:32 +0000

    lightning: update 2.1.3 bottle.
    Closes #15973.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/lightning.rb
+++ Formula/lightning.rb
@@ -10,6 +10,7 @@ class Lightning < Formula
     sha256 "543bb685d72b8e9b10b14f3dcd615d38f8f499d10e1d27e40604240fc3f65ac3" => :catalina
     sha256 "c767959e901e6f47f9bbfe243e629508edbdb138376443d7943c4c4a5a52d4f2" => :mojave
     sha256 "da42166b5dd858cad42eeb7fc69a9ef870d23b67da6fa978d4bc58d3a464a0d4" => :high_sierra
+    sha256 "7c848b9a98637b222e8fa9bd4fa9a53855e43e62c3fc81819b7fbe46aefcad25" => :x86_64_linux
   end
 
   depends_on "binutils" => :build
