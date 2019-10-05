5a5d394e24f63a7392c9fde5c8b1ebc7e6e772b1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-05 06:58:01 +0000

    minizinc: update 2.3.2 bottle.
    Closes #15974.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/minizinc.rb
+++ Formula/minizinc.rb
@@ -10,6 +10,7 @@ class Minizinc < Formula
     sha256 "cdcacd5a4ca033867a061448461bc5a740c9f860e6adb16a6b9baa5c1a2f360c" => :catalina
     sha256 "4f879b71bc2a436e141b0391a95c9b59ad0f8b6547e3c22d54095af820cb9c01" => :mojave
     sha256 "2d5e0d69ea0d4622e665a80a2dfa76e8b01903d22bdce2362c6b58eea023c4b3" => :high_sierra
+    sha256 "18f4fa447309caf679cb147f62636522356e49be1d841f9345e874d1f1f540cd" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
