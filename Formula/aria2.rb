292902bf7764533506e5309e36ca78270d4a567c
Dawid Dziurla <dawidd0811@gmail.com>
2019-10-08 11:53:44 +0200

    aria2: remove cellar
    Closes #16058.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/aria2.rb
+++ Formula/aria2.rb
@@ -5,7 +5,6 @@ class Aria2 < Formula
   sha256 "1e2b7fd08d6af228856e51c07173cfcf987528f1ac97e04c5af4a47642617dfd"
 
   bottle do
-    cellar :any
     sha256 "9cc5e04be8b0a58d1f2b60b8abfc636168edbf23e7018003c40f1dd6952aab0c" => :catalina
     sha256 "761836ac608eb0a59d4a6f6065860c0e809ce454692e0937d9d0d89ad47f3ce4" => :mojave
     sha256 "70cc7566a23c283015368f92dfeaa0d119e53cfc7c1b2276a73ff9f6167b529d" => :high_sierra
