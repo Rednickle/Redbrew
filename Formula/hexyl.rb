10ff9d9889d025581218040fac67f1d84ed8a0af
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-09 21:26:52 +0200

    hexyl: update 0.6.0 bottle.
    Closes #16086.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/hexyl.rb
+++ Formula/hexyl.rb
@@ -9,6 +9,7 @@ class Hexyl < Formula
     sha256 "e7fe7b6f9430c7c6ffe0645ba7c5c921791723538272e102f82d3c69f62c4c01" => :catalina
     sha256 "b35036056ad68140fae0a7e0bed52112bee652edea69039527d4f2b0444680cb" => :mojave
     sha256 "e425d90bb0892027917cd36b34b78c08faa5fb4d82ade2eecf1df53181d76ed9" => :high_sierra
+    sha256 "6f862b9ae4d494126ca74388bf6fa6041c84f957948f1de5e058ba4abcbdeebf" => :x86_64_linux
   end
 
   depends_on "rust" => :build
