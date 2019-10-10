22be0c7da30867b816189b7d13ce52c10e138a86
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-10 17:21:34 +0200

    librdkafka: update 1.2.1 bottle.
    Closes #16100.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/librdkafka.rb
+++ Formula/librdkafka.rb
@@ -10,6 +10,7 @@ class Librdkafka < Formula
     sha256 "b250dd98bf323cd44c51cb6c0c662a459fb37c5d4c4f0e50390765e96867492a" => :catalina
     sha256 "4a998c5569421cbfba8f17c857f3b56d4f141380e86b01d76cd3e9194826e6dc" => :mojave
     sha256 "d3b92b192f7f6e47149afea2afde0aa80a6e1f9a85481fb5e8847172fa08aa14" => :high_sierra
+    sha256 "6690ea3a35c4ab5db154eabf0dbd959c6a9f1f6e28e906e2ae167b9d05eb34c8" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
