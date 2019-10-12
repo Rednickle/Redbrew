6ac71ab63a60b10492de0bb037ffe30cc89e82d1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-12 12:02:51 +0000

    skaffold: update 0.40.0 bottle.
    Closes #16195.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/skaffold.rb
+++ Formula/skaffold.rb
@@ -11,6 +11,7 @@ class Skaffold < Formula
     sha256 "1fa3b24593703367e10169828677c882e8c719a1143a818ebfcb1cdb0c663b6b" => :catalina
     sha256 "0c79db2008e2c4173e6ee910ac57d729c1dd03c61f42ad434d4247f5d256905b" => :mojave
     sha256 "dc8dd5401ffc9768c257e7b10b66fd866fbda5ca7c25db5d21a1eed2379c897d" => :high_sierra
+    sha256 "e5efa8945df7859a67b84a83dcc0ab077581c95b5cf71c48c34840440abfeea4" => :x86_64_linux
   end
 
   depends_on "go" => :build
