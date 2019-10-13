16f4e35d14873d80e2cfd9bb54ead8f0a6bf81e7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 16:08:52 +0000

    aptly: update 1.4.0 bottle.

--- Formula/aptly.rb
+++ Formula/aptly.rb
@@ -10,6 +10,7 @@ class Aptly < Formula
     sha256 "3a745c910d6d3bf19602b8802c27699405aef1bce208430d0b3c7ac10847575c" => :mojave
     sha256 "d4d197b4b9269cea1f0f86d6e7711e2547931de3fff413ce12e2817f72c11e42" => :high_sierra
     sha256 "301a22114bd639602d1ec97fba77a06d7952e123532fe4a35a347ca026a93994" => :sierra
+    sha256 "8057b70ce573e296868fc1cd6682bb70a0da3a98d298770f241f1d6c653953eb" => :x86_64_linux
   end
 
   depends_on "go" => :build
