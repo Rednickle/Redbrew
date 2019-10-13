8f2034bdddffb531287fb079679b0c15f8bac5bf
Dawid Dziurla <dawidd0811@gmail.com>
2019-10-13 10:33:50 +0200

    vifm: uses 'ncurses' from macos, build depend on groff and skip check on linux
    Closes #16218.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/vifm.rb
+++ Formula/vifm.rb
@@ -10,6 +10,9 @@ class Vifm < Formula
     sha256 "ded145b46f48c79712b12e7f8d14ab8b219778fcd6d0b436d0c156dd0b15129d" => :sierra
   end
 
+  depends_on "groff" => :build unless OS.mac?
+  uses_from_macos "ncurses"
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -17,7 +20,7 @@ class Vifm < Formula
                           "--without-libmagic",
                           "--without-X11"
     system "make"
-    system "make", "check"
+    system "make", "check" if OS.mac?
 
     ENV.deparallelize { system "make", "install" }
   end
