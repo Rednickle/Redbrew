6fecc948d4a55b3e915901f9b793c8b40216ff37
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 09:14:03 +0000

    wireguard-go: update 0.0.20190908 bottle.

--- Formula/wireguard-go.rb
+++ Formula/wireguard-go.rb
@@ -11,6 +11,7 @@ class WireguardGo < Formula
     sha256 "29959d4a87c09636b0f3dd89905dc0f9d81862ccf942c67834b162f7cac66d0e" => :mojave
     sha256 "bbe9ef9b46de18a4133f62b93204aa5d06c3129e9ffbf90e9e87a1505193c2ce" => :high_sierra
     sha256 "a4483d505d39194f2e0c091eec94332f3be8c59d5b019f71eec4e3703739f377" => :sierra
+    sha256 "2cd07a70d9a91534af56e834182bffc3d226f2b7005d45fbc6e94de3ad8e4e11" => :x86_64_linux
   end
 
   depends_on "go" => :build
