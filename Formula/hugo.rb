42c6d8f4162b3564a52ac48ff0687db9b0d47559
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 19:36:59 +0000

    hugo: update 0.58.3 bottle.
    Closes #16296.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/hugo.rb
+++ Formula/hugo.rb
@@ -11,6 +11,7 @@ class Hugo < Formula
     sha256 "9fdca915591f7b84426a8c259979d955266dffd8fa89c705114c14ffd871443b" => :mojave
     sha256 "8fa4d09414c4d746d14d518b862bfd9adc9d6cc3de6e35588097c7d2678249d0" => :high_sierra
     sha256 "c32035708c6d60fce971eee46857f0b29b62abb4d24028be68878fa1cf6cc082" => :sierra
+    sha256 "62274b3b9175a793daff08aedb8a4f675cf624a9c555cacafff4fbbe2c1639df" => :x86_64_linux
   end
 
   depends_on "go" => :build
