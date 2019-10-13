ca5bf4a64974227040e30bce6e497b8fbf4bd358
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 11:02:28 +0000

    tmate: update 2.3.1 bottle.

--- Formula/tmate.rb
+++ Formula/tmate.rb
@@ -10,6 +10,7 @@ class Tmate < Formula
     sha256 "644a6f94ff04529b22d2a7e297ebddf3e6882c8591282d2f295a41fdf1e2cf61" => :catalina
     sha256 "d31d9aaa27d2fb8bd605e26cf4eb9721a821c1ea3262a2a9f9740de08dd296b9" => :mojave
     sha256 "6095c9b251e9a3c5b76155dee4b7866e157bc563ee9155f8976c86afaa5cbf3e" => :high_sierra
+    sha256 "38e92fd13bdea4aa1a384ce38d5e4b8062c0ae7df37e85ae3b6b969e81e0d9c6" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
