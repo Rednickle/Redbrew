309ad954b04ba0a8c22e1729b36d97a9c3583b77
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 09:55:00 +0000

    whalebrew: update 0.2.3 bottle.

--- Formula/whalebrew.rb
+++ Formula/whalebrew.rb
@@ -11,6 +11,7 @@ class Whalebrew < Formula
     sha256 "9abfd2e5951f589dde2cc9847b44e77741785a8b9ff5208c958bd8233def74b8" => :mojave
     sha256 "66e059ff26ef8e1a7d15416bf0b3a946716e03e60cf7f1281e572dcd78b1f4d8" => :high_sierra
     sha256 "a6357b16b2285d65c523902fa90180f3b6e77b08196cf6edce3f32b38202ff54" => :sierra
+    sha256 "2c9aded52aa7f5457948df8bf5dd6f713e5950d13965e05b35df0a560a42acb8" => :x86_64_linux
   end
 
   depends_on "go" => :build
