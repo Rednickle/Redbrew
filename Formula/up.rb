d5d1d5323e4157104acea8e501fba165d8b505e8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 09:34:20 +0000

    up: update 0.3.2 bottle.

--- Formula/up.rb
+++ Formula/up.rb
@@ -9,6 +9,7 @@ class Up < Formula
     sha256 "0c453761279cdc6a995ae471841b2e8513215c3d30f4f448c3cf82f548376fa5" => :mojave
     sha256 "f9ea40f11e458e2bda259fa428a9f390d9a9efce1d7983f9325eda17b4655501" => :high_sierra
     sha256 "558f89d83bd23a28ef31a1d72f7749521f68ebf0d767a8cffb2c6b9311461e13" => :sierra
+    sha256 "76219e31703806b0c911b10f8edb5077f6ec423c6bc6d6882445e439c452f54c" => :x86_64_linux
   end
 
   depends_on "go" => :build
