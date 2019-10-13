f9cff43ffec8b5454a146c7a365d2b172884c3c9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 19:36:10 +0200

    vaulted: update 2.4.1 bottle.
    Closes #16277.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/vaulted.rb
+++ Formula/vaulted.rb
@@ -10,6 +10,7 @@ class Vaulted < Formula
     sha256 "5b448f1ea890af2ec60633eb6aec8939be2893c7a134b748af5db26fb4926291" => :mojave
     sha256 "6cc74771b207de479c1f45e5385e7b55a46daa256377ec0098746d1e0d3a6441" => :high_sierra
     sha256 "b5a81beb6acdbb14c1d83304c2cf12c21b37a41c93dc48d11b588025f777ed68" => :sierra
+    sha256 "b9f214946229d2021a09d1c5a1bbd82fd34e9462ea077dcde88ceb049894d0a0" => :x86_64_linux
   end
 
   depends_on "go" => :build
