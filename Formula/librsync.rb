da7218ab3f435ac7718f740478dd622fb79ff25f
Dawid Dziurla <dawidd0811@gmail.com>
2019-10-13 12:24:14 +0200

    librsync: set cellar
    Closes #16230.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/librsync.rb
+++ Formula/librsync.rb
@@ -5,6 +5,7 @@ class Librsync < Formula
   sha256 "9eac82c09c4404ba4edadd25dbbcc14008e553c9b2fb492ff9ee05cad9fa012c"
 
   bottle do
+    cellar :any_skip_relocation
     sha256 "b9b3afb3ec1694152e3c0bd32044d3a6fedae7143837004ce3ebb6dd4350c4a5" => :catalina
     sha256 "18e5e0e833f9ce29038c02bf88f08e05bb319d0e52b9a28da2f7dac5931076d6" => :mojave
     sha256 "4e58b9ce861bd356f6626e7e8d2b0e6b80d4c0f54387c51adab6561a41c873a6" => :high_sierra
