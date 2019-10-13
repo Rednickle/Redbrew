ffbc853446be5eb4b46f39bcbfd46b6a5a5e3721
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 17:53:14 +0200

    lf: update 13 bottle.
    Closes #16262.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/lf.rb
+++ Formula/lf.rb
@@ -9,6 +9,7 @@ class Lf < Formula
     sha256 "ad4cfc498d617d41c02b9482c0411b8048d7eaa934cfed114861555de742cf7c" => :mojave
     sha256 "e25cfc6246e04b13e90260433cf1fc27a28af17b82ac7d6bb93e09123faa4f61" => :high_sierra
     sha256 "49a61cf93df5beac903c441087755cc81a4062bcd6f4665aa678cbf21e5c286b" => :sierra
+    sha256 "967dcd8c7fd15afb124c7a344e8a8a41748f985e0625ec245c87fe8bef98833f" => :x86_64_linux
   end
 
   depends_on "go" => :build
