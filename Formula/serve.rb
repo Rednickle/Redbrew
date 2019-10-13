c363cf01397c8e79ffba4893531f8336ac6bf593
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 19:27:59 +0000

    serve: update 0.4.0 bottle.
    Closes #16289.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/serve.rb
+++ Formula/serve.rb
@@ -10,6 +10,7 @@ class Serve < Formula
     sha256 "a339885f14479144ebe771f5caba2239290d89926a1a8bbbaa56751b9ef62f8f" => :mojave
     sha256 "2b1474e49ed747c67fde6ed65e404da4c4e4d2f39190995241e10e62a99cca17" => :high_sierra
     sha256 "c23f691164cbd2f96630d273215f300e1e05ad99427b63e830db52c01eb9ac08" => :sierra
+    sha256 "e6e12f8c0d18dae0d1251ab45f2508388750ff748fbf1c74012ac75a2d205978" => :x86_64_linux
   end
 
   depends_on "go" => :build
