20a05d26019a7911be7826aa311baabde16824b7
Issy Long <me@issyl0.co.uk>
2019-10-14 22:39:07 +0100

    homebank: Fix intltool XML::Parser dependency
    Closes #16357.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/homebank.rb
+++ Formula/homebank.rb
@@ -23,6 +23,9 @@ class Homebank < Formula
   depends_on "libsoup"
 
   def install
+    # Needed by intltool (xml::parser)
+    ENV.prepend_path "PERL5LIB", "#{Formula["intltool"].libexec}/lib/perl5" unless OS.mac?
+
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}", "--with-ofx"
     chmod 0755, "./install-sh"
