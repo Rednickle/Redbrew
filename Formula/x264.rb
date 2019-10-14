46bef77ed1a301c80c7dbafca22a0a6e390cad9a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-14 17:26:06 +0200

    x264: update r2917_1 bottle.
    Closes #16350.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/x264.rb
+++ Formula/x264.rb
@@ -17,6 +17,7 @@ class X264 < Formula
     sha256 "9e49fa8cc8e0bd02bdb85f8b2def682a8c6aab5d3f7bfe6bb51e2e78da1b2eb9" => :catalina
     sha256 "07d6a4de866c38296a3cb788c3370857bd745e88cd7e1723fc0261c4e44a1081" => :mojave
     sha256 "80b6d49faed147546c8639bdc09143968587d7fed7c45dcd9c4e0f56cdb932ff" => :high_sierra
+    sha256 "749ed57861f80c753f35380898e4d82a67de5471254a0a18a30752d22a1d9917" => :x86_64_linux
   end
 
   depends_on "nasm" => :build
