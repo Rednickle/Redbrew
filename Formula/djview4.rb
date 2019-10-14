b9501c7ba1261e4d07c13b96f5cee42847b0e176
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-14 17:27:17 +0200

    djview4: update 4.10.6_1 bottle.
    Closes #16346.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/djview4.rb
+++ Formula/djview4.rb
@@ -11,6 +11,7 @@ class Djview4 < Formula
     sha256 "11c318b224adb9e2575c754c1a3ad6a4c5f4e2febe4dd0a81d63e6ee748af765" => :catalina
     sha256 "f8e5afe939077fd62f6c946323e9f857572ba8c696dd6f1caccb33fbe84dd328" => :mojave
     sha256 "a9c95fcc6bf1dec71109b4bf32f827db003375682b522efe20743f6cb2e8a800" => :high_sierra
+    sha256 "73a2c1bf15cedd1c253302302edd4f68bb49ec00364d8e614fa6ba5f246bbf45" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
