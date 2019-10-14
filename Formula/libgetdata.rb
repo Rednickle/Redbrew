480f999615e00276b8af78fa1ed4c883939f770a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-14 20:42:20 +0000

    libgetdata: update 0.10.0_2 bottle.

--- Formula/libgetdata.rb
+++ Formula/libgetdata.rb
@@ -11,6 +11,7 @@ class Libgetdata < Formula
     sha256 "f133f438e1833bff0f5cf43109e27768a983a068dec90a767ba9027d2bc2f0b9" => :catalina
     sha256 "6c5f143bb202c280c3b3e340a420a1cf6c6d936cba70faf837cd215e451987fe" => :mojave
     sha256 "6b8b5f7801a6cf31ecd5ac82ee02ca344f9634ad01c235a828e3875d0354931b" => :high_sierra
+    sha256 "aaa7acceffef19f5e77f6934418c8c394b59e88c8de869bedb2de8163df3a441" => :x86_64_linux
   end
 
   depends_on "libtool"
