f9942fdc6a8599c75b66c0de38d18132548aa4f8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-15 09:22:52 +0000

    libdbi: update 0.9.0 bottle.

--- Formula/libdbi.rb
+++ Formula/libdbi.rb
@@ -10,6 +10,7 @@ class Libdbi < Formula
     sha256 "ce66e90000681c5f9174c3698ac4ceefd5d1be6ca4ffa574053f0705217c6837" => :catalina
     sha256 "3aff10515535dc3f99dfa56644229daba74f719838d3e580754b3bbdc3c0429d" => :mojave
     sha256 "eb3d8474601267d835b74b5a29944dc6d987486745dcfd17389be3a44b2c0175" => :high_sierra
+    sha256 "bf81a3dfe0450e286917c856af5b630e5e02983f0d59d466316806c87523c500" => :x86_64_linux
   end
 
   def install
