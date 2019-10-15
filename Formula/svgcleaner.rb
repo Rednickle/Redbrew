c7d540fac16b50e42c1d29d43ec4c60d841a290a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-15 12:52:39 +0200

    svgcleaner: update 0.9.5 bottle.
    Closes #16382.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/svgcleaner.rb
+++ Formula/svgcleaner.rb
@@ -11,6 +11,7 @@ class Svgcleaner < Formula
     sha256 "43533727baf2ed09cdce9fe64357c1bc1f70fed57d70f37cfd824b664ab1266f" => :catalina
     sha256 "bf18c353316b7a46ed2cecad188a638e359ce77acdcf501f578e5f96149ed667" => :mojave
     sha256 "7e6df86bb8f994b157ff6de9bb7f43605b813a6a476f6f2d3af4d3483c1b6483" => :high_sierra
+    sha256 "8357ea0709abd6d2794a1b7430c57e09a080cb28a87e4d4e6f08172d7c3d76bd" => :x86_64_linux
   end
 
   depends_on "rust" => :build
