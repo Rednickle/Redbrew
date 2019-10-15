e36b1e36bcb68bca079781d1c2e27df6a5df2ff0
Lewis Russell <lewis.russell@arm.com>
2019-10-15 09:07:59 +0000

    texinfo: Depend on perl for linux
    Closes #16378.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/texinfo.rb
+++ Formula/texinfo.rb
@@ -5,14 +5,19 @@ class Texinfo < Formula
   mirror "https://ftpmirror.gnu.org/texinfo/texinfo-6.7.tar.xz"
   sha256 "988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa"
 
+  revision 1 unless OS.mac?
+
   bottle do
     sha256 "0686381d97b0448c10d11eaba59722c029d17c8423c17ad524b76ec086790f44" => :catalina
     sha256 "419fccc89f850de008e954984c65eea9b7f82940178f7ee439e42c2c892a2e52" => :mojave
     sha256 "a634a1bd15d3d7735e4934fcf26bfa295ce17108912ae7451d2761c6d578de6a" => :high_sierra
-    sha256 "04ab49bf558c688a179f9d992e375e1523b08df2a7132c0224c0d33ed4685612" => :x86_64_linux
   end
 
   depends_on "gettext" if MacOS.version <= :high_sierra
+
+  # Don't use system perl since texinfo requires Encode and Data::Dumper which may not
+  # be installed.
+  uses_from_macos "perl"
 
   keg_only :provided_by_macos, <<~EOS
     software that uses TeX, such as lilypond and octave, require a newer
