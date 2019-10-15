e59203d0df08e3b1eff4143168431096cefc1c90
Dawid Dziurla <dawidd0811@gmail.com>
2019-10-15 13:32:58 +0200

    ssed: remove cellar
    Closes #16391.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/ssed.rb
+++ Formula/ssed.rb
@@ -5,7 +5,6 @@ class Ssed < Formula
   sha256 "af7ff67e052efabf3fd07d967161c39db0480adc7c01f5100a1996fec60b8ec4"
 
   bottle do
-    cellar :any_skip_relocation
     rebuild 1
     sha256 "21b9139163cd1f1ddf11ad063e4e9c2409d7c73cba2473912d9117a1631205b8" => :catalina
     sha256 "3684fa95549fe291253881be5f173e9cff43940be146842f3576a48c7052e234" => :mojave
