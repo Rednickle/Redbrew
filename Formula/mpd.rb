be2983b819cbea7f66587bad659f8675ce7dbaf9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-15 17:36:16 +0000

    mpd: update 0.21.15 bottle.

--- Formula/mpd.rb
+++ Formula/mpd.rb
@@ -9,6 +9,7 @@ class Mpd < Formula
     sha256 "d362c926d1b8b974dc0c1e931afc38d1468352d087eea19b7ac4492aea70fca8" => :catalina
     sha256 "f17c82181023f774bab28323f9ec6b8df08c9c18ad59d97dd7683f02f95880dd" => :mojave
     sha256 "2d694f8dfabd1a90062b1a0f307f8afe5c8c63d25e0183da58c93e254180df65" => :high_sierra
+    sha256 "f9e5a715a2d60a0fdb10e764764d78eca4e2cd7b1a3e4ccf5ad7fa4b0dca2e09" => :x86_64_linux
   end
 
   depends_on "boost" => :build
