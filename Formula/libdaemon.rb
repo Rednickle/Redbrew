ec23a8d9ee9ccdadfd3a3ecc3d266e7fbf49db30
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-15 09:20:31 +0000

    libdaemon: update 0.14 bottle.

--- Formula/libdaemon.rb
+++ Formula/libdaemon.rb
@@ -10,6 +10,7 @@ class Libdaemon < Formula
     sha256 "ad96f0b0e09c3e0c178d3e903659d65ae34fea18365197924a4911c291d02531" => :catalina
     sha256 "1fe52d810eca4471b4d285de02a09ea9e4b78d762f1a2a292d6da1eb10e9626d" => :mojave
     sha256 "0933bb1dde0237f4079fefcd228ea644be36fbf814aa96762ebbae3537886558" => :high_sierra
+    sha256 "26283261454649e12e8060adcfe6e4e4d80d6c65d6ecfc4a6b0c706bfc65f0d2" => :x86_64_linux
   end
 
   def install
