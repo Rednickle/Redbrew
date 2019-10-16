7dbf8e35f50ceecdb478b8af785981b9aab5ef13
Dawid Dziurla <dawidd0811@gmail.com>
2019-10-16 22:36:38 +0200

    binwalk: fix build for linux
    Closes #16435.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/binwalk.rb
+++ Formula/binwalk.rb
@@ -14,30 +14,16 @@ class Binwalk < Formula
 
   depends_on "swig" => :build
   depends_on "gcc" # for gfortran
+  depends_on "numpy"
   depends_on "p7zip"
   depends_on "python"
+  depends_on "scipy"
   depends_on "ssdeep"
   depends_on "xz"
-
-  resource "numpy" do
-    url "https://files.pythonhosted.org/packages/ac/36/325b27ef698684c38b1fe2e546e2e7ef9cecd7037bcdb35c87efec4356af/numpy-1.17.2.zip"
-    sha256 "73615d3edc84dd7c4aeb212fa3748fb83217e00d201875a47327f55363cef2df"
-  end
-
-  resource "scipy" do
-    url "https://files.pythonhosted.org/packages/ee/5b/5afcd1c46f97b3c2ac3489dbc95d6ca28eacf8e3634e51f495da68d97f0f/scipy-1.3.1.tar.gz"
-    sha256 "2643cfb46d97b7797d1dbdb6f3c23fe3402904e3c90e6facfe6a9b98d808c1b5"
-  end
 
   def install
     xy = Language::Python.major_minor_version "python3"
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
-    %w[numpy scipy].each do |r|
-      resource(r).stage do
-        system "python3", *Language::Python.setup_install_args(libexec/"vendor")
-      end
-    end
-
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
     system "python3", *Language::Python.setup_install_args(libexec)
     bin.install Dir["#{libexec}/bin/*"]
