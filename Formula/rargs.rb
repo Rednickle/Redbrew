b27012b53bb7ef83eb6176547f64eb1b06462cc6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-17 18:39:59 +0200

    rargs: update 0.2.2 bottle.
    Closes #16456.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/rargs.rb
+++ Formula/rargs.rb
@@ -10,6 +10,7 @@ class Rargs < Formula
     sha256 "61300f0d8d770b58077ef97efdf5f50bf2f5df244cfc1dcd7c75d940948bdb0f" => :catalina
     sha256 "eb92709c9127872b57c807c5a110329bea94cc43a6c6a030ff6fee6fe8240100" => :mojave
     sha256 "7d5d38da14e10453dfac30d784f7bf4f7c75e7f188c7b3df556ebd5f2df8e76c" => :high_sierra
+    sha256 "4e848dc8a091f1266f916d9bfc5f8cc700a51aaa31f0b8499cd5d4c900415ce1" => :x86_64_linux
   end
 
   depends_on "rust" => :build
