921c947da6be171501ecf1eb86dc39efd8d281d5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-17 19:09:48 +0200

    bvi: update 1.4.1 bottle.
    Closes #16449.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/bvi.rb
+++ Formula/bvi.rb
@@ -8,6 +8,7 @@ class Bvi < Formula
     sha256 "83cfa7a1fe8848d8eab7f01da94a32e75eb7c57221854a3f3f06a05417975977" => :catalina
     sha256 "567e9512dfd4fcf7768c442a6e609f7b798b887e4cdd59c1b38970940b9c528f" => :mojave
     sha256 "28ba5db61217dfe797423fd55b5fce06def1d5760aa466685759f1e315459777" => :high_sierra
+    sha256 "b2d9959aa4e3c0a638e71e63adec26ecf582eaf67204d4feabadd1dc177e26e4" => :x86_64_linux
   end
 
   uses_from_macos "ncurses"
