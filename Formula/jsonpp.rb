59e72526fae734123bcff3b86b0f2dfccec0e546
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-18 20:47:32 +0200

    jsonpp: update 1.3.0_1 bottle.
    Closes #16506.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/jsonpp.rb
+++ Formula/jsonpp.rb
@@ -12,6 +12,7 @@ class Jsonpp < Formula
     sha256 "d81995103192bb58f66d7089939eb6682f117a7044d3a84804db62b4c31a3c81" => :high_sierra
     sha256 "219f8a6bfdf1d0e8435fa1c1fdf0cc22b91cae8ec7d62581d312927fabcf9388" => :sierra
     sha256 "3161f55711eea589c5036078fbf3a5df47484767f025adda7c0692d4dda5f2b4" => :el_capitan
+    sha256 "f6b0ca281c47061e910fda6992e548196ec11e3e5147a80467b6da48479f4097" => :x86_64_linux
   end
 
   depends_on "go" => :build
