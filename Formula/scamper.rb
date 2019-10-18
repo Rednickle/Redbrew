f4aed025fdca830707c0c9085cdf887be2d4e8e7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-18 20:56:21 +0200

    scamper: update 20190916 bottle.
    Closes #16486.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/scamper.rb
+++ Formula/scamper.rb
@@ -9,6 +9,7 @@ class Scamper < Formula
     sha256 "5465091cd5142c1db0bc400897bdfc3045d3111f6fd8c6ed903f785a285e0128" => :catalina
     sha256 "6d3ebdb312d61258244b3e537b8a210d50e4933975ffa12677c7e40e0866b7d3" => :mojave
     sha256 "6d714da5b057f7033a2b9590c989ebf6a01dd71c785e71cb6f3f9217d54f05a8" => :high_sierra
+    sha256 "93b46c7f1c1548b4d6b001c3b88790d8db47052f43dcd43d760f4714f20961c0" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
