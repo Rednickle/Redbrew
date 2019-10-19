1cdca24a3cd6155307abcaa61002d75458ead304
Michka Popoff <michkapopoff@gmail.com>
2019-10-19 11:07:12 +0200

    go-statik: fix test for linux
    Closes #16531.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/go-statik.rb
+++ Formula/go-statik.rb
@@ -3,6 +3,7 @@ class GoStatik < Formula
   homepage "https://github.com/rakyll/statik"
   url "https://github.com/rakyll/statik/archive/v0.1.6.tar.gz"
   sha256 "f157a1ada813eb643ddd9a60a0efe3158f1da25b1d11bc1ef6c7fa219d4b23bf"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
@@ -10,7 +11,6 @@ class GoStatik < Formula
     sha256 "4e347596a83f169bdb0b72cd81d69ea275966fead97f7decb67c873380c179b8" => :mojave
     sha256 "4ec52d9626abcbb07f25640c9ecec4e620faa9ee619f6f289f172aa7bc509590" => :high_sierra
     sha256 "e0e8c356eb1ed32cf074d4abfa81fd8fa9d6d8dfb7c7724c696a521f974a3b44" => :sierra
-    sha256 "ed1a7016131c7d1f134337a26d72a57750054d816159efd37354614b8f68b9e7" => :x86_64_linux
   end
 
   depends_on "go" => :build
@@ -29,7 +29,8 @@ class GoStatik < Formula
 
   test do
     font_name = (MacOS.version >= :catalina) ? "Arial Unicode.ttf" : "Arial.ttf"
-    system bin/"statik", "-src", "/Library/Fonts/#{font_name}"
+    font_path = OS.mac? ? "/Library/Fonts/#{font_name}" : "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+    system bin/"statik", "-src", font_path
     assert_predicate testpath/"statik/statik.go", :exist?
     refute_predicate (testpath/"statik/statik.go").size, :zero?
   end
