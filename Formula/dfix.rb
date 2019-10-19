a9c108851c1bc49f2c0bea5b1d627ac02df84aa0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-19 14:28:39 +0000

    dfix: update 0.3.5 bottle.

--- Formula/dfix.rb
+++ Formula/dfix.rb
@@ -12,6 +12,7 @@ class Dfix < Formula
     sha256 "ff3b76977bcbfb5b7a04bbebb53a794cc522b64987f724fe5f8a8236812eb1f8" => :catalina
     sha256 "24d234e206efa754f8bd900102720280d8efb1af6ec93059a467589acddca3ee" => :mojave
     sha256 "13a2621737c198bd0540f507293a9b015a0ebe36cd3373589a69ec834a863d8d" => :high_sierra
+    sha256 "8f27a62cadfd35f223e416b0b68b05b57f4d8d5b0710c735a579f6aeeae2385b" => :x86_64_linux
   end
 
   depends_on "dmd" => :build
