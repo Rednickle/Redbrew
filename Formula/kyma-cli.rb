ae426c4a6dc89bcfca237d7f50196f66c521c6a4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-20 09:03:54 +0000

    kyma-cli: update 1.6.0 bottle.

--- Formula/kyma-cli.rb
+++ Formula/kyma-cli.rb
@@ -11,6 +11,7 @@ class KymaCli < Formula
     sha256 "96b57388a6328157114c8c14e7d3291668f175f471ff957a4a9a6d69599a6a03" => :catalina
     sha256 "7be49d03b5e679073fd2c962003178d32a763497f806c5de90195e986d1aaacf" => :mojave
     sha256 "58723114b1043e44686c6d07bdc241bb1bcd2f1f6e85317017b480acedc52800" => :high_sierra
+    sha256 "94b872a8abed2acf2f948ea9b49731edaedaf65c68476ee4aa2c9701af97ec63" => :x86_64_linux
   end
 
   depends_on "dep" => :build
