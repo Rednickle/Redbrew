d848a79f4b44a4d1899dfb2edbd7c243e1b7563c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-20 10:28:32 +0200

    goreleaser: update 0.120.0 bottle.
    Closes #16575.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/goreleaser.rb
+++ Formula/goreleaser.rb
@@ -10,6 +10,7 @@ class Goreleaser < Formula
     sha256 "f1e47acd3363497d2a3a271be7a0ed715d0c2efbda70000f81ea918af0ae1e10" => :catalina
     sha256 "2b4542aa4f0b24bf7118c1bb36b8d222baf3261fcca1ed21c9b392a1c58b827e" => :mojave
     sha256 "4154cf60e3d6abb30cb38456c6301eac4505836ec708eff74f72aa517d186168" => :high_sierra
+    sha256 "9f5e0da8caff75a245ab32883a600835c8e015a21306e9c5fabf0692ee8a8a43" => :x86_64_linux
   end
 
   depends_on "go" => :build
