7f9737a3b1f8038e79c56f6b97f289b02ad083f0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-20 22:53:35 +0200

    bettercap: update 2.26 bottle.
    Closes #16589.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/bettercap.rb
+++ Formula/bettercap.rb
@@ -9,6 +9,7 @@ class Bettercap < Formula
     sha256 "4eae0c301515c74beac150ebfab016733cd48ad80ddeceaa0d77ea9ef6d76fd0" => :catalina
     sha256 "202a73b72ee8bc22945e6e41905126308345d868d17b2330a0ffd20b5d51710b" => :mojave
     sha256 "eee01b5a166fa40860fb3424cb903994ecf295778fea133d28ec04ee2bd1e368" => :high_sierra
+    sha256 "7f8483404785756868ed4085eeb5770354e2c01aa7ce3774715c3a1ce5f9fb3c" => :x86_64_linux
   end
 
   depends_on "dep" => :build
