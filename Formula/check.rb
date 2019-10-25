dd127202729f4ead772fc15813bc3daeb19be721
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-25 18:46:26 +0200

    check: update 0.13.0 bottle.
    Closes #16696.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/check.rb
+++ Formula/check.rb
@@ -9,6 +9,7 @@ class Check < Formula
     sha256 "7e43575b92dbfe052ea8eee6f488799faa6c9e0a411040b10b0734cc3f50e375" => :catalina
     sha256 "7ceb61ee5e716184068334f62a40c708270f88e7e0709e7d464eefd71366d272" => :mojave
     sha256 "12147cd97f24af6c18fa393200f063e1d24ea95ffbd1b29cb2dd1f2a4f8ef24b" => :high_sierra
+    sha256 "ace3f377135dfd5363d53987a6c5cce7107190ea738d04309b04c47bd6e4cf9b" => :x86_64_linux
   end
 
   uses_from_macos "gawk"
