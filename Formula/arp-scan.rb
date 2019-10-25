f0004b3692639147066d3e7f0e509bfb3024d336
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-25 18:46:00 +0200

    arp-scan: update 1.9.6 bottle.
    Closes #16697.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/arp-scan.rb
+++ Formula/arp-scan.rb
@@ -9,6 +9,7 @@ class ArpScan < Formula
     sha256 "6b6621b80ffaccbbbd1f9942e7279ac786aaa258003980c6111e27d49ab199d5" => :catalina
     sha256 "d17e1ad7dac154571c7e48fe2d9aac7b0555c10a60c1184ffc8b273e5b1238a7" => :mojave
     sha256 "e6dbdf4a4c2cc9f42012c76192bf562858aa8838895cda7e507543497b253003" => :high_sierra
+    sha256 "a89a80ae6bbf2256c9d11cdf766481adf13dc679054dabf8ce3872d07dc35287" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
