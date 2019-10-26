be8da06f8fa620adef549e56dc5109667f71da2d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-26 22:18:47 +0000

    openssl@1.1: update 1.1.1d_1 bottle.

--- Formula/openssl@1.1.rb
+++ Formula/openssl@1.1.rb
@@ -13,6 +13,7 @@ class OpensslAT11 < Formula
     sha256 "104ef018b7bb8fcc49f57e5a60359a28a02d480d85a959e6141394b0571cbb28" => :mojave
     sha256 "c7681ee40cb3680cd9fafcdb092bde153b9d4903907d67858baa5f19025f927b" => :high_sierra
     sha256 "a95d756e9aa3a8d118833f9083112048bf635f20c33943de04163bdcf7412328" => :sierra
+    sha256 "010a4e5257eb685ac700f495208bed1e05155663f8083ab6ab2e4a91ef69c1cd" => :x86_64_linux
   end
 
   if OS.mac?
