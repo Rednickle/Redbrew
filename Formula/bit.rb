fc5f46f2f4f93258edf484c04515ca92945c72a0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-28 12:01:52 +0100

    bit: update 14.4.3 bottle.
    Closes #16743.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -12,6 +12,7 @@ class Bit < Formula
     sha256 "ca194dc7e25d0e37c3c082a4fe2b0229db71cafddf0832d86b08a28d7f7f4318" => :catalina
     sha256 "80f6d859190166242df5fa8e14c8b6534772a3bc6d6eedd054a2acd34df38114" => :mojave
     sha256 "92f5f4278d08078b63ef62c5169617fa73cf13062e0023a6f46fd949175ec852" => :high_sierra
+    sha256 "17f4f14ac216561bb5a46fccb9a94e3d713cf5aea3407fc4219a90f437f5ce09" => :x86_64_linux
   end
 
   depends_on "node"
