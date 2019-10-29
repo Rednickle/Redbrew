9b28a81750f13dbf586d11d95d37a05903f11407
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-29 20:38:40 +0000

    quickjs: update 2019-10-27 bottle.
    Closes #16768.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/quickjs.rb
+++ Formula/quickjs.rb
@@ -8,6 +8,7 @@ class Quickjs < Formula
     sha256 "a8e771d888b7e3f540cbff0a9de250a86c591d4979ce0b92aaa0fa861a88eefd" => :catalina
     sha256 "83c8e56fb121b974a608a6364d8805a7b2e5aa685f2ee2e68a3374eb08885945" => :mojave
     sha256 "9447184b11119f128e4a24284a08e2fc6598bd7b49f461025270711b0dfd117a" => :high_sierra
+    sha256 "11c7fab8cf6d1e2c5f40a858da3b7e69417c5e2a9677bf0fba24108627644120" => :x86_64_linux
   end
 
   def install
