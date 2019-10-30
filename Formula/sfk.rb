1f032e8ca9be14c691e56ba0f2f8ff0ef5431bc8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-30 06:44:21 +0000

    sfk: update 1.9.5.2 bottle.

--- Formula/sfk.rb
+++ Formula/sfk.rb
@@ -11,6 +11,7 @@ class Sfk < Formula
     sha256 "01a8fdc14835ec662924df0ec6772afa80585e78ff92d58e92596d84e18e0d99" => :catalina
     sha256 "c8ba1f86897c1091d54528e2d3e1269ab140197b86c4eaea47ac5aa39d7bf2cd" => :mojave
     sha256 "47d8b16e909564e1f04667c725c0a0ae4668f915af7528646e37d03f9ec9f021" => :high_sierra
+    sha256 "25a45209dcf60963f0894c621d53a479b4509a5494cafba9dfaf1537b9093d6f" => :x86_64_linux
   end
 
   def install
