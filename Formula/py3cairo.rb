a8424ad28fdf916a8a2f136418f4662010ba1475
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-31 16:40:28 +0100

    py3cairo: update 1.18.2 bottle.
    Closes #16824.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/py3cairo.rb
+++ Formula/py3cairo.rb
@@ -9,6 +9,7 @@ class Py3cairo < Formula
     sha256 "6677109a62ba0067b4da18eaa3b49fa7046602787563ef9070c01770e8b9317c" => :catalina
     sha256 "d11fba86f2d505548d02700480cb6a9791b3c5e7d973c17fbf5b70f79492fa01" => :mojave
     sha256 "080b8420adc3a72c4f140d0149300b4f97eceee6e06ada4ffff15fa79cbaa099" => :high_sierra
+    sha256 "cbfe2c99c2ab96c52b0391a94df87311ed0aed2e317ff530a77fd45a64449d60" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
