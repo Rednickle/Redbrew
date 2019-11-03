d4591286a25da8495b7339a38ec138968d3016be
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-03 15:46:18 +0100

    fwup: update 1.4.0 bottle.
    Closes #16960.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/fwup.rb
+++ Formula/fwup.rb
@@ -9,6 +9,7 @@ class Fwup < Formula
     sha256 "411beb6a6a976a22f59c202a02308c339894da416ae459a747ef337b012df5f9" => :catalina
     sha256 "749cdac349bff16d3cd48ea942488e6a936645a405c21bf29c1ba65602a789df" => :mojave
     sha256 "0ff887162dbb375b7fd85e1a136ffd739fdedbac72072d2af38727c621d6f3ce" => :high_sierra
+    sha256 "18d606f9b35e73fb2da310d9f20443db06520830be64a2d8c000e6e352ae2feb" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
