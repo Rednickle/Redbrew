1551c89d4a191b920502f7e9feebbee1210af94e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-03 12:43:40 +0100

    dust: update 0.3.1 bottle.
    Closes #16928.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/dust.rb
+++ Formula/dust.rb
@@ -11,6 +11,7 @@ class Dust < Formula
     sha256 "3b49364aa67239151c5cf3f133a3fe8f06ad06e8e625545605dba09c1da654f0" => :catalina
     sha256 "3a5a22375b9dc464a23226667bc4c4e0d3711ceef4ac691fb3de2fd96fa17521" => :mojave
     sha256 "f0abd07b12a44556813165f4a5266fad616dd62dce055fd4258205751aff2285" => :high_sierra
+    sha256 "9ad6b3ffb1c8e729b5da3f2a4e68f7ab7f3b746c356023a05cda223102fb795c" => :x86_64_linux
   end
 
   depends_on "rust" => :build
