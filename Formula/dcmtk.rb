51fcc7df44aba33407d4a80e956c14c545c73745
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-04 11:12:49 +0100

    dcmtk: update 3.6.5 bottle.
    Closes #16996.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/dcmtk.rb
+++ Formula/dcmtk.rb
@@ -9,6 +9,7 @@ class Dcmtk < Formula
     sha256 "5b49e8b8d34cd1472f5091fc4d6e4b4a05133ac3cd85f6f428aef6c1013234b0" => :catalina
     sha256 "3d149b4aa7f6d76df7ddda09d845c4c43a0aa0617800b9ea25602183e213f5b0" => :mojave
     sha256 "70ba2b42c6a522f2da68ac47834f240db316afd12b388edb4bc7c1f586a18f2c" => :high_sierra
+    sha256 "32e89cab68de6f2e4ef50ae0c975f344bc5feba5b2a6d91c28e02ecb13756120" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
