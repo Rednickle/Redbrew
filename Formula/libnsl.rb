1398e6c2e8b6211359294888e7532c3471954225
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-05 20:22:10 +0100

    libnsl 1.2.0 (new formula)
    Closes #17050.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

---
+++ Formula/libnsl.rb
@@ -0,0 +1,45 @@
+class Libnsl < Formula
+  desc "Public client interface for NIS(YP) and NIS+"
+  homepage "https://github.com/thkukuk/libnsl"
+  url "https://github.com/thkukuk/libnsl/archive/v1.2.0.tar.gz"
+  sha256 "a5a28ef17c4ca23a005a729257c959620b09f8c7f99d0edbfe2eb6b06bafd3f8"
+  # tag "linuxbrew"
+
+  bottle do
+  end
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext" => :build
+  depends_on "libtool" => :build
+  depends_on "m4" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libtirpc"
+
+  def install
+    inreplace "po/Makefile.in.in" do |s|
+      s.gsub! /GETTEXT_MACRO_VERSION =.*/, "GETTEXT_MACRO_VERSION = #{Formula["gettext"].version.to_s[/(\d\.\d+)/, 1]}"
+    end
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <rpcsvc/nis.h>
+
+      int main(int argc, char *argv[])
+      {
+        if(NIS_PK_NONE != 0)
+          return 1;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lnsl", "-o", "test"
+    system "./test"
+  end
+end
