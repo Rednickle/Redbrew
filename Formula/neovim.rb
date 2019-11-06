e0544bdd0715ece9aa6a07bcfac70020b8f5da00
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-06 16:14:54 +0100

    neovim: depend on libnsl
    Closes: #17035
    
    Closes #17066.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -3,12 +3,12 @@ class Neovim < Formula
   homepage "https://neovim.io/"
   url "https://github.com/neovim/neovim/archive/v0.4.2.tar.gz"
   sha256 "9f874d3d2a74f33b931db62adebe28f8d2ec116270d1e13998b58a73348b6e56"
+  revision 1 unless OS.mac?
 
   bottle do
     sha256 "5436532a1574329e386cf29df04e7d528730a7bd95c073b6ea76a37499ececc0" => :mojave
     sha256 "7d839a0c108ea2a95b7a61bde2fb08f667938bcad2e4a5d908d51a35d899c85c" => :high_sierra
     sha256 "73bc96be3bccacb2c6c02e4c3aaf06ad0dc4f3b34569a46adda8ec7b41db2f52" => :sierra
-    sha256 "813c9f350433dc4dfa6a5a43b17a5eea1cdfc2e554259f862168263c8b9149c3" => :x86_64_linux
   end
 
   head do
@@ -30,6 +30,7 @@ class Neovim < Formula
     depends_on "python@2"
     depends_on "unzip" => :build
     depends_on "gperf"
+    depends_on "libnsl"
   end
 
   resource "mpack" do
