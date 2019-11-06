986ad8e6105e7336ded9be7b89ad7352543d53fc
Michka Popoff <michkapopoff@gmail.com>
2019-11-06 23:11:23 +0100

    openjdk@11: Fix build for Linux
    Closes #17074.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/openjdk@11.rb
+++ Formula/openjdk@11.rb
@@ -15,16 +15,35 @@ class OpenjdkAT11 < Formula
   keg_only :versioned_formula
 
   depends_on "autoconf" => :build
+  unless OS.mac?
+    depends_on "pkg-config" => :build
+    depends_on "alsa-lib"
+    depends_on "cups"
+    depends_on "fontconfig"
+    depends_on "unzip"
+    depends_on "zip"
+    depends_on "linuxbrew/xorg/libx11"
+    depends_on "linuxbrew/xorg/libxext"
+    depends_on "linuxbrew/xorg/libxrandr"
+    depends_on "linuxbrew/xorg/libxrender"
+    depends_on "linuxbrew/xorg/libxt"
+    depends_on "linuxbrew/xorg/libxtst"
+  end
 
   resource "boot-jdk" do
-    url "https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_osx-x64_bin.tar.gz"
-    sha256 "77ea7675ee29b85aa7df138014790f91047bfdafbc997cb41a1030a0417356d7"
+    if OS.mac?
+      url "https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_osx-x64_bin.tar.gz"
+      sha256 "77ea7675ee29b85aa7df138014790f91047bfdafbc997cb41a1030a0417356d7"
+    else
+      url "https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_linux-x64_bin.tar.gz"
+      sha256 "f3b26abc9990a0b8929781310e14a339a7542adfd6596afb842fa0dd7e3848b2"
+    end
   end
 
   def install
     boot_jdk_dir = Pathname.pwd/"boot-jdk"
     resource("boot-jdk").stage boot_jdk_dir
-    boot_jdk = boot_jdk_dir/"Contents/Home"
+    boot_jdk = OS.mac? ? boot_jdk_dir/"Contents/Home" : boot_jdk_dir
     java_options = ENV.delete("_JAVA_OPTIONS")
 
     short_version, _, build = version.to_s.rpartition("+")
@@ -34,19 +53,27 @@ class OpenjdkAT11 < Formula
                           "--without-version-opt",
                           "--with-version-build=#{build}",
                           "--with-toolchain-path=/usr/bin",
-                          "--with-extra-ldflags=-headerpad_max_install_names",
+                          ("--with-extra-ldflags=-headerpad_max_install_names" if OS.mac?),
                           "--with-boot-jdk=#{boot_jdk}",
                           "--with-boot-jdk-jvmargs=#{java_options}",
                           "--with-debug-level=release",
                           "--with-native-debug-symbols=none",
                           "--enable-dtrace=auto",
-                          "--with-jvm-variants=server"
+                          "--with-jvm-variants=server",
+                          ("--with-x=#{HOMEBREW_PREFIX}" unless OS.mac?),
+                          ("--with-cups=#{HOMEBREW_PREFIX}" unless OS.mac?),
+                          ("--with-fontconfig=#{HOMEBREW_PREFIX}" unless OS.mac?)
 
     ENV["MAKEFLAGS"] = "JOBS=#{ENV.make_jobs}"
     system "make", "images"
 
-    libexec.install "build/macosx-x86_64-normal-server-release/images/jdk-bundle/jdk-#{short_version}.jdk" => "openjdk.jdk"
-    bin.install_symlink Dir["#{libexec}/openjdk.jdk/Contents/Home/bin/*"]
+    if OS.mac?
+      libexec.install "build/macosx-x86_64-server-release/images/jdk-bundle/jdk-#{short_version}.jdk" => "openjdk.jdk"
+      bin.install_symlink Dir["#{libexec}/openjdk.jdk/Contents/Home/bin/*"]
+    else
+      libexec.install Dir["build/linux-x86_64-normal-server-release/images/jdk/*"]
+      bin.install_symlink Dir["#{libexec}/bin/*"]
+    end
   end
 
   def caveats
