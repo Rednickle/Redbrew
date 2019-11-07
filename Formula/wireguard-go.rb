fa3d07c719b26aab1eeb1731b975a4193262f84d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-07 17:23:44 +0100

    wireguard-go: update 0.0.20191012 bottle.
    Closes #17110.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/wireguard-go.rb
+++ Formula/wireguard-go.rb
@@ -10,6 +10,7 @@ class WireguardGo < Formula
     sha256 "8a7b16f346fc85f4a6f5c3a251c115f8912fc97975e38bb06d89cbc6a86bf5ae" => :catalina
     sha256 "a83b5669aa33d227ef2a20f2db58fc09ccdbadde417d7cbd4fab69e696a3b86f" => :mojave
     sha256 "af7d625e4662a343bbbe16c87693331722e8435808e90dcbbe0a2e6df545d2f1" => :high_sierra
+    sha256 "f1e176f8a0982ca34f14499a390fe89a1723a861a718bd4b20374aa4f9d62798" => :x86_64_linux
   end
 
   depends_on "go" => :build
