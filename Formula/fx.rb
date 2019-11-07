4ef0949cd53c1dd698e536a7b3c5f367f65f2e4d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-07 16:55:08 +0100

    fx: update 14.1.0 bottle.
    Closes #17081.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/fx.rb
+++ Formula/fx.rb
@@ -11,6 +11,7 @@ class Fx < Formula
     sha256 "4ceee78aab36c25dc0aaafb55eed34e632bba6a7d91c0b8ae0b624a94f96475f" => :catalina
     sha256 "e430433053034f7a86d5808f2af93aa4b9f53bca4c334b76991e21cecb555ab5" => :mojave
     sha256 "a2ea9c508a5b9e98a43cdedf5b6eaa00f2091de3d716368d95239a0a3d77c398" => :high_sierra
+    sha256 "d3a9ec788ef6f397f7e493c5d2677ec16b926d7c0a4b38941646a04646f0cad6" => :x86_64_linux
   end
 
   depends_on "node"
