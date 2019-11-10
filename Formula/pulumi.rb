8f9340f198018d2b25fbe5bfbc7f7b5b2d88805a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-10 11:49:16 +0100

    pulumi: update 1.5.1 bottle.
    Closes #17172.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/pulumi.rb
+++ Formula/pulumi.rb
@@ -10,6 +10,7 @@ class Pulumi < Formula
     sha256 "2f9f8302f5a2c6efd1193f3661cc85bbbda7b6a301ab323d99bf21cae43a5286" => :catalina
     sha256 "d9159ffca790247683988e6acf910da07226fffe15a5267a6fdd2873ec829bbc" => :mojave
     sha256 "8f6f78e6aa8f29fde06919df9fa3b7f96e3284d28bb2eb1620c32a50cc3aea64" => :high_sierra
+    sha256 "4865927eee263e459d2b6268309ed81d9567a2f7ae28d97cd34b52b487ccbe35" => :x86_64_linux
   end
 
   depends_on "go@1.12" => :build
