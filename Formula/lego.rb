21d6b4a66e66e32309f71f7a1ad4af8db0c3cd3d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-10 21:57:28 +0100

    lego: update 3.2.0 bottle.
    Closes #17183.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/lego.rb
+++ Formula/lego.rb
@@ -10,6 +10,7 @@ class Lego < Formula
     sha256 "ffdee52b3c648683f4fabbcdc56baa0dc9cb20b44ac877155f08a6d627d2cc69" => :catalina
     sha256 "9dea300001bce666a42667cbf8c967bf2c73d0494b072bd147dd99c6b8aa2b2f" => :mojave
     sha256 "506828353d7593b758035b0e3302661c78b75c347b5165829f2ab0ae18224422" => :high_sierra
+    sha256 "85589e6311741ac7fa7241c9a8ae940b7da6ab426e21e2ed2a2ed0f73387f935" => :x86_64_linux
   end
 
   depends_on "go" => :build
