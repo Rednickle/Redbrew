80cf35c4fd6ecf162ef6e157fcf81072954f3d39
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-11 18:09:46 +0100

    terragrunt: update 0.21.5 bottle.
    Closes #17196.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -10,6 +10,7 @@ class Terragrunt < Formula
     sha256 "ee3fdd0a7dc5034f85ac102176bdc31f198335a07a5208c7ef0589c729a38a0e" => :catalina
     sha256 "5efb5b254fe215acf133ae182429c98041477d55d0ffb321366de3b3fae47902" => :mojave
     sha256 "75cc3e08ee14ce2053dd0e4a29c01d2df6bcbd2436cd9e851cbaabb2c38d1188" => :high_sierra
+    sha256 "e2060a1652a2e525a10c71c61e794221bdef1fe838ae8e319771208e6f82d255" => :x86_64_linux
   end
 
   depends_on "dep" => :build
