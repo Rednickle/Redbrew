a620652d4c90657dc57372acd01ad8e5301e3840
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-13 22:47:46 +0100

    tnef: update 1.4.18 bottle.
    Closes #17260.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/tnef.rb
+++ Formula/tnef.rb
@@ -9,6 +9,7 @@ class Tnef < Formula
     sha256 "ff92eb820b2efae9e87e42491a590601f400160f27ea2804b176b02b1648be66" => :catalina
     sha256 "2700f31ebcc1e2ba9219d6b6ac040846eba21ccc25baca4fea8b7d630b6673d2" => :mojave
     sha256 "842ba6bbe666302bd39c1cf7d29caa7d5180c20757b8dfe91b99d3fe1d3da841" => :high_sierra
+    sha256 "15a0c084a7f3a8a830201b4de92c6e250a155fc49546d8dffc003dedead98630" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
