fab8940d5719d81e7f37a7b27351cc6ade58af40
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-13 22:36:19 +0100

    bitrise: update 1.36.0 bottle.
    Closes #17242.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/bitrise.rb
+++ Formula/bitrise.rb
@@ -9,6 +9,7 @@ class Bitrise < Formula
     sha256 "83b166d70e6ec9708d770c92f68c52cfa381737799ce03512879dcff1800875f" => :catalina
     sha256 "0955e039c7b22b065e0d7e2b90583af0b506a0a23833819277f23e15eae65505" => :mojave
     sha256 "1a5b0c22877f76d872e605a44d0e17f132a5fbac79e787e19880b1d3a9c5827f" => :high_sierra
+    sha256 "fbde8ee2b22bfcbdc75d5de992653531df97373133ae9fa9faafd209729e3cee" => :x86_64_linux
   end
 
   depends_on "go" => :build
