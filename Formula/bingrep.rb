f406cccb879ee814c8a02da4c17671db078a0cc6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-14 19:38:39 +0000

    bingrep: update 0.8.1 bottle.
    Closes #17286.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/bingrep.rb
+++ Formula/bingrep.rb
@@ -9,6 +9,7 @@ class Bingrep < Formula
     sha256 "c370465c0e36f286d7051007df4cc8e9830f68e8b04ca1b41471396ab5bd3300" => :catalina
     sha256 "9f15cee5770b2bab318aac00b20c60f2b82c81881d99f934420c863c64bc2295" => :mojave
     sha256 "352b163b9239facdfe846d9eb0d0c73f50d2681ced80645ecdd69818942dcfed" => :high_sierra
+    sha256 "54b7f4e0114bc93ff65faf59b5cfc2b49463de6487d99c69d69d7cbfb6efef53" => :x86_64_linux
   end
 
   depends_on "rust" => :build
