7ad9c893f76fcdc6161028c2291616b09c42adbf
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-14 19:43:22 +0000

    pdal: update 2.0.1 bottle.
    Closes #17284.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pdal.rb
+++ Formula/pdal.rb
@@ -10,6 +10,7 @@ class Pdal < Formula
     sha256 "cd83816acd8f7f4328f7e327eb33fda0d40dece97255a40415fc1f3c19539e52" => :catalina
     sha256 "deb26dd7d9f1fff59e075aa2e290bc6b8ce5a9852838a232f2954e77ef220907" => :mojave
     sha256 "f2865ee1f77d01b1f5b62b24ef4f3a4aeb4db2cdab9cbd0b315c3d9666fd8b31" => :high_sierra
+    sha256 "6a2b3f0fd70ea54404c434a486faff6f74154d4e09b0b2da67908d0a829ba3d4" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
