88fbb10c35f470f2954afa35f8ba126699a6c227
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-15 12:54:41 +0100

    libming: Fix build for Linux
    Closes #17314.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libming.rb
+++ Formula/libming.rb
@@ -18,6 +18,12 @@ class Libming < Formula
   depends_on "freetype"
   depends_on "giflib"
   depends_on "libpng"
+  unless OS.mac?
+    depends_on "bison" => :build
+    depends_on "flex" => :build
+  end
+  uses_from_macos "perl"
+  uses_from_macos "python@2"
 
   def install
     system "autoreconf", "-fiv"
