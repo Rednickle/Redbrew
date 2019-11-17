33fde1512537513aa0b3e15a165bc9e87cd9dfd1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-17 15:30:45 +0100

    lazydocker: update 0.7.6 bottle.
    Closes #17353.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/lazydocker.rb
+++ Formula/lazydocker.rb
@@ -10,6 +10,7 @@ class Lazydocker < Formula
     sha256 "e67ef3109ad1a907b47b13b21b618a6f689c054e5ca77b731517d2fc28b22d29" => :catalina
     sha256 "2f9240f41656d68cc0334c4bf8b5d3c960de18d504a32ef5f019eeff4a0b99fc" => :mojave
     sha256 "50094916596e0bc7eb2b377ee80f5f474970b4b2bdbae2693c005860df994584" => :high_sierra
+    sha256 "ac1f4a4276a7d504d6bb8dda415d13b7996eb590ed02ee92a3ad51a4da458c78" => :x86_64_linux
   end
 
   depends_on "go" => :build
