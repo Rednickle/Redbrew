80b0c282b9e81e9002cfe42611627211fc8f682f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-18 20:25:19 +0000

    angle-grinder: update 0.12.0_1 bottle.

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -11,6 +11,7 @@ class AngleGrinder < Formula
     sha256 "cd030afe0fdd2879bdf9db6e060a0d003cab1fcd017ee9900f49155e99b24ee0" => :catalina
     sha256 "59333e0ccb022eba3f3f2b8e448e658533636665632e0368386c4e15e7713804" => :mojave
     sha256 "9450f13f39e8a57a55634625f654594d7c1be801530b785d4ff9a1e6df92f316" => :high_sierra
+    sha256 "76d2f37d7079bb8cc6aeba90471191265c799c9bc7c1fb515f52a81156aa7c91" => :x86_64_linux
   end
 
   depends_on "rust" => :build
