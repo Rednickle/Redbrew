7ca6955e545ccafea73296c744ddf471e86b3508
Issy Long <me@issyl0.co.uk>
2019-11-18 19:22:09 +0000

    angle-grinder: depends_on "openssl@1.1"
    - Bump the revision to build a new bottle with the new OpenSSL.
    
    Closes #17404.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/angle-grinder.rb
+++ Formula/angle-grinder.rb
@@ -3,6 +3,7 @@ class AngleGrinder < Formula
   homepage "https://github.com/rcoh/angle-grinder"
   url "https://github.com/rcoh/angle-grinder/archive/v0.12.0.tar.gz"
   sha256 "a27d8776684ac704114ef9f721c55eb9d41926907985132f2ecb7db15f3932c9"
+  revision 1
 
   bottle do
     cellar :any_skip_relocation
@@ -10,12 +11,13 @@ class AngleGrinder < Formula
     sha256 "cd030afe0fdd2879bdf9db6e060a0d003cab1fcd017ee9900f49155e99b24ee0" => :catalina
     sha256 "59333e0ccb022eba3f3f2b8e448e658533636665632e0368386c4e15e7713804" => :mojave
     sha256 "9450f13f39e8a57a55634625f654594d7c1be801530b785d4ff9a1e6df92f316" => :high_sierra
-    sha256 "81bcd37e196ffc31bae2123dc3f4d3fee51d69fd50b1eb95b7a56d348dcba677" => :x86_64_linux
   end
 
   depends_on "rust" => :build
-  depends_on "pkg-config" => :build unless OS.mac?
-  depends_on "openssl" unless OS.mac?
+  unless OS.mac?
+    depends_on "pkg-config" => :build
+    depends_on "openssl@1.1"
+  end
 
   def install
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
