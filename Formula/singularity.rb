dc6d081c4a4776fac47fe10773da3061746c6760
Issy Long <me@issyl0.co.uk>
2019-11-18 22:06:44 +0000

    singularity: depends_on "openssl@1.1"
    Closes #17409.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/singularity.rb
+++ Formula/singularity.rb
@@ -3,15 +3,15 @@ class Singularity < Formula
   homepage "https://www.sylabs.io/singularity/"
   url "https://github.com/sylabs/singularity/releases/download/v3.3.0/singularity-3.3.0.tar.gz"
   sha256 "070530a472e7e78492f1f142c8d4b77c64de4626c4973b0589f0d18e1fcf5b4f"
+  revision 1
   # tag "linux"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "d2191946bacd5ff5f095b835edda008cea994e9834e0744aacc43e7de8022295" => :x86_64_linux
   end
 
   depends_on "go" => :build
-  depends_on "openssl" => :build
+  depends_on "openssl@1.1" => :build
   depends_on "libarchive"
   depends_on "pkg-config"
   depends_on "squashfs"
