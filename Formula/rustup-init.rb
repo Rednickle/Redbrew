9925c092f8c97558eef2efccf0c1fa6d65882668
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-19 14:16:43 +0100

    rustup-init: depends_on "openssl@1.1"
    Closes #17443.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/rustup-init.rb
+++ Formula/rustup-init.rb
@@ -2,7 +2,8 @@ class RustupInit < Formula
   desc "The Rust toolchain installer"
   homepage "https://github.com/rust-lang/rustup.rs"
   url "https://github.com/rust-lang/rustup.rs/archive/1.20.2.tar.gz"
-  sha256 "4d47ef7468c615fd10fbb07a16c15754336263d7417ee1685dfbd6e3ec151084"
+  sha256 "28207ee4c2d66840ca903df152b23b916326a5d3eeb643a1de0f24a16afa4209"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
@@ -10,13 +11,12 @@ class RustupInit < Formula
     sha256 "eeae5fd83583db5ccaef4d02e7d4bab357dc3983ba28856240b21a16c7e31fe4" => :catalina
     sha256 "e5e40bdec0a7e7e370d841a1251142d872653bf2b1601a1d81889af21ef6a94f" => :mojave
     sha256 "da5d48864bb15a73c7d9b20cf338fd6840ce3f77c1083598ebeac12f1243e779" => :high_sierra
-    sha256 "df19993e2fc2266c02ded534dbab06cf421c486664f9bbb55a705bc444d99f47" => :x86_64_linux
   end
 
   depends_on "rust" => :build
   unless OS.mac?
     depends_on "curl"
-    depends_on "openssl"
+    depends_on "openssl@1.1"
     depends_on "pkg-config" => :build
   end
 
