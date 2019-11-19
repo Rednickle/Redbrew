913ef5beb81b2ac1437b1c6a975a76b56235a666
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-19 14:28:41 +0100

    mdcat: depends_on "openssl@1.1"
    Closes #17446.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -3,6 +3,7 @@ class Mdcat < Formula
   homepage "https://github.com/lunaryorn/mdcat"
   url "https://github.com/lunaryorn/mdcat/archive/mdcat-0.13.0.tar.gz"
   sha256 "9528a0dedcb9db559c9973001787f474f87559366a2c7a2ff01148c5ab31eac1"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
@@ -10,15 +11,14 @@ class Mdcat < Formula
     sha256 "9d1828626a16a9be233143eff8d8abe7d2257157b929d62c307a5c3557ea0dd8" => :catalina
     sha256 "b7a0aa28deba05d9488d9717ba37a1da23ffff794ff7422332069d24f7731b13" => :mojave
     sha256 "24466bc6fb718df66d2c61b9a53d446e0bace8678b2ebb13fadb7caf4fde4da7" => :high_sierra
-    sha256 "175f0fe964c8561fe8a3eed76cfe310bd1547919e396b9059180b7d511cd0692" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
   depends_on "rust" => :build
+  uses_from_macos "openssl@1.1"
   unless OS.mac?
     depends_on "llvm" => :build
     depends_on "pkg-config" => :build
-    uses_from_macos "openssl"
   end
 
   def install
