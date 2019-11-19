7c64ab4841648b22e1b35778f6b15a02245a65b9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-19 14:42:47 +0100

    docker-compose: update 1.25.0 bottle.
    Closes #17426.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/docker-compose.rb
+++ Formula/docker-compose.rb
@@ -12,6 +12,7 @@ class DockerCompose < Formula
     sha256 "9dac324e835fd2d6b5fd2f0fbcdde899d4278578afe85d07779739ff7fd216d4" => :catalina
     sha256 "a15bc3f65ba481e63bb511026bd1deaddb792e70f5d9ef96509ff89688cf7994" => :mojave
     sha256 "7b7cbb154fcb50ce765cb370bf031d59ea25f50662299d4ae6c5e4d657e2481b" => :high_sierra
+    sha256 "19fc26449b8c6a8461fc84428ff39ca4d014ff7b3746827562e2cd4d36d1b495" => :x86_64_linux
   end
 
   depends_on "libyaml"
