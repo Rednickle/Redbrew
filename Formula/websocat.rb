29dac386a014c915783a6b9faa7d45fd3a04375b
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-19 14:28:26 +0100

    websocat: depends_on "openssl@1.1"
    Closes #17445.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/websocat.rb
+++ Formula/websocat.rb
@@ -3,6 +3,7 @@ class Websocat < Formula
   homepage "https://github.com/vi/websocat"
   url "https://github.com/vi/websocat/archive/v1.5.0.tar.gz"
   sha256 "3ffdb25e937095d95b564eafd805cb7ca141570f95c1296b6dd336d4d016445f"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
@@ -10,12 +11,11 @@ class Websocat < Formula
     sha256 "fd1061d818e394d5678661b0c8559bcd61d586e0a0498e1a58a6a72d271adc33" => :catalina
     sha256 "011b0892734d27b6a978b372fd342bbecccd75e9f29d8d2cf8e9944b30d2ff50" => :mojave
     sha256 "b59d7c3fc70e1e643986c0414b7d8cc4897f611cb15affa21092f43d69ccbab2" => :high_sierra
-    sha256 "637618ca329bb7630da1a0b53c8804b373fd21a649eff9385f1d5655db424075" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
-  uses_from_macos "openssl"
+  uses_from_macos "openssl@1.1"
 
   def install
     system "cargo", "install", "--locked", "--root", prefix,
