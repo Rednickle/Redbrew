a7f9746f7963bfcf49bbe447d2b3c977514703a6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-19 06:28:52 +0000

    qpdf: update 9.1.0 bottle.
    Closes #17418.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/qpdf.rb
+++ Formula/qpdf.rb
@@ -9,6 +9,7 @@ class Qpdf < Formula
     sha256 "142d72298eb8b6be1159b944faad65c04f0036c554c08380c0c2880bd820cf78" => :catalina
     sha256 "729439409ba0644093f521870a9d280703239dd56f261b3613ba1d8847146237" => :mojave
     sha256 "bf691b5ffd8f3075a0fe781f4b566dbca06619a375c39f40e191de7a4785b57b" => :high_sierra
+    sha256 "760d1eb76c0ddbddbfb6908b119e19b29dab295749987235930fd1dfa57f2b72" => :x86_64_linux
   end
 
   depends_on "jpeg"
