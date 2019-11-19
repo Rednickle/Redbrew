632d9cd230c3a425157cef453c436e61e8e74836
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-19 14:10:57 +0000

    mdcat: update 0.13.0_1 bottle.

--- Formula/mdcat.rb
+++ Formula/mdcat.rb
@@ -11,6 +11,7 @@ class Mdcat < Formula
     sha256 "9d1828626a16a9be233143eff8d8abe7d2257157b929d62c307a5c3557ea0dd8" => :catalina
     sha256 "b7a0aa28deba05d9488d9717ba37a1da23ffff794ff7422332069d24f7731b13" => :mojave
     sha256 "24466bc6fb718df66d2c61b9a53d446e0bace8678b2ebb13fadb7caf4fde4da7" => :high_sierra
+    sha256 "7790d4ca34826bd6f74e1735eff09ca981d6b4e53e5c11436fe1481a6ac85046" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
