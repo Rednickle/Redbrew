31778f6c931f26d4e3849c100c40b5617803d8dc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-19 14:43:22 +0100

    amazon-ecs-cli: update 1.18.0 bottle.
    Closes #17425.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/amazon-ecs-cli.rb
+++ Formula/amazon-ecs-cli.rb
@@ -10,6 +10,7 @@ class AmazonEcsCli < Formula
     sha256 "6933d2da6d4c7e83e0f42827b1567a52a992be14a0da20ca99df064b5c82c006" => :catalina
     sha256 "0e697cd142e5e8ba85b69048714f4f3e1e85832dc75122b54287c09b2a5cae4c" => :mojave
     sha256 "60c8f6eb21498f312f8cc76037f9ebb854dc9ef1853a9730038be84401e6c0bc" => :high_sierra
+    sha256 "89b86787778c038cf470d9a8f92181e5b1b05b9f3ec5c73a76008d031b47da84" => :x86_64_linux
   end
 
   depends_on "go" => :build
