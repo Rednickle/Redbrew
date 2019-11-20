d6c813ff92c985faf028164266e1b55d49d8d752
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-20 20:19:46 +0000

    berglas: update 0.3.0 bottle.
    Closes #17473.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/berglas.rb
+++ Formula/berglas.rb
@@ -9,6 +9,7 @@ class Berglas < Formula
     sha256 "5f347e69919162e5abd4ee3f2e614527e1faa3cdcf2fe915e11b921b9c8a28b8" => :catalina
     sha256 "8ee48f0d0a24874e5e9f7f3244875612a2f68de5e226dac7f90a1dfdff3fb425" => :mojave
     sha256 "3ac1a5de0e26b07541ff87c24ffb62e1f04e5d5b1141d112ec8040569727c3ff" => :high_sierra
+    sha256 "e018a6350f411944beb967c705a5b239f8efb2177d636b6533a0b10d9ecbb931" => :x86_64_linux
   end
 
   depends_on "go" => :build
