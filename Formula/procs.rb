10ed0cc1add63b0a2cb72b307176850370bfdba4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-21 23:23:06 +0100

    procs: update 0.8.14 bottle.
    Closes #17527.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -9,6 +9,7 @@ class Procs < Formula
     sha256 "67f080e39c4698911fe4c9c90a514a627f1e94a7e2dae54c85bba728a344647c" => :catalina
     sha256 "9fe4865bba5c79e933eaf3e43353f39c7571ac6875e9abb80cea25cf5a40ac37" => :mojave
     sha256 "b6442f29ec0bad4589f91ba36e31103b1aff3829dc1ba8adda594d7d6debe6cc" => :high_sierra
+    sha256 "2e4457c4e3adfc825afb219352cb9b44366f6a312ac2b1a9c1bef4f45a401754" => :x86_64_linux
   end
 
   depends_on "rust" => :build
