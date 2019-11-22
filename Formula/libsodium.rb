6be0f5a66be771fb4ce2e4073c695ff31c324b4b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-22 15:36:46 +0100

    libsodium: update 1.0.18_1 bottle.
    Closes #17540.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/libsodium.rb
+++ Formula/libsodium.rb
@@ -10,6 +10,7 @@ class Libsodium < Formula
     sha256 "db372521cd0b1861a5b578bee22426f3a1f4f7cb3c382be1f842da4715dc65bd" => :catalina
     sha256 "55245bfcf6654b0914d3f7459b99a08c54ef2560587bf583a1c1aff4cfc81f28" => :mojave
     sha256 "fc972755eb60f4221d7b32e58fc0f94e99b913fefefc84c4c76dc4bca1c5c445" => :high_sierra
+    sha256 "f7a0d2d788866c83dfe70624ea4e990a1c9a055f89d07ab3556dafb14e1d5931" => :x86_64_linux
   end
 
   head do
