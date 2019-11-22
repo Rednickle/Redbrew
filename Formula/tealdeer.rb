f53351199df87f6f49743cc7c4f27988d89d7207
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-22 18:51:33 +0000

    tealdeer: update 1.2.0 bottle.

--- Formula/tealdeer.rb
+++ Formula/tealdeer.rb
@@ -10,6 +10,7 @@ class Tealdeer < Formula
     sha256 "778935472a63f1b04b68597158467aa4ccf4633592aa6b9a781634bd4774c877" => :catalina
     sha256 "1c8932dac2f578a4b64d472a6fce712309c998b05128a77e6fb4be4353112bc5" => :mojave
     sha256 "b9c9181ba84b47b2dadaf49bfc9e8726273a071615e8229ed646181d4cbe5a49" => :high_sierra
+    sha256 "63547f5bd7281a03955992c9a34f14dd8bd6ad34a733a13c47c7219766278692" => :x86_64_linux
   end
 
   depends_on "rust" => :build
