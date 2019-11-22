92cc3bea3302409c06a8ca1ce36339f8909e1872
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-22 10:58:24 +0000

    libgraphqlparser: update 0.7.0_1 bottle.

--- Formula/libgraphqlparser.rb
+++ Formula/libgraphqlparser.rb
@@ -11,6 +11,7 @@ class Libgraphqlparser < Formula
     sha256 "e4cea535715f0ed46ff1713df73dbc43d2845f4e0152f0137d4ad18def845050" => :catalina
     sha256 "f2d46a3bfb5fd3aef9f8b47a5d1c50d204f6dbd74d1a387ca664e36022b7ddc4" => :mojave
     sha256 "64779ec3108d9eef789d279abfafa90437c6a76b2ed3973d45979cd1051dc170" => :high_sierra
+    sha256 "451370cc5e18a9420015806109c94f492a787c2743136ca6f1dab4a84739f532" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
