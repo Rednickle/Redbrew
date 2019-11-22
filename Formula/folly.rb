2ff652326eb77e23d566b72b3fa449315093edeb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-22 15:34:49 +0000

    folly: update 2019.11.11.00_1 bottle.

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -11,6 +11,7 @@ class Folly < Formula
     sha256 "a31ad6656a7c29184694594bbfb36bd01c5e6a2d92122db4a6ae7f1cefe2007f" => :catalina
     sha256 "28de37ae5613f0da104d696bb67ae801d7d741c6d6f9594cec82388b150b3ceb" => :mojave
     sha256 "4343e3782e251854eae23d5c5eb354eafc13ee9422e7845760ae357c4c5ec382" => :high_sierra
+    sha256 "0b19ff03409d45d5f30b108119e39bfe73cb6ac23b6d172ba4db13d95fae54ca" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
