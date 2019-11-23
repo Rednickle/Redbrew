0864c849b0b1f81c3407b32818f3eed432f7036d
Michka Popoff <michkapopoff@gmail.com>
2019-11-23 10:09:17 +0100

    scipy: Fix build for Linux
    Closes #17583.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/scipy.rb
+++ Formula/scipy.rb
@@ -6,7 +6,6 @@ class Scipy < Formula
   head "https://github.com/scipy/scipy.git"
 
   bottle do
-    cellar :any
     sha256 "90c66bb5eb57a751896a07b8a44fcaa9bd7440e5aa073b5c96a1df2e04e88894" => :catalina
     sha256 "125d98ba98493a26d7e1f0466f941de4fd7a295af34ff628a6e8b623d7d5aba3" => :mojave
     sha256 "a7dc8831f6566a55aa266cc20b6653015bf29a18d68f315ca0f001bd39b61480" => :high_sierra
@@ -23,7 +22,8 @@ class Scipy < Formula
   def install
     openblas = Formula["openblas"].opt_prefix
     ENV["ATLAS"] = "None" # avoid linking against Accelerate.framework
-    ENV["BLAS"] = ENV["LAPACK"] = "#{openblas}/lib/libopenblas.dylib"
+    dylib = OS.mac? ? "dylib" : "so"
+    ENV["BLAS"] = ENV["LAPACK"] = "#{openblas}/lib/libopenblas.#{dylib}"
 
     config = <<~EOS
       [DEFAULT]
@@ -40,7 +40,7 @@ class Scipy < Formula
     version = Language::Python.major_minor_version "python3"
     ENV["PYTHONPATH"] = Formula["numpy"].opt_lib/"python#{version}/site-packages"
     ENV.prepend_create_path "PYTHONPATH", lib/"python#{version}/site-packages"
-    system "python3", "setup.py", "build", "--fcompiler=gnu95"
+    system "python3", "setup.py", "build", (OS.mac? ? "--fcompiler=gnu95" : "--fcompiler=gfortran")
     system "python3", *Language::Python.setup_install_args(prefix)
   end
 
