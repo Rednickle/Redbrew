ea394d453982bffd19062e60ede6063b1efae80a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-23 13:45:01 +0000

    php@7.2: update 7.2.25_1 bottle.

--- Formula/php@7.2.rb
+++ Formula/php@7.2.rb
@@ -9,6 +9,7 @@ class PhpAT72 < Formula
     sha256 "a951eaf1cc000be90aa3f8d212898c7374f889711d29812392dbb087d8177664" => :catalina
     sha256 "eed6eb7d281b72d7abc3f42a58d3257101c00633d9e7a057957628165ac1c553" => :mojave
     sha256 "1414b7e45bd689e56dfb1b83294185577b46196121966b14a3981614bd0acc48" => :high_sierra
+    sha256 "eb37da6517c662149bd4104c70aee509b43315375b2b10d78d74bc5403bc88f3" => :x86_64_linux
   end
 
   keg_only :versioned_formula
