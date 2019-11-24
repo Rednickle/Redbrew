e31c26ee4bdd17a7c35281d905dc370aa363acb0
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-24 15:12:11 +0100

    cpio 2.13
    Closes #17625.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cpio.rb
+++ Formula/cpio.rb
@@ -1,13 +1,12 @@
 class Cpio < Formula
   desc "Copies files into or out of a cpio or tar archive"
   homepage "https://www.gnu.org/software/cpio/"
-  url "https://ftp.gnu.org/gnu/cpio/cpio-2.12.tar.bz2"
-  mirror "https://ftpmirror.gnu.org/cpio/cpio-2.12.tar.bz2"
-  sha256 "70998c5816ace8407c8b101c9ba1ffd3ebbecba1f5031046893307580ec1296e"
+  url "https://ftp.gnu.org/gnu/cpio/cpio-2.13.tar.bz2"
+  mirror "https://ftpmirror.gnu.org/cpio/cpio-2.13.tar.bz2"
+  sha256 "eab5bdc5ae1df285c59f2a4f140a98fc33678a0bf61bdba67d9436ae26b46f6d"
   # tag "linux"
 
   bottle do
-    sha256 "2e8018465c5abc4e3a825653530ae4dd8f1c06219bf6aceac4e8a74fd4e69cbe" => :x86_64_linux # glibc 2.19
   end
 
   def install
