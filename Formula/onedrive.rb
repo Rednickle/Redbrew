960de0c1317ef274e51e8d654cd0da6fae08e054
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-24 15:04:32 +0100

    onedrive 2.3.11
    Closes #17619.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/onedrive.rb
+++ Formula/onedrive.rb
@@ -1,22 +1,21 @@
 class Onedrive < Formula
   desc "Folder synchronization with OneDrive"
   homepage "https://github.com/abraunegg/onedrive"
-  url "https://github.com/abraunegg/onedrive/archive/v2.3.8.tar.gz"
-  sha256 "4268f54835592ef8d2d42bd086877bb403da3dbdda485175893bf73b3a561028"
+  url "https://github.com/abraunegg/onedrive/archive/v2.3.11.tar.gz"
+  sha256 "fff9b77612e02e38e5406e5de93e43ab2288daa59643bf2df25ae8aa74add5c3"
   # tag "linux"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "690d7e3090cc3ae0a1f5b1dab1bae31755c152a6d58e7cb493db734afc41c7aa" => :x86_64_linux
   end
 
-  depends_on "ldc" => :build
+  depends_on "dmd" => :build
   depends_on "pkg-config" => :build
   depends_on "curl-openssl"
   depends_on "sqlite"
 
   def install
-    ENV["DC"] = "ldc2"
+    ENV["DC"] = "dmd"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
     bash_completion.install "contrib/completions/complete.bash"
