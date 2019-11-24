7a38a512d90fae0046103ced23f9c9e21d0ed2de
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-24 15:11:20 +0100

    libseccomp 2.4.2
    Closes #17624.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libseccomp.rb
+++ Formula/libseccomp.rb
@@ -1,9 +1,12 @@
 class Libseccomp < Formula
   desc "Interface to the Linux Kernel's syscall filtering mechanism"
   homepage "https://github.com/seccomp/libseccomp"
-  url "https://github.com/seccomp/libseccomp/releases/download/v2.4.1/libseccomp-2.4.1.tar.gz"
-  sha256 "1ca3735249af66a1b2f762fe6e710fcc294ad7185f1cc961e5bd83f9988006e8"
+  url "https://github.com/seccomp/libseccomp/releases/download/v2.4.2/libseccomp-2.4.2.tar.gz"
+  sha256 "b54f27b53884caacc932e75e6b44304ac83586e2abe7a83eca6daecc5440585b"
   # tag "linux"
+
+  bottle do
+  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
