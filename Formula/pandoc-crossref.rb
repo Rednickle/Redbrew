d843da97bf06f056e3f6aaa89043aecfa5b8bff1
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-24 22:44:12 +0100

    pandoc-crossref: fix for Linux
    Closes #17640.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pandoc-crossref.rb
+++ Formula/pandoc-crossref.rb
@@ -9,7 +9,6 @@ class PandocCrossref < Formula
   sha256 "646ea9b0d1564f894528036724d7a112d54e6946555602cd15c421b48fc301f4"
 
   bottle do
-    cellar :any_skip_relocation
     sha256 "2da24328a336390ba205bfc232a0cefbe93af52c1298d4be5c8819fe7bf1d9e2" => :catalina
     sha256 "4d0cee81d5f7b0c8c3e1c83ca8c03de0ea2374c35e6d2dbd7b5af886db3c61f8" => :mojave
     sha256 "a5ca3bca940585a151ec92280162da834f120d99e486fe6741bec69aa8422c6f" => :high_sierra
