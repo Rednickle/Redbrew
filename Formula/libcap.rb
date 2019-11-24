0bcde9a15e6ebb4d5dcde976c7da8400e9e3f3a6
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-24 15:18:41 +0100

    libcap 2.27
    Closes #17628.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libcap.rb
+++ Formula/libcap.rb
@@ -1,13 +1,12 @@
 class Libcap < Formula
   desc "User-space interfaces to POSIX 1003.1e capabilities"
   homepage "https://sites.google.com/site/fullycapable/"
-  url "https://www.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.25.tar.xz"
-  sha256 "693c8ac51e983ee678205571ef272439d83afe62dd8e424ea14ad9790bc35162"
+  url "https://mirrors.edge.kernel.org/pub/linux/libs/security/linux-privs/libcap2/libcap-2.27.tar.xz"
+  sha256 "dac1792d0118bee6aae6ba7fb93ff1602c6a9bda812fd63916eee1435b9c486a"
   # tag "linux"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "3892f036977db5de038b2379203c12ccae9beb21841dadbb36c44c047954301c" => :x86_64_linux # glibc 2.19
   end
 
   def install
