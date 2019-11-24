80ba58dbd568351128d58254d2f50049061fb3b0
Dawid Dziurla <dawidd0811@gmail.com>
2019-11-24 15:05:50 +0100

    strace 5.3
    Closes #17620.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/strace.rb
+++ Formula/strace.rb
@@ -1,13 +1,12 @@
 class Strace < Formula
   desc "Diagnostic, instructional, and debugging tool for the Linux kernel"
   homepage "https://strace.io/"
-  url "https://github.com/strace/strace/releases/download/v5.2/strace-5.2.tar.xz"
-  sha256 "d513bc085609a9afd64faf2ce71deb95b96faf46cd7bc86048bc655e4e4c24d2"
+  url "https://github.com/strace/strace/releases/download/v5.3/strace-5.3.tar.xz"
+  sha256 "6c131198749656401fe3efd6b4b16a07ea867e8f530867ceae8930bbc937a047"
   # tag "linux"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "0784954f127ecf884dcdc9fbb148b8c808097c06b322ba629fc37dc3e4639b9c" => :x86_64_linux
   end
 
   head do
