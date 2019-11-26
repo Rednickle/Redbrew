fd82d2f28bcf550837fc7c5deca240f383e0fc9d
Michka Popoff <michkapopoff@gmail.com>
2019-11-26 07:32:44 +0100

    pianod: depend on ffmpeg for libavcodec
    Closes #17665.
    
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/pianod.rb
+++ Formula/pianod.rb
@@ -5,7 +5,6 @@ class Pianod < Formula
   sha256 "d6fa01d786af65fe3b4e6f4f97fa048db6619b9443e23f655d3ea8ab4766caee"
 
   bottle do
-    cellar :any
     sha256 "6698a4353e50aed190386ccdf407af24f6b623691ebabdef7f8a0b7dca77b338" => :catalina
     sha256 "c0dfa015b5c5546a2fc4788d716c00520282773a93e7d6669f891f2b1126f587" => :mojave
     sha256 "d7c6ba07d0b46d393c4dc5de718e4dcd3130d6b0f8f2ee5aa5d00efd8ec69e93" => :high_sierra
@@ -15,6 +14,7 @@ class Pianod < Formula
   depends_on "json-c"
   depends_on "libao"
   depends_on "libgcrypt"
+  depends_on "ffmpeg" unless OS.mac?
 
   def install
     ENV["OBJCXXFLAGS"] = "-std=c++11"
