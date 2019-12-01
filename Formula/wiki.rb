eb91810b8f6342b262ba2e76ec46342355199631
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-01 13:14:08 +0000

    wiki: update 1.4.1 bottle.
    Closes #17792.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/wiki.rb
+++ Formula/wiki.rb
@@ -9,6 +9,7 @@ class Wiki < Formula
     sha256 "ff424f6afbc0d2baab91cee289157d9c90623fa19b7d51574b75df455da76cd6" => :catalina
     sha256 "316687b381ca23ee0e81eb6e396d2c8c21a5eeaf05a9219ec56dd0024a8d9722" => :mojave
     sha256 "bd1b52730bbf5bc503d3fece003b069e248261616d9d02767ef019d87659bdd8" => :high_sierra
+    sha256 "f9d508afc498cbebb8ba71e06b71a21d467c85a62fce277ac85db40395edee2e" => :x86_64_linux
   end
 
   depends_on "go" => :build
