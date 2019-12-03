832f2fab9fca83a060df5313e6cc7e194f30866e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-03 19:41:27 +0000

    oniguruma: update 6.9.4 bottle.
    Closes #17822.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/oniguruma.rb
+++ Formula/oniguruma.rb
@@ -10,6 +10,7 @@ class Oniguruma < Formula
     sha256 "05e25bc53db0cf338a7f765da76c66260972d6b4e259be415bc17807a8b60fe9" => :catalina
     sha256 "ab2bb92e40e17569c54dda0ed3b3a0fc6f98be761107fba918754af75817de6f" => :mojave
     sha256 "1a97b801983a8929de40bcc829d50e3f37413dbb7076370e203deff76dad4357" => :high_sierra
+    sha256 "c62e5991b68157abdbf3e969e6372e4fd01e3dbf6d9ed701d58b1f23ba9d2420" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
