82466f6ec12ffa0938d7623827790c5ee4dbf1db
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-04 15:14:45 +0100

    contentful-cli: update 1.2.1 bottle.
    Closes #17861.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/contentful-cli.rb
+++ Formula/contentful-cli.rb
@@ -12,6 +12,7 @@ class ContentfulCli < Formula
     sha256 "f098e43e67bea0f5c6615651f8e9ee9035b77d9d43b7965a408da96545ab9297" => :catalina
     sha256 "c0b8b2e3c74401016d593f6507fabdf6ad76e4cf44600451478acd8053e2dd41" => :mojave
     sha256 "bf72c13e10d50dcd4969a582a570603ce5f2134ec49a93f7477871d99cc38b24" => :high_sierra
+    sha256 "fb48e06b92571841781192baddc95543f4f6acb2ae7131a581d154b0a9e20a90" => :x86_64_linux
   end
 
   depends_on "node"
