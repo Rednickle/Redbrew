6cace6ee988f406a41c0c76b817585fdf7972894
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-05 17:12:02 +0100

    nnn: update 2.8.1 bottle.
    Closes #17880.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/nnn.rb
+++ Formula/nnn.rb
@@ -10,6 +10,7 @@ class Nnn < Formula
     sha256 "ceaa1d0df4e726017c45a50c07cb463338aebedb7ef078aaf02df7a9ba559fba" => :catalina
     sha256 "05fee70609df45e3f9ebc99df751bbe20eb373691caeb4d1e89c1d2c0c24812f" => :mojave
     sha256 "b4700186efd6ab8984bd788100b7b5e78702a7ab2bea50cf1177499ba752c191" => :high_sierra
+    sha256 "5fee312a74f8ed70e1d0876010bcdae745ab65ac38d6d3dfed02b0c6b18fb97f" => :x86_64_linux
   end
 
   depends_on "readline"
