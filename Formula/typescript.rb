e96ba8e64a2439b1805d92b28e97fb94727d9996
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-05 17:11:35 +0100

    typescript: update 3.7.3 bottle.
    Closes #17881.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -12,6 +12,7 @@ class Typescript < Formula
     sha256 "b7a17906ffc0cb58e46c070d6c7caf9dcae8af40ff593f3dfcdf7fa2ebb5438d" => :catalina
     sha256 "7d8702cfb661fa12f00d0886ac65712ee4672d46b192fb7f528988b4f665f1b6" => :mojave
     sha256 "3187a2112e800b0d0bacadc6e6e0e30e814f89e2bbcd0e29a9eb5328f1e24b11" => :high_sierra
+    sha256 "56e3d45e2dec1ab3af7187123899fe4206aa5bebac8a5773ffb2cfc45cad7e35" => :x86_64_linux
   end
 
   depends_on "node"
