b92299208a47592c7040662b554d6b41f7368624
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-05 11:22:49 +0000

    terragrunt: update 0.21.7 bottle.
    Closes #17870.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -10,6 +10,7 @@ class Terragrunt < Formula
     sha256 "b844b8023db001c52de749b09961d3e36cf2ddf7254178dedfff87b3c1e4d0a2" => :catalina
     sha256 "412a0f2f0723126c06ac81682575a0ede4e1343dba5c39e4711567b2a890a1ca" => :mojave
     sha256 "7634bd2928f46b6305bd35d05f7be453ee248f0cd8d9ffb573962137eb6b3682" => :high_sierra
+    sha256 "08b84e2cf352e1ebdab9a19ce458fd2a3765bf208115435d10dd5ff290f7937a" => :x86_64_linux
   end
 
   depends_on "dep" => :build
