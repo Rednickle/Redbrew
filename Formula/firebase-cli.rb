6fb48c2b408515315ccd0b83dad792be1668cdd8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-05 11:20:03 +0000

    firebase-cli: update 7.9.0 bottle.
    Closes #17876.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/firebase-cli.rb
+++ Formula/firebase-cli.rb
@@ -12,6 +12,7 @@ class FirebaseCli < Formula
     sha256 "cd270cd930992d77be077cef0a02c5dbb676f64d2a45e9d10834a76b0af641d8" => :catalina
     sha256 "67baa21abdfd785b0024df7fb5051fad40a26016abb93e78b06fa704e661b6b0" => :mojave
     sha256 "973116803d42f7ab827bbd2a30fbfd2a5bdf3fe8d2a5f851206a81847fb91372" => :high_sierra
+    sha256 "982e9a693d7fc6cacd883f13d794c4688c112691507175658fc0effea00b139d" => :x86_64_linux
   end
 
   depends_on "node"
