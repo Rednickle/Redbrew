46cdb115926a99e6cb1bc7ec020747b5e422a391
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-06 16:33:39 +0100

    git-delta: update 0.0.15 bottle.
    Closes #17900.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/git-delta.rb
+++ Formula/git-delta.rb
@@ -10,6 +10,7 @@ class GitDelta < Formula
     sha256 "30fce1792a9f663a036fc17517ff81dc7da6f6d0849d27f1e341ed402c0799d7" => :catalina
     sha256 "035f3a35e619f7f8b4248b07b9b3edd14df8246d2fbb6c0f8e56522bd9b76a75" => :mojave
     sha256 "12a7db2b8a6fbef2322133e884cf0ec9ab188827f668ca5385529c51780e1707" => :high_sierra
+    sha256 "f102e18671a0c335a8f9ed7310f77b01deae2090578229a75ae9b15a59180c00" => :x86_64_linux
   end
 
   depends_on "rust" => :build
