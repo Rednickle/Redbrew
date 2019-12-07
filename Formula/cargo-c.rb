027e9b686c493a3ffb6698d6486def2118979217
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-07 19:07:44 +0100

    cargo-c: update 0.4.0 bottle.
    Closes #17936.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/cargo-c.rb
+++ Formula/cargo-c.rb
@@ -9,6 +9,7 @@ class CargoC < Formula
     sha256 "5778730dfaa610a4bfc26b4d83eb3534842ff67e7c3799723ffe30f004d4e7d0" => :catalina
     sha256 "0f74fea38935d02e0245d816d5f7d1f7596f484d295099ba01c7de2140fbe5f8" => :mojave
     sha256 "3f464dcf67dc234aebdaee4c43f221453122f366cafaad7ca53deb0764dbe3de" => :high_sierra
+    sha256 "51a8891cdfcae7521ff2a4581cc1477258efcc9797a5d5e184efc630fbf6358f" => :x86_64_linux
   end
 
   depends_on "rust" => [:build, :test]
