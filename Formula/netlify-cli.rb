11ca6495878f329d43c6cc894fd66509615f01b2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-07 18:11:21 +0100

    netlify-cli: update 2.24.0 bottle.
    Closes #17932.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/netlify-cli.rb
+++ Formula/netlify-cli.rb
@@ -12,6 +12,7 @@ class NetlifyCli < Formula
     sha256 "ae7aacf3a091cb4e7886b3530c1d308a2c1997079066332e9273ec5146df51b0" => :catalina
     sha256 "f5a07e5d51edbad89ff3074713e1500441cf847717e9e289f8eb0a164be85e1b" => :mojave
     sha256 "580e88707509ae8b758db02666d006e266aeaa1b66d6dc8bddc79c9e754a1df8" => :high_sierra
+    sha256 "e6d0a7f9bdb388346ac4e5f4f775e26715e60c98899efb05b69c01588e7b3371" => :x86_64_linux
   end
 
   depends_on "node"
