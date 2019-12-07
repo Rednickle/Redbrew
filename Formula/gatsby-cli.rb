190847a2f182b17dda28d47873cf428b80268b20
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-07 12:17:14 +0000

    gatsby-cli: update 2.8.16 bottle.
    Closes #17921.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gatsby-cli.rb
+++ Formula/gatsby-cli.rb
@@ -11,6 +11,7 @@ class GatsbyCli < Formula
     sha256 "ba6cab8a7f73e25ccbee7903c22bc9824ab7c2127aadddce7a641292b84d139b" => :catalina
     sha256 "151793270378da68b910408a789548536c892a5b7f9e673da5d2ba853aaba926" => :mojave
     sha256 "2734c7ffc061b8f9ef8244cc33acd6bf315757c7db8643e4270550c07de1229a" => :high_sierra
+    sha256 "d57c87d2316076c754502cb4d41a81318bdcf8cc2ed8fdb39ca52506c71f4207" => :x86_64_linux
   end
 
   depends_on "node"
