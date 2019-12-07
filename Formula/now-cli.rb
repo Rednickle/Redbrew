f64f36d0ce372e7ea6f5c94d008e413c63c49e97
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-07 08:14:12 +0000

    now-cli: update 16.6.1 bottle.
    Closes #17908.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/now-cli.rb
+++ Formula/now-cli.rb
@@ -11,6 +11,7 @@ class NowCli < Formula
     sha256 "10f66f09f3f8edcbb7d06a62a37c2cc166bc9b98bbd99058866b0e4214616a70" => :catalina
     sha256 "54ac2f38dec1cdfd5d2de1e7f8d917835499ff480a12ab4dc611298ac331392b" => :mojave
     sha256 "58cf06c353761e34e727fe32174c4ec59805bafa77627d544242d319dd35e44e" => :high_sierra
+    sha256 "edf43fb582fa2266dfa63a78319b4c906e5f12fc753eec0e3cbbc560d263181c" => :x86_64_linux
   end
 
   depends_on "node"
