28be242f3210dc854bb1cc19b0dee63ce8e3c00f
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-09 17:29:57 +0100

    dhall-json: remove cellar
    Closes #17953.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/dhall-json.rb
+++ Formula/dhall-json.rb
@@ -10,7 +10,6 @@ class DhallJson < Formula
   head "https://github.com/dhall-lang/dhall-haskell.git"
 
   bottle do
-    cellar :any_skip_relocation
     sha256 "1cc81f1c9f6a97d9ac4a5539b9c8c0c71c318c247253b34dba98eaf7031bd93f" => :catalina
     sha256 "76daf58beb302c6dfb451015dacbfddea53e44807e0708acce70f89a17d65771" => :mojave
     sha256 "ec70a72b710b3351d999943fc6dc3d2e0727dee5d22cc4002cbe2714dbe110a5" => :high_sierra
