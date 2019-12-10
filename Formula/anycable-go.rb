54bddebf3e88bb9ff4ec7b677f6818ec02f6d28a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-10 19:55:57 +0100

    anycable-go: update 0.6.4 bottle.
    Closes #17983.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/anycable-go.rb
+++ Formula/anycable-go.rb
@@ -10,6 +10,7 @@ class AnycableGo < Formula
     sha256 "7b15a2340fa1fb7ecf97079a1cedbdef0c1f1cf7379f847e70b3f44f08b141e0" => :catalina
     sha256 "12e069a770ebdd191bc1d6ffbe5de94242e5d54c1e4d8b46600d133fc4d92871" => :mojave
     sha256 "0ca4a9b558458aa15fbf81ff5aeabff78102fb6031a5c8295b65c2d9f6c558df" => :high_sierra
+    sha256 "4e32b9f241f8db2bafa244ce521d3a93d6673d4c82cf41bed84234caf44cb0e5" => :x86_64_linux
   end
 
   depends_on "go" => :build
