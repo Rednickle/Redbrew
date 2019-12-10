7926efb4c3b3b1aff55eeeb1733128c51af7feb9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-10 14:15:55 +0000

    bumpversion: update 0.5.11 bottle.

--- Formula/bumpversion.rb
+++ Formula/bumpversion.rb
@@ -13,6 +13,7 @@ class Bumpversion < Formula
     sha256 "12e99c24dbc2104191bfe9b21d19ce955e13fb456ab0cb3eb7f2bab9d77e2e4d" => :catalina
     sha256 "b11228119eac36538c4fcaaca83fdb83c516ea43391f9291e6935ef66db8b966" => :mojave
     sha256 "d7873c668cb0394e15652059d67466faae74a422cf3f1da275782f31b3c492e5" => :high_sierra
+    sha256 "79a611595b30a955958e04be1b2dcd982365687a5bc82e576f843bd08a11928a" => :x86_64_linux
   end
 
   depends_on "python"
