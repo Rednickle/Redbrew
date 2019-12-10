cc2906e5769a0b7e61de9abdf769c53070b40c83
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-10 19:54:06 +0100

    aurora: update 2.2 bottle.
    Closes #17986.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/aurora.rb
+++ Formula/aurora.rb
@@ -10,6 +10,7 @@ class Aurora < Formula
     sha256 "f3b45006b5b5c6f15166d11d1a740fb14f3b22c1d64b3b64397ed2958e9c882d" => :catalina
     sha256 "21abebb582fbac2ebb400328b455c890206f78ae0910f75ded8019bfc6a40c1f" => :mojave
     sha256 "e3e9b06b4b9053afb4b75b48d90555d00fcc8404309d8b2b2b336538810746cb" => :high_sierra
+    sha256 "35bc4d2607dbf232b1e8e0e9c6bc14acaa9a5937d522c3fc21fb420dc1a7ed62" => :x86_64_linux
   end
 
   depends_on "go" => :build
