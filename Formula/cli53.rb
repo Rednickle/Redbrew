2c34997c267c3f80df1ea639d86bd2e43a42659d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-10 21:14:52 +0000

    cli53: update 0.8.16 bottle.
    Closes #17998.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cli53.rb
+++ Formula/cli53.rb
@@ -10,6 +10,7 @@ class Cli53 < Formula
     sha256 "1dc8fc0a54263784bfed7d0ff6802ef66ef753d276410be52f49620b1bde89ad" => :catalina
     sha256 "9d4dc2fce304be814b276f23415c736a426011642609c6625a45181b3a894857" => :mojave
     sha256 "a9cdffb7125145c1d86e6a9bb3f75049254a66168ef41387cf25d98a253d0756" => :high_sierra
+    sha256 "594f9ae5642cd605ee071600c8a04834843c995f14439c7048f4e3a41a33e46e" => :x86_64_linux
   end
 
   depends_on "go" => :build
