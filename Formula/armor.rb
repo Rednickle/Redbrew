30bda1b7d1c0b2c0b3e51e0f05ce977ac07e8df8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-11 09:19:48 +0000

    armor: update 0.4.14 bottle.

--- Formula/armor.rb
+++ Formula/armor.rb
@@ -11,6 +11,7 @@ class Armor < Formula
     sha256 "d0bbf39148c0dabb28f777b951492814a708dc64610106587b1315fcd6a08559" => :catalina
     sha256 "538f2c340ec151aa7c22847a61d3c8e1d255d121a2b2a75fe2fe7d22f5067347" => :mojave
     sha256 "8fc3b2ebb6d8bc978f6dd04c92e2a43573b052e51d69398deb4f5a2b04e0f87d" => :high_sierra
+    sha256 "22317bcb46b542191211393d8c0f460b142991b9b779c0d9c98c0d6b71a8dc3d" => :x86_64_linux
   end
 
   depends_on "go" => :build
