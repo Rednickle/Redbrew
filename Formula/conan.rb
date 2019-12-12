c0160b374bed7be04e62facc6ded0744eb721a58
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-12 16:32:32 +0100

    conan: update 1.21.0 bottle.
    Closes #18025.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/conan.rb
+++ Formula/conan.rb
@@ -12,6 +12,7 @@ class Conan < Formula
     sha256 "9710d9a0a44f6a1359deb277d19c4276e211a0eed295058ca55517dd8d1d740b" => :catalina
     sha256 "c66c07b3d730521b91f712b961449581ccc2583e18f5ebb1e75f9b9e5c9db8bf" => :mojave
     sha256 "e6566da34df4fee485ef80fbcbc94a2238ad2a10ff0e6bd89de655ade3cf14a8" => :high_sierra
+    sha256 "8bd3679c1c3072a253376b1e4c1c01c48a07354f135d74a6b53f70b31eab3098" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
