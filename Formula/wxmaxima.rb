aa541e0a5f1fa5e5791cb6768aec534cb419254d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-13 06:26:58 +0000

    wxmaxima: update 19.12.1 bottle.
    Closes #18071.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/wxmaxima.rb
+++ Formula/wxmaxima.rb
@@ -10,6 +10,7 @@ class Wxmaxima < Formula
     sha256 "713e5a4c38c8df73e4494356c9a7fde6f1bedfcaad3029ea729af3387ff43c2e" => :catalina
     sha256 "9a4c9089f8a13451b76a9e310ddc486e9fe75bbb699b79b7ec11ddc30979fbed" => :mojave
     sha256 "6bc73da6650a0806a29b93136957a0d1814aaada9a9d1f2807a9fc81520f9134" => :high_sierra
+    sha256 "07b16b76d7b24737873ba2f16ebf44e03cf6eebce8bb14d1de567dee43e4bd13" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
