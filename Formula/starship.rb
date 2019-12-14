4dadb73ea359b031dcf881c23c3ba6a313c278b8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-14 22:04:52 +0100

    starship: update 0.30.1 bottle.
    Closes #18106.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/starship.rb
+++ Formula/starship.rb
@@ -10,6 +10,7 @@ class Starship < Formula
     sha256 "5cb27bf76973bcb68e84c941b76b92e27d5e1521990b5cf6ce8f62179029cdcd" => :catalina
     sha256 "80f9703982e2d5e8f46b98b9e9d39635a19a2ae9fb21f00a42e4d1eb797b6414" => :mojave
     sha256 "ea97014294507ee73972f3eb8b27b7151adc98a3d453456dbf4029234e9543a2" => :high_sierra
+    sha256 "59952397de2e5d2db69b71ba87ecee5671be3a630c4e1c6f3973655731e9f7ce" => :x86_64_linux
   end
 
   depends_on "rust" => :build
