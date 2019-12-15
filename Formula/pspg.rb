0e0122e9fb012608c9190464d17919fb2717136b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-15 09:02:33 +0000

    pspg: update 2.6.1 bottle.
    Closes #18122.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pspg.rb
+++ Formula/pspg.rb
@@ -10,6 +10,7 @@ class Pspg < Formula
     sha256 "9a4790e9a52ebc240b126bfffffb5de551af189ae3b70556976b47fe82a83111" => :catalina
     sha256 "1ec39b50bd344692740069f60f009fa8b6a4d032c7298db4d2f2168435e0fbd4" => :mojave
     sha256 "80ebbbf3457b508426cab75935129a36b979edbca03f5e5e2aaa3f98ae854146" => :high_sierra
+    sha256 "6f122801170e7e6459fc776fb27262a116aad4f89dd3b73679ea155336cd0c6c" => :x86_64_linux
   end
 
   depends_on "libpq"
