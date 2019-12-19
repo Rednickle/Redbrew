1d92d6dc457964a6f1cbc8550c9e922c2870ec2b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-19 18:52:17 +0100

    tflint: update 0.13.3 bottle.
    Closes #18223.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/tflint.rb
+++ Formula/tflint.rb
@@ -11,6 +11,7 @@ class Tflint < Formula
     sha256 "c376b79e46214519b33a540f191fca4301b8a4bbb6762f334414529c54ccf051" => :catalina
     sha256 "895c6119330b168d74e2aa0773fba528182e9a1b052dfa40ccfd4a600b62f6e4" => :mojave
     sha256 "6540cdaa0b282abb6b4e209e835dbf44d219cc0fd54966e5594d0e90f2933b40" => :high_sierra
+    sha256 "5a337bc1820fe12d78fc68339254a75f4bb58a1b8b6bf0f0ab6af60a68e8fb24" => :x86_64_linux
   end
 
   depends_on "go" => :build
