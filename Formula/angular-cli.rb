e7bf781696c124a5e12980bd2577f8ab96e97228
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-21 12:20:25 +0000

    angular-cli: update 8.3.21 bottle.
    Closes #18237.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/angular-cli.rb
+++ Formula/angular-cli.rb
@@ -11,6 +11,7 @@ class AngularCli < Formula
     sha256 "0f2d9314c9886aeba258bf3059994e8f05c970d0665f86e69c4614dec46f8674" => :catalina
     sha256 "58c2d26ad2c7149a7b5191c6a9318bdf0d7e376d72beb6c4c9cbc61a3315131a" => :mojave
     sha256 "88d06f691920c79f5c7910d8f536b2bc5512f949e1819eacf45944e2bca4e5d6" => :high_sierra
+    sha256 "35ef93f49ccd4308de30640c44b2bb315bb5d0ceb153aac17ecd83d114e777b6" => :x86_64_linux
   end
 
   depends_on "node"
