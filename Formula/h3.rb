e849ea17d5abcb5464a9153c88eb9e8349fddd1f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-22 08:44:24 +0000

    h3: update 3.6.2 bottle.
    Closes #18265.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/h3.rb
+++ Formula/h3.rb
@@ -9,6 +9,7 @@ class H3 < Formula
     sha256 "2af0d72ef4f2c32b5259e699f2e539090fdd1b75f32e4079bb5ef02624485b48" => :catalina
     sha256 "57ea8592019ec9ae43262b9fba49461611bf360bf2c5ef6bb33d3b938d8f98d2" => :mojave
     sha256 "d78e6b17d7d5dcd8120a85f10f4d00cf3a6475736172626336432628bab74ea2" => :high_sierra
+    sha256 "5337c5c45ee55602cbac3d163a1c6c5da29f2a66747cf39fbbf3c89bee74c31a" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
