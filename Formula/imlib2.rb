b5f59c510a884278204c2ba85689d63889d5e60d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-22 17:43:52 +0000

    imlib2: update 1.6.1 bottle.
    Closes #18275.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/imlib2.rb
+++ Formula/imlib2.rb
@@ -8,6 +8,7 @@ class Imlib2 < Formula
     sha256 "3f02ff8333cfc8a046432fb078a7ede7d45f0bcda7b7b1dfb7726306eee91a13" => :catalina
     sha256 "5134d4a325c2af69112bd5f3b5408843b513a6546f32f4840671a3575deddb6a" => :mojave
     sha256 "8bf5d8afb1f8a34db501bc46301fd6bbbef3b3a3f9c9863dea180e8f1288f485" => :high_sierra
+    sha256 "2b31a6123d18abefae9c7f299a58c1c2cd647813b61b2cafacda9770552e3c2d" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
