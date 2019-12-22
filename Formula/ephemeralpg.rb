a1ecc9a041e07eaf7a71433c8090f45926b1f353
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-22 17:45:46 +0000

    ephemeralpg: update 2.9 bottle.
    Closes #18272.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/ephemeralpg.rb
+++ Formula/ephemeralpg.rb
@@ -9,6 +9,7 @@ class Ephemeralpg < Formula
     sha256 "c02e04cf822d9a12c30126152a1003c2fe545e6c0dbda9c893ef5d165a342722" => :catalina
     sha256 "b880f6d03f12cc1c0f0c9b4a4726bbc0870cfa2c14e4eacc8a62f8a7ddfa0082" => :mojave
     sha256 "b3c09234c2cfd5739e55758c0ff0516b4776213fbe0b157b25a7f60010977bb2" => :high_sierra
+    sha256 "e2b24c6fef652dfff2370141ecac264385845a029930e5d0b506d1327bfd5ce1" => :x86_64_linux
   end
 
   depends_on "postgresql"
