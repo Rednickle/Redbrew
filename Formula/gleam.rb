e1bb4609e16e81f5dc880e2faa4f4398379b4d30
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-23 06:28:23 +0000

    gleam: update 0.5.0 bottle.
    Closes #18284.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gleam.rb
+++ Formula/gleam.rb
@@ -9,6 +9,7 @@ class Gleam < Formula
     sha256 "24c80811158e22e2ef1d9d4d5e7d3669b6e79733a0390bd919e35fbb323bb19b" => :catalina
     sha256 "e6992a3016f71029e490c2f1469b74b4397ef3095e2cfcee3ae41d3bf98e3713" => :mojave
     sha256 "57a840fb371cfb73856afce4dd3ce162b371da0b4879b6c9efe085029df1b40f" => :high_sierra
+    sha256 "c1d2df73333d9840135cab0253dd8703a260a7a9b78d8cb6a170ec2d800f6e78" => :x86_64_linux
   end
 
   depends_on "rust" => :build
