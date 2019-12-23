f2a9b27835b900154d617209ee041f4732a3eef5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-23 12:26:01 +0000

    stress-ng: update 0.10.14 bottle.
    Closes #18292.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/stress-ng.rb
+++ Formula/stress-ng.rb
@@ -9,6 +9,7 @@ class StressNg < Formula
     sha256 "e52795fd0d34109f2eb431b6d84000559ee898c1e56e2e9ba1cbe8cd7711e9b1" => :catalina
     sha256 "921174484d1907176a15602ed29ee281ff2a60266a5436cca78e02daaeccb80b" => :mojave
     sha256 "ac5bb01106618d99e6b503ab019c17cd74105a6aba77635369acf123c144c26b" => :high_sierra
+    sha256 "b8d0eb0c3854f6623e54d6fe14b6552e742128a327821bcfc1fca110e93b10b7" => :x86_64_linux
   end
 
   depends_on :macos => :sierra if OS.mac?
