8e33d0822ec6ba309e56522c0f076f09e42d6d0c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-24 11:09:22 +0000

    libebml: update 1.3.10 bottle.
    Closes #18323.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/libebml.rb
+++ Formula/libebml.rb
@@ -10,6 +10,7 @@ class Libebml < Formula
     sha256 "ab827d4f47936ff0ac3333ca38843a5171b8bd0211ac4c1f81f7a1ba6c4d3a6c" => :catalina
     sha256 "c6b199ff5aa3980d1a296f2e3d2ec50149f3ffcca2748ecd4a31e62b470e50c3" => :mojave
     sha256 "01cec2d9346a4e154d5d92327756212527b5e3b4980882b916ca05bc0aad41d7" => :high_sierra
+    sha256 "1281d702d344d9d41691228f2d7fa4bd4a58ba3e264d72587dcc204b49fd5ec4" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
