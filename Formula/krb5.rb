a9acf0113d69cc04a1a8b041955ea10c53bc2d55
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-24 11:09:54 +0000

    krb5: update 1.17.1 bottle.
    Closes #18322.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/krb5.rb
+++ Formula/krb5.rb
@@ -8,6 +8,7 @@ class Krb5 < Formula
     sha256 "e91d8a852288b491ceecd46e7324636d884bbb3b9a1085c08e7be9854e98dc91" => :catalina
     sha256 "68ea3e43598dd64035ea922507c22666d059836f337e882c6543e91d05f939d0" => :mojave
     sha256 "be007d08ecc596af4636657d618c845a1934ed0787705b3f906d408e977a7d26" => :high_sierra
+    sha256 "2d1bc16d1d5a8c7a5e8836b836426e473a243560cca4ec458d84b684956d053c" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
