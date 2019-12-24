c9299a0cc46d3d8c268cefceca2d50e0a7245c02
Michka Popoff <michkapopoff@gmail.com>
2019-12-24 09:42:45 +0100

    nushell: Fix build for Linux
    Closes #18327.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/nushell.rb
+++ Formula/nushell.rb
@@ -16,6 +16,8 @@ class Nushell < Formula
 
   depends_on "openssl@1.1"
 
+  uses_from_macos "zlib"
+
   def install
     system "cargo", "install", "--features", "stable", "--locked", "--root", prefix, "--path", "."
   end
