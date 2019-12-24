ec84fe0d3663de9044362c92ab55d5d81ddd56e2
Issy Long <me@issyl0.co.uk>
2019-12-24 18:40:06 +0000

    udunits: Use `?full_index=1` parameter for Linux patch

--- Formula/udunits.rb
+++ Formula/udunits.rb
@@ -19,7 +19,7 @@ class Udunits < Formula
     depends_on "texinfo"
 
     patch :p1 do
-      url "https://github.com/Unidata/UDUNITS-2/commit/0bb56200221ad960bc2da11fc0b4a70ec3c5d7c9.diff"
+      url "https://github.com/Unidata/UDUNITS-2/commit/0bb56200221ad960bc2da11fc0b4a70ec3c5d7c9.diff?full_index=1"
       sha256 "302fc33a7df84d8a60a21a0024d2676d5d16c08d3eb61d48e04d70f9499616f2"
     end
   end
