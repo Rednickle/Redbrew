f25c9db133927d943f59e28a7db8f0304788d425
Dawid Dziurla <dawidd0811@gmail.com>
2019-12-24 17:45:52 +0100

    xrootd: depend on util-linux instead of libuuid
    both `libuuid` and `util-linux` install libuuid.a file, but `root`
    formula depends on `xrootd` which has `util-linux` in its dependency
    tree
    
    Closes #18343.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/xrootd.rb
+++ Formula/xrootd.rb
@@ -4,19 +4,19 @@ class Xrootd < Formula
   url "https://xrootd.slac.stanford.edu/download/v4.11.1/xrootd-4.11.1.tar.gz"
   sha256 "87fa867168e5accc36a37cfe66a3b64f2cf2a91e2975b85adbf5efda6c9d7029"
   head "https://github.com/xrootd/xrootd.git"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any
     sha256 "d29cb551e03ec90aa36831aa84cb2d28ad99face2d169feb7f4a3eafd9f17c7b" => :catalina
     sha256 "271295a2f20f4f1cc34644078a0f3d95b2a7353fcae19eefb8bcf78212cafff8" => :mojave
     sha256 "5c02484e8eef5000acad850087bbdd06e6ea995b2ce83e5c317e42ca64fba442" => :high_sierra
-    sha256 "671aa312f2a1c2c958b29cf1b310fc163d93fa14422654437281b9cf2c1cb9cf" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
   depends_on "openssl@1.1"
   depends_on "readline"
-  depends_on "libuuid" unless OS.mac?
+  depends_on "util-linux" unless OS.mac?
 
   uses_from_macos "libxml2"
   uses_from_macos "zlib"
