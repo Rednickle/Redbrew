9772ebc8fff73b6017e316a15d71b2dc4862d608
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-24 16:26:07 +0000

    pandoc: update 2.9.1 bottle.
    Closes #18340.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/pandoc.rb
+++ Formula/pandoc.rb
@@ -13,6 +13,7 @@ class Pandoc < Formula
     sha256 "02ea8a0f8e79cbdd081c5464074269b4e3c4b0b7dc15b63a169552591894f1ed" => :catalina
     sha256 "426c54af7e4b1a13011c911a636e990cbf87ef2903b7b64940adba17ac70214b" => :mojave
     sha256 "448ebb5eea5eef2df4cfbb070aeb3c02d0a9567ae15984c0ab89854e65f348aa" => :high_sierra
+    sha256 "4a90a255815daeef64af31b8d37681ac446a7d32186cde650f8af7d7889f3785" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
