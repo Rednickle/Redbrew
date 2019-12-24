225338c4132a79689c8db926f7d7f1bf9847d903
Michka Popoff <michkapopoff@gmail.com>
2019-12-24 09:41:17 +0100

    infer: fix for linux
    Closes #18320.
    
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/infer.rb
+++ Formula/infer.rb
@@ -7,7 +7,6 @@ class Infer < Formula
       :revision => "99464c01da5809e7159ed1a75ef10f60d34506a4"
 
   bottle do
-    cellar :any
     sha256 "b6e5941d9be1c640b2dd0430801be1b59eb91ddc56c9e8e454c45af01c812476" => :catalina
     sha256 "166a3baf77f343a2bdd43fb772b93bd3610d6baf4f1e39e8fc6aa83e46029ef8" => :mojave
     sha256 "c7bb9d37a9d77fbc2019ffd6f80ce7e7a3992c4ac0a95235c335ce61f43993e9" => :high_sierra
@@ -24,12 +23,21 @@ class Infer < Formula
   depends_on "gmp"
   depends_on "mpfr"
   depends_on "sqlite"
-  unless OS.mac?
-    depends_on "m4" => :build
-    depends_on "unzip" => :build
-    depends_on "ncurses"
-    depends_on "zlib"
-    depends_on :java
+
+  uses_from_macos "m4" => :build
+  uses_from_macos "unzip" => :build
+  uses_from_macos "ncurses"
+  uses_from_macos "python@2"
+  uses_from_macos "xz"
+  uses_from_macos "zlib"
+
+  depends_on "patchelf" => :build unless OS.mac?
+
+  # Remove camlp4 dependency, which is deprecated
+  # Addressed in 0.18.x
+  patch do
+    url "https://github.com/facebook/infer/commit/f52b5fc981c692776210d7eb9681c2b8c3117c93.patch?full_index=1"
+    sha256 "5487b9b39607c94821bede8d4f0ec2a0ed08d5213d5f048b1344819dac53b2f5"
   end
 
   def install
@@ -44,6 +52,7 @@ class Infer < Formula
     ENV["OPAMROOT"] = opamroot
     ENV["OPAMYES"] = "1"
     ENV["OPAMVERBOSE"] = "1"
+    ENV["PATCHELF"] = Formula["patchelf"].opt_bin/"patchelf" unless OS.mac?
 
     system "opam", "init", "--no-setup", "--disable-sandboxing"
 
