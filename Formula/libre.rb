ba0c1f3c1173847ac1d91d1fb60026222ca225f7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-28 12:52:24 +0000

    libre: update 0.6.1 bottle.

--- Formula/libre.rb
+++ Formula/libre.rb
@@ -9,6 +9,7 @@ class Libre < Formula
     sha256 "0ca7e76631b5f30d72b4bc4248e894d00f05cfb785c98856d82cd5cc13e591f9" => :catalina
     sha256 "5d43d79ef2406e40c858463189ca8a40f0b13ede8a7090b56ba0fd1ef942dabc" => :mojave
     sha256 "32787ca36540a0c7c330560076e25726bcca0f08a7b77014d3837bd9c7ca1840" => :high_sierra
+    sha256 "4144fefdf5e1f8e51a1fed247b043ab65d2fbcee4ba00e935f559e5ec69562c6" => :x86_64_linux
   end
 
   depends_on "openssl@1.1"
