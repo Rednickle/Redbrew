9a61b225f0c6506dd01bbb51c8bca57cfe4a5b91
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-28 11:03:45 +0000

    tbb: update 2020_U0 bottle.
    Closes #18398.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/tbb.rb
+++ Formula/tbb.rb
@@ -10,6 +10,7 @@ class Tbb < Formula
     sha256 "c4d3405259d8440e81d912ac01720a867ded5e5f4bde53e14ea82b7ea7e4d466" => :catalina
     sha256 "f60db056d5f4de935893b827d0df58f8f2f47c9a631867ecb3687f40637e8449" => :mojave
     sha256 "f0ceef7716e50a16ad915ed00c3483a1dff3f4cb66f0e1c0d403d06833ef7834" => :high_sierra
+    sha256 "9c02618e7696a3c62a6c2b1759429afb976f21cb08d46af2d98582e0e271e88f" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
