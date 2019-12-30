9ee83ba2889260306adc393f4e1b4338fa2a2aa1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-30 19:59:26 +0000

    cointop: update 1.4.3 bottle.
    Closes #18512.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/cointop.rb
+++ Formula/cointop.rb
@@ -9,6 +9,7 @@ class Cointop < Formula
     sha256 "cf0bb3d7e03ce83a8b10b07d4370d36abc287f848db489b8a8c34326a5650814" => :catalina
     sha256 "607735a23086bb09d69bbd42dc03bbf28a71b106a825b1b3e6237a295c4bbd00" => :mojave
     sha256 "72169ab43d9ebeca68aa2bb8675c949ec9f7fd41a2b4ffecee890c3ebbb5e9f0" => :high_sierra
+    sha256 "4d0fdadc6f947a0d5b76734ce7234fb82553e19cad735dd401ceae3907597643" => :x86_64_linux
   end
 
   depends_on "go" => :build
