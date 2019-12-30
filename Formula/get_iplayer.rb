73e495c159aeca7cc9e5a986a82a21fe3c6f7832
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-30 19:56:53 +0000

    get_iplayer: update 3.24 bottle.
    Closes #18514.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/get_iplayer.rb
+++ Formula/get_iplayer.rb
@@ -10,6 +10,7 @@ class GetIplayer < Formula
     sha256 "d34ae270c9139aebcf176e7c6671cc838fe9b50b49fa1ee1c818f5d618c721c1" => :catalina
     sha256 "6643cc486c2bf11db3a7440ad5cd3e844bc9300229f951286bd1ec577ef225e6" => :mojave
     sha256 "9fb60cab0e9fa40714958f85108f23de3761e7b59b60f7efadf2c3988ec40044" => :high_sierra
+    sha256 "9d4c6b0e3719ccaf4133c3c0af09d1202eb7b02d71f050436a5c4d8cb91f812a" => :x86_64_linux
   end
 
   depends_on "atomicparsley"
