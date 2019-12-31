4385e80c22538415a63f65dd3280fd8ebd1719c7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-31 08:48:27 +0000

    gcc@8: update 8.3.0_3 bottle.
    Closes #18523.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gcc@8.rb
+++ Formula/gcc@8.rb
@@ -14,6 +14,7 @@ class GccAT8 < Formula
     sha256 "b410ceed5dd0e68839a663792c0744c7669cae633f200d4be690b6df0f955169" => :catalina
     sha256 "db70df625d79fc4eb3cff8a4669173c216a0a3c38b3718899713b3c682ded0ee" => :mojave
     sha256 "3c3c877cc99a599ad73695b6745ea05a096fb081c4c18070c30c7d5fa08dc532" => :high_sierra
+    sha256 "4958feb462a3fb723f566c6b70342c4e9137fddbca7c208161932584f147f8c8" => :x86_64_linux
   end
 
   # The bottles are built on systems with the CLT installed, and do not work
