29db3cdcfac0815578d84f21a5da714285db1c1a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-31 15:29:14 +0000

    xvid: update 1.3.7 bottle.
    Closes #18547.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/xvid.rb
+++ Formula/xvid.rb
@@ -9,6 +9,7 @@ class Xvid < Formula
     sha256 "ace5fea6272f3594b5c8fca6f1fe03c41c50a14af8599751571c5e44a49a5a53" => :catalina
     sha256 "4e119534a1351c85799944eb35f6f5675192e67e077fb3452f73f210a57eabe3" => :mojave
     sha256 "79ea46af3061561427ab0af36b09d61e057084c76f655ec21074fba375a36b01" => :high_sierra
+    sha256 "417f434031756b976767e0a59a02c507d675d7b5e3414f58eadf1209e28edc3b" => :x86_64_linux
   end
 
   def install
