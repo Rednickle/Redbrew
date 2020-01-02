0fca1f57ec0889240d6e06269027162d79d71a34
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-02 19:43:03 +0000

    polyml: update 5.8 bottle.
    Closes #18563.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/polyml.rb
+++ Formula/polyml.rb
@@ -10,6 +10,7 @@ class Polyml < Formula
     sha256 "1439f4258d7fa8adfab0b037ae43c5effaffd9b1c7793c05f73a0f130b65d403" => :catalina
     sha256 "356373c5c6483a552164e3aa815076688f37e41d74f8350a52321205f5d4547d" => :mojave
     sha256 "d69da52fe77cd77d079de8ba2b389ced34800cebf55202d023c6800d584a5212" => :high_sierra
+    sha256 "1cd4c95878ee8cab396f2aa2521f80757318d1a50f5ad9acb23c52ee22499c62" => :x86_64_linux
   end
 
   def install
