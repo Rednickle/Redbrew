8864d2135cc704cec0f6ff12c1530eefdd4f6cd7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-02 19:54:47 +0000

    click: update 0.4.3 bottle.
    Closes #18551.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/click.rb
+++ Formula/click.rb
@@ -10,6 +10,7 @@ class Click < Formula
     sha256 "d4f0029a38738bd96c34e257ffa4d32a65452354b94c9feee6d12c35af8be770" => :catalina
     sha256 "57598c45faa3379b4585686fbc6074f9755a01a733f4a6370ca5034756340b8f" => :mojave
     sha256 "1da2b335adabf93bb8fd8bcfb772721ba6f89bb4d71ee9c8ef99e93afa0a6ca7" => :high_sierra
+    sha256 "1fd0cb5553b1f6abe138bf7047a7651bafbc22c615d46f7f172967277b911b1c" => :x86_64_linux
   end
 
   depends_on "rust" => :build
