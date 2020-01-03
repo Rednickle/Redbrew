8b90e6a7d167ab0b5d06e8abcd642897b376be45
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-03 15:24:08 +0000

    pianod: update 301 bottle.

--- Formula/pianod.rb
+++ Formula/pianod.rb
@@ -8,6 +8,7 @@ class Pianod < Formula
     sha256 "6698a4353e50aed190386ccdf407af24f6b623691ebabdef7f8a0b7dca77b338" => :catalina
     sha256 "c0dfa015b5c5546a2fc4788d716c00520282773a93e7d6669f891f2b1126f587" => :mojave
     sha256 "d7c6ba07d0b46d393c4dc5de718e4dcd3130d6b0f8f2ee5aa5d00efd8ec69e93" => :high_sierra
+    sha256 "50b13a02b3aba6fb33fc38f66e1f370ebe60657ed7e5b59d2bc33e65d655a4c3" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
