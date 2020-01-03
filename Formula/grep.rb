2ab6443567833cc7977551e9950daca1d464327b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-03 21:11:46 +0000

    grep: update 3.4 bottle.
    Closes #18595.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/grep.rb
+++ Formula/grep.rb
@@ -10,6 +10,7 @@ class Grep < Formula
     sha256 "52fb744dfc1f2766b41d90bc4126bc6101663d12c3e31446719ef723b7883266" => :catalina
     sha256 "cbce0c10ed3edc352347287ec36a8a199bb7649892943a6948e4dbe4127e83ac" => :mojave
     sha256 "bcc9802f916e1db94b2879ff66b058d42142b391bfbc2002fd1fb13fd9e4c8ca" => :high_sierra
+    sha256 "e9085dfde4534662328a63c2a15d333db06517eff1f8b12112e0ce9b94b770e3" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
