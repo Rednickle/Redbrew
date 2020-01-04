7155ebf6adfca517ebf98b826b90bf5a140d573b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-04 19:10:50 +0100

    jdupes: update 1.14.0 bottle.
    Closes #18607.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/jdupes.rb
+++ Formula/jdupes.rb
@@ -9,6 +9,7 @@ class Jdupes < Formula
     sha256 "041e4580efb9e9b7d02c81a3865ff346e5793c7678332218d4b883a4d73d9341" => :catalina
     sha256 "c2482575162f38c552774b8d833100a465a9861a43d8f748c4565eb2fdfa1302" => :mojave
     sha256 "1268810769bc7b1f6ee99a7d600ab599aeca5ac37ae3a89ae7ed8994355f191b" => :high_sierra
+    sha256 "3933186b8fbd492b5add92c7979b3a49142b7b1e5298c63afea89259bd52fce8" => :x86_64_linux
   end
 
   def install
