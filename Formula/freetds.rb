82316e6feaac191b4ee4d2edf9f73641ba5c4de1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-06 15:43:54 +0100

    freetds: update 1.1.24 bottle.
    Closes #18627.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/freetds.rb
+++ Formula/freetds.rb
@@ -8,6 +8,7 @@ class Freetds < Formula
     sha256 "363d44ed3a0ec8f9768987bc6d873913dd30818933ea9bb9c290580dbd6d11ab" => :catalina
     sha256 "0af97f13a63bcdc54f9f5963e6a935e2779ae76e11cccfa75b8f60f0affe6574" => :mojave
     sha256 "49347305bf6ae124626f8d74d0660adf73cfee4d36b7cf0b6fbf312041b7aee5" => :high_sierra
+    sha256 "9b945c36c4a76236b993a7890a9e7f806a08179a5b94d4b3d2c25e9905d17e19" => :x86_64_linux
   end
 
   head do
