b2dc400157af030c2d4eeed7426f360414a5de7b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-06 19:19:50 +0000

    prometheus: update 2.15.2 bottle.
    Closes #18669.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/prometheus.rb
+++ Formula/prometheus.rb
@@ -9,6 +9,7 @@ class Prometheus < Formula
     sha256 "4c1fec08803cee24252ac7ae3459b928e609a42a2151ed17017b2182ef183877" => :catalina
     sha256 "d4807cdd70fa35152127626e79a1f966ae3e7a50be06fd3c239a8bd039492212" => :mojave
     sha256 "0a75a53d47767302e3014e98a227cc5b61a9536b31d1a0c50ed506593d014852" => :high_sierra
+    sha256 "edcef4169e072ae0d90f29d6d3eff79e7c529f2541a91c316c2dd649e51a1c30" => :x86_64_linux
   end
 
   depends_on "go" => :build
