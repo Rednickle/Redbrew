c9dc8363a86363f8c1e2ef403d73c925f78c40f9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-07 19:33:29 +0000

    gitmoji: update 3.1.0 bottle.
    Closes #18688.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gitmoji.rb
+++ Formula/gitmoji.rb
@@ -11,6 +11,7 @@ class Gitmoji < Formula
     sha256 "9254410f6e98034dcffa618547c79b241e31b0a9d6124d726f0ba9c5e637b046" => :catalina
     sha256 "1e529ccf9af26fe632f43587922dcc1fd4c02662a51b09fd73c01b4e1cd28830" => :mojave
     sha256 "6900c89d434b421418b94b81014a250e24d3288c89d0379080ddeedfeac8acca" => :high_sierra
+    sha256 "a636c706ba85e80c6c9872267edb6b9709677ffe412f7c9a5d25b17c6df0b591" => :x86_64_linux
   end
 
   depends_on "node"
