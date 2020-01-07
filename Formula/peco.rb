40fff39a29045347308fb9041d023c53894a6697
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-07 19:29:19 +0000

    peco: update 0.5.6 bottle.
    Closes #18690.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/peco.rb
+++ Formula/peco.rb
@@ -10,6 +10,7 @@ class Peco < Formula
     sha256 "bb85a67b0efb4791324cf34b00a9006a20f939934d0992ed49a3c5e208d4eb39" => :catalina
     sha256 "84a8ffecb7b52a934e6c57a70ab073b46a13099dccf2fbf5b8a56b9eb5162aad" => :mojave
     sha256 "9b962800f1a3bd1ddb36692a5157059bfea38897259be46abcf8f144b5da1866" => :high_sierra
+    sha256 "e37f0fa88adaceccf0cc8744ad1bd5fa7af4322ab5ec64ff0a2111ae895520ce" => :x86_64_linux
   end
 
   depends_on "go" => :build
