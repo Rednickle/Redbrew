06a30711cd1fc2df1fbf6b1f38a3f885301bd10d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-07 22:55:53 +0000

    entr: update 4.4 bottle.
    Closes #18682.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/entr.rb
+++ Formula/entr.rb
@@ -9,6 +9,7 @@ class Entr < Formula
     sha256 "98f508565c8dd087b780fda140099fca3afb457ca27fcf3864bb508c87c403cc" => :catalina
     sha256 "d18935ecc0bf78504d6acd00b2adb889389af2586cafc2602e38599f2590183f" => :mojave
     sha256 "25fba36721d2857ca91efc7b82a8cbe15ff0a83f20e9febe57648fc173377629" => :high_sierra
+    sha256 "a224c62e5f95c6284d93afc3f6005aff55b84fb8c046e27d443c1a71e8db750e" => :x86_64_linux
   end
 
   head do
