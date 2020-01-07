24b22b74dec2c62eb7c8465c4a1d87450734b900
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-07 22:53:55 +0000

    ccache: update 3.7.7 bottle.
    Closes #18677.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/ccache.rb
+++ Formula/ccache.rb
@@ -9,6 +9,7 @@ class Ccache < Formula
     sha256 "8cdd14e7b2bb913f3597d0ba651c8db8745f8e460a96af1d2e590037bbad5e54" => :catalina
     sha256 "e41456c1bac6d2ee59a40f133d72dd5bdeb7e2a1e98a40720decae3d2f52df02" => :mojave
     sha256 "6052db4c0f8e65d74e66ac01aa79ae245dc1ae2afcd98a651b9c3d680683926f" => :high_sierra
+    sha256 "30651218d910eb257fd7e0be583381e21a75bba7d2fbbe48a0c010e7c196691f" => :x86_64_linux
   end
 
   uses_from_macos "zlib"
