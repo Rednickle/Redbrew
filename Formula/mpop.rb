e2e915f149cdd7aa9bba7a17bad67e098720e9df
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-08 19:21:42 +0100

    mpop: update 1.4.7 bottle.
    Closes #18735.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -8,6 +8,7 @@ class Mpop < Formula
     sha256 "e1e49451607c9f43c5d25be5f84959d89f0037bac6b579760af425052d36e442" => :catalina
     sha256 "cae5ba211272387489bd78abb54c0173ce692d393f5e87af7a7f3b1653e9f8cc" => :mojave
     sha256 "c532568b1ed5d21d145f0bfe641c795240fa7a9dd061bc3216dfd1e8b3029836" => :high_sierra
+    sha256 "ddbd6612c712228f6eb083e38e0ebc8c730599c3dae966f919606582d69a0d7e" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
