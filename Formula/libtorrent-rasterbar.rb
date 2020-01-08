f658c8709e9f5d5bac418ba52718f4a84ef6934c
Dawid Dziurla <dawidd0811@gmail.com>
2020-01-08 12:26:22 +0100

    libtorrent-rasterbar: fix test on linux
    gcc parameters order
    
    Closes #18728.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/libtorrent-rasterbar.rb
+++ Formula/libtorrent-rasterbar.rb
@@ -48,12 +48,23 @@ class LibtorrentRasterbar < Formula
   end
 
   test do
-    system ENV.cxx, "-std=c++11", "-I#{Formula["boost"].include}/boost",
-                    "-L#{lib}", "-ltorrent-rasterbar",
-                    "-L#{Formula["boost"].lib}", "-lboost_system",
-                    "-framework", "SystemConfiguration",
-                    "-framework", "CoreFoundation",
-                    libexec/"examples/make_torrent.cpp", "-o", "test"
+    if OS.mac?
+      system ENV.cxx, "-std=c++11", "-I#{Formula["boost"].include}/boost",
+                      "-L#{lib}", "-ltorrent-rasterbar",
+                      "-L#{Formula["boost"].lib}", "-lboost_system",
+                      "-framework", "SystemConfiguration",
+                      "-framework", "CoreFoundation",
+                      libexec/"examples/make_torrent.cpp", "-o", "test"
+    else
+      system ENV.cxx, libexec/"examples/make_torrent.cpp",
+                      "-std=c++11",
+                      "-I#{Formula["boost"].include}/boost", "-L#{Formula["boost"].lib}",
+                      "-I#{include}", "-L#{lib}",
+                      "-lpthread",
+                      "-lboost_system",
+                      "-ltorrent-rasterbar",
+                      "-o", "test"
+    end
     system "./test", test_fixtures("test.mp3"), "-o", "test.torrent"
     assert_predicate testpath/"test.torrent", :exist?
   end
