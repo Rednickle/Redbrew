b8e8f9ac7486ef75f7c87f7e012878ebf9b79da0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-08 19:03:04 +0100

    spin: update 6.5.2 bottle.
    Closes #18747.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -9,6 +9,7 @@ class Spin < Formula
     sha256 "6432ab186b64f64851fa0f60dae53c13b6c9bfbc6195c41abc08f1ddfd824bf6" => :catalina
     sha256 "eae932021ba8a15f713dd60ca2a29267f5df53a832895c5ab1a342d2568c6f45" => :mojave
     sha256 "3ffbbe34633fa0e177bd25343b3bbd35d706988ab04c4a617fff530cf3dc542a" => :high_sierra
+    sha256 "e1301a7a73b5dce1a61afc251890ed77fc5b53fa5e8d7a9cea51611c95248ce1" => :x86_64_linux
   end
 
   depends_on "bison" => :build unless OS.mac?
