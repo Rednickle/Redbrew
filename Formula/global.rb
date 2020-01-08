4aacfb5ca7c4fe7dac70c3c284852ce9c8a0511a
Michka Popoff <michkapopoff@gmail.com>
2020-01-08 07:37:21 +0100

    global: use python3
    Closes #18706.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/global.rb
+++ Formula/global.rb
@@ -38,6 +38,11 @@ class Global < Formula
   end
 
   def install
+    unless OS.mac?
+      inreplace "plugin-factory/pygments_parser.py", "#!/usr/bin/env python", "#!/usr/bin/env python3"
+      inreplace "plugin-factory/pygments_parser.py.in", "#!/usr/bin/env python", "#!/usr/bin/env python3"
+    end
+
     system "sh", "reconf.sh" if build.head?
 
     xy = Language::Python.major_minor_version "python3"
