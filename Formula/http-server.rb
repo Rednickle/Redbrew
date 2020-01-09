a368f72e28b496c5eb41fd9763cd26219e3a618a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-09 17:28:59 +0100

    http-server: update 0.12.1 bottle.
    Closes #18791.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/http-server.rb
+++ Formula/http-server.rb
@@ -12,6 +12,7 @@ class HttpServer < Formula
     sha256 "d54d568edf8a41ba191780c8a5afbc283afc49a71db6a47259752eedbcd157b1" => :catalina
     sha256 "abbcfb975903d901ed6e2d81e93f527143c76ac1813d181d8a4cbb6c4de58dbe" => :mojave
     sha256 "df39f65c35d983da959f812f94ba676aa616b7c79ccb4385978ac37ec0055274" => :high_sierra
+    sha256 "2035110533f6eaf63b8d0015a60ea4b847176cd363e214a181304636fdd77b37" => :x86_64_linux
   end
 
   depends_on "node"
