0dd3dbfeaeed4bb4619f4ccc884b20d96c4251df
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-12 14:58:02 +0000

    httpie: update 2.0.0 bottle.
    Closes #18900.
    Signed-off-by: Issy Long <me@issyl0.co.uk>

--- Formula/httpie.rb
+++ Formula/httpie.rb
@@ -12,6 +12,7 @@ class Httpie < Formula
     sha256 "56b55a29dde9f5288dc1803b42b9934a012939cd7c05ed1d21ae3c9a95024d9b" => :catalina
     sha256 "39f6e57a70ee1998f730123a85c893757ecbe4660c949906590456a07a3bbdd6" => :mojave
     sha256 "4a77117bde4316dbf115c3d636d00047f10ecffd219423608821f09afc71a19a" => :high_sierra
+    sha256 "b9d78e50e8c50b6687d7bddc14f2e06c1eb87400bd8c135e8e133698a2fda2b0" => :x86_64_linux
   end
 
   depends_on "python@3.8"
