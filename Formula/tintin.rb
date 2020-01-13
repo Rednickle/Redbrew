1522d320422189ade94bbc915266bd3879c4f65f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-13 17:59:30 +0000

    tintin: update 2.02.00 bottle.
    Closes #18939.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/tintin.rb
+++ Formula/tintin.rb
@@ -9,6 +9,7 @@ class Tintin < Formula
     sha256 "3da12a97145f035ed9ae5bf0f038800ff4a4f7aba014546e0e54960f64d414d8" => :catalina
     sha256 "ec78955fa2d9637332d29399c9b790e78567854b97847af5f6bc1fbb3a0ecaee" => :mojave
     sha256 "d907ef0afbbd3bc01792d8806968861e13b712b2228c5e505ba1996c075478e1" => :high_sierra
+    sha256 "dffd9aafa295296c22e71b16ab07ac1ae6293a5f3f33210eb9c4d81054d3907d" => :x86_64_linux
   end
 
   depends_on "gnutls"
