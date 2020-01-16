f5b81f7f6a5c7b2f444950483204d1cfb387e7f8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-16 13:04:31 +0100

    fastmod: update 0.3.0 bottle.
    Closes #18969.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/fastmod.rb
+++ Formula/fastmod.rb
@@ -9,6 +9,7 @@ class Fastmod < Formula
     sha256 "274ec8fcf9eec3ace63784576b6f48e801c60aa0d87911c652ade74e918c04bd" => :catalina
     sha256 "4ded8ec0f1c13f363620f02098fd6b699cf8280cc4852f6006b85a6ac90e4555" => :mojave
     sha256 "03753429f472a6c12ead72534ee006c93da7d0da865c2bc393c655a9670b0dbc" => :high_sierra
+    sha256 "176d5f3b41229606ebe8d6df929934d54cd8d5f32d19c725834f66c6e7e7f523" => :x86_64_linux
   end
 
   depends_on "rust" => :build
