399451f03e88e12ea5819c3d19fdb51f19ddff52
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-16 13:07:32 +0100

    gnu-sed: update 4.8 bottle.
    Closes #18972.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/gnu-sed.rb
+++ Formula/gnu-sed.rb
@@ -9,6 +9,7 @@ class GnuSed < Formula
     sha256 "726be75d6d7155820b408a10e5c1a5ba1406374a7fc167af62524a4f4bbbc099" => :catalina
     sha256 "093f16752e7dfb115c055f20aed090108b94edd47c40f5e50878d961359251b2" => :mojave
     sha256 "865abe618c67037a4a419a05e0df2c6814fb3abdd6f631ea546aeba0aaf8eb78" => :high_sierra
+    sha256 "0ddd750074b40c2fa24f81ec6057ead7eca21b76ca9191fe292da13afdc5a274" => :x86_64_linux
   end
 
   conflicts_with "ssed", :because => "both install share/info/sed.info"
