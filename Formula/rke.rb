f55bbb7d8a4bdcfa4599ba3c84a373c49af01719
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-18 00:31:37 +0100

    rke: update 1.0.2 bottle.
    Closes #19033.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/rke.rb
+++ Formula/rke.rb
@@ -10,6 +10,7 @@ class Rke < Formula
     sha256 "d921148be1b3a2b16c33564c19122e7aa1ffb0f7c1fc0428ea79878d9065f6c3" => :catalina
     sha256 "a86478d4fd5a804d52418f8421b0734466a52aef530fc7a63478c4b70851b930" => :mojave
     sha256 "c36bda489d3e81e1e30815a237fe0be2cd88e9466553a3b8128befe53c2e5db2" => :high_sierra
+    sha256 "877f6db1cf8c10eb36148b30ba5f1dc9377c0ad8f0f8f3c519b5697190c2de27" => :x86_64_linux
   end
 
   depends_on "go" => :build
