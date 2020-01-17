ba65994cb3fcd4acfeceb597d2d1acb980a4c16c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-18 00:31:01 +0100

    teleport: update 4.2.1 bottle.
    Closes #19036.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/teleport.rb
+++ Formula/teleport.rb
@@ -11,6 +11,7 @@ class Teleport < Formula
     sha256 "2e9c4f231b3d6dd31b4e817d8e7c063228d2e2ab60ccc2298dacc8fdd2149be3" => :catalina
     sha256 "5e0f3f443d7ea4263f3c4c4380ce4c1e08380c1e164fafb1110a6f3a931f4c5e" => :mojave
     sha256 "9a1f3cc349b73c270eaed8a721cf30a935d61e25144a94afe870948845343114" => :high_sierra
+    sha256 "235dc13527d1570301b63152579b8de94cc1854e4141b5b3b7cab8ba34daf4ac" => :x86_64_linux
   end
 
   depends_on "go" => :build
