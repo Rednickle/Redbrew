ae92b496ce47cae32be81824d25c5932970f7824
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-19 12:17:16 +0000

    wabt: update 1.0.13 bottle.
    Closes #19075.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/wabt.rb
+++ Formula/wabt.rb
@@ -10,6 +10,7 @@ class Wabt < Formula
     sha256 "11975b7122f6106a969064cd684e9a77d24443df26b873a225f48d8e1304fff9" => :catalina
     sha256 "26d32821ca26815974d699a5665ce3406fb9373a9d8970d7349ef6e3aee18208" => :mojave
     sha256 "5d028ba6b144571da2407e65351aebd93fabb84d453601971a4c5c02d3a51b17" => :high_sierra
+    sha256 "0150ceef5ecd3180fb336d174299f14eefe91097b497e1c5d4536a68c4568048" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
