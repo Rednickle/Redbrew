436a1786ae7e31c7b11a4527f4428c56befe24e6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-20 22:39:06 +0100

    gitleaks: update 3.2.0 bottle.
    Closes #19093.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/gitleaks.rb
+++ Formula/gitleaks.rb
@@ -9,6 +9,7 @@ class Gitleaks < Formula
     sha256 "1149bb0efb00de9960f036a8cb44df9a70d43f6171e3a1bdcec40054500e2dcb" => :catalina
     sha256 "b325c01b93d5eb5270e732fb5c1812d3883f4f0e6acbd06537183348a78b8b70" => :mojave
     sha256 "f063a806f1b52d099691324072df85ed664950327f21b2a156396c9fedc1df0b" => :high_sierra
+    sha256 "778d99e944a846b858b94a3ef2c973df8c895ed0317a96c4298de45381630d7f" => :x86_64_linux
   end
 
   depends_on "go" => :build
