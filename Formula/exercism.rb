7a7e8b2e40921cd80c9a8ce8fc7d3b57b111a74d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-22 11:48:22 +0100

    exercism: update 3.0.13 bottle.
    Closes #19138.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/exercism.rb
+++ Formula/exercism.rb
@@ -11,6 +11,7 @@ class Exercism < Formula
     sha256 "9a4080f7e35f37dc4eb15e733692314cec32cba7e0f76e8f58eb99850f708cb1" => :catalina
     sha256 "7319920cfd6779984dfabbecdf3e15a37603f6bfbecfc1121bfa2a044fb8ed17" => :mojave
     sha256 "b094a8441575b02f312f04760589f94d9f2b1d76330c07a67f7d07a40ad561a9" => :high_sierra
+    sha256 "8ff44fc535ac7f7ab9acc6e52aafbeac8f159d4e5e3a53823324048af3daa33b" => :x86_64_linux
   end
 
   depends_on "go" => :build
