71c03c32986cde09b185bd3c00d0a18c7a3c8a70
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-22 11:40:35 +0100

    serverless: update 1.61.3 bottle.
    Closes #19145.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/serverless.rb
+++ Formula/serverless.rb
@@ -11,6 +11,7 @@ class Serverless < Formula
     sha256 "a0557c15e49f5ec0e846cec5882851d1b9f51b77946852d439c5198aae8b88af" => :catalina
     sha256 "d3c6ca27c5f67e512e91ec5a48f3dfaea650671dd37bd1c13c9cd59ff5fdcca4" => :mojave
     sha256 "2b10727cf1d402764b1820604cfbe148d127b665445bdc0062a146e2c045ecc8" => :high_sierra
+    sha256 "82de6168f5c5299a9349dd0001a8c14f4a17e2e2d7e44928823b91d83c35e00e" => :x86_64_linux
   end
 
   depends_on "node"
