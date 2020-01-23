19e823c4e3612c69305638210a68ab1f408caae6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-23 18:25:12 +0100

    ghq: update 1.0.3 bottle.
    Closes #19154.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/ghq.rb
+++ Formula/ghq.rb
@@ -11,6 +11,7 @@ class Ghq < Formula
     sha256 "e933efa26b91c0d75d87c0c7f2d1872cf658823abe3714042cb4af71389295a1" => :catalina
     sha256 "c5aba963d67cc884f0f15d5095b4af803e967dbe37022c47cf53e4c896c486d3" => :mojave
     sha256 "8f429d8db1e7b14abaa12b8e24e12131be1883765fe06c8fcca5816fbc679168" => :high_sierra
+    sha256 "030bfa58df6ecb346e1b501b3069d264deb445e9d6a12ca130a0d993c2b62215" => :x86_64_linux
   end
 
   depends_on "go" => :build
