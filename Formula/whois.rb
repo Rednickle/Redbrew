3d96d708d7146ba6a955f95bb708be5e7a765f81
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-24 12:34:12 +0100

    whois: update 5.5.5 bottle.
    Closes #19190.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/whois.rb
+++ Formula/whois.rb
@@ -10,6 +10,7 @@ class Whois < Formula
     sha256 "fbdbe5c9d7e53c178d38dd2cc96f9b7c7c7e6d66ac3c99612c934c9fae09c434" => :catalina
     sha256 "5433f102ffe597e2eb1ed81285967701b7a77bbe79ddcba9dc2bc7d53106fe1e" => :mojave
     sha256 "ad4c83c260322ab6eec10cd3e4a26603d60d48ca103984d47ee3bf76af3b43ae" => :high_sierra
+    sha256 "eae6b2d7d12bf7397c64cfc329007b84d98878214af71e0cf01eab316544ea6a" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
