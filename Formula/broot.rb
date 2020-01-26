8f1b73d29fc155c99c289f24922eda5a27351e50
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-26 12:12:03 +0100

    broot: update 0.12.1 bottle.
    Closes #19212.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/broot.rb
+++ Formula/broot.rb
@@ -10,6 +10,7 @@ class Broot < Formula
     sha256 "1c5c86dfad2b3366fe19183e9f07f21da6b7f8f673021aee0e1f72f45bcfd0d9" => :catalina
     sha256 "e591afd42fe2ca6648809964fefa303b323a9b77f36f5055fc7015e59c65697d" => :mojave
     sha256 "81f8417222d607159d2ca31f453ada1aa51088dcde128aa534fcfe9772b88551" => :high_sierra
+    sha256 "9f1e0e413c6de160d70e74db175685131463ddbfe59082e740646a8cbfb568a8" => :x86_64_linux
   end
 
   depends_on "rust" => :build
