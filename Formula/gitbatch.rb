d20804b72e1535b9202d22a0d897a4afd6bf30a9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-26 12:08:33 +0100

    gitbatch: update 0.5.0 bottle.
    Closes #19233.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/gitbatch.rb
+++ Formula/gitbatch.rb
@@ -9,6 +9,7 @@ class Gitbatch < Formula
     sha256 "58b204bd1779e99cade465f98457ec14cadaee9a3b65afe099245faba640da0c" => :catalina
     sha256 "31d8b72293ceacef7d44beb203887ff80628cb5cd3b56a9f0b467704d153b261" => :mojave
     sha256 "73fc219e77776b78635c672111736a1ce26f6f1afe0df4ce7c571341578cd1e9" => :high_sierra
+    sha256 "11b14438381986b8202dea1849759f07241a3a7193f5db67638eb3ef013b1018" => :x86_64_linux
   end
 
   depends_on "go" => :build
