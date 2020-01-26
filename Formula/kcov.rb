2a4233fc24d442a7bf70198ee97066d17c85a465
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-26 12:00:23 +0100

    kcov: update 38 bottle.
    Closes #19222.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/kcov.rb
+++ Formula/kcov.rb
@@ -10,6 +10,7 @@ class Kcov < Formula
     sha256 "7a3af28e85c85c6f1dc684086884c724dfdbcf72efca48add536c5dd08bda4c0" => :catalina
     sha256 "833750a5d75e99a392010b305841daca6d0007e5a9b2ccd2ab5d54f18c01b6ad" => :mojave
     sha256 "e5c6cc5b5ed21b5609107cb80ac67dec4ffc9b9227e272464b9eeade66932bd3" => :high_sierra
+    sha256 "753c990c08ad1f08aaf0595db7418289f0fd586e08182e16bffb50748ff49b3e" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
