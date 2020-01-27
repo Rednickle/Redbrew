dec70403189391e8c37534c57cb29fe6f7e437d5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-27 19:09:29 +0000

    wcslib: update 7.1_1 bottle.
    Closes #19248.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/wcslib.rb
+++ Formula/wcslib.rb
@@ -10,6 +10,7 @@ class Wcslib < Formula
     sha256 "6b13b97c4df588d435da3cb4e09c70b4db1ad2a19c3a15bf5d80108514062904" => :catalina
     sha256 "db830d1e0e3c8b7da3f61d9fb007232703458200b90165a03688dea6b93fcf18" => :mojave
     sha256 "ab7122ab82a0ab67a87f4d51fb08e0d59b9b16309ea7a830707a7207ddfd0b0c" => :high_sierra
+    sha256 "573b39c638596238d829776270536b7e06ebd886ca52ecb1d67509c268025de0" => :x86_64_linux
   end
 
   depends_on "cfitsio"
