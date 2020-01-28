8e679a39dbc5a51c2aaff63f8b63f5c2204409fc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-28 22:41:05 +0000

    just: update 0.5.7 bottle.
    Closes #19282.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/just.rb
+++ Formula/just.rb
@@ -9,6 +9,7 @@ class Just < Formula
     sha256 "357266240ea70600c0d5c00a6888e31a64612b352f1e613180dff95b7ee93d69" => :catalina
     sha256 "af376e4550db79eb97c3bb91e285c2f0fdeae1f8f80ef2a9627183766e2bdb55" => :mojave
     sha256 "7cd62c90e0d81b96c16ad54ccd0fc1fb26327184a2f4618f2e59eb2285b306f9" => :high_sierra
+    sha256 "fd5e5284947f78785b5151f5f58088c6b7c23fa934f0fdf61690a9e97de6a0a2" => :x86_64_linux
   end
 
   depends_on "rust" => :build
