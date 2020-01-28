4cb6d9a7e3e5deb0c864670af2bcc3dacc2437eb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-28 12:49:29 +0000

    sfcgal: update 1.3.7_2 bottle.
    Closes #19275.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/sfcgal.rb
+++ Formula/sfcgal.rb
@@ -10,6 +10,7 @@ class Sfcgal < Formula
     sha256 "fefaaab69ba9fc4664303b558c01a9bcc584fbe39b0d34e2373d384c4a371e2e" => :catalina
     sha256 "e69cc15c8b93ddd06a9c65acd55afc338d520939d8781cbe97d8238548eee380" => :mojave
     sha256 "58cd7e79ae765402b421fc1f4a6b7fd3b9d9e4a54bfe06fb5687b6b886c25bb4" => :high_sierra
+    sha256 "a987ab2e217dcaf07b4f8a8cd226c7522a877c376f15ae01371bf579e889cf6d" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
