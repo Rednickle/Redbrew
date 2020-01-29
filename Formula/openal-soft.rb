2a5dfd52bf4c70e8773ad02283a7af5c66c9289a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-29 06:37:24 +0000

    openal-soft: update 1.20.1 bottle.
    Closes #19288.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/openal-soft.rb
+++ Formula/openal-soft.rb
@@ -10,6 +10,7 @@ class OpenalSoft < Formula
     sha256 "b62337bb563eaaacec777b6861de4b6ba1d33ad8e19418b4b5a0cdfb37473fcf" => :catalina
     sha256 "f6448e18550e62cd283bebe24e98f6253c5c18758aeb7c44f3816f1840082b25" => :mojave
     sha256 "26dbb8819cd991e4a901f3f90f0cc2339c9e4220330329dcf7caa37b14d048d7" => :high_sierra
+    sha256 "078bd103e43fd7378671d0b8c332d5798cdf3bc562813d74e6e485f2447cd56b" => :x86_64_linux
   end
 
   keg_only :provided_by_macos, "macOS provides OpenAL.framework"
