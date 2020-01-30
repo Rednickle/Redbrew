813493dc1b4434c177927c3df319f715f4536d35
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-30 21:56:15 +0100

    topgrade: update 3.8.1 bottle.
    Closes #19327.
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/topgrade.rb
+++ Formula/topgrade.rb
@@ -9,6 +9,7 @@ class Topgrade < Formula
     sha256 "6c2dd259356d2db4ca03bfe928a45cdd77e2c0d7ea1745af8ea7c6c27d185bf6" => :catalina
     sha256 "9e5519fd59535d1a9c42a27b28e9ca2f89f9aa405f693f00e57d542677cbfa46" => :mojave
     sha256 "ab8195878f1ffdd404c7f33c80a7d31e56b950780ec93675d468908517e6b9d3" => :high_sierra
+    sha256 "4ddc803d1508a3bd2fb52062ac3848cbe89a1f77c3e663cbc8d412fba8e7ef3f" => :x86_64_linux
   end
 
   depends_on "rust" => :build
