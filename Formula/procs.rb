4a04b4ea77202d4cb1e89bab5091540ae6ac9eca
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-30 22:26:20 +0000

    procs: update 0.9.5 bottle.
    Closes #19340.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -9,6 +9,7 @@ class Procs < Formula
     sha256 "a2ac131640c53bce4a285749e10bc9493528d4a6df800d80cc3376bce592496f" => :catalina
     sha256 "3fa35891e7014ffc62b3424006a3a84c74240337ef5763aa31b26d8157a2a953" => :mojave
     sha256 "df67246f12cc23c1c21ef8cb51f03107f35ea50e95281e98bedc9f0cc58de21b" => :high_sierra
+    sha256 "7dfc2a3f90d322db63d6bf3325b0cd1092ed762bc2afba042d7cba611f4d1c19" => :x86_64_linux
   end
 
   depends_on "rust" => :build
