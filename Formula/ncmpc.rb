8eb55cac5568e468d67b52e1b867e9626c71f631
Dawid Dziurla <dawidd0811@gmail.com>
2020-01-30 21:37:07 +0100

    ncmpc: rm cellar
    Closes #19358.
    
    Signed-off-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -5,7 +5,6 @@ class Ncmpc < Formula
   sha256 "7c8eb727f6e12d8f97a53915b1b5632898b4afb335a1121c5e01c81df695615c"
 
   bottle do
-    cellar :any
     sha256 "d21aef0ebf95c77cdcaa82cc26b9752a3d7882a6c248139654fae3be5261baa5" => :catalina
     sha256 "379fa2e570a4987b05068b6ecb4454c9819da9e9c9670f65a0e158e43ea75afc" => :mojave
     sha256 "e05d6ca991df52846ca07fb2d536cf4644bc225f6a729ae3f180fbb5967356a5" => :high_sierra
