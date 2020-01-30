98392ecf94b06690f0f7ee351de70b670c81e692
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-30 22:46:44 +0000

    gifski: update 0.10.1 bottle.
    Closes #19350.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/gifski.rb
+++ Formula/gifski.rb
@@ -9,6 +9,7 @@ class Gifski < Formula
     sha256 "7885c2a2a873c0c83a5190c2f83799b520d51fc9a26cc508928baf0543662a36" => :catalina
     sha256 "22b1f168132d3df440629e225e10cdf6ca7c728dc9209171b78cee446d466e67" => :mojave
     sha256 "1ba054d25b36ee72fba66d8fe109c087a2b6ff085b7bd336de3a1c01f042dc91" => :high_sierra
+    sha256 "319de0a1bc71206664ceef38d0f61fd298f21e9b23dab9060bc7f9c841334f12" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
