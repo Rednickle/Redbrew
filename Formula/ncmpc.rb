8166b27bf254673ce4650887266f73d2d734ec96
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-01-31 13:19:14 +0000

    ncmpc: update 0.37 bottle.

--- Formula/ncmpc.rb
+++ Formula/ncmpc.rb
@@ -8,6 +8,7 @@ class Ncmpc < Formula
     sha256 "d21aef0ebf95c77cdcaa82cc26b9752a3d7882a6c248139654fae3be5261baa5" => :catalina
     sha256 "379fa2e570a4987b05068b6ecb4454c9819da9e9c9670f65a0e158e43ea75afc" => :mojave
     sha256 "e05d6ca991df52846ca07fb2d536cf4644bc225f6a729ae3f180fbb5967356a5" => :high_sierra
+    sha256 "09b81dee20fda48322318416c4b6e9b8ca31b405fff26628a0b796ee7e6685f0" => :x86_64_linux
   end
 
   depends_on "boost" => :build
