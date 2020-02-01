faea0f0edd23ad0c9c2551fd41f5ed556a204906
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-01 23:44:23 +0000

    git-cinnabar: update 0.5.3 bottle.

--- Formula/git-cinnabar.rb
+++ Formula/git-cinnabar.rb
@@ -10,6 +10,7 @@ class GitCinnabar < Formula
     sha256 "14d1692f6e1ad6300606bb326dd51de8e0ecfc2f1e717c9a284bf3f340135ca0" => :catalina
     sha256 "49ad5487c52527003f21530fc3b90798f0e98c9e06349dd4b54ed1da1cd01e90" => :mojave
     sha256 "ff5dfe2f7f2dbd0af4ba0224c926a74790f1389ccb0a0192537240c6982120a5" => :high_sierra
+    sha256 "20af19fe1b26dd5228bc98f543f5a03f57882489d036482ba574e1cb8448bddd" => :x86_64_linux
   end
 
   depends_on "mercurial"
