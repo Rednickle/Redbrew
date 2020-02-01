5ba72374661768e571ad6ab4084796cc4fc42a12
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-01 08:53:20 +0000

    eccodes: update 2.16.0 bottle.
    Closes #19389.
    Signed-off-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/eccodes.rb
+++ Formula/eccodes.rb
@@ -8,6 +8,7 @@ class Eccodes < Formula
     sha256 "30d7d624aff97a5a9d75520fbeefc157f619e468169c3d813cd1506bd0c3dd1b" => :catalina
     sha256 "57b799b93c7211206b93445ac91700cb8ba559ab7c2f0dcbd8679ae6e41e0f94" => :mojave
     sha256 "c9a369d43375af207cbd90d6c148552d04341a79d67b4fa1e4126c17813c3bd0" => :high_sierra
+    sha256 "033798c9582e356a72e07ca17bc5d1a831e9c261f2957ad309644e114c992da3" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
