8deb6be1f163f7cdf79409ece8a8e1947628f0f0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-03 07:29:39 +0000

    cheat: update 3.5.1 bottle.

--- Formula/cheat.rb
+++ Formula/cheat.rb
@@ -10,6 +10,7 @@ class Cheat < Formula
     sha256 "5b5659889c08fe22d962c955804c210a508cc5ba4cefa69d81076c81d55c63b9" => :catalina
     sha256 "1165dcff4c53fdb3adc322101373d2ada3f4eaeced8a133e982377055ed8c57b" => :mojave
     sha256 "8160f43431907f2d7aed7a9a527b5f4c5760c67eeeb788deb2196396ab297a55" => :high_sierra
+    sha256 "5c6664639c37fb77cb407d7c7c97c25dfe2226c12614e2a88142ee8877ead7f0" => :x86_64_linux
   end
 
   depends_on "go" => :build
