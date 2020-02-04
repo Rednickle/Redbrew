6efd291888189076a7c8cfa61f945e94fc10c0cb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-04 21:05:09 +0000

    tmpwatch: update 2.11 bottle.
    Co-authored-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/tmpwatch.rb
+++ Formula/tmpwatch.rb
@@ -10,6 +10,7 @@ class Tmpwatch < Formula
     sha256 "d2e77af7339bd09c9b7352a1646c79e15db6b43c27537b4af59efbc51e23f114" => :catalina
     sha256 "4f7b1e540daebe79e1ad64b2e4e4b7214074c05150dcd9de7a5ffe9c12e6b9bb" => :mojave
     sha256 "24a734b4cf32ce5720e1c089060fa66dcc2d9dde437804a4dc147f81d9cd8512" => :high_sierra
+    sha256 "ad948fff4e49cb83eaec2e20f57f8038a6a0ffa9c8cc2ca8657e91a2e201aee1" => :x86_64_linux
   end
 
   def install
