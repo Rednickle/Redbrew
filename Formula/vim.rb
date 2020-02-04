ed1cadfed75270811c435095df9ad30d972f5fb9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-04 20:16:45 +0000

    vim: update 8.2.0200 bottle.
    Co-authored-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/vim.rb
+++ Formula/vim.rb
@@ -10,6 +10,7 @@ class Vim < Formula
     sha256 "cf70a7dfe2cd79a2c70b71b826989d1525f345352177311b3efdd2c7d3e3bde7" => :catalina
     sha256 "2ae924535b4b74c831b586f00547978e0c7afa1af71f203caaefd4e90551d88a" => :mojave
     sha256 "b8cf0690810047454a3425f27ef7189219c2c8c59efca9712620a6ec33ae4ac1" => :high_sierra
+    sha256 "f9fa105f2138db21b52569f4bc0a26109dc620de0eed5f6209774be3c665099e" => :x86_64_linux
   end
 
   depends_on "gettext"
