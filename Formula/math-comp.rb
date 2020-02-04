070d84efc34933461f2d5a2ec689fa76f1464f16
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-04 21:07:14 +0000

    math-comp: update 1.10.0_2 bottle.
    Co-authored-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/math-comp.rb
+++ Formula/math-comp.rb
@@ -11,6 +11,7 @@ class MathComp < Formula
     sha256 "6a5e3378d663d69eae75fe6b59a0531a00d88e474f8f7f5a3e255cac76b14e63" => :catalina
     sha256 "127f7df93e47816106aa637d9ee65190d23c3d6829efa648e03bb4e5fda8df8d" => :mojave
     sha256 "6a5e3378d663d69eae75fe6b59a0531a00d88e474f8f7f5a3e255cac76b14e63" => :high_sierra
+    sha256 "805a842a5d103f372fa1d4218166b0e9125be08584d00b45a892410aa9f31ad6" => :x86_64_linux
   end
 
   depends_on "ocaml" => :build
