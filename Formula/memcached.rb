b3fff421ccc10e704611f2efb2403ba4a92388fe
Dawid Dziurla <dawidd0811@gmail.com>
2020-02-04 16:23:03 +0100

    memcached: allow to run as root in test

--- Formula/memcached.rb
+++ Formula/memcached.rb
@@ -54,7 +54,11 @@ class Memcached < Formula
   test do
     pidfile = testpath/"memcached.pid"
     # Assumes port 11211 is not already taken
-    system bin/"memcached", "--listen=127.0.0.1:11211", "--daemon", "--pidfile=#{pidfile}"
+    if !OS.mac? && ENV["CI"]
+      system bin/"memcached", "-u", ENV["USER"], "--listen=127.0.0.1:11211", "--daemon", "--pidfile=#{pidfile}"
+    else
+      system bin/"memcached", "--listen=localhost:11211", "--daemon", "--pidfile=#{pidfile}"
+    end
     sleep 1
     assert_predicate pidfile, :exist?, "Failed to start memcached daemon"
     pid = (testpath/"memcached.pid").read.chomp.to_i
