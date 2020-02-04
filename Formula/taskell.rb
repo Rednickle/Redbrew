ad4de79b8b1c5e1d245243c4a14a23c33b47d5f1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-04 20:39:48 +0000

    taskell: update 1.9.2 bottle.
    Co-authored-by: Michka Popoff <michkapopoff@gmail.com>

--- Formula/taskell.rb
+++ Formula/taskell.rb
@@ -13,6 +13,7 @@ class Taskell < Formula
     sha256 "111fd4a43d0dd5053517872aaa1fdf8cceb24b212ffa422e580db44b93f3a37f" => :catalina
     sha256 "9e18af03b8c6cd94d01724f6d7247f48b92c73c11eb0c14176ce5ade0d0f47a7" => :mojave
     sha256 "78819149a4d0cd8e528d64b4b37ddc9f98c2a1aa56c0a223fb062b8be06530e3" => :high_sierra
+    sha256 "e5ff98b60c6410274383f138876791fc852fc95a514343e8511396620c2af079" => :x86_64_linux
   end
 
   depends_on "cabal-install" => :build
