7fb338487c000d15b78363cd26b67e0874476522
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-04 20:11:40 +0000

    pstoedit: update 3.75 bottle.
    Co-authored-by: iMichka <michkapopoff@gmail.com>

--- Formula/pstoedit.rb
+++ Formula/pstoedit.rb
@@ -8,6 +8,7 @@ class Pstoedit < Formula
     sha256 "f048d902c088f0625c0c9e18d84b159493775b40e742812b040e7b517900260a" => :catalina
     sha256 "1f3ec91e58d95e08081694b43e031ed83f13a73cecff15c55c532268282b0ad1" => :mojave
     sha256 "22710dd8997d40cec3492c40960a9966b80b386bdbd3fed46515c66bb25053d7" => :high_sierra
+    sha256 "75adb28562720aab3b02a1cd11ffa1dd0aa39a8e0ff26f398bcf0831d6c44c90" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
