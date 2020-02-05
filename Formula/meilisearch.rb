b86b2ac6aedf9955ad06bf483482eebaf465a18f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-05 06:58:56 +0000

    meilisearch: update 0.8.4 bottle.
    Co-authored-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/meilisearch.rb
+++ Formula/meilisearch.rb
@@ -9,6 +9,7 @@ class Meilisearch < Formula
     sha256 "4c0cf2771d4f20ac3d1e48e92c5d5e8e2ef9306b0d3689b45e87c2db064dfddd" => :catalina
     sha256 "dc832f214ea74897de8b6713bc6fa0e037209af0823770f3c6a7e5206c3394a4" => :mojave
     sha256 "f1e152cd4e0856051e2227ae869ff37b11181f645b5c544ffce00c035d8ce252" => :high_sierra
+    sha256 "a41be5cf0e99e3e2b8f628791fe8d89c54395a439cc5c05a3a961729377f0feb" => :x86_64_linux
   end
 
   depends_on "rust" => :build
