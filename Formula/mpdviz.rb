132872646a5ec785e1a2008a887344e1c5a8d91b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-05 05:47:23 +0000

    mpdviz: update 0.4.6 bottle.
    Co-authored-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/mpdviz.rb
+++ Formula/mpdviz.rb
@@ -12,6 +12,7 @@ class Mpdviz < Formula
     sha256 "f65d98aebf9bee4de6dce79983fe91b20f95d731be71999021639bdd7c8c14e9" => :catalina
     sha256 "dcc5deb65626637f6a4182f96e1ccd441a462c55657e134d2277a70be8246278" => :mojave
     sha256 "1f1b0b01dcfd1c37b7820ae93a9775abcd5b1abcdb8a5a4038d348e517b3c87a" => :high_sierra
+    sha256 "73630b8bb9a93571f252428d2eb19b1ae446cea166726ea44e2f5d1f854efd9b" => :x86_64_linux
   end
 
   depends_on "go" => :build
