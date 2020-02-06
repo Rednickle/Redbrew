f7856cf7df11b240976b0cc8d73a1ad7bd49e392
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 18:57:29 +0000

    vttest: update 20190710 bottle.

--- Formula/vttest.rb
+++ Formula/vttest.rb
@@ -10,6 +10,7 @@ class Vttest < Formula
     sha256 "a8dbf4d1fa88673fc6aba2c41b13ff917be9669220852bd4a2295d44c60c8f7a" => :mojave
     sha256 "8d3bd4169069a0e8b87b9715db1d6518acb4e335157f7be25b867a48c13d023f" => :high_sierra
     sha256 "206744ef6b93ed8a6be0921ebd964b4e8e03e92b057afbcb9dff0806bf6ef747" => :sierra
+    sha256 "d75d58300b280427b23a44d88f233c1f7d7324b88e433d289cf4aea52372975b" => :x86_64_linux
   end
 
   def install
