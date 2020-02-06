92c458a1821c237e5f41b46d958a4d4aca8eec37
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 17:50:15 +0000

    fdupes: update 1.6.1 bottle.

--- Formula/fdupes.rb
+++ Formula/fdupes.rb
@@ -14,6 +14,7 @@ class Fdupes < Formula
     sha256 "4838e3104ea06e61d7acce5f482ff80bae1d634f29a1edd44e388b9f8c63f19b" => :sierra
     sha256 "b0b7afcd64459cfc3c2bb95ac92e1aa7f6531fbf05603e472c97c5d4e72c94b7" => :el_capitan
     sha256 "ce706b289e019a30c4d07a307ae2c5c10ef1b886e4ee8e5e62f7275a9213a370" => :yosemite
+    sha256 "a5e2a52f2502942856899ae9332e0f279b83fb27fcfc9fee00ace198d95fc202" => :x86_64_linux
   end
 
   def install
