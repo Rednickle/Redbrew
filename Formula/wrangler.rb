64ecef730303ba47d23291c4e369fa5c4b768b4d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 19:13:26 +0000

    wrangler: update 1.2_1 bottle.

--- Formula/wrangler.rb
+++ Formula/wrangler.rb
@@ -21,6 +21,7 @@ class Wrangler < Formula
     sha256 "129a7cacfe094491f22d885546ac36dcdd60c601f391b290970fadbc5777ca22" => :catalina
     sha256 "6ca8b450fc9c98d43c9e6494bd725d2785ed70f010193237bb34c13f54be7303" => :mojave
     sha256 "7708561c89c92c61b67907ca43fa351e9a39da572c43e1f9d15d4dc0cd4855da" => :high_sierra
+    sha256 "c5b90dd316ebf81b24f0379ffc289139d9dbb8838a4be4fac5555ffc576798a6" => :x86_64_linux
   end
 
   depends_on "erlang@20"
