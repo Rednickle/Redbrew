d4f0c75ad2ffdcdb7c0b3d9103bd243d7e7beb49
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 17:08:54 +0000

    helmfile: update 0.99.0 bottle.

--- Formula/helmfile.rb
+++ Formula/helmfile.rb
@@ -9,6 +9,7 @@ class Helmfile < Formula
     sha256 "e6db438757918f8cf0f7db535fd87764006fcec67be04f33b59878e669d58cfd" => :catalina
     sha256 "9cc8f043ed41b5b7d4c6c0ba3e616f1c7e1d31fb77c94f0c8835fa81741b64f2" => :mojave
     sha256 "912f944cea0c4ae64a28bb124da57fe146366658d15f30a575d8d523ca2234e5" => :high_sierra
+    sha256 "ddcdfca865fe32d15ebbf3f85c0259ec64794438b2e11c2f3d6c2437bb42c0f0" => :x86_64_linux
   end
 
   depends_on "go" => :build
