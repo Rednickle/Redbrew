38afdee74097bf325e2b1269f884d7509815634a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 17:16:38 +0000

    procs: update 0.9.6 bottle.

--- Formula/procs.rb
+++ Formula/procs.rb
@@ -9,6 +9,7 @@ class Procs < Formula
     sha256 "afb83ea340dd2d4afc89e897e1d300b74a3cbbc00b43a956ecfca79a0d30b291" => :catalina
     sha256 "7df07639eca8edd6382ef39b8c04fbe62b130f9125e9a617ba5d1d28c27c7a94" => :mojave
     sha256 "2d77df66127b1ad242cad0250aeca50c1c8692061d5c405ea7b17604b636c8ee" => :high_sierra
+    sha256 "fd9aa2a2e652553d282623bf4765f8e784ef87b0ef808252bd67bbadf8e04cdd" => :x86_64_linux
   end
 
   depends_on "rust" => :build
