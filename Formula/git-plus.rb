10734f0e4f68761178a0105c2aead31daea87d42
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 18:00:57 +0000

    git-plus: update 0.3.3_1 bottle.

--- Formula/git-plus.rb
+++ Formula/git-plus.rb
@@ -13,6 +13,7 @@ class GitPlus < Formula
     sha256 "6bdcfe17d7687fad2e523e104b473744831806b1bf5915d9248a6d36dd6d28e6" => :catalina
     sha256 "ee281bb04fe93d458c63dba2648861dd30e2003f5bb8871e650959134c6daca7" => :mojave
     sha256 "72ec0aecb258232a837709d55ae03b4068db623c5f6b214bb2cbedd826c1152e" => :high_sierra
+    sha256 "ba8f4296af3ed44179ec598b8f60fe644d308ed7fccd29e862c6f1a7e0edd4c9" => :x86_64_linux
   end
 
   depends_on "python@3.8"
