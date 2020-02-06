c47a79549d6f2c72cc77d234dd758e0ddf439e4d
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 18:07:14 +0000

    internetarchive: update 1.9.0 bottle.

--- Formula/internetarchive.rb
+++ Formula/internetarchive.rb
@@ -11,6 +11,7 @@ class Internetarchive < Formula
     sha256 "57f86174583ae4ce3a151c2caddaca95da89acc177fa15be72cf4a88f7800013" => :catalina
     sha256 "de550a1ea15165b80feb9d0b0d75a0879107071c6e0d67543c1b0635a481b509" => :mojave
     sha256 "6c78f9ce965b08e08272f1c0c54ad0b704878671773c7b6f0a67b26d04f04996" => :high_sierra
+    sha256 "60edade43ec1c7adca282bfcc65a7bef05b4af55610ab00329a5fc8ea551907d" => :x86_64_linux
   end
 
   depends_on "python@3.8"
