9156b9b196a9e4e69612c8ed9ab9ba41fc49db99
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 17:18:44 +0000

    embree: update 3.8.0 bottle.

--- Formula/embree.rb
+++ Formula/embree.rb
@@ -10,6 +10,7 @@ class Embree < Formula
     sha256 "eeadbb69a2e0439b9c2d67391dec7ceefc77de75ed0535ec49af93d8ed8c41aa" => :catalina
     sha256 "fd0ce3dbe8248f382168ce9bccf6d31145de02ee062f333cba5eb6a966f4d532" => :mojave
     sha256 "5a5641cf4b93a026bc362ba695a5b92788c92ed975222959797c697e20a8be16" => :high_sierra
+    sha256 "c5282d1d6b6829c017b82e54ce56a92ea0087247ce08d43c05b6813b4e724517" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
