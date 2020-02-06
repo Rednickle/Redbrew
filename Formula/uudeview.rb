89385f9e895e3d300765e3e1a954e2acddcf0fdc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 18:56:25 +0000

    uudeview: update 0.5.20_1 bottle.

--- Formula/uudeview.rb
+++ Formula/uudeview.rb
@@ -11,6 +11,7 @@ class Uudeview < Formula
     sha256 "9b5990b5b763e90614bd2d074e670c20e834541d60082a4e78f90d67a65da5c3" => :catalina
     sha256 "2869df0b09975172227dc83be6d667b3d0f8e4f2cf0f6d9ec0cd3fdca02727f4" => :mojave
     sha256 "7bb4c57755efed1b4208d234a0017d785757da04ca8f8e43c92980f3fe16b85c" => :high_sierra
+    sha256 "d4baa6b550891384a0a771d33082e52d819b4271f60cac54750aa03f17280920" => :x86_64_linux
   end
 
   # Fix function signatures (for clang)
