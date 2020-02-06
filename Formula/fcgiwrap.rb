4050b6fe6fbfeec274275e7ee75a857817e8c523
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 17:53:19 +0000

    fcgiwrap: update 1.1.0 bottle.

--- Formula/fcgiwrap.rb
+++ Formula/fcgiwrap.rb
@@ -12,6 +12,7 @@ class Fcgiwrap < Formula
     sha256 "c0a70c3cc726788dfac52d8b23c79c1a4ef31a8c7e1418ac335cfe182b94f05d" => :el_capitan
     sha256 "ea03eeafcd71e07c2e608bc974a00cf642b253de24eb7bd587155c89db2fffad" => :yosemite
     sha256 "15a4dc62dba901bdc25f8d898674069b8cad09b3d2c00458900f31c143305a4e" => :mavericks
+    sha256 "085d05ae121ccf4e9fbd71acafb4687b44648b878628780c5bfeed3d8f6101ee" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
