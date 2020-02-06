84b1d972c5a5c4603424f233fc5e9e387187b4cc
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 11:05:18 +0000

    kcptun: update 20200103 bottle.
    Co-authored-by: Dawid Dziurla <dawidd0811@gmail.com>

--- Formula/kcptun.rb
+++ Formula/kcptun.rb
@@ -10,6 +10,7 @@ class Kcptun < Formula
     sha256 "44a2f2976e90c45e543513bae02ce519e38936f33075674202ec58496e43d0ca" => :catalina
     sha256 "737d4c27a6c05bfd8d7010c308961bc22896e3604d895cd59be4d9d71abd6d03" => :mojave
     sha256 "fad9acf377d769198c33185c3259577c8773d83974da353b266894a674852620" => :high_sierra
+    sha256 "909b69752864665e906420672a84cdb44ad21d714d1d3f3e0b4aa0eda45ae986" => :x86_64_linux
   end
 
   depends_on "go" => :build
