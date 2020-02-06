c70629c0a49bacc6080d27ff4f796cef058e41ec
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-06 17:11:56 +0000

    screen: update 4.8.0 bottle.

--- Formula/screen.rb
+++ Formula/screen.rb
@@ -19,6 +19,7 @@ class Screen < Formula
     sha256 "f3787a0e1c889106ab14d89c4f1bed001716ce1eb79e44e56b20e71b7448e172" => :catalina
     sha256 "30dfe7b1bc6c74d64be57224852e50ebd5d4c6d4939872eaceac5f06d9935208" => :mojave
     sha256 "1e63b4fd4ae798111980a7d9ed47c3fcb867cbad2c4253164b55722efc65d53e" => :high_sierra
+    sha256 "090ace939980839facaeefb986074a6a564e31d8b0ddd0569c2fc35a26a07365" => :x86_64_linux
   end
 
   head do
