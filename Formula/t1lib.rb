a1e9ea11fdb08da41c14a8e18e554e57f0ac0014
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-07 22:55:07 +0000

    t1lib: update 5.1.2 bottle.

--- Formula/t1lib.rb
+++ Formula/t1lib.rb
@@ -14,6 +14,7 @@ class T1lib < Formula
     sha256 "fa356a5405f5b0cf57c15ebd5b680c215e1e498189914e9b9663eb132655a8c1" => :el_capitan
     sha256 "6d1bf242eb7a5201180b9d4b505a7f83663802383d358180cea696714ae57fc8" => :yosemite
     sha256 "ec107b30d4b9a95bbc094a7a944cab862ed78a875c5ed0002aa7232ec514e9d7" => :mavericks
+    sha256 "a6946d44932900ae0218f48f9f56c3353f66b11ce1b947c2d832f55f0d5964de" => :x86_64_linux
   end
 
   def install
