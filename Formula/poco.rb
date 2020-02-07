bd15661ef28757e1b418f19e4918ecf022ad6833
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-07 22:33:43 +0000

    poco: update 1.10.0 bottle.

--- Formula/poco.rb
+++ Formula/poco.rb
@@ -10,6 +10,7 @@ class Poco < Formula
     sha256 "67ee47718a0d6aa3cd20cdc676925c2b9e15e3776d049c6fa1e04002cc475884" => :catalina
     sha256 "53bb526ee1c9b8b5dd58b22c53aa06b7d59dabafdadddc83d10abbd5db04d364" => :mojave
     sha256 "dec904f7fb887043b0ddea47622a359278e08988c6b5121e3c4fd09e76703274" => :high_sierra
+    sha256 "0e4e7e4cadfb66e81bf0eb79d14ee30b4c94bae4ad909329c03c532dc3e0e4bb" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
