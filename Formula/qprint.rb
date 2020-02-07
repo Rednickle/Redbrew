a87494dc7abefc07189f57a8bca1d7dd93207f1e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-07 22:34:42 +0000

    qprint: update 1.1 bottle.

--- Formula/qprint.rb
+++ Formula/qprint.rb
@@ -13,6 +13,7 @@ class Qprint < Formula
     sha256 "9660443356a1f9571b39ea496349482e17f7c0d06829dd06945ca7680291c0bf" => :el_capitan
     sha256 "92470bcb0bd97c4d13181969ebeb0339faa85338ad139bf4a5ac19550635f5c1" => :yosemite
     sha256 "fbf2eadbf60b30557e3741e28545070bb377602aa8f1c1c49b5f65375381a2c4" => :mavericks
+    sha256 "7b59ae91b286cf33e2a00853cf570d7d2113f46322383188a8419e06b7b86ea7" => :x86_64_linux
   end
 
   def install
