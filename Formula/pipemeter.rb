04a0ee75fff41e99d6b2b8e0910b1ca7591c3ced
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-07 22:28:56 +0000

    pipemeter: update 1.1.3 bottle.

--- Formula/pipemeter.rb
+++ Formula/pipemeter.rb
@@ -13,6 +13,7 @@ class Pipemeter < Formula
     sha256 "bf99d50927a1277c1a481af0eea25314dfbd1449dbd1394368f7a933da367e9a" => :el_capitan
     sha256 "cee0b494c5f7647d0c597e90dbc8be2c7b759d53a12cd87f89f9620b9260c3ac" => :yosemite
     sha256 "a1bd1a5466eb44aeeba7ab2563f3bd34978d248deec50964624f985f066fe2bf" => :mavericks
+    sha256 "464fd74a6d4674581f7b416007baec492924646e87a8dd782b939fd9936688e4" => :x86_64_linux
   end
 
   def install
