099b086b394abf6713304fcbecbc0f1b7ee02183
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-07 22:36:31 +0000

    qdae: update 0.0.10 bottle.

--- Formula/qdae.rb
+++ Formula/qdae.rb
@@ -9,6 +9,7 @@ class Qdae < Formula
     sha256 "d951231205b4f4faf3e4f829665d25c82d236f3f52339dd5664fb8adb46e68eb" => :catalina
     sha256 "290d931e61684c53227e0a16d808427eb7218fbec76c57eb250c03dbf15bb6b8" => :mojave
     sha256 "945b28c4354053f3ebd81bb868ef6a14d8fef1c32d6cebd73455bd17f17332ae" => :high_sierra
+    sha256 "741517aa60c4fc9356f075b46d40393d33dfd9987c1c1b51608a55d0e08a03bf" => :x86_64_linux
   end
 
   depends_on "libxml2"
