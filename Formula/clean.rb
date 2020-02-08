63333380899f8895ec237be9f020da8e669a0752
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 18:58:41 +0000

    clean: update 3.4 bottle.

--- Formula/clean.rb
+++ Formula/clean.rb
@@ -13,6 +13,7 @@ class Clean < Formula
     sha256 "7a433c07eb3c8a3846d352ddf27a6ac32fdc6528b6b2e6212f78318ff0f04a6a" => :el_capitan
     sha256 "f06ca56bca5a139222603fac5d84555a1af9812a2dd669e44501b2022a16eef8" => :yosemite
     sha256 "a847561f68c5d636ee3c0802dccfc7a7b1a5cf66030f026b074c000f6cea258d" => :mavericks
+    sha256 "21a316333cbdf717809703b81bb43aa4ef5541dc00a87fd24fdff0fe06474cc4" => :x86_64_linux
   end
 
   def install
