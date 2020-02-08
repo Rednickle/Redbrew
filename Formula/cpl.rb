ddf77835d1638e9903d561c80ed93d0dcce30ebf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 19:03:54 +0000

    cpl: update 7.1.2_3 bottle.

--- Formula/cpl.rb
+++ Formula/cpl.rb
@@ -10,6 +10,7 @@ class Cpl < Formula
     sha256 "9f2525cb8eec2b2312a9b139b30b0f1ac4b1a0fe8e417f82bb31b2d4fe3c7372" => :catalina
     sha256 "ad2412ebad8b4711e1acd593a2e314c325f8def7a54d958b5adc9e65412283d8" => :mojave
     sha256 "81988a1d17cddd8d5b6d9167086ed3cf2a4ff19e295e3302beff793f2ddc3854" => :high_sierra
+    sha256 "311124b6e6616708403d20fce7e67f0947869a5e1f36b87e884e67d51f8208f0" => :x86_64_linux
   end
 
   depends_on "cfitsio"
