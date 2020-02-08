c0d7bc2ab57e3d1cff667c7f181bf54f1d4762db
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 21:29:26 +0000

    dosbox: update 0.74-3 bottle.

--- Formula/dosbox.rb
+++ Formula/dosbox.rb
@@ -10,6 +10,7 @@ class Dosbox < Formula
     sha256 "de46ee6c3c638829ba3b9dc3ee009811d26a19359d10804b9ff93706df2a6863" => :mojave
     sha256 "66b1b073b1ae7db629c64f66249254aefcb8fb6585c065c858a364bd258785d4" => :high_sierra
     sha256 "3bd2c41c7f76e214c0964acec02723d2a2a611eca92cf5edb93c029333a78adf" => :sierra
+    sha256 "e39aea7e583b6ec129959c2bba3aa75d70a80ce21820bfc54bd15a57b106197e" => :x86_64_linux
   end
 
   head do
