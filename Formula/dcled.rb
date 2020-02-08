852d89cf956627ebcba2ce95dfd568b57f163671
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 21:27:31 +0000

    dcled: update 2.2 bottle.

--- Formula/dcled.rb
+++ Formula/dcled.rb
@@ -13,6 +13,7 @@ class Dcled < Formula
     sha256 "53d07c9548eaeba12645e944ce92c27a02667758176815220dc4ee2a8945c661" => :el_capitan
     sha256 "2ead7c4eb3c170690890c294936a2d3fc39def2fc332ce4c1da6d17cc8f91b50" => :yosemite
     sha256 "47a0b2e1eba58932936c25726d631d19f0f2a0a7b8872aff9e1d3a83b4e3cfc9" => :mavericks
+    sha256 "f38a543b5462687bb4a85af64c955326f5aaa0d635186d585b1b93ed01d1297c" => :x86_64_linux
   end
 
   depends_on "libhid"
