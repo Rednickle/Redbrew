97e22bfb32b29b2125519111cf40dbb621e22058
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 21:27:34 +0000

    docx2txt: update 1.4 bottle.

--- Formula/docx2txt.rb
+++ Formula/docx2txt.rb
@@ -13,6 +13,7 @@ class Docx2txt < Formula
     sha256 "c3a67138c91e968e6c2a6ff1033bca0fe8527ebdcaaa208194c073b4f75dd453" => :el_capitan
     sha256 "78154a4b95613538a9d508c521d74d0bc6b398b005de4468b4cb4e62c3208b8e" => :yosemite
     sha256 "2d9f0f37b4c6c5a37f22a4b0e7cdc6d440e842d2d3e7df433ccebf1b03cf80cd" => :mavericks
+    sha256 "e97a9776dc1cd6984be64900928a3dfc0b2e18c8b4fde1d5f4b866909c7143a9" => :x86_64_linux
   end
 
   resource "sample_doc" do
