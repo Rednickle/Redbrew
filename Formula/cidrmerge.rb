353ba3d9be73056dffee7de30bbb35ac064675ef
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 18:57:48 +0000

    cidrmerge: update 1.5.3 bottle.

--- Formula/cidrmerge.rb
+++ Formula/cidrmerge.rb
@@ -13,6 +13,7 @@ class Cidrmerge < Formula
     sha256 "7e607252679cd1648e6c9f48ebbeaa2379ce089ad87815bd6636e65dcedebc7b" => :el_capitan
     sha256 "20c6f57fc6081c8d27d2e68b81e3d4c5cd68e7c799dc30e076f45ee71b42e69d" => :yosemite
     sha256 "89b2d5b31bd190e0aa8837b84f3a684cf01b4501321e898507e5d8dd809f09d7" => :mavericks
+    sha256 "0d9f0abe1ec4904e40318954285c522fa2bfce4117c3ae70386b7f86cd01a0d2" => :x86_64_linux
   end
 
   def install
