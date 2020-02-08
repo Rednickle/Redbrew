27adb5e759604026fef49317059c6770ae2012c9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 18:57:24 +0000

    cheapglk: update 1.0.6 bottle.

--- Formula/cheapglk.rb
+++ Formula/cheapglk.rb
@@ -13,6 +13,7 @@ class Cheapglk < Formula
     sha256 "d76d29db8ea0201fcef949e02cbddb1c06311dece796a263192ffef487a3aa2c" => :sierra
     sha256 "497a5399738c026d318d3213b764f20fb80ccea94181919fad2e80eb75086055" => :el_capitan
     sha256 "8351c9dec39fd8e860e50a8e693e1c648def81c3ddcbdd3a856f656d585b0082" => :yosemite
+    sha256 "bef666b5ffc26b22b763537ee7dc2b26854f2e329fb950194e4b5fd80151e50f" => :x86_64_linux
   end
 
   keg_only "it conflicts with other Glk libraries"
