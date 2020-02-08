7d6ec6fe661da89b0b203e973a5980834c8b24f2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 18:56:30 +0000

    c-blosc: update 1.17.1 bottle.

--- Formula/c-blosc.rb
+++ Formula/c-blosc.rb
@@ -9,6 +9,7 @@ class CBlosc < Formula
     sha256 "4896df1cf1f325a0499f55801d5c9cace67ce4234e029532a1a5acb912bb8a12" => :catalina
     sha256 "fd241bcebdb57fa54ffb06ab55f0f59cb213346e7d9f217a7aece1a4317cec6f" => :mojave
     sha256 "8f961c2826d79cc0e80a5154896120d05ccd57621dbfb1257349979e9ce4ddf3" => :high_sierra
+    sha256 "a3ba4c4d6edacaa10b3ea6bea1e7103ad2cc5be1f4e6e62f44a0fc30e24724ea" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
