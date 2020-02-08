0ecf8f0e5a7bf754dd357711d536710baa53cbb0
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-08 21:26:50 +0000

    disktype: update 9 bottle.

--- Formula/disktype.rb
+++ Formula/disktype.rb
@@ -14,6 +14,7 @@ class Disktype < Formula
     sha256 "c1f45dc2bdcec2e3b56741bf03d673f3a99534f851d1c77de59d6832d0f75236" => :el_capitan
     sha256 "cc767e7be270b683021ecb2ef3dd16c77b05e9cdf34ed524c942a89514284f57" => :yosemite
     sha256 "36d5db65030be2813c20d3d9df48ae5f2114ea1a3b4f5fb2db324169a52c3c87" => :mavericks
+    sha256 "6aeac4c1730506db19d26ac09455d04351060539fe9210bbf12263181fee22d7" => :x86_64_linux
   end
 
   def install
