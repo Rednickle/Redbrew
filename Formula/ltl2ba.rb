28e62220161cfb2c594db7c2f5ca520293524f11
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 19:48:28 +0000

    ltl2ba: update 1.2b1 bottle.

--- Formula/ltl2ba.rb
+++ Formula/ltl2ba.rb
@@ -14,6 +14,7 @@ class Ltl2ba < Formula
     sha256 "45916a60bb32849e1bc3709d019dcf6ac9d140d92e6f6b65bb5ca05de5c63e3b" => :el_capitan
     sha256 "d4c93b1f70b126540dae9d77e16f7e0b42e58ff1997fce63f51820621693588d" => :yosemite
     sha256 "75c0b88b4be2658bc02feb02214f833f7b27758381924b3e3a742ee9309579f6" => :mavericks
+    sha256 "3f06921081ec96fbb05c3802678389c21870823300ed6ba97c136568fa5c11d0" => :x86_64_linux
   end
 
   def install
