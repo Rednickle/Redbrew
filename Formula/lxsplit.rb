2f6858b37de21cd4005ced52b3ec2f776b845ff9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 19:49:05 +0000

    lxsplit: update 0.2.4 bottle.

--- Formula/lxsplit.rb
+++ Formula/lxsplit.rb
@@ -13,6 +13,7 @@ class Lxsplit < Formula
     sha256 "25699d54183a01f446015fb02521a50b3967ef2d250e56bb1fe3fd0a5aaec2e1" => :el_capitan
     sha256 "d7d8d9eb204599056a4e109c63114c90e3be797d2be700e114cc3675d8eba0bb" => :yosemite
     sha256 "da0ee88012d21dc120c7247fd856305fe14a213e38babcd39ab652af06483b7e" => :mavericks
+    sha256 "10c851e0f17adfd0985dc049a29d806953bea2dfff0420002ad0c31da71eb0ab" => :x86_64_linux
   end
 
   def install
