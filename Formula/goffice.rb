bb01efc032b5b80e9656ddef873c661b01ef3da8
Issy Long <me@issyl0.co.uk>
2020-02-09 12:39:32 +0000

    goffice: Fix build for Linux (#19546)
    https://github.com/Homebrew/linuxbrew-core/runs/434444981?check_suite_focus=true
    
    ```
    checking for perl >= 5.8.1... 5.22.1
    checking for XML::Parser... configure: error: XML::Parser perl module is
    required for intltool
    
    ```

--- Formula/goffice.rb
+++ Formula/goffice.rb
@@ -32,6 +32,9 @@ class Goffice < Formula
   uses_from_macos "libxslt"
 
   def install
+    # Needed by intltool (xml::parser)
+    ENV.prepend_path "PERL5LIB", "#{Formula["intltool"].libexec}/lib/perl5" unless OS.mac?
+
     args = %W[--disable-dependency-tracking --prefix=#{prefix}]
     if build.head?
       system "./autogen.sh", *args
