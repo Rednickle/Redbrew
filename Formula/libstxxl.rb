727a7cb87461f3f2f5179ba4f9f652b0701c5840
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 19:44:36 +0000

    libstxxl: update 1.4.1 bottle.

--- Formula/libstxxl.rb
+++ Formula/libstxxl.rb
@@ -13,6 +13,7 @@ class Libstxxl < Formula
     sha256 "8753b0c9ec11f1f6195fa52c346b4d6015652931908156dcdb4468cc94103d1c" => :el_capitan
     sha256 "fbefd1ceb6f328ef77b9a005f5bc3c93fe23fa68d60b4a5e126a68a6a9d8e17d" => :yosemite
     sha256 "5a64626281265368e41f51089271c031ae8782651d65fe012f8a86949e37573b" => :mavericks
+    sha256 "d1454663e06c340f15a4452fe41aeea606768df5958d9f71db6d62148cfa574a" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
