dc9871c5d10a5c21b5bd0ea7e97dffff81007309
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 22:31:26 +0000

    ncdc: update 1.22.1 bottle.

--- Formula/ncdc.rb
+++ Formula/ncdc.rb
@@ -10,6 +10,7 @@ class Ncdc < Formula
     sha256 "4490571bb2101e00e3edc39ab25baf75a63dfd9617381ae0f102a274a2fbf622" => :mojave
     sha256 "bd143dd032839762597253796c562607e43c654c128d4f4162fd382fd660dbfd" => :high_sierra
     sha256 "59f5011b9c39b78c75a2bf5d93ae398a16660852a3b8fd8ed3a3389ea463fbf7" => :sierra
+    sha256 "f72b8b67691feae62042732b2915c4fb47e9e653fb57d4c2fab44f62c8eef68e" => :x86_64_linux
   end
 
   head do
