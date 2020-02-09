69e671e17c706706488c89c3e4ff8d33925944ff
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 10:56:00 +0000

    gnome-common: update 3.18.0 bottle.

--- Formula/gnome-common.rb
+++ Formula/gnome-common.rb
@@ -13,6 +13,7 @@ class GnomeCommon < Formula
     sha256 "a5ad22711bdc05e9dbe4c4891ad06f146bc81b4d0d7d737d582f32e36f4e6fc7" => :el_capitan
     sha256 "7e3512e3a7c39f759ab9f3642831961b355f3f83ae6e19a26fdaf91739870e23" => :yosemite
     sha256 "a96e5dedc2888b6caa326da0abd8eb7d3f1426407e8bef82a6ba0f41adb7016a" => :mavericks
+    sha256 "b8256d52c330d2b70a505bba4df870cc7efe1b8b4f534f38f598d74a2ae2626b" => :x86_64_linux
   end
 
   def install
