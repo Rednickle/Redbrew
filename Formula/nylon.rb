08621d237609088f87e86b2a3cdb9ec937bfa174
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 22:33:04 +0000

    nylon: update 1.21_2 bottle.

--- Formula/nylon.rb
+++ Formula/nylon.rb
@@ -11,6 +11,7 @@ class Nylon < Formula
     sha256 "cb2cbfbd8df94b8581a116807075daf9fadbe9b9c5cfa537ea30dfa76537dd5c" => :mojave
     sha256 "3df9b3197c8dc9a227221027047c8de77ddb6ad9ce2edd14544c2d6e4923b660" => :high_sierra
     sha256 "b7eeab5896aaaca9c73166e519d092a71f15a36e800a28742729f8cbc270e6d8" => :sierra
+    sha256 "efbd60188c728e74d073afe553a4512222850758de3221a4fe8a51486a48dc09" => :x86_64_linux
   end
 
   depends_on "libevent"
