5b43158106700d18630685b959e79a49031ce7f9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 06:46:14 +0000

    flint: update 2.5.2 bottle.

--- Formula/flint.rb
+++ Formula/flint.rb
@@ -10,6 +10,7 @@ class Flint < Formula
     sha256 "f6df4b3a018320efc58b97580af06979ade00007812c6deb83ba150911f1bff9" => :catalina
     sha256 "464d909acc9801c865e85023ab33db1abea9e9a28f81808979d7650370d67417" => :mojave
     sha256 "952ffcbee931dc8554a69b2828b6ac0293e3981223762eeff0dabab42e365d0a" => :high_sierra
+    sha256 "25d5b36dd29ab3b814394f71d363180c92386a33abec8d5dbf353806be4d9cc0" => :x86_64_linux
   end
 
   depends_on "gmp"
