63c1db8b7df7674d0269db9624380b5349389b5a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 19:46:45 +0000

    lpc21isp: update 1.97 bottle.

--- Formula/lpc21isp.rb
+++ Formula/lpc21isp.rb
@@ -14,6 +14,7 @@ class Lpc21isp < Formula
     sha256 "c12b33d514be2490a3a5bb9d3c1f8468e7e24d13eee0a636a9d067f486af59fc" => :el_capitan
     sha256 "10deab3f8de3cb88b27ea38344ba7c641b758faf45f9a247f3fca968f6db456b" => :yosemite
     sha256 "5631ccbccd2bb128a1592399a558cbb837d63eb9b1ac2d8187c34fb42064b226" => :mavericks
+    sha256 "cf85362b4d7e620016c81503c5b60d43045bde99fe5130a3946179ce0a7b0275" => :x86_64_linux
   end
 
   def install
