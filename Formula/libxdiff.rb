6e6724664588b7f402aee373658bf833cdd63186
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 19:45:23 +0000

    libxdiff: update 0.23 bottle.

--- Formula/libxdiff.rb
+++ Formula/libxdiff.rb
@@ -14,6 +14,7 @@ class Libxdiff < Formula
     sha256 "55d89877bd5457b7a5b77cf68187f544ff413228ec17a701e7879644ae528f35" => :el_capitan
     sha256 "6269c8d291cea44aceda9bd8e1e93a061e64d76852e47b781ae49aee28f0c31c" => :yosemite
     sha256 "eb1a482e6da44ad722af99334618ddb17da926f4c88a8c85361e04ce0e59bb54" => :mavericks
+    sha256 "29b24110584476d786fce5b591069bc30c793ad85f5592c9e782b8211fb3399b" => :x86_64_linux
   end
 
   def install
