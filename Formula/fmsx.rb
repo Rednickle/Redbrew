300e15c5c8c4a08a55cb07573b03a85942217292
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 10:57:43 +0000

    fmsx: update 5.4 bottle.

--- Formula/fmsx.rb
+++ Formula/fmsx.rb
@@ -12,6 +12,7 @@ class Fmsx < Formula
     sha256 "888f28e2d549ffcd8507c126c18047c64f36670a0d6d86d9469bceb47245c0e3" => :high_sierra
     sha256 "dd7617947a4e85063cc5d41fc82eabdd54a3d456475533ad65431d1f2fd6338a" => :sierra
     sha256 "f17025b7856d7e9fc17d58361a8686bd6d27b4182e28632b0e9d797d0fcf22f9" => :el_capitan
+    sha256 "971ab144eb143004d31676ed8912a0b5a6d5d5e8702933a85a0c10488377f7c7" => :x86_64_linux
   end
 
   depends_on "pulseaudio"
