7631ecd8de73b389a59b8b27c3426ece368ac4f4
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 22:44:06 +0000

    open-tyrian: update 2.1.20130907 bottle.

--- Formula/open-tyrian.rb
+++ Formula/open-tyrian.rb
@@ -12,6 +12,7 @@ class OpenTyrian < Formula
     sha256 "4f55db177b78370597bc596797cfb9f08c3a7249ce3e53130fc349d4b4fdb6d5" => :sierra
     sha256 "90612b1c31262d8e0ae681842197370d278b51fdb46cce6a0a40e2595a4c831a" => :el_capitan
     sha256 "6c3ebd2d00d744211373e8457a3df645a5d402e521b40f7c46139cc9e4c33dc6" => :yosemite
+    sha256 "d0fff79f3d38efbb4375c01b1d952f8edea2f56bdfdd152cb2311056e32ec8f0" => :x86_64_linux
   end
 
   depends_on "sdl"
