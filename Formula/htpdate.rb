a368d1474a63c35a8bcd3f43569645f2aa19c915
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 11:28:01 +0000

    htpdate: update 1.2.2 bottle.

--- Formula/htpdate.rb
+++ Formula/htpdate.rb
@@ -9,6 +9,7 @@ class Htpdate < Formula
     sha256 "ed41231b1e7d1760603e39f3e161be7cf817abc978f70c0dcbaec3bb206d9d8d" => :catalina
     sha256 "4da5825b9f51a83c7de24d289719f0d341b79685a7e1580f2de867e53941934a" => :mojave
     sha256 "437b8823d451f79f1ad8e2420387a3f50c3dc5919ef19717d41c437a88b77247" => :high_sierra
+    sha256 "78dcbfa60dd8e130a501b828bfa1409352b453031613bd1ec28c1d03eb3ba901" => :x86_64_linux
   end
 
   depends_on :macos => :high_sierra # needs <sys/timex.h>
