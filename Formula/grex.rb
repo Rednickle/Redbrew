babba115319a12b189e8da0b342d2b28b01d6364
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 11:02:20 +0000

    grex: update 1.0.0 bottle.

--- Formula/grex.rb
+++ Formula/grex.rb
@@ -9,6 +9,7 @@ class Grex < Formula
     sha256 "90050fb3e32743d9d3f2ed89e04942b11996d1de1dbc9cf16ac68d0556b1429b" => :catalina
     sha256 "2f0e00fd6bcea11069e26d0079dc326b5111319630c54132816f401d532ddce0" => :mojave
     sha256 "3d31d46adb5eb71ee68095f80e619b4890eff95d024be8934bd27373ed6de12f" => :high_sierra
+    sha256 "b5b8a253f5a9f4da2748ce79d1bea7d54494b91735a0cf63c80aa5ee9954ec3c" => :x86_64_linux
   end
 
   depends_on "rust" => :build
