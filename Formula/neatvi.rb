2c1a1877a915af8372d1d212d511e0974b0399b3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 22:29:03 +0000

    neatvi: update 07 bottle.

--- Formula/neatvi.rb
+++ Formula/neatvi.rb
@@ -11,6 +11,7 @@ class Neatvi < Formula
     sha256 "3c839cf593245c6d026d4a547669c29978e3ec64ef9f247630e4d1183db1376f" => :catalina
     sha256 "12703985422c0a7dd73003a874c0ce747cf3fb3022f674cbb344e7a0d7836ce9" => :mojave
     sha256 "e7046e2ff2dbe83c98fa1ddf5c06f299ffc45f825c421fccb95b624d104eae3e" => :high_sierra
+    sha256 "fd9c23f04a139246fff40319182aed35c51885db463621005792817318fa6f5b" => :x86_64_linux
   end
 
   def install
