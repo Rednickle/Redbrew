1071df351ef20629bc02693915e724520bc83313
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 19:49:16 +0000

    lzfse: update 1.0 bottle.

--- Formula/lzfse.rb
+++ Formula/lzfse.rb
@@ -11,6 +11,7 @@ class Lzfse < Formula
     sha256 "e2a28bc48a8d90dd26cf2fe92d9186cbe0f19c8a58a5d15c8591826cd047b43b" => :high_sierra
     sha256 "2da23959f27fe8a141b2967a591052c6ec081224b7b3c9c65c4a854faba77456" => :sierra
     sha256 "4fcadd0779483cf14e95f7566002af22e9b488585c37fba1b5e75f715b930c01" => :el_capitan
+    sha256 "19104cde7a280d1029728ade6450182301ffc4c1eca611c8e4be6030578bf81b" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
