027143c48e7c6cccc47364072aa2ab3bcdfd018c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 20:53:18 +0000

    mscgen: update 0.20_3 bottle.

--- Formula/mscgen.rb
+++ Formula/mscgen.rb
@@ -12,6 +12,7 @@ class Mscgen < Formula
     sha256 "1f194eb67147772b362ae5446b2e369b35ee9ffa935c8e22d37cdb4c1364349b" => :mojave
     sha256 "0f125ab1fbaf04c670f252f05358771f1663b3fc59857bcfd855bbb52e01f88b" => :high_sierra
     sha256 "08345683137541d79b6422afd2e269b1ab8c195722e5e71cffa6298a3986d563" => :sierra
+    sha256 "5c8f670a4eb96be9ab3696b54609ace8213dcd1f404316c643c7cbce9f975050" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
