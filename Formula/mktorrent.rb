fa3973556d7484835a34c4fbdb4b37d8214c8170
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-09 20:48:04 +0000

    mktorrent: update 1.1_1 bottle.

--- Formula/mktorrent.rb
+++ Formula/mktorrent.rb
@@ -11,6 +11,7 @@ class Mktorrent < Formula
     sha256 "22bc8649ce5fea25549610eec4110d45f3fa1d05335cfc982df82806ff34d71b" => :mojave
     sha256 "60be732dfea657c6faffa7e9d644f6ade7f974e7fea6ec46fa2941baac5eee80" => :high_sierra
     sha256 "3e7f91587dbea47713351b40a99b50728a878a9eb720eca14bd125541e62606f" => :sierra
+    sha256 "425b126bd8bc925e4ddaf214cca4fc6399f4dfaff25be70b736919e001c2bf47" => :x86_64_linux
   end
 
   depends_on "openssl@1.1"
