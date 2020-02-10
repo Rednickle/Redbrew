a871e3c315321ec25bb6fe0bea76b95b4ef4eaf8
Michka Popoff <michkapopoff@gmail.com>
2020-02-10 18:59:39 +0100

    autojump: depend on macos due to Python 2

--- Formula/autojump.rb
+++ Formula/autojump.rb
@@ -14,6 +14,7 @@ class Autojump < Formula
     sha256 "922ca87c1c1f1cb50bb7a8d68d06c0709ea5ed6e5aa71ecd5b72eb370fcc7449" => :x86_64_linux
   end
 
+  depends_on :macos # Due to Python 2
   uses_from_macos "python@2"
 
   def install
