85964a2e888edca1308c621f6258f943b4cbe6e6
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 21:10:31 +0000

    zssh: update 1.5c bottle.

--- Formula/zssh.rb
+++ Formula/zssh.rb
@@ -13,6 +13,7 @@ class Zssh < Formula
     sha256 "04212f19c1d9a6b97fd56ffe937606f1779849fdf04b93e3f285889599845c8f" => :el_capitan
     sha256 "94280569f9e1c1deb9d8c3be4256cd501399fd51758f8e2ea6d77fd9f1b6ef2e" => :yosemite
     sha256 "94b16bb29616a839134527fd869ac40a8fb5fa88b0048d1a93a828e306c2a270" => :mavericks
+    sha256 "4a4148c0f7cd9a073155ccc89dd34914d8dc2d0901d3dbe0b889dba2ec9be725" => :x86_64_linux
   end
 
   depends_on "lrzsz"
