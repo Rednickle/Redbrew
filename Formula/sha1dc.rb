9121f9c6699f0ce1e449942bb0aa22d15be07f64
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 09:30:31 +0000

    sha1dc: update 1.0.3 bottle.

--- Formula/sha1dc.rb
+++ Formula/sha1dc.rb
@@ -16,6 +16,7 @@ class Sha1dc < Formula
     sha256 "9eba4b19247672b715376e2086689e7418235d850a158636d2ba3deb46851933" => :sierra
     sha256 "32d59c039a26d232b35f3c1877ca8c78ba0a303866adefee002c017359b03267" => :el_capitan
     sha256 "939388a0fe029d8cba8080a778269322489c55f787301947c82fb30cf8433b08" => :yosemite
+    sha256 "0a0f7f393040e0469b52c6f8f14f663dfc9d59b8b33749ea09c469e68d779c65" => :x86_64_linux
   end
 
   depends_on "coreutils" => :build # GNU install
