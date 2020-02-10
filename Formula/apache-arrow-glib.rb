81129281ce374205a81da4677a4afcdbef99d877
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 12:02:08 +0000

    apache-arrow-glib: update 0.16.0 bottle.

--- Formula/apache-arrow-glib.rb
+++ Formula/apache-arrow-glib.rb
@@ -9,6 +9,7 @@ class ApacheArrowGlib < Formula
     sha256 "53ec05d5303a8436826b0e2d5f63c75e02c0d94f4e9603e9320c51215da50592" => :catalina
     sha256 "e39f24b25d36332864fc4af1fbe8c4b043c77a029d5f02927cb30428502bd522" => :mojave
     sha256 "f7232a9864c688cc6147a921f6f000be309009a45921f4461e469ef0f2214873" => :high_sierra
+    sha256 "b3511911c08813d07765d9a945e8c3ba069328615bdb62cae21479dac47c6491" => :x86_64_linux
   end
 
   depends_on "gobject-introspection" => :build
