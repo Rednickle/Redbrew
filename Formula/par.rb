81eb03041b51d8c5ab09bdbd0bc96519dde553af
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 05:57:32 +0000

    par: update 1.52 bottle.

--- Formula/par.rb
+++ Formula/par.rb
@@ -15,6 +15,7 @@ class Par < Formula
     sha256 "efa3ba3bdd3b34ad8e5089b8cd5562d8b8cf4a5e5488e54e43dfb45760a1b4fa" => :sierra
     sha256 "3683d5918dc91fcd073fc8e35af2fca416b3756aff8479ff549598bcd2500e8b" => :el_capitan
     sha256 "cb1042ef12ead6645653775571ebe84798b707194922030563ff4056687954e3" => :yosemite
+    sha256 "f109e671da133603da2075c3625fa8c53ad554c12e9f13096fb90e9fbb3fe837" => :x86_64_linux
   end
 
   conflicts_with "rancid", :because => "both install `par` binaries"
