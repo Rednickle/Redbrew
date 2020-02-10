ea15e3d14f71b964ff1d0320be0c8fdabd0dfb26
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 12:56:00 +0000

    x86_64-elf-binutils: update 2.33.1 bottle.

--- Formula/x86_64-elf-binutils.rb
+++ Formula/x86_64-elf-binutils.rb
@@ -9,6 +9,7 @@ class X8664ElfBinutils < Formula
     sha256 "18ea6dbe526c6c14961968ab697cfa44852f99716c57df5ee1f4c80eeae4e44e" => :catalina
     sha256 "dc730e66867b76359b10193f959ed8c19cd28f5a84e702cc482a264b05909f7a" => :mojave
     sha256 "77187bb70a15356d0580f95fa37cbbe872d80afe569f4aa34aabf54ee5c71ee6" => :high_sierra
+    sha256 "ba0767c52c7f886c3f43e22451680c1e0a42bb23b9607c8dc0bc51f7b7955d64" => :x86_64_linux
   end
 
   def install
