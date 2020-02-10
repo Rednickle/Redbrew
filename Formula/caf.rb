1f728e6513f84e2edb25dbba6cfed18ff3448c48
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 11:00:45 +0000

    caf: update 0.17.4 bottle.

--- Formula/caf.rb
+++ Formula/caf.rb
@@ -11,6 +11,7 @@ class Caf < Formula
     sha256 "7381c914063cfdf1bf8bae87db703cd08852d4c4404a1bda1aed0ff61f6e73bf" => :catalina
     sha256 "62bd03d54ab09b73df5ded4472ef155a010cddce1127cc6e36ba26a766016617" => :mojave
     sha256 "a7bf159890ed0df47fb59a94ee5d196187cc5ee18a2488516afe0ccb9fc2b948" => :high_sierra
+    sha256 "b6b5bbca06286f3f9bf6fadcc85dc5ec1fe591de2a66cbaf5a64fe6397bacb2a" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
