701dfc58c74c4493775dfa23787f1885b043ac6a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 09:35:29 +0000

    spandsp: update 0.0.6_1 bottle.

--- Formula/spandsp.rb
+++ Formula/spandsp.rb
@@ -13,6 +13,7 @@ class Spandsp < Formula
     sha256 "c0b8349525680304f99e464d328df804279be85398969974f2b708e5167c89fe" => :sierra
     sha256 "428120be3841ca77961f2d19fe8f98f1615972db4f568e3cfa2c0a44cae44e77" => :el_capitan
     sha256 "65fd095ea758180f18ca9c39864dbd3432115610b0db2439dbc923d3c0002f7b" => :yosemite
+    sha256 "fbfef7cc4ea42ef6fbab88703fc1caa6091b4e6bed0762a044859c27f95bd42d" => :x86_64_linux
   end
 
   depends_on "libtiff"
