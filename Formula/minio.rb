1d1d4ad23dc071e019eb1386d509e34c8f4bc503
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 11:51:59 +0000

    minio: update 20200207232816 bottle.

--- Formula/minio.rb
+++ Formula/minio.rb
@@ -11,6 +11,7 @@ class Minio < Formula
     sha256 "29acf707f92bab5f01e83e2fee7275349c96c6d80b97b7bc0cadeae0828e3ee6" => :catalina
     sha256 "386651aa51d657f213d6da88878a947afc4819fc90b7ac0f3cedd929ccd60b93" => :mojave
     sha256 "1e176451f160d65590c57674f2031171370f39bcd87edd6897427c9f684b7028" => :high_sierra
+    sha256 "292dad6b8f67e7b72abde8d32ecc0d2ccd942024318c5eafebfcbda7095c78a3" => :x86_64_linux
   end
 
   depends_on "go" => :build
