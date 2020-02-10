ae3c90b140f01e624a7bc81d9f09ea7ebd9042f9
Michka Popoff <michkapopoff@gmail.com>
2020-02-10 18:55:35 +0100

    chapel: use Python 3 (#19477)

--- Formula/chapel.rb
+++ Formula/chapel.rb
@@ -3,19 +3,20 @@ class Chapel < Formula
   homepage "https://chapel-lang.org/"
   url "https://github.com/chapel-lang/chapel/releases/download/1.20.0/chapel-1.20.0.tar.gz"
   sha256 "08bc86df13e4ad56d0447f52628b0f8e36b0476db4e19a90eeb2bd5f260baece"
-  revision 1 unless OS.mac?
+  revision 2 unless OS.mac?
 
   bottle do
     sha256 "057e5c71d41f2ff71434f446ffd8f9aa932b553612313729d4651fdc58233650" => :catalina
     sha256 "8fcaebe6a3c465a29a66e691581b88b2fc9960726e1f94b3f21aa0f53c424044" => :mojave
     sha256 "4aee5a0ddf8a44897a2f03c458a8e7e70d76b07f04024119ed482fbc06cf330c" => :high_sierra
     sha256 "34a5eac538de8fb6ac632109a0154e1d14ff8551bc8f4fec8df8359568697338" => :sierra
-    sha256 "d53975690305ffbce08f16407e25e3c1d1a510f3ddb271260b1a594af5f7abe5" => :x86_64_linux
   end
 
-  depends_on "python@2" unless OS.mac?
+  depends_on "python@3.8" unless OS.mac?
 
   def install
+    ENV.prepend_path "PATH", Formula["python@3.8"].opt_libexec/"bin" unless OS.mac?
+
     libexec.install Dir["*"]
     # Chapel uses this ENV to work out where to install.
     ENV["CHPL_HOME"] = libexec
@@ -49,6 +50,7 @@ class Chapel < Formula
     # Fix [Fail] chpl not found. Make sure it available in the current PATH.
     # Makefile:203: recipe for target 'check' failed
     ENV.prepend_path "PATH", bin unless OS.mac?
+    ENV.prepend_path "PATH", Formula["python@3.8"].opt_libexec/"bin" unless OS.mac?
 
     ENV["CHPL_HOME"] = libexec
     cd libexec do
