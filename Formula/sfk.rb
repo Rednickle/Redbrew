aa92a368425b20c245075a7b4f6e7bfb0ce45b9f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-10 10:56:05 +0000

    sfk: update 1.9.6.0 bottle.

--- Formula/sfk.rb
+++ Formula/sfk.rb
@@ -10,6 +10,7 @@ class Sfk < Formula
     sha256 "1b9c2addd71a6703f7c7f75f38368ebf37ba06af3161b06c4bdadc2179d79e0c" => :catalina
     sha256 "27cd6edcb20275bddc23f7cd405788191745f5721d8f974064eabbdaee1c4011" => :mojave
     sha256 "ff756db64c5e9d78cb01aa8f2645b4e23ccf0784d34b06e1b8a1f690c365d6bd" => :high_sierra
+    sha256 "776e2b78fe76559ff62d87c517a489ae011dc0927769ce3e1801d7d109f39e27" => :x86_64_linux
   end
 
   def install
