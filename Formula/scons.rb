6f98312530079dc7b88ad0bee1c3650abd0d40d9
Michka Popoff <michkapopoff@gmail.com>
2020-02-11 07:49:54 +0100

    scons: use python 3 (#19573)

--- Formula/scons.rb
+++ Formula/scons.rb
@@ -3,26 +3,33 @@ class Scons < Formula
   homepage "https://www.scons.org/"
   url "https://downloads.sourceforge.net/project/scons/scons/3.1.2/scons-3.1.2.tar.gz"
   sha256 "7801f3f62f654528e272df780be10c0e9337e897650b62ddcee9f39fde13f8fb"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
     sha256 "eacddf72a0a5803c6053eb8bda0f9b1c0a27f8d7bd30c239c56aa3608802ea15" => :catalina
     sha256 "eacddf72a0a5803c6053eb8bda0f9b1c0a27f8d7bd30c239c56aa3608802ea15" => :mojave
     sha256 "eacddf72a0a5803c6053eb8bda0f9b1c0a27f8d7bd30c239c56aa3608802ea15" => :high_sierra
-    sha256 "3880ad9e8f43d6a5d909042e6a748176382a98707f11b2dc0b51b754539dce4e" => :x86_64_linux
   end
 
-  uses_from_macos "python@2"
+  depends_on "python@3.8"
 
   def install
     unless OS.mac?
       inreplace "engine/SCons/Platform/posix.py",
         "env['ENV']['PATH']    = '/usr/local/bin:/opt/bin:/bin:/usr/bin'",
         "env['ENV']['PATH']    = '#{HOMEBREW_PREFIX}/bin:/usr/local/bin:/opt/bin:/bin:/usr/bin'"
+
+      Dir["**/*"].each do |f|
+        next unless File.file?(f)
+        next unless File.read(f).include?("/usr/bin/env python")
+
+        inreplace f, %r{#! ?/usr/bin/env python}, "#! /usr/bin/env python3"
+      end
     end
 
     man1.install gzip("scons-time.1", "scons.1", "sconsign.1")
-    system (OS.mac? ? "/usr/bin/python" : "python"), "setup.py", "install",
+    system (OS.mac? ? "/usr/bin/python" : Formula["python@3.8"].opt_bin/"python3"), "setup.py", "install",
              "--prefix=#{prefix}",
              "--standalone-lib",
              # SCons gets handsy with sys.path---`scons-local` is one place it
