b2d5c1f822a892806eb21411c9f29899b16b2c0f
Michka Popoff <michkapopoff@gmail.com>
2020-02-11 10:07:49 +0100

    llvm@7: Depend on Python@3.8 (#19592)

--- Formula/llvm@7.rb
+++ Formula/llvm@7.rb
@@ -3,14 +3,13 @@ class LlvmAT7 < Formula
   homepage "https://llvm.org/"
   url "https://releases.llvm.org/7.1.0/llvm-7.1.0.src.tar.xz"
   sha256 "1bcc9b285074ded87b88faaedddb88e6b5d6c331dfcfb57d7f3393dd622b3764"
-  revision 1
+  revision OS.mac? ? 1 : 2
 
   bottle do
     cellar :any
     sha256 "3db0aaac0de7c0e664288f14d051c69afb9294c1c7ceca616040ad662022c049" => :catalina
     sha256 "50d86b01a2f722cc7570193e22ba852e640f90c781d31af6b4feb94643a87585" => :mojave
     sha256 "b3786a0bb86a273e0c4b2f6591462dde29a634bc7978960b31752e9910730bd4" => :high_sierra
-    sha256 "d94919b00ed84237cad9392cbc7093924c3019e5ec673890bdb8b2985f7430ab" => :x86_64_linux
   end
 
   # Clang cannot find system headers if Xcode CLT is not installed
@@ -34,7 +33,7 @@ class LlvmAT7 < Formula
     depends_on "libelf" # openmp requires <gelf.h>
     depends_on "ncurses"
     depends_on "libxml2"
-    depends_on "python@2"
+    depends_on "python@3.8"
     depends_on "zlib"
     depends_on "libomp"
   end
@@ -169,8 +168,9 @@ class LlvmAT7 < Formula
     man1.install_symlink share/"clang/tools/scan-build/man/scan-build.1"
 
     # install llvm python bindings
-    (lib/"python2.7/site-packages").install buildpath/"bindings/python/llvm"
-    (lib/"python2.7/site-packages").install buildpath/"tools/clang/bindings/python/clang"
+    xz = OS.mac? ? "2.7": "3.8"
+    (lib/"python#{xz}/site-packages").install buildpath/"bindings/python/llvm"
+    (lib/"python#{xz}/site-packages").install buildpath/"tools/clang/bindings/python/clang"
 
     unless OS.mac?
       # Remove conflicting libraries.
