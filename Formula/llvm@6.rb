7bb444b9bc6448335ade3d01153a0ba661ec09e2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-11 16:16:29 +0000

    llvm@6: update 6.0.1_3 bottle.

--- Formula/llvm@6.rb
+++ Formula/llvm@6.rb
@@ -10,6 +10,7 @@ class LlvmAT6 < Formula
     sha256 "a9b2f8263268e19f68d5bc8e1791526c867e6a41e22d5231a7494a96ed4af8fc" => :catalina
     sha256 "fff84017bc2b346184e39edeff42bf5003efa5bc1c18d262c3200c5f6f8c2a76" => :mojave
     sha256 "bd0b6ad2bbd65461491adfe0fd1c8415e917bc395f9aa8bbc14c754c9c09f84d" => :high_sierra
+    sha256 "c31ce6bd740ba3c2eaaec0db264a30758942aa9a9f4483b6ac7752ba606b6332" => :x86_64_linux
   end
 
   # Clang cannot find system headers if Xcode CLT is not installed
