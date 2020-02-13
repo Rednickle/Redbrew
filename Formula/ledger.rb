ceda4330478a64633a767147f025b24e94320ea7
Michka Popoff <michkapopoff@gmail.com>
2020-02-13 22:51:13 +0100

    ledger: use Python@3.8 (#19616)

--- Formula/ledger.rb
+++ Formula/ledger.rb
@@ -3,27 +3,26 @@ class Ledger < Formula
   homepage "https://ledger-cli.org/"
   url "https://github.com/ledger/ledger/archive/v3.1.3.tar.gz"
   sha256 "b248c91d65c7a101b9d6226025f2b4bf3dabe94c0c49ab6d51ce84a22a39622b"
-  revision 4
+  revision OS.mac? ? 4 : 5
   head "https://github.com/ledger/ledger.git"
 
   bottle do
     sha256 "9b9be8385bbf69fcd7e910bce409f97c08542b89aecacde4d615d5a5ca9af65c" => :catalina
     sha256 "f36940017c56792a42d010b1b1094d355575adb4d51f74acaf4ea8e57024aba0" => :mojave
     sha256 "a8313d55f2a5070ac82c01260c11a77273c5dc6d0ee2c38d06eb0b9a143cb5cb" => :high_sierra
-    sha256 "7f858c7c4a8065b841824f166a0efb9e6b09d139c381004f2772de5d391e8d00" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "gmp"
   depends_on "mpfr"
+  depends_on "python@3.8"
 
-  depends_on :macos # Due to Python 2
   uses_from_macos "groff"
-  uses_from_macos "python@2"
 
   def install
     ENV.cxx11
+    ENV.prepend_path "PATH", Formula["python@3.8"].opt_libexec/"bin"
 
     args = %W[
       --jobs=#{ENV.make_jobs}
@@ -34,6 +33,7 @@ class Ledger < Formula
       -DBUILD_DOCS=1
       -DBUILD_WEB_DOCS=1
       -DBoost_NO_BOOST_CMAKE=ON
+      -DPython_FIND_VERSION_MAJOR=3
     ]
     system "./acprep", "opt", "make", *args
     system "./acprep", "opt", "make", "doc", *args
