a04e3fb61a120d53a3bbbea7aec053df2149d96c
Dawid Dziurla <dawidd0811@gmail.com>
2020-02-13 15:41:43 +0100

    libprelude: modernize (#19620)
    - delete options
    - delete python@2
    - update

--- Formula/libprelude.rb
+++ Formula/libprelude.rb
@@ -1,32 +1,24 @@
 class Libprelude < Formula
   desc "Universal Security Information & Event Management (SIEM) system"
   homepage "https://www.prelude-siem.org/"
-  url "https://www.prelude-siem.org/attachments/download/721/libprelude-3.1.0.tar.gz"
-  sha256 "b8fbaaa1f2536bd54a7f69fe905ac84d936435962c8fc9de67b2f2b375c7ac96"
-  revision 3
+  url "https://www.prelude-siem.org/attachments/download/1172/libprelude-5.1.0.tar.gz"
+  sha256 "a5fa3ca1e428291afe4bb5095f01c05fd0d9ebd517a0ce3d07ca9977abcf41fa"
   # tag "linux"
 
   bottle do
-    sha256 "3062c5bc227c0febbc753b1fc33b11ee17e03de9a887d066044d2878952c43ea" => :x86_64_linux
   end
 
-  option "without-ruby", "Build without Ruby bindings"
-
-  depends_on :macos # Due to Python 2
   depends_on "libtool" => :build
-  depends_on "lua" => [:build, :optional]
-  depends_on "perl" => [:build, :optional]
+  depends_on "perl" => :build
   depends_on "pkg-config" => :build
-  depends_on "python@2" => [:build, :recommended]
-  depends_on "swig" => [:build, :recommended]
-  depends_on "valgrind" => [:build, :recommended]
+  depends_on "python" => :build
+  depends_on "ruby" => :build
+  depends_on "swig" => :build
+  depends_on "valgrind" => :build
   depends_on "gnutls"
   depends_on "libgcrypt"
   depends_on "libgpg-error"
-  depends_on "ruby"
-  depends_on "python" => :recommended
-
-  skip_clean "etc", "lib64", "var", :la
+  depends_on "lua"
 
   def install
     ENV["HAVE_CXX"] = "yes"
@@ -35,14 +27,15 @@ class Libprelude < Formula
       --prefix=#{prefix}
       --disable-rpath
       --with-pic
+      --with-python3
+      --with-valgrind
+      --with-lua
+      --with-ruby
+      --with-perl
+      --with-swig
+      --with-libgcrypt-prefix=#{Formula["libgcrypt"].opt_prefix}
+      --with-libgnutls-prefix=#{Formula["gnutls"].opt_prefix}
     ]
-
-    args << "--with-libgcrypt-prefix=#{Formula["libgcrypt"].opt_prefix}" if build.with? "libgcrypt"
-    args << "--with-libgnutls-prefix=#{Formula["gnutls"].opt_prefix}" if build.with? "gnutls"
-
-    %w[swig perl python2 python3 valgrind lua ruby].each do |r|
-      args << "--with-#{r}=#{build.with?(r) ? "yes" : "no"}"
-    end
 
     system "./configure", *args
     system "make"
