89e9c082b56a3f45a791f71c6db7d749cb3322bc
Michka Popoff <michkapopoff@gmail.com>
2020-02-14 00:06:55 +0100

    libssh: no need to depend on python (#19626)

--- Formula/libssh.rb
+++ Formula/libssh.rb
@@ -3,6 +3,7 @@ class Libssh < Formula
   homepage "https://www.libssh.org/"
   url "https://www.libssh.org/files/0.9/libssh-0.9.3.tar.xz"
   sha256 "2c8b5f894dced58b3d629f16f3afa6562c20b4bdc894639163cf657833688f0c"
+  revision 1 unless OS.mac?
   head "https://git.libssh.org/projects/libssh.git"
 
   bottle do
@@ -10,16 +11,12 @@ class Libssh < Formula
     sha256 "b4cff56528c3a0a8c960f57539b402173c360451d9786aee5dfb42880dfea721" => :catalina
     sha256 "09b06812c37f7e2213224e92a0d579e0e9bf56bc4d1cd3f017f1877b22eb757f" => :mojave
     sha256 "db8b47069aadc848fec558351e684464d1d9f3ddc1b6105e6720dddffde30823" => :high_sierra
-    sha256 "1d155d0fcd90ad1ac84de13ecffa70b1a1fb7209ec1cc150985c855f59d1ccc6" => :x86_64_linux
   end
 
-  depends_on :macos # Due to Python 2
   depends_on "cmake" => :build
   depends_on "openssl@1.1"
-  unless OS.mac?
-    depends_on "python@2" => :build
-    depends_on "zlib"
-  end
+
+  uses_from_macos "zlib"
 
   def install
     mkdir "build" do
