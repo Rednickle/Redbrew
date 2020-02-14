0096ce19b59bdf08b6f7162aa825d982a1ee638a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-14 18:12:56 +0000

    icecream: update 1.3.1 bottle.

--- Formula/icecream.rb
+++ Formula/icecream.rb
@@ -8,6 +8,7 @@ class Icecream < Formula
     sha256 "666f827a6a686e6d2e81dc1d0eb5aae8374f01d7d1524ef6c695e3bf207c4af5" => :catalina
     sha256 "fb94b2d8e763469a2b0112523f89496f4a81e22ed9b7290f4280178f726853da" => :mojave
     sha256 "6cc11bcddd969e9aeb7e83692e9714d5891f0530bacbc1c52b019b298bce3d24" => :high_sierra
+    sha256 "61972acf33fce83d9ddc20a148cc4294b4d913e2de7c3ffb380b606504162b24" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
