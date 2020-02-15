f88c18fdeea2dc3deff6cc9897d5d710e4ce495f
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-15 10:08:01 +0000

    devspace: update 4.5.2 bottle.

--- Formula/devspace.rb
+++ Formula/devspace.rb
@@ -10,6 +10,7 @@ class Devspace < Formula
     sha256 "21c5f12f0ce3323314e31e838408c6d12f5d92bbb1b8d0bd25a10119e0af0ae1" => :catalina
     sha256 "805c4d2c388ab8f5e25157f3cbe0c19e6c20b07ebd8875aa0de7613638401a72" => :mojave
     sha256 "2baa5736260fbe4a0f63501216ef5d17aa7fcc331a76a6e3ae79d177b15dd246" => :high_sierra
+    sha256 "ce479a39cb26fbb42ed18650dee0141d983830b3896569e2526ce04a274cc723" => :x86_64_linux
   end
 
   depends_on "go" => :build
