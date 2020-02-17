5b572cbff03b419f9c782b81cce574253d867134
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-17 20:20:38 +0000

    cheat: update 3.6.0_1 bottle.

--- Formula/cheat.rb
+++ Formula/cheat.rb
@@ -11,6 +11,7 @@ class Cheat < Formula
     sha256 "ed808bc433039e37248800f8cdb3bcf131404981806cb039a9cdc86a2f01f575" => :catalina
     sha256 "5982d0f2f6477de5fa58efbbd4c64a686e4ddf31b423bcb49712ec55aca2b265" => :mojave
     sha256 "c8357412a3f8ecb5042b93d8e80c03e70138bb20d9c99c6bea28914093e4736f" => :high_sierra
+    sha256 "03907d38f252d12d9f67da810c6593f371f0c4f006cef88fb7038850b5785a43" => :x86_64_linux
   end
 
   depends_on "go" => :build
