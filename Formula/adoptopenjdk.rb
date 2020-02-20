0ec221dc7e5c95a82648848740a00356b93d9d49
Steve Kinzler <steve@kinzler.com>
2020-02-20 17:40:22 -0500

    adoptopenjdk 1.8.0.242 (#19662)

--- Formula/adoptopenjdk.rb
+++ Formula/adoptopenjdk.rb
@@ -1,9 +1,9 @@
 class Adoptopenjdk < Formula
   desc "Prebuilt binaries produced from OpenJDK class libraries"
   homepage "https://adoptopenjdk.net/"
-  url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u212-b03/OpenJDK8U-jdk_x64_linux_hotspot_8u212b03.tar.gz"
-  version "1.8.0.212"
-  sha256 "dd28d6d2cde2b931caf94ac2422a2ad082ea62f0beee3bf7057317c53093de93"
+  url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u242-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u242b08.tar.gz"
+  version "1.8.0.242"
+  sha256 "f39b523c724d0e0047d238eb2bb17a9565a60574cf651206c867ee5fc000ab43"
 
   bottle :unneeded
 
