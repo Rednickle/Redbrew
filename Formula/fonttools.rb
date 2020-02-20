423397fc7cf8ed6616993bceda96de2f55a7143b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-20 20:22:04 +0000

    fonttools: update 4.4.0 bottle.

--- Formula/fonttools.rb
+++ Formula/fonttools.rb
@@ -12,6 +12,7 @@ class Fonttools < Formula
     sha256 "9759d192635405425da19b70ca494de3b2e4b32c5eac7091c99651d25fbab2ae" => :catalina
     sha256 "a77b259422c646a46a9f4157633e82d354c59437de0b546f71860332bacb7b52" => :mojave
     sha256 "bbe6390c7b202fbe0635e91a1d0df9de0a47d48237fd88eeb5f0043f3181508f" => :high_sierra
+    sha256 "cd7253137b1b4b7de1f965440864ffc34983f6485a9c487a7546c1d5637f4e24" => :x86_64_linux
   end
 
   depends_on "python@3.8"
