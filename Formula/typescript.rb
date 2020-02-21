d4a640fe2c311ec45637c3a6bde36ff2519c62ab
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-21 17:51:03 +0000

    typescript: update 3.8.2 bottle.

--- Formula/typescript.rb
+++ Formula/typescript.rb
@@ -12,6 +12,7 @@ class Typescript < Formula
     sha256 "a4ae9f4a7860e5c88a0bcf293a1d5ba9b8825e73924da21e58d94434f0e27dca" => :catalina
     sha256 "c69183330ca2f50a07cc625fab3339c37bccda7ce1159cd492259f25f9e1c57e" => :mojave
     sha256 "436a68487e6253e9328f0cc85ff381eee066c8f7d45a51c82c5d27c8fda09eec" => :high_sierra
+    sha256 "ba900a04c6935ca0a914ce67d37d79590f72d71dc2bb65bc2eae40a233c21065" => :x86_64_linux
   end
 
   depends_on "node"
