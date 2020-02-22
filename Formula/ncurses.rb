9559590dd3c12fb4f939f4e78c47ca04f05c6d21
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-02-22 19:25:14 +0000

    ncurses: update 6.2 bottle.

--- Formula/ncurses.rb
+++ Formula/ncurses.rb
@@ -9,6 +9,7 @@ class Ncurses < Formula
     sha256 "eae51ad3391edafe3d6c649ba44f607ee1464b4b5d9ee48770e9817ee5f0ccdd" => :catalina
     sha256 "1771e0ce821cf8cbe38d0ce8d1843fd559532923222edc5dbf5b31fcf24fed90" => :mojave
     sha256 "4648be8457b081026d3da80f290abaf3fbfdcb49d62914861a63fc706f9adabe" => :high_sierra
+    sha256 "fc5a0983a8afae8ef46945f3bec9dba9d66e81e879027d100d05088ea6f8043d" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
