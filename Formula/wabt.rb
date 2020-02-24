fbbd1b4023435e6dda69bafd79525c4a5dd09d3d
Dawid Dziurla <dawidd0811@gmail.com>
2020-02-24 18:01:35 +0100

    wabt: use python@3.8 (#19705)

--- Formula/wabt.rb
+++ Formula/wabt.rb
@@ -4,18 +4,17 @@ class Wabt < Formula
   url "https://github.com/WebAssembly/wabt.git",
     :tag      => "1.0.13",
     :revision => "7c80d05e37dc02870c680869ae3f04ac6d9637ee"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
     sha256 "11975b7122f6106a969064cd684e9a77d24443df26b873a225f48d8e1304fff9" => :catalina
     sha256 "26d32821ca26815974d699a5665ce3406fb9373a9d8970d7349ef6e3aee18208" => :mojave
     sha256 "5d028ba6b144571da2407e65351aebd93fabb84d453601971a4c5c02d3a51b17" => :high_sierra
-    sha256 "0150ceef5ecd3180fb336d174299f14eefe91097b497e1c5d4536a68c4568048" => :x86_64_linux
   end
 
-  depends_on :macos # Due to Python 2
   depends_on "cmake" => :build
-  depends_on "python@2" => :build unless OS.mac?
+  depends_on "python@3.8" => :build
 
   def install
     mkdir "build" do
