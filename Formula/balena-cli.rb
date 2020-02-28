a5b1a2a9a7db94614eb7dbb75d3b2938a3546c76
Michka Popoff <michkapopoff@gmail.com>
2020-02-28 07:22:07 +0100

    balena-cli: Fix build for Linuxbrew (#19647)

--- Formula/balena-cli.rb
+++ Formula/balena-cli.rb
@@ -16,7 +16,7 @@ class BalenaCli < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    system "npm", "install", *("--unsafe-perm" if ENV["CI"]), *Language::Node.std_npm_install_args(libexec)
     bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
