57a1df1f35442879bd5613af54cab984b16160fb
Michka Popoff <michkapopoff@gmail.com>
2020-03-02 22:07:48 +0100

    qt: do not depend on Python (#19595)
    and fix build

--- Formula/qt.rb
+++ Formula/qt.rb
@@ -7,7 +7,7 @@ class Qt < Formula
   mirror "https://mirrors.dotsrc.org/qtproject/archive/qt/5.14/5.14.1/single/qt-everywhere-src-5.14.1.tar.xz"
   mirror "https://mirrors.ocf.berkeley.edu/qt/archive/qt/5.14/5.14.1/single/qt-everywhere-src-5.14.1.tar.xz"
   sha256 "6f17f488f512b39c2feb57d83a5e0a13dcef32999bea2e2a8f832f54a29badb8"
-
+  revision 1 unless OS.mac?
   head "https://code.qt.io/qt/qt5.git", :branch => "dev", :shallow => false
 
   bottle do
@@ -15,12 +15,10 @@ class Qt < Formula
     sha256 "e40589965586f2c1132da117aca2e0cf12f3ea4bb1029d26b4b0819d8aae3bd5" => :catalina
     sha256 "149a1c2d2af7afda9910e1d4e3956c27ffa31ea511a8320930abf7a9079d0330" => :mojave
     sha256 "69a7f1ad615f78735b6635da1b1fab5e4eea57dd9be560c695f82b796457870a" => :high_sierra
-    sha256 "d5150abde791998b5b802ccdd0f020a987d00aa995114663d143dad9aef8f029" => :x86_64_linux
   end
 
   keg_only "Qt 5 has CMake issues when linked"
 
-  depends_on :macos # Due to Python 2
   depends_on "pkg-config" => :build
   depends_on :xcode => :build if OS.mac?
   depends_on :macos => :sierra if OS.mac?
@@ -28,23 +26,34 @@ class Qt < Formula
   unless OS.mac?
     depends_on "fontconfig"
     depends_on "glib"
+    depends_on "gperf"
     depends_on "icu4c"
     depends_on "libproxy"
-    depends_on "pulseaudio"
-    depends_on "python@2"
-    depends_on "sqlite"
-    depends_on "systemd"
     depends_on "libxkbcommon"
+    depends_on "linuxbrew/xorg/libdrm"
+    depends_on "linuxbrew/xorg/libice"
+    depends_on "linuxbrew/xorg/libsm"
+    depends_on "linuxbrew/xorg/libxcomposite"
     depends_on "linuxbrew/xorg/mesa"
+    depends_on "linuxbrew/xorg/wayland"
+    depends_on "linuxbrew/xorg/xcb-util"
     depends_on "linuxbrew/xorg/xcb-util-image"
     depends_on "linuxbrew/xorg/xcb-util-keysyms"
     depends_on "linuxbrew/xorg/xcb-util-renderutil"
-    depends_on "linuxbrew/xorg/xcb-util"
     depends_on "linuxbrew/xorg/xcb-util-wm"
-    depends_on "linuxbrew/xorg/xorg"
+    depends_on "pulseaudio"
+    depends_on "python"
+    depends_on "systemd"
+    depends_on "zstd"
   end
 
+  uses_from_macos "sqlite"
+  uses_from_macos "bison"
+  uses_from_macos "flex"
+
   def install
+    system "/home/linuxbrew/.linuxbrew/bin/brew", "cleanup", "--prune=0"
+
     args = %W[
       -verbose
       -prefix #{prefix}
@@ -67,10 +76,18 @@ class Qt < Formula
     elsif OS.linux?
       args << "-system-xcb"
       args << "-R#{lib}"
-      # https://bugreports.qt.io/projects/QTBUG/issues/QTBUG-71564
+      # https://bugreports.qt.io/browse/QTBUG-71564
       args << "-no-avx2"
       args << "-no-avx512"
       args << "-qt-zlib"
+      # https://bugreports.qt.io/browse/QTBUG-60163
+      # https://codereview.qt-project.org/c/qt/qtwebengine/+/191880
+      args += %w[-skip qtwebengine]
+      args -= ["-proprietary-codecs"]
+    end
+
+    inreplace %w[qtdeclarative/qtdeclarative.pro qtdeclarative/src/3rdparty/masm/masm.pri] do |s|
+      s.gsub! "python ", "python3 "
     end
 
     system "./configure", *args
