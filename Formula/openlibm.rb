c56b88189b7b550ca070b6bbba6642f63a8fba17
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-04 11:28:57 +0000

    openlibm: update 0.7.0 bottle.

--- Formula/openlibm.rb
+++ Formula/openlibm.rb
@@ -9,6 +9,7 @@ class Openlibm < Formula
     sha256 "a7d18405ee96e1409a3de89904263280b364ae5e0d046be6a1391031f6ec5bb9" => :catalina
     sha256 "d7f73575a6b6b7efb4307165f578e172a1563955ed476683b27bbaca6c0cafb6" => :mojave
     sha256 "4f769ffeda11f2bc17a5cd52a4bcd59723586b9258b789adcdb628fb700cca86" => :high_sierra
+    sha256 "f49019b9ce5a9639565cd9cac9c3ebe8673dea687cc1d576b15726ec85af0bde" => :x86_64_linux
   end
 
   keg_only :provided_by_macos
