a07389446485031b6fb66c4076615734cc10a1bd
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-04 17:56:48 +0000

    draco: update 1.3.6 bottle.

--- Formula/draco.rb
+++ Formula/draco.rb
@@ -9,6 +9,7 @@ class Draco < Formula
     sha256 "54ea5852087b08f08945c7a73f6fc3b9d91958e6f23685c9848e08c2bce1ac03" => :catalina
     sha256 "cd40c18a7c54185a154785e7245ae1b3ddeff275a96854488e625f9a1b6701a5" => :mojave
     sha256 "341560bdd2d7831f274feaa1646f10c79a00618d8c00f9522e885f23af1f2f87" => :high_sierra
+    sha256 "19af4530991cdb4a4f9de3998d2040d3655bd083dda1fa033dd6c609528d5f37" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
