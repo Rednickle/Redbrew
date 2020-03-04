63653e910066759c1766d4e5d955732b98d29a4a
Michka Popoff <michkapopoff@gmail.com>
2020-03-04 19:42:37 +0100

    curl: make cellar any again

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -5,6 +5,7 @@ class Curl < Formula
   sha256 "668d451108a7316cff040b23c79bc766e7ed84122074e44f662b8982f2e76739"
 
   bottle do
+    cellar :any
     sha256 "24aa9504342f77774aee3567b70b5067bf610fcdb5863cd0791ecaec67a8fa1f" => :catalina
     sha256 "d0d9389ecd80e156f43bbbec2cdf90a09502f4bd300b74868a9bb82358e80e58" => :mojave
     sha256 "540af1e3466f38cc2fe0a4d9cde8e3cf11b644dba4bc9aa2e97a0f4ecca142a0" => :high_sierra
