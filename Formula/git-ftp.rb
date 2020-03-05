51549741969b43b34788c0b678544f114e465a5e
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-05 22:01:25 +0000

    git-ftp: update 1.6.0 bottle.

--- Formula/git-ftp.rb
+++ Formula/git-ftp.rb
@@ -10,6 +10,7 @@ class GitFtp < Formula
     sha256 "0a61ca11e69370dfecfd3c82d6d03aeec377bf9db660658403556ea71b84bae0" => :catalina
     sha256 "f878c4015697794bb8b2c3f034a167b750d3871c0d320d903536128f01880ca2" => :mojave
     sha256 "63c8b94fd89eb635d8c2056efdf933de45dca7fdb04793b620750f8b338fbb88" => :high_sierra
+    sha256 "80d507de96861546cf465d4bc2042c936bbbd3b7873fb34e43954751ce54f10c" => :x86_64_linux
   end
 
   depends_on "pandoc" => :build
