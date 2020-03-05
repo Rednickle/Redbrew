119e7a00199a4cfe084441e540226286bdc16eb1
Michka Popoff <michkapopoff@gmail.com>
2020-03-05 22:49:10 +0100

    stgit: depend on Python@3.8 (#19745)

--- Formula/stgit.rb
+++ Formula/stgit.rb
@@ -3,6 +3,7 @@ class Stgit < Formula
   homepage "https://github.com/ctmarinas/stgit"
   url "https://github.com/ctmarinas/stgit/releases/download/v0.21/stgit-0.21.tar.gz"
   sha256 "0f67a3c0ed3e0408aa8e9be6ff6c7be0a2981ca43639bc94bda7b6124717e71f"
+  revision 1 unless OS.mac?
   head "https://github.com/ctmarinas/stgit.git"
 
   bottle do
@@ -10,14 +11,12 @@ class Stgit < Formula
     sha256 "736d0fb7ba2e2f09acb9f3c12e7a232d975c1f20306b1d6b56dbc8fa9622bb0e" => :catalina
     sha256 "a8c5a52941bb5c524f97bddf295dbf65b79ec74b4ec5a0d0ebcdb25429e1e03d" => :mojave
     sha256 "a8c5a52941bb5c524f97bddf295dbf65b79ec74b4ec5a0d0ebcdb25429e1e03d" => :high_sierra
-    sha256 "4ba24cc6d6fb561bd886b78eaefdcf8737b4953501eeb76ecffe9fea0e938985" => :x86_64_linux
   end
 
-  depends_on :macos # Due to Python 2
-  depends_on "python@2" unless OS.mac?
+  depends_on "python@3.8"
 
   def install
-    ENV["PYTHON"] = "python" # overrides 'python2' built into makefile
+    ENV["PYTHON"] = Formula["python@3.8"].opt_bin/"python3"
     system "make", "prefix=#{prefix}", "all"
     system "make", "prefix=#{prefix}", "install"
   end
