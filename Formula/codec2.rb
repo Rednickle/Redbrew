a3f6f6f4ef06ee0a9844812442ff831c7a64d201
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-07 15:02:24 +0000

    codec2: update 0.9.2 bottle.

--- Formula/codec2.rb
+++ Formula/codec2.rb
@@ -10,6 +10,7 @@ class Codec2 < Formula
     sha256 "d41040646585e5b91438818ff4dfdccc94b3a2567d31d6960710f8f2455bab04" => :catalina
     sha256 "460c2febeb64e913796ee3f161b8fa50bbc00904c99228d5122324ebadd91fe9" => :mojave
     sha256 "5f4530a54adbc38253b993bdbb21da86b4d11725b2edcbec79e43ed739875208" => :high_sierra
+    sha256 "1abf3196c1426021346c7d023af9ea7c1540e20f25977080ee22f5e78e47909f" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
