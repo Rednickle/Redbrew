a334ed543a77f1b78bed911ae7e212c6d6bc2938
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-07 16:37:27 +0000

    isl: update 0.22.1 bottle.

--- Formula/isl.rb
+++ Formula/isl.rb
@@ -16,6 +16,7 @@ class Isl < Formula
     sha256 "b5319e3bbbb36ef3536d841999b7497b3dce4bf9e07fb04f6b0db716e087896d" => :catalina
     sha256 "29213891860c971e084d1e2a3d1ad00c92371140dea599aae2894e26ec0d6874" => :mojave
     sha256 "a1193c8b06c31abc4bf9c1ef9bb93c4879ff5ba4050b4a06c22c2a0048d3c87f" => :high_sierra
+    sha256 "f211005727b3ff0a59cbca6521f9a5aaaef54cfc801e3725a1b2708f9576fa0c" => :x86_64_linux
   end
 
   head do
