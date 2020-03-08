2d0699e933bc42fd36b7fcdbf5e2196146d8c040
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-08 20:19:41 +0100

    curl: add another patch (#19800)

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -3,14 +3,12 @@ class Curl < Formula
   homepage "https://curl.haxx.se/"
   url "https://curl.haxx.se/download/curl-7.69.0.tar.bz2"
   sha256 "668d451108a7316cff040b23c79bc766e7ed84122074e44f662b8982f2e76739"
-  revision 1 unless OS.mac?
+  revision 2 unless OS.mac?
 
   bottle do
-    cellar :any
     sha256 "24aa9504342f77774aee3567b70b5067bf610fcdb5863cd0791ecaec67a8fa1f" => :catalina
     sha256 "d0d9389ecd80e156f43bbbec2cdf90a09502f4bd300b74868a9bb82358e80e58" => :mojave
     sha256 "540af1e3466f38cc2fe0a4d9cde8e3cf11b644dba4bc9aa2e97a0f4ecca142a0" => :high_sierra
-    sha256 "a278581b9df7c197dbf1d7becd3a82cf54388358b1a200dc78caca27dccec8a3" => :x86_64_linux
   end
 
   pour_bottle? do
@@ -38,6 +36,11 @@ class Curl < Formula
     patch do
       url "https://github.com/curl/curl/commit/8aa04e9a24932b830bc5eaf6838dea5a3329341e.patch"
       sha256 "ea52049e73581acc3e3bf795010c6e11a928ac75380dd6f5dbb32c19b8dd0a4e"
+    end
+
+    patch do
+      url "https://github.com/curl/curl/commit/e040146f22608fd92c44be2447a6505141a8a867.patch"
+      sha256 "22b7faa70902820e447a22c28a830e917359c64d09d92505d728105a3ce4497b"
     end
   end
 
