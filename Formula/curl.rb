14f7ddce84cf18873b642c7c941a9d0e0d3e8ee2
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-08 13:03:16 +0100

    curl: add linux only patch (#19796)
    https://github.com/rust-lang/cargo/issues/7974

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -3,13 +3,12 @@ class Curl < Formula
   homepage "https://curl.haxx.se/"
   url "https://curl.haxx.se/download/curl-7.69.0.tar.bz2"
   sha256 "668d451108a7316cff040b23c79bc766e7ed84122074e44f662b8982f2e76739"
+  revision 1 unless OS.mac?
 
   bottle do
-    cellar :any
     sha256 "24aa9504342f77774aee3567b70b5067bf610fcdb5863cd0791ecaec67a8fa1f" => :catalina
     sha256 "d0d9389ecd80e156f43bbbec2cdf90a09502f4bd300b74868a9bb82358e80e58" => :mojave
     sha256 "540af1e3466f38cc2fe0a4d9cde8e3cf11b644dba4bc9aa2e97a0f4ecca142a0" => :high_sierra
-    sha256 "4cdb75bcbf159d4156d537ee409571ff0cc5bfeec9d2cf1a2cd33dc31908ac2a" => :x86_64_linux
   end
 
   pour_bottle? do
@@ -31,6 +30,14 @@ class Curl < Formula
   depends_on "openssl@1.1" unless OS.mac?
 
   uses_from_macos "zlib"
+
+  # TODO: Remove with next release after 7.69.0
+  unless OS.mac?
+    patch do
+      url "https://github.com/curl/curl/commit/8aa04e9a24932b830bc5eaf6838dea5a3329341e.patch"
+      sha256 "ea52049e73581acc3e3bf795010c6e11a928ac75380dd6f5dbb32c19b8dd0a4e"
+    end
+  end
 
   def install
     system "./buildconf" if build.head?
