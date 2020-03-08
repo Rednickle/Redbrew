1268195dea9da8ffb6de78cf9e56aab91011a36c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-08 18:13:13 +0000

    pygobject3: update 3.36.0 bottle.

--- Formula/pygobject3.rb
+++ Formula/pygobject3.rb
@@ -8,6 +8,7 @@ class Pygobject3 < Formula
     sha256 "63c1f6a01fd1d27a252cdeabdd0e084598ec9b79eb05a523e48f7f13c4f05746" => :catalina
     sha256 "9b3863938691e7992ed55d37837a30f6bc851f0af1444d4eb32b735f4a1b10c8" => :mojave
     sha256 "f2cdd475ed9816f815bb1d6874780df09b82d3f925b2571a4567ee90ab89a073" => :high_sierra
+    sha256 "ff690f4fc77e9fcc826f461e955f4dfe9fd37ee47cdcb8ab027ae3390affb25a" => :x86_64_linux
   end
 
   depends_on "meson" => :build
