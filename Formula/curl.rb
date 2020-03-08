2a26515da333f2facd2a5b1851876e8391384cbc
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-08 20:46:37 +0100

    curl: restore cellar :any

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -6,6 +6,7 @@ class Curl < Formula
   revision 2 unless OS.mac?
 
   bottle do
+    cellar :any
     sha256 "24aa9504342f77774aee3567b70b5067bf610fcdb5863cd0791ecaec67a8fa1f" => :catalina
     sha256 "d0d9389ecd80e156f43bbbec2cdf90a09502f4bd300b74868a9bb82358e80e58" => :mojave
     sha256 "540af1e3466f38cc2fe0a4d9cde8e3cf11b644dba4bc9aa2e97a0f4ecca142a0" => :high_sierra
