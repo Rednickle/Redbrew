f08786b3b91058cc58885b3e9f9640ac248d4d99
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-08 11:37:06 +0000

    hostess: update 0.5.0 bottle.

--- Formula/hostess.rb
+++ Formula/hostess.rb
@@ -10,6 +10,7 @@ class Hostess < Formula
     sha256 "6a9d9913afdfdc168e8cde3851e4d3aee36a3f29cbb37c7eee553146b5e4c87c" => :catalina
     sha256 "04ea74921e0739da95c77825f1e7c1ba95b045ad7ed560dc236cf18ff66dbf09" => :mojave
     sha256 "a4ee18464f1ae186c1281dc17a3ed6a65c1d7ffd585e97cdf4d1d872e4a5575d" => :high_sierra
+    sha256 "f95cb67c69ccfe53b6fc0c03899dbf3a948ac34d020853c8221332832118ec5d" => :x86_64_linux
   end
 
   depends_on "go" => :build
