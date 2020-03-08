a3e5f1a595129a1dc6d13e7d9409c499fee0ca0c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-08 12:04:32 +0000

    curl: update 7.69.0_1 bottle.

--- Formula/curl.rb
+++ Formula/curl.rb
@@ -9,6 +9,7 @@ class Curl < Formula
     sha256 "24aa9504342f77774aee3567b70b5067bf610fcdb5863cd0791ecaec67a8fa1f" => :catalina
     sha256 "d0d9389ecd80e156f43bbbec2cdf90a09502f4bd300b74868a9bb82358e80e58" => :mojave
     sha256 "540af1e3466f38cc2fe0a4d9cde8e3cf11b644dba4bc9aa2e97a0f4ecca142a0" => :high_sierra
+    sha256 "a278581b9df7c197dbf1d7becd3a82cf54388358b1a200dc78caca27dccec8a3" => :x86_64_linux
   end
 
   pour_bottle? do
