69b1854f89524f754bac25df8ef76708c4d48c00
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-09 15:09:03 +0000

    ipmiutil: update 3.1.5 bottle.

--- Formula/ipmiutil.rb
+++ Formula/ipmiutil.rb
@@ -9,6 +9,7 @@ class Ipmiutil < Formula
     sha256 "e2ea930b55754e8e0b27b675ebabdf2c12a73ddd0c8d994484c8b7e351350ec5" => :catalina
     sha256 "c1d4a86dae7aae9e8ce807741c42e91884156eda9b9004ab2f151e2cfda6b74d" => :mojave
     sha256 "9e4ae6d909010bcff31df4fcef39a9cb0de2083aded6b960483db942c9e18f0f" => :high_sierra
+    sha256 "c690bcf78bf44633e919a4c604da7d457eaf79a5fa5d7c3e2e9ad81d9a74ba59" => :x86_64_linux
   end
 
   depends_on "autoconf" => :build
