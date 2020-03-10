94c3ec3a189e77973b34cb75419c4655b35fdebf
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-10 19:12:19 +0000

    grv: update 0.3.2 bottle.

--- Formula/grv.rb
+++ Formula/grv.rb
@@ -11,6 +11,7 @@ class Grv < Formula
     sha256 "2f223f7ca56ee01201a05e8660c219a2f70d7ead2c5e4f0dac65f4a9b8cd5941" => :mojave
     sha256 "d51249eec72ee11cc90b0d5b4c06e10b77eec0bd7ddf8b53df6d0a0d8a291605" => :high_sierra
     sha256 "19f2e8bedb458d0b339160b275e196add00abcca7db92ba141aaccae255bb973" => :sierra
+    sha256 "84ec8581649a0367f502b39d1803f4e70bce7b4837ddc5e7c61886d2af5d00a7" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
