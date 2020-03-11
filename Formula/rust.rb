47c7dbb0ca9861c5ff5929fa80baa51dc1b427f8
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-11 14:45:29 +0000

    rust: update 1.41.1_3 bottle.

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -18,6 +18,7 @@ class Rust < Formula
     sha256 "cb6f748139b5a6b36cb859840183bbe295734d11cdfa4bf91f2f4b213aa706cf" => :catalina
     sha256 "8e02732d6b8337d03422c206c8644975ae3e31caa41686c1e298494d0e45ccc2" => :mojave
     sha256 "0b7a0246cbb9364eae54df7e182bcfbb35542c3feb5f3c166ba1535ffcf58432" => :high_sierra
+    sha256 "ba4781c0e702dc69bf125df6d5f208e19184473f17857b2bd828a543579adaeb" => :x86_64_linux
   end
 
   head do
