31a882ae313f08d39bbe3212763a55f317519a86
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-11 15:44:02 +0100

    rust: rebuild for curl again (#19818)

--- Formula/rust.rb
+++ Formula/rust.rb
@@ -1,7 +1,7 @@
 class Rust < Formula
   desc "Safe, concurrent, practical language"
   homepage "https://www.rust-lang.org/"
-  revision 2 unless OS.mac?
+  revision 3 unless OS.mac?
 
   stable do
     url "https://static.rust-lang.org/dist/rustc-1.41.1-src.tar.gz"
@@ -18,7 +18,6 @@ class Rust < Formula
     sha256 "cb6f748139b5a6b36cb859840183bbe295734d11cdfa4bf91f2f4b213aa706cf" => :catalina
     sha256 "8e02732d6b8337d03422c206c8644975ae3e31caa41686c1e298494d0e45ccc2" => :mojave
     sha256 "0b7a0246cbb9364eae54df7e182bcfbb35542c3feb5f3c166ba1535ffcf58432" => :high_sierra
-    sha256 "1f9509f409106ec33822597e65d5171ed217509471c6ffde743142d09f19a5ac" => :x86_64_linux
   end
 
   head do
