5782653659a4798456f1b4b34532db8e308bfd41
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-11 22:39:18 +0000

    juju-wait: update 2.7.0_1 bottle.

--- Formula/juju-wait.rb
+++ Formula/juju-wait.rb
@@ -12,6 +12,7 @@ class JujuWait < Formula
     sha256 "8e0d6f6c1aa40131623c5887e7672ef24717c7a44e6612415058a2255a911d9a" => :catalina
     sha256 "b39426fec696dedd4087810b09af692931f020f015e784dad756aa38dd269445" => :mojave
     sha256 "7708ed278031301cc73db50c41aafeab82d5d25d7152e7f8c906a6ea7d0accf7" => :high_sierra
+    sha256 "2b25a4f6910a21acd38423af4a9360ec640977ccdd320da7ac28456e95629591" => :x86_64_linux
   end
 
   depends_on "juju"
