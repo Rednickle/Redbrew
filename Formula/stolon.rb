80ff45f3c6f4cba5642bfe9a05b37a90c56fcea2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-12 13:15:45 +0000

    stolon: update 0.16.0 bottle.

--- Formula/stolon.rb
+++ Formula/stolon.rb
@@ -10,6 +10,7 @@ class Stolon < Formula
     sha256 "8bbf533b32cba6f798e17aad03e6268c6fe66be84cac9801226217e40c9cc0a9" => :catalina
     sha256 "fac96dde0102d1b0b7cd6e6dad8cb3bb3b7dfdee16f0fe98cbb439e4602a48e5" => :mojave
     sha256 "3f82acb97ffd0f586b5c6c643237205f0df3c5d03ed2cbd01143aba404d0e1b7" => :high_sierra
+    sha256 "f9a42e2d84e9a0a7e6edbe81878b02e25f8e1ce62129e3e45b7b696bbcdbe17c" => :x86_64_linux
   end
 
   depends_on "go" => :build
