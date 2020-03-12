725f4968d56c64e4c3648c9185a9d79347fc61e0
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-12 14:06:06 +0100

    fdroidserver: build (#19835)

--- Formula/fdroidserver.rb
+++ Formula/fdroidserver.rb
@@ -12,7 +12,6 @@ class Fdroidserver < Formula
     sha256 "1699a24a781e6fd51a36ca8a7a97ef0478a80119b27bbea9f8d8eaa4b4d21d70" => :catalina
     sha256 "f07f50564384845bcbb8622d718f9471b59214b7487bfed7180d5fe75605ab5b" => :mojave
     sha256 "657d6aaf9e40e10d4644f14f97a02394260da7ea35bc1fa42a09c9b21d2693fc" => :high_sierra
-    sha256 "7a4abb185dcf7c54819df75e099b99aef9616b893995e8583695f8c2548e603d" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
