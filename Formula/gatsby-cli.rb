2003102ed798834c8b74e6ee7878765eb3247cb9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-14 08:39:55 +0000

    gatsby-cli: update 2.10.10 bottle.

--- Formula/gatsby-cli.rb
+++ Formula/gatsby-cli.rb
@@ -11,6 +11,7 @@ class GatsbyCli < Formula
     sha256 "876d5f77292ae492181a3faca55edea2cb09539a858bf246124be01f969ce49d" => :catalina
     sha256 "8c529808625f4925a7975f50f68ef166fb9435be215d61183b2920f377b9c1b9" => :mojave
     sha256 "b41ab9b242ec0d4abab615570d81db3eda0cb87c9d1e1e60754c1f4c7a35fbeb" => :high_sierra
+    sha256 "e0bb950192e78e5935e939e43edba81704aa9111ceccaf922852916ae0d85a39" => :x86_64_linux
   end
 
   depends_on "node"
