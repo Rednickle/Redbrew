38270438313cf42b3b1e3a06a4cf5a1cb39ed09b
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-19 13:19:11 +0100

    docbook-xsl: remove patch (#19898)

--- Formula/docbook-xsl.rb
+++ Formula/docbook-xsl.rb
@@ -13,11 +13,6 @@ class DocbookXsl < Formula
 
   depends_on "docbook"
 
-  # Fixes: #12 replacement target string #13 target string #14 string
-  # error: file man/systemd.index.xml xsltRunStylesheet : run failed
-  # when building systemd
-  patch :DATA unless OS.mac?
-
   resource "ns" do
     url "https://github.com/docbook/xslt10-stylesheets/releases/download/release%2F1.79.2/docbook-xsl-1.79.2.tar.bz2"
     sha256 "316524ea444e53208a2fb90eeb676af755da96e1417835ba5f5eb719c81fa371"
@@ -48,45 +43,22 @@ class DocbookXsl < Formula
     bin.write_exec_script "#{prefix}/docbook-xsl/epub/bin/dbtoepub"
   end
 
+  def xmlcatalog
+    OS.mac? ? "xmlcatalog" : "#{Formula["libxml2"].opt_bin}/xmlcatalog"
+  end
+
   def post_install
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
     [prefix/"docbook-xsl/catalog.xml", prefix/"docbook-xsl-ns/catalog.xml"].each do |catalog|
-      system (OS.mac? ? "xmlcatalog" : "#{Formula["libxml2"].opt_bin}/xmlcatalog"), "--noout", "--del", "file://#{catalog}", "#{etc}/xml/catalog"
-      system (OS.mac? ? "xmlcatalog" : "#{Formula["libxml2"].opt_bin}/xmlcatalog"), "--noout", "--add", "nextCatalog", "", "file://#{catalog}", "#{etc}/xml/catalog"
+      system xmlcatalog, "--noout", "--del", "file://#{catalog}", "#{etc}/xml/catalog"
+      system xmlcatalog, "--noout", "--add", "nextCatalog", "", "file://#{catalog}", "#{etc}/xml/catalog"
     end
   end
 
   test do
-    xmlcatalog = OS.mac? ? "xmlcatalog" : "#{Formula["libxml2"].opt_bin}/xmlcatalog"
     system xmlcatalog, "#{etc}/xml/catalog", "http://cdn.docbook.org/release/xsl/current/"
     system xmlcatalog, "#{etc}/xml/catalog", "http://cdn.docbook.org/release/xsl/#{version}/"
     system xmlcatalog, "#{etc}/xml/catalog", "http://cdn.docbook.org/release/xsl-nons/current/"
     system xmlcatalog, "#{etc}/xml/catalog", "http://cdn.docbook.org/release/xsl-nons/#{version}/"
   end
 end
-
-__END__
---- A/lib/lib.xsl
-+++ B/lib/lib.xsl
-@@ -10,7 +10,10 @@
-      This module implements DTD-independent functions
-
-      ******************************************************************** -->
--<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
-+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
-+                xmlns:str="http://exslt.org/strings"
-+                exclude-result-prefixes="str"
-+                version="1.0">
-
- <xsl:template name="dot.count">
-   <!-- Returns the number of "." characters in a string -->
-@@ -56,6 +59,9 @@
-   <xsl:param name="replacement"/>
-
-   <xsl:choose>
-+    <xsl:when test="function-available('str:replace')">
-+      <xsl:value-of select="str:replace($string, string($target), string($replacement))"/>
-+    </xsl:when>
-     <xsl:when test="contains($string, $target)">
-       <xsl:variable name="rest">
-         <xsl:call-template name="string.subst">
