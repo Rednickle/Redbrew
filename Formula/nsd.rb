ffe6f1964de511fa67c4da699cf9ea439c9f46ba
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-20 19:43:24 +0000

    nsd: update 4.3.0 bottle.

--- Formula/nsd.rb
+++ Formula/nsd.rb
@@ -8,6 +8,7 @@ class Nsd < Formula
     sha256 "07d7fdac349bb1f35b746247f29b97e2379f9d19fd66cc6e1a81c6b5641c2b7d" => :catalina
     sha256 "e43570ffacdf14e58a5d80d955cd154b17ab4b5f4558b120b8d694733aa6e539" => :mojave
     sha256 "1e402a7ed8f5fbf048dd855072c38826f549d20af621f5829bab4a12422ab233" => :high_sierra
+    sha256 "636fc284935be929807b52446dbccc64ec36b9ecf14066154444cd6b2a82df77" => :x86_64_linux
   end
 
   depends_on "libevent"
