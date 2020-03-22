75dc2e903f6232051f036ee7a5de786015f54922
Michka Popoff <michkapopoff@gmail.com>
2020-03-22 19:14:41 +0100

    ghostscript: add linux sha256 back

--- Formula/ghostscript.rb
+++ Formula/ghostscript.rb
@@ -8,6 +8,7 @@ class Ghostscript < Formula
     sha256 "8cd0efa1e5525f849be3ee1e50e1635b99667cb4d1eb6c3002a45378346882f4" => :catalina
     sha256 "8906a4dbf2513963a4710f351e3426622c259bd888c760e4c08a9436860b4014" => :mojave
     sha256 "cd5e55d0429d7e88ba2d580e79934c157d5bd2981d71a7f40db4573abe79af67" => :high_sierra
+    sha256 "aaaf9ccc219b445e2a798656533596b8aa77e1eed69d1ee95882e22259561416" => :x86_64_linux
   end
 
   head do
