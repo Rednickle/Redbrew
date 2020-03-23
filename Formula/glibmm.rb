abdc4bf120698184f4c1bd76b4869b44ad842959
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-23 09:17:57 +0000

    glibmm: update 2.64.2 bottle.

--- Formula/glibmm.rb
+++ Formula/glibmm.rb
@@ -9,6 +9,7 @@ class Glibmm < Formula
     sha256 "8b39f15570f8ec9281554ec8db93e4011ad2e13a1248047c18c7f8570a548d53" => :catalina
     sha256 "316a5f0f84491a62cf1c48a12cd4f8d9b7f7de9aa8092f72256f5114aa8730d3" => :mojave
     sha256 "7d224a2283e08715a1f7f286fcdc3e1c5cc277101bb3e2cc4bce488ec776cc02" => :high_sierra
+    sha256 "0c4cc228b4fdcb78f2f96726c0413c8fa7c22d0bed11e346fa9f3b1d62e212c0" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
