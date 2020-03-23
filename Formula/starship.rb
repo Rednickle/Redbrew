7e8c452d7812e57d81e56baf97848cde6cdd1af9
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-23 09:20:50 +0000

    starship: update 0.38.1 bottle.

--- Formula/starship.rb
+++ Formula/starship.rb
@@ -10,6 +10,7 @@ class Starship < Formula
     sha256 "bb42b5ac227280bb6dafe0f7c1f70851cde6273a375461d201f94c96777c87ca" => :catalina
     sha256 "d9a07177b9b2af743fb294cc391a79df0327e11c9848874e3b170b7ab7a5458c" => :mojave
     sha256 "14ac37580fb454c23e8d2c6d1a916dc7ea43b60c880012a51c7b0bc8a22549f7" => :high_sierra
+    sha256 "2cb748fd6aba62b1ea0d89000a5834f92557b88577753f9c2a6cb965c53f0fb3" => :x86_64_linux
   end
 
   depends_on "rust" => :build
