01462a7600c5a048cc0d2835de335ee5922dcba2
Dawid Dziurla <dawidd0811@gmail.com>
2020-03-23 10:30:22 +0100

    git: add bottle

--- Formula/git.rb
+++ Formula/git.rb
@@ -10,6 +10,7 @@ class Git < Formula
     sha256 "174f1f2513dfc0be1997c4385cc4579c37479aa1038e5f73088fb0a1edfbe5e9" => :catalina
     sha256 "872641963bc853b4e421a0d8fa3a535812fc1a81d07fe00cbd8b1f35bc66558c" => :mojave
     sha256 "d811df72427c53248ef4ba9e954ecd998c9b72cae87c620af37fa0813e0a0a5f" => :high_sierra
+    sha256 "424967e3343bd5c2ac682c77b8bfaf3f95eee7d0be070ce9c097ac665d18b096" => :x86_64_linux
   end
 
   depends_on "gettext"
