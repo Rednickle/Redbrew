d5c937b680da77050e81f6ef8c261147ccaede46
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-25 11:18:55 +0000

    prettier: update 2.0.2 bottle.

--- Formula/prettier.rb
+++ Formula/prettier.rb
@@ -12,6 +12,7 @@ class Prettier < Formula
     sha256 "eaf8a28b352b700dd50fc7a474e95433f59cd9e9f42671c2d9c099f331b8f658" => :catalina
     sha256 "392d5e6ded1a2662c6531ea6984a76b122e17767e4f28423f8f7db71a3bc9816" => :mojave
     sha256 "1618556927d9a4a0303fa6a464433079eaba3a6561bc8398d43490d8840ee08d" => :high_sierra
+    sha256 "f90fa6f0551aea61ad950a36cb7807f63230e0d81c772fe4f8026dd7a60ecef1" => :x86_64_linux
   end
 
   depends_on "node"
