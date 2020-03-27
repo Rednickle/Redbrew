7dc23c044a1d5e19013b7f9b53127952bdc13681
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-27 21:13:55 +0000

    vault: update 1.3.4 bottle.

--- Formula/vault.rb
+++ Formula/vault.rb
@@ -14,6 +14,7 @@ class Vault < Formula
     sha256 "38d5259e448cfc9ed592c80e55c062f9472cb672f30e87b3e7915a0b014ea52e" => :catalina
     sha256 "541f6e33d95ba47c199408052038cccaba9b5e3cdf43fb2b23e84645febd382e" => :mojave
     sha256 "550fda7af152696f0f8c7e1bbd228b7aa7802e6d9cb6f3a61e1fcbbc65462216" => :high_sierra
+    sha256 "4db2e41d0ce0a65ab2f683e8ef8e2bd317091576a0056aeb74f41175fc18cfad" => :x86_64_linux
   end
 
   depends_on "go@1.12" => :build
