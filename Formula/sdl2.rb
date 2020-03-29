e7375352e48d3a0c9f27befe78699d5c16caca77
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-29 16:22:46 +0000

    sdl2: update 2.0.12_1 bottle.

--- Formula/sdl2.rb
+++ Formula/sdl2.rb
@@ -10,6 +10,7 @@ class Sdl2 < Formula
     sha256 "4dcd635465d16372ca7a7bb2b94221aa21de02f681a22e9239d095b66fb00c63" => :catalina
     sha256 "8733b127dd4ba6179e6ad9e6336418df9dbad8eb13f05597c05e6916f2ff0543" => :mojave
     sha256 "b71346aebd499ed30f6de2f58a333c50575bc3bf73fbba6dcaef5a04c58282c5" => :high_sierra
+    sha256 "f8b79add67cd5cba1c73f7afa7df16291c934d6ebafa8ad7d3bee6a2512aefac" => :x86_64_linux
   end
 
   head do
