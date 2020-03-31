faffc3b8d3a3b97be2837f1874bb3c383c0d6178
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-03-31 18:08:44 +0000

    anycable-go: update 0.6.5 bottle.

--- Formula/anycable-go.rb
+++ Formula/anycable-go.rb
@@ -10,6 +10,7 @@ class AnycableGo < Formula
     sha256 "c3f6edcc690d9e4f1ecf50fa4625d76d0fca2858a9161685e43e3685b547e05b" => :catalina
     sha256 "f310bb7559e50790bf25124b1ef2565fa7a6ebd4b59f0c251d8bbffd832960de" => :mojave
     sha256 "b3d5e34b11249ddc4e8b9494434cd308719321d63a62788d7bef46bb95155a73" => :high_sierra
+    sha256 "8bb694bf8488933b911ce74e155632de395ca9a125967479e285458cc3bd4de7" => :x86_64_linux
   end
 
   depends_on "go" => :build
