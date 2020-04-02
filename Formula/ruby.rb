86d92d44c325e89f6b7dc183358497426fb5b2a2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-02 05:56:21 +0000

    ruby: update 2.7.1 bottle.

--- Formula/ruby.rb
+++ Formula/ruby.rb
@@ -8,6 +8,7 @@ class Ruby < Formula
     sha256 "7f5ed2afb15b25f9616b617ecca2ee376eb67cf6c105790df22221b7be9c1ef9" => :catalina
     sha256 "cc995a06e761acc8a807b040d8e7fe301902a4fc63d2beaaae40e04c710e7338" => :mojave
     sha256 "2605ad1636e40d747878a8df7d1ef51a15e58e70ce03510b9ea582c02a448e26" => :high_sierra
+    sha256 "bcf6ad258d596eaa6c75bf38f01937d471a75339828957a88b873a4ee02e298b" => :x86_64_linux
   end
 
   head do
