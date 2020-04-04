1bb9cec7d9ee6a736d0607db5095f42f6a008663
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-04 07:08:33 +0000

    you-get: update 0.4.1432 bottle.

--- Formula/you-get.rb
+++ Formula/you-get.rb
@@ -12,6 +12,7 @@ class YouGet < Formula
     sha256 "00978ff19ead4913f71581da459eedc79c51d51b3e3c2482db80cdba29e2bf3f" => :catalina
     sha256 "2baed6bb0c7bdba492eab79e19f267dae5a9c00d40ccdafcb230b2fa1b471cda" => :mojave
     sha256 "7e57020149ef1a994005275b9a09594d368f9ae59261fd4bb86a9386ba8e2ddd" => :high_sierra
+    sha256 "cd6a00db74b04fc97ef1d336b5bcdc44b9dbb63fb3a3c8a3273cb0b56d15063e" => :x86_64_linux
   end
 
   depends_on "python@3.8"
