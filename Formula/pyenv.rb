a34123f27e3d5d564d7e0dd4bd3a9e4859b3db6b
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-04 07:11:46 +0000

    pyenv: update 1.2.18_2 bottle.

--- Formula/pyenv.rb
+++ Formula/pyenv.rb
@@ -12,6 +12,7 @@ class Pyenv < Formula
     sha256 "bd9f719f153e9574dcc65dc7fea28a3816557bd46b0ff90ad8de43f3f8dc72c4" => :catalina
     sha256 "05f0414ec85ad0eb46a4046d9bee6da318b2b68fd9b64b11875cb2457ac808ec" => :mojave
     sha256 "e789844a5fc1efd1e53a60d87032b279823933cb1a79839fb67dd34a9fd7e96a" => :high_sierra
+    sha256 "2e15ad9f46e14dab5b0e1be5ab22846110a3d918f71e1b2419a148595e2b5596" => :x86_64_linux
   end
 
   depends_on "autoconf"
