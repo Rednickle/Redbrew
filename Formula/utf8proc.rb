79542b2f7df96b13a4affc2f6a45a191a44b4e59
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-06 21:35:59 +0000

    utf8proc: update 2.5.0 bottle.

--- Formula/utf8proc.rb
+++ Formula/utf8proc.rb
@@ -9,6 +9,7 @@ class Utf8proc < Formula
     sha256 "f215acf62769b7036110950b65285742b6550c631b0606befa12b98f32c53130" => :catalina
     sha256 "1bd0f29df929fff62b33895ca76162b9d376aefea09115c84a3841793fa7a9a3" => :mojave
     sha256 "12b05c0477b8f6fd029f3723d74712f267960fb9d212a21520f9a557d3cd516c" => :high_sierra
+    sha256 "7a3ec8a2ecf38cc5162005df79dd73e60ef70e631e7cd847c51e589ff185d05a" => :x86_64_linux
   end
 
   def install
