7294b8aa252fa5e06d8fc8b4c493a1c9da590101
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-07 20:38:07 +0000

    baidupcs-go: update 3.6.2 bottle.

--- Formula/baidupcs-go.rb
+++ Formula/baidupcs-go.rb
@@ -9,6 +9,7 @@ class BaidupcsGo < Formula
     sha256 "d7151a5d1fd737835fdb61759fca16da43e5b016ccb9d40ef980e725b3ac2537" => :catalina
     sha256 "33405e1303ee57e6cb5bca3ff9672c81352b9b18cc05054175859b1c47535791" => :mojave
     sha256 "4ca9d8864e8c9029afd0f3777e3c3cbe11bd6a7f25fdefd48ad140d6d50d3324" => :high_sierra
+    sha256 "983478a2b7b18f9c53614b087b4c7f75c1d629c0dff85e3ca1cfc043836af36b" => :x86_64_linux
   end
 
   depends_on "go" => :build
