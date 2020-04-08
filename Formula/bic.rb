17c16b9d66770b578f702d6ee02dc7de6f34bd94
Michka Popoff <michkapopoff@gmail.com>
2020-04-08 08:32:12 +0200

    bic: fix syntax

--- Formula/bic.rb
+++ Formula/bic.rb
@@ -24,7 +24,7 @@ class Bic < Formula
   end
 
   depends_on "gmp"
-  depends_on "readline" uness OS.mac?
+  depends_on "readline" unless OS.mac?
 
   def install
     system "autoreconf", "-fi" if build.head?
