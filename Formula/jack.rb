2eaca8ea646d4d83040c680b3f88ad2cbe32f9f1
Michka Popoff <michkapopoff@gmail.com>
2020-04-08 08:05:52 +0200

    jack: add missing dependency back

--- Formula/jack.rb
+++ Formula/jack.rb
@@ -30,6 +30,8 @@ class Jack < Formula
   depends_on "libsamplerate"
   depends_on "libsndfile"
 
+  depends_on "util-linux" unless OS.mac?
+
   def install
     if OS.mac?
       sdk = MacOS.sdk_path_if_needed ? MacOS.sdk_path : ""
