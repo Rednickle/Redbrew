0eba1e6d7e8c894eee50f1c30f05e78a0a14acdb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-09 21:15:36 +0000

    librsync: update 2.3.0 bottle.

--- Formula/librsync.rb
+++ Formula/librsync.rb
@@ -9,6 +9,7 @@ class Librsync < Formula
     sha256 "8ee8d0fe9d8d8db9fda7aca3322e4caf68a674f44859117e539e875377f69406" => :catalina
     sha256 "4cce7ef51ecbad4186fb781231a45c9618ad5fd26d2cc04953783b4906a456e8" => :mojave
     sha256 "bcc75443712ad6346dae0a31554c79451495afd089d2e9faba37bb0f46a36413" => :high_sierra
+    sha256 "4ba1d90fea3216b3b3ab63d61c05ac3cfea3e10fdbf4c7cb66ee4fc4c4ea483f" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
