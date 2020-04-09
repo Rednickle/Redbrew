fbe1acb5dde52b6115ab4a39f310fc34f4210343
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-09 21:16:06 +0000

    lsdvd: update 0.17_3 bottle.

--- Formula/lsdvd.rb
+++ Formula/lsdvd.rb
@@ -10,6 +10,7 @@ class Lsdvd < Formula
     sha256 "63e52d4264e765ca6095153de37b08dee02d59676ae98b2f0c36b29c0fdae48e" => :catalina
     sha256 "1489bcdd29d86d725cbf76a68ff77335729c676fcf1155555da799d7096ee933" => :mojave
     sha256 "d5a945ffb9e6172df4625d0ed5b3e5c6aecd2dfc6c9ab5b2a83d88267027edc8" => :high_sierra
+    sha256 "d42eae6d2f9f286e280f5696999df705a3e14680ea44caa02d17510b809f9d31" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
