8a9b9d60d689fabf61c0170817b25e93380a7925
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-10 10:44:39 +0000

    fbi-servefiles: update 2.6.0_1 bottle.

--- Formula/fbi-servefiles.rb
+++ Formula/fbi-servefiles.rb
@@ -12,6 +12,7 @@ class FbiServefiles < Formula
     sha256 "d04625bf981ccec4ad79cc872fa7eefcd2cadbdf4b62b09f6d5e4e692325b2d5" => :catalina
     sha256 "2654398f6f8d65a5f24371a32c9f11f1bb5b02ce309056b9e9a13d38dbac2fc0" => :mojave
     sha256 "b0aae581bf9247f6e282df341dfcffed362587539e7a443433d376319837961f" => :high_sierra
+    sha256 "227ff91176a441dd34fd71bbe0457fae33a40304870aaec319ac3c89e9ba2cd8" => :x86_64_linux
   end
 
   depends_on "python@3.8"
