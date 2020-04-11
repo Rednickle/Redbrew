227dbb150cd9bc115ce44897975bf664a985b867
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-11 13:55:06 +0000

    n: update 6.5.0 bottle.

--- Formula/n.rb
+++ Formula/n.rb
@@ -10,6 +10,7 @@ class N < Formula
     sha256 "22ec6a5836ebdd491cd8bfbf1b1f2d0b55c29305dc665af799aeb109ddb99b45" => :catalina
     sha256 "22ec6a5836ebdd491cd8bfbf1b1f2d0b55c29305dc665af799aeb109ddb99b45" => :mojave
     sha256 "22ec6a5836ebdd491cd8bfbf1b1f2d0b55c29305dc665af799aeb109ddb99b45" => :high_sierra
+    sha256 "4ee5535f97bcdde51aa35285fdec7dfcc9be96b3f6f4e0e0b0dcda13dbdbc9c4" => :x86_64_linux
   end
 
   def install
