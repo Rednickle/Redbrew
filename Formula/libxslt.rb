aef7c65329529f45113ecbcf3fde875bf8bf5f34
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-11 16:49:30 +0200

    libxslt: rebuild for libxml2 (#20071)

--- Formula/libxslt.rb
+++ Formula/libxslt.rb
@@ -3,13 +3,13 @@ class Libxslt < Formula
   homepage "http://xmlsoft.org/XSLT/"
   url "http://xmlsoft.org/sources/libxslt-1.1.34.tar.gz"
   sha256 "98b1bd46d6792925ad2dfe9a87452ea2adebf69dcb9919ffd55bf926a7f93f7f"
+  revision 1 unless OS.mac?
 
   bottle do
     cellar :any_skip_relocation
     sha256 "cbadecf3186f45754220dff4cbdfbb576882a211d615b52249a4c9d8ba4d7c3a" => :catalina
     sha256 "6feb1b8d57dd0d8b651733720d4dac728d2e27cf8c9fa9f88e60612fe0a0c882" => :mojave
     sha256 "733c15b756070866d08196dcd5eef9facea1ce98d9c233cd6bf73fa426e0d062" => :high_sierra
-    sha256 "a233b6f6d6a823349ad94b99bc5bf478e195dd7ed81b4582241801cafd2adc26" => :x86_64_linux
   end
 
   head do
