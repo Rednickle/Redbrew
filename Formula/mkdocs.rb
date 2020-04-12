521492a103733d4c3b86ec069e68f3199013aa75
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-12 10:33:04 +0000

    mkdocs: update 1.1 bottle.

--- Formula/mkdocs.rb
+++ Formula/mkdocs.rb
@@ -11,6 +11,7 @@ class Mkdocs < Formula
     sha256 "b65a09b5522567f318a750443f1470b8859cf2dc693615b8b32a0c555801af63" => :catalina
     sha256 "f967c60bc8154dc0e28475891c6148e50e6dc6f37b1a99f7778aa2733b73d02f" => :mojave
     sha256 "80196ec89ffe059a03f03c05d5f1401e6a8514d7b314c7d24f1838e26d8d1975" => :high_sierra
+    sha256 "2d647e17662dd296c08175873695f95d6eedb8a410bd873bbf20f05c51ef5419" => :x86_64_linux
   end
 
   depends_on "python@3.8"
