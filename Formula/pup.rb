e1712a7c57872dffa74b1a8f370affeb95b76ab0
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-13 20:01:52 +0200

    pup: fix binary building (#20099)
    closes: #20098

--- Formula/pup.rb
+++ Formula/pup.rb
@@ -3,6 +3,7 @@ class Pup < Formula
   homepage "https://github.com/EricChiang/pup"
   url "https://github.com/ericchiang/pup/archive/v0.4.0.tar.gz"
   sha256 "0d546ab78588e07e1601007772d83795495aa329b19bd1c3cde589ddb1c538b0"
+  revision 1 unless OS.mac?
   head "https://github.com/EricChiang/pup.git"
 
   bottle do
@@ -13,7 +14,6 @@ class Pup < Formula
     sha256 "f470de75187b994ef9612c5404dc7622a356c8ee6af21f6b2549b5d7c5d88d32" => :high_sierra
     sha256 "4ba84cffa7cfd01bd252223055abdf5fd8b6cfc27474131cf313e688ea8eeecf" => :sierra
     sha256 "a1aa49640871c127c76f4aea6db65487db964a055e2aa4d86ee2d8b7f5dcb561" => :el_capitan
-    sha256 "cd3466638887295c61ccfb54a926672849f669baffbf8b20bf1bbefba346d065" => :x86_64_linux
   end
 
   depends_on "go" => :build
@@ -23,10 +23,11 @@ class Pup < Formula
     ENV["GOPATH"] = buildpath
     dir = buildpath/"src/github.com/ericchiang/pup"
     dir.install buildpath.children
+    os = OS.mac? ? "darwin" : "linux"
 
     cd dir do
-      system "gox", "-arch", "amd64", "-os", "darwin", "./..."
-      bin.install "pup_darwin_amd64" => "pup"
+      system "gox", "-arch", "amd64", "-os", os, "./..."
+      bin.install "pup_#{os}_amd64" => "pup"
     end
 
     prefix.install_metafiles dir
