16246e37f50ae73ec4d12a6cb024800ddc71dc60
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-13 08:51:09 +0000

    whistle: update 2.5.0 bottle.

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -11,6 +11,7 @@ class Whistle < Formula
     sha256 "09282bd5c79b1749c6fcee85f30d3e0bf189d6188e6da1005516978142ecffbf" => :catalina
     sha256 "c84424b92cd4145a2f3171f4210d88cb8070f9e5ff9d105a33531c9df40c6bbb" => :mojave
     sha256 "d59a9791b30474c6b5757e69f2e71f88eab3d70cc7414c8c82af823551fdda39" => :high_sierra
+    sha256 "dfdb5ff92e52dc574bbc0c2f0a608872e26bab3d0484d757599d4a0df24edd6d" => :x86_64_linux
   end
 
   depends_on "node"
