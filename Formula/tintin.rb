227e61437da766879c724c758f48dcc75550d7ce
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-14 08:47:21 +0000

    tintin: update 2.02.02 bottle.

--- Formula/tintin.rb
+++ Formula/tintin.rb
@@ -9,6 +9,7 @@ class Tintin < Formula
     sha256 "895be37bb90b25cdc58591aa166bd464538715c3104901c7159d3c58906a485b" => :catalina
     sha256 "ecdd874018dd20cd4d8807a8c2adc5a8955622f8c97277a3a6fdc79e3d8a4198" => :mojave
     sha256 "29c68a3db90dee5cef629f629629d50469f2dd0ef9426b4bd8aaa374d2b6334e" => :high_sierra
+    sha256 "3a465960b2df69791d1d202dd18c6ba1a6141d0f2a90c1600bdcc0a7701bbab5" => :x86_64_linux
   end
 
   depends_on "gnutls"
