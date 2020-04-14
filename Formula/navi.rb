4223c9c77c924353a4c8fca57546d88b48cde38a
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-14 08:49:21 +0000

    navi: update 2.5.1 bottle.

--- Formula/navi.rb
+++ Formula/navi.rb
@@ -9,6 +9,7 @@ class Navi < Formula
     sha256 "a1582261e962b8935dd9cb531a20f1b07913ee5216e38d42f275edeb75e0a1c1" => :catalina
     sha256 "b72d0751b8c3086a751e9fcb8a9eb2ddc352d84dfc6b5244d20d3c0acf9c455d" => :mojave
     sha256 "6c3afa49b15f8a8e15bbaba15ebab0a683cd270c97121d7639ddf86fa43d6d3a" => :high_sierra
+    sha256 "80e6b717fb42a71b729a1890859407b436c7b7de93a9728d7d1fc7684ac96af0" => :x86_64_linux
   end
 
   depends_on "rust" => :build
