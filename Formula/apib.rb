0d69ed20c3df452126b3ebc203c9dfbad2d82cb3
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-17 08:58:18 +0000

    apib: update 1.2.1 bottle.

--- Formula/apib.rb
+++ Formula/apib.rb
@@ -10,6 +10,7 @@ class Apib < Formula
     sha256 "b925a48cd3a9047184b5373e4718e4142117a487467cea95be9f43f6c9951712" => :catalina
     sha256 "fd1f74b58c7a51240d463c8ec1203bce33677409aca81ba0ffba9ea718471433" => :mojave
     sha256 "aee416aff5715b96b81cfeda0be036a2510ffe760187e396c143a64bae2c25c2" => :high_sierra
+    sha256 "68841cdea73b58feb482c92e38dcbdb88fd23279c487ee4c07720fca05ec9381" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
