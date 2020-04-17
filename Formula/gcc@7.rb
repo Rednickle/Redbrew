872ed82d017f2b5ef84c836e74d37bde2b33d891
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-17 10:09:31 +0000

    gcc@7: update 7.5.0_2 bottle.

--- Formula/gcc@7.rb
+++ Formula/gcc@7.rb
@@ -13,6 +13,7 @@ class GccAT7 < Formula
     sha256 "4dca3b07173bffba262e003b345970119626a4d60a25c167d6bc216c46f1d83e" => :catalina
     sha256 "5d4086421866078dc4d9bfe623a38160dbcf04ff88b4d0284dee9da66ff50f4c" => :mojave
     sha256 "3c2135fc0b1c7f0ce048c1f610d2131a237911bdbd66d8bff731bbf9c6d84bcc" => :high_sierra
+    sha256 "6437a3ac47e16db165b34c81eb9baffd9615821ba62cf038db14a4bb64da9183" => :x86_64_linux
   end
 
   # The bottles are built on systems with the CLT installed, and do not work
