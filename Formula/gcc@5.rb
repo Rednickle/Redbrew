8c75a0a9087bb7f3fd61afab8f4622793ad468f5
Michka Popoff <michkapopoff@gmail.com>
2020-04-18 19:06:40 +0200

    gcc*: remove options

--- Formula/gcc@5.rb
+++ Formula/gcc@5.rb
@@ -36,7 +36,7 @@ class GccAT5 < Formula
     depends_on "isl@0.18"
     depends_on "zlib"
     depends_on "binutils" if build.with? "glibc"
-    depends_on "glibc" => (Formula["glibc"].installed? || OS::Linux::Glibc.system_version < Formula["glibc"].version) ? :recommended : :optional
+    depends_on "glibc" if (Formula["glibc"].installed? || OS::Linux::Glibc.system_version < Formula["glibc"].version)
   end
 
   # GCC bootstraps itself, so it is OK to have an incompatible C++ stdlib
