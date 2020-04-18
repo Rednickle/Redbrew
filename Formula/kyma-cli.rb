784addfb3362bf36729d79aba18a42e39c7b2842
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-18 09:55:30 +0000

    kyma-cli: update 1.11.1 bottle.

--- Formula/kyma-cli.rb
+++ Formula/kyma-cli.rb
@@ -11,6 +11,7 @@ class KymaCli < Formula
     sha256 "7acf496e7cd150936a9f90324f080c01c8e4d93434e7c7304cf0ce1e042cf088" => :catalina
     sha256 "686006e04837f0a776517d14f35939cc3176dff2040e625f116c0deab0c0c082" => :mojave
     sha256 "67fbd741876cdb523d4e4ecaaa6964d63bc9d54f67f09b82bd3791bf3ad39f4c" => :high_sierra
+    sha256 "1a9320353e0d3833c324283c9a93fade1f4124aa1e83fd67f1df972664ca05a7" => :x86_64_linux
   end
 
   depends_on "go" => :build
