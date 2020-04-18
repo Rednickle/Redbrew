b70d3ad149172f51c4db9a73e8103291059eae74
Michka Popoff <michkapopoff@gmail.com>
2020-04-18 19:03:35 +0200

    isl@0.18: fix style for Linuxbrew

--- Formula/isl@0.18.rb
+++ Formula/isl@0.18.rb
@@ -8,7 +8,7 @@ class IslAT018 < Formula
   # result in isl_version() function returning "UNKNOWN" and hence break
   # package detection.
   url "http://isl.gforge.inria.fr/isl-0.18.tar.xz"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/i/isl/isl_0.18.orig.tar.xz"
+  mirror "https://deb.debian.org/debian/pool/main/i/isl/isl_0.18.orig.tar.xz"
   sha256 "0f35051cc030b87c673ac1f187de40e386a1482a0cfdf2c552dd6031b307ddc4"
 
   bottle do
