53dbe780e460b14063c80ad7c142e262cfd070eb
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-18 20:54:10 +0000

    terragrunt: update 0.23.10 bottle.

--- Formula/terragrunt.rb
+++ Formula/terragrunt.rb
@@ -10,6 +10,7 @@ class Terragrunt < Formula
     sha256 "d6924802f5cdfd17feae2b561ab9d9dfe72bb6d7cae2083a3b57bd165f2f41d3" => :catalina
     sha256 "146254b4920a19e8bf71b7e50dfcea9c3d49af69ccd81045fd1425bfc8cdf6a0" => :mojave
     sha256 "55281793d4e0871ee1a31c37d88be02f3e50888b7fa762ee17a568d9c689afda" => :high_sierra
+    sha256 "44cbc34d87e5b6258c4809e327984dab43636741431022803f9b7c520b979a47" => :x86_64_linux
   end
 
   depends_on "go" => :build
