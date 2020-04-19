db95d79856eca1945fcdfaa7031d865550fe72a2
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-04-19 09:51:13 +0000

    triangle: update 1.0.5 bottle.

--- Formula/triangle.rb
+++ Formula/triangle.rb
@@ -9,6 +9,7 @@ class Triangle < Formula
     sha256 "4fb744b3878eb0a3b661b939155929dd3e24050b6bdf79a21926309b9e37c030" => :catalina
     sha256 "e9028dc0560174e50099fdb6efc3dcbb8ed75a8cfc983e2b923a18f54acb1807" => :mojave
     sha256 "aca7f95503e929a3bc8b585e64177dec4d8625bd3d365b18d8fecc8fa463a859" => :high_sierra
+    sha256 "48ae218b4ecdf86ebe47e77d1d88661675a466bf98e7521b85373297d5504525" => :x86_64_linux
   end
 
   depends_on "go" => :build
